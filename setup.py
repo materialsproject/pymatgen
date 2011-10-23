83d50e02a67344c1e7abb82d1b79845589726285
Shyue Ping Ong <shyue@mit.edu>
2011-10-23 08:33:25 -0400

    Update setup.py.
    Former-commit-id: 9d12617170982fc1b6b01d109d986f5cd45e0552

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup (
 
   # Declare your packages' dependencies here, for eg:
 
-  install_requires = ['numpy','matplotlib','pymongo','PyCIFRW','psycopg2'],
-
+  install_requires = ['numpy','scipy','matplotlib','PyCIFRW'],
 
   author = 'Shyue Ping Ong, Anubhav Jain, Michael Kocher, Dan Gunter',
   author_email = 'shyue@mit.edu, anubhavj@mit.edu, mpkocher@lbnl.gov, dkgunter@lbl.gov',
@@ -16,7 +15,7 @@ setup (
   summary = 'The Materials Project Python Library',
   url = 'www.materialsproject.org',
   license = '',
-  long_description= 'pymatgen is a Python library for the Materials Project. It includes core structure definition and utilities, electronic structure objects, database access APIs, and convenient IO from VASP and CIF files.',
+  long_description= 'pymatgen is a Python library for the Materials Project. It includes core structure definition and utilities, electronic structure objects, and convenient IO from VASP and CIF files.',
 
   # could also include long_description, download_url, classifiers, etc.
 )
