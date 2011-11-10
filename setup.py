65ccbe5c10f2c1b54e1736caf7d8d88c7345610f
Shyue Ping Ong <shyue@mit.edu>
2011-11-10 10:49:13 -0500

    Update to v1.0.3 given all the bug fixes.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup (
   name = 'pymatgen',
-  version = '1.0.2',
+  version = '1.0.3',
   packages = find_packages(),
 
   # Declare your packages' dependencies here, for eg:
