f4dce50fa09f900eed4b19044cf8636e5c6d98f8
Shyue Ping Ong <shyue@mit.edu>
2011-11-10 22:58:36 -0500

    Slightly modify unittest for structure.
    Former-commit-id: ec843fbda756274e6a42b31ee6492d8bdbbf8aa2 [formerly fee8d31999ec4721a416c34af048de402affd971]
    Former-commit-id: c17029832f0c85654b36e34e719ba4d7355bb042

--- pymatgen/core/tests/test_structure.py
+++ pymatgen/core/tests/test_structure.py
@@ -143,10 +143,9 @@ class StructureTest(unittest.TestCase):
         struct = Structure(self.lattice,[{si:0.5, mn:0.5},{si:0.5}],coords)
         self.assertIn("lattice", struct.to_dict)
         self.assertIn("sites", struct.to_dict)
-        
+    
+    def test_from_dict(self):
         test_dict = {'lattice': {'a': 3.8401979336999998, 'volume': 40.044794644251596, 'c': 3.8401979337177736, 'b': 3.8401989943442438, 'matrix': [[3.8401979337, 0.0, 0.0], [1.9200989668, 3.3257101909, 0.0], [0.0, -2.2171384943, 3.1355090603]], 'alpha': 119.99999086398419, 'beta': 90.0, 'gamma': 60.000009137322195}, 'sites': [{'occu': 0.5, 'abc': [0, 0, 0], 'xyz': [0.0, 0.0, 0.0], 'species': [{'occu': 0.5, 'element': 'Mn'}, {'occu': 0.5, 'oxidation_state': 4, 'element': 'Si'}], 'label': 'Mn: 0.5000, Si4+: 0.5000'}, {'occu': 0.5, 'abc': [0.75, 0.5, 0.75], 'xyz': [3.8401979336749994, 1.2247250003039056e-06, 2.3516317952249999], 'species': [{'occu': 0.5, 'oxidation_state': 4, 'element': 'Ge'}], 'label': 'Ge4+: 0.5000'}]}
-
-        struct.to_dict
         s = Structure.from_dict(test_dict)
         self.assertEqual(s.composition.formula, 'Mn0.5 Si0.5 Ge0.5')
 
