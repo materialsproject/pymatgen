2252ef0d662a22087f6798c8cd10e97e559c3d5a
Shyue Ping Ong <shyue@mit.edu>
2011-11-11 21:28:12 -0500

    Add a bunch of additional fields to setup.py, including PyYAML dependency.
    Former-commit-id: 6ffeaadb4b023a5a67f558a6e17fac15330c5bee [formerly 3e19b8bb8aab6618cb0aed81e1e77ca9a33420ed]
    Former-commit-id: f7e9073d27d83ce7974b96a6b19b3f4c577e1527

--- setup.py
+++ setup.py
@@ -1,21 +1,30 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 setup (
   name = 'pymatgen',
   version = '1.0.3',
   packages = find_packages(),
 
-  # Declare your packages' dependencies here, for eg:
-
-  install_requires = ['numpy','scipy','matplotlib','PyCIFRW'],
+  install_requires = ['numpy', 'scipy', 'matplotlib', 'PyCIFRW', 'PyYAML'],
 
   author = 'Shyue Ping Ong, Anubhav Jain, Michael Kocher, Dan Gunter',
   author_email = 'shyue@mit.edu, anubhavj@mit.edu, mpkocher@lbnl.gov, dkgunter@lbl.gov',
-
+  maintainer = 'Shyue Ping Ong',
   summary = 'The Materials Project Python Library',
   url = 'www.materialsproject.org',
-  license = '',
-  long_description= 'pymatgen is a Python library for the Materials Project. It includes core structure definition and utilities, electronic structure objects, and convenient IO from VASP and CIF files.',
-
-  # could also include long_description, download_url, classifiers, etc.
+  license = 'MIT',
+  long_description = 'pymatgen is a Python library for the Materials Project. It includes core structure definition and utilities, electronic structure objects, and convenient IO from VASP and CIF files.',
+  keywords = ["vasp", "materials", "project", "electronic", "structure"],
+  classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7.2",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Scientists, Developers",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Science :: Materials Science",
+  ],
+  download_url = "http://github.com/downloads/CederGroupMIT/pymatgen_repo/pymatgen_1.0.3.tar.gz",
 )
+
