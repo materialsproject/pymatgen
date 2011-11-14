9ca3e44884aa15192ab3afc8eb7032436f6e3a29
Shyue Ping Ong <shyue@mit.edu>
2011-11-13 20:35:36 -0500

    Minor bug fix for INCAR parsing to recognize types better. Add help to diff_incar

--- scripts/diff_incar.py
+++ scripts/diff_incar.py
@@ -13,13 +13,27 @@ __date__ = "Nov 12, 2011"
 
 import sys
 import itertools
+import argparse
+
 from pymatgen.io.vaspio import Incar
 from pymatgen.util.string_utils import str_aligned
 
-filepath1 = sys.argv[1]
-filepath2 = sys.argv[2]
+
+parser = argparse.ArgumentParser(description='''Convenient INCAR diff. 
+Author: Shyue Ping Ong
+Version: 1.0
+Last updated: Oct 26 2011''')
+parser.add_argument('incar_file', metavar='filename', type=str, nargs = 2, help='files to process')
+
+args = parser.parse_args()
+
+filepath1 = args.incar_file[0]
+filepath2 = args.incar_file[1]
 incar1 = Incar.from_file(filepath1)
-incar2 = Incar.from_file(filepath2)        
+incar2 = Incar.from_file(filepath2)
+
+print type(incar1['EDIFF'])
+print type(incar2['EDIFF'])
 
 def format_lists(v):
     if isinstance(v, (tuple, list)):
