28a25d1c1442b8802c56cccdf604c1cdf4a77fc5
Shyue Ping Ong <shyue@mit.edu>
2011-11-15 13:00:15 -0500

    Update setup.py to v1.0.5. Change data files definitions.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup (
   name = 'pymatgen',
-  version = '1.0.4',
+  version = '1.0.5',
   packages = find_packages(),
   install_requires = ['numpy', 'scipy', 'matplotlib', 'PyCIFRW', 'PyYAML'],
 
-  data_files=[('data', ['pymatgen/core/periodic_table.yaml']),
+  data_files=[('data', ['pymatgen/core/periodic_table.json']),
               ('config', ['pymatgen/io/VaspParameterSets.cfg'])],
   author = 'Shyue Ping Ong, Anubhav Jain, Michael Kocher, Dan Gunter',
   author_email = 'shyue@mit.edu, anubhavj@mit.edu, mpkocher@lbnl.gov, dkgunter@lbl.gov',
