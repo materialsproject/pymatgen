ed34d569df776ec32dc12d646745875e5d333eb5
Shyue Ping Ong <shyue@mit.edu>
2011-11-24 14:25:15 -0500

    Update version number to 1.1.1 to take into account bug fixes and KPOINTS capabilities.
    Former-commit-id: cef5e33abb100c644ae6613c166bed9574784366 [formerly 89d4c5b75323b9312432f6b7a387b9935ae66320]
    Former-commit-id: 2955455b18929e4e3ddaf4f122a11d8751fafef0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup (
   name = 'pymatgen',
-  version = '1.1.0',
+  version = '1.1.1',
   packages = find_packages(),
   install_requires = ['numpy', 'scipy', 'matplotlib', 'PyCIFRW'],
 
