b2f7c369bf34796bada591c24faaa4e44764209b
Shyue Ping Ong <shyue@mit.edu>
2011-11-30 13:49:03 -0500

    Update version number to 1.1.2 to take into account bug fix for POSCAR generator.
    Former-commit-id: aefff02358d16148ca1ffe08d1869c40bcc452c1 [formerly 17c2c4a6c2886a957f2809b3a9f911aebb2d319f]
    Former-commit-id: fdf8f4084e89e844b2d0d5c202fff5b06c4e2ea7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup (
   name = 'pymatgen',
-  version = '1.1.1',
+  version = '1.1.2',
   packages = find_packages(),
   install_requires = ['numpy', 'scipy', 'matplotlib', 'PyCIFRW'],
 
