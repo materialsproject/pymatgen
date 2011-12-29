8fa9d3ef87cda3c81c836c8a895ddf1e2340d233
William Richards <wrichard@mit.edu>
2011-12-29 18:08:10 -0500

    Added EwaldSumMatrix - calculate ewald energy as matrix of pairwise interactions

--- pymatgen/analysis/ewald.py
+++ pymatgen/analysis/ewald.py
@@ -37,6 +37,8 @@ class EwaldSummation:
     Calculates the electrostatic energy of a periodic array of charges using the Ewald technique. 
     References : http://www.ee.duke.edu/~ayt/ewaldpaper/ewaldpaper.html
     
+    This matrix can be used to do fast calculations of ewald sums after species removal
+    
     E = E_recip + E_real + E_point
     
     Atomic units used in the code, then converted to eV.
@@ -192,3 +194,159 @@ class EwaldSummation:
         output.append("Total = " + str(self.total_energy))
         output.append("Forces:\n" + str(self.forces))
         return "\n".join(output)
+    
+    
+class EwaldSumMatrix:
+    """
+    Calculates the electrostatic energy of a periodic array of charges using the Ewald technique.
+    Returns the energies as a matrix of pairwise interactions (energy due to site i is
+    obtained by summing the cells in row i and column i, remembering not to double count cell i,i)
+    Doesn't do forces
+    References : http://www.ee.duke.edu/~ayt/ewaldpaper/ewaldpaper.html
+                 http://micro.stanford.edu/mediawiki/images/4/46/Ewald_notes.pdf
+    
+    E = E_recip + E_real + E_point
+    
+    Atomic units used in the code, then converted to eV.
+    """
+
+    # taken from convasp. converts unit of q*q/r into eV 
+    CONV_FACT = 1e10 * sc.e / (4 * pi * sc.epsilon_0)
+    
+    def __init__(self, structure, real_space_cut = -1.0, recip_space_cut = -1.0, eta = -1.0, acc_factor = 8.0):
+        """
+        Initializes and calculates the Ewald sum. Default convergence parameters have been
+        specified, but you can override them if you wish.
+        
+        Args:
+            structure: input structure that must have proper Specie on all sites, i.e.
+                       Element with oxidation state. Use OxidationStateDecorator in 
+                       pymatgen.core.structure_modifier for example.
+            real_space_cut: Real space cutoff radius dictating how many terms are used in the 
+                            real space sum. negative means determine automagically using the
+                            formula given in gulp 3.1 documentation.
+            recip_space_cut: Reciprocal space cutoff radius. negative means determine automagically using
+                             formula given in gulp 3.1 documentation.
+            eta: The screening parameter. negative means determine automatically.
+                 calculate_forces: Set to true if forces are desired
+            acc_factor: No. of significant figures each sum is converged to. See the gulp manual. 
+        """
+        self._s = structure
+        self._vol = structure.volume
+        
+        self._acc_factor = acc_factor
+
+        # set screening length
+        self._eta = eta if eta > 0 else (len(structure) * 0.01 / self._vol) ** (1/3) * pi        
+        self._sqrt_eta = sqrt(self._eta)
+        
+        # acc factor used to automatically determine the optimal real and 
+        # reciprocal space cutoff radii
+        self._accf = sqrt(log(10 ** acc_factor))  
+        
+        self._rmax = real_space_cut if real_space_cut  > 0 else self._accf/self._sqrt_eta 
+        self._gmax = recip_space_cut if recip_space_cut > 0 else 2* self._sqrt_eta * self._accf
+        self._oxi_states = [compute_average_oxidation_state(site) for site in structure]
+        self._total_q = sum(self._oxi_states)        
+        self._forces = np.zeros((len(structure),3))
+        self._calc_recip()
+        self._calc_real_and_point()
+        
+    @property
+    def reciprocal_space_energy(self):
+        return self._recip
+    
+    @property
+    def real_space_energy(self):
+        return self._real
+    
+    @property
+    def point_energy(self):
+        return self._point
+    
+    @property
+    def total_energy(self):
+        totalenergy = self._recip + self._real
+        for i in range(len(self._point)):
+            totalenergy[i][i] += self._point[i]
+        return totalenergy
+    
+    @property
+    def forces(self):
+        return self._forces
+    
+    def _calc_recip(self):
+        """
+        Perform the reciprocal space summation. Calculates the quantity
+        E_recip = 1/(2PiV) sum_{G < Gmax} exp(-(G.G/4/eta))/(G.G) S(G)S(-G) where
+        S(G) = sum_{k=1,N} q_k exp(-i G.r_k)
+        S(G)S(-G) = |S(G)|**2
+        """
+        prefactor = 2 * pi / self._vol
+        erecip = np.zeros( (self._s.num_sites,self._s.num_sites) )
+                
+        recip = Structure(self._s.lattice.reciprocal_lattice, ["H"], [np.array([0,0,0])])
+        nn = recip.get_neighbors(recip[0], self._gmax)
+        for (n,dist) in nn:
+            gvect = n.coords
+            gsquare = np.linalg.norm(gvect) ** 2
+            expval = exp(-1.0 * gsquare / (4.0 * self._eta))
+                        
+            #calculate the structure factor
+            sfactor = np.zeros( (self._s.num_sites,self._s.num_sites) )
+            for i in range(self._s.num_sites):
+                sitei = self._s[i]
+                qi = self._oxi_states[i]
+                for j in range(self._s.num_sites):
+                    sitej = self._s[j]
+                    qj = self._oxi_states[j]
+                    exparg = np.dot(gvect, sitei.coords-sitej.coords) 
+                    sfactor[i][j] = qi * qj * (cos(exparg) + sin(exparg))
+                    
+            erecip += expval/gsquare*sfactor
+        
+        self._recip = erecip * prefactor * EwaldSumMatrix.CONV_FACT 
+    
+
+    def _calc_real_and_point(self):
+        """
+         * determines the self energy
+         * 
+         * -(eta/pi)**(1/2) * sum_{i=1}^{N} q_i**2
+         * 
+         * a compensating background is added (i.e. a G=0 term) for all sites
+         * (if the cell is charge neutral these sum to 0)
+         *  
+         * @return
+         */
+        """
+        all_nn = self._s.get_all_neighbors(self._rmax, True)
+        
+        ereal = np.zeros( (self._s.num_sites,self._s.num_sites) )
+        epoint = np.zeros( (self._s.num_sites) )
+        
+        for i in range(self._s.num_sites):
+            nn = all_nn[i] #self._s.get_neighbors(site, self._rmax)
+            qi = self._oxi_states[i]
+            epoint[i] = qi*qi
+            epoint[i] *= -1.0 * sqrt(self._eta/pi)
+            epoint[i] += qi * pi / (2.0 * self._vol * self._eta)   #add jellium term
+            for j in range(len(nn)):  #for (nsite, rij)  in nn:
+                qj = compute_average_oxidation_state(nn[j][0])  #nsite 
+                erfcval = erfc(self._sqrt_eta*nn[j][1])
+                ereal[nn[j][2]][i] += erfcval * qi * qj / nn[j][1]
+                    
+        self._real = ereal * 0.5 * EwaldSumMatrix.CONV_FACT
+        self._point = epoint * EwaldSumMatrix.CONV_FACT 
+    
+    @property
+    def eta(self):
+        return self._eta
+    
+    def __str__(self):
+        output = ["Real = " + str(sum(sum(self.real_space_energy)))]
+        output.append("Reciprocal = " + str(sum(sum(self.reciprocal_space_energy))))
+        output.append("Point = " + str(sum(self.point_energy)))
+        output.append("Total = " + str(sum(sum(self.total_energy))))
+        return "\n".join(output)
+    
--- pymatgen/analysis/tests/test_ewald.py
+++ pymatgen/analysis/tests/test_ewald.py
@@ -3,8 +3,9 @@ import os
 
 import pymatgen.io.vaspio 
 from pymatgen.core.structure_modifier import OxidationStateDecorator
-from pymatgen.analysis.ewald import EwaldSummation
+from pymatgen.analysis.ewald import EwaldSummation, EwaldSumMatrix
 from pymatgen.io.vaspio import Poscar
+import numpy as np
 
 class EwaldSummationTest(unittest.TestCase):
 
@@ -26,3 +27,40 @@ class EwaldSummationTest(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+    
+class EwaldSumMatrixTest(unittest.TestCase):
+
+    def test_init(self):
+        module_path = os.path.dirname(pymatgen.io.vaspio.__file__)
+        filepath = os.path.join(module_path, 'tests','vasp_testfiles', 'POSCAR')
+        p = Poscar.from_file(filepath)
+        s = p.struct
+
+        modifier = OxidationStateDecorator(s,{"Li":1, "Fe":2, "P":5, "O":-2})
+        s = modifier.modified_structure
+        ham = EwaldSumMatrix(s)
+        self.assertAlmostEqual(sum(sum(ham.real_space_energy)), -354.91294268, 4, "Real space energy incorrect!")
+        self.assertAlmostEqual(sum(sum(ham.reciprocal_space_energy)), 25.475754801, 4, "Reciprocal space energy incorrect!")
+        self.assertAlmostEqual(sum(ham.point_energy), -790.463835033, 4, "Point space energy incorrect!")
+        self.assertAlmostEqual(sum(sum(ham.total_energy)), -1119.90102291, 2, "Total space energy incorrect!")
+        
+        
+        #remove matrix elements due to P
+        premovedenergy = ham.total_energy
+        for i in [7,6,5,4]:     
+            premovedenergy = np.delete(premovedenergy,i,0)
+            premovedenergy = np.delete(premovedenergy,i,1)
+        premovedenergy = sum(sum(premovedenergy))
+        
+        #remove charge of P and recalculate ewald sum
+        modifier = OxidationStateDecorator(s,{"Li":1, "Fe":2, "P":0, "O":-2})
+        s = modifier.modified_structure
+        ham = EwaldSumMatrix(s)
+        
+        self.assertAlmostEqual(sum(sum(ham.total_energy)), premovedenergy, 2, "Total space energy incorrect!")
+        
+
+if __name__ == "__main__":
+    unittest.main()    
+    
+    
