74fc27ca0640f24a28b405b85615a4a451980396
Shyue Ping Ong <shyue@mit.edu>
2012-01-22 11:54:08 -0500

    Add alias for setup. Update minor version number to 1.2.1 to take into account new improvements in Ewaldsum.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = open(README).read() + '\n\n'
 
 setup (
   name = 'pymatgen',
-  version = '1.2.0',
+  version = '1.2.1',
   packages = find_packages(),
   install_requires = ['numpy', 'scipy', 'matplotlib', 'PyCIFRW'],
 
