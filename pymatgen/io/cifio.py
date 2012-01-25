60d56047564acfa8639bbef87d4ec17c8cdc3a31
Shyue Ping Ong <shyue@mit.edu>
2012-01-25 14:35:10 -0500

    Bug fix for Cif parser when sym data is missing.
    Former-commit-id: 686e9ba557881ead628799d0a91c6f42f51cba02 [formerly 6ed7d27b32f86ad5532eb17b817ffee0f644e7aa]
    Former-commit-id: 4017ba92d6e88ce595b6ab815af17b7a67102ca3

--- pymatgen/io/cifio.py
+++ pymatgen/io/cifio.py
@@ -89,13 +89,12 @@ class CifParser:
                 sympos =  data['_symmetry_equiv_pos_as_xyz_']
             except:
                 warnings.warn("No _symmetry_equiv_pos_as_xyz type key found. Defaulting to P1.")
-                sympos
+                sympos = ['x, y, z']
         def parse_symbol(sym):
             m = re.search("([A-Z][a-z]*)",sym)
             if m:
                 return m.group(1)
             return ''
-        
         #oxi_states = None
         try:
             oxi_states = dict()
