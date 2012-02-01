ee03bea953eb392befb44775ec104594d0b97265
Maarten de Jong <MDEJONG1@fenrir.bw01.berkeley.edu>
2012-01-31 22:10:18 -0800

    Expanded class DeformGeometry; use existing code for generation of pymatgen core struct files from CIF, pushed some code to StructureEditor, added method append_stress_tensors, added method fit_cij and added function fit_elas.py

--- pymatgen/phonons/genstrain.py
+++ pymatgen/phonons/genstrain.py
@@ -1,7 +1,9 @@
 from __future__ import division
+import warnings
+import sys
+#sys.path.append('/home/MDEJONG1/pythonplayground/pymatgen/pymatgen_repo/pymatgen_repo') # (If one does not want to change $PYTHONPATH)
 import unittest
 import pymatgen
-import sys
 from pymatgen.io.vaspio import Poscar
 from pymatgen.io.vaspio import Vasprun
 from pymatgen.io.cifio import CifWriter
@@ -12,6 +14,7 @@ from pymatgen.transformations.standard_transformations import *
 from pymatgen.core.structure_modifier import StructureEditor
 import numpy as np
 import os
+import fit_elas
 
 __author__="Maarten de Jong"
 __copyright__ = "Copyright 2011, The Materials Project"
@@ -21,6 +24,9 @@ __maintainer__ = "Maarten de Jong"
 __email__ = "maartendft@gmail.com"
 __status__ = "Development"
 __date__ ="Jan 24, 2012"
+
+np.set_printoptions(precision=3)			# pretty printing of numpy matrices
+np.set_printoptions(suppress=True)			# pretty printing of numpy matrices
 
 class DeformGeometry(object):
 	"""
@@ -36,9 +42,8 @@ class DeformGeometry(object):
 		
 		Args: 
 			path_to_cif - string with full path to CIF-file, e.g.
-			/home/MDEJONG1/pythonplayground/pymatgen/classes/7048.cif
+			'/home/MDEJONG1/pythonplayground/pymatgen/classes/7048.cif'
 		"""
-		
 		self._path = path_to_cif
 	
 	def CIF2struct(self):
@@ -48,81 +53,46 @@ class DeformGeometry(object):
 		Args: 
 			none
 		"""
-
-		coords = list()
-		species = []
 		myCIF = CifParser(self._path).get_structures()[0]
-#		print myCIF
-		s = StructureEditor(myCIF)
-# 		this works		
-# 		add code
-
-
-#		S2 = StructureEditor(myCIF).replace_species({'Al', 'O'})
-#		print S2	
-
-
-#		print type(myCIF)
-#		print myCIF.__dict__.keys()
-#		print myCIF._lattice.__dict__.keys()
-#		print myCIF._lattice._mc2d
-#		print myCIF._lattice._md2c
-#		print myCIF._lattice._matrix
-
-		
-
-#		check this part, cifio-code does this already!
-
-#		print myCIF
-
-
-		for k in range(len(myCIF._sites)):
-			coords.append(Lattice(np.linalg.inv(myCIF._lattice._mc2d)).get_fractional_coords(myCIF._sites[k]._coords))
-			species.append(myCIF._sites[k]._species.keys()[0])
-
-		lattice = Lattice(myCIF._lattice._matrix)
-		struct = Structure(lattice,species,coords)
-
-		
-
-#		print struct
-
-		self.base_struct = struct
-#		self._coords = coords
-#		self._lattice = myCIF._lattice._matrix
-#		self._species = species
+		self.base_struct = myCIF
 
 		return self.base_struct
 
-	def get_residual_stress(self):
+	def get_residual_stress(self, residual_stress=np.zeros((3,3)), res_tresh_hold=2):
 		"""
-		Given a crystal structure, this method starts up a static calculation of the stress tensor. These
-		are residual stresses which need to be subtracted later on. 
-		
+	    Parse the residual stress tensor 
+
 		Args:
-			none
-		"""
+			residual_stress - 3x3 numpy matrix containing residual stresses, 
+			defaults to zero stress tensor
+			res_tresh_hold - what residual stresses (in kB) are acceptable
+		"""		
+		self._residual_stress = residual_stress
 		
-		self._residual_stress = np.zeros((3,3))
-
-
+		if (np.abs(residual_stress)>res_tresh_hold).any() == True:	
+			warnings.warn('***Residual stresses are quite large, structural relaxation might not have converged...***')
+	
 		return self._residual_stress
 
-	def deform(self, nd=0.02, ns=0.02, m=6, n=6): # this class still requires some user input
+	def deform(self, nd=0.02, ns=0.02, m=6, n=6): # this class still requires some (optional) user input
 		"""
 		Take original geometry (from CIF-file) and apply a range of deformations. 
-		Default values generally work well for metals. However, one might need to
+		Default values generally work well for metals (in my experience). However, one might need to
 		make changes for material such as oxides.
+
+		Note: deformed structures are stored in a dict, named defstructures. Also stored are
+		deformation gradient tensor, strain tensor, and strain tensor-indices.
+		defstructures[0] contains first deformed structure and its properties, defstructures[1]
+		contains the second etc.
 
 		Args: 
 			nd - maximum amount of normal strain  
 			ns - maximum amount of shear strain
-			m - number of normal deformations used for fitting elastic constants per deformation mode, even integer required
-			n - number of shear deformations used for fitting elastic constants per deformation mode, even integer required
-		"""
-		
-		self.msteps = m
-		self.nsteps = n
+			m - number of normal deformations used for structural deformations, even integer required
+			n - number of shear deformations used for structural deformations, even integer required
+		"""	
+		self.msteps = np.int(m)
+		self.nsteps = np.int(n)
 
 		if m%2!=0:
 			raise ValueError("m has to be even.")
@@ -134,119 +104,179 @@ class DeformGeometry(object):
 
 		defs = np.linspace(-nd, nd, num=m+1)
 		defs = np.delete(defs, np.int(m/2), 0)
-		defstructures = []
+		sheardef = np.linspace(-ns, ns, num=n+1)
+		sheardef = np.delete(sheardef, np.int(n/2), 0)
+	
+		defstructures = dict()
 
 		counter = 0
 		# First apply non-shear deformations
-		# Depending on crystal symmetry, these may yield shear stresses, however
 		for i1 in range(0, 3):
 
 			for i2 in range(0, len(defs)):
-				
-				F = np.eye(3)
-				F[i1, i1] = F[i1, i1] + defs[i2]
-				C = np.transpose(F)*F
-				E = 0.5*(C-np.eye(3)) # Green-Lagrange strain tensor
-				Lp = self._lattice*F  # Deformation mapping of lattice vectors
-				mystrains[:, :, counter] = E
 
-				lat = Lattice(Lp)
-				struc = Structure(lat,self._species,self._coords)
-				defstructures.append(struc)
+				s = StructureEditor(self.base_struct)
+				F = np.identity(3)
+				F[i1, i1] = F[i1, i1] + defs[i2]
+				E = 0.5*(np.transpose(F)*F-np.eye(3))      # Green-Lagrange strain tensor
+				s.apply_strain_transformation(F)           # let deformation gradient tensor act on undistorted lattice
+				defstructures[counter] = [s.modified_structure, F, E, (i1, i1)]
 				counter += 1
 
 		# Now apply shear deformations #		
-		sheardef = np.linspace(-ns, ns, num=n+1)
-		sheardef = np.delete(sheardef, np.int(n/2), 0)
-
 		F_index = [[0, 1], [0, 2], [1, 2]]
 		for j1 in range(0, 3):
 		
 			for j2 in range(0, len(sheardef)):
 
-				F = np.zeros((3,3))
-				F = np.eye(3)
+				s = StructureEditor(self.base_struct)
+				F = np.identity(3)
 				F[F_index[j1][0], F_index[j1][1]] = F[F_index[j1][0], F_index[j1][1]] + sheardef[j2]
-				F = np.matrix(F)
-		
-				C = np.transpose(F)*F
-				E = 0.5*(C-np.eye(3)) # Green-Lagrange strain tensor
-				Lp = self._lattice*F  # Deformation mapping of lattice vectors
-				mystrains[:, :, counter] = E
-				
-				lat = Lattice(Lp)
-				struc = Structure(lat,self._species,self._coords)
-				defstructures.append(struc)
+				F = np.matrix(F)						   # Not sure why, but without this statement, calculation of strain tensor gets meesed up...
+				E = 0.5*(np.transpose(F)*F-np.eye(3))      # Green-Lagrange strain tensor
+				s.apply_strain_transformation(F)           # let deformation gradient tensor act on undistorted lattice
+				defstructures[counter] = [s.modified_structure, F, E, (F_index[j1][0], F_index[j1][1])]
 				counter += 1
 		
 		self.defstructures = defstructures
-		self.mystrains = mystrains
 
-		return self.defstructures, self.mystrains
+		return self.defstructures
 
-
-	def get_stress_tensors(self, path_list):
+	def append_stress_tensors(self, stress_tensor_dict):
 		"""
-		This method takes a list of paths, which contain output files (currently only vasprun.xml's are supported)
-		containing the stress tensor, and stores these in a 3D-matrix.
-		
+		After the ab initio engine is run, call this method to append the stress tensors to the corresponding structures,
+		stored in defstructures.
+
 		Args:
-			path_list - list of paths that contain vasprun.xml's which contain calculated stress tensor for every
-			deformed geometry
-		"""
-
-		self.path_list = path_list
-
-		if np.int((self.msteps + self.nsteps)*3) != len(path_list):
-			raise ValueError("Number of stress tensors does not match number of strain tensors.")
-
-		stress_tensors = np.zeros((3, 3, np.int(self.msteps*3) + np.int(self.nsteps*3)))
-		
-		counter = 0
-		for stress_path in path_list:
+			stress_tensor_dict - a dict with  3x3 numpy matrices, containing the stresses. Key should be structure number,
+			value should be the computed stress tensor, corresponding to that specific structure.  
 			
-			A = Vasprun(stress_path)
-			stressmatrix = A.ionic_steps[-1]['stress']
-			stress_tensors[:, :, counter] = stressmatrix - self._residual_stress
-			counter += 1
-
-
-		self.stress_tensors = stress_tensors
-
-
-	def fit_cij(self, tol1=0.02, sym=False):
 		"""
-		Take stress and strain tensors and fit elastic constants. By default, the elastic tensor is not symmetrized.
+		if np.int(3*(self.msteps + self.nsteps)) != len(stress_tensor_dict):
+			raise ValueError("Number of stress tensors should match number of strain tensors.")
+		
+		for i in range(0, len(stress_tensor_dict)):
+			self.defstructures[i].append(stress_tensor_dict[i])
+
+		return self.defstructures
+
+	def fit_cij(self, tol1=1.0, sym=False, origin=False):
+		"""
+		 Use the strain tensors and computed stress tensors to fit the elastic constants.
+		 By default, the elastic tensor is not symmetrized. For every elastic constant, we
+		 choose the largest set of strains for fitting the stress-strain relationship that
+		 obeys the "linearity criterion".
 
 		Args:
-			tol1 - tolerance used for comparing linear and quadratic fits of elastic constants
+			tol1 - tolerance used for comparing linear parts of linear and quadratic fits of elastic constants
+			these may not differ by more than [tol1] GPa
 			sym - specifies whether or not the elastic tensor is symmetrized
+			origin - specifies whether or not the linear least squares-fit should 
+			be forced to pass through the origin (zero stress-zero strain point)
+		"""
+		self.tol1 = tol1
+
+		Cij = np.zeros((6,6))	# elastic tensor in Voigt notation
+		count = 0
+
+		for n1 in range(0,3):	# loop over normal modes
+		
+			eps_tot = []
+			sig_tot = []
+
+			for n2 in range(0, np.int(self.msteps/2)):	# loop over def. magnitudes
+
+				start_index = np.int(self.msteps/2) - n2 - 1
+				end_index = np.int(self.msteps/2) + n2 + 1
+
+				eps_array = []
+				sig_array = []
+				
+				for n3 in range(start_index, end_index):
+
+					n3 = np.int(n3 + n1*self.msteps)
+					eps_array.append(Q.defstructures[n3][2][Q.defstructures[n3][3]])                           
+					
+					sig_array.append([Q.defstructures[n3][4][0,0],Q.defstructures[n3][4][1,1],Q.defstructures[n3][4][2,2],  
+					Q.defstructures[n3][4][0,1], Q.defstructures[n3][4][0,2], Q.defstructures[n3][4][1,2]]) 
+				
+				eps_tot.append(eps_array)
+				sig_tot.append(sig_array)
+			
+			Cij_col = fit_elas.fit_elas(eps_tot, sig_tot, tol1)
+			
+			for Cijn in range(0,6):
+
+				Cij[Cijn, n1] = Cij_col[Cijn, 0]					# fill up elastic tensor
+
+		for n1 in range(3,6):   # loop over shear modes
+
+			eps_tot = []
+			sig_tot = []
+
+			for n2 in range(0, np.int(self.nsteps/2)):  # loop over def. magnitudes
+
+				start_index = np.int(self.nsteps/2) - n2 - 1
+				end_index = np.int(self.nsteps/2) + n2 + 1
+
+				eps_array = []
+				sig_array = []
+
+				for n3 in range(start_index, end_index):
+
+					n3 = np.int(n3 + n1*self.nsteps)
+					eps_array.append(Q.defstructures[n3][2][Q.defstructures[n3][3]])
+
+					sig_array.append([Q.defstructures[n3][4][0,0],Q.defstructures[n3][4][1,1],Q.defstructures[n3][4][2,2],
+					Q.defstructures[n3][4][0,1], Q.defstructures[n3][4][0,2], Q.defstructures[n3][4][1,2]])
+
+				eps_tot.append(eps_array)
+				sig_tot.append(sig_array)
+
+				Cij_col = fit_elas.fit_elas(eps_tot, sig_tot, tol1)
+
+				for Cijn in range(0,6):
+
+					Cij[Cijn, n1] = 0.5*Cij_col[Cijn, 0]			# factor 0.5 is required for shear modes only
+
+		self.Cij = Cij
+
+	def born_huang(self):
+		"""
+		This method checks the Born-Huang criterions for the mechanical stability of the crystal.
 		"""
 
+#		To be implemented
 
 
-Q = DeformGeometry('/home/MDEJONG1/pythonplayground/pymatgen/classes/7048.cif')
 
+
+#Q = DeformGeometry('/home/MDEJONG1/pythonplayground/pymatgen/classes/7048.cif')
+Q = DeformGeometry('/home/MDEJONG1/pythonplayground/pymatgen/pymatgen_repo/pymatgen_repo/pymatgen/phonons/aluminum.cif')
 
 ## Calling sequence ##
 Q.CIF2struct()
+Q.deform(0.015, 0.015, 6, 6)
+Q.get_residual_stress()
 
+stress_dict = dict()
 
+for i in range(0, 36):
 
-#Q.deform(0.02, 0.02, 4, 4)
-#Q.get_residual_stress()
+	A = Vasprun('/home/MDEJONG1/pythonplayground/pymatgen/pymatgen_repo/pymatgen_repo/pymatgen/phonons/test4/F'+str(i)+'/vasprun.xml')
+	stress_dict[i] = A.ionic_steps[-1]['stress']
 
-# Perform VASP-calculations in specified paths #
-#Q.get_stress_tensors(['/home/MDEJONG1/pythonplayground/pymatgen/classes/vasprun.xml']*24)
+Q.append_stress_tensors(stress_dict)
+Q.fit_cij()
 
+print Q.Cij
 
+#for c in range(0, len(Q.defstructures)):
+
+#   w = Poscar(Q.defstructures[c][0])
+#   w.write_file('POSCAR_' + str(c))
 
 #print Q.__dict__.keys()
-#print Q.defstructures
-#print Q.mystrains
-
-#A = Vasprun('vasprun.xml')
-#print A.ionic_steps[-1]['stress']
 
 
 
