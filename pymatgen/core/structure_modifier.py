1e44c4e2e5b406c52f9badd042626475409f00bb
Shyue Ping Ong <shyue@mit.edu>
2012-03-12 21:13:32 -0400

    Add a TranslateSitesTransformation with inverse and unittests. Add a convenience static constructor for SupercellTransformaion.
    Former-commit-id: bf3b6507ba56cfc034cab3faa44bfd839279689b [formerly 7fba2b50c24c4e1621a79d54d9d49cbbfafb7c14]
    Former-commit-id: f66663762bfdb4929384b3a0c64b02722eb3d04f

--- pymatgen/core/structure_modifier.py
+++ pymatgen/core/structure_modifier.py
@@ -6,13 +6,13 @@ This module provides classes used to modify structures.
 
 from __future__ import division
 
-__author__="Shyue Ping Ong"
+__author__ = "Shyue Ping Ong"
 __copyright__ = "Copyright 2011, The Materials Project"
 __version__ = "1.0"
 __maintainer__ = "Shyue Ping Ong"
 __email__ = "shyue@mit.edu"
 __status__ = "Production"
-__date__ ="Sep 23, 2011"
+__date__ = "Sep 23, 2011"
 
 import abc
 import itertools
@@ -28,27 +28,27 @@ class StructureModifier(object):
     Abstract class definition for all classes that modify structures.
     """
     __metaclass__ = abc.ABCMeta
-    
+
     @abc.abstractproperty
     def modified_structure(self):
         """
         Returns the modified structure.
         """
-        return 
-    
+        return
+
     @abc.abstractproperty
     def original_structure(self):
         '''
         Returns the original structure.
         '''
-        return 
-    
-class StructureEditor(StructureModifier):   
+        return
+
+class StructureEditor(StructureModifier):
     """
     Editor for adding, removing and changing sites from a structure
     """
     DISTANCE_TOLERANCE = 0.01
-    
+
     def __init__(self, structure):
         """
         Arguments:
@@ -58,7 +58,7 @@ class StructureEditor(StructureModifier):
         self._original_structure = structure
         self._lattice = structure.lattice
         self._sites = list(structure.sites)
-    
+
     def replace_species(self, species_mapping):
         """
         Swap species in a structure.
@@ -68,17 +68,17 @@ class StructureEditor(StructureModifier):
                 dict of species to swap. Species can be elements too.
                 e.g., {Element("Li"): Element("Na")} performs a Li for Na substitution.
         """
-        
+
         def mod_site(site):
             new_atom_occu = dict()
             for sp, amt in site.species_and_occu.items():
                 if sp in species_mapping:
                     new_atom_occu[species_mapping[sp]] = amt
                 else:
-                    new_atom_occu[sp] = amt 
-            return PeriodicSite(new_atom_occu, self._lattice.get_fractional_coords(site.coords), self._lattice)    
+                    new_atom_occu[sp] = amt
+            return PeriodicSite(new_atom_occu, self._lattice.get_fractional_coords(site.coords), self._lattice)
         self._sites = map(mod_site, self._sites)
-    
+
     def replace_single_site(self, index, species = None, atoms_n_occu = None):
         """
         Replace a single site. Takes either a species or a dict of occus
@@ -91,9 +91,9 @@ class StructureEditor(StructureModifier):
         if atoms_n_occu == None:
             atoms_n_occu = dict()
             atoms_n_occu[species] = 1
-        
+
         self._sites[index] = PeriodicSite(atoms_n_occu, self._lattice.get_fractional_coords(self._sites[index].coords), self._lattice)
-    
+
     def remove_species(self, species):
         """
         Remove all occurrences of a species from a structure.
@@ -101,15 +101,15 @@ class StructureEditor(StructureModifier):
         Args:
             species:
                 species to remove
-        """    
+        """
         new_sites = []
         for site in self._sites:
             new_sp_occu = {sp:amt for sp, amt in site.species_and_occu.items() if sp not in species}
             if len(new_sp_occu) > 0:
                 new_sites.append(PeriodicSite(new_sp_occu, site.frac_coords, self._lattice))
         self._sites = new_sites
-    
-    def append_site(self, species, coords, coords_are_cartesian = False, validate_proximity = True): 
+
+    def append_site(self, species, coords, coords_are_cartesian = False, validate_proximity = True):
         """
         Append a site to the structure at the end.
         
@@ -123,9 +123,9 @@ class StructureEditor(StructureModifier):
             validate_proximity:
                 Whether to check if inserted site is too close to an existing site. Defaults to True.
         
-        """       
+        """
         self.insert_site(len(self._sites), species, coords, coords_are_cartesian, validate_proximity)
-        
+
     def insert_site(self, i, species, coords, coords_are_cartesian = False, validate_proximity = True):
         """
         Insert a site to the structure.
@@ -143,17 +143,17 @@ class StructureEditor(StructureModifier):
                 Whether to check if inserted site is too close to an existing site. Defaults to True.
         """
         if not coords_are_cartesian:
-            new_site = PeriodicSite(species,coords,self._lattice)
+            new_site = PeriodicSite(species, coords, self._lattice)
         else:
             new_site = PeriodicSite(species, self._lattice.get_fractional_coords(coords), self._lattice)
-            
+
         if validate_proximity:
             for site in self._sites:
                 if site.distance(new_site) < self.DISTANCE_TOLERANCE:
                     raise ValueError("New site is too close to an existing site!")
-        
+
         self._sites.insert(i, new_site)
-        
+
     def delete_site(self, i):
         """
         Delete site at index i.
@@ -163,7 +163,7 @@ class StructureEditor(StructureModifier):
                 index of site to delete.
         """
         del(self._sites[i])
-        
+
     def delete_sites(self, indices):
         """
         Delete sites with at indices.
@@ -173,7 +173,7 @@ class StructureEditor(StructureModifier):
                 sequence of indices of sites to delete.
         """
         self._sites = [self._sites[i] for i in xrange(len(self._sites)) if i not in indices]
-    
+
     def apply_operation(self, symmop):
         """
         Apply a symmetry operation to the structure and return the new structure.
@@ -183,13 +183,13 @@ class StructureEditor(StructureModifier):
         Args:
             symmop:
                 Symmetry operation to apply.
-        """        
+        """
         self._lattice = Lattice([symmop.apply_rotation_only(row) for row in self._lattice.matrix])
         def operate_site(site):
             new_cart = symmop.operate(site.coords)
-            return PeriodicSite(site.species_and_occu, self._lattice.get_fractional_coords(new_cart), self._lattice) 
+            return PeriodicSite(site.species_and_occu, self._lattice.get_fractional_coords(new_cart), self._lattice)
         self._sites = map(operate_site, self._sites)
-    
+
     def modify_lattice(self, new_lattice):
         """
         Modify the lattice of the structure.  Mainly used for changing the basis.
@@ -203,7 +203,7 @@ class StructureEditor(StructureModifier):
         for site in self._sites:
             new_sites.append(PeriodicSite(site.species_and_occu, self._lattice.get_fractional_coords(site.coords), self._lattice))
         self._sites = new_sites
-        
+
     def translate_sites(self, indices, vector, frac_coords = True):
         """
         Translate specific sites by some vector, keeping the sites within the unit cell
@@ -223,7 +223,7 @@ class StructureEditor(StructureModifier):
                 fcoords = self._lattice.get_fractional_coords(site.coords + vector)
             new_site = PeriodicSite(site.species_and_occu, fcoords, self._lattice, to_unit_cell = True, coords_are_cartesian = False)
             self._sites[i] = new_site
-            
+
     def perturb_structure(self, distance = 0.1):
         '''
         performs a random perturbation of the sites in a structure to break symmetries
@@ -233,24 +233,24 @@ class StructureEditor(StructureModifier):
         '''
         for i in range(len(self._sites)):
             vector = np.random.rand(3)
-            vector /= np.linalg.norm(vector)/distance
+            vector /= np.linalg.norm(vector) / distance
             self.translate_sites([i], vector, frac_coords = False)
-        
-    
+
+
     @property
     def original_structure(self):
         return self._original_structure
-    
+
     @property
     def modified_structure(self):
         return Structure(self._lattice, [site.species_and_occu for site in self._sites], [site.frac_coords for site in self._sites], False)
-    
+
 class SupercellMaker(StructureModifier):
     """
     Makes a supercell
     """
-    
-    def __init__(self, structure, scaling_matrix = [[1,0,0],[0,1,0],[0,0,1]]):
+
+    def __init__(self, structure, scaling_matrix = ((1, 0, 0), (0, 1, 0), (0, 0, 1))):
         """
         Create a supercell.
         
@@ -270,7 +270,7 @@ class SupercellMaker(StructureModifier):
         new_species = []
         new_fcoords = []
         def range_vec(i):
-            return xrange(max(scale_matrix[:][:,i]) - min(scale_matrix[:][:,i]))
+            return xrange(max(scale_matrix[:][:, i]) - min(scale_matrix[:][:, i]))
         for site in structure.sites:
             for (i, j, k) in itertools.product(range_vec(0), range_vec(1), range_vec(2)):
                 new_species.append(site.species_and_occu)
@@ -278,14 +278,15 @@ class SupercellMaker(StructureModifier):
                 coords = old_lattice.get_cartesian_coords(fcoords + np.array([i, j, k]))
                 new_fcoords.append(new_lattice.get_fractional_coords(coords))
         self._modified_structure = Structure(new_lattice, new_species, new_fcoords, False)
-                        
+
     @property
     def original_structure(self):
         return self._original_structure
-    
+
     @property
     def modified_structure(self):
         return self._modified_structure
+
 
 class OxidationStateDecorator(StructureModifier):
     """
@@ -293,7 +294,7 @@ class OxidationStateDecorator(StructureModifier):
     by replacing each Element at a site with a Specie with an oxidation state.
     Useful for higher level functions. 
     """
-    
+
     def __init__(self, structure, oxidation_states):
         """
         Decorates a structure with oxidation states.
@@ -307,24 +308,25 @@ class OxidationStateDecorator(StructureModifier):
         self._original_structure = structure
         try:
             new_species = [{Specie(el.symbol, oxidation_states[el.symbol]) : occu for el, occu in site.species_and_occu.items()} for site in structure]
-        except KeyError as ex:
+        except KeyError:
             raise ValueError("Oxidation state of all elements must be specified in the dictionary.")
         self._modified_structure = Structure(structure.lattice, new_species, structure.frac_coords, False)
-        
+
     @property
     def original_structure(self):
         return self._original_structure
-    
+
     @property
     def modified_structure(self):
         return self._modified_structure
-    
+
+
 class OxidationStateRemover(StructureModifier):
     """
     Replace each Specie at a site with an element.
     Useful for doing structure comparisons after applying higher level functions
     """
-    
+
     def __init__(self, structure):
         """
         Removes oxidation states from a structure
@@ -336,20 +338,21 @@ class OxidationStateRemover(StructureModifier):
         self._original_structure = structure
         new_species = [{Element(el.symbol) : occu for el, occu in site.species_and_occu.items()} for site in structure]
         self._modified_structure = Structure(structure.lattice, new_species, structure.frac_coords, False)
-                        
+
     @property
     def original_structure(self):
         return self._original_structure
-    
+
     @property
     def modified_structure(self):
         return self._modified_structure
+
 
 class BasisChange(StructureModifier):
     """
     Given a new basis, we express the structure in this new basis
     """
-    
+
     def __init__(self, structure, new_lattice):
         """
         Express a given structure in a new basis.
@@ -364,16 +367,16 @@ class BasisChange(StructureModifier):
         sp = [site.species_and_occu for site in structure._sites]
         coords = [site.coords for site in structure._sites]
         self._modified_structure = Structure(new_lattice, sp, coords, validate_proximity = False, to_unit_cell = True, coords_are_cartesian = True)
-                        
+
     @property
     def original_structure(self):
         return self._original_structure
-    
+
     @property
     def modified_structure(self):
-        return self._modified_structure    
+        return self._modified_structure
 
-    
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
--- pymatgen/transformations/tests/test_standard_transformations.py
+++ pymatgen/transformations/tests/test_standard_transformations.py
@@ -5,7 +5,7 @@ from __future__ import division
 Created on Sep 23, 2011
 '''
 
-__author__="Shyue Ping Ong"
+__author__ = "Shyue Ping Ong"
 __copyright__ = "Copyright 2011, The Materials Project"
 __version__ = "0.1"
 __maintainer__ = "Shyue Ping Ong"
@@ -21,172 +21,196 @@ from pymatgen.core.structure import Structure
 from pymatgen.analysis.ewald import EwaldSummation
 
 class TransformationsTest(unittest.TestCase):
-    
+
     def setUp(self):
         coords = list()
-        coords.append([0,0,0])
-        coords.append([0.75,0.5,0.75])
-        lattice = Lattice([[ 3.8401979337, 0.00, 0.00],[1.9200989668, 3.3257101909, 0.00],[0.00,-2.2171384943,3.1355090603]])
-        self.struct = Structure(lattice,["Si"] *2 ,coords)
+        coords.append([0, 0, 0])
+        coords.append([0.75, 0.5, 0.75])
+        lattice = Lattice([[ 3.8401979337, 0.00, 0.00], [1.9200989668, 3.3257101909, 0.00], [0.00, -2.2171384943, 3.1355090603]])
+        self.struct = Structure(lattice, ["Si"] * 2 , coords)
 
     def test_identity_transformation(self):
         t = IdentityTransformation()
         self.assertEqual(self.struct, t.apply_transformation(self.struct))
-    
+
     def test_to_dict(self):
         t = IdentityTransformation()
         self.assertIn("version", t.to_dict)
         self.assertIn("init_args", t.to_dict)
-        
+
     def test_rotation_transformation(self):
-        t = RotationTransformation([0,1,0], 30, False)
+        t = RotationTransformation([0, 1, 0], 30, False)
         s2 = t.apply_transformation(self.struct)
         s1 = t.inverse.apply_transformation(s2)
         self.assertTrue((abs(s1.lattice.matrix - self.struct.lattice.matrix) < 1e-8).all())
 
 
 class RemoveSpeciesTransformationTest(unittest.TestCase):
-    
+
     def test_apply_transformation(self):
         t = RemoveSpeciesTransformation(["Li+"])
         coords = list()
-        coords.append([0,0,0])
-        coords.append([0.75,0.75,0.75])
-        coords.append([0.5,0.5,0.5])
-        coords.append([0.25,0.25,0.25])
-        lattice = Lattice([[ 3.8401979337, 0.00, 0.00],[1.9200989668, 3.3257101909, 0.00],[0.00,-2.2171384943,3.1355090603]])
-        struct = Structure(lattice,["Li+", "Li+", "O2-", "O2-"],coords)
+        coords.append([0, 0, 0])
+        coords.append([0.75, 0.75, 0.75])
+        coords.append([0.5, 0.5, 0.5])
+        coords.append([0.25, 0.25, 0.25])
+        lattice = Lattice([[ 3.8401979337, 0.00, 0.00], [1.9200989668, 3.3257101909, 0.00], [0.00, -2.2171384943, 3.1355090603]])
+        struct = Structure(lattice, ["Li+", "Li+", "O2-", "O2-"], coords)
         s = t.apply_transformation(struct)
         self.assertEqual(s.composition.formula, "O2")
-        
+
 
 class SubstitutionTransformationTest(unittest.TestCase):
-    
+
     def test_apply_transformation(self):
         t = SubstitutionTransformation({"Li+":"Na+", "O2-":"S2-"})
         coords = list()
-        coords.append([0,0,0])
-        coords.append([0.75,0.75,0.75])
-        coords.append([0.5,0.5,0.5])
-        coords.append([0.25,0.25,0.25])
-        lattice = Lattice([[ 3.8401979337, 0.00, 0.00],[1.9200989668, 3.3257101909, 0.00],[0.00,-2.2171384943,3.1355090603]])
-        struct = Structure(lattice,["Li+", "Li+", "O2-", "O2-"],coords)
+        coords.append([0, 0, 0])
+        coords.append([0.75, 0.75, 0.75])
+        coords.append([0.5, 0.5, 0.5])
+        coords.append([0.25, 0.25, 0.25])
+        lattice = Lattice([[ 3.8401979337, 0.00, 0.00], [1.9200989668, 3.3257101909, 0.00], [0.00, -2.2171384943, 3.1355090603]])
+        struct = Structure(lattice, ["Li+", "Li+", "O2-", "O2-"], coords)
         s = t.apply_transformation(struct)
         self.assertEqual(s.composition.formula, "Na2 S2")
 
 class SupercellTransformationTest(unittest.TestCase):
-    
+
     def test_apply_transformation(self):
-        t = SupercellTransformation([[2,1,0],[0,2,0],[1,0,2]])
+        t = SupercellTransformation([[2, 1, 0], [0, 2, 0], [1, 0, 2]])
         coords = list()
-        coords.append([0,0,0])
-        coords.append([0.75,0.75,0.75])
-        coords.append([0.5,0.5,0.5])
-        coords.append([0.25,0.25,0.25])
-        lattice = Lattice([[ 3.8401979337, 0.00, 0.00],[1.9200989668, 3.3257101909, 0.00],[0.00,-2.2171384943,3.1355090603]])
-        struct = Structure(lattice,["Li+", "Li+", "O2-", "O2-"],coords)
+        coords.append([0, 0, 0])
+        coords.append([0.75, 0.75, 0.75])
+        coords.append([0.5, 0.5, 0.5])
+        coords.append([0.25, 0.25, 0.25])
+        lattice = Lattice([[ 3.8401979337, 0.00, 0.00], [1.9200989668, 3.3257101909, 0.00], [0.00, -2.2171384943, 3.1355090603]])
+        struct = Structure(lattice, ["Li+", "Li+", "O2-", "O2-"], coords)
         s = t.apply_transformation(struct)
         self.assertEqual(s.composition.formula, "Li16 O16")
 
 class OxidationStateDecorationTransformationTest(unittest.TestCase):
-    
+
     def test_apply_transformation(self):
         t = OxidationStateDecorationTransformation({"Li":1, "O":-2})
         coords = list()
-        coords.append([0,0,0])
-        coords.append([0.75,0.75,0.75])
-        coords.append([0.5,0.5,0.5])
-        coords.append([0.25,0.25,0.25])
-        lattice = Lattice([[ 3.8401979337, 0.00, 0.00],[1.9200989668, 3.3257101909, 0.00],[0.00,-2.2171384943,3.1355090603]])
-        struct = Structure(lattice,["Li", "Li", "O", "O"],coords)
+        coords.append([0, 0, 0])
+        coords.append([0.75, 0.75, 0.75])
+        coords.append([0.5, 0.5, 0.5])
+        coords.append([0.25, 0.25, 0.25])
+        lattice = Lattice([[ 3.8401979337, 0.00, 0.00], [1.9200989668, 3.3257101909, 0.00], [0.00, -2.2171384943, 3.1355090603]])
+        struct = Structure(lattice, ["Li", "Li", "O", "O"], coords)
         s = t.apply_transformation(struct)
         self.assertEqual(str(s[0].specie), "Li+")
         self.assertEqual(str(s[2].specie), "O2-")
 
 
 class PartialRemoveSpecieTransformationTest(unittest.TestCase):
-    
+
     def test_apply_transformation(self):
-        t = PartialRemoveSpecieTransformation("Li+", 1.0/3, True)
+        t = PartialRemoveSpecieTransformation("Li+", 1.0 / 3, True)
         coords = list()
-        coords.append([0,0,0])
-        coords.append([0.75,0.75,0.75])
-        coords.append([0.5,0.5,0.5])
-        coords.append([0.25,0.25,0.25])
-        lattice = Lattice([[ 3.8401979337, 0.00, 0.00],[1.9200989668, 3.3257101909, 0.00],[0.00,-2.2171384943,3.1355090603]])
-        struct = Structure(lattice,["Li+", "Li+", "Li+", "O2-"],coords)
+        coords.append([0, 0, 0])
+        coords.append([0.75, 0.75, 0.75])
+        coords.append([0.5, 0.5, 0.5])
+        coords.append([0.25, 0.25, 0.25])
+        lattice = Lattice([[ 3.8401979337, 0.00, 0.00], [1.9200989668, 3.3257101909, 0.00], [0.00, -2.2171384943, 3.1355090603]])
+        struct = Structure(lattice, ["Li+", "Li+", "Li+", "O2-"], coords)
         t.apply_transformation(struct)
         self.assertEqual(len(t.all_structures), 3)
-        
+
     def test_apply_transformation_fast(self):
         t = PartialRemoveSpecieTransformation("Li+", 0.5)
         coords = list()
-        coords.append([0,0,0])
-        coords.append([0.75,0.75,0.75])
-        coords.append([0.5,0.5,0.5])
-        coords.append([0.25,0.25,0.25])
-        coords.append([0.1,0.1,0.1])
-        coords.append([0.3,0.75,0.3])
-        lattice = Lattice([[ 10, 0.00, 0.00],[0, 10, 0.00],[0.00,0,10]])
-        struct = Structure(lattice,["Li+"] * 6,coords)
+        coords.append([0, 0, 0])
+        coords.append([0.75, 0.75, 0.75])
+        coords.append([0.5, 0.5, 0.5])
+        coords.append([0.25, 0.25, 0.25])
+        coords.append([0.1, 0.1, 0.1])
+        coords.append([0.3, 0.75, 0.3])
+        lattice = Lattice([[ 10, 0.00, 0.00], [0, 10, 0.00], [0.00, 0, 10]])
+        struct = Structure(lattice, ["Li+"] * 6, coords)
         fast_opt_s = t.apply_transformation(struct)
         t = PartialRemoveSpecieTransformation("Li+", 0.5, True)
         slow_opt_s = t.apply_transformation(struct)
         self.assertAlmostEqual(EwaldSummation(fast_opt_s).total_energy, EwaldSummation(slow_opt_s).total_energy, 4)
         self.assertEqual(fast_opt_s, slow_opt_s)
-        
-        
+
+
 class OrderDisorderedStructureTransformationTest(unittest.TestCase):
 
     def test_apply_transformation(self):
         t = OrderDisorderedStructureTransformation(num_structures = 50)
         coords = list()
-        coords.append([0,0,0])
-        coords.append([0.75,0.75,0.75])
-        coords.append([0.5,0.5,0.5])
-        coords.append([0.25,0.25,0.25])
-        lattice = Lattice([[ 3.8401979337, 0.00, 0.00],[1.9200989668, 3.3257101909, 0.00],[0.00,-2.2171384943,3.1355090603]])
-        
-        struct = Structure(lattice,[{"Si4+":0.5, "O2-": 0.25, "P5+": 0.25}, {"Si4+":0.5, "O2-": 0.25, "P5+": 0.25}, {"Si4+":0.5, "O2-": 0.25, "P5+": 0.25}, {"Si4+":0.5, "O2-": 0.25, "P5+": 0.25}] ,coords)
+        coords.append([0, 0, 0])
+        coords.append([0.75, 0.75, 0.75])
+        coords.append([0.5, 0.5, 0.5])
+        coords.append([0.25, 0.25, 0.25])
+        lattice = Lattice([[ 3.8401979337, 0.00, 0.00], [1.9200989668, 3.3257101909, 0.00], [0.00, -2.2171384943, 3.1355090603]])
+
+        struct = Structure(lattice, [{"Si4+":0.5, "O2-": 0.25, "P5+": 0.25}, {"Si4+":0.5, "O2-": 0.25, "P5+": 0.25}, {"Si4+":0.5, "O2-": 0.25, "P5+": 0.25}, {"Si4+":0.5, "O2-": 0.25, "P5+": 0.25}] , coords)
         t.apply_transformation(struct)
         self.assertEqual(len(t.all_structures), 12)
-        
-        struct = Structure(lattice,[{"Si4+":0.5}, {"Si4+":0.5}, {"P5+":0.5, "O2-": 0.5}, {"P5+":0.5, "O2-": 0.5}] ,coords)
+
+        struct = Structure(lattice, [{"Si4+":0.5}, {"Si4+":0.5}, {"P5+":0.5, "O2-": 0.5}, {"P5+":0.5, "O2-": 0.5}] , coords)
         t.apply_transformation(struct)
         self.assertEqual(len(t.all_structures), 4)
-        
-        struct = Structure(lattice,[{"Si4+":0.5}, {"Si4+":0.5}, {"O2-": 0.5}, {"O2-": 0.5}] ,coords)
+
+        struct = Structure(lattice, [{"Si4+":0.5}, {"Si4+":0.5}, {"O2-": 0.5}, {"O2-": 0.5}] , coords)
         t.apply_transformation(struct)
         self.assertEqual(len(t.all_structures), 4)
-        
-        struct = Structure(lattice,[{"Si4+":0.333}, {"Si4+":0.333}, {"Si4+":0.333}, "O2-"] ,coords)
+
+        struct = Structure(lattice, [{"Si4+":0.333}, {"Si4+":0.333}, {"Si4+":0.333}, "O2-"] , coords)
         t.apply_transformation(struct)
         self.assertEqual(len(t.all_structures), 3)
-        
+
 class PrimitiveCellTransformationTest(unittest.TestCase):
-    
+
     def test_apply_transformation(self):
         t = PrimitiveCellTransformation()
         coords = list()
-        coords.append([0,0,0])
-        coords.append([0.375,0.375,0.375])
-        coords.append([.5,.5,.5])
-        coords.append([0.875,0.875,0.875])
-        coords.append([0.125,0.125,0.125])
-        coords.append([0.25,0.25,0.25])
-        coords.append([0.625,0.625,0.625])
-        coords.append([0.75,0.75,0.75])
-        
-        lattice = Lattice([[ 3.8401979337, 0.00, 0.00],[1.9200989668, 3.3257101909, 0.00],[0.00,-2.2171384943,3.1355090603]])
-        struct = Structure(lattice,["Li+", "Li+","Li+", "Li+", "O2-", "O2-", "O2-", "O2-"],coords)
+        coords.append([0, 0, 0])
+        coords.append([0.375, 0.375, 0.375])
+        coords.append([.5, .5, .5])
+        coords.append([0.875, 0.875, 0.875])
+        coords.append([0.125, 0.125, 0.125])
+        coords.append([0.25, 0.25, 0.25])
+        coords.append([0.625, 0.625, 0.625])
+        coords.append([0.75, 0.75, 0.75])
+
+        lattice = Lattice([[ 3.8401979337, 0.00, 0.00], [1.9200989668, 3.3257101909, 0.00], [0.00, -2.2171384943, 3.1355090603]])
+        struct = Structure(lattice, ["Li+", "Li+", "Li+", "Li+", "O2-", "O2-", "O2-", "O2-"], coords)
         s = t.apply_transformation(struct)
         self.assertEqual(len(s), 4)
-        
+
+
+class TranslateSitesTransformationTest(unittest.TestCase):
+
+    def test_apply_transformation(self):
+        t = TranslateSitesTransformation([0], [0.1, 0.2, 0.3])
+        coords = list()
+        coords.append([0, 0, 0])
+        coords.append([0.375, 0.375, 0.375])
+        coords.append([.5, .5, .5])
+        coords.append([0.875, 0.875, 0.875])
+        coords.append([0.125, 0.125, 0.125])
+        coords.append([0.25, 0.25, 0.25])
+        coords.append([0.625, 0.625, 0.625])
+        coords.append([0.75, 0.75, 0.75])
+
+        lattice = Lattice([[ 3.8401979337, 0.00, 0.00], [1.9200989668, 3.3257101909, 0.00], [0.00, -2.2171384943, 3.1355090603]])
+        struct = Structure(lattice, ["Li+", "Li+", "Li+", "Li+", "O2-", "O2-", "O2-", "O2-"], coords)
+        s = t.apply_transformation(struct)
+        self.assertTrue(np.allclose(s[0].frac_coords, [0.1, 0.2, 0.3]))
+        inv_t = t.inverse
+        s = inv_t.apply_transformation(s)
+        self.assertTrue(np.allclose(s[0].frac_coords, [0, 0, 0]))
+
+
 class TransformationJsonTest(unittest.TestCase):
-    
+
     def test_from_json(self):
         self.assertIsInstance(transformation_from_json('{"name": "IdentityTransformation", "init_args": {}}'), IdentityTransformation)
         self.assertIsInstance(transformation_from_json('{"name": "RotationTransformation", "init_args": {"angle": 30, "angle_in_radians": false, "axis": [0, 1, 0]}}'), RotationTransformation)
-        
+
 if __name__ == "__main__":
     unittest.main()
