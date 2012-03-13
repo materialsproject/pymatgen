564d4c6d377d9e8a1fcd3292ea0b3507dbb9b78c
William Richards <wrichard@mit.edu>
2012-03-13 13:08:52 -0400

    Started work on a transformedstructuretree

--- pymatgen/alchemy/materials.py
+++ pymatgen/alchemy/materials.py
@@ -23,6 +23,7 @@ from pymatgen.core.structure import Structure
 from pymatgen.io.cifio import CifParser
 from pymatgen.io.vaspio import Poscar
 from pymatgen.transformations.standard_transformations import transformation_from_dict
+from collections import Iterable
 
 class TransformedStructure(object):
     """
@@ -211,6 +212,198 @@ class TransformedStructure(object):
         d['history'] = history
         d['version'] = __version__
         return d
+    
+class TransformedStructureTree(object):
+    """
+    Container object for groups of structures that include history of transformations. This is used for generating
+    multiple output structures for a single or multiple input structures through transformations that have multiple
+    outputs
+    
+    Each structure is stored as a transformed structure. Transformations that produce multiple output structures
+    increase the number of structures in the tree. A pruning method removes duplicate structures from the tree.
+    Care must be used to ensure that the tree doesn't get too big
+    """
+
+    def __init__(self, structures_list, transformations, histories = None):
+        """
+        Standard constructor for a TransformedStructureTree
+        
+        Args:
+            structures_list:
+                input structures (must be a list)
+            transformations:
+                sequence of transformations to be applied to the input structures.
+            histories:
+                optional history list for the input structures, which provides a way
+                to track structures having undergone multiple series of 
+                transformations. Must be the same length as structures_list
+        """
+        self._transformed_structures = []
+        
+        if histories:
+            if not len(structures_list) == len(histories):
+                raise ValueError("histories must have same length as structures_list")
+            for i in range(len(structures_list)):
+                self._transformed_structures.append(TransformedStructure(structures_list[i], transformations, histories[i]))
+        else:
+            for i in range(len(structures_list)):
+                self._transformed_structures.append(TransformedStructure(structures_list[i], transformations, None))
+
+    def undo_last_transformation(self):
+        """
+        Undo the last transformation in the TransformedStructure.
+        
+        Raises:
+            IndexError if already at the oldest change.
+        """
+        for x in self._transformed_structures:
+            x.undo_last_transformation()
+
+    def redo_next_transformation(self):
+        """
+        Redo the last undone transformation in the TransformedStructure.
+        
+        Raises:
+            IndexError if already at the latest change.
+        """
+        for x in self._transformed_structures:
+            x.redo_next_transformation()
+
+    def __getitem__(self, index):
+        return [x.__getitem__(self, index) for x in self._transformed_structures]
+
+    def __len__(self):
+        return len(self._transformed_structures)
+
+    def append_transformation(self, transformation, clear_redo = True):
+        """
+        Appends a transformation to the TransformedStructure.
+        
+        Arguments:
+            transformation:
+                Transformation to append
+            clear_redo:
+                Boolean indicating whether to clear the redo list. By default,
+                this is True, meaning any appends clears the history of undoing.
+                However, when using append_transformation to do a redo, the redo
+                list should not be cleared to allow multiple redos.
+        """
+        new_transformed_structures = []
+        structures_to_delete = []
+        for x in self._transformed_structures:
+            new_structs = transformation.apply_transformation(x.final_structure)
+            if isinstance(new_structs, Iterable):
+                structures_to_delete.append(x)
+                for new_struct in new_structs:
+                    new_tstruct = x
+                    new_tstruct._structures.append(new_struct)
+                    new_tstruct._transformations.append(transformation)
+                    new_transformed_structures.append(new_tstruct)
+            else:
+                x._structures.append(new_structs)
+                x._transformations.append(transformation)
+            if clear_redo:
+                x._redo_trans = []
+        for x in structures_to_delete:
+            self._transformed_structures.remove(x)
+        self._transformed_structures.extend(new_transformed_structures)
+                    
+
+    def extend_transformations(self, transformations):
+        """
+        Extends a sequence of transformations to the TransformedStructure.
+        
+        Arguments:
+            transformations:
+                Sequence of Transformations
+        """
+        for t in transformations:
+            self.append_transformation(t)
+
+    def get_vasp_input(self, vasp_input_set, generate_potcar = True):
+        """
+        Returns VASP input as a dict of vaspio objects.
+        
+        Args:
+            vasp_input_set:
+                pymatgen.io.vaspio_set.VaspInputSet like object that creates
+                vasp input files from structures
+            generate_potcar:
+                Set to False to generate a POTCAR.spec file instead of a POTCAR,
+                which contains the POTCAR labels but not the actual POTCAR. Defaults
+                to True.
+        """
+        return [x.__getitem__(self, vasp_input_set, generate_potcar) for x in self._transformed_structures]
+
+    def write_vasp_input(self, vasp_input_set, output_dir, create_directory = True):
+        """
+        Writes VASP input to an output_dir.
+        
+        Args:
+            vasp_input_set:
+                pymatgen.io.vaspio_set.VaspInputSet like object that creates vasp input files from structures
+            output_dir:
+                Directory to output files
+            create_directory:
+                Create the directory if not present. Defaults to True.
+        """
+        vasp_input_set.write_input(self._structures[-1], output_dir, make_dir_if_not_present = create_directory)
+        with open(os.path.join(output_dir, 'transformations.json'), 'w') as fp:
+            json.dump(self.to_dict, fp)
+
+    def __str__(self):
+        output = ["Current structures"]
+        output.append("------------")
+        for x in self._transformed_structures:
+            output.append(str(x._structures[-1]))
+        return "\n".join(output)
+
+#TODO: Code these following methods
+'''
+    @property
+    def structures(self):
+        """
+        Returns a copy of all structures in the TransformedStructure. A structure
+        is stored after every single transformation.
+        """
+        return [s for s in self._structures]
+
+    @property
+    def transformations(self):
+        """
+        Returns a copy of all transformations in the TransformedStructure. 
+        """
+        return [t for t in self._transformations]
+
+    @property
+    def final_structure(self):
+        """
+        Returns the final structure in the TransformedStructure.
+        """
+        return self._structures[-1]
+
+    @staticmethod
+    def from_dict(d):
+        """
+        Creates a TransformedStructure from a dict.
+        """
+        s = Structure.from_dict(d)
+        return TransformedStructure(s, [], d['history'])
+
+    @property
+    def to_dict(self):
+        """
+        Returns a dict representation of the TransformedStructure.
+        """
+        d = self._structures[-1].to_dict
+        history = [self._source]
+        for i, t in enumerate(self._transformations):
+            tdict = t.to_dict
+            tdict['input_structure'] = self._structures[i].to_dict
+            history.append(tdict)
+        d['history'] = history
+        d['version'] = __version__
+        return d'''
 
 
 class CifTransformedStructure(TransformedStructure):
