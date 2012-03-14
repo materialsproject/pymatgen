337b70db5409b8bb5a7b905be92969089cc49023
William Richards <wrichard@mit.edu>
2012-03-14 14:01:23 -0400

    Implemented basic functionality for TransformedStructureCollection

--- pymatgen/alchemy/materials.py
+++ pymatgen/alchemy/materials.py
@@ -18,12 +18,14 @@ import os
 import re
 import datetime
 import json
+import collections
 
 from pymatgen.core.structure import Structure
 from pymatgen.io.cifio import CifParser
 from pymatgen.io.vaspio import Poscar
 from pymatgen.transformations.standard_transformations import transformation_from_dict
-from collections import Iterable
+
+
 
 class TransformedStructure(object):
     """
@@ -91,8 +93,16 @@ class TransformedStructure(object):
 
     def __len__(self):
         return len(self._structures)
+    
+    def _alternative_transformed_structures(self, transformation, structures):
+        if len(structures)>0:
+            self._transformations.append(transformation)
+            history = self.history
+            self._transformations.pop()
+        for x in structures:
+            yield TransformedStructure(x, [], history)
 
-    def append_transformation(self, transformation, clear_redo = True):
+    def append_transformation(self, transformation, clear_redo = True, return_alternatives = False):
         """
         Appends a transformation to the TransformedStructure.
         
@@ -105,13 +115,22 @@ class TransformedStructure(object):
                 However, when using append_transformation to do a redo, the redo
                 list should not be cleared to allow multiple redos.
         """
+        
         new_s = transformation.apply_transformation(self._structures[-1])
-        if isinstance(new_s, Iterable):
+        alternative_s = None
+        
+        if not isinstance(new_s, Structure):
+            alternative_s = self._alternative_transformed_structures(transformation, new_s[1:])
             new_s = new_s[0]
+        
+        self._transformations.append(transformation)   
         self._structures.append(new_s)
-        self._transformations.append(transformation)
+        
         if clear_redo:
             self._redo_trans = []
+        
+        if return_alternatives:
+            return alternative_s
 
     def extend_transformations(self, transformations):
         """
@@ -199,58 +218,38 @@ class TransformedStructure(object):
         """
         s = Structure.from_dict(d)
         return TransformedStructure(s, [], d['history'])
-
+    
+    @property
+    def history(self):
+        history = [self._source]
+        for i, t in enumerate(self._transformations):
+            tdict = t.to_dict
+            tdict['input_structure'] = self._structures[i].to_dict
+            history.append(tdict)
+        return history
+    
     @property
     def to_dict(self):
         """
         Returns a dict representation of the TransformedStructure.
         """
         d = self._structures[-1].to_dict
-        history = [self._source]
-        for i, t in enumerate(self._transformations):
-            tdict = t.to_dict
-            tdict['input_structure'] = self._structures[i].to_dict
-            history.append(tdict)
-        d['history'] = history
+        d['history'] = self.history
         d['version'] = __version__
         return d
     
-class TransformedStructureTree(object):
-    """
-    Container object for groups of structures that include history of transformations. This is used for generating
-    multiple output structures for a single or multiple input structures through transformations that have multiple
-    outputs
     
-    Each structure is stored as a transformed structure. Transformations that produce multiple output structures
-    increase the number of structures in the tree. A pruning method removes duplicate structures from the tree.
-    Care must be used to ensure that the tree doesn't get too big
-    """
-
-    def __init__(self, structures_list, transformations, histories = None):
-        """
-        Standard constructor for a TransformedStructureTree
-        
-        Args:
-            structures_list:
-                input structures (must be a list)
-            transformations:
-                sequence of transformations to be applied to the input structures.
-            histories:
-                optional history list for the input structures, which provides a way
-                to track structures having undergone multiple series of 
-                transformations. Must be the same length as structures_list
-        """
-        self._transformed_structures = []
-        
-        if histories:
-            if not len(structures_list) == len(histories):
-                raise ValueError("histories must have same length as structures_list")
-            for i in range(len(structures_list)):
-                self._transformed_structures.append(TransformedStructure(structures_list[i], transformations, histories[i]))
-        else:
-            for i in range(len(structures_list)):
-                self._transformed_structures.append(TransformedStructure(structures_list[i], transformations, None))
-
+class TransformedStructureCollection(object):
+    
+    def __init__(self, transformed_structures, transformations, extend_collection = False):
+        self._extend_collection = extend_collection
+        self._transformed_structures = transformed_structures
+        for trans in transformations:
+            self.append_transformation(trans)
+    
+    def __getitem__(self, index):
+        return self._transformed_structures[index]
+    
     def undo_last_transformation(self):
         """
         Undo the last transformation in the TransformedStructure.
@@ -271,14 +270,13 @@ class TransformedStructureTree(object):
         for x in self._transformed_structures:
             x.redo_next_transformation()
 
-    def __getitem__(self, index):
-        return [x.__getitem__(self, index) for x in self._transformed_structures]
-
     def __len__(self):
         return len(self._transformed_structures)
 
     def append_transformation(self, transformation, clear_redo = True):
         """
+        TODO: clean this up a lot
+        
         Appends a transformation to the TransformedStructure.
         
         Arguments:
@@ -290,26 +288,13 @@ class TransformedStructureTree(object):
                 However, when using append_transformation to do a redo, the redo
                 list should not be cleared to allow multiple redos.
         """
-        new_transformed_structures = []
-        structures_to_delete = []
+        new_structures = []
+        
         for x in self._transformed_structures:
-            new_structs = transformation.apply_transformation(x.final_structure)
-            if isinstance(new_structs, Iterable):
-                structures_to_delete.append(x)
-                for new_struct in new_structs:
-                    new_tstruct = x
-                    new_tstruct._structures.append(new_struct)
-                    new_tstruct._transformations.append(transformation)
-                    new_transformed_structures.append(new_tstruct)
-            else:
-                x._structures.append(new_structs)
-                x._transformations.append(transformation)
-            if clear_redo:
-                x._redo_trans = []
-        for x in structures_to_delete:
-            self._transformed_structures.remove(x)
-        self._transformed_structures.extend(new_transformed_structures)
-                    
+            new = x.append_transformation(transformation, clear_redo, return_alternatives = self._extend_collection)
+            if new:
+                new_structures.extend(new)
+        self._transformed_structures.extend(new_structures)
 
     def extend_transformations(self, transformations):
         """
@@ -321,37 +306,29 @@ class TransformedStructureTree(object):
         """
         for t in transformations:
             self.append_transformation(t)
-
-    def get_vasp_input(self, vasp_input_set, generate_potcar = True):
+            
+    def batch_write_vasp_input(self, vasp_input_set, output_dir, create_directory = True):
         """
-        Returns VASP input as a dict of vaspio objects.
+        Batch write vasp input for a sequence of transformed structures to output_dir,
+        following the format output_dir/{formula}_{number}.
         
         Args:
+            transformed_structures:
+                Sequence of TransformedStructures.
             vasp_input_set:
                 pymatgen.io.vaspio_set.VaspInputSet like object that creates
                 vasp input files from structures
-            generate_potcar:
-                Set to False to generate a POTCAR.spec file instead of a POTCAR,
-                which contains the POTCAR labels but not the actual POTCAR. Defaults
-                to True.
-        """
-        return [x.__getitem__(self, vasp_input_set, generate_potcar) for x in self._transformed_structures]
-
-    def write_vasp_input(self, vasp_input_set, output_dir, create_directory = True):
-        """
-        Writes VASP input to an output_dir.
-        
-        Args:
-            vasp_input_set:
-                pymatgen.io.vaspio_set.VaspInputSet like object that creates vasp input files from structures
             output_dir:
                 Directory to output files
             create_directory:
                 Create the directory if not present. Defaults to True.
         """
-        vasp_input_set.write_input(self._structures[-1], output_dir, make_dir_if_not_present = create_directory)
-        with open(os.path.join(output_dir, 'transformations.json'), 'w') as fp:
-            json.dump(self.to_dict, fp)
+        dnames_count = collections.defaultdict(int)
+        for s in self._transformed_structures:
+            formula = re.sub("\s+", "", s.final_structure.formula)
+            dirname = os.path.join(output_dir, '{}_{}'.format(formula, dnames_count[formula] + 1))
+            s.write_vasp_input(vasp_input_set, dirname, create_directory = True)
+            dnames_count[formula] += 1
 
     def __str__(self):
         output = ["Current structures"]
@@ -359,107 +336,110 @@ class TransformedStructureTree(object):
         for x in self._transformed_structures:
             output.append(str(x._structures[-1]))
         return "\n".join(output)
-
-#TODO: Code these following methods
-'''
-    @property
-    def structures(self):
-        """
-        Returns a copy of all structures in the TransformedStructure. A structure
-        is stored after every single transformation.
-        """
-        return [s for s in self._structures]
-
-    @property
-    def transformations(self):
-        """
-        Returns a copy of all transformations in the TransformedStructure. 
-        """
-        return [t for t in self._transformations]
-
-    @property
-    def final_structure(self):
-        """
-        Returns the final structure in the TransformedStructure.
-        """
-        return self._structures[-1]
+    
+    def remove_duplicates(self):
+        '''
+        TODO: write this method
+        '''
+        pass
 
     @staticmethod
-    def from_dict(d):
+    def from_structures(structures_list, transformations = [], histories = None, extend_collection = False):
         """
-        Creates a TransformedStructure from a dict.
-        """
-        s = Structure.from_dict(d)
-        return TransformedStructure(s, [], d['history'])
-
-    @property
-    def to_dict(self):
-        """
-        Returns a dict representation of the TransformedStructure.
-        """
-        d = self._structures[-1].to_dict
-        history = [self._source]
-        for i, t in enumerate(self._transformations):
-            tdict = t.to_dict
-            tdict['input_structure'] = self._structures[i].to_dict
-            history.append(tdict)
-        d['history'] = history
-        d['version'] = __version__
-        return d'''
-
-
-class CifTransformedStructure(TransformedStructure):
-    """
-    Extended version of TransformedStructure to generates new materials from cifs.
-    """
-
-    def __init__(self, cif_string, transformations, primitive = True):
-        """
+        Constructor for a TransformedStructureCollection from list of structures
+        
         Args:
-            cif_string:
-                Input cif string. Should contain only one structure. For cifs
-                containing multiple structures, please use CifTransmuter.
+            structures_list:
+                input structures (must be a list)
             transformations:
-                Sequence of transformations to be applied to the input structure.
-            primitive:
-                Option to set if the primitive cell should be extracted. Defaults
-                to True. However, there are certain instances where you might want
-                to use a non-primitive cell, e.g., if you are trying to generate
-                all possible orderings of partial removals or order a disordered
-                structure.
+                sequence of transformations to be applied to the input structures.
+            histories:
+                optional history list for the input structures, which provides a way
+                to track structures having undergone multiple series of 
+                transformations. Must be the same length as structures_list
         """
-        parser = CifParser.from_string(cif_string)
-        raw_string = re.sub("'", "\"", cif_string)
-        cif_dict = parser.to_dict
-        cif_keys = cif_dict.keys()
-        s = parser.get_structures(primitive)[0]
-        partial_cif = cif_dict[cif_keys[0]]
-        if '_database_code_ICSD' in partial_cif:
-            source = partial_cif['_database_code_ICSD'] + "-ICSD"
+        transformed_structures = []
+        
+        if histories:
+            if not len(structures_list) == len(histories):
+                raise ValueError("histories must have same length as structures_list")
+            for i in range(len(structures_list)):
+                transformed_structures.append(TransformedStructure(structures_list[i], None, histories[i]))
         else:
-            source = 'uploaded cif'
-        source_info = {'source':source, 'datetime':str(datetime.datetime.utcnow()), 'original_file':raw_string, 'cif_data':cif_dict[cif_keys[0]]}
-        super(CifTransformedStructure, self).__init__(s, transformations, [source_info])
-
-
-class PoscarTransformedStructure(TransformedStructure):
-    """
-    Generates a transformed structure from Poscar.
-    """
-
-    def __init__(self, poscar_string, transformations):
-        """
+            for i in range(len(structures_list)):
+                transformed_structures.append(TransformedStructure(structures_list[i], None, None))
+        return TransformedStructureCollection(transformed_structures, transformations, extend_collection)
+    
+    @staticmethod
+    def from_cifs(cif_filenames, transformations = [], primitive = True, extend_collection = False):
+        '''
         Args:
-            poscar_string:
-                Input POSCAR string.
-            transformations:
-                Sequence of transformations to be applied to the input structure.
-        """
-        p = Poscar.from_string(poscar_string)
-        if not p.true_names:
-            raise ValueError("Transformation can be craeted only from POSCAR strings with proper VASP5 element symbols.")
-        raw_string = re.sub("'", "\"", poscar_string)
-        s = p.struct
-        source_info = {'source': "uploaded POSCAR", 'datetime':str(datetime.datetime.utcnow()), 'original_file':raw_string}
-        super(PoscarTransformedStructure, self).__init__(s, transformations, [source_info])
+            cif_filenames:
+                List of strings of the cif files
+        '''
+        
+        def cif_transformed_parser(cif_string, primitive = True):
+            """
+            Args:
+                cif_string:
+                    Input cif string. Should contain only one structure. For cifs
+                    containing multiple structures, please use CifTransmuter.
+                transformations:
+                    Sequence of transformations to be applied to the input structure.
+                primitive:
+                    Option to set if the primitive cell should be extracted. Defaults
+                    to True. However, there are certain instances where you might want
+                    to use a non-primitive cell, e.g., if you are trying to generate
+                    all possible orderings of partial removals or order a disordered
+                    structure.
+            """
+            parser = CifParser.from_string(cif_string)
+            raw_string = re.sub("'", "\"", cif_string)
+            cif_dict = parser.to_dict
+            cif_keys = cif_dict.keys()
+            s = parser.get_structures(primitive)[0]
+            partial_cif = cif_dict[cif_keys[0]]
+            if '_database_code_ICSD' in partial_cif:
+                source = partial_cif['_database_code_ICSD'] + "-ICSD"
+            else:
+                source = 'uploaded cif'
+            source_info = {'source':source, 'datetime':str(datetime.datetime.utcnow()), 'original_file':raw_string, 'cif_data':cif_dict[cif_keys[0]]}
+            return TransformedStructure(s, [], [source_info])
+            
+        transformed_structures = []
+        for filename in cif_filenames:
+            with open(filename, "r") as f:
+                structure_data = []
+                read_data = False
+                for line in f:
+                    if re.match("^\s*data", line):
+                        structure_data.append([])
+                        read_data = True
+                    if read_data:
+                        structure_data[-1].append(line)
+                transformed_structures.extend([cif_transformed_parser("".join(data), primitive) for data in structure_data])
+        return TransformedStructureCollection(transformed_structures, transformations, extend_collection)
+    
+    @staticmethod
+    def from_poscars(poscar_filenames, transformations = [], extend_collection = False):
+        def poscar_transformed_parser(poscar_string):
+            """
+            Args:
+                poscar_string:
+                    Input POSCAR string.
+            """
+            p = Poscar.from_string(poscar_string)
+            if not p.true_names:
+                raise ValueError("Transformation can be craeted only from POSCAR strings with proper VASP5 element symbols.")
+            raw_string = re.sub("'", "\"", poscar_string)
+            s = p.struct
+            source_info = {'source': "uploaded POSCAR", 'datetime':str(datetime.datetime.utcnow()), 'original_file':raw_string}
+            return TransformedStructure(s, [], [source_info])
+        
+        transformed_structures = []
+        for filename in poscar_filenames:
+            with open(filename, "r") as f:
+                transformed_structures.append(poscar_transformed_parser(f.read()))
+        return TransformedStructureCollection(transformed_structures, transformations, extend_collection)
+    
 
