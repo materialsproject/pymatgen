374a07ef1edc8d8fe93d826a1baa22bf4798161c
Maarten de Jong <MDEJONG1@fenrir.bw01.berkeley.edu>
2012-03-17 15:55:34 -0700

    expanded test fot fit_elas further
    Former-commit-id: 1ad4555186fd4c45e1705bdc582302e66051feac [formerly 11dbac4bd16cee874aa4ae1d6e6296f82a429148]
    Former-commit-id: e7b0e9cef560215069cf4ead8d454992eee66f5e

--- pymatgen/phonons/test_fit_elas.py
+++ pymatgen/phonons/test_fit_elas.py
@@ -2,6 +2,7 @@ import warnings
 import sys
 sys.path.append('/home/MDEJONG1/pythonplayground/pymatgen/pymatgen_repo/') # (If one does not want to change $PYTHONPATH)
 import unittest
+from StringIO import StringIO as sio
 import pymatgen
 from pymatgen.io.vaspio import Poscar
 from pymatgen.io.vaspio import Poscar
@@ -16,15 +17,65 @@ import numpy as np
 import random
 from pymatgen.phonons.strain import Strain
 from pymatgen.phonons.strain import IndependentStrain
+from pymatgen.phonons.genstrain import DeformGeometry
+
+if __name__ == "__main__":
+
+    f = open('stresses', 'r') 
+    lines = f.readlines()
+    f.close()
+
+    lines = [line.strip() for line in lines]
+    sig1 = np.matrix(np.loadtxt(sio('\n'.join(lines[0:3]))))
+
+    stress_dict = {}
+
+    struct = CifParser('/home/MDEJONG1/pythonplayground/pymatgen/pymatgen_repo/pymatgen/phonons/aluminum.cif').get_structures()[0]
+    D = DeformGeometry(struct)
+    D = D.deform()
+
+#   stress_dict: {IndependentStrain:stress matrix}
+
+    strain_array = []
+    count = 0
+    
+    for c in D:
+#        stress_dict[c] = np.matrix(np.loadtxt(sio('\n'.join(lines[3*count:3*count+3]))))
+        if c.i==0 and c.j==0:
+            strain_array.append(c.strain[c.i, c.j])
+
+    count = 0
+    strain_array = sorted(strain_array)
+    
+    for n in strain_array:
+        for c in D:
+            if c.i==0 and c.j==0 and np.abs(c.strain[c.i, c.j] - n) < 0.00001:
+                stress_dict[c] = np.matrix(np.loadtxt(sio('\n'.join(lines[3*count:3*count+3]))))
+                count+=1 
 
 
-for i in range(0, 24):
-
-    path = '/home/MDEJONG1/Re/elasticity/Cij_redo/Re_Ta/M' + str(i) + '/vasprun.xml'
-    A = Vasprun(path)
-    sigma =  A.ionic_steps[-1]['stress']
-    print sigma[0,0], sigma[0,1], sigma[0,2]
-    print sigma[1,0], sigma[1,1], sigma[1,2]
-    print sigma[2,0], sigma[2,1], sigma[2,2]
+    
 
 
+#    print stress_dict
+             
+
+#    print strain_array
+
+
+    for mode in stress_dict:
+
+#        print stress_dict[mode]
+        print mode._dfm
+
+
+    
+#       if mode.i==0 and mode.j==0:
+
+#           print mode.strain
+
+#        print mode.i, mode.j, mode.strain[mode.i, mode.j]       
+#        print i
+#        print stress_dict[i]
+    
+ 
