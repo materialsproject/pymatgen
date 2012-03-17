971258b08174f9c23a4c75eca8027fca3a088c08
Geoffroy Hautier <geoffroy.hautier@uclouvain.be>
2012-03-17 16:53:24 +0100

    fix a dangerous bug in kpoints and add a start for the Brillouin zone plotting
    Former-commit-id: 3d4ee7f2e6ddde792267e7cb7deb5aacdd6c6fef [formerly ab670a991c14a010f397d1802ea48de19e9c4e10]
    Former-commit-id: 50693cce878d64855a47f070b48beee1e4a96061

--- pymatgen/command_line/qhull_caller.py
+++ pymatgen/command_line/qhull_caller.py
@@ -29,6 +29,7 @@ def run_qhull_command(command, data, proc_command = int, output_skip=1):
     prep_str += "\n".join([' '.join([str(i) for i in row]) for row in data])
     p = subprocess.Popen(command, stdout = subprocess.PIPE, 
                          stdin = subprocess.PIPE, close_fds = True)
+    print prep_str
     output = p.communicate(input = prep_str)[0]
     output = re.split("\n", output)
     for i in xrange(output_skip):
@@ -62,5 +63,83 @@ def qvertex(data):
     Input data should be in the form of a list of a list of floats.
     Returns the facets of voronoi construction as a list of a list of float.
     """
-    return run_qhull_command(['qvoronoi','p'], data, float, 2)
+    return run_qhull_command(['qvoronoi','p QV13'], data, float, 2)
 
+def get_lines_voronoi(data):
+    prep_str = str(len(data[0])) + "\n"
+    prep_str += str(len(data)) +"\n"
+    prep_str += "\n".join([' '.join([str(i) for i in row]) for row in data])
+    print prep_str
+    p = subprocess.Popen(['qconvex', 'o'], stdout = subprocess.PIPE,stdin = subprocess.PIPE, close_fds = True)
+    output = p.communicate(input = prep_str)[0]
+    output = re.split("\n", output)
+    print output
+    nb_points=int(output[1].split(" ")[0])
+    points=[]
+    list_lines=[]
+    list_points=[]
+    for i in range(2,2+nb_points):
+        #if(not output[i]==''):
+            #print [float(c) for c in output[i].strip().split(" ")]
+            list_points.append([float(c) for c in output[i].strip().split(" ")])
+    #print len(list_points)
+    facets=[]
+    for i in range(2+nb_points,len(output)):
+        if (output[i]==''):
+            continue
+        tmp=output[i].strip().split(" ")
+        #if(len(tmp)<=4):
+           # continue
+        facets.append([int(tmp[j]) for j in range(1,len(tmp)) ])
+        #for j in range(1,len(indices)-1):
+            #check
+            #list_lines.append({'start':list_points[indices[j]],'end':list_points[indices[j+1]]})
+    #go through each combinations of two vertices and see if it is included in two facets
+    import itertools
+    import numpy
+    import math
+    #import pylab as plt
+    #from mpl_toolkits.mplot3d import Axes3D
+    
+    for i in range(len(facets)):
+        #print i
+        print "start"+str(facets[i])
+        #vector1=numpy.array(list_points[facets[i][0]])
+        #vector2=numpy.array(list_points[facets[i][1]])
+        #n2=numpy.cross(vector1,vector2)
+        #fig = plt.figure()
+        #ax=Axes3D(fig)
+        #ax.scatter([list_points[facets[i][j]][0] for j in range(len(facets[i]))],[list_points[facets[i][j]][1] for j in range(len(facets[i]))],[list_points[facets[i][j]][2] for j in range(len(facets[i]))],color='r')
+        #plt.show()
+        for line in itertools.combinations(facets[i],2):
+            #print line[0]
+            #print line[1]
+            for j in range(len(facets)):
+                if(i==j):
+                    continue
+                if(line[0] in facets[j] and line[1] in facets[j]):
+                    #check if the two facets i and j are not coplanar
+                    vector1=numpy.array(list_points[facets[j][0]])-numpy.array(list_points[facets[j][1]])
+                    vector2=numpy.array(list_points[facets[j][0]])-numpy.array(list_points[facets[j][2]])
+                    n1=numpy.cross(vector1,vector2)
+                    vector1=numpy.array(list_points[facets[i][0]])-numpy.array(list_points[facets[i][1]])
+                    vector2=numpy.array(list_points[facets[i][0]])-numpy.array(list_points[facets[i][2]])
+                    n2=numpy.cross(vector1,vector2)
+                    
+                    dot=math.fabs(numpy.dot(n1,n2)/(numpy.linalg.norm(n1)*numpy.linalg.norm(n2)))
+                    print "common"+str(facets[j])
+                    print n1
+                    print n2
+                    print dot
+                    if(dot<1.05 and dot>0.95):
+                        continue
+                    #print j
+                   # print facets[j]
+                    list_lines.append({'start':list_points[line[0]],'end':list_points[line[1]]})
+                    break
+    print list_lines        
+    return list_lines
+    #list=run_qhull_command(['qconvex','o'], data, float, 1)
+    #print list
+    #return None
+    
