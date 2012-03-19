bd0e77289350d95ffabda15aa02eed710490896e
Maarten de Jong <MDEJONG1@fenrir.bw01.berkeley.edu>
2012-03-18 19:38:49 -0700

    considerably expanded test_fit_elas, added r-squared method, started fitting method for Cij
    Former-commit-id: 445d541fd05a4a5e6544e7dec7360f71727c6c35 [formerly 4f427a10edc41324f22af8b50419d1968c629729]
    Former-commit-id: a300367d8cbeb5b0797bc12a353478781ca231c4

--- pymatgen/phonons/test_fit_elas.py
+++ pymatgen/phonons/test_fit_elas.py
@@ -1,5 +1,6 @@
 import warnings
 import sys
+import operator
 sys.path.append('/home/MDEJONG1/pythonplayground/pymatgen/pymatgen_repo/') # (If one does not want to change $PYTHONPATH)
 import unittest
 from StringIO import StringIO as sio
@@ -15,9 +16,13 @@ from pymatgen.transformations.standard_transformations import *
 from pymatgen.core.structure_modifier import StructureEditor
 import numpy as np
 import random
+from scipy import stats
+import math
+
 from pymatgen.phonons.strain import Strain
 from pymatgen.phonons.strain import IndependentStrain
 from pymatgen.phonons.genstrain import DeformGeometry
+
 
 if __name__ == "__main__":
 
@@ -36,46 +41,195 @@ if __name__ == "__main__":
 
 #   stress_dict: {IndependentStrain:stress matrix}
 
-    strain_array = []
-    count = 0
-    
-    for c in D:
-#        stress_dict[c] = np.matrix(np.loadtxt(sio('\n'.join(lines[3*count:3*count+3]))))
-        if c.i==0 and c.j==0:
-            strain_array.append(c.strain[c.i, c.j])
+    strain_array1 = []
+    strain_array2 = []
+    strain_array3 = []
+    strain_array4 = []
+    strain_array5 = []
+    strain_array6 = []
 
     count = 0
-    strain_array = sorted(strain_array)
     
-    for n in strain_array:
+    numdefnormal = 0
+    numdefshear = 0
+
+    for c in D:
+        if c.i==0 and c.j==0:
+            strain_array1.append(c.strain[c.i, c.j])
+            numdefnormal +=1
+
+        elif c.i==1 and c.j==1:
+            strain_array2.append(c.strain[c.i, c.j])
+
+        elif c.i==2 and c.j==2:
+            strain_array3.append(c.strain[c.i, c.j])
+
+        elif c.i==1 and c.j==2:
+            strain_array4.append(c.strain[c.i, c.j])
+            numdefshear +=1
+
+        elif c.i==0 and c.j==2:
+            strain_array5.append(c.strain[c.i, c.j])
+
+        elif c.i==0 and c.j==1:
+            strain_array6.append(c.strain[c.i, c.j])
+        
+    count = 0
+    
+    strain_array1 = sorted(strain_array1)
+    strain_array2 = sorted(strain_array2)
+    strain_array3 = sorted(strain_array3)
+    strain_array4 = sorted(strain_array4)
+    strain_array5 = sorted(strain_array5)
+    strain_array6 = sorted(strain_array6)
+
+#   fill up 
+
+    for n in strain_array1:
         for c in D:
             if c.i==0 and c.j==0 and np.abs(c.strain[c.i, c.j] - n) < 0.00001:
                 stress_dict[c] = np.matrix(np.loadtxt(sio('\n'.join(lines[3*count:3*count+3]))))
                 count+=1 
 
+    for n in strain_array2:
+        for c in D:
+            if c.i==1 and c.j==1 and np.abs(c.strain[c.i, c.j] - n) < 0.00001:
+                stress_dict[c] = np.matrix(np.loadtxt(sio('\n'.join(lines[3*count:3*count+3]))))
+                count+=1
 
-    
+    for n in strain_array3:
+        for c in D:
+            if c.i==2 and c.j==2 and np.abs(c.strain[c.i, c.j] - n) < 0.00001:
+                stress_dict[c] = np.matrix(np.loadtxt(sio('\n'.join(lines[3*count:3*count+3]))))
+                count+=1
+
+    for n in strain_array4:
+        for c in D:
+            if c.i==1 and c.j==2 and np.abs(c.strain[c.i, c.j] - n) < 0.00001:
+                stress_dict[c] = np.matrix(np.loadtxt(sio('\n'.join(lines[3*count:3*count+3]))))
+                count+=1
+
+    for n in strain_array5:
+        for c in D:
+            if c.i==0 and c.j==2 and np.abs(c.strain[c.i, c.j] - n) < 0.00001:
+                stress_dict[c] = np.matrix(np.loadtxt(sio('\n'.join(lines[3*count:3*count+3]))))
+                count+=1
+
+    for n in strain_array6:
+        for c in D:
+            if c.i==0 and c.j==1 and np.abs(c.strain[c.i, c.j] - n) < 0.00001:
+                stress_dict[c] = np.matrix(np.loadtxt(sio('\n'.join(lines[3*count:3*count+3]))))
+                count+=1
+
+#   at this point, the stress_dict is created in such a way that fit_elas can operate on it, i.e. it is in the format stress_dict: {IndependentStrain:stress matrix}
+
+    def sort_stress_strain(strain, stress):
+        indices = [i for (i,j) in sorted(enumerate(strain), key=operator.itemgetter(1))]
+        epsilon = sorted(strain)
+        sigma = []
+
+        for i in indices:
+            sigma.append(-1.000*stress[i])
+        return epsilon, sigma
+
+    def chain_stresses(stress, ind1, ind2):
+        s = []
+        for el in stress:
+            s.append(el[ind1,ind2])
+        return s
+
+    def rsquared(yi, fi):
+
+        ybar = 1.0/len(yi)*sum(yi)
+        v1 = yi-fi
+        v2 = yi-ybar
+        
+        sum1 = 0
+        sum2 = 0
+
+        for n in v1:
+            sum1 = sum1 + n**2
+
+        for n in v2:
+            sum2 = sum2 + n**2
+
+        r2 = 1.0-sum1/sum2
+#        print math.isnan(1-sum1/sum2)
+
+        return r2
+
+    def fitCij(stress_dict):
+
+        Cij = np.zeros((6, 6))
+
+        eps11 = []
+        eps22 = []
+        eps33 = []
+        eps12 = []
+        eps23 = []
+        eps13 = []
+
+        stress11 = []
+        stress22 = []
+        stress33 = []
+        stress12 = []
+        stress23 = []
+        stress13 = []
+
+        for c in stress_dict:
+
+            if c.i==0 and c.j==0:
+                eps11.append(c.strain[c.i, c.j])
+                stress11.append(stress_dict[c])
+
+            if c.i==1 and c.j==1:
+                eps22.append(c.strain[c.i, c.j])
+                stress22.append(stress_dict[c])
+
+            if c.i==2 and c.j==2:
+                eps33.append(c.strain[c.i, c.j])
+                stress33.append(stress_dict[c])
+
+            if c.i==0 and c.j==1:
+                eps12.append(c.strain[c.i, c.j])
+                stress12.append(stress_dict[c])
+
+            if c.i==1 and c.j==2:
+                eps23.append(c.strain[c.i, c.j])
+                stress23.append(stress_dict[c])
+
+            if c.i==0 and c.j==2:
+                eps13.append(c.strain[c.i, c.j])
+                stress13.append(stress_dict[c])
 
 
-#    print stress_dict
-             
+        [epsilon11, sigma11] = sort_stress_strain(eps11,stress11)
+        [epsilon22, sigma22] = sort_stress_strain(eps22,stress22)
+        [epsilon33, sigma33] = sort_stress_strain(eps33,stress33)
+        [epsilon12, sigma12] = sort_stress_strain(eps12,stress12)
+        [epsilon23, sigma23] = sort_stress_strain(eps23,stress23)
+        [epsilon13, sigma13] = sort_stress_strain(eps13,stress13)
 
-#    print strain_array
+        inds = [[0,0], [1,1], [2,2], [1,2], [0,2], [0,1]]
+
+        count1 = 0
+        for k in inds:
+
+            true_data = chain_stresses(sigma11, k[0], k[1])
+
+            p1 = np.polyfit(epsilon11, true_data, 1)
+            p2 = np.polyfit(epsilon11, true_data, 2)
+            p3 = np.polyfit(epsilon11, true_data, 3)
+
+            f1 = np.polyval(p1, epsilon11)
+            f2 = np.polyval(p2, epsilon11)
+            f3 = np.polyval(p3, epsilon11)
+
+            r2 = rsquared(true_data, f1)
+            print 0.10*p1[0]
 
 
-    for mode in stress_dict:
-
-#        print stress_dict[mode]
-        print mode._dfm
+    fitCij(stress_dict)
 
 
-    
-#       if mode.i==0 and mode.j==0:
 
-#           print mode.strain
-
-#        print mode.i, mode.j, mode.strain[mode.i, mode.j]       
-#        print i
-#        print stress_dict[i]
-    
- 
