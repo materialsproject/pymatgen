22e87daf151cdf49bd2c8991bf25bfb1fce3c42e
Shyue Ping Ong <shyue@mit.edu>
2012-03-20 19:36:38 -0400

    Fix Mo U
    Former-commit-id: 34cea2271667f35b6fdc7227f9391209ca4fc349 [formerly 944d6a5363a23791a13d67b4619eca228862035a]
    Former-commit-id: fa398762965420dc7991e1ef935beca56685d176

--- pymatgen/electronic_structure/dos.py
+++ pymatgen/electronic_structure/dos.py
@@ -8,7 +8,7 @@ from __future__ import division
 
 __author__ = "Shyue Ping Ong"
 __copyright__ = "Copyright 2012, The Materials Project"
-__version__ = "0.1"
+__version__ = "1.0"
 __maintainer__ = "Shyue Ping Ong"
 __email__ = "shyue@mit.edu"
 __date__ = "Mar 20, 2012"
