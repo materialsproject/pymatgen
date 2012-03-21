bc286a3ba9cab52845df82f345ac80655e6da46a
Shyue Ping Ong <shyue@mit.edu>
2012-03-20 22:46:23 -0400

    Improvement in internal representations of Spin and Orbital classes. Much more robust unittesting of these two highly important classes.

--- pymatgen/electronic_structure/tests/test_core.py
+++ pymatgen/electronic_structure/tests/test_core.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python
 
 import unittest
-import os
 
 from pymatgen.electronic_structure.core import Orbital, Spin
-
-import pymatgen
-
-test_dir = os.path.join(os.path.dirname(os.path.abspath(pymatgen.__file__)), '..', 'test_files')
 
 class SpinTest(unittest.TestCase):
 
@@ -15,11 +10,23 @@ class SpinTest(unittest.TestCase):
         self.assertEquals(int(Spin.up), 1)
         self.assertEquals(int(Spin.down), -1)
 
+    def test_from_int(self):
+        self.assertEquals(Spin.from_int(1), Spin.up)
+        self.assertEquals(Spin.from_int(-1), Spin.down)
+        self.assertRaises(ValueError, Spin.from_int, 0)
+
+    def test_cached(self):
+        self.assertEquals(id(Spin.from_int(1)), id(Spin.up))
+
 class OrbitalTest(unittest.TestCase):
 
     def test_init(self):
-        self.assertEqual(Orbital.from_vasp_index(1), Orbital.py)
+        for i, orb in enumerate(Orbital.all_orbitals):
+            self.assertEqual(Orbital.from_vasp_index(i), orb)
+        self.assertRaises(IndexError, Orbital.from_vasp_index, 100)
 
+    def test_cached(self):
+        self.assertEquals(id(Orbital.from_vasp_index(0)), id(Orbital.s))
 
 if __name__ == '__main__':
     unittest.main()
