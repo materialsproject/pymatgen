78d6361c721b04cfecd518b109e0fc1d8eaac5d2
Maarten de Jong <MDEJONG1@fenrir.bw01.berkeley.edu>
2012-03-23 09:40:42 -0700

    Created class for general operations on tensors/matrices

---
+++ pymatgen/phonons/tensors.py
@@ -0,0 +1,32 @@
+import warnings, sys, os
+sys.path.append('/home/MDEJONG1/pythonplayground/pymatgen/pymatgen_repo/') # (If one does not want to change $PYTHONPATH)
+import unittest
+import pymatgen
+from pymatgen.io.vaspio import Poscar
+from pymatgen.io.vaspio import Poscar
+from pymatgen.io.vaspio import Vasprun
+from pymatgen.io.cifio import CifWriter
+from pymatgen.io.cifio import CifParser
+from pymatgen.core.lattice import Lattice
+from pymatgen.core.structure import Structure
+from pymatgen.transformations.standard_transformations import *
+from pymatgen.core.structure_modifier import StructureEditor
+import numpy as np
+
+__author__="Maarten de Jong"
+__copyright__ = "Copyright 2012, The Materials Project"
+__credits__ = "Mark Asta, Anubhav Jain"
+__version__ = "1.0"
+__maintainer__ = "Maarten de Jong"
+__email__ = "maartendft@gmail.com"
+__status__ = "Development"
+__date__ ="March 22, 2012"
+
+
+class Cij(object):
+ 
+    def __init__(self, stress_matrix):
+        self._sigma = stress_matrix
+
+
+
