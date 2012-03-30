18456e7be294c867841df8f13ff220ba28b2cbac
Maarten de Jong <MDEJONG1@fenrir.bw01.berkeley.edu>
2012-03-29 23:12:00 -0700

    genstrain is not a function, not a class; class was not reauired here

--- pymatgen/phonons/genstrain.py
+++ pymatgen/phonons/genstrain.py
@@ -27,83 +27,56 @@ __email__ = "maartendft@gmail.com"
 __status__ = "Development"
 __date__ ="Jan 24, 2012"
 
-class DeformGeometry(object):
-
-    """
-    Class that takes a pymatgen core structure object and applies a range of deformation with the aim at determining the elastic tensor.
-
-    Args:
-        - rlxd_str: pymatgen core structure object (fully relaxed)
-    """
-
-    def __init__(self, rlxd_str):
-        self._rlxd_str = rlxd_str
-
-    def deform(self, nd=0.02, ns=0.02, m=4, n=4): 
-        """
-        This method applies deformations and stores the result in a dict with StrainObjects as keys
-
-        Args:
-            - nd, ns: maximum amount of normal and shear deformation, respectively
-            - m,n: number of normal and shear deformation, respectively
-        """	
+def DeformGeometry(rlxd_str, nd=0.02, ns=0.02, m=4, n=4):
         
-        self.msteps = np.int(m)
-        self.nsteps = np.int(n)
+    msteps = np.int(m)
+    nsteps = np.int(n)
 
-        if m%2!=0:
-            raise ValueError("m has to be even.")
+    if m%2!=0:
+        raise ValueError("m has to be even.")
          
-        if n%2!=0:
-            raise ValueError("n has to be even.")
+    if n%2!=0:
+        raise ValueError("n has to be even.")
         
-        mystrains = np.zeros((3, 3, np.int(m*3) + np.int(n*3)))
+    mystrains = np.zeros((3, 3, np.int(m*3) + np.int(n*3)))
         
-        defs = np.linspace(-nd, nd, num=m+1)
-        defs = np.delete(defs, np.int(m/2), 0)
-        sheardef = np.linspace(-ns, ns, num=n+1)
-        sheardef = np.delete(sheardef, np.int(n/2), 0)
-        defstructures = {}
+    defs = np.linspace(-nd, nd, num=m+1)
+    defs = np.delete(defs, np.int(m/2), 0)
+    sheardef = np.linspace(-ns, ns, num=n+1)
+    sheardef = np.delete(sheardef, np.int(n/2), 0)
+    defstructures = {}
 
-        # First apply normal deformations
-        for i1 in range(0, 3):
+    # First apply normal deformations
+    for i1 in range(0, 3):
 
-            for i2 in range(0, len(defs)):
+        for i2 in range(0, len(defs)):
 
-                s = StructureEditor(self._rlxd_str)
-                F = np.identity(3)
-                F[i1, i1] = F[i1, i1] + defs[i2] 
-                StrainObject = IndependentStrain(F)
-                s.apply_strain_transformation(F)      
-                defstructures[StrainObject] = s.modified_structure
+            s = StructureEditor(rlxd_str)
+            F = np.identity(3)
+            F[i1, i1] = F[i1, i1] + defs[i2] 
+            StrainObject = IndependentStrain(F)
+            s.apply_strain_transformation(F)      
+            defstructures[StrainObject] = s.modified_structure
 
-        # Now apply shear deformations #		
-        F_index = [[0, 1], [0, 2], [1, 2]]
-        for j1 in range(0, 3):
+    # Now apply shear deformations #		
+    F_index = [[0, 1], [0, 2], [1, 2]]
+    for j1 in range(0, 3):
 		
-            for j2 in range(0, len(sheardef)):
+        for j2 in range(0, len(sheardef)):
 
-                s = StructureEditor(self._rlxd_str)
-                F = np.identity(3)
-                F[F_index[j1][0], F_index[j1][1]] = F[F_index[j1][0], F_index[j1][1]] + sheardef[j2]
-#                F = np.matrix(F)   # this needs to be checked carefully
-                StrainObject = IndependentStrain(F)
-                s.apply_strain_transformation(F)   
-                defstructures[StrainObject] = s.modified_structure          
+            s = StructureEditor(rlxd_str)
+            F = np.identity(3)
+            F[F_index[j1][0], F_index[j1][1]] = F[F_index[j1][0], F_index[j1][1]] + sheardef[j2]
+#           F = np.matrix(F)   # this needs to be checked carefully
+            StrainObject = IndependentStrain(F)
+            s.apply_strain_transformation(F)   
+            defstructures[StrainObject] = s.modified_structure          
 
-        self.defstructures = defstructures
-        
-        return self.defstructures
-
+    return defstructures
 
 if __name__ == "__main__":
 
     struct = CifParser('/home/MDEJONG1/pythonplayground/pymatgen/pymatgen_repo/pymatgen/phonons/aluminum.cif').get_structures()[0]
     D = DeformGeometry(struct)    
-    D = D.deform()
-    mynum = np.float64(35.91)
-#    print type(D)
-#    print D[mynum]
-   
 
 
