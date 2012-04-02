29df2536b9d2f326ff665987641d08e0e67554ef
Shyue Ping Ong <shyue@mit.edu>
2012-04-02 13:57:17 -0400

    Update unittests. Update version numbers to 1.7.0.
    Former-commit-id: d5f5a2eeb4863366c235ae5392fa859d6d771388 [formerly 50ec5cad42be08b565ce65ccec3c3e17a7a4c71a]
    Former-commit-id: b0f4bee0045c0938c3d84375e2167cc68e0d5352

--- setup.py
+++ setup.py
@@ -27,22 +27,22 @@ at http://materialsproject.github.com/pymatgen.
 """
 
 setup (
-  name = 'pymatgen',
-  version = '1.6.0',
-  packages = find_packages(),
-  install_requires = ['numpy', 'scipy', 'PyCIFRW'],
-  package_data = {'pymatgen.core': ['*.json'],
+  name='pymatgen',
+  version='1.7.0',
+  packages=find_packages(),
+  install_requires=['numpy', 'scipy', 'PyCIFRW'],
+  package_data={'pymatgen.core': ['*.json'],
                   'pymatgen.io': ['*.cfg'],
                   'pymatgen.vis': ['ElementColorSchemes.cfg']},
-  author = 'Shyue Ping Ong, Anubhav Jain, Michael Kocher, Geoffroy Hautier, Will Richards, Dan Gunter, Vincent L Chevrier, Rickard Armiento',
-  author_email = 'shyue@mit.edu, anubhavj@mit.edu, mpkocher@lbnl.gov, geoffroy.hautier@uclouvain.be, wrichard@mit.edu, dkgunter@lbl.gov, vincentchevrier@gmail.com, armiento@mit.edu',
-  maintainer = 'Shyue Ping Ong',
-  url = 'https://github.com/materialsproject/pymatgen/',
-  license = 'MIT',
-  description = "pymatgen is the Python library powering the Materials Project (www.materialsproject.org).",
-  long_description = long_description,
-  keywords = ["vasp", "materials", "project", "electronic", "structure"],
-  classifiers = [
+  author='Shyue Ping Ong, Anubhav Jain, Michael Kocher, Geoffroy Hautier, Will Richards, Dan Gunter, Vincent L Chevrier, Rickard Armiento',
+  author_email='shyue@mit.edu, anubhavj@mit.edu, mpkocher@lbnl.gov, geoffroy.hautier@uclouvain.be, wrichard@mit.edu, dkgunter@lbl.gov, vincentchevrier@gmail.com, armiento@mit.edu',
+  maintainer='Shyue Ping Ong',
+  url='https://github.com/materialsproject/pymatgen/',
+  license='MIT',
+  description="pymatgen is the Python library powering the Materials Project (www.materialsproject.org).",
+  long_description=long_description,
+  keywords=["vasp", "materials", "project", "electronic", "structure"],
+  classifiers=[
         "Programming Language :: Python :: 2.7",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
@@ -53,8 +53,8 @@ setup (
         "Topic :: Scientific/Engineering :: Chemistry",
         "Topic :: Software Development :: Libraries :: Python Modules",
   ],
-  download_url = "https://github.com/materialsproject/pymatgen/tarball/master",
-  test_suite = 'nose.collector',
-  test_requires = ['nose']
+  download_url="https://github.com/materialsproject/pymatgen/tarball/master",
+  test_suite='nose.collector',
+  test_requires=['nose']
 )
 
