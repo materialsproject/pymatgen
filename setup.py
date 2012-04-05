6345ae5fe28d14cd50e17779ac9ab1d00067a65b
Shyue Ping Ong <shyue@mit.edu>
2012-04-05 14:23:54 -0400

    Cut a new release of pymatgen to v1.7.1 to take into account bandstrucutre mods
    Former-commit-id: 6990fad3c797971fdc3b92065a0b206d904fb2b1 [formerly 3792fcab01d3440907159b1729839f88e011616e]
    Former-commit-id: ec070269430e83a8b666a90505b532adaa63c7e1

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ use_setuptools()
 from setuptools import setup, find_packages
 
 long_description = """
-Pymatgen is the python library that powers the Materials Project 
-(http://www.materialsproject.org). These are some of the key features:
+Pymatgen (python materials genomics) is the python library that powers the 
+Materials Project (http://www.materialsproject.org). These are some of the main 
+features:
 
-1. Highly flexible classes for the representation of Element, Site, Structure 
-   objects.
-2. Powerful io capabilities to manipulate many VASP input and output files 
-   (http://cms.mpi.univie.ac.at/vasp/) and the crystallographic information 
-   file format.  This includes generating Structure objects from vasp input and 
-   output.
-3. A comprehensive tool to generate and view compositional and grand canonical 
-   phase diagrams.
+1. Highly flexible classes for the representation of Element, Site, Structure objects.
+2. Extensive io capabilities to manipulate many VASP input and output files 
+   (http://cms.mpi.univie.ac.at/vasp/) and the crystallographic information file 
+   format.  This includes generating Structure objects from vasp input and output.
+3. Comprehensive tool to generate and view compositional and grand canonical phase 
+   diagrams.
+4. Electronic structure analyses (DOS and Bandstructure).
 
 The public version of pymatgen is free (as in free beer) to download and to use. 
 However, we would also like you to help us improve this library by making your 
@@ -22,13 +22,15 @@ tools or modules you develop, or even simple things such as bug reports.  Please
 contact the maintainer of this library (shyue@mit.edu) to find out how to include 
 your contributions via github or for bug reports.
 
-For documentation, usage examples and change log, please read the documentation 
-at http://materialsproject.github.com/pymatgen.
+Note that pymatgen, like all scientific research, will always be a work in
+progress. While the development team will always strive to avoid backward 
+incompatible changes, they are sometimes unavoidable, and tough decisions have 
+to be made for the long term health of the code.
 """
 
 setup (
   name='pymatgen',
-  version='1.7.0',
+  version='1.7.1',
   packages=find_packages(),
   install_requires=['numpy', 'scipy', 'PyCIFRW'],
   package_data={'pymatgen.core': ['*.json'],
