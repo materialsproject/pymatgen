0ac235aca335ac83a7f3d2e932dd0091ec3d9406
Shyue Ping Ong <shyue@mit.edu>
2012-04-06 17:16:41 -0400

    Fix vaspio U value for oxide and fluorides.
    Former-commit-id: 658753bc263e9e878542dc84a979b271d1bdb394 [formerly 8e443787ee1e98605f4c338a2270ebab0b972838]
    Former-commit-id: 90975c453897d0edb5062181dfd373cb55824148

--- pymatgen/io/tests/test_vaspio_set.py
+++ pymatgen/io/tests/test_vaspio_set.py
@@ -12,53 +12,66 @@ from numpy import array
 test_dir = os.path.join(os.path.dirname(os.path.abspath(pymatgen.__file__)), '..', 'test_files')
 
 class MITMaterialsProjectVaspInputSetTest(unittest.TestCase):
-    
+
     def setUp(self):
-        filepath = os.path.join(test_dir,'POSCAR')
+        filepath = os.path.join(test_dir, 'POSCAR')
         poscar = Poscar.from_file(filepath)
         self.struct = poscar.struct
-        
+
         self.mitparamset = MITVaspInputSet()
         self.mithseparamset = MITHSEVaspInputSet()
         self.paramset = MaterialsProjectVaspInputSet()
-        
+
     def test_get_potcar_symbols(self):
         syms = self.paramset.get_potcar_symbols(self.struct)
         self.assertEquals(syms, ['Fe_pv', 'P', 'O'])
-        
+
     def test_get_incar(self):
         incar = self.paramset.get_incar(self.struct)
         self.assertEqual(incar['LDAUU'], [5.3, 0, 0])
         self.assertAlmostEqual(incar['EDIFF'], 0.0012)
-        
+
         incar = self.mitparamset.get_incar(self.struct)
         self.assertEqual(incar['LDAUU'], [4.0, 0, 0])
         self.assertAlmostEqual(incar['EDIFF'], 0.0012)
-         
+
         si = 14
         coords = list()
-        coords.append(array([0,0,0]))
-        coords.append(array([0.75,0.5,0.75]))
+        coords.append(array([0, 0, 0]))
+        coords.append(array([0.75, 0.5, 0.75]))
 
         #Silicon structure for testing.
-        latt = Lattice(array([[ 3.8401979337, 0.00, 0.00],[1.9200989668, 3.3257101909, 0.00],[0.00,-2.2171384943,3.1355090603]]))
-        struct = Structure(latt,[si,si],coords)
+        latt = Lattice(array([[ 3.8401979337, 0.00, 0.00], [1.9200989668, 3.3257101909, 0.00], [0.00, -2.2171384943, 3.1355090603]]))
+        struct = Structure(latt, [si, si], coords)
         incar = incar = self.paramset.get_incar(struct)
         self.assertNotIn("LDAU", incar)
-        
+
         incar = self.mithseparamset.get_incar(self.struct)
         self.assertTrue(incar['LHFCALC'])
-        
-        
+
+        coords = list()
+        coords.append([0, 0, 0])
+        coords.append([0.75, 0.5, 0.75])
+        lattice = Lattice([[ 3.8401979337, 0.00, 0.00], [1.9200989668, 3.3257101909, 0.00], [0.00, -2.2171384943, 3.1355090603]])
+        struct = Structure(lattice, ["Fe", "Mn"], coords)
+
+        incar = self.paramset.get_incar(struct)
+        self.assertNotIn('LDAU', incar)
+
+        struct = Structure(lattice, ["Fe", "F"], coords)
+
+        incar = self.paramset.get_incar(struct)
+        self.assertEqual(incar['LDAUU'], [5.3, 0])
+
     def test_get_kpoints(self):
         kpoints = self.paramset.get_kpoints(self.struct)
-        self.assertEquals(kpoints.kpts, [[2,4,6]])
+        self.assertEquals(kpoints.kpts, [[2, 4, 6]])
         self.assertEquals(kpoints.style, 'Monkhorst')
-        
+
         kpoints = self.mitparamset.get_kpoints(self.struct)
-        self.assertEquals(kpoints.kpts, [[2,4,4]])
+        self.assertEquals(kpoints.kpts, [[2, 4, 4]])
         self.assertEquals(kpoints.style, 'Monkhorst')
-        
+
 if __name__ == '__main__':
     unittest.main()
 
