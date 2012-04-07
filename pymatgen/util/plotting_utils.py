8ae65a2ff8f1f10cc08e92870f41ad5f001dbae9
Shyue Ping Ong <shyue@mit.edu>
2012-04-07 16:26:27 -0400

    Add Gaussian smearing to plot utilities. Add method to get smeared densities from DOS. Add option to plot dos with smearing.

--- pymatgen/util/plotting_utils.py
+++ pymatgen/util/plotting_utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 '''
-Created on Mar 13, 2012
+Utilities for generating nicer plots.
 '''
 
 from __future__ import division
@@ -13,20 +13,55 @@ __maintainer__ = "Shyue Ping Ong"
 __email__ = "shyue@mit.edu"
 __date__ = "Mar 13, 2012"
 
-
-import matplotlib.pyplot as plt
 import math
 
+import matplotlib.pyplot as plt
+import numpy as np
 
-def get_publication_quality_plot(width = 8, height = None):
+def gaussian_smear(x, y, mean_x, sigma):
+    """
+    Convolute densities with gaussian smearing function.
+    
+    Args:
+        x:
+            Sequence of x values (independent variable). Must contain mean_x
+            within sequence.
+        y:
+            Sequence of y values (dependent variable). Must be same length as 
+            x.
+        mean_x:
+            Mean of smearing Gaussian function.
+        sigma:
+            Standard deviation of smearing Gaussian function.
+            
+    Returns:
+        Smeared y values.
+    """
+    from scipy.signal import convolve
+    gauss = [1 / sigma / math.sqrt(2 * math.pi) * math.exp(-(val - mean_x) ** 2 / (2 * sigma ** 2)) for val in x]
+    #need to normalize the gaussian pdf values to avoid scaling output
+    gauss = np.array(gauss) / sum(gauss)
+    smeared_y = convolve(gauss, y, 'full')
+    #Locate index of the mean level, needed to perform shift
+    shift = None
+    for i in xrange(len(x)):
+        if x[i] > mean_x:
+            shift = i
+            break
+    if not shift:
+        raise ValueError("mean_x not in range of x array.")
+    return smeared_y[shift:shift + len(y)]
+
+
+def get_publication_quality_plot(width=8, height=None):
     golden_ratio = (math.sqrt(5) - 1.0) / 2.0
     if not height:
         height = int(width * golden_ratio)
-    plt.figure(figsize = (width, height), facecolor = 'w')
-    plt.ylabel('', fontsize = 36)
-    plt.xlabel('', fontsize = 36)
-    plt.xticks(fontsize = 30)
-    plt.yticks(fontsize = 30)
-    plt.title('', fontsize = 26)
+    plt.figure(figsize=(width, height), facecolor='w')
+    plt.ylabel('', fontsize=36)
+    plt.xlabel('', fontsize=36)
+    plt.xticks(fontsize=30)
+    plt.yticks(fontsize=30)
+    plt.title('', fontsize=26)
 
     return plt
