3499e956d4dd07034f101d93c0ff3c8e7d6cddcd
Shyue Ping Ong <shyue@mit.edu>
2012-04-07 20:29:05 -0400

    Switch to scipy.ndimage.filter.gaussian1d instead for Gaussian smearing.

--- pymatgen/util/plotting_utils.py
+++ pymatgen/util/plotting_utils.py
@@ -16,41 +16,6 @@ __date__ = "Mar 13, 2012"
 import math
 
 import matplotlib.pyplot as plt
-import numpy as np
-
-def gaussian_smear(x, y, mean_x, sigma):
-    """
-    Convolute densities with gaussian smearing function.
-    
-    Args:
-        x:
-            Sequence of x values (independent variable). Must contain mean_x
-            within sequence.
-        y:
-            Sequence of y values (dependent variable). Must be same length as 
-            x.
-        mean_x:
-            Mean of smearing Gaussian function.
-        sigma:
-            Standard deviation of smearing Gaussian function.
-            
-    Returns:
-        Smeared y values.
-    """
-    from scipy.signal import convolve
-    gauss = [1 / sigma / math.sqrt(2 * math.pi) * math.exp(-(val - mean_x) ** 2 / (2 * sigma ** 2)) for val in x]
-    #need to normalize the gaussian pdf values to avoid scaling output
-    gauss = np.array(gauss) / sum(gauss)
-    smeared_y = convolve(gauss, y, 'full')
-    #Locate index of the mean level, needed to perform shift
-    shift = None
-    for i in xrange(len(x)):
-        if x[i] > mean_x:
-            shift = i
-            break
-    if not shift:
-        raise ValueError("mean_x not in range of x array.")
-    return smeared_y[shift:shift + len(y)]
 
 
 def get_publication_quality_plot(width=8, height=None):
