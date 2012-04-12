ece1ed136e03f795877319fb939853b59932871e
Shyue Ping Ong <shyue@mit.edu>
2012-04-12 12:26:40 -0400

    Move new TransformedStructure format to master.
    Former-commit-id: 985770e93af71f5dca092c11f4d2f6360413dbdd [formerly e5b6c8ed6b45d8298c4cd68b2deee7b66d8fd2e3]
    Former-commit-id: 6bb19b6501964d0536032d3f5c5988b606e95096

--- pymatgen/alchemy/tests/test_transmuters.py
+++ pymatgen/alchemy/tests/test_transmuters.py
@@ -6,7 +6,7 @@ Created on Mar 5, 2012
 
 from __future__ import division
 
-__author__="Shyue Ping Ong"
+__author__ = "Shyue Ping Ong"
 __copyright__ = "Copyright 2012, The Materials Project"
 __version__ = "0.1"
 __maintainer__ = "Shyue Ping Ong"
@@ -15,7 +15,6 @@ __date__ = "Mar 5, 2012"
 
 import unittest
 import os
-from pymatgen.alchemy.materials import TransformedStructure
 from pymatgen.alchemy.transmuters import TransformedStructureTransmuter
 from pymatgen.transformations.standard_transformations import SubstitutionTransformation, RemoveSpeciesTransformation, OrderDisorderedStructureTransformation
 
@@ -24,7 +23,7 @@ test_dir = os.path.join(os.path.dirname(os.path.abspath(pymatgen.__file__)), '..
 
 
 class TransformedStructureTransmuterTest(unittest.TestCase):
-        
+
     def test_cif_transmute(self):
         trans = []
         trans.append(SubstitutionTransformation({"Fe":"Mn", "Fe2+":"Mn2+"}))
@@ -34,7 +33,7 @@ class TransformedStructureTransmuterTest(unittest.TestCase):
         for s in tsc:
             els = set([el.symbol for el in s.final_structure.composition.elements])
             self.assertEqual(expected_ans, els)
-          
+
     def test_poscar_transmute(self):
         trans = []
         trans.append(SubstitutionTransformation({"Fe":"Mn"}))
@@ -44,28 +43,28 @@ class TransformedStructureTransmuterTest(unittest.TestCase):
         for s in tsc:
             els = set([el.symbol for el in s.final_structure.composition.elements])
             self.assertEqual(expected_ans, els)
-            
+
     def test_transmuter(self):
         tsc = TransformedStructureTransmuter.from_poscars([os.path.join(test_dir, "POSCAR")])
         tsc.append_transformation(RemoveSpeciesTransformation('O'))
         self.assertEqual(len(tsc[0].final_structure), 8)
-        
+
         tsc.append_transformation(SubstitutionTransformation({"Fe":{"Fe2+":.25, "Mn3+":.75}, "P":"P5+"}))
-        tsc.append_transformation(OrderDisorderedStructureTransformation(num_structures = 50), extend_collection = True)
+        tsc.append_transformation(OrderDisorderedStructureTransformation(num_structures=50), extend_collection=True)
         self.assertEqual(len(tsc), 4)
-        
-        tsc.branch_collection([SubstitutionTransformation({"Fe2+":"Mg2+"}),SubstitutionTransformation({"Fe2+":"Zn2+"})], retention_level = 1)
+
+        tsc.branch_collection([SubstitutionTransformation({"Fe2+":"Mg2+"}), SubstitutionTransformation({"Fe2+":"Zn2+"})], retention_level=1)
         self.assertEqual(len(tsc), 8)
-        
-        tsc.branch_collection([SubstitutionTransformation({"Zn2+":"Fe2+"})], retention_level = 0)
+
+        tsc.branch_collection([SubstitutionTransformation({"Zn2+":"Fe2+"})], retention_level=0)
         self.assertEqual(len(tsc), 4)
 
         self.assertEqual(len(tsc[0]), 6)
         #5 transformations plus initial structure
-        
-        tsc = TransformedStructureTransmuter([tsc[0]],[SubstitutionTransformation({"Mn3+":{"Fe2+":0.333}}), OrderDisorderedStructureTransformation(num_structures = 50)], extend_collection = False)
+
+        tsc = TransformedStructureTransmuter([tsc[0]], [SubstitutionTransformation({"Mn3+":{"Fe2+":0.333}}), OrderDisorderedStructureTransformation(num_structures=50)], extend_collection=False)
         self.assertEqual(len(tsc), 1)
-        
+
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
