404ffdbf413572eb912eb43fbed4934e04a16852
Shyue Ping Ong <shyue@mit.edu>
2012-04-19 00:38:21 -0400

    Fix setup.py intro.
    Former-commit-id: fff45919bd2a4795284d3f2fb7b83752275c14af [formerly bc7130110626bd25ff8c6b004158bb18d98399f1]
    Former-commit-id: 77b2aae22eb981eb6ad5d6143d8cc4191fdb47b0

--- setup.py
+++ setup.py
@@ -26,6 +26,9 @@ Note that pymatgen, like all scientific research, will always be a work in
 progress. While the development team will always strive to avoid backward 
 incompatible changes, they are sometimes unavoidable, and tough decisions have 
 to be made for the long term health of the code.
+
+For documentation and usage guide, please refer to the latest documentation at
+our github page (http://materialsproject.github.com/pymatgen/).
 """
 
 setup (
