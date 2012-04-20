9a766c822351f578165c3b0d4c5efe8db7c0e23a
Shyue Ping Ong <shyue@mit.edu>
2012-04-19 22:01:01 -0400

    Better from dict method for Structure.
    Former-commit-id: e9ca45499f9a76e8e24c66d885e1ddfe5dd5ce3c [formerly 76c8368645ba9e48f8ae110f0d1a97ce71beea7c]
    Former-commit-id: 738b0112e9fb25be9de39cd88c1c3b0df95c2339

--- pymatgen/core/tests/test_structure.py
+++ pymatgen/core/tests/test_structure.py
@@ -185,9 +185,6 @@ class StructureTest(unittest.TestCase):
         self.assertEqual(d['sites'][0]['species'][0]['properties']['spin'], 3)
 
     def test_from_dict(self):
-        test_dict = {'lattice': {'a': 3.8401979336999998, 'volume': 40.044794644251596, 'c': 3.8401979337177736, 'b': 3.8401989943442438, 'matrix': [[3.8401979337, 0.0, 0.0], [1.9200989668, 3.3257101909, 0.0], [0.0, -2.2171384943, 3.1355090603]], 'alpha': 119.99999086398419, 'beta': 90.0, 'gamma': 60.000009137322195}, 'sites': [{'occu': 0.5, 'abc': [0, 0, 0], 'xyz': [0.0, 0.0, 0.0], 'species': [{'occu': 0.5, 'element': 'Mn'}, {'occu': 0.5, 'oxidation_state': 4, 'element': 'Si'}], 'label': 'Mn: 0.5000, Si4+: 0.5000'}, {'occu': 0.5, 'abc': [0.75, 0.5, 0.75], 'xyz': [3.8401979336749994, 1.2247250003039056e-06, 2.3516317952249999], 'species': [{'occu': 0.5, 'oxidation_state': 4, 'element': 'Ge'}], 'label': 'Ge4+: 0.5000'}]}
-        s = Structure.from_dict(test_dict)
-        self.assertEqual(s.composition.formula, 'Mn0.5 Si0.5 Ge0.5')
         d = self.propertied_structure.to_dict
         s = Structure.from_dict(d)
         self.assertEqual(s[0].magmom, 5)
