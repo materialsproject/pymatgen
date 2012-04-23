98f4a3a1016b22042329be5a0dc529ea0ec9b8c5
Shyue Ping Ong <shyue@mit.edu>
2012-04-23 17:36:47 -0400

    Better doc for various partial removal and order algos.
    Former-commit-id: 45ae4e7026526d6ec488e96a76a631217f78e0e7 [formerly 80bcb03194355c4d5d11743579aed9621a9194bc]
    Former-commit-id: 2bee44b6c37067a5deae461a0a1ceec6742dc86e

--- pymatgen/transformations/site_transformations.py
+++ pymatgen/transformations/site_transformations.py
@@ -149,9 +149,9 @@ class PartialRemoveSitesTransformation(AbstractTransformation):
     Requires an oxidation state decorated structure for ewald sum to be 
     computed.
     
-    Given that the solution to selecting the right removals is NP-hard, there are
-    several algorithms provided with varying degrees of accuracy and speed. The 
-    options are as follows:
+    Given that the solution to selecting the right removals is NP-hard, there
+    are several algorithms provided with varying degrees of accuracy and speed.
+    The options are as follows:
     
     ALGO_FAST:
         This is a highly optimized algorithm to quickly go through the search 
@@ -163,16 +163,17 @@ class PartialRemoveSitesTransformation(AbstractTransformation):
         The complete algo ensures that you get all symmetrically distinct 
         orderings, ranked by the estimated Ewald energy. But this can be an
         extremely time-consuming process if the number of possible orderings is
-        very large.
+        very large. Use this if you really want all possible orderings. If you
+        want just the lowest energy ordering, ALGO_FAST is accurate and faster.
         
     ALGO_BEST_FIRST:
-        This algorithm is for ordering the really large cells which defeats even 
-        the ALGO_FAST.  For example, if you have 48 sites of which you want to
+        This algorithm is for ordering the really large cells that defeats even 
+        ALGO_FAST.  For example, if you have 48 sites of which you want to
         remove 16 of them, the number of possible orderings is around 2 x 10^12.
         ALGO_BEST_FIRST shortcircuits the entire search tree by removing the 
-        highest energy site first, then followed by the next highest energy site,
-        and so on.  It is guaranteed to find a solution in a reasonable time, but it
-        is also likely to be highly inaccurate. 
+        highest energy site first, then followed by the next highest energy
+        site, and so on.  It is guaranteed to find a solution in a reasonable
+        time, but it is also likely to be highly inaccurate. 
     """
 
     ALGO_FAST = 0
@@ -232,7 +233,8 @@ class PartialRemoveSitesTransformation(AbstractTransformation):
         mod = StructureEditor(structure)
         mod.delete_sites(to_delete)
         self.logger.debug('Minimizing Ewald took {} seconds.'.format(time.time() - starttime))
-        return [{'energy':sum(sum(ematrix)), 'structure': mod.modified_structure.get_sorted_structure()}]
+        return [{'energy':sum(sum(ematrix)),
+                 'structure': mod.modified_structure.get_sorted_structure()}]
 
     def complete_ordering(self, structure, num_remove_dict):
         self.logger.debug('Performing complete ordering...')
