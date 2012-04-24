5d2d6b35033fa39450b5531cd1c586e58cb8aa36
Shyue Ping Ong <shyue@mit.edu>
2012-04-24 18:10:43 -0400

    Add a tolerance option to det_spacegroup.
    Former-commit-id: 2bcea216907e7bed1f34419b2f3de4cf11e80946 [formerly 7f895c3d6953ce9b1b543cd5fe39ffee2b7c553c]
    Former-commit-id: c9852d69cd670f391c5df85f9f2ae0f9599a94d8

--- scripts/vis_structure.py
+++ scripts/vis_structure.py
@@ -6,7 +6,7 @@ Simple structure visualizer script.
 
 from __future__ import division
 
-__author__="Shyue Ping Ong"
+__author__ = "Shyue Ping Ong"
 __copyright__ = "Copyright 2011, The Materials Project"
 __version__ = "0.1"
 __maintainer__ = "Shyue Ping Ong"
@@ -23,10 +23,10 @@ parser = argparse.ArgumentParser(description='''Convenient structure file viewer
 Author: Shyue Ping Ong
 Version: 1.0
 Last updated: Nov 29 2011''')
-parser.add_argument('input_file', metavar='input file', type=str, nargs = 1, help='input file')
+parser.add_argument('input_file', metavar='input file', type=str, nargs=1, help='input file')
 
-parser.add_argument('-f', '--format', dest='format', type=str, nargs = 1, choices=['cif','poscar'], default='poscar', help='Format of input file. Defaults to POSCAR')
-parser.add_argument('-e', '--exclude_bonding', dest='exclude_bonding', type=str, nargs = 1, help='List of elements to exclude from bonding analysis. E.g., Li,Na')
+parser.add_argument('-f', '--format', dest='format', type=str, nargs=1, choices=['cif', 'poscar'], default='poscar', help='Format of input file. Defaults to POSCAR')
+parser.add_argument('-e', '--exclude_bonding', dest='exclude_bonding', type=str, nargs=1, help='List of elements to exclude from bonding analysis. E.g., Li,Na')
 
 args = parser.parse_args()
 excluded_bonding_elements = args.exclude_bonding[0].split(',') if args.exclude_bonding else []
@@ -38,17 +38,17 @@ if args.input_file[0].endswith(".cif"):
 elif args.input_file[0].startswith("POSCAR"):
     file_format = "poscar"
 
+s = None
+
 if file_format == 'poscar':
     p = Poscar.from_file(args.input_file[0])
     s = p.struct
-    vis = StructureVis(excluded_bonding_elements=excluded_bonding_elements)
-    vis.set_structure(s)
-    vis.show()
 else:
     r = CifParser(args.input_file[0])
     s = r.get_structures(False)[0]
+
+if s:
     vis = StructureVis(excluded_bonding_elements=excluded_bonding_elements)
     vis.set_structure(s)
     vis.show()
-    
-    
+
