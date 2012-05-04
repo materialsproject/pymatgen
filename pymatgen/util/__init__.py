1d77a259480525653b7009398faea5b0e935b471
Shyue Ping Ong <shyue@mit.edu>
2012-05-03 20:01:36 -0400

    More doc changes.

--- pymatgen/util/__init__.py
+++ pymatgen/util/__init__.py
@@ -1,2 +1,7 @@
-__author__="Shyue"
-__date__ ="$Jun 6, 2011 7:30:05 AM$"
+"""
+The util package implements various utilities that are commonly used by various
+packages.
+"""
+
+__author__ = "Shyue"
+__date__ = "$Jun 6, 2011 7:30:05 AM$"
