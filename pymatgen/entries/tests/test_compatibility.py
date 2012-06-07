ecd3b21223078ab4f78f5234ef41d372a91ed93f
Shyue Ping Ong <shyue@mit.edu>
2012-06-07 12:44:35 -0400

    Minor cleanup of compatibility test.

--- pymatgen/entries/tests/test_compatibility.py
+++ pymatgen/entries/tests/test_compatibility.py
@@ -25,26 +25,27 @@ class MaterialsProjectCompatibilityTest(unittest.TestCase):
         compat = MaterialsProjectCompatibility()
 
         #Correct parameters
-        entry = ComputedEntry(Composition.from_formula('Fe2O3'), -1, 0.0, parameters = {'is_hubbard':True, 'hubbards':{'Fe':5.3, 'O':0}, 'run_type':'GGA+U', 'potcar_symbols':['PAW_PBE Fe_pv 06Sep2000', 'PAW_PBE O 08Apr2002']})
+        entry = ComputedEntry(Composition.from_formula('Fe2O3'), -1, 0.0, parameters={'is_hubbard':True, 'hubbards':{'Fe':5.3, 'O':0}, 'run_type':'GGA+U', 'potcar_symbols':['PAW_PBE Fe_pv 06Sep2000', 'PAW_PBE O 08Apr2002']})
         self.assertIsNotNone(compat.process_entry(entry))
 
         #Wrong U value
-        entry = ComputedEntry(Composition.from_formula('Fe2O3'), -1, 0.0, parameters = {'is_hubbard':True, 'hubbards':{'Fe':5.2, 'O':0}, 'run_type':'GGA+U', 'potcar_symbols':['PAW_PBE Fe_pv 06Sep2000', 'PAW_PBE O 08Apr2002']})
+        entry = ComputedEntry(Composition.from_formula('Fe2O3'), -1, 0.0, parameters={'is_hubbard':True, 'hubbards':{'Fe':5.2, 'O':0}, 'run_type':'GGA+U', 'potcar_symbols':['PAW_PBE Fe_pv 06Sep2000', 'PAW_PBE O 08Apr2002']})
         self.assertIsNone(compat.process_entry(entry))
 
         #GGA run
-        entry = ComputedEntry(Composition.from_formula('Fe2O3'), -1, 0.0, parameters = {'is_hubbard':False, 'hubbards':None, 'run_type':'GGA', 'potcar_symbols':['PAW_PBE Fe_pv 06Sep2000', 'PAW_PBE O 08Apr2002']})
+        entry = ComputedEntry(Composition.from_formula('Fe2O3'), -1, 0.0, parameters={'is_hubbard':False, 'hubbards':None, 'run_type':'GGA', 'potcar_symbols':['PAW_PBE Fe_pv 06Sep2000', 'PAW_PBE O 08Apr2002']})
         self.assertIsNone(compat.process_entry(entry))
 
         #Wrong psp
-        entry = ComputedEntry(Composition.from_formula('Fe2O3'), -1, 0.0, parameters = {'is_hubbard':True, 'hubbards':{'Fe':5.3, 'O':0}, 'run_type':'GGA+U', 'potcar_symbols':['PAW_PBE Fe 06Sep2000', 'PAW_PBE O 08Apr2002']})
+        entry = ComputedEntry(Composition.from_formula('Fe2O3'), -1, 0.0, parameters={'is_hubbard':True, 'hubbards':{'Fe':5.3, 'O':0}, 'run_type':'GGA+U', 'potcar_symbols':['PAW_PBE Fe 06Sep2000', 'PAW_PBE O 08Apr2002']})
         self.assertIsNone(compat.process_entry(entry))
 
         #Testing processing of elements.
-        entry = ComputedEntry(Composition.from_formula('O'), -1, 0.0, parameters = {'is_hubbard':False, 'hubbards':{}, 'potcars':['O'], 'run_type':'GGA'})
+        entry = ComputedEntry(Composition.from_formula('O'), -1, 0.0, parameters={'is_hubbard':False, 'hubbards':{}, 'potcar_symbols':['PAW_PBE O 08Apr2002'], 'run_type':'GGA'})
         entry = compat.process_entry(entry)
         self.assertEqual(entry.structureid, -8)
         self.assertAlmostEqual(entry.energy, -4.22986844926)
+
 
 class MITCompatibilityTest(unittest.TestCase):
 
@@ -52,27 +53,26 @@ class MITCompatibilityTest(unittest.TestCase):
         compat = MITCompatibility()
 
         #Correct parameters
-        entry = ComputedEntry(Composition.from_formula('Fe2O3'), -1, 0.0, parameters = {'is_hubbard':True, 'hubbards':{'Fe':4.0, 'O':0}, 'run_type':'GGA+U', 'potcar_symbols':['PAW_PBE Fe 06Sep2000', 'PAW_PBE O 08Apr2002']})
+        entry = ComputedEntry(Composition.from_formula('Fe2O3'), -1, 0.0, parameters={'is_hubbard':True, 'hubbards':{'Fe':4.0, 'O':0}, 'run_type':'GGA+U', 'potcar_symbols':['PAW_PBE Fe 06Sep2000', 'PAW_PBE O 08Apr2002']})
         self.assertIsNotNone(compat.process_entry(entry))
 
         #Wrong U value
-        entry = ComputedEntry(Composition.from_formula('Fe2O3'), -1, 0.0, parameters = {'is_hubbard':True, 'hubbards':{'Fe':5.2, 'O':0}, 'run_type':'GGA+U', 'potcar_symbols':['PAW_PBE Fe 06Sep2000', 'PAW_PBE O 08Apr2002']})
+        entry = ComputedEntry(Composition.from_formula('Fe2O3'), -1, 0.0, parameters={'is_hubbard':True, 'hubbards':{'Fe':5.2, 'O':0}, 'run_type':'GGA+U', 'potcar_symbols':['PAW_PBE Fe 06Sep2000', 'PAW_PBE O 08Apr2002']})
         self.assertIsNone(compat.process_entry(entry))
 
         #GGA run
-        entry = ComputedEntry(Composition.from_formula('Fe2O3'), -1, 0.0, parameters = {'is_hubbard':False, 'hubbards':None, 'run_type':'GGA', 'potcar_symbols':['PAW_PBE Fe 06Sep2000', 'PAW_PBE O 08Apr2002']})
+        entry = ComputedEntry(Composition.from_formula('Fe2O3'), -1, 0.0, parameters={'is_hubbard':False, 'hubbards':None, 'run_type':'GGA', 'potcar_symbols':['PAW_PBE Fe 06Sep2000', 'PAW_PBE O 08Apr2002']})
         self.assertIsNone(compat.process_entry(entry))
 
         #Wrong psp
-        entry = ComputedEntry(Composition.from_formula('Fe2O3'), -1, 0.0, parameters = {'is_hubbard':True, 'hubbards':{'Fe':4.0, 'O':0}, 'run_type':'GGA+U', 'potcar_symbols':['PAW_PBE Fe_pv 06Sep2000', 'PAW_PBE O 08Apr2002']})
+        entry = ComputedEntry(Composition.from_formula('Fe2O3'), -1, 0.0, parameters={'is_hubbard':True, 'hubbards':{'Fe':4.0, 'O':0}, 'run_type':'GGA+U', 'potcar_symbols':['PAW_PBE Fe_pv 06Sep2000', 'PAW_PBE O 08Apr2002']})
         self.assertIsNone(compat.process_entry(entry))
 
         #Testing processing of elements.
-        entry = ComputedEntry(Composition.from_formula('O'), -1, 0.0, parameters = {'is_hubbard':False, 'hubbards':{}, 'potcars':['O'], 'run_type':'GGA'})
+        entry = ComputedEntry(Composition.from_formula('O'), -1, 0.0, parameters={'is_hubbard':False, 'hubbards':{}, 'potcar_symbols':['PAW_PBE O 08Apr2002'], 'run_type':'GGA'})
         entry = compat.process_entry(entry)
         self.assertEqual(entry.structureid, -8)
         self.assertAlmostEqual(entry.energy, -4.25915626315)
-
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
