5fc144aeffad3497dfc3c1ca54a0ea027372c618
Shyue Ping Ong <shyue@mit.edu>
2012-06-28 22:47:55 -0400

    Self-regulating version number for pymatgen in root module.
    Former-commit-id: 763880820fd0f84f2a6082e893d75b005cf21bc1 [formerly 3ab40e3dd1cefc224a06cc0b3d818f6191825e3d]
    Former-commit-id: 9d56f84bed930d41fbd9922e3b2e6f3fbd5a3d99

--- pymatgen/__init__.py
+++ pymatgen/__init__.py
@@ -1,2 +1,3 @@
-__author__="Shyue"
-__date__ ="$Dec 22, 2010 2:00:07 PM$"
+__author__ = "Shyue Ping Ong, Anubhav Jain, Michael Kocher, Geoffroy Hautier, Will Richards, Dan Gunter, Shreyas Cholia, Vincent L Chevrier, Rickard Armiento"
+__date__ = "Jun 28, 2012"
+__version__ = "2.0.0"
