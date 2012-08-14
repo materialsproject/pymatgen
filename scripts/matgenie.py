15644dee3afecf481cddace35143bd3b20bb964e
Shyue Ping Ong <shyue@mit.edu>
2012-08-14 17:00:14 +0800

    Integrate viewing of structures into matgenie.

--- scripts/matgenie.py
+++ scripts/matgenie.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-'''
+"""
 A master convenience script for many tools for vasp analysis.
-'''
+"""
 
 from __future__ import division
 
@@ -34,7 +34,7 @@ from pymatgen.io.smartio import read_structure, write_structure
 from pymatgen.io.cssrio import Cssr
 from pymatgen.symmetry.finder import SymmetryFinder
 
-save_file = 'vasp_data.gz'
+save_file = "vasp_data.gz"
 
 
 def get_energies(rootdir, reanalyze, verbose, pretty, detailed, sort):
@@ -46,37 +46,37 @@ def get_energies(rootdir, reanalyze, verbose, pretty, detailed, sort):
         drone = SimpleVaspToComputedEntryDrone(inc_structure=True)
     else:
         drone = VaspToComputedEntryDrone(inc_structure=True,
-                                         data=['filename',
-                                               'initial_structure'])
+                                         data=["filename",
+                                               "initial_structure"])
 
     ncpus = multiprocessing.cpu_count()
-    logging.info('Detected {} cpus'.format(ncpus))
+    logging.info("Detected {} cpus".format(ncpus))
     queen = BorgQueen(drone, number_of_drones=ncpus)
     if os.path.exists(save_file) and not reanalyze:
-        msg = 'Using previously assimilated data from {}.'.format(save_file) \
-            + ' Use -f to force re-analysis.'
+        msg = "Using previously assimilated data from {}.".format(save_file) \
+            + " Use -f to force re-analysis."
         queen.load_data(save_file)
     else:
         queen.parallel_assimilate(rootdir)
-        msg = 'Analysis results saved to {} for faster '.format(save_file) + \
-              'subsequent loading.'
+        msg = "Analysis results saved to {} for faster ".format(save_file) + \
+              "subsequent loading."
         queen.save_data(save_file)
 
     entries = queen.get_data()
     if sort == "energy_per_atom":
         entries = sorted(entries, key=lambda x: x.energy_per_atom)
     elif sort == "filename":
-        entries = sorted(entries, key=lambda x: x.data['filename'])
+        entries = sorted(entries, key=lambda x: x.data["filename"])
 
     all_data = []
     for e in entries:
         if not detailed:
-            delta_vol = "{:.2f}".format(e.data['delta_volume'] * 100)
+            delta_vol = "{:.2f}".format(e.data["delta_volume"] * 100)
         else:
             delta_vol = e.structure.volume / \
-                e.data['initial_structure'].volume - 1
+                e.data["initial_structure"].volume - 1
             delta_vol = "{:.2f}".format(delta_vol * 100)
-        all_data.append((e.data['filename'].replace("./", ""),
+        all_data.append((e.data["filename"].replace("./", ""),
                      re.sub("\s+", "", e.composition.formula),
                      "{:.5f}".format(e.energy),
                      "{:.5f}".format(e.energy_per_atom),
@@ -107,7 +107,7 @@ def get_magnetizations(mydir, ion_list):
                     fullpath = os.path.join(parent, f)
                     outcar = Outcar(fullpath)
                     mags = outcar.magnetization
-                    mags = [m['tot'] for m in mags]
+                    mags = [m["tot"] for m in mags]
                     all_ions = xrange(len(mags))
                     row.append(fullpath.lstrip("./"))
                     if ion_list:
@@ -134,14 +134,14 @@ def plot_dos(args):
     dos = v.complete_dos
 
     all_dos = OrderedDict()
-    all_dos['Total'] = dos
+    all_dos["Total"] = dos
 
     structure = v.final_structure
 
     if args.site:
         for i in xrange(len(structure)):
             site = structure[i]
-            all_dos['Site ' + str(i) + " " + site.specie.symbol] = \
+            all_dos["Site " + str(i) + " " + site.specie.symbol] = \
                 dos.get_site_dos(site)
 
     if args.element:
@@ -225,15 +225,29 @@ def parse_symmetry(args):
             finder = SymmetryFinder(s, tolerance)
             dataset = finder.get_symmetry_dataset()
             print filename
-            print "Spacegroup  : {}".format(dataset['international'])
-            print "Int number  : {}".format(dataset['number'])
+            print "Spacegroup  : {}".format(dataset["international"])
+            print "Int number  : {}".format(dataset["number"])
             print "Hall symbol : {}".format(dataset["hall"])
             print
 
+
+def parse_view(args):
+    from pymatgen.vis.structure_vtk import StructureVis
+    excluded_bonding_elements = args.exclude_bonding[0].split(",") \
+        if args.exclude_bonding else []
+    s = read_structure(args.filename[0])
+    vis = StructureVis(excluded_bonding_elements=excluded_bonding_elements)
+    vis.set_structure(s)
+    vis.show()
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='''
+    parser = argparse.ArgumentParser(description="""
     matgenie is a convenient script that uses pymatgen to perform many analyses
-    , plotting and format conversions.''',
+    , plotting and format conversions. This script works based on several
+    sub-commands with their own options. To see the options for the sub-
+    commands,
+    type "matgenie.py sub-command -h".""",
     epilog="""
     Author: Shyue Ping Ong
     Version: {}
@@ -241,61 +255,56 @@ if __name__ == "__main__":
 
     subparsers = parser.add_subparsers()
 
-    parser_vasp = subparsers.add_parser('analyze', help="Analysis tools. Do"
-                                        " vasper.py analyze -h for subcommand"
-                                        " help.")
-    parser_vasp.add_argument('directories', metavar='dir', default='.',
-                             type=str, nargs='*',
-                             help='directory to process (default to .)')
-    parser_vasp.add_argument('-e', '--energies', dest='get_energies',
-                             action='store_true', help='Print energies')
-    parser_vasp.add_argument('-m', '--mag', dest="ion_list", type=str, nargs=1,
-                             help='Print magmoms. ION LIST can be a range '
-                             '(e.g., 1-2) or the string "All" for all ions.')
-    parser_vasp.add_argument('-f', '--force', dest="reanalyze",
-                             action='store_true',
-                             help='Force reanalysis. Typically, vasp_analyzer'
-                             ' will just reuse a vasp_analyzer_data.gz if '
-                             'present. This forces the analyzer to reanalyze '
-                             'the data.')
-    parser_vasp.add_argument('-v', '--verbose', dest="verbose",
-                             action='store_true',
-                             help='verbose mode. Provides detailed output on '
-                             'progress.')
-    parser_vasp.add_argument('-p', '--pretty', dest="pretty",
-                             action='store_const',
-                             const=True, help='pretty mode. Uses prettytable '
-                             'to format output. Must have prettytable module '
-                             'installed.')
-    parser_vasp.add_argument('-d', '--detailed', dest="detailed",
-                             action='store_true',
-                             help='Detailed mode. Parses vasprun.xml instead '
-                             'of separate vasp input. Slower.')
-    parser_vasp.add_argument('-s', '--sort', dest="sort", type=str, nargs=1,
-                             default=['energy_per_atom'],
-                             help='Sort criteria. Defaults to energy / atom.')
+    parser_vasp = subparsers.add_parser("analyze", help="Vasp run analysis.")
+    parser_vasp.add_argument("directories", metavar="dir", default=".",
+                             type=str, nargs="*",
+                             help="directory to process (default to .)")
+    parser_vasp.add_argument("-e", "--energies", dest="get_energies",
+                             action="store_true", help="Print energies")
+    parser_vasp.add_argument("-m", "--mag", dest="ion_list", type=str, nargs=1,
+                             help="Print magmoms. ION LIST can be a range "
+                             "(e.g., 1-2) or the string 'All' for all ions.")
+    parser_vasp.add_argument("-f", "--force", dest="reanalyze",
+                             action="store_true",
+                             help="Force reanalysis. Typically, vasp_analyzer"
+                             " will just reuse a vasp_analyzer_data.gz if "
+                             "present. This forces the analyzer to reanalyze "
+                             "the data.")
+    parser_vasp.add_argument("-v", "--verbose", dest="verbose",
+                             action="store_true",
+                             help="verbose mode. Provides detailed output on "
+                             "progress.")
+    parser_vasp.add_argument("-p", "--pretty", dest="pretty",
+                             action="store_const",
+                             const=True, help="pretty mode. Uses prettytable "
+                             "to format output. Must have prettytable module "
+                             "installed.")
+    parser_vasp.add_argument("-d", "--detailed", dest="detailed",
+                             action="store_true",
+                             help="Detailed mode. Parses vasprun.xml instead "
+                             "of separate vasp input. Slower.")
+    parser_vasp.add_argument("-s", "--sort", dest="sort", type=str, nargs=1,
+                             default=["energy_per_atom"],
+                             help="Sort criteria. Defaults to energy / atom.")
     parser_vasp.set_defaults(func=parse_vasp)
 
-    parser_plot = subparsers.add_parser('plot', help="Plotting tools. Do"
-                                        " vasper.py plot -h for subcommand"
-                                        " help.")
-    parser_plot.add_argument('filename', metavar='filename', type=str, nargs=1,
-                             help='vasprun.xml file to plot')
-    parser_plot.add_argument('-s', '--site', dest='site', action='store_const',
-                        const=True, help='plot site projected DOS')
-    parser_plot.add_argument('-e', '--element', dest='element', type=str,
+    parser_plot = subparsers.add_parser("plot", help="Plotting for dos.")
+    parser_plot.add_argument("filename", metavar="filename", type=str, nargs=1,
+                             help="vasprun.xml file to plot")
+    parser_plot.add_argument("-s", "--site", dest="site", action="store_const",
+                        const=True, help="plot site projected DOS")
+    parser_plot.add_argument("-e", "--element", dest="element", type=str,
                              nargs=1,
-                             help='List of elements to plot as comma-separated'
-                             ' values e.g., Fe,Mn')
-    parser_plot.add_argument('-o', '--orbital', dest="orbital",
-                             action='store_const', const=True,
-                             help='plot orbital projected DOS')
+                             help="List of elements to plot as comma-separated"
+                             " values e.g., Fe,Mn")
+    parser_plot.add_argument("-o", "--orbital", dest="orbital",
+                             action="store_const", const=True,
+                             help="plot orbital projected DOS")
     parser_plot.set_defaults(func=plot_dos)
 
-    parser_convert = subparsers.add_parser('convert',
+    parser_convert = subparsers.add_parser("convert",
                                            help="File format conversion tools."
-                                           " Do vasper.py convert -h for "
-                                           "subcommand help.")
+                                           )
     parser_convert.add_argument("input_filename", metavar="input_filename",
                                 type=str, nargs=1, help="Input filename.")
     parser_convert.add_argument("output_filename", metavar="output_filename",
@@ -323,20 +332,26 @@ if __name__ == "__main__":
                                 "directory.")
     parser_convert.set_defaults(func=convert_fmt)
 
-    parser_symm = subparsers.add_parser('symm',
-                                        help="Symmetry tools."
-                                        " Do vasper.py symmetry -h for "
-                                        "subcommand help.")
-    parser_symm.add_argument('filenames', metavar='filenames', type=str,
-                             nargs='+',
-                             help='Filenames to determine symmetry.')
-    parser_symm.add_argument('-t', '--tolerance', dest='tolerance', type=float,
+    parser_symm = subparsers.add_parser("symm", help="Symmetry tools.")
+    parser_symm.add_argument("filenames", metavar="filenames", type=str,
+                             nargs="+",
+                             help="Filenames to determine symmetry.")
+    parser_symm.add_argument("-t", "--tolerance", dest="tolerance", type=float,
                              nargs=1, default=[0.1],
-                             help='Tolerance for symmetry determination')
-    parser_symm.add_argument('-s', '--spacegroup', dest='spacegroup',
-                             action='store_true',
-                             help='Determine symmetry')
+                             help="Tolerance for symmetry determination")
+    parser_symm.add_argument("-s", "--spacegroup", dest="spacegroup",
+                             action="store_true",
+                             help="Determine symmetry")
     parser_symm.set_defaults(func=parse_symmetry)
+
+    parser_view = subparsers.add_parser("view", help="Visualize structures")
+    parser_view.add_argument("filename", metavar="filename", type=str,
+                             nargs=1, help="Filename")
+    parser_view.add_argument("-e", "--exclude_bonding", dest="exclude_bonding",
+                             type=str, nargs=1,
+                             help="List of elements to exclude from bonding "
+                             "analysis. E.g., Li,Na")
+    parser_view.set_defaults(func=parse_view)
 
     args = parser.parse_args()
     args.func(args)
