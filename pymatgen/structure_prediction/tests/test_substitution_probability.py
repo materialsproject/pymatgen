85b25fd179fe33cf4bac59c246e126c7c8f7c275
William Richards <wmdrichards@gmail.com>
2012-08-30 14:37:53 -0400

    changes to substitution probability to do conditional probability in the same way as BURP

--- pymatgen/structure_prediction/tests/test_substitution_probability.py
+++ pymatgen/structure_prediction/tests/test_substitution_probability.py
@@ -6,6 +6,7 @@ from pymatgen.core.periodic_table import Specie
 from pymatgen.structure_prediction.substitution_probability import SubstitutionProbability
 
 
+
 class SubstitutionProbabilityTest(unittest.TestCase):
     def test_substitution_probability(self):
         """
@@ -13,10 +14,6 @@ class SubstitutionProbabilityTest(unittest.TestCase):
         these tests will have to be as well
         """
         pc = SubstitutionProbability.from_defaults()
-        sp1 = Specie('Ag', 2)
-        sp3 = Specie('Cl', -1)
-        prob1 = pc.list_probability([sp1, sp3], [sp1, sp3])
-        print prob1
         sp1 = Specie('Fe', 4)
         sp3 = Specie('Mn', 3)
         prob1 = pc.probability(sp1, sp3)
@@ -25,8 +22,8 @@ class SubstitutionProbabilityTest(unittest.TestCase):
         sp4 = Specie('Pd', 4)
         prob2 = pc.probability(sp2, sp4)
         self.assertAlmostEqual(prob2, 3.0514797917e-05, 5, "probability isn't correct")
-        prob3 = pc.list_probability([sp1, sp2], [sp3, sp4])
-        self.assertAlmostEqual(prob3, prob1 * prob2, 5, "probability isn't correct")
+        prob3 = pc.substitution_probability([sp1, sp2], [sp3, sp4])
+        self.assertAlmostEqual(prob3, 4.39320136113e-05, 5, "probability isn't correct")
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
