46156d056f77898fc2e365b63c11cd58b63dcce7
Shyue Ping Ong <shyue@mit.edu>
2012-09-06 13:31:08 -0400

    Some cleaning.

--- pymatgen/structure_prediction/substitution_probability.py
+++ pymatgen/structure_prediction/substitution_probability.py
@@ -15,50 +15,51 @@ import math
 import os
 import pymatgen
 
+
 def test_table():
     """
-    loads a lightweight lambda table for use in unit tests to 
-    reduce initialization time, and make unit tests insensitive
-    to changes in the default lambda table
+    Loads a lightweight lambda table for use in unit tests to reduce
+    initialization time, and make unit tests insensitive to changes in the
+    default lambda table.
     """
     module_dir = os.path.dirname(pymatgen.__file__)
-    json_file = os.path.join(module_dir, 'structure_prediction'
-                             , 'tests', 'test_data'
-                             , 'test_lambda.json')
+    json_file = os.path.join(module_dir, 'structure_prediction', 'tests',
+                             'test_data', 'test_lambda.json')
     with open(json_file) as f:
         lambda_table = json.load(f)
     return lambda_table
+
 
 class SubstitutionProbability():
     """
     This class finds substitution probabilities given lists of atoms
     to substitute.
-    The inputs make more sense if you look through the from_defaults 
+    The inputs make more sense if you look through the from_defaults
     static method
+
     Args:
         lambda_table:
-            json table of the weight functions lambda (theta 
-            in BURP)
-            if None, will use the default lambda.json table
+            json table of the weight functions lambda (theta in BURP) if None,
+            will use the default lambda.json table
         alpha:
             weight function for never observed substitutions
     """
-    def __init__(self, lambda_table = None, alpha = -5.):
+    def __init__(self, lambda_table=None, alpha= -5):
         #Something seems to be weird about the SubsProbaMRF file:
-        #alpha is VERY high. alpha essentially replaces the lambda 
-        #value for never-observed compounds, but is positive even 
+        #alpha is VERY high. alpha essentially replaces the lambda
+        #value for never-observed compounds, but is positive even
         #though many (most?) are negative numbers
-        
+
         #store the input table for the to_dict method
         self._lambda_table = lambda_table
-        
+
         if not lambda_table:
             module_dir = os.path.dirname(pymatgen.__file__)
-            json_file = os.path.join(module_dir, 'structure_prediction'
-                                     , 'data', 'lambda.json')
+            json_file = os.path.join(module_dir, 'structure_prediction',
+                                     'data', 'lambda.json')
             with open(json_file) as f:
                 lambda_table = json.load(f)
-            
+
         #build map of specie pairs to lambdas
         l = {}
         for row in lambda_table:
@@ -66,11 +67,11 @@ class SubstitutionProbability():
                 s1 = Specie.from_string(row[0])
                 s2 = Specie.from_string(row[1])
                 l[frozenset([s1, s2])] = float(row[2])
-        
-        self._lambda = l 
+
+        self._lambda = l
         self._alpha = alpha
         self.update_normalizations()
-         
+
     def update_normalizations(self):
         """
         updates the partition functions Z and px,
@@ -80,21 +81,21 @@ class SubstitutionProbability():
         for key in self._lambda.keys():
             sp_set.update(key)
         px = dict.fromkeys(sp_set, 0.)
-        Z=0
-        for s1, s2 in itertools.product(sp_set,repeat = 2):
-            value = math.exp(self._lambda.get(frozenset([s1, s2])
-                                              , self._alpha))
-            #not sure why the factor of 2 is here but it matches up 
-            #with BURP. BURP may actually be missing a factor of 2, 
+        Z = 0
+        for s1, s2 in itertools.product(sp_set, repeat=2):
+            value = math.exp(self._lambda.get(frozenset([s1, s2]),
+                                              self._alpha))
+            #not sure why the factor of 2 is here but it matches up
+            #with BURP. BURP may actually be missing a factor of 2,
             #but it doesn't have a huge effect
-            px[s1] += value/2 
-            px[s2] += value/2 
+            px[s1] += value / 2
+            px[s2] += value / 2
             Z += value
-        
+
         self._Z = Z
         self._px = px
         self.species_list = list(sp_set)
-        
+
     def prob(self, s1, s2):
         """
         get the probability of 2 species substitution
@@ -104,37 +105,40 @@ class SubstitutionProbability():
         object functionality
         """
         l = self._lambda.get(frozenset([s1, s2]), self._alpha)
-        return math.exp(l)/self._Z
-    
+        return math.exp(l) / self._Z
+
     def cond_prob(self, s1, s2):
         """
         Args:
             s1: The VARIABLE specie
             s2: The FIXED specie
-        this is the conditional probability used by burp structure 
-        predictor (which is why its in a wierd order)
+
+        Returns:
+            Conditional probability used by burp structure predictor (which
+            is why its in a wierd order)
         """
         l = self._lambda.get(frozenset([s1, s2]), self._alpha)
-        return math.exp(l)/self._px[s2]
-    
+        return math.exp(l) / self._px[s2]
+
     def pair_corr(self, s1, s2):
         """
         returns the pair correlation of 2 species
         """
         l = self._lambda.get(frozenset([s1, s2]), self._alpha)
-        return math.exp(l)*self._Z/(self._px[s1] * self._px[s2])
-        
+        return math.exp(l) * self._Z / (self._px[s1] * self._px[s2])
+
     def cond_prob_list(self, l1, l2):
         """
-        Find the probabilities of 2 lists. These should include ALL 
-        species. This is the probability conditional on l2
+        Find the probabilities of 2 lists. These should include ALL species.
+        This is the probability conditional on l2
+
         Args:
             l1, l2:
                 lists of species
+
         Returns:
-            the conditional probability (assuming these species are in
+            The conditional probability (assuming these species are in
             l2)
-            
         """
         assert len(l1) == len(l2)
         p = 1.
@@ -142,7 +146,7 @@ class SubstitutionProbability():
             s2 = l2[i]
             p *= self.cond_prob(s1, s2)
         return p
-    
+
     @property
     def to_dict(self):
         d = {"name": self.__class__.__name__, "version": __version__}
@@ -151,8 +155,7 @@ class SubstitutionProbability():
         d["@module"] = self.__class__.__module__
         d["@class"] = self.__class__.__name__
         return d
-    
+
     @staticmethod
     def from_dict(d):
         return SubstitutionProbability(**d['init_args'])
-    
--- pymatgen/structure_prediction/substitutor.py
+++ pymatgen/structure_prediction/substitutor.py
@@ -10,14 +10,17 @@ __date__ = "Aug 31, 2012"
 from pymatgen.serializers.json_coders import MSONable
 from pymatgen.structure_prediction.substitution_probability \
     import SubstitutionProbability
-    
+
 from operator import mul
 
+
 class Substitutor(MSONable):
-    def __init__(self, threshold = 1e-3, **kwargs):
+
+    def __init__(self, threshold=1e-3, **kwargs):
         """
-        This substitutor uses the substitution probability class to 
-        find good substitutions for a given chemistry or structure. 
+        This substitutor uses the substitution probability class to
+        find good substitutions for a given chemistry or structure.
+
         Args:
             threshold:
                 probability threshold for predictions
@@ -28,7 +31,7 @@ class Substitutor(MSONable):
         self._kwargs = kwargs
         self._sp = SubstitutionProbability(**kwargs)
         self._threshold = threshold
-        
+
     def pred_from_list(self, species_list):
         """
         Args:
@@ -38,31 +41,30 @@ class Substitutor(MSONable):
             list of dictionaries, each including a substitutions
             dictionary, and a probability value
 
-        There are an exceptionally large number of substitutions to 
-        look at (260^n), where n is the number of species in the 
-        list. We need a more efficient than brute force way of going 
+        There are an exceptionally large number of substitutions to
+        look at (260^n), where n is the number of species in the
+        list. We need a more efficient than brute force way of going
         through these possibilities. The brute force method would be:
-        
+
         output = []
         for p in itertools.product(self._sp.species_list
                                    , repeat = len(species_list)):
-            if self._sp.conditional_probability_list(p, species_list) 
+            if self._sp.conditional_probability_list(p, species_list)
                                    > self._threshold:
                 output.append(dict(zip(species_list,p)))
         return output
-        
+
         Instead of that we do a branch and bound
         """
-        #calculate the highest probabilities to help us stop the 
-        #recursion
+        #calculate the highest probabilities to help us stop the recursion
         max_probabilities = []
         for s2 in species_list:
             max_p = 0
             for s1 in self._sp.species_list:
-                max_p = max([self._sp.cond_prob(s1, s2),max_p])
+                max_p = max([self._sp.cond_prob(s1, s2), max_p])
             max_probabilities.append(max_p)
         output = []
-        
+
         def _recurse(output_prob, output_species):
             best_case_prob = list(max_probabilities)
             best_case_prob[:len(output_prob)] = output_prob
@@ -77,15 +79,15 @@ class Substitutor(MSONable):
                 for sp in self._sp.species_list:
                     i = len(output_prob)
                     prob = self._sp.cond_prob(sp, species_list[i])
-                    _recurse(output_prob+[prob], output_species + [sp])
-                    
-        _recurse([],[])
+                    _recurse(output_prob + [prob], output_species + [sp])
+
+        _recurse([], [])
         return output
-    
+
     def pred_from_comp(self, composition):
         """
-        Similar to pred_from_list except this method returns a list 
-        after checking that compositions are charge balanced
+        Similar to pred_from_list except this method returns a list after
+        checking that compositions are charge balanced.
         """
         output = []
         predictions = self.pred_from_list(composition.elements)
@@ -98,7 +100,7 @@ class Substitutor(MSONable):
             if charge == 0:
                 output.append(p)
         return output
-    
+
     @property
     def to_dict(self):
         d = {"name": self.__class__.__name__, "version": __version__}
@@ -107,12 +109,9 @@ class Substitutor(MSONable):
         d["@module"] = self.__class__.__module__
         d["@class"] = self.__class__.__name__
         return d
-    
+
     @staticmethod
     def from_dict(d):
         t = d['threshold']
         kwargs = d['kwargs']
-        return Substitutor(threshold = t, **kwargs)
-        
-        
-    
+        return Substitutor(threshold=t, **kwargs)
