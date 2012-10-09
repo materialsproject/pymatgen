7fdc7fd5b5e9daf462711ae1c02dddadfe40c87b
Shyue Ping Ong <shyuep@gmail.com>
2012-10-08 22:38:48 -0400

    Fix unittests.

--- pymatgen/io/tests/test_feffio_set.py
+++ pymatgen/io/tests/test_feffio_set.py
@@ -23,7 +23,7 @@ class FeffInputSetTest(unittest.TestCase):
 TITLE Source:  CoO19128
 TITLE Structure Summary:  Co2 O2
 TITLE Reduced formula:  CoO
-TITLE space group: (Cmc2_1), space number:  (36)
+TITLE space group: (Cm), space number:  (8)
 TITLE abc:  3.297078   3.297078   5.254213
 TITLE angles: 90.000000  90.000000 120.000000
 TITLE sites: 4
