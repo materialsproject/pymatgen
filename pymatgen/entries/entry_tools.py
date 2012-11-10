b438f11f9bc0d215d905d3fdb98d93cc6f1cfe14
Shyue Ping Ong <shyuep@gmail.com>
2012-11-09 22:58:12 -0500

    Support for parallel processing in entry_tools group structures.

--- pymatgen/entries/entry_tools.py
+++ pymatgen/entries/entry_tools.py
@@ -14,9 +14,14 @@ __email__ = "shyue@mit.edu"
 __date__ = "Feb 24, 2012"
 
 import logging
+import json
+import datetime
+import collections
 
+from pymatgen.symmetry.finder import SymmetryFinder
 from pymatgen.core.structure_modifier import StructureEditor
 from pymatgen.analysis.structure_fitter import StructureFitter
+from pymatgen.serializers.json_coders import PMGJSONEncoder, PMGJSONDecoder
 
 logger = logging.getLogger(__name__)
 
@@ -30,33 +35,12 @@ def _get_host(structure, species_to_remove):
         return structure
 
 
-def group_entries_by_structure(entries, species_to_remove=None,
-                               anonymized=False, symmetry_tol=0,
-                               fitting_accuracy=StructureFitter.FAST_FIT):
-    """
-    Given a sequence of ComputedStructureEntries, use structure fitter to group
-    them by structural similarity.
-
-    Args:
-        entries:
-            Sequence of entries.
-        species_to_remove:
-            Sometimes you want to compare a host framework (e.g., in Li-ion
-            battery analysis). This allows you to specify species to remove
-            before structural comparison.
-        anonymized:
-            Allow anonymized fitting.
-        fitting_accuracy:
-            The fitting accuracy to use for StructureFitter. Defaults to
-            FAST_FIT.
-
-    Returns:
-        Sequence of sequence of entries by structural similarity. e.g,
-        [[ entry1, entry2], [entry3, entry4, entry5]]
-    """
-    all_matches = []
-    unmatched = [(entry, _get_host(entry.structure, species_to_remove))
-                 for entry in entries]
+def _perform_grouping(args):
+    (entries_json, hosts_json, anonymized, symmetry_tol, fitting_accuracy,
+     groups) = args
+    entries = json.loads(entries_json, cls=PMGJSONDecoder)
+    hosts = json.loads(hosts_json, cls=PMGJSONDecoder)
+    unmatched = zip(entries, hosts)
     while len(unmatched) > 0:
         ref_host = unmatched[0][1]
         logger.info(
@@ -82,7 +66,76 @@ def group_entries_by_structure(entries, species_to_remove=None,
                 if fitter.fit_found:
                     logger.info("Fit found")
                     matches.append(unmatched[i])
-        all_matches.append([m[0] for m in matches])
+        groups.append(json.dumps([m[0] for m in matches], cls=PMGJSONEncoder))
         unmatched = filter(lambda x: x not in matches, unmatched)
         logger.info("{} unmatched remaining".format(len(unmatched)))
-    return all_matches
+
+
+def group_entries_by_structure(entries, species_to_remove=None,
+                               anonymized=False, symmetry_tol=0,
+                               fitting_accuracy=StructureFitter.FAST_FIT,
+                               ncpus=None):
+    """
+    Given a sequence of ComputedStructureEntries, use structure fitter to group
+    them by structural similarity.
+
+    Args:
+        entries:
+            Sequence of entries.
+        species_to_remove:
+            Sometimes you want to compare a host framework (e.g., in Li-ion
+            battery analysis). This allows you to specify species to remove
+            before structural comparison.
+        anonymized:
+            Allow anonymized fitting.
+        fitting_accuracy:
+            The fitting accuracy to use for StructureFitter. Defaults to
+            FAST_FIT.
+        ncpus:
+            Number of cpus to use. Use of multiple cpus can greatly improve
+            fitting speed. Default of None means serial processing.
+
+    Returns:
+        Sequence of sequence of entries by structural similarity. e.g,
+        [[ entry1, entry2], [entry3, entry4, entry5]]
+    """
+
+    start = datetime.datetime.now()
+    logger.info("Started at {}".format(start))
+    entries_host = [(entry, _get_host(entry.structure, species_to_remove))
+                    for entry in entries]
+    if ncpus:
+        symm_entries = collections.defaultdict(list)
+        for entry, host in entries_host:
+            key = [host.composition.anonymized_formula
+                   if anonymized
+                   else host.composition.reduced_formula]
+            if symmetry_tol:
+                finder = SymmetryFinder(host, symmetry_tol)
+                key.append(finder.get_spacegroup_number())
+            symm_entries[tuple(key)].append((entry, host))
+        import multiprocessing as mp
+        logging.info("Using {} cpus".format(ncpus))
+        manager = mp.Manager()
+        groups = manager.list()
+        p = mp.Pool(ncpus)
+        #Parallel processing only supports Python primitives and not objects.
+        p.map(_perform_grouping,
+              [(json.dumps([e[0] for e in eh], cls=PMGJSONEncoder),
+                json.dumps([e[1] for e in eh], cls=PMGJSONEncoder),
+                anonymized,
+                symmetry_tol, fitting_accuracy, groups)
+               for eh in symm_entries.values()])
+    else:
+        groups = []
+        hosts = [host for entry, host in entries_host]
+        _perform_grouping((json.dumps(entries, cls=PMGJSONEncoder),
+                           json.dumps(hosts, cls=PMGJSONEncoder),
+                           anonymized,
+                           symmetry_tol, fitting_accuracy, groups))
+    entry_groups = []
+    for g in groups:
+        entry_groups.append(json.loads(g, cls=PMGJSONDecoder))
+    logging.info("Finished at {}".format(datetime.datetime.now()))
+    logging.info("Took {}".format(datetime.datetime.now() - start))
+    return entry_groups
