0494a320e941effa1aea7be1cc1a5452f2b870eb
Shyue Ping Ong <shyuep@gmail.com>
2012-11-16 19:20:53 -0500

    Support for old style Lattice dicts.

--- pymatgen/core/tests/test_lattice.py
+++ pymatgen/core/tests/test_lattice.py
@@ -182,6 +182,12 @@ class  LatticeTestCase(unittest.TestCase):
         for i in range(3):
             self.assertEqual(t.abc[i], self.tetragonal.abc[i])
             self.assertEqual(t.angles[i], self.tetragonal.angles[i])
+        del d["matrix"]
+        print d
+        t = Lattice.from_dict(d)
+        for i in range(3):
+            self.assertEqual(t.abc[i], self.tetragonal.abc[i])
+            self.assertEqual(t.angles[i], self.tetragonal.angles[i])
 
 if __name__ == '__main__':
     unittest.main()
