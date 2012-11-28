5e52d3396425f1562516349bb249c0b66be76782
Shyue Ping Ong <shyue@mit.edu>
2012-11-28 14:03:47 -0500

    Reorganize some code in cifio to be general PBC coord utilities.

--- pymatgen/util/tests/test_coord_utils.py
+++ pymatgen/util/tests/test_coord_utils.py
@@ -15,8 +15,9 @@ __date__ = "Apr 25, 2012"
 
 import unittest
 
+import numpy as np
 from pymatgen.util.coord_utils import get_linear_interpolated_value, \
-    in_coord_list
+    in_coord_list, pbc_diff, in_coord_list_pbc
 
 class CoordUtilsTest(unittest.TestCase):
 
@@ -27,10 +28,29 @@ class CoordUtilsTest(unittest.TestCase):
         self.assertRaises(ValueError, get_linear_interpolated_value, xvals, yvals, 6)
 
     def test_in_coord_list(self):
-        coords = [[0, 0, 0], [1, 1, 0.3]]
+        coords = [[0, 0, 0], [0.5, 0.5, 0.5]]
         test_coord = [0.1, 0.1, 0.1]
         self.assertFalse(in_coord_list(coords, test_coord))
         self.assertTrue(in_coord_list(coords, test_coord, atol=0.15))
+        self.assertFalse(in_coord_list([0.99,0.99,0.99], test_coord,
+                                       atol=0.15))
+
+    def test_pbc_diff(self):
+        ([0.9, 0.1, 1.01], [0.3, 0.5, 0.9])
+        self.assertTrue(np.allclose(pbc_diff([0.1, 0.1, 0.1], [0.3, 0.5, 0.9]),
+                                    [-0.2, -0.4, 0.2]))
+        self.assertTrue(np.allclose(pbc_diff([0.9, 0.1, 1.01], [0.3, 0.5, 0.9]),
+                                    [-0.4, -0.4, 0.11]))
+
+    def test_in_coord_list_pbc(self):
+        coords = [[0, 0, 0], [0.5, 0.5, 0.5]]
+        test_coord = [0.1, 0.1, 0.1]
+        self.assertFalse(in_coord_list_pbc(coords, test_coord))
+        self.assertTrue(in_coord_list_pbc(coords, test_coord, atol=0.15))
+        test_coord = [0.99, 0.99, 0.99]
+        self.assertFalse(in_coord_list_pbc(coords, test_coord, atol=0.01))
+
+
 
 if __name__ == "__main__":
     unittest.main()
