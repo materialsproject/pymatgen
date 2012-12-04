e1eaf706784a6f7d7164c419e0f03fe9819f2f28
Shyue Ping Ong <shyuep@gmail.com>
2012-12-03 22:03:01 -0500

    Add find_in_coord_list_* which returns indices.

--- pymatgen/util/coord_utils.py
+++ pymatgen/util/coord_utils.py
@@ -20,9 +20,9 @@ import math
 import itertools
 
 
-def in_coord_list(coord_list, coord, atol=1e-8):
+def find_in_coord_list(coord_list, coord, atol=1e-8):
     """
-    Tests if a particular coord is within a coord_list using numpy.allclose.
+    Find the indices of matches of a particular coord in a coord_list.
 
     Args:
         coord_list:
@@ -31,12 +31,33 @@ def in_coord_list(coord_list, coord, atol=1e-8):
             Specific coordinates
         atol:
             Absolute tolerance. Defaults to 1e-8.
+
+    Returns:
+        Indices of matches, e.g., [0, 1, 2, 3]. Empty list if not found.
     """
     if len(coord_list) == 0:
-        return False
+        return []
     coords = np.tile(coord, (len(coord_list), 1))
     diff = coord_list - coords
-    return np.any(np.all(np.abs(diff) < atol, axis=1))
+    return np.where(np.all(np.abs(diff) < atol, axis=1))[0]
+
+
+def in_coord_list(coord_list, coord, atol=1e-8):
+    """
+    Tests if a particular coord is within a coord_list.
+
+    Args:
+        coord_list:
+            List of coords to test
+        coord:
+            Specific coordinates
+        atol:
+            Absolute tolerance. Defaults to 1e-8.
+
+    Returns:
+        True if coord is in the coord list.
+    """
+    return len(find_in_coord_list(coord_list, coord, atol=atol)) > 0
 
 
 def get_linear_interpolated_value(x_values, y_values, x):
@@ -91,24 +112,47 @@ def pbc_diff(fcoord1, fcoord2):
     return fdist - np.round(fdist)
 
 
-def in_coord_list_pbc(coord_list, coord, atol=1e-8):
+def find_in_coord_list_pbc(coord_list, coord, atol=1e-8):
     """
-    Tests if a particular coord is within a coord_list using numpy.allclose.
+    Get the indices of all points in a fracitonal coord list that are
+    equal to a fractional coord (with a tolerance), taking into account
+    periodic boundary conditions.
 
     Args:
         coord_list:
-            List of coords to test
+            List of fractional coords
         coord:
-            Specific coordinates
+            A specific fractional coord to test.
         atol:
             Absolute tolerance. Defaults to 1e-8.
+
+    Returns:
+        Indices of matches, e.g., [0, 1, 2, 3]. Empty list if not found.
     """
     if len(coord_list) == 0:
-        return False
+        return []
     coords = np.tile(coord, (len(coord_list), 1))
     fdist = np.mod(coord_list, 1) - np.mod(coords, 1)
     fdist -= np.round(fdist)
-    return np.any(np.all(np.abs(fdist) < atol, axis=1))
+    return np.where(np.all(np.abs(fdist) < atol, axis=1))[0]
+
+
+def in_coord_list_pbc(coord_list, coord, atol=1e-8):
+    """
+    Tests if a particular fractional coord is within a fractional coord_list.
+
+    Args:
+        coord_list:
+            List of fractional coords to test
+        coord:
+            A specific fractional coord to test.
+        atol:
+            Absolute tolerance. Defaults to 1e-8.
+
+    Returns:
+        True if coord is in the coord list.
+    """
+    return len(find_in_coord_list(coord_list, coord, atol=atol)) > 0
 
 
 def get_points_in_sphere_pbc(lattice, frac_points, center, r):
--- pymatgen/util/tests/test_coord_utils.py
+++ pymatgen/util/tests/test_coord_utils.py
@@ -19,7 +19,8 @@ import itertools
 import numpy as np
 from pymatgen.core.lattice import Lattice
 from pymatgen.util.coord_utils import get_linear_interpolated_value, \
-    in_coord_list, pbc_diff, in_coord_list_pbc, get_points_in_sphere_pbc
+    in_coord_list, pbc_diff, in_coord_list_pbc, get_points_in_sphere_pbc,\
+    find_in_coord_list, find_in_coord_list_pbc
 
 
 class CoordUtilsTest(unittest.TestCase):
@@ -37,6 +38,18 @@ class CoordUtilsTest(unittest.TestCase):
         self.assertTrue(in_coord_list(coords, test_coord, atol=0.15))
         self.assertFalse(in_coord_list([0.99,0.99,0.99], test_coord,
                                        atol=0.15))
+
+    def test_find_in_coord_list(self):
+        coords = [[0, 0, 0], [0.5, 0.5, 0.5]]
+        test_coord = [0.1, 0.1, 0.1]
+        self.assertFalse(find_in_coord_list(coords, test_coord))
+        self.assertEqual(find_in_coord_list(coords, test_coord, atol=0.15)[0],
+                         0)
+        self.assertFalse(find_in_coord_list([0.99,0.99,0.99], test_coord,
+                                       atol=0.15))
+        coords = [[0, 0, 0], [0.5, 0.5, 0.5], [0.1, 0.1, 0.1]]
+        self.assertTrue(all(np.equal(find_in_coord_list(coords, test_coord,
+                                                  atol=0.15), [0, 2])))
 
     def test_pbc_diff(self):
         ([0.9, 0.1, 1.01], [0.3, 0.5, 0.9])
@@ -56,6 +69,20 @@ class CoordUtilsTest(unittest.TestCase):
         self.assertTrue(in_coord_list_pbc(coords, test_coord, atol=0.15))
         test_coord = [0.99, 0.99, 0.99]
         self.assertFalse(in_coord_list_pbc(coords, test_coord, atol=0.01))
+
+    def test_find_in_coord_list_pbc(self):
+        coords = [[0, 0, 0], [0.5, 0.5, 0.5]]
+        test_coord = [0.1, 0.1, 0.1]
+        self.assertFalse(find_in_coord_list_pbc(coords, test_coord))
+        self.assertEqual(find_in_coord_list_pbc(coords, test_coord,
+                                                atol=0.15)[0],
+                         0)
+        test_coord = [0.99, 0.99, 0.99]
+        self.assertEqual(find_in_coord_list_pbc(coords, test_coord,
+                                                atol=0.02)[0], 0)
+        test_coord = [-0.499, -0.499, -0.499]
+        self.assertEqual(find_in_coord_list_pbc(coords, test_coord,
+                                                atol=0.01)[0], 1)
 
     def test_get_points_in_sphere_pbc(self):
         latt = Lattice.cubic(1)
