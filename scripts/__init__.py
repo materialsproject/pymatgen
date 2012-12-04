b32e733e4693d7587f0db6aa76fe9bd817ca4657
Shyue Ping Ong <shyue@mit.edu>
2012-12-04 10:47:53 -0500

    Vastly improved POTCAR setup script in scripts directly that is now installed as part of a default pymatgen installation.
    Former-commit-id: 0e7ab478a14eadb067530a5b56dc93ad57658881 [formerly 81a89321ca74cfb625185f50ddb6e7dfdf336cdc]
    Former-commit-id: d61bbb0c8ac66712e9f1aed69aba64d7dcc8f79e

---
+++ scripts/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""
+TODO: Change the module doc.
+"""
+
+from __future__ import division
+
+__author__ = "shyuepingong"
+__version__ = "0.1"
+__maintainer__ = "Shyue Ping Ong"
+__email__ = "shyuep@gmail.com"
+__status__ = "Beta"
+__date__ = "12/4/12"
+
