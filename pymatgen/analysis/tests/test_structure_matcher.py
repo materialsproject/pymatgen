b2675e3b14fdbb75277a5bb18eedc3662a2de4b4
Stephen Dacek <sdacek@mit.edu>
2012-12-10 16:27:43 -0500

    New unittests for structure_matcher

---
+++ pymatgen/analysis/tests/test_structure_matcher.py
@@ -0,0 +1,76 @@
+import unittest
+import os
+import json
+import numpy as np
+import random
+from pymatgen.analysis.structure_matcher import StructureMatcher
+from pymatgen.serializers.json_coders import PMGJSONDecoder
+from pymatgen.core.operations import SymmOp
+from pymatgen.core.structure_modifier import StructureEditor
+from pymatgen.core.structure_modifier import SupercellMaker
+
+
+test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..",
+                        'test_files')
+
+class StructureMatcherTest(unittest.TestCase):
+    
+    def setUp(self):
+        with open(os.path.join(test_dir, "TiO2_entries.json"), 'rb') as fp:
+            testset = json.load(fp, cls=PMGJSONDecoder)
+            
+        self.struct_list = [testcase.structure for testcase in testset]
+    
+    def test_fit(self):
+        """
+        Take two known matched structures
+            1) Ensure match
+            2) Ensure match after translation and rotations
+            3) Ensure no-match after large site translation
+            4) Ensure match after site shuffling
+            """
+        sm = StructureMatcher()
+        
+        self.assertTrue(sm.fit(self.struct_list[0],self.struct_list[1]) == True)
+        
+        """Test rotational/translational invariance"""
+        op = SymmOp.from_axis_angle_and_translation([0, 0, 1], 30, False,
+                                                    np.array([0.4, 0.7, 0.9]))
+        editor = StructureEditor(self.struct_list[1])
+        editor.apply_operation(op)
+        
+        self.assertTrue(sm.fit(self.struct_list[0],editor.modified_structure) == True)
+        
+        """Test failure under large atomic translation"""
+        editor.translate_sites([0], [.4,.4,.2], frac_coords = True)
+        
+        self.assertTrue(sm.fit(self.struct_list[0],editor.modified_structure) != True)
+        
+        editor.translate_sites([0], [-.4,-.4,-.2], frac_coords = True)
+        
+        """Test match under shuffling of sites"""
+        random.shuffle(editor._sites)
+        
+        self.assertTrue(sm.fit(self.struct_list[0],editor.modified_structure) == True)
+        
+    def test_primitive(self):
+        """Test primitive cell reduction"""
+        sm = StructureMatcher(primitive_cell = True)
+        
+        mod = SupercellMaker(self.struct_list[1], scaling_matrix=[[2, 0, 0], [0, 3, 0],
+                                                     [0, 0, 1]])
+        super_cell = mod.modified_structure
+        
+        self.assertTrue(sm.fit(self.struct_list[0],super_cell) == True)
+        
+    def test_class(self):
+        """Tests entire class as single working unit"""
+        sm = StructureMatcher()
+        """ Test group_structures and find_indicies"""
+        out = sm.group_structures(self.struct_list)
+        
+        self.assertTrue(sm.find_indexes(self.struct_list, out)==[0, 0, 0, 1, 2, 3, 4, 0, 5, 6, 7, 8, 8, 9, 9, 10])
+        
+        
+if __name__ == '__main__':
+    unittest.main()
