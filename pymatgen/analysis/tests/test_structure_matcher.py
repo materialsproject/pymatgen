de915bb35c6d3bd0f8e34e6b0e0d75635fc399a6
Shyue Ping Ong <shyue@mit.edu>
2012-12-24 08:35:03 +0800

    Additional unittest for structure matcher to group structures with multiple formula.s

--- pymatgen/analysis/tests/test_structure_matcher.py
+++ pymatgen/analysis/tests/test_structure_matcher.py
@@ -10,22 +10,22 @@ from pymatgen.serializers.json_coders import PMGJSONDecoder
 from pymatgen.core.operations import SymmOp
 from pymatgen.core.structure_modifier import StructureEditor
 from pymatgen.core.structure_modifier import SupercellMaker
-from pymatgen.core.sites import PeriodicSite
+from pymatgen.io.smartio import read_structure
 
 
 test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..",
                         'test_files')
 
 class StructureMatcherTest(unittest.TestCase):
-    
+
     def setUp(self):
         with open(os.path.join(test_dir, "TiO2_entries.json"), 'rb') as fp:
             entries = json.load(fp, cls=PMGJSONDecoder)
         self.struct_list = [e.structure for e in entries]
-        
+
         self.oxi_structs = [io.read_structure(os.path.join(test_dir,"Li2O.cif")),
                             io.read_structure(os.path.join(test_dir,"POSCAR.Li2O"))]
-    
+
     def test_fit(self):
         """
         Take two known matched structures
@@ -35,33 +35,33 @@ class StructureMatcherTest(unittest.TestCase):
             4) Ensure match after site shuffling
             """
         sm = StructureMatcher()
-        
+
         self.assertTrue(sm.fit(self.struct_list[0], self.struct_list[1]))
-        
+
         """Test rotational/translational invariance"""
         op = SymmOp.from_axis_angle_and_translation([0, 0, 1], 30, False,
                                                     np.array([0.4, 0.7, 0.9]))
         editor = StructureEditor(self.struct_list[1])
         editor.apply_operation(op)
         self.assertTrue(sm.fit(self.struct_list[0],editor.modified_structure))
-        
+
         """Test failure under large atomic translation"""
         editor.translate_sites([0], [.4,.4,.2], frac_coords = True)
         self.assertFalse(sm.fit(self.struct_list[0],
                                 editor.modified_structure))
-        
+
         editor.translate_sites([0], [-.4,-.4,-.2], frac_coords = True)
         """Test match under shuffling of sites"""
         random.shuffle(editor._sites)
         self.assertTrue(sm.fit(self.struct_list[0],editor.modified_structure))
-        
+
     def test_oxi(self):
         """Test oxidation state removal matching"""
         sm = StructureMatcher()
         self.assertFalse(sm.fit(self.oxi_structs[0], self.oxi_structs[1]))
         sm = StructureMatcher(comparator=ElementComparator())
         self.assertTrue(sm.fit(self.oxi_structs[0], self.oxi_structs[1]))
-        
+
     def test_primitive(self):
         """Test primitive cell reduction"""
         sm = StructureMatcher(primitive_cell = True)
@@ -69,16 +69,30 @@ class StructureMatcherTest(unittest.TestCase):
                              scaling_matrix=[[2, 0, 0], [0, 3, 0], [0, 0, 1]])
         super_cell = mod.modified_structure
         self.assertTrue(sm.fit(self.struct_list[0],super_cell))
-        
+
     def test_class(self):
         """Tests entire class as single working unit"""
         sm = StructureMatcher()
         """ Test group_structures and find_indicies"""
         out = sm.group_structures(self.struct_list)
-        
+
         self.assertEqual(sm.find_indexes(self.struct_list, out),
             [0, 0, 0, 1, 2, 3, 4, 0, 5, 6, 7, 8, 8, 9, 9, 10])
-        
-        
+
+    def test_mix(self):
+        structures = []
+        for fname in ["POSCAR.Li2O", "Li2O.cif", "Li2O2.cif", "LiFePO4.cif",
+                       "POSCAR.LiFePO4"]:
+            structures.append(read_structure(os.path.join(test_dir, fname)))
+        sm = StructureMatcher(comparator=ElementComparator())
+        groups = sm.group_structures(structures)
+        for g in groups:
+            formula = g[0].composition.reduced_formula
+            if formula in ["Li2O", "LiFePO4"]:
+                self.assertEqual(len(g), 2)
+            else:
+                self.assertEqual(len(g), 1)
+
+
 if __name__ == '__main__':
     unittest.main()
