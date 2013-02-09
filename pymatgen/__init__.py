dda11868bb9356324b2db0714b68ba3c41f01722
Shyue Ping Ong <shyuep@gmail.com>
2013-02-08 23:09:12 -0500

    Add to README an example. Update version to 2.5.2b.

--- pymatgen/__init__.py
+++ pymatgen/__init__.py
@@ -3,7 +3,7 @@ __author__ = ", ".join(["Shyue Ping Ong", "Anubhav Jain", "Geoffroy Hautier",
                         "Michael Kocher", "Dan Gunter", "Shreyas Cholia",
                         "Vincent L Chevrier", "Rickard Armiento"])
 __date__ = "Jan 27 2013"
-__version__ = "2.5.1"
+__version__ = "2.5.2b"
 
 #Useful aliases for commonly used objects and modules.
 
