20e79cfd2610ab42505b604f146918ceb9710233
Shyue Ping Ong <shyue@mit.edu>
2013-02-13 17:34:01 -0500

    Fix potcar generatino script for Osmium.

--- scripts/potcar_setup.py
+++ scripts/potcar_setup.py
@@ -12,7 +12,7 @@ while not os.path.exists(pspdir):
     if count != 0:
         print("Invalid vasp dir!")
     pspdir = raw_input("Please enter full path where the POT_GGA_PAW_PBE, "
-                       "etc. subdirs are present. If you obtain the PSPs "
+                       "etc. subdirs are present. If you obtained the PSPs "
                        "directly from VASP, this should typically be the "
                        "directory that you untar the files to : ")
     print
@@ -27,7 +27,7 @@ print("Generating pymatgen resources directory")
 for (parent, subdirs, files) in os.walk(pspdir):
     for subdir in subdirs:
         filenames = glob.glob(os.path.join(parent, subdir, "POTCAR*"))
-        if len(filenames) == 1:
+        if len(filenames) > 0:
             basedir = os.path.join(targetdir, os.path.basename(parent))
             if not os.path.exists(basedir):
                 os.makedirs(basedir)
@@ -39,6 +39,8 @@ for (parent, subdirs, files) in os.walk(pspdir):
                 subprocess.Popen(["gunzip", dest]).communicate()
             elif ext.upper() in ["BZ2"]:
                 subprocess.Popen(["bunzip2", dest]).communicate()
+            if subdir == "Osmium":
+                subdir = "Os"
             dest = os.path.join(basedir, "POTCAR.{}".format(subdir))
             shutil.move(os.path.join(basedir, "POTCAR"), dest)
             subprocess.Popen(["gzip", dest]).communicate()
