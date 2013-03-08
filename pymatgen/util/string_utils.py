947b162b8910c46ea7d910da4c2ccd4d833c3532
gmatteo <gmatteo@gmail.com>
2013-03-08 23:01:56 +0100

    Add  *.swp files to gitignore
    Former-commit-id: f4861a38bd6560ab183ff7aa65d04a19d4a6dbca [formerly 57923ceec392670909bcb2a2173035c33155a188]
    Former-commit-id: 4567ec2124f9cd30a725dabc878f4505284aa9ae

--- pymatgen/util/string_utils.py
+++ pymatgen/util/string_utils.py
@@ -216,6 +216,45 @@ def list_strings(arg):
         return arg
 
 
+#############################################################################################################
+def stream_has_colours(stream):
+    "True if stream supports colours. Python cookbook, #475186"
+    if not hasattr(stream, "isatty"):
+        return False
+
+    if not stream.isatty():
+        return False # auto color only on TTYs
+    try:
+        import curses
+        curses.setupterm()
+        return curses.tigetnum("colors") > 2
+    except:
+        return False # guess false in case of error
+
+class StringColorizer(object):
+    colours={"default":"",
+             "blue" : "\x1b[01;34m",
+             "cyan" : "\x1b[01;36m",
+             "green": "\x1b[01;32m",
+             "red"  : "\x1b[01;31m",
+             # lighting colours.
+             #"lred":    "\x1b[01;05;37;41m"
+             }
+
+    def __init__(self, stream):
+        self.has_colours = stream_has_colours(stream)
+
+    def __call__(self, string, colour):
+        if self.has_colours:
+            code = self.colours.get(colour, "")
+            if code:
+                return code + string + "\x1b[00m"
+            else:
+                return string
+        else:
+            return string
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
