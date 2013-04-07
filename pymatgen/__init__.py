0c9118ae8b0488b1eaaba27d07ab1d093e30d286
Shyue Ping Ong <shyuep@gmail.com>
2013-04-07 11:28:40 -0400

    Add alias for MPRester.

--- pymatgen/__init__.py
+++ pymatgen/__init__.py
@@ -8,7 +8,8 @@ __version__ = "2.6.5b"
 #Useful aliases for commonly used objects and modules.
 
 from .core import *
-from pymatgen.serializers.json_coders import PMGJSONEncoder, PMGJSONDecoder
-from pymatgen.electronic_structure.core import Spin, Orbital
-from pymatgen.util.io_utils import zopen
-from pymatgen.io.smartio import read_structure, write_structure
+from .serializers.json_coders import PMGJSONEncoder, PMGJSONDecoder
+from .electronic_structure.core import Spin, Orbital
+from .util.io_utils import zopen
+from .io.smartio import read_structure, write_structure
+from .matproj.rest import MPRester
