9eccf729cb6a12940e639e4407711b4100bf9f3c
Shyue Ping Ong <shyuep@gmail.com>
2013-04-07 11:27:15 -0400

    Update available aliases.

--- pymatgen/__init__.py
+++ pymatgen/__init__.py
@@ -7,10 +7,7 @@ __version__ = "2.6.5b"
 
 #Useful aliases for commonly used objects and modules.
 
-from pymatgen.core.periodic_table import Element, Specie
-from pymatgen.core.composition import Composition
-from pymatgen.core.structure import Structure, Molecule
-from pymatgen.core.lattice import Lattice
+from .core import *
 from pymatgen.serializers.json_coders import PMGJSONEncoder, PMGJSONDecoder
 from pymatgen.electronic_structure.core import Spin, Orbital
 from pymatgen.util.io_utils import zopen
