8209fdfabd5d4769e49b10c553ee3c0e75fc08fd
Shyue Ping Ong <shyuep@gmail.com>
2013-04-16 15:08:01 -0400

    Cleanup matcher.
    Former-commit-id: 7301aa18de76c1f59f91227b22db9822a0877fe1 [formerly 746eb03c21ba424a2a2812fc05359b03ef065f45]
    Former-commit-id: 501986ea795a59484abe3db35ae06a69ef604a93

--- pymatgen/analysis/structure_matcher.py
+++ pymatgen/analysis/structure_matcher.py
@@ -255,7 +255,8 @@ class StructureMatcher(MSONable):
     """
 
     def __init__(self, ltol=0.2, stol=0.3, angle_tol=5, primitive_cell=True,
-                 scale=True, attempt_supercell=False, comparator=SpeciesComparator()):
+                 scale=True, attempt_supercell=False,
+                 comparator=SpeciesComparator()):
         """
         Args:
             ltol:
@@ -452,7 +453,7 @@ class StructureMatcher(MSONable):
         frac_tol = \
             np.array([self.stol / ((1 - self.ltol) * np.pi) * i for
                       i in struct1.lattice.reciprocal_lattice.abc]) * \
-            ((nl1.volume + fu * nl2.volume) / (2 * struct1.num_sites)) ** (1.0 / 3)
+            ((nl1.volume + fu * nl2.volume) / (2 * struct1.num_sites)) ** (1 / 3)
 
         #generate structure coordinate lists
         species_list = []
@@ -486,12 +487,14 @@ class StructureMatcher(MSONable):
             aligned_m, rotation_matrix, scale_matrix = \
                 nl2.find_mapping(nl, ltol=self.ltol, atol=self.angle_tol)
 
-            scm = SupercellMaker(struct2, np.linalg.inv(scale_matrix).astype('int'))
+            scm = SupercellMaker(struct2,
+                                 np.linalg.inv(scale_matrix).astype('int'))
 
             for site in scm.modified_structure.sites:
                 found = False
                 for i, species in enumerate(species_list):
-                    if self._comparator.are_equal(site.species_and_occu, species):
+                    if self._comparator.are_equal(site.species_and_occu,
+                                                  species):
                         found = True
                         s2_cart[i].append(site.coords)
                         break
@@ -594,8 +597,9 @@ class StructureMatcher(MSONable):
         # Same number of sites
         if struct1.num_sites != struct2.num_sites:
             #if mismatch try to fit a supercell or return None
-            if self._supercell and ((struct1.num_sites % struct2.num_sites == 0)
-                                    or (struct2.num_sites % struct1.num_sites == 0)):
+            if self._supercell and (
+                    (struct1.num_sites % struct2.num_sites == 0) or
+                    (struct2.num_sites % struct1.num_sites == 0)):
                 return self._supercell_fit(struct1, struct2, break_on_match)
             else:
                 return None
