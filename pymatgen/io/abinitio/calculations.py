0a61ba2a1d7a5d61cc693f7e23329ade08de67ca
Shyue Ping Ong <shyue@mit.edu>
2013-04-29 12:46:36 -0400

    Minor tweaks.
    Former-commit-id: 0058cf619c003701726d8d0973bf3cc8aa7e4bb4 [formerly 5ff2d8a157d12acd697cb4103ca4735db5b59c82]
    Former-commit-id: ae261d805fcd266738f8c6641e5fbd6113b44353

--- pymatgen/io/abinitio/calculations.py
+++ pymatgen/io/abinitio/calculations.py
@@ -3,11 +3,12 @@ from __future__ import division, print_function
 
 import os
 
-#from pprint import pprint
-#
-from pymatgen.io.abinitio.abiobjects import Smearing, KSampling, Screening, SelfEnergy #, AbiStructure, PPModel, 
-from pymatgen.io.abinitio.strategies import ScfStrategy, NscfStrategy, ScreeningStrategy, SelfEnergyStrategy
-from pymatgen.io.abinitio.workflow import PseudoIterativeConvergence, PseudoConvergence, BandStructure, GW_Workflow #, Relaxation, 
+from pymatgen.io.abinitio.abiobjects import Smearing, KSampling, Screening, \
+    SelfEnergy
+from pymatgen.io.abinitio.strategies import ScfStrategy, NscfStrategy, \
+    ScreeningStrategy, SelfEnergyStrategy
+from pymatgen.io.abinitio.workflow import PseudoIterativeConvergence, \
+    PseudoConvergence, BandStructure, GW_Workflow
 
 __author__ = "Matteo Giantomassi"
 __copyright__ = "Copyright 2013, The Materials Project"
@@ -17,34 +18,33 @@ __email__ = "gmatteo at gmail.com"
 __status__ = "Development"
 __date__ = "$Feb 21, 2013M$"
 
-#__all__ = [
-#]
 
-##########################################################################################
+################################################################################
 
 class PPConvergenceFactory(object):
     "Factory object"
 
-    def work_for_pseudo(self, workdir, pseudo, ecut_range, runmode="sequential", atols_mev=(10, 1, 0.1), 
-                        spin_mode="polarized", acell=(8, 9, 10), smearing="fermi_dirac:0.1 eV",):
+    def work_for_pseudo(self, workdir, pseudo, ecut_range, runmode="sequential",
+                        atols_mev=(10, 1, 0.1), spin_mode="polarized",
+                        acell=(8, 9, 10), smearing="fermi_dirac:0.1 eV",):
         """
         Return a Work object given the pseudopotential pseudo
 
         Args:
-            workdir: 
+            workdir:
                 Working directory.
-            pseudo: 
+            pseudo:
                 Pseudo object.
             ecut_range:
                 range of cutoff energies in Ha units.
-            runmode: 
+            runmode:
             atols_mev:
                 Tolerances in meV for accuracy in ["low", "normal", "high"]
             spin_mode:
                 Spin polarization.
-            acell: 
+            acell:
                 Length of the real space lattice (Bohr units)
-            smearing: 
+            smearing:
                 Smearing technique.
         """
         workdir = os.path.abspath(workdir)
@@ -52,34 +52,32 @@ class PPConvergenceFactory(object):
         smearing = Smearing.assmearing(smearing)
 
         if isinstance(ecut_range, slice):
-            workflow = PseudoIterativeConvergence(workdir, pseudo, ecut_range, atols_mev,
-                                              runmode    = runmode,
-                                              spin_mode  = spin_mode,
-                                              acell      = acell, 
-                                              smearing   = smearing,
-                                             )
+            workflow = PseudoIterativeConvergence(
+                workdir, pseudo, ecut_range, atols_mev, runmode=runmode,
+                spin_mode=spin_mode, acell=acell, smearing=smearing)
 
         else:
-            workflow = PseudoConvergence(workdir, pseudo, ecut_range, atols_mev,
-                                     runmode    = runmode,
-                                     spin_mode  = spin_mode,
-                                     acell      = acell, 
-                                     smearing   = smearing,
-                                    )
+            workflow = PseudoConvergence(
+                workdir, pseudo, ecut_range, atols_mev, runmode=runmode,
+                spin_mode=spin_mode, acell=acell, smearing=smearing)
 
         return workflow
 
-##########################################################################################
+################################################################################
 
-def bandstructure(workdir, runmode, structure, pseudos, scf_kppa, nscf_nband, ndivsm, 
-                  accuracy="normal", spin_mode="polarized", smearing="fermi_dirac:0.1 eV", 
-                  charge=0.0, scf_solver=None, dos_kppa=None,
-                 ):
 
-    scf_ksampling = KSampling.automatic_density(structure, scf_kppa, chksymbreak=0)
+def bandstructure(workdir, runmode, structure, pseudos, scf_kppa, nscf_nband,
+                  ndivsm, accuracy="normal", spin_mode="polarized",
+                  smearing="fermi_dirac:0.1 eV", charge=0.0, scf_solver=None,
+                  dos_kppa=None):
 
-    scf_strategy = ScfStrategy(structure, pseudos, scf_ksampling, accuracy=accuracy, spin_mode=spin_mode, 
-                               smearing=smearing, charge=charge, scf_solver=scf_solver)
+    scf_ksampling = KSampling.automatic_density(structure, scf_kppa,
+                                                chksymbreak=0)
+
+    scf_strategy = ScfStrategy(structure, pseudos, scf_ksampling,
+                               accuracy=accuracy, spin_mode=spin_mode,
+                               smearing=smearing, charge=charge,
+                               scf_solver=scf_solver)
 
     nscf_ksampling = KSampling.path_from_structure(ndivsm, structure)
 
@@ -89,45 +87,60 @@ def bandstructure(workdir, runmode, structure, pseudos, scf_kppa, nscf_nband, nd
 
     if dos_kppa is not None:
         raise NotImplementedError("DOS must be tested")
-        dos_ksampling = KSampling.automatic_density(structure, kppa, chksymbreak=0)
-        dos_strategy = NscfStrategy(scf_strategy, dos_ksampling, nscf_nband, nscf_solver=None)
+        dos_ksampling = KSampling.automatic_density(structure, kppa,
+                                                    chksymbreak=0)
+        dos_strategy = NscfStrategy(scf_strategy, dos_ksampling, nscf_nband,
+                                    nscf_solver=None)
 
-    return BandStructure(workdir, runmode, scf_strategy, nscf_strategy, dos_strategy=dos_strategy)
+    return BandStructure(workdir, runmode, scf_strategy, nscf_strategy,
+                         dos_strategy=dos_strategy)
 
-##########################################################################################
+################################################################################
 
-def g0w0_with_ppmodel(workdir, runmode, structure, pseudos, scf_kppa, nscf_nband, ecuteps, ecutsigx,
-                      accuracy="normal", spin_mode="polarized", smearing="fermi_dirac:0.1 eV", 
-                      ppmodel="godby", charge=0.0, scf_solver=None, inclvkb=2, sigma_nband=None, scr_nband=None,
-                     ):
+
+def g0w0_with_ppmodel(workdir, runmode, structure, pseudos, scf_kppa,
+                      nscf_nband, ecuteps, ecutsigx, accuracy="normal",
+                      spin_mode="polarized", smearing="fermi_dirac:0.1 eV",
+                      ppmodel="godby", charge=0.0, scf_solver=None,
+                      inclvkb=2, sigma_nband=None, scr_nband=None):
 
     # TODO: Cannot use istwfk != 1.
     extra_abivars = {"istwfk": "*1"}
 
-    scf_ksampling = KSampling.automatic_density(structure, scf_kppa, chksymbreak=0)
-                                                                                                           
-    scf_strategy = ScfStrategy(structure, pseudos, scf_ksampling, accuracy=accuracy, spin_mode=spin_mode, 
-                               smearing=smearing, charge=charge, scf_solver=None, **extra_abivars)
-                                                                                                           
+    scf_ksampling = KSampling.automatic_density(structure, scf_kppa,
+                                                chksymbreak=0)
+
+    scf_strategy = ScfStrategy(structure, pseudos, scf_ksampling,
+                               accuracy=accuracy, spin_mode=spin_mode,
+                               smearing=smearing, charge=charge,
+                               scf_solver=None, **extra_abivars)
+
     nscf_ksampling = KSampling.automatic_density(structure, 1, chksymbreak=0)
-                                                                                                           
-    nscf_strategy = NscfStrategy(scf_strategy, nscf_ksampling, nscf_nband, **extra_abivars)
-                                                                                                           
+
+    nscf_strategy = NscfStrategy(scf_strategy, nscf_ksampling, nscf_nband,
+                                 **extra_abivars)
+
     if scr_nband is None:
         scr_nband = nscf_nband
 
     if sigma_nband is None:
         sigma_nband = nscf_nband
 
-    screening = Screening(ecuteps, scr_nband, w_type="RPA", sc_mode="one_shot", 
-                          freq_mesh=None, hilbert_transform=None, ecutwfn=None, inclvkb=inclvkb)
+    screening = Screening(ecuteps, scr_nband, w_type="RPA", sc_mode="one_shot",
+                          freq_mesh=None, hilbert_transform=None, ecutwfn=None,
+                          inclvkb=inclvkb)
 
-    self_energy = SelfEnergy("gw", "one_shot", sigma_nband, ecutsigx, screening, ppmodel=ppmodel)
+    self_energy = SelfEnergy("gw", "one_shot", sigma_nband, ecutsigx, screening,
+                             ppmodel=ppmodel)
 
-    scr_strategy = ScreeningStrategy(scf_strategy, nscf_strategy, screening, **extra_abivars)
+    scr_strategy = ScreeningStrategy(scf_strategy, nscf_strategy, screening,
+                                     **extra_abivars)
 
-    sigma_strategy = SelfEnergyStrategy(scf_strategy, nscf_strategy, scr_strategy, self_energy, **extra_abivars)
+    sigma_strategy = SelfEnergyStrategy(scf_strategy, nscf_strategy,
+                                        scr_strategy, self_energy,
+                                        **extra_abivars)
 
-    return GW_Workflow(workdir, runmode, scf_strategy, nscf_strategy, scr_strategy, sigma_strategy)
+    return GW_Workflow(workdir, runmode, scf_strategy, nscf_strategy,
+                       scr_strategy, sigma_strategy)
 
-##########################################################################################
+################################################################################
