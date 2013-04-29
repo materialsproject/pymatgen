9160cb8f4f48c020b2165e94bb5555ebc9f5e78b
Shyue Ping Ong <shyue@mit.edu>
2013-04-29 13:55:40 -0400

    More cleanup.
    Former-commit-id: e8573d0511fbe66088fadd713597aa2267a25721 [formerly 3f25f2a8e1d16eedcad4f194e6306d90fae198a5]
    Former-commit-id: beeedfe5e4f86b02efea727ec7cbbefda0fcbd52

--- pymatgen/util/num_utils.py
+++ pymatgen/util/num_utils.py
@@ -7,45 +7,52 @@ import itertools
 import collections
 import numpy as np
 
+
 def iterator_from_slice(s):
     """
     Constructs an iterator given a slice object s.
 
-    :note: The function returns an infinite iterator if s.stop is None
+    .. note::
+
+        The function returns an infinite iterator if s.stop is None
     """
     start = s.start if s.start is not None else 0
-    step  = s.step  if s.step is not None else 1
+    step = s.step if s.step is not None else 1
 
-    if s.stop is None: 
+    if s.stop is None:
         # Infinite iterator.
         return itertools.count(start=start, step=step)
     else:
         # xrange-like iterator that suppors float.
         return iter(np.arange(start, s.stop, step))
 
+
 def sort_dict(d, key=None, reverse=False):
     """
-    Returns an OrderedDict object whose keys are ordered according to their value.
+    Returns an OrderedDict object whose keys are ordered according to their
+    value.
 
     Args:
         d:
             Input dictionary
-        key: 
-            function which takes an tuple (key, object) and returns a value to compare and sort by. 
-            By default, the function compares the values of the dict i.e. key = lambda t : t[1]
-        reverse: 
+        key:
+            function which takes an tuple (key, object) and returns a value to
+            compare and sort by. By default, the function compares the values
+             of the dict i.e. key = lambda t : t[1]
+        reverse:
             allows to reverse sort order.
     """
     kv_items = [kv for kv in d.items()]
 
     # Sort kv_items according to key.
     if key is None:
-        kv_items.sort(key= lambda t : t[1], reverse=reverse)
+        kv_items.sort(key=lambda t: t[1], reverse=reverse)
     else:
         kv_items.sort(key=key, reverse=reverse)
 
     # Build ordered dict.
     return collections.OrderedDict(kv_items)
+
 
 def chunks(items, n):
     """
@@ -61,11 +68,11 @@ def chunks(items, n):
         yield items[i:i+n]
 
 
-def min_max_indexes(seq): 
+def min_max_indexes(seq):
     """
-    Uses enumerate, max, and min to return the indices of the values 
+    Uses enumerate, max, and min to return the indices of the values
     in a list with the maximum and minimum value:
     """
-    minimum = min(enumerate(seq), key=lambda s: s[1]) 
-    maximum = max(enumerate(seq), key=lambda s: s[1]) 
+    minimum = min(enumerate(seq), key=lambda s: s[1])
+    maximum = max(enumerate(seq), key=lambda s: s[1])
     return minimum[0], maximum[0]
