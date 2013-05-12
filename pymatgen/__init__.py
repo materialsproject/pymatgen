09a6a1b5d6f00db691f15186cd62481ff4ca8cf0
bHb <shyuep@gmail.com>
2013-05-12 15:57:46 -0400

    Add a D5d molecule for symmetry testing.
    Former-commit-id: 10544843596038574b66109c94a1c7b5a69799ce [formerly 9a92c87e140827ad7ddd6cf380b2448dca65c29f]
    Former-commit-id: e4195cc5f4b6233ebca8f15aa92d2d36d52de13f

--- pymatgen/__init__.py
+++ pymatgen/__init__.py
@@ -2,8 +2,8 @@ __author__ = ", ".join(["Shyue Ping Ong", "Anubhav Jain", "Geoffroy Hautier",
                         "William Davidson Richard", "Stephen Dacek",
                         "Michael Kocher", "Dan Gunter", "Shreyas Cholia",
                         "Vincent L Chevrier", "Rickard Armiento"])
-__date__ = "Apr 19 2013"
-__version__ = "2.7.1b"
+__date__ = "May 12 2013"
+__version__ = "2.7.1"
 
 #Useful aliases for commonly used objects and modules.
 
