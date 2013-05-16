6329efa821ebb857eeefff206ac3dcb2b15821d4
bHb <shyuep@gmail.com>
2013-05-15 22:45:16 -0400

    Added the very useful pmg_load and pmg_dump functions, which allows someone to quickly load and dump most pymatgen objects (lists, dicts or otherwise nested) from a filename.
    Former-commit-id: dae02ed43e3f8695f652705299f12756df3f0fa2 [formerly a160e498087d3ef13a54eed731446dea764f04d8]
    Former-commit-id: fd193fe40c113ca0a1eef47fda532f106b856b33

--- pymatgen/__init__.py
+++ pymatgen/__init__.py
@@ -3,8 +3,9 @@ __author__ = ", ".join(["Shyue Ping Ong", "Anubhav Jain", "Geoffroy Hautier",
                         "Michael Kocher", "Dan Gunter", "Shreyas Cholia",
                         "Vincent L Chevrier", "Rickard Armiento"])
 __date__ = "May 12 2013"
-__version__ = "2.7.1"
+__version__ = "2.7.2b"
 
+import json
 #Useful aliases for commonly used objects and modules.
 
 from .core import *
@@ -13,3 +14,34 @@ from .electronic_structure.core import Spin, Orbital
 from .util.io_utils import zopen
 from .io.smartio import read_structure, write_structure, read_mol, write_mol
 from .matproj.rest import MPRester
+
+
+def pmg_load(filename, **kwargs):
+    """
+    Loads a json file and deserialize it with PMGJSONDecoder.
+
+    Args:
+        filename:
+            Filename of file to open. Can be gzipped or bzipped.
+        **kwargs:
+            Any of the keyword arguments supported by the json.load method.
+
+    Returns:
+        Deserialized pymatgen object.
+    """
+    return json.load(zopen(filename), cls=PMGJSONDecoder, **kwargs)
+
+
+def pmg_dump(obj, filename, **kwargs):
+    """
+    Dump an object to a json file using PMGJSONEncoder.
+
+    Args:
+        obj:
+            Object to dump.
+        filename:
+            Filename of file to open. Can be gzipped or bzipped.
+        **kwargs:
+            Any of the keyword arguments supported by the json.load method.
+    """
+    return json.dump(obj, zopen(filename, "w"), cls=PMGJSONEncoder, **kwargs)
