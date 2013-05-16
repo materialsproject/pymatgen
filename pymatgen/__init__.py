c8828a64fc4fe28cc85b01d9bc16f41ce003a078
bHb <shyuep@gmail.com>
2013-05-15 22:51:06 -0400

    Move pmg_load and pmg_dump to json_coders module for better maintenance, but add alias to pymatgen/__init__.py

--- pymatgen/__init__.py
+++ pymatgen/__init__.py
@@ -9,43 +9,9 @@ import json
 #Useful aliases for commonly used objects and modules.
 
 from .core import *
-from .serializers.json_coders import PMGJSONEncoder, PMGJSONDecoder
+from .serializers.json_coders import PMGJSONEncoder, PMGJSONDecoder, \
+    pmg_dump, pmg_load
 from .electronic_structure.core import Spin, Orbital
 from .util.io_utils import zopen
 from .io.smartio import read_structure, write_structure, read_mol, write_mol
 from .matproj.rest import MPRester
-
-
-def pmg_load(filename, **kwargs):
-    """
-    Loads a json file and deserialize it with PMGJSONDecoder.
-
-    Args:
-        filename:
-            Filename of file to open. Can be gzipped or bzipped.
-        **kwargs:
-            Any of the keyword arguments supported by the json.load method.
-
-    Returns:
-        Deserialized pymatgen object. Note that these objects can be lists,
-        dicts or otherwise nested pymatgen objects that support the to_dict
-        and from_dict MSONAble protocol.
-    """
-    return json.load(zopen(filename), cls=PMGJSONDecoder, **kwargs)
-
-
-def pmg_dump(obj, filename, **kwargs):
-    """
-    Dump an object to a json file using PMGJSONEncoder. Note that these
-    objects can be lists, dicts or otherwise nested pymatgen objects that
-    support the to_dict and from_dict MSONAble protocol.
-
-    Args:
-        obj:
-            Object to dump.
-        filename:
-            Filename of file to open. Can be gzipped or bzipped.
-        **kwargs:
-            Any of the keyword arguments supported by the json.load method.
-    """
-    return json.dump(obj, zopen(filename, "w"), cls=PMGJSONEncoder, **kwargs)
