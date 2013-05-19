af2f789e96fa870a12af8553dfd295f6d4d3c16f
bHb <shyuep@gmail.com>
2013-05-19 17:07:01 -0400

    Switch operations ot use pymatgen test.

--- pymatgen/core/tests/test_operations.py
+++ pymatgen/core/tests/test_operations.py
@@ -1,36 +1,34 @@
 #!/usr/bin/python
 
-import unittest
+from pymatgen.util.testing import PymatgenTest
 from pymatgen.core.operations import SymmOp
 import numpy as np
 
 
-class SymmOpTestCase(unittest.TestCase):
+class SymmOpTestCase(PymatgenTest):
 
     def setUp(self):
         self.op = SymmOp.from_axis_angle_and_translation([0, 0, 1], 30, False,
-                                                         np.array([0, 0, 1]))
+                                                         [0, 0, 1])
 
     def test_properties(self):
         rot = self.op.rotation_matrix
         vec = self.op.translation_vector
-        self.assertTrue((abs(rot - np.array([[0.8660254, -0.5, 0.],
-                                             [0.5, 0.8660254, 0.],
-                                             [0., 0., 1.]])) < 0.01).all())
-        self.assertTrue((abs(vec - np.array([0, 0, 1])) < 0.01).all())
+        self.assertArrayAlmostEqual(rot, [[0.8660254, -0.5, 0.],
+                                          [0.5, 0.8660254, 0.],
+                                          [0., 0., 1.]], 2)
+        self.assertArrayAlmostEqual(vec, [0, 0, 1], 2)
 
     def test_operate(self):
         point = np.array([1, 2, 3])
         newcoord = self.op.operate(point)
-        self.assertTrue((abs(newcoord -
-                             np.array([-0.1339746, 2.23205081, 4.])) < 0.01)
-                        .all())
+        self.assertArrayAlmostEqual(newcoord, [-0.1339746, 2.23205081, 4.], 2)
 
     def test_inverse(self):
         point = np.random.rand(3)
         newcoord = self.op.operate(point)
-        self.assertTrue((abs(self.op.inverse.operate(newcoord) - point) < 0.01)
-                        .all())
+        self.assertArrayAlmostEqual(self.op.inverse.operate(newcoord),
+                                    point, 2)
 
     def test_reflection(self):
         normal = np.random.rand(3)
@@ -47,8 +45,8 @@ class SymmOpTestCase(unittest.TestCase):
         point = np.random.rand(3)
         newcoord = self.op.operate(point)
         rotate_only = self.op.apply_rotation_only(point)
-        self.assertTrue((abs(rotate_only - newcoord +
-                             self.op.translation_vector) < 0.01).all())
+        self.assertArrayAlmostEqual(
+            rotate_only + self.op.translation_vector, newcoord, 2)
 
     def test_are_symmetrically_related(self):
         point = np.random.rand(3)
@@ -68,7 +66,8 @@ class SymmOpTestCase(unittest.TestCase):
         op = SymmOp.inversion(origin)
         pt = np.random.rand(3)
         inv_pt = op.operate(pt)
-        self.assertTrue(np.allclose(pt - origin, origin - inv_pt))
+        self.assertArrayAlmostEqual(pt - origin, origin - inv_pt)
 
 if __name__ == '__main__':
+    import unittest
     unittest.main()
