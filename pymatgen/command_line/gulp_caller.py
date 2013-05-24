c692fe20b59161b3127ad3e249552248ee954f99
Shyue Ping Ong <shyue@mit.edu>
2013-05-24 15:28:47 -0400

    Cleanup.
    Former-commit-id: 525fe709a21b84b3eee2280c743b9c1a93f692ad [formerly 0e0c55d2a1486cb1da44b0944a0fe68ab54dbba5]
    Former-commit-id: 6a7374f81996acdcbc405d00d68fe147eac45da8

--- pymatgen/command_line/gulp_caller.py
+++ pymatgen/command_line/gulp_caller.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 """
-Interface with command line GULP. http://projects.ivec.org/gulp/help/manuals.html
+Interface with command line GULP.
+http://projects.ivec.org/gulp/help/manuals.html
 WARNING: you need to have GULP in your path for this to work
 """
 
@@ -15,31 +16,32 @@ __date__ = "$Jan 22, 2013M$"
 
 import subprocess
 import os
-from pymatgen.io.vaspio.vasp_input import Poscar
+
 from pymatgen.command_line.aconvasp_caller import run_aconvasp_command
+
 
 def get_gulpinput(structure):
     """
     From a structure, get a starting gulp.in file using aconvasp
     """
-    output=run_aconvasp_command(["aconvasp", "--gulp"], structure)
+    output = run_aconvasp_command(["aconvasp", "--gulp"], structure)
     gin_string = ""
     for line in output[0].split("\n"):
-        if gin_string=="":
+        if gin_string == "":
             gin_string = gin_string + line
         else:
-            gin_string = gin_string +  "\n" +line
-    
+            gin_string = gin_string + "\n" +line
+
     return gin_string
+
 
 def run_gulp_input(ginfile):
     """
     Given a complete gulp.in file, run GULP
     """
-    command=["gulp"]
+    command = ["gulp"]
     p = subprocess.Popen(command, stdout=subprocess.PIPE,
-                     stdin=subprocess.PIPE,
-                     stderr=subprocess.PIPE)
+                         stdin=subprocess.PIPE, stderr=subprocess.PIPE)
     output = p.communicate(ginfile)
     if "Error" in output[1]:
         raise GulpError(output[1])
@@ -49,101 +51,104 @@ def run_gulp_input(ginfile):
         gout_string = gout_string + line + "\n"
     return gout_string
 
+
 def _gulp_energy(gulpout):
     for line in gulpout.split("\n"):
         if "Total lattice energy" in line and "eV" in line:
-            energy=line.split()
+            energy = line.split()
     return energy[4]
 
+
 def binaryoxide_tersoff_gulpinput(structure):
-    '''
+    """
     Gets a GULP input for an oxide structure
     CURRENTLY ONLY WORKS FOR BINARY OXIDES WITH A SINGLE OXIDATION STATE
     Calculates oxidation state from the formula
-    '''
-    
-    gin=get_gulpinput(structure)
-    gin="static noelectrostatics \n "+gin
-    specs=structure.sites
-    
-    comp=structure.composition.get_el_amt_dict()
+    """
 
-    ginput=""
-    c=0
+    gin = get_gulpinput(structure)
+    gin = "static noelectrostatics \n " + gin
+    specs = structure.sites
+
+    comp = structure.composition.get_el_amt_dict()
+
+    ginput = ""
+    c = 0
     for line in gin.split("\n"):
-        c=c+1
+        c += 1
         if c != 2 and c != 3 and c != 8:
-            if c==1:
-                ginput = ginput+line   
-            elif c>=10 and c < 10+structure.num_sites:
-                d=c-10
-                ginput = ginput + "\n" + str(specs[d].specie) +" core "+line
+            if c == 1:
+                ginput += line
+            elif 10 <= c < 10 + structure.num_sites:
+                d = c - 10
+                ginput += "\n" + str(specs[d].specie) + " core " + line
             else:
-                ginput = ginput+"\n"+line 
-    ginput = ginput + "species \n"
-    
-    lastspec=[]
-    metaloxi=[]
-    endstring=""
+                ginput += "\n" + line
+    ginput += "species \n"
+
+    lastspec = []
+    metaloxi = []
+    endstring = ""
     for ii in specs:
         if lastspec != ii.specie:
-            lastspec=ii.specie
-            
+            lastspec = ii.specie
+
             '''ONE DAY UPDATE THIS WITH BVANALYZER'''
             '''The only challenge is, I don't know how to deal with cation-cation potential'''
-            
+
             if str(lastspec) != "O":
-                nummet=comp[str(lastspec)]
-                numoxi=comp["O"]
-                oxistate=numoxi*2/nummet
-                if oxistate%1 != 0:
+                nummet = comp[str(lastspec)]
+                numoxi = comp["O"]
+                oxistate = numoxi*2/nummet
+                if oxistate % 1 != 0:
                     raise SystemError("Oxide has mixed valence on metal")
-                oxidationstring=str(lastspec)+" core "+str(oxistate)
+                oxidationstring = str(lastspec)+" core "+str(oxistate)
                 metaloxi.append(lastspec)
             else:
-                oxidationstring=str(lastspec)+" core -2"
-                
-            ginput=ginput+oxidationstring+ "\n"
-            endstring=endstring+"qerfc \n"+str(lastspec)+" "+str(lastspec)+"  0.6000 10.0000 \n" 
-    
-    ginput=ginput+"# noelectrostatics \n Morse \n"
-    
+                oxidationstring = str(lastspec)+" core -2"
+
+            ginput += oxidationstring+ "\n"
+            endstring += "qerfc \n" + str(lastspec) + " " + str(lastspec) + \
+                         "  0.6000 10.0000 \n"
+
+    ginput += "# noelectrostatics \n Morse \n"
+
     for metal in metaloxi:
-        metal=str(metal)+"("+str(int(oxistate))+")"
-        MetOxiTers=Tersoff_pot().data[metal]
-        ginput=ginput+MetOxiTers
-    
-    ginput=ginput+endstring
-    
+        metal = str(metal) + "(" + str(int(oxistate)) + ")"
+        MetOxiTers = Tersoff_pot().data[metal]
+        ginput += MetOxiTers
+
+    ginput += endstring
+
     return ginput
 
+
 def gulpduplicatecheck(structure):
-    
-    gin=get_gulpinput(structure)
-    gin="single \n "+gin
-    
-    ginput=""
-    c=0
+
+    gin = get_gulpinput(structure)
+    gin = "single \n "+gin
+
+    ginput = ""
+    c = 0
     for line in gin.split("\n"):
-        c=c+1
+        c += 1
         if c != 2 and c != 3 and c != 8:
-            if c==1:
-                ginput = ginput+line   
-            elif c>=10 and c < 10+structure.num_sites:
-                d=c-10
-                ginput = ginput + "\n" + "Au core "+line
+            if c == 1:
+                ginput += line
+            elif 10 <= c < 10+structure.num_sites:
+                ginput += "\n" + "Au core "+line
             else:
-                ginput = ginput+"\n"+line 
-    ginput = ginput + "lennard 12 6 \n"
-    ginput = ginput + "Au core Au core 214180.2000 625.482 40.000 0 0"
-    
-    output2=run_gulp_input(ginput)
+                ginput += "\n" + line
+    ginput += "lennard 12 6 \n"
+    ginput += "Au core Au core 214180.2000 625.482 40.000 0 0"
+
+    output2 = run_gulp_input(ginput)
     return float(_gulp_energy(output2))
 
 
 def get_binoxi_gulp_energy(structure):
-    output=binaryoxide_tersoff_gulpinput(structure)
-    output2=run_gulp_input(output)
+    output = binaryoxide_tersoff_gulpinput(structure)
+    output2 = run_gulp_input(output)
     return float(_gulp_energy(output2))
 
 
@@ -152,12 +157,8 @@ class GulpError(Exception):
     Exception class for aconvasp.
     Raised when the aconvasp gives an error
     """
+    pass
 
-    def __init__(self, msg):
-        self.msg = msg
-
-    def __str__(self):
-        return "GulpError : " + self.msg
 
 class Tersoff_pot(object):
     def __init__(self, verbose=False):
@@ -165,8 +166,8 @@ class Tersoff_pot(object):
         fid = open(os.path.join(module_dir, "OxideTersoffPotentials"), "rU")
         data = dict()
         for row in fid:
-            metaloxi=row.split()[0]
-            line=row.split(")")
-            data[metaloxi]=line[1]
+            metaloxi = row.split()[0]
+            line = row.split(")")
+            data[metaloxi] = line[1]
         fid.close()
-        self.data=data
+        self.data = data
