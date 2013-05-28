dd4512703cd15f62df7c6e7947bfac3206da1642
__sp__ <shyue@mit.edu>
2013-05-28 15:12:15 -0400

    Update dev version number.
    Former-commit-id: 5cc5090e16556d868420138c7d0d2c2d8bd7a460 [formerly 2cc0a11dcd0f972ffced8f236d1a87b3398e0584]
    Former-commit-id: 1211a8f06494634eb66ef0d6537c0b4dd5af45f5

--- pymatgen/__init__.py
+++ pymatgen/__init__.py
@@ -3,7 +3,7 @@ __author__ = ", ".join(["Shyue Ping Ong", "Anubhav Jain", "Geoffroy Hautier",
                         "Michael Kocher", "Dan Gunter", "Shreyas Cholia",
                         "Vincent L Chevrier", "Rickard Armiento"])
 __date__ = "May 12 2013"
-__version__ = "2.7.2b"
+__version__ = "2.7.3b"
 
 import json
 #Useful aliases for commonly used objects and modules.
