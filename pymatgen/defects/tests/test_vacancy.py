e3369f8cc86101b80bc7bfc95e0bf76bd81125f4
Bharat K. Medasani <mbkumar@gmail.com>
2013-06-05 14:07:39 -0700

    Bug corrections in point defects

--- pymatgen/defects/tests/test_vacancy.py
+++ pymatgen/defects/tests/test_vacancy.py
@@ -11,18 +11,45 @@ import numpy as np
 class VacancyTest(unittest.TestCase):
     def setUp(self):
         """
-        Generate MgO rocksalt structure and compare the vacancies
+        Setup MgO rocksalt structure for testing Vacancy
         """
         mgo_latt = [[4.212, 0, 0], [0, 4.212, 0], [0, 0, 4.212]]
         mgo_specie = ["Mg2+"]*4 +  ["O2-"]*4
         mgo_frac_cord = [[0, 0, 0], [0.5, 0.5, 0], [0.5, 0, 0.5], [0, 0.5, 0.5],
                          [0.5, 0, 0], [0, 0.5, 0], [0, 0, 0.5], [0.5, 0.5, 0.5]]
         self.mgo_uc = Structure(mgo_latt, mgo_specie, mgo_frac_cord, True, True)
-        #vac1 = mgo_uc.remove(0)
-        #vac2 = mgo_uc.remove(4)
-        #mgo_symmfinder = SymmetryFinder(mgo_uc)
     
-    def test_init(self):
+    def test_enumerate_uniq_defectsites(self):
+        """
+        The vacancy sites should be the lattice sites. 
+        And there should be only two unique vacancy sites for MgO.
+        """
+        mgo_uc1 = self.mgo_uc.copy()
+        uniq_sites = []
+        uniq_sites.append(mgo_uc1.sites[3])
+        uniq_sites.append(mgo_uc1.sites[7])
+        mgo_vac = Vacancy(mgo_uc1)
+        uniq_def_sites = mgo_vac.enumerate_uniq_defectsites()
+        self.assertTrue(len(uniq_def_sites) == 2, "Vacancy init failed")
+        #mgo_spg = Spacegroup(int_number=225)
+        #self.assertTrue(mgo_spg.are_symmetrically_equivalent(uniq_sites,
+        #                uniq_def_sites),  "Vacancy init failed")
+        
+class InterstitialTest(unittest.TestCase):
+    def setUp(self):
+        """
+        Setup MgO rocksalt structure for testing Interstitial
+        """
+        mgo_latt = [[4.212, 0, 0], [0, 4.212, 0], [0, 0, 4.212]]
+        mgo_specie = ["Mg2+"]*4 +  ["O2-"]*4
+        mgo_frac_cord = [[0, 0, 0], [0.5, 0.5, 0], [0.5, 0, 0.5], [0, 0.5, 0.5],
+                         [0.5, 0, 0], [0, 0.5, 0], [0, 0, 0.5], [0.5, 0.5, 0.5]]
+        self.mgo_uc = Structure(mgo_latt, mgo_specie, mgo_frac_cord, True, True)
+    
+    def test_enumerate_uniq_defectsites(self):
+        """
+        The interstitial sites should be within the lattice
+        """
         mgo_uc1 = self.mgo_uc.copy()
         uniq_sites = []
         uniq_sites.append(mgo_uc1.sites[3])
