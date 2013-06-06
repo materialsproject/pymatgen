c58045fc35f485f4826fbb62f22ce7433d6b4be6
__sp__ <shyuep@gmail.com>
2013-06-05 22:08:40 -0400

    Refactor nwcheminput module.
    Former-commit-id: 40941c8ed127b21eeff7a17821d23e1199d91724 [formerly 9c147a31728f9c1d4fa4b137a1b71d32865dd5ee]
    Former-commit-id: 777d96e8a878d1e4655a64fcd34dc5112ba72304

--- pymatgen/io/nwchemio.py
+++ pymatgen/io/nwchemio.py
@@ -21,33 +21,72 @@ from pymatgen.util.io_utils import zopen
 
 
 class NwTask(object):
+    """
+    Base task for Nwchem. Very flexible arguments to support many types of
+    potential setups. Users should use more friendly subclasses unless they
+    need the flexibility.
+    """
 
-    def __init__(self, elements, title=None, theory="dft",
-                 operation="optimize", basis_set="6-31++G**",
-                 theory_directives=None):
+    def __init__(self, mol, charge=None, spin_multiplicity=None,
+                 title=None, theory="dft", operation="optimize",
+                 basis_set="6-31++G**", theory_directives=None):
+
         self.title = title if title is not None else "{} {} {}".format(
-            "".join(map(str, elements)), theory, operation)
+            re.sub("\s", "", mol.formula), theory, operation)
+
+        self.charge = charge if charge is not None else mol.charge
+        nelectrons = - self.charge + mol.charge + mol.nelectrons
+        if spin_multiplicity is not None:
+            self.spin_multiplicity = spin_multiplicity
+            if (nelectrons + spin_multiplicity) % 2 != 1:
+                raise ValueError(
+                    "Charge of {} and spin multiplicity of {} is"
+                    " not possible for this molecule".format(
+                        charge, spin_multiplicity))
+        else:
+            self.spin_multiplicity = 1 if nelectrons % 2 == 0 else 2
+
+        elements = set(mol.composition.get_el_amt_dict().keys())
+
         self.theory = theory
         self.basis_set = basis_set
         self.elements = elements
         self.operation = operation
-        self.directives = theory_directives if theory_directives is not None \
-            else {}
+        self.theory_directives = theory_directives \
+            if theory_directives is not None else {}
 
     def __str__(self):
         o = ["title \"{}\"".format(self.title),
+             "charge {}".format(self.charge),
              "basis"]
         for el in self.elements:
             o.append(" {} library {}".format(el, self.basis_set))
         o.append("end")
-        if self.directives:
+        if self.theory_directives:
             o.append("{}".format(self.theory))
-            for k, v in self.directives.items():
+            for k, v in self.theory_directives.items():
                 o.append(" {} {}".format(k, v))
             o.append("end")
         o.append("task {} {}".format(self.theory, self.operation))
         return "\n".join(o)
 
+
+class DFTTask(NwTask):
+    """
+    Creates a DFT task from a molecule with some other basic init.
+    """
+
+    def __init__(self, mol, xc="b3lyp", charge=None, spin_multiplicity=None,
+                 title=None, operation="optimize", basis_set="6-31++G*",
+                 theory_directives=None):
+
+        super(DFTTask, self).__init__(
+            mol, charge=charge, spin_multiplicity=spin_multiplicity,
+            title=title, theory="dft", operation=operation,
+            basis_set=basis_set, theory_directives=theory_directives)
+
+        self.theory_directives.update({"xc": xc,
+                                       "mult": self.spin_multiplicity})
 
 
 class NwInput(object):
@@ -55,8 +94,7 @@ class NwInput(object):
     An object representing a Nwchem input file.
     """
 
-    def __init__(self, mol, tasks, directives=None, charge=None,
-                 spin_multiplicity=None):
+    def __init__(self, mol, tasks, directives=None):
         """
         Args:
             mol:
@@ -66,39 +104,14 @@ class NwInput(object):
             tasks:
                 List of NwTasks.
             job_title:
-                Title of job. Typically
-            charge:
-                Charge of the molecule. If None, charge on molecule is used.
-                Defaults to None. This allows the input file to be set a
-                charge independently from the molecule itself.
-            spin_multiplicity:
-                Spin multiplicity of molecule. Defaults to None,
-                which means that the spin multiplicity is set to 1 if the
-                molecule has no unpaired electrons and to 2 if there are
-                unpaired electrons.
+                Title of job.
         """
         self._mol = mol
-        self.charge = charge if charge is not None else mol.charge
-        nelectrons = - self.charge + mol.charge + mol.nelectrons
-        if spin_multiplicity is not None:
-            self.spin_multiplicity = spin_multiplicity
-            if (nelectrons + spin_multiplicity) % 2 != 1:
-                raise ValueError(
-                    "Charge of {} and spin multiplicity of {} is"
-                    " not possible for this molecule".format(
-                        self.charge, spin_multiplicity))
-        else:
-            self.spin_multiplicity = 1 if nelectrons % 2 == 0 else 2
-        self.tasks = tasks
-        if self.spin_multiplicity != 1:
-            for t in self.tasks:
-                if t.theory.lower() == "dft":
-                    t.directives["mult"] = self.spin_multiplicity
-
         if directives is None:
             self.directives = [("start", re.sub("\s", "", self._mol.formula))]
         else:
             self.directives = self.directives
+        self.tasks = tasks
 
     @property
     def molecule(self):
@@ -112,14 +125,14 @@ class NwInput(object):
         o = []
         for d in self.directives:
             o.append("{} {}".format(d[0], d[1]))
-        o.append("charge {}".format(self.charge))
         o.append("geometry units angstroms")
         for site in self._mol:
             o.append(" {} {} {} {}".format(site.specie.symbol, site.x, site.y,
                                            site.z))
-        o.append("end")
+        o.append("end\n")
         for t in self.tasks:
             o.append(str(t))
+            o.append("")
         return "\n".join(o)
 
     def write_file(self, filename):
@@ -135,16 +148,25 @@ test_dir = os.path.join(os.path.dirname(__file__), "..", "..",
                         'test_files', "molecules")
 
 
+coords = [[0.000000, 0.000000, 0.000000],
+                  [0.000000, 0.000000, 1.089000],
+                  [1.026719, 0.000000, -0.363000],
+                  [-0.513360, -0.889165, -0.363000],
+                  [-0.513360, 0.889165, -0.363000]]
+mol = Molecule(["C", "H", "H", "H", "H"], coords)
+
+
 class NwTaskTest(unittest.TestCase):
 
     def setUp(self):
-        self.task = NwTask(["H", "O"], theory_directives={"xc": "b3lyp"})
+        self.task = NwTask(mol, theory_directives={"xc": "b3lyp"})
 
     def test_str_and_from_string(self):
-        ans = """title "HO dft optimize"
+        ans = """title "H4C1 dft optimize"
+charge 0
 basis
  H library 6-31++G**
- O library 6-31++G**
+ C library 6-31++G**
 end
 dft
  xc b3lyp
@@ -153,27 +175,114 @@ task dft optimize"""
         self.assertEqual(str(self.task), ans)
 
 
+class DFTTaskTest(unittest.TestCase):
+
+    def setUp(self):
+
+        self.task = DFTTask(mol, charge=1, operation="energy")
+
+    def test_str_and_from_string(self):
+        ans = """title "H4C1 dft energy"
+charge 1
+basis
+ H library 6-31++G*
+ C library 6-31++G*
+end
+dft
+ xc b3lyp
+ mult 2
+end
+task dft energy"""
+        self.assertEqual(str(self.task), ans)
+
+
 class NwInputTest(unittest.TestCase):
 
     def setUp(self):
-        coords = [[0.000000, 0.000000, 0.000000],
-                  [0.000000, 0.000000, 1.089000],
-                  [1.026719, 0.000000, -0.363000],
-                  [-0.513360, -0.889165, -0.363000],
-                  [-0.513360, 0.889165, -0.363000]]
-        mol = Molecule(["C", "H", "H", "H", "H"], coords)
         tasks = [
-            NwTask(["H", "C"], operation="optimize",
-                   theory_directives={"xc": "b3lyp"}),
-            NwTask(["H", "C"], operation="freq",
-                   theory_directives={"xc": "b3lyp"})
+            DFTTask(mol, operation="optimize", xc="b3lyp",
+                    basis_set="6-31++G*"),
+            DFTTask(mol, operation="freq", xc="b3lyp", basis_set="6-31++G*"),
+            DFTTask(mol, operation="energy", xc="b3lyp",
+                    basis_set="6-311++G**"),
+            DFTTask(mol, charge=mol.charge + 1, operation="energy", xc="b3lyp",
+                    basis_set="6-311++G**"),
+            DFTTask(mol, charge=mol.charge - 1, operation="energy", xc="b3lyp",
+                    basis_set="6-311++G**")
         ]
-        self.nwi = NwInput(
-            mol, tasks, charge=1)
+        self.nwi = NwInput(mol, tasks)
 
     def test_str(self):
-        print self.nwi
-        self.nwi.write_file("CH4.nw")
+        ans = """start H4C1
+geometry units angstroms
+ C 0.0 0.0 0.0
+ H 0.0 0.0 1.089
+ H 1.026719 0.0 -0.363
+ H -0.51336 -0.889165 -0.363
+ H -0.51336 0.889165 -0.363
+end
+
+title "H4C1 dft optimize"
+charge 0
+basis
+ H library 6-31++G*
+ C library 6-31++G*
+end
+dft
+ xc b3lyp
+ mult 1
+end
+task dft optimize
+
+title "H4C1 dft freq"
+charge 0
+basis
+ H library 6-31++G*
+ C library 6-31++G*
+end
+dft
+ xc b3lyp
+ mult 1
+end
+task dft freq
+
+title "H4C1 dft energy"
+charge 0
+basis
+ H library 6-311++G**
+ C library 6-311++G**
+end
+dft
+ xc b3lyp
+ mult 1
+end
+task dft energy
+
+title "H4C1 dft energy"
+charge 1
+basis
+ H library 6-311++G**
+ C library 6-311++G**
+end
+dft
+ xc b3lyp
+ mult 2
+end
+task dft energy
+
+title "H4C1 dft energy"
+charge -1
+basis
+ H library 6-311++G**
+ C library 6-311++G**
+end
+dft
+ xc b3lyp
+ mult 2
+end
+task dft energy
+"""
+        self.assertEqual(str(self.nwi), ans)
 
 
 if __name__ == "__main__":
