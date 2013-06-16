3a2cf254f9944369ae213dbe87d9dcee68acfe0a
__sp__ <shyuep@gmail.com>
2013-06-15 23:56:27 -0400

    Improve nwchem parsing.

--- pymatgen/io/tests/test_nwchemio.py
+++ pymatgen/io/tests/test_nwchemio.py
@@ -197,12 +197,16 @@ class NwOutputTest(unittest.TestCase):
 
     def test_read(self):
         nwo = NwOutput(os.path.join(test_dir, "CH4.nwout"))
+
+        self.assertEqual(0, nwo.data[0]["Charge"])
+        self.assertEqual(-1, nwo.data[-1]["Charge"])
+
         self.assertAlmostEqual(-1102.622361621359, nwo.data[0]["energies"][-1])
         self.assertAlmostEqual(-1102.9985415777337, nwo.data[2]["energies"][-1])
-        ie = (nwo.data[3]["energies"][-1] - nwo.data[2]["energies"][-1])
-        ea = (nwo.data[2]["energies"][-1] - nwo.data[4]["energies"][-1])
-        self.assertAlmostEqual(14.997876767843081, ie)
-        self.assertAlmostEqual(-0.7575358046858582, ea)
+        ie = (nwo.data[4]["energies"][-1] - nwo.data[2]["energies"][-1])
+        ea = (nwo.data[2]["energies"][-1] - nwo.data[3]["energies"][-1])
+        self.assertAlmostEqual(0.7575358046858582, ie)
+        self.assertAlmostEqual(-14.997876767843081, ea)
 
 
 if __name__ == "__main__":
