2ecb5fbce4b9d065fdcefb465d0565560a5a5265
__sp__ <shyuep@gmail.com>
2013-08-30 14:23:38 -0700

    Sexy new units management system.
    Former-commit-id: f50bf491da70ddfba6b2d6c4a39e47a72d156668 [formerly 90110733c7ee11bbd1893962f106835a05c351c1]
    Former-commit-id: e10d433a0b4f61f95be109cee6130b5817bbc9e4

--- pymatgen/core/units.py
+++ pymatgen/core/units.py
@@ -3,8 +3,74 @@ Unit conversion tools. These functions are defined here instead of
 core.physical_constants because we have to import and inspect the attributes
 of the module.
 """
+from __future__ import division
+
 import numpy as np
 import pymatgen.core.physical_constants as c
+from functools import partial
+
+#The values
+
+SUPPORTED_UNITS = {
+    "energy": {
+        "eV": 1,
+        "Ha": 27.21138386,
+        "Ry": 13.605698066,
+        "J": 6.24150934e18
+    },
+    "length": {
+        "ang": 1,
+        "m": 1e10,
+        "cm": 1e8,
+        "bohr": 0.5291772083
+    },
+    "mass": {
+        "kg": 1,
+        "u": 1.660538921e-27
+    }
+}
+
+
+class Unit(float):
+    """
+    Subclasses float to attach a unit type.
+    """
+
+    def __new__(cls, val, unit, unit_type):
+        return float.__new__(cls, val)
+
+    def __init__(self, val, unit, unit_type):
+        self.val = val
+        self.unit_type = unit_type
+        if unit not in SUPPORTED_UNITS[unit_type]:
+            raise ValueError(
+                "{} is not a supported unit for {}".format(unit, unit_type))
+        self.unit = unit
+        super(Unit, self).__init__(val)
+
+    def __repr__(self):
+        return "{} {}".format(self.val, self.unit)
+
+    def __str__(self):
+        return "{} {}".format(self.val, self.unit)
+
+    def to(self, new_unit):
+        if new_unit not in SUPPORTED_UNITS[self.unit_type]:
+            raise ValueError(
+                "{} is not a supported unit for {}".format(new_unit,
+                                                           self.unit_type))
+        conversion = SUPPORTED_UNITS[self.unit_type]
+        return Unit(
+            self.val / conversion[new_unit] * conversion[self.unit],
+            unit_type=self.unit_type, unit=new_unit)
+
+
+Energy = partial(Unit, unit_type="energy")
+
+Length = partial(Unit, unit_type="length")
+
+Mass = partial(Unit, unit_type="mass")
+
 
 def _nop(values):
     return np.asanyarray(values)
