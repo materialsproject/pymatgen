f2f7d24bb4085621093b3aa16b7128f07d37239a
__sp__ <shyuep@gmail.com>
2013-08-31 16:02:12 -0700

    Make it less easy to modify internal unit variables.
    Former-commit-id: 36297a13c5c129fba609d30bccbfe0c65db0f236 [formerly 236e49d6b8d8df8074eefd011e8a83b6ea694f55]
    Former-commit-id: 288c0a53904c41800ca318e1c2d73e3bc5b8291d

--- pymatgen/core/units.py
+++ pymatgen/core/units.py
@@ -86,16 +86,16 @@ class Unit(float):
         return float.__new__(cls, val)
 
     def __init__(self, val, unit, unit_type):
-        self.unit_type = unit_type
+        self._unit_type = unit_type
         if unit not in SUPPORTED_UNITS[unit_type]:
             raise ValueError(
                 "{} is not a supported unit for {}".format(unit, unit_type))
-        self.unit = unit
+        self._unit = unit
         super(Unit, self).__init__(val)
 
     def __repr__(self):
         s = super(Unit, self).__repr__()
-        return "{} {}".format(s, self.unit)
+        return "{} {}".format(s, self._unit)
 
     def __str__(self):
         return self.__repr__()
@@ -103,53 +103,61 @@ class Unit(float):
     def __add__(self, other):
         if not hasattr(other, "unit_type"):
             return super(Unit, self).__add__(other)
-        if other.unit_type != self.unit_type:
+        if other.unit_type != self._unit_type:
             raise ValueError("Adding different types of units is not allowed")
         val = other
-        if other.unit != self.unit:
-            val = other.to(self.unit)
-        return Unit(float(self) + val, unit_type=self.unit_type,
-                    unit=self.unit)
+        if other.unit != self._unit:
+            val = other.to(self._unit)
+        return Unit(float(self) + val, unit_type=self._unit_type,
+                    unit=self._unit)
 
     def __sub__(self, other):
         if not hasattr(other, "unit_type"):
             return super(Unit, self).__sub__(other)
-        if other.unit_type != self.unit_type:
+        if other.unit_type != self._unit_type:
             raise ValueError("Subtracting different units is not allowed")
         val = other
-        if other.unit != self.unit:
-            val = other.to(self.unit)
-        return Unit(float(self) - val, unit_type=self.unit_type,
-                    unit=self.unit)
+        if other.unit != self._unit:
+            val = other.to(self._unit)
+        return Unit(float(self) - val, unit_type=self._unit_type,
+                    unit=self._unit)
 
     def __mul__(self, other):
         val = super(Unit, self).__mul__(other)
         if isinstance(other, (float, int)):
-            return Unit(val, unit_type=self.unit_type, unit=self.unit)
+            return Unit(val, unit_type=self._unit_type, unit=self._unit)
         return val
 
     def __rmul__(self, other):
         val = super(Unit, self).__rmul__(other)
         if isinstance(other, (float, int)):
-            return Unit(val, unit_type=self.unit_type, unit=self.unit)
+            return Unit(val, unit_type=self._unit_type, unit=self._unit)
         return val
 
     def __div__(self, other):
         val = super(Unit, self).__div__(other)
         if isinstance(other, (float, int)):
-            return Unit(val, unit_type=self.unit_type, unit=self.unit)
+            return Unit(val, unit_type=self._unit_type, unit=self._unit)
         return val
 
     def __truediv__(self, other):
         val = super(Unit, self).__truediv__(other)
         if isinstance(other, (float, int)):
-            return Unit(val, unit_type=self.unit_type, unit=self.unit)
+            return Unit(val, unit_type=self._unit_type, unit=self._unit)
         return val
 
     def __neg__(self):
-        return Unit(super(Unit, self).__neg__(), unit_type=self.unit_type,
-                    unit=self.unit)
+        return Unit(super(Unit, self).__neg__(), unit_type=self._unit_type,
+                    unit=self._unit)
 
+    @property
+    def unit_type(self):
+        return self._unit_type
+
+    @property
+    def unit(self):
+        return self._unit
+    
     def to(self, new_unit):
         """
         Conversion to a new_unit.
@@ -167,21 +175,21 @@ class Unit(float):
         >>> e.to("eV")
         29.932522246 eV
         """
-        if new_unit not in SUPPORTED_UNITS[self.unit_type]:
+        if new_unit not in SUPPORTED_UNITS[self._unit_type]:
             raise ValueError(
                 "{} is not a supported unit for {}".format(new_unit,
-                                                           self.unit_type))
-        conversion = SUPPORTED_UNITS[self.unit_type]
+                                                           self._unit_type))
+        conversion = SUPPORTED_UNITS[self._unit_type]
         return Unit(
-            self / conversion[new_unit] * conversion[self.unit],
-            unit_type=self.unit_type, unit=new_unit)
+            self / conversion[new_unit] * conversion[self._unit],
+            unit_type=self._unit_type, unit=new_unit)
 
     @property
     def supported_units(self):
         """
         Supported units for specific unit type.
         """
-        return SUPPORTED_UNITS[self.unit_type]
+        return SUPPORTED_UNITS[self._unit_type]
 
 
 Energy = partial(Unit, unit_type="energy")
