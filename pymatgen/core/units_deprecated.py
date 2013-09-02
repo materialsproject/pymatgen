c3ddcbc93d1e3aadbb34dbf394983ee8ae0893ca
gmatteo <gmatteo@gmail.com>
2013-09-02 17:00:59 +0200

    Add support for volumes in units.py
    Former-commit-id: 60c66dceca4ea1c90cda733c34bd13ac869d41f3 [formerly 5faa196ba268da988909f003df0f9745df366132]
    Former-commit-id: 4668b0bee7c439ccd1a6c4ce6d41ba27e11b7a7b

--- pymatgen/core/units_deprecated.py
+++ pymatgen/core/units_deprecated.py
@@ -2,14 +2,14 @@ import numpy as np
 import pymatgen.core.physical_constants as c
 
 
-def _nop(values):
-    return np.asanyarray(values)
-
-_any2Ha = {"Ha": _nop}
-for var in vars(c):
-    attr = c.__getattribute__(var)
-    if var.endswith("2Ha") and callable(attr):
-        _any2Ha[var[:-3]] = attr
+#def _nop(values):
+#    return np.asanyarray(values)
+#
+#_any2Ha = {"Ha": _nop}
+#for var in vars(c):
+#    attr = c.__getattribute__(var)
+#    if var.endswith("2Ha") and callable(attr):
+#        _any2Ha[var[:-3]] = attr
 
 
 #def any2Ha(units):
@@ -41,59 +41,14 @@ for var in vars(c):
 #    return _any2eV[units]
 
 
-_any2Bohr = {"Bohr": _nop}
-for var in vars(c):
-    attr = c.__getattribute__(var)
-    if var.endswith("2Bohr") and callable(attr):
-        _any2Bohr[var[:-5]] = attr
+#_any2Bohr = {"Bohr": _nop}
+#for var in vars(c):
+#    attr = c.__getattribute__(var)
+#    if var.endswith("2Bohr") and callable(attr):
+#        _any2Bohr[var[:-5]] = attr
+#
+#
+#    1.0
+#    """
+#    return _any2Ang[units]
 
-
-def any2Bohr(units):
-    """
-    Returns a callable that converts length(s) from units to Bohr.
-
-    >>> any2Bohr("Bohr")(5)
-    array(5)
-
-    >>> any2Bohr("Ang")(1.0)
-    1.8897261339212517
-    """
-    return _any2Bohr[units]
-
-
-_any2Ang = {"Ang": _nop}
-for var in vars(c):
-    attr = c.__getattribute__(var)
-    if var.endswith("2Ang") and callable(attr):
-        _any2Ang[var[:-4]] = attr
-
-
-def any2Ang(units):
-    """
-    Returns a callable that converts length(s) from units to Angstrom
-
-    >>> any2Ang("Bohr")(any2Bohr("Ang")(1))
-    1.0
-    """
-    return _any2Ang[units]
-
-
-def any2Ang3(len_units):
-    """
-    Returns a callable that converts volume(s) given in unit length len_units to Angstrom**3.
-
-    >>> any2Ang3("Ang")([1.,2.,3.])
-    array([ 1.,  2.,  3.])
-
-    >>> any2Ang3("Bohr")([1,2,3])
-    array([ 0.14818471,  0.29636942,  0.44455413])
-    """
-    def func(values):
-        func._len_units = len_units[:]
-        values = np.asarray(values)
-        if func._len_units == "Ang":
-            return values
-        else:
-            f = any2Ang(func._len_units)
-            return f(values**(1/3.)) ** 3
-    return func
