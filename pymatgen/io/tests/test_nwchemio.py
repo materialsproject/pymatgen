ee337ffedefa6b43bf696cdd1e97bc35f46ae8bd
navnidhirajput <nnrajput@lbl.gov>
2013-09-13 14:05:10 -0700

    unit test for nwchem output

--- pymatgen/io/tests/test_nwchemio.py
+++ pymatgen/io/tests/test_nwchemio.py
@@ -13,7 +13,6 @@ __maintainer__ = "Shyue Ping Ong"
 __email__ = "shyuep@gmail.com"
 __date__ = "6/6/13"
 
-
 import unittest
 import os
 import json
@@ -25,7 +24,6 @@ from pymatgen.io.nwchemio import NwTask, NwInput, NwInputError, NwOutput
 test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..",
                         'test_files', "molecules")
 
-
 coords = [[0.000000, 0.000000, 0.000000],
           [0.000000, 0.000000, 1.089000],
           [1.026719, 0.000000, -0.363000],
@@ -35,14 +33,13 @@ mol = Molecule(["C", "H", "H", "H", "H"], coords)
 
 
 class NwTaskTest(unittest.TestCase):
-
     def setUp(self):
         self.task = NwTask(0, 1, basis_set={"H": "6-31g"}, theory="dft",
                            theory_directives={"xc": "b3lyp"})
         self.task_cosmo = NwTask(0, 1, basis_set={"H": "6-31g"}, theory="dft",
-                           theory_directives={"xc": "b3lyp"},alternate_directives={'cosmo':"cosmo"})
-        self.task_esp = NwTask(0, 1, basis_set={"H": "6-31g"}, theory="esp",
-                           theory_directives={"restrain": "harmonic 0.001"})
+                                 theory_directives={"xc": "b3lyp"},
+                                 alternate_directives={'cosmo': "cosmo"})
+        self.task_esp = NwTask(0, 1, basis_set={"H": "6-31g"}, theory="esp")
 
 
     def test_multi_bset(self):
@@ -63,7 +60,6 @@ task dft optimize"""
         self.assertEqual(str(t), ans)
 
     def test_str_and_from_string(self):
-
         ans = """title "dft optimize"
 charge 0
 basis
@@ -103,7 +99,8 @@ task dft energy"""
 
     def test_dft_cosmo_task(self):
         task = NwTask.dft_task(mol, charge=mol.charge, operation="energy",
-                        xc="b3lyp", basis_set="6-311++G**",alternate_directives={'cosmo':"cosmo"})
+                               xc="b3lyp", basis_set="6-311++G**",
+                               alternate_directives={'cosmo': "cosmo"})
         ans = """title "H4C1 dft energy"
 charge 0
 basis
@@ -122,7 +119,7 @@ task dft energy"""
 
     def test_esp_task(self):
         task = NwTask.esp_task(mol, charge=mol.charge, operation="",
-                         basis_set="6-311++G**")
+                               basis_set="6-311++G**")
         ans = """title "H4C1 esp "
 charge 0
 basis
@@ -134,7 +131,6 @@ task esp"""
 
 
 class NwInputTest(unittest.TestCase):
-
     def setUp(self):
         tasks = [
             NwTask.dft_task(mol, operation="optimize", xc="b3lyp",
@@ -290,14 +286,32 @@ task dft energy
 
 
 class NwOutputTest(unittest.TestCase):
-
     def test_read(self):
         nwo = NwOutput(os.path.join(test_dir, "CH4.nwout"))
+        nwo_cosmo = NwOutput(os.path.join(test_dir, "N2O4.nwout"))
 
         self.assertEqual(0, nwo.data[0]["charge"])
         self.assertEqual(-1, nwo.data[-1]["charge"])
         self.assertAlmostEqual(-1102.622361621359, nwo.data[0]["energies"][-1])
         self.assertAlmostEqual(-1102.9985415777337, nwo.data[2]["energies"][-1])
+        self.assertAlmostEqual(-11156.353144819144,
+                               nwo_cosmo.data[5]["energies"][0])
+        self.assertAlmostEqual(-11153.37324779646,
+                               nwo_cosmo.data[5]["energies"][1])
+        self.assertAlmostEqual(-11156.353144818084,
+                               nwo_cosmo.data[5]["energies"][2])
+        self.assertAlmostEqual(-11168.818445621277,
+                               nwo_cosmo.data[6]["energies"][0])
+        self.assertAlmostEqual(-11166.361953878302,
+                               nwo_cosmo.data[6]["energies"][1])
+        self.assertAlmostEqual(-11168.818445621277,
+                               nwo_cosmo.data[6]["energies"][2])
+        self.assertAlmostEqual(-11165.227470577684,
+                               nwo_cosmo.data[7]["energies"][0])
+        self.assertAlmostEqual(-11165.02495508804,
+                               nwo_cosmo.data[7]["energies"][1])
+        self.assertAlmostEqual(-11165.227470576949,
+                               nwo_cosmo.data[7]["energies"][2])
         ie = (nwo.data[4]["energies"][-1] - nwo.data[2]["energies"][-1])
         ea = (nwo.data[2]["energies"][-1] - nwo.data[3]["energies"][-1])
         self.assertAlmostEqual(0.7575358046858582, ie)
