2d499c1404c7994856ea7664315f621eac91f423
William Richards <wmdrichards@gmail.com>
2013-09-30 17:43:01 -0400

    add test for correct POTCAR ordering

--- pymatgen/io/tests/test_vaspio_set.py
+++ pymatgen/io/tests/test_vaspio_set.py
@@ -55,8 +55,21 @@ class MITMPVaspInputSetTest(unittest.TestCase):
         
 
     def test_get_potcar_symbols(self):
-        syms = self.paramset.get_potcar_symbols(self.struct)
+        coords = list()
+        coords.append([0, 0, 0])
+        coords.append([0.75, 0.5, 0.75])
+        coords.append([0.75, 0.25, 0.75])
+        lattice = Lattice([[3.8401979337, 0.00, 0.00],
+                           [1.9200989668, 3.3257101909, 0.00],
+                           [0.00, -2.2171384943, 3.1355090603]])
+        struct = Structure(lattice, ["P", "Fe", "O"], coords)
+        
+        syms = self.paramset.get_potcar_symbols(struct)
         self.assertEquals(syms, ['Fe_pv', 'P', 'O'])
+        
+        syms = MPVaspInputSet(sort_structure=False).get_potcar_symbols(struct)
+        self.assertEquals(syms, ['P', 'Fe_pv', 'O'])
+        
 
     def test_get_incar(self):
         incar = self.paramset.get_incar(self.struct)
