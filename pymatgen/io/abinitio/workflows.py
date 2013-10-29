ba8d13c94d493cf1f7ed96cb925c73f1822957c4
gmatteo <gmatteo@gmail.com>
2013-10-29 21:19:11 +0100

    Force scheduler shutdown if the number of Task.Error is > MAX_NUM_ABIERRS
    Former-commit-id: 35b48406ce5fdec86add2267200867697c079537 [formerly d63273c3cb526c857d2384882af9a420132d1635]
    Former-commit-id: 72f77376c9a29f21393722b32a8557b814ec7328

--- pymatgen/io/abinitio/workflows.py
+++ pymatgen/io/abinitio/workflows.py
@@ -340,6 +340,7 @@ class Workflow(BaseWorkflow):
         """True if PAW calculation."""
         return all(task.ispaw for task in self)
 
+    @property
     def status_counter(self):
         """
         Returns a `Counter` object that counts the number of task with 
@@ -1067,9 +1068,8 @@ class BandStructureWorkflow(Workflow):
         # Register the NSCF run and its dependency.
         self.nscf_task = self.register(nscf_input, deps={self.scf_task: "DEN"}, task_class=NscfTask)
 
-        # Add DOS computation if requested.
+        # Add DOS computation(s) if requested.
         if dos_inputs is not None:
-
             if not isinstance(dos_inputs, (list, tuple)):
                 dos_inputs = [dos_inputs]
 
@@ -1301,13 +1301,13 @@ class G0W0_Workflow(Workflow):
         super(G0W0_Workflow, self).__init__(workdir=workdir, manager=manager)
 
         # Register the GS-SCF run.
-        self.scf_task = self.register(scf_input, task_class=ScfTask)
+        self.scf_task = scf_task = self.register(scf_input, task_class=ScfTask)
 
         # Construct the input for the NSCF run.
-        self.nscf_task = self.register(nscf_input, deps={self.scf_task: "DEN"}, task_class=NscfTask)
+        self.nscf_task = nscf_task = self.register(nscf_input, deps={scf_task: "DEN"}, task_class=NscfTask)
 
         # Register the SCREENING run.
-        self.scr_task = self.register(scr_input, deps={self.nscf_task: "WFK"})
+        self.scr_task = scr_task = self.register(scr_input, deps={nscf_task: "WFK"})
 
         # Register the SIGMA runs.
         if not isinstance(sigma_inputs, (list, tuple)): 
@@ -1315,7 +1315,7 @@ class G0W0_Workflow(Workflow):
 
         self.sigma_tasks = []
         for sigma_input in sigma_inputs:
-            self.sigma_tasks.append(self.register(sigma_input, deps={self.nscf_task: "WFK", self.scr_task: "SCR"}))
+            self.sigma_tasks.append(self.register(sigma_input, deps={nscf_task: "WFK", scr_task: "SCR"}))
 
 
 #class SCGW_Workflow(Workflow):
