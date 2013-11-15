c93076ef2a5d00d2425c8bcfcead92a6225aa59a
Xiaohui Qu <xqu@lbl.gov>
2013-11-15 15:22:04 -0800

    add test class for QcInput
    Former-commit-id: ec50d8c4930f55fde966ee39b2270e9480c3b011 [formerly ee03c4ff1e13b4059966887582700c5c4ed95fab]
    Former-commit-id: 3cd637827b8e9d62d4a2fe8c6a11dddc0c2e56db

---
+++ pymatgen/io/tests/test_qchemio.py
@@ -0,0 +1,24 @@
+import os
+from unittest import TestCase
+from pymatgen import Molecule
+from pymatgen.io.qchemio import QcInput
+
+__author__ = 'xiaohuiqu'
+
+
+test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..",
+                        'test_files', "molecules")
+
+
+coords = [[0.000000, 0.000000, 0.000000],
+          [0.000000, 0.000000, 1.089000],
+          [1.026719, 0.000000, -0.363000],
+          [-0.513360, -0.889165, -0.363000],
+          [-0.513360, 0.889165, -0.363000]]
+mol = Molecule(["C", "H", "H", "H", "H"], coords)
+
+
+class TestQcInput(TestCase):
+    def test_str(self):
+        qcinp = QcInput(mol, title="Test Methane", exchange="XYGJOS")
+        print qcinp
