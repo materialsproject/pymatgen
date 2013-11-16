06b5c0ee01fc1557bb50307dbbe65df675d2e8cc
Xiaohui Qu <xqu@lbl.gov>
2013-11-15 21:34:43 -0800

    add element specific basis set support

--- pymatgen/io/tests/test_qchemio.py
+++ pymatgen/io/tests/test_qchemio.py
@@ -15,12 +15,17 @@ coords = [[0.000000, 0.000000, 0.000000],
           [1.026719, 0.000000, -0.363000],
           [-0.513360, -0.889165, -0.363000],
           [-0.513360, 0.889165, -0.363000]]
-mol = Molecule(["C", "H", "H", "H", "H"], coords)
+mol = Molecule(["C", "H", "H", "H", "Cl"], coords)
 
 
 class TestQcInput(TestCase):
     def test_str(self):
-        qcinp = QcInput(mol, title="Test Methane", exchange="XYGJOS",
-                        job_type="Freq")
+        qcinp = QcInput(mol, title="Test Methane", exchange="xygjos",
+                        job_type="Freq",
+                        basis_set={"C": "6-31G*", "h": "6-31g*",
+                                   "CL":"6-31+g*"},
+                        aux_basis_set={"c": "rimp2-cc-pvdz",
+                                       "H": "rimp2-cc-pvdz",
+                                       "Cl": "rimp2-aug-cc-pvdz"})
         print qcinp
         print "#"*20
