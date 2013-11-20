0a4895ae34cc812741129480f385ce82b7007ce5
__sp__ <shyuep@gmail.com>
2013-11-19 18:33:30 -0800

    Add energy model module.

---
+++ pymatgen/analysis/tests/test_energy_models.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+"""
+TODO: Modify module doc.
+"""
+
+from __future__ import division
+
+__author__ = "Shyue Ping Ong"
+__copyright__ = "Copyright 2012, The Materials Project"
+__version__ = "0.1"
+__maintainer__ = "Shyue Ping Ong"
+__email__ = "shyuep@gmail.com"
+__date__ = "11/19/13"
+
+from pymatgen.analysis.energy_models import EwaldElectrostaticModel, \
+    SymmetryModel
+from pymatgen.core.lattice import Lattice
+from pymatgen.core.structure import Structure
+
+import os
+import unittest
+
+from pymatgen.io.smartio import read_structure
+
+test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..",
+                        'test_files')
+
+
+class EwaldElectrostaticModelTest(unittest.TestCase):
+
+    def test_get_energy(self):
+        coords = [[0, 0, 0], [0.75, 0.75, 0.75], [0.5, 0.5, 0.5],
+                  [0.25, 0.25, 0.25]]
+        lattice = Lattice([[3.0, 0.0, 0.0],
+                           [1.0, 3.0, 0.00],
+                           [0.00, -2.0, 3.0]])
+        s = Structure(lattice,
+                      [{"Si4+": 0.5, "O2-": 0.25, "P5+": 0.25},
+                       {"Si4+": 0.5, "O2-": 0.25, "P5+": 0.25},
+                       {"Si4+": 0.5, "O2-": 0.25, "P5+": 0.25},
+                       {"Si4+": 0.5, "O2-": 0.25, "P5+": 0.25}], coords)
+
+        m = EwaldElectrostaticModel()
+        self.assertAlmostEqual(m.get_energy(s), 44.1070954178)
+        s2 = read_structure(os.path.join(test_dir, "Li2O.cif"))
+        self.assertAlmostEqual(m.get_energy(s2), -36.3476248117)
+
+    def test_to_from_dict(self):
+        m = EwaldElectrostaticModel()
+        d = m.to_dict
+        self.assertIsInstance(EwaldElectrostaticModel.from_dict(d),
+                              EwaldElectrostaticModel)
+
+class SymmetryModelTest(unittest.TestCase):
+
+    def test_get_energy(self):
+        m = SymmetryModel()
+        s2 = read_structure(os.path.join(test_dir, "Li2O.cif"))
+        self.assertEqual(m.get_energy(s2), -225)
+
+    def test_to_from_dict(self):
+        m = SymmetryModel(symprec=0.2)
+        d = m.to_dict
+        o = SymmetryModel.from_dict(d)
+        self.assertIsInstance(o, SymmetryModel)
+        self.assertEqual(o.symprec, 0.2)
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
