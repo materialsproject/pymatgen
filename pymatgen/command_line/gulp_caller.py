3e6d3dd13a99711f1baaf545e1d9f2aff8c1c6c8
Bharat K. Medasani <mbkumar@gmail.com>
2013-12-12 10:11:50 -0800

    Camelcase for Tersoff_pot and BuckinghamPotLewis BuckinghamPotBush merged
    Former-commit-id: 7a67352383f93f5e05edd6b4dac74ce406ae5837 [formerly 96a840af7e81a7f8f1a20329611d12decbee1d72]
    Former-commit-id: 23b90ed4e1f596ac60f482a3949d5bbf441929b9

--- pymatgen/command_line/gulp_caller.py
+++ pymatgen/command_line/gulp_caller.py
@@ -285,8 +285,10 @@ class GulpIO:
             val_dict = dict(zip(el, valences))
 
         #Try bush library first
-        bpb = BuckinghamPotBush()
-        bpl = BuckinghamPotLewis()
+        #bpb = BuckinghamPotBush()
+        #bpl = BuckinghamPotLewis()
+        bpb = BuckinghamPotential('bush')
+        bpl = BuckinghamPotential('lewis')
         gin = ""
         for key in val_dict.keys():
             use_bush = True
@@ -376,7 +378,7 @@ class GulpIO:
             qerfstring += key + " " + key + " 0.6000 10.0000 \n"
 
         gin += "# noelectrostatics \n Morse \n"
-        met_oxi_ters = Tersoff_pot().data
+        met_oxi_ters = TersoffPot().data
         for key in el_val_dict.keys():
             if key != "O":
                 metal = key + "(" + str(int(el_val_dict[key])) + ")"
@@ -762,8 +764,82 @@ class BuckinghamPotBush(object):
             self.pot_dict = pot_dict
             self.spring_dict = spring_dict
 
+class BuckinghamPotential(object):
+    """
+        Generate the Buckingham Potential Table from the bush.lib
+        
+        Ref:
+        T.S.Bush, J.D.Gale, C.R.A.Catlow and P.D. Battle,  J. Mater Chem.,
+        4, 831-837 (1994).
+        """
+    
+    def __init__(self, bush_lewis_flag):
+        pot_file = "bush.lib" if bush_lewis_flag == "bush" else "lewis.lib"
+        with open(os.path.join(os.environ["GULP_LIB"], pot_file), 'rU') as f:
+            # In lewis.lib there is no shell for cation
+            species_dict, pot_dict, spring_dict = {}, {}, {}
+            sp_flg, pot_flg, spring_flg = False, False, False
+            for row in f:
+                if row[0] == "#":
+                    continue
+                if row.split()[0] == "species":
+                    sp_flg, pot_flg, spring_flg = True, False, False
+                    continue
+                if row.split()[0] == "buckingham":
+                    sp_flg, pot_flg, spring_flg = False, True, False
+                    continue
+                if row.split()[0] == "spring":
+                    sp_flg, pot_flg, spring_flg = False, False, True
+                    continue
+                
+                elmnt = row.split()[0]
+                if sp_flg:
+                    if bush_lewis_flag == "bush":
+                        if elmnt not in species_dict.keys():
+                            species_dict[elmnt] = {'inp_str': '', 'oxi': 0}
+                        species_dict[elmnt]['inp_str'] += row
+                        species_dict[elmnt]['oxi'] += float(row.split()[2])
+                    elif bush_lewis_flag == "lewis":
+                        if elmnt == "O":
+                            if row.split()[1] == "core":
+                                species_dict["O_core"] = row
+                            if row.split()[1] == "shel":
+                                species_dict["O_shel"] = row
+                        else:
+                            metal = elmnt.split('_')[0]
+                            #oxi_state = metaloxi.split('_')[1][0]
+                            species_dict[elmnt] = metal + " core " + \
+                                row.split()[2] + "\n"
+                    continue
+                
+                if pot_flg:
+                    if bush_lewis_flag == "bush":
+                        pot_dict[elmnt] = row
+                    elif bush_lewis_flag == "lewis":
+                        if elmnt == "O":
+                            pot_dict["O"] = row
+                        else:
+                            metal = elmnt.split('_')[0]
+                            #oxi_state = metaloxi.split('_')[1][0]
+                            pot_dict[elmnt] = metal + " " + " ".join(
+                            row.split()[1:]) + "\n"
+                    continue
+                
+                if spring_flg:
+                    spring_dict[elmnt] = row
+            
+            if bush_lewis_flag == "bush":
+                #Fill the null keys in spring dict with empty strings
+                for key in pot_dict.keys():
+                    if key not in spring_dict.keys():
+                        spring_dict[key] = ""
+            
+            self.species_dict = species_dict
+            self.pot_dict = pot_dict
+            self.spring_dict = spring_dict
 
-class Tersoff_pot(object):
+
+class TersoffPot(object):
     """
     Generate Tersoff Potential Table from "OxideTersoffPotentials" file
     """
