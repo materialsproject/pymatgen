ad0eab081951a35e06ba0793d4eafa0e4ef87c41
Xiaohui Qu <xqu@lbl.gov>
2014-01-23 14:34:23 -0800

    add function get_bonds

--- pymatgen/analysis/tests/test_molecule_structure_comparator.py
+++ pymatgen/analysis/tests/test_molecule_structure_comparator.py
@@ -1,5 +1,8 @@
 import os
 from unittest import TestCase
+from pymatgen.analysis.molecule_structure_comparator import \
+    MoleculeStructureComparator
+from pymatgen.io.smartio import read_mol
 
 __author__ = 'xiaohuiqu'
 
@@ -12,4 +15,13 @@ class TestMoleculeStructureComparator(TestCase):
         pass
 
     def test_get_bonds(self):
-        print "hello"
+        mol = read_mol(os.path.join(test_dir, "t1.xyz"))
+        msc = MoleculeStructureComparator()
+        # noinspection PyProtectedMember
+        bonds = msc._get_bonds(mol)
+        bonds_ref = [(0, 1), (0, 2), (0, 3), (0, 23), (3, 4), (3, 5), (5, 6),
+                     (5, 7), (7, 8), (7, 9), (7, 21), (9, 10), (9, 11),
+                     (9, 12), (12, 13), (12, 14), (12, 15), (15, 16), (15, 17),
+                     (15, 18), (18, 19), (18, 20), (18, 21), (21, 22),
+                     (21, 23), (23, 24), (23, 25)]
+        self.assertEqual(bonds, bonds_ref)
