728fbd6511e42db132c5da2ce6c5d9d0e7e1dea7
__sp__ <shyuep@gmail.com>
2014-02-14 00:11:38 -0800

    Add doctest to num_utils. Make chunks work with iterators.
    Former-commit-id: d3836a5e8fc9d82af8905a408c43c39c5ea0a6b4 [formerly 3be04f21493b1136ea9bb56c659efbd8253bacdd]
    Former-commit-id: cf6f65a28c7699a22bda3f140733b68d947dda34

--- pymatgen/util/num_utils.py
+++ pymatgen/util/num_utils.py
@@ -62,13 +62,15 @@ def chunks(items, n):
 
     >>> import pprint
     >>> pprint.pprint(list(chunks(range(1, 25), 10)))
-    [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-     [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-     [21, 22, 23, 24]]
+    [(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+     (11, 12, 13, 14, 15, 16, 17, 18, 19, 20),
+     (21, 22, 23, 24)]
     """
-    for i in range(0, len(items), n):
-        yield items[i:i+n]
-
+    it = iter(items)
+    chunk = tuple(itertools.islice(it, n))
+    while chunk:
+        yield chunk
+        chunk = tuple(itertools.islice(it, n))
 
 def min_max_indexes(seq):
     """
@@ -141,14 +143,11 @@ def iuptri(items, diago=True, with_inds=False):
     A generator that yields the upper triangle of the matrix (items x items)
 
     Args:
-        items:
-            Iterable object with elements [e0, e1, ...]
-        diago:
-            False if diagonal matrix elements should be excluded
-        with_inds:
-            If True, (i,j) (e_i, e_j) is returned else (e_i, e_j)
+        items: Iterable object with elements [e0, e1, ...]
+        diago: False if diagonal matrix elements should be excluded
+        with_inds: If True, (i,j) (e_i, e_j) is returned else (e_i, e_j)
 
-    >>> for (ij, mate) in iuptri([0,1], with_inds=True): 
+    >>> for (ij, mate) in iuptri([0,1], with_inds=True):
     ...     print("ij:", ij, "mate:", mate)
     ij: (0, 0) mate: (0, 0)
     ij: (0, 1) mate: (0, 1)
@@ -169,14 +168,11 @@ def ilotri(items, diago=True, with_inds=False):
     A generator that yields the lower triangle of the matrix (items x items)
 
     Args:
-        items:
-            Iterable object with elements [e0, e1, ...]
-        diago:
-            False if diagonal matrix elements should be excluded
-        with_inds:
-            If True, (i,j) (e_i, e_j) is returned else (e_i, e_j)
+        items: Iterable object with elements [e0, e1, ...]
+        diago: False if diagonal matrix elements should be excluded
+        with_inds: If True, (i,j) (e_i, e_j) is returned else (e_i, e_j)
 
-    >>> for (ij, mate) in ilotri([0,1], with_inds=True): 
+    >>> for (ij, mate) in ilotri([0,1], with_inds=True):
     ...     print("ij:", ij, "mate:", mate)
     ij: (0, 0) mate: (0, 0)
     ij: (1, 0) mate: (1, 0)
@@ -190,3 +186,8 @@ def ilotri(items, diago=True, with_inds=False):
                     yield (ii, jj), (item1, item2)
                 else:
                     yield item1, item2
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
