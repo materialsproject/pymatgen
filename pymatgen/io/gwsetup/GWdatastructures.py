9c247a22915355b31b30d63536e62d2a6cbaf672
setten <setten@gmail.com>
2014-02-18 13:55:14 +0100

    netcdf reading of abinit output
    Former-commit-id: 7b23219ef963c8d6df56d8f028378a9dcce5dd9b [formerly 7656457f57f75f978aefd270d79ea7ec10641318]
    Former-commit-id: f9af77d7a3b335ebf7b249b2e04f28f0a7d7d9fa

--- pymatgen/io/gwsetup/GWdatastructures.py
+++ pymatgen/io/gwsetup/GWdatastructures.py
@@ -18,6 +18,8 @@ __date__ = "Oct 23, 2013"
 import os
 import os.path
 
+from pymatgen.io.abinitio.netcdf import NetcdfReader
+
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -29,13 +31,19 @@ class GWConvergenceData():
         self.name = structure.composition.reduced_formula
 
     def read(self):
+        print 'read'
         if self.spec['code'] == 'ABINIT':
             read_next = True
             n = 3
+            print n, read_next
             while read_next:
-                output = os.path.join(self.name,  'work_0', 'task_' + str(n), 'run.abo')
+                output = os.path.join(self.name,  'work_0', 'task_' + str(n), 'outdata', 'out_SIGRES.nc')
+                print output
                 if os.path.isfile(output):
                     n += 1
+                    data = NetcdfReader(output)
+                    data.print_tree()
+                    data.close()
                 else:
                     read_next = False
 
