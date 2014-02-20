06636ae7cecc834b9692c28f4f80dcc6d5ff230b
__sp__ <shyuep@gmail.com>
2014-02-20 07:13:42 -0800

    Update dev docs
    Former-commit-id: d6878dc1843d83d91a13f356578ace9808cc01f7 [formerly 88bb67e1a9bd0927b9c4b1666900e1fbb14cef38]
    Former-commit-id: 16b6046b338157d1cd17ab83bf3fb1d19113e655

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ with open("README.rst") as f:
 setup(
     name="pymatgen",
     packages=find_packages(),
-    version="2.9.1",
+    version="2.9.2",
     install_requires=["numpy>=1.5", "pyhull>=1.4.3", "PyCifRW>=3.3",
                       "requests>=1.0", "pybtex>=0.16", "pyyaml>=3.0",
                       "monty>=0.1.5"],
