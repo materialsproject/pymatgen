79fb1352bc5c7edf8c8095023a754a691b40a409
setten <setten@gmail.com>
2014-03-10 12:25:28 +0100

    bug fixing
    Former-commit-id: 175fde6a1be328e85cbf6511ff1425d1eb9e6126 [formerly 56daf2dd5ef7dbf71ff3d64186e9f87aba2d9b1f]
    Former-commit-id: 843f0ab006887b24029afe3c6e8abbe7f8b8f81d

--- pymatgen/io/gwsetup/GWhelpers.py
+++ pymatgen/io/gwsetup/GWhelpers.py
@@ -58,7 +58,7 @@ def test_conv(xs, ys, tol=0.0001):
     y_value = None
     n_value = None
     if len(xs) > 1:
-        ds = get_derivatives(xs, ys)
+        ds = get_derivatives(xs[0:len(ys)], ys)
         for n in range(0, len(ds), 1):
             if abs(ds[n]) < tol:
                 conv = True
