c53ee69a075ea4edd6f589b4592ef1bac2029ce1
setten <setten@gmail.com>
2014-04-14 12:45:41 +0200

    scheduler errors
    Former-commit-id: c9d7839ce21237f1c09d97eb75e29f0d6d27eab1 [formerly 7d951c84b6a1e72215083afbba92f8491b2a0ac0]
    Former-commit-id: 94e2f9cd3654f1660083187cae318bf3368c153a

--- pymatgen/io/gwsetup/scheduler_error_handlers.py
+++ pymatgen/io/gwsetup/scheduler_error_handlers.py
@@ -12,19 +12,23 @@ __email__ = "mjvansetten@gmail.com"
 __date__ = "Mar 24, 2014"
 
 from pymatgen.io.gwsetup.scheduler_error_parsers import get_parser
-from pymatgen.io.gwsetup.scheduler_error_correcters import get_correcter
 from custodian.custodian import ErrorHandler
 
 
 class SchedulerErrorHandler(ErrorHandler):
     """
     Custodian error handler for scheduler related errors
-      scheduler takes the schduler, currently: slurm is supported
+      scheduler_adapter takes the scheduler, it should at least provide a .name attribute indentifying the scheduler,
+      currently 'slurm' is supported.
+      If the scheduler adapter also provides the methods defined in CorrectorProtocolScheduler, problems can also be
+      fixed by .apply_corrections.
+      If a application_adapter is also provided and it provides the methods defined in CorrectorProtocolApplication
+      problems can also be fixed a the level of the application, e.g. making the application require less memory.
     """
-    def __init__(self, scheduler, code, err_file='queue.err', out_file='queue.out', run_err_file='run.err',
-                 batch_err_file='batch.err'):
-        self.scheduler = scheduler
-        self.code = code
+    def __init__(self, scheduler_adapter, application_adapter=None, err_file='queue.err', out_file='queue.out',
+                 run_err_file='run.err', batch_err_file='batch.err'):
+        self.scheduler_adapter = scheduler_adapter
+        self.application_adapter = application_adapter
         self.err_file = err_file
         self.out_file = out_file
         self.run_err_file = run_err_file
@@ -33,7 +37,11 @@ class SchedulerErrorHandler(ErrorHandler):
         self.corrections = {}
 
     def check(self):
-        parser = get_parser(self.scheduler, err_file=self.err_file, out_file=self.out_file,
+        """
+        Check for the defined errors, put all found errors in self.errors, return True if any were found False if no
+        errors were found
+        """
+        parser = get_parser(self.scheduler_adapter.name, err_file=self.err_file, out_file=self.out_file,
                             run_err_file=self.run_err_file, batch_err_file=self.batch_err_file)
         parser.parse()
         self.errors = parser.errors
@@ -43,7 +51,30 @@ class SchedulerErrorHandler(ErrorHandler):
             return True
 
     def correct(self):
-        correcter = get_correcter(self.scheduler, self.code)
-        correcter.correct()
-        self.corrections = correcter.corrections
+        """
+        For custodian compatibility
+        """
+        self.return_corrections()
+
+    def return_corrections(self):
+
+        for error in self.errors:
+            self.corrections.update({error: {'scheduler_adapter_solutions': [], 'aplication_adapter_solutions': []}})
+            self.corrections[error]['scheduler_adapter_solutions'].append(error.scheduler_adapter_solutions)
+            self.corrections[error]['application_adapter_solutions'].append(error.application_adapter_solutions)
         return self.corrections
+
+    def apply_corrections(self):
+        """
+        Method to directly apply the corrections.
+        """
+        for error in self.errors:
+            for solution in error.scheduler_adapter_solutions:
+                if self.scheduler_adapter is not None:
+                    if self.scheduler_adapter.__getattribut__(solution[0].__name__)(solution[1]):
+                        return True
+            for solution in error.application_adapter_solutions:
+                if self.application_adapter is not None:
+                    if self.application_adapter.__getattribut__(solution[0].__name__)(solution[1]):
+                        return True
+        return False
