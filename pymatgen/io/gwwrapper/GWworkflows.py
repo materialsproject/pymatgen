a96727d1a8e7b5c74043a3e26d4831844ea79da5
setten <setten@gmail.com>
2014-04-17 12:01:59 +0200

    pulled an abstact abinitio spec from GW spec
    Former-commit-id: 5cb70911482e02b54444527faf5a64b0796ffa12 [formerly 458eb50297dcd89e744a31c060c0edfa6cf70351]
    Former-commit-id: 75cd8b0095f0e953bf34ff5c85ca46b0891af5f7

--- pymatgen/io/gwwrapper/GWworkflows.py
+++ pymatgen/io/gwwrapper/GWworkflows.py
@@ -118,7 +118,7 @@ class SingleAbinitGWWorkFlow():
         else:
             path_add = ''
         self.work_dir = self.structure.composition.reduced_formula+path_add
-        abi_pseudo = '.GGA_PBE-JTH-paw.xml'
+        abi_pseudo = os.environ['ABINIT_PS_EXT']
         abi_pseudo_dir = os.path.join(os.environ['ABINIT_PS'], 'GGA_PBE-JTH-paw')
         pseudos = []
         for element in self.structure.composition.element_composition:
