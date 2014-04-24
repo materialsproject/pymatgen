2c8e0fa1e34319556a0f7a1bd90b8966fba2d6d0
setten <setten@gmail.com>
2014-04-24 16:01:39 +0200

    error parsers
    Former-commit-id: 9051b552a608781bcd6738f31e84b2b0d8fd99a8 [formerly 0b5adc6de4caf6a0764ae1ad8781b0ca24e15bb4]
    Former-commit-id: adc670392f4d9ac489d08d2159f79f4f4783e683

--- pymatgen/io/gwwrapper/bandstructure_output_script.py
+++ pymatgen/io/gwwrapper/bandstructure_output_script.py
@@ -25,6 +25,9 @@ from pymatgen.matproj.rest import MPRester
 from pymatgen.io.vaspio.vasp_output import Vasprun
 from abipy.electrons.scissors import ScissorsBuilder
 
+import abipy.data as abidata
+from abipy.abilab import abiopen, ElectronBandsPlotter
+
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 """
@@ -48,6 +51,49 @@ except (IOError, OSError):
     print 'no vasp output found'
 
 try:
-    scissor = ScissorsBuilder.from_file('outdata/')
+    # Get the quasiparticle results from the SIGRES.nc database.
+    sigma_file = abiopen(abidata.ref_file("tgw1_9o_DS4_SIGRES.nc"))
+    #sigma_file.plot_qps_vs_e0()
+    qplist_spin = sigma_file.qplist_spin
+
+    print qplist_spin
+
+    # Construct the scissors operator
+    domains = [[-10, 6.1], [6.1, 18]]
+    scissors = qplist_spin[0].build_scissors(domains, bounds=None)
+
+    # Read the KS band energies computed on the k-path
+    ks_bands = abiopen(abidata.ref_file("si_nscf_GSR.nc")).ebands
+
+    # Read the KS band energies computed on the Monkhorst-Pack (MP) mesh
+    # and compute the DOS with the Gaussian method
+    ks_mpbands = abiopen(abidata.ref_file("si_scf_GSR.nc")).ebands
+    ks_dos = ks_mpbands.get_edos()
+
+    # Apply the scissors operator first on the KS band structure
+    # along the k-path then on the energies computed with the MP mesh.
+    qp_bands = ks_bands.apply_scissors(scissors)
+
+    qp_mpbands = ks_mpbands.apply_scissors(scissors)
+
+    # Compute the DOS with the modified QPState energies.
+    qp_dos = qp_mpbands.get_edos()
+
+    # Plot the LDA and the QPState band structure with matplotlib.
+    plotter = ElectronBandsPlotter()
+
+    plotter.add_ebands("LDA", ks_bands, dos=ks_dos)
+
+    plotter.add_ebands("LDA+scissors(e)", qp_bands, dos=qp_dos)
+
+    # Define the mapping reduced_coordinates -> name of the k-point.
+    klabels = {
+        (0.5,  0.0,  0.0): "L",
+        (0.0,  0.0,  0.0): "$\Gamma$",
+        (0.0,  0.5,  0.5): "X",
+    }
+
+    plotter.plot(title="Silicon band structure", klabels=klabels)
+
 except (IOError, OSError):
     print 'no abinit output found'
