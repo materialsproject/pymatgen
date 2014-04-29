61e16c1fef094c1ca364f09f0b28715b4c79bd83
Bruno Rousseau <rousseau.bruno@gmail.com>
2014-04-29 18:53:39 -0400

    Modified code that probes queue in SGE.
    This modification implements the correct way to
    apply qstat with SGE.

--- pymatgen/io/abinitio/qadapters.py
+++ pymatgen/io/abinitio/qadapters.py
@@ -710,7 +710,7 @@ class SGEAdapter(AbstractQueueAdapter):
             process = Popen(cmd, stdout=PIPE, stderr=PIPE)
             process.wait()
 
-            # grab the returncode. PBS returns 0 if the job was successful
+            # grab the returncode. SGE returns 0 if the job was successful
             if process.returncode == 0:
                 try:
                     # output should of the form 
@@ -741,13 +741,12 @@ class SGEAdapter(AbstractQueueAdapter):
             username = getpass.getuser()
 
         # run qstat
-        qstat = Command(['qstat', '-a', '-u', username])
+        qstat = Command(['qstat', '-u', username])
         process = qstat.run(timeout=5)
 
         # parse the result
         if process[0] == 0:
-            # lines should have this form
-            # '1339044.sdb          username  queuename    2012-02-29-16-43  20460   --   --    --  00:20 C 00:09'
+            # lines should contain username
             # count lines that include the username in it
 
             # TODO: only count running or queued jobs. or rather, *don't* count jobs that are 'C'.
