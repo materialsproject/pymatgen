842b6addd967fecdd542ad2770df5458ad11873c
__sp__ <shyuep@gmail.com>
2014-05-05 13:47:23 -0700

    REmove test files for spglib.

--- dependencies/spglib-1.6.0/test/extconf.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'mkmf'
-extension_name = 'getspg'
-dir_config(extension_name)
-
-if have_header('spglib.h') and have_library('symspg')
-	create_makefile(extension_name)
-end
