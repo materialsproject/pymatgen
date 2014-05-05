13ee7e9b9b80240043bfb24081eafaffd30cf56a
setten <setten@gmail.com>
2014-05-05 11:41:04 +0200

    test times

--- pymatgen/io/gwwrapper/tests/test_times.py
+++ pymatgen/io/gwwrapper/tests/test_times.py
@@ -3,12 +3,11 @@
 __author__ = 'setten'
 
 import timeit
-
-n = 10000
+import os
 
 
 def test_write():
-    """Stupid test function"""
+    """file write test function"""
     l = []
     for i in range(100000):
         l.append(i)
@@ -16,6 +15,23 @@ def test_write():
     f.write(str(l))
     f.close()
 
+
+def test_read():
+    f = open('test', mode='r')
+    line = f.read()
+    f.close()
+
+
+def test_folders():
+    for n in range(0, 100, 1):
+        os.mkdir('test_folder')
+        os.chdir('test_folder')
+        os.chdir('..')
+        os.rmdir('test_folder')
+
+
 if __name__ == '__main__':
     n = 100
-    print(timeit.timeit("test_write()", setup="from __main__ import test_write", number=n))
+    print 'write', timeit.timeit("test_write()", setup="from __main__ import test_write", number=n)
+    print 'read', timeit.timeit("test_read()", setup="from __main__ import test_read", number=n)
+    print 'folders', timeit.timeit("test_folders()", setup="from __main__ import test_folders", number=n)
