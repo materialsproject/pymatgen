0dd9eb96bedd5feaff6c233355077eb6bfc4c55d
setten <setten@gmail.com>
2014-05-05 14:01:54 +0200

    test times

--- pymatgen/io/gwwrapper/tests/test_times.py
+++ pymatgen/io/gwwrapper/tests/test_times.py
@@ -6,12 +6,12 @@ import timeit
 import os
 
 
-def test_write():
+def test_write(m):
     """
     file write test function
     """
     l = []
-    for i in range(100000):
+    for i in range(m):
         l.append(i)
     f = open('test', 'w')
     f.write(str(l))
@@ -55,21 +55,27 @@ if __name__ == '__main__':
 
     n = 100
 
-    test_write()
-    size = os.path.getsize('test')
-    print 'file size', size
-
-    write = timeit.timeit("test_write()", setup="from __main__ import test_write", number=n)
-    read = timeit.timeit("test_read()", setup="from __main__ import test_read", number=n*200)
     mk_folders = timeit.timeit("test_make_folders()", setup="from __main__ import test_make_folders", number=n)
     cd_folders = timeit.timeit("test_cd()", setup="from __main__ import test_cd", number=n)
 
-    print n, 'times written: ', write, ', ', size * n / write, 'b/s'
-    print n*200, 'times read:  ', read, ', ', size * 200 * n / write, 'b/s'
-    print 'made and removed 100 folders', mk_folders
-    print 'made, moved to, returned and removed 100 folders', cd_folders
+    print 'made and removed 100 folders in', mk_folders, 's'
+    print 'made, moved to, returned and removed 100 folders in', cd_folders, 's'
 
-    assert write < 5 * n / 100
-    assert read < 5 * n / 100
     assert mk_folders < 1 * n / 100
     assert cd_folders < 1 * n / 100
+
+    for my_m in [50000, 100000, 200000]:
+        test_write(my_m)
+        size = os.path.getsize('test')
+        print 'file size', size
+
+        setup = "from __main__ import test_write\nmy_m =" + str(my_m)
+
+        write = timeit.timeit("test_write(my_m)", setup=setup, number=n)
+        read = timeit.timeit("test_read()", setup="from __main__ import test_read", number=n*200)
+
+        print n, 'times written in ', write, 's, ', size * n / write / 1000000, 'MB/s'
+        print n*200, 'times read in  ', read, 's, ', size * 200 * n / write / 1000000, 'MB/s'
+
+        assert write < 10 * n / 100 * my_m / 100000
+        assert read < 10 * n / 100 * my_m / 100000
