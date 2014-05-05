822cb7b41e3ce9956913a200c3efc6dadc75a42e
setten <setten@gmail.com>
2014-05-05 12:10:04 +0200

    test times

--- pymatgen/io/gwwrapper/tests/test_times.py
+++ pymatgen/io/gwwrapper/tests/test_times.py
@@ -28,7 +28,6 @@ def test_make_folders():
         os.rmdir('test_folder_%s' % i)
 
 
-
 def test_cd():
     for i in range(0, 100, 1):
         os.mkdir('test_folder_%s' % i)
@@ -40,6 +39,11 @@ def test_cd():
 if __name__ == '__main__':
     n = 100
     print 'write', timeit.timeit("test_write()", setup="from __main__ import test_write", number=n)
-    print 'read', timeit.timeit("test_read()", setup="from __main__ import test_read", number=n)
+    print 'read', timeit.timeit("test_read()", setup="from __main__ import test_read", number=n*200)
     print 'mk folders', timeit.timeit("test_make_folders()", setup="from __main__ import test_make_folders", number=n)
     print 'cd folders', timeit.timeit("test_cd()", setup="from __main__ import test_cd", number=n)
+
+    assert timeit.timeit("test_write()", setup="from __main__ import test_write", number=n) < 5
+    assert timeit.timeit("test_read()", setup="from __main__ import test_read", number=n*200) < 5
+    assert timeit.timeit("test_make_folders()", setup="from __main__ import test_make_folders", number=n) < 1
+    assert timeit.timeit("test_cd()", setup="from __main__ import test_cd", number=n) < 1
