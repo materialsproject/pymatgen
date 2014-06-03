0b0f24589a6325b79c82f583230c5cfa0eaecb85
setten <setten@gmail.com>
2014-06-03 11:39:53 +0200

    developing convergence on ecut
    Former-commit-id: bce5fbdbf0d28fb01d738dfc8333fe674bb0c325 [formerly f903f14fffa1a453bf851e81c632730760931e44]
    Former-commit-id: b8a510ada4b8226b32c6df36d2735780eb608826

--- pymatgen/io/gwwrapper/datastructures.py
+++ pymatgen/io/gwwrapper/datastructures.py
@@ -569,7 +569,7 @@ class GWConvergenceData():
         self.conv_res['control'].update({'x_name': conv_data[0]})
         self.conv_res['values'].update({'x_name': conv_data[1]})
         self.conv_res['derivatives'].update({'x_name': conv_data[5]})
-        print conv_data
+        return conv_data
 
     def test_full_kp_results(self, tol_rel=0.5, tol_abs=0.001):
         """
