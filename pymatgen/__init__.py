41bcd1a2ef5c1c145466af7cf574dce8b8b1fb33
__sp__ <shyuep@gmail.com>
2014-06-05 16:10:01 -0700

    Update dev docs

--- pymatgen/__init__.py
+++ pymatgen/__init__.py
@@ -3,8 +3,8 @@ __author__ = ", ".join(["Shyue Ping Ong", "Anubhav Jain", "Geoffroy Hautier",
                         "Sai Jayaraman", "Michael Kocher", "Dan Gunter",
                         "Shreyas Cholia", "Vincent L Chevrier",
                         "Rickard Armiento"])
-__date__ = "May 22 2014"
-__version__ = "2.9.9"
+__date__ = "Jun 5 2014"
+__version__ = "2.9.10"
 
 #Useful aliases for commonly used objects and modules.
 
