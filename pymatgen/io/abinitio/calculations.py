76e98681fa30891f7edd640d6f6ce153245585f8
setten <setten@gmail.com>
2014-06-05 14:38:38 +0200

    bug fixing

--- pymatgen/io/abinitio/calculations.py
+++ pymatgen/io/abinitio/calculations.py
@@ -348,7 +348,7 @@ def g0w0_extended(structure, pseudos, scf_kppa, nscf_nband, ecuteps, ecutsigx, a
             to_add.update({k: values[-1]})
             for value in values:
                 print(value)
-                extra_abivars['ecut'] = [value]
+                extra_abivars[var] = value
                 extra_abivars['pawecutdg'] = extra_abivars['ecut'][0]*2
                 print(extra_abivars)
                 scf_strategy.append(ScfStrategy(structure, pseudos, scf_ksampling, accuracy=accuracy, spin_mode=spin_mode,
