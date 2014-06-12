071e95b909252f8875915401517dfe450090491b
setten <setten@gmail.com>
2014-06-12 15:18:42 +0200

    scf convergence studie
    Former-commit-id: f9f53322b991b64ba5326b90b837390acad75be3 [formerly a2c8452bb1527d4fa67f9634017e80ed410ef2e4]
    Former-commit-id: 83e89432043b8290f5f656ad535f07e2e8f5153c

--- pymatgen/io/gwwrapper/GWhelpers.py
+++ pymatgen/io/gwwrapper/GWhelpers.py
@@ -123,9 +123,10 @@ def exponential(x, a, b, n):
 
 
 def p0exp(xs, ys):
-    b0 = (1.5 ** -xs[-1] - 1.5 ** -xs[0]) / (ys[-1] - ys[0])
-    a0 = ys[0] - b0 * 1.5 ** -xs[0]
-    return [a0, b0, 1.5]
+    n0 = 1.1
+    b0 = (n0 ** -xs[-1] - n0 ** -xs[0]) / (ys[-1] - ys[0])
+    a0 = ys[0] - b0 * n0 ** -xs[0]
+    return [a0, b0, n0]
 
 
 def double_reciprocal(x, a, b, c):
