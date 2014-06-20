beb41d22ccd7288d951fe61f57256464c5fa869f
setten <setten@gmail.com>
2014-06-20 10:41:39 +0200

    convergence

--- pymatgen/io/gwwrapper/convergence.py
+++ pymatgen/io/gwwrapper/convergence.py
@@ -219,13 +219,13 @@ def print_plot_line(function, popt, xs, ys, name, extra=''):
     f.close()
     line = ''
     if function is exponential:
-        line = "plot %s + %s * %s ** -x, 'convdat.%s  pointsize 4 lt 0', %s" % (popt[0], popt[1], popt[2], idp, popt[0])
+        line = "plot %s + %s * %s ** -x, 'convdat.%s w pointsize 4 lt 0', %s" % (popt[0], popt[1], popt[2], idp, popt[0])
         #print 'plot ', popt[0], ' + ', popt[1], "* ", popt[2], " ** -x," "'"+'convdat.'+idp+"'"
     elif function is reciprocal:
-        line = "plot %s + %s / x**%s, 'convdat.%s pointsize 4 lt 0', %s" % (popt[0], popt[1], popt[2], idp, popt[0])
+        line = "plot %s + %s / x**%s, 'convdat.%s w pointsize 4 lt 0', %s" % (popt[0], popt[1], popt[2], idp, popt[0])
         #print 'plot ', popt[0], ' + ', popt[1], "/x**", popt[2], "," "'"+'convdat.'+idp+"'"
     elif function is single_reciprocal:
-        line = "plot %s + %s / (x - %s), 'convdat.%s pointsize 4 lt 0', %s" % (popt[0], popt[1], popt[2], idp, popt[0])
+        line = "plot %s + %s / (x - %s), 'convdat.%s w pointsize 4 lt 0', %s" % (popt[0], popt[1], popt[2], idp, popt[0])
         #print 'plot ', popt[0], ' + ', popt[1], "/ (x - ", popt[2], ")," "'"+'convdat.'+idp+"'"
     f = open('plot-fits', mode='a')
     f.write('pause -1 \n')
