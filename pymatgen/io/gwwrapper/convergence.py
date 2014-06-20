0d5aa773c57c7093e0ee4f192565252c0438e2c4
setten <setten@gmail.com>
2014-06-20 15:09:41 +0200

    convergence

--- pymatgen/io/gwwrapper/convergence.py
+++ pymatgen/io/gwwrapper/convergence.py
@@ -282,7 +282,9 @@ def get_weigts(xs, ys, mode=2):
             weights.append(abs((mind / d)))
     if mode == 2:
         maxxs = max(xs)**2
-        weights = xs**2 / maxxs
+        weights = []
+        for x in xs:
+            weights.append(x**2 / maxxs)
     else:
         weights = [1] * len(xs)
     print weights
