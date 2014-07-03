07f0c8c181d0eaf74c44e75703465bb505866107
__sp__ <shyuep@gmail.com>
2014-07-03 16:37:43 -0700

    IMprove super transformation. Remove ParallelCombinator.

--- pymatgen/transformations/tests/test_defect_transformations.py
+++ pymatgen/transformations/tests/test_defect_transformations.py
@@ -17,8 +17,8 @@ from pymatgen.core.lattice import Lattice
 from pymatgen.core.structure import Structure
 from pymatgen.transformations.defect_transformations import \
     VacancyTransformation, SubstitutionDefectTransformation, \
-    AntisiteDefectTransformation, InterstitialTransformation, \
-    ParallelCombinatorTransformation
+    AntisiteDefectTransformation, InterstitialTransformation
+
 try:
     import zeo
 except ImportError:
@@ -103,31 +103,6 @@ class InterstitialTransformationTest(unittest.TestCase):
             #print sc.composition.formula
             self.assertIn(sc.composition.formula,
                           ["Li16 O16", "Na1 Li16 O16", "Li16 Na1 O16"])
-
-
-class ParallelCombinatorTransformationTest(unittest.TestCase):
-
-    def test_apply_transformation(self):
-        t1 = VacancyTransformation([2,2,2])
-        t2 = AntisiteDefectTransformation([2,2,2])
-        t3 = SubstitutionDefectTransformation({"Li+":"Na+","O2-":"S2-"},[2,2,2])
-        t = ParallelCombinatorTransformation([t1,t2,t3])
-        coords = list()
-        coords.append([0, 0, 0])
-        coords.append([0.75, 0.75, 0.75])
-        coords.append([0.5, 0.5, 0.5])
-        coords.append([0.25, 0.25, 0.25])
-        lattice = Lattice([[3.8401979337, 0.00, 0.00],
-                           [1.9200989668, 3.3257101909, 0.00],
-                           [0.00, -2.2171384943, 3.1355090603]])
-        struct = Structure(lattice, ["Li+", "Li+", "O2-", "O2-"], coords)
-        scs = t.apply_transformation(struct)
-        self.assertEqual(len(scs),6)
-        for sc in scs:
-            pass
-            print sc.composition.formula
-            self.assertIn(sc.composition.formula, ["Li15 O16", "Li16 O15", 
-                "Li17 O15", "Li15 O17", "Na1 Li15 O16", "Li16 S1 O15"])
 
 
 if __name__ == '__main__':
