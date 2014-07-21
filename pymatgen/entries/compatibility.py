a44630befbad91e74cde0ecb5cce741348507a73
Sai Jayaraman <jayaraman.sai@gmail.com>
2014-07-21 17:33:13 -0400

    Bugfix in compatibility

--- pymatgen/entries/compatibility.py
+++ pymatgen/entries/compatibility.py
@@ -451,33 +451,6 @@ class MITCompatibility(Compatibility):
                    UCorrection(name, compat_type)])
 
 
-class MaterialsProjectCompatibility(Compatibility):
-    """
-    This class implements the GGA/GGA+U mixing scheme, which allows mixing of
-    entries. Note that this should only be used for VASP calculations using the
-    MaterialsProject parameters (see pymatgen.io.vaspio_set.MPVaspInputSet).
-    Using this compatibility scheme on runs with different parameters is not
-    valid.
-
-    Args:
-        compat_type: Two options, GGA or Advanced.  GGA means all GGA+U
-            entries are excluded.  Advanced means mixing scheme is
-            implemented to make entries compatible with each other,
-            but entries which are supposed to be done in GGA+U will have the
-            equivalent GGA entries excluded. For example, Fe oxides should
-            have a U value under the Advanced scheme. A GGA Fe oxide run
-            will therefore be excluded under the scheme.
-        correct_peroxide: Specify whether peroxide/superoxide/ozonide
-            corrections are to be applied or not.
-    """
-
-    def __init__(self, compat_type="Advanced", correct_peroxide=True):
-        name = "MP"
-        Compatibility.__init__(
-            self, [PotcarCorrection(name), GasCorrection(name, correct_peroxide=correct_peroxide),
-                   UCorrection(name, compat_type)])
-
-
 class MITAqueousCompatibility(Compatibility):
     """
     This class implements the GGA/GGA+U mixing scheme, which allows mixing of
