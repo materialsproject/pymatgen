80a71bd0c4e9c5b37cec1b6097980b3347ad0f19
__sp__ <shyuep@gmail.com>
2014-08-12 08:05:04 -0700

    Super speedup of VasprunET and vast code simplfication.
    Former-commit-id: d674161abcb32ac4c79a62f202aadbcc317d9d95 [formerly efa63cca3c058416c34629a30314d9e5dbe97815]
    Former-commit-id: 614b3a883445d79bc582435656339ca6e98541d9

--- pymatgen/io/vaspio/vasp_output.py
+++ pymatgen/io/vaspio/vasp_output.py
@@ -1156,33 +1156,20 @@ class VasprunET(object):
         with zopen(filename) as f:
             structures = []
             syms = []
-            text = []
-            read_atoms = False
-            read_structure = False
-            for event, elem in iterparse(f, events=("start", "end")):
-                if (not syms) and elem.tag == "array" and \
-                        elem.attrib.get("name") == "atoms":
-                    read_atoms = event == "start"
-                    if not read_atoms:
-                        text = [t for t in text if t]
-                        syms = map(str.strip, text[0::2])
-                        text = []
+            for event, elem in iterparse(f, events=("end", )):
+                if elem.tag == "array" and elem.attrib.get("name") == "atoms":
+                    for rc in elem.find("set"):
+                        syms.append(rc.find("c").text.strip())
+                    elem.clear()
                 elif elem.tag == "structure":
-                    read_structure = event == "start"
-                    if not read_structure:
-                        text = [t for t in text if t]
-                        data = []
-                        for l in text:
-                            data.append(map(float, l.split()))
-                        latt = data[0:3]
-                        pos = data[6:6+len(syms)]
-                        structures.append(Structure(latt, syms, pos))
-                        text = []
-                        elem.clear()
-                elif read_atoms and elem.tag == "c" and event == "end":
-                    text.append(elem.text)
-                elif read_structure and elem.tag == "v" and event == "end":
-                    text.append(elem.text)
+                    latt = []
+                    pos = []
+                    for v in elem.find("crystal").find("varray"):
+                        latt.append([float(i) for i in v.text.split()])
+                    for v in elem.find("varray"):
+                        pos.append([float(i) for i in v.text.split()])
+                    structures.append(Structure(latt, syms, pos))
+                    elem.clear()
         self.initial_structure = structures.pop(0)
         self.final_structure = structures[-1]
         self.structures = structures
