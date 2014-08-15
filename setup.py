012001c43f79bf65a0d0f37b8374f5c99012b813
__sp__ <shyuep@gmail.com>
2014-08-15 07:01:33 -0700

    Update dev docs

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ with open("README.rst") as f:
 setup(
     name="pymatgen",
     packages=find_packages(),
-    version="2.9.14",
+    version="2.10.0",
     install_requires=["numpy>=1.8", "pyhull>=1.4.3", "PyCifRW>=3.3",
                       "requests>=1.0", "pybtex>=0.16", "pyyaml>=3.0",
                       "monty>=0.3.1"],
