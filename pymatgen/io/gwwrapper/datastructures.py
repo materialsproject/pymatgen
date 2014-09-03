5e7add22386d626235c869a1fb2c43c25e1ca056
setten <setten@gmail.com>
2014-09-03 12:39:50 +0200

    hasing  data storage
    Former-commit-id: e4652e5e7b9ddba46cc7354a1cb456cf4295c429 [formerly eeabbd74b0b6b3261ec5fa61110541fd563870c6]
    Former-commit-id: 949ab363f2e49016991ae23e92a99106ce0e223c

--- pymatgen/io/gwwrapper/datastructures.py
+++ pymatgen/io/gwwrapper/datastructures.py
@@ -295,7 +295,7 @@ class GWSpecs(AbstractAbinitioSpec):
         tmp = copy.deepcopy(self.data)
         tmp['jobs'] = tuple(tmp['jobs'])
         print tmp
-        return hash(frozenset(self.data.items()))
+        return hash(frozenset(tmp.items()))
 
     def hash_str(self):
         """
