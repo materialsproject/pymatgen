73b642e532603d690e0c26a253f2297f5d4f027f
__sp__ <ongsp@eng.ucsd.edu>
2014-09-05 08:59:28 -0700

    Fix unittests.

--- pymatgen/matproj/tests/test_rest.py
+++ pymatgen/matproj/tests/test_rest.py
@@ -219,7 +219,7 @@ class MPResterTest(unittest.TestCase):
         #Let's test some invalid symbols
 
         self.assertRaises(KeyError, MPRester.parse_criteria, "li-fe")
-        self.assertRaises(ValueError, MPRester.parse_criteria, "LO2")
+        self.assertRaises(CompositionError, MPRester.parse_criteria, "lO2")
 
 
 if __name__ == "__main__":
