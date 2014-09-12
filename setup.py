3deffff6ee185620a25fc1f62cbe83e9405c47ae
__sp__ <ongsp@eng.ucsd.edu>
2014-09-12 12:15:51 -0700

    Update trove classifiers to not include Py3.2.

--- setup.py
+++ setup.py
@@ -88,7 +88,6 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Development Status :: 4 - Beta",
