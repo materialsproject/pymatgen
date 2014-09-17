b550256b58fc3fd6aeacf9d526e28815d84d50e1
gmatteo <gmatteo@gmail.com>
2014-09-17 21:14:11 +0200

    Use frozendict from monty

--- pymatgen/core/design_patterns.py
+++ pymatgen/core/design_patterns.py
@@ -61,19 +61,3 @@ class NullStream(object):
     """A fake stream with a no-op write.."""
     def write(*args):
         """no-op"""
-
-
-class FrozenDict(dict):
-    """A dictionary that does not permit to redefine its keys."""
-    def __init__(self, *args, **kwargs):
-        self.update(*args, **kwargs)
-
-    def __setitem__(self, key, val):
-        if key in self:
-            raise KeyError("Cannot overwrite existent key: %s" % str(key))
-
-        dict.__setitem__(self, key, val)
-
-    def update(self, *args, **kwargs):
-        for k, v in dict(*args, **kwargs).items():
-            self[k] = v
