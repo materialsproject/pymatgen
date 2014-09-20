ca4b9d63b1e1e5a54f1a89e4701465b1bba86233
__sp__ <ongsp@eng.ucsd.edu>
2014-09-19 22:06:43 -0700

    More py3k fixes
    Former-commit-id: 9bd3f51f5880895f43e0efac65bc9b03f6f8dd56 [formerly b5afe2e6801f4eeb287d8503b3a9b89b09aaf081]
    Former-commit-id: 7b5c8532ed7e8b623a04a778f08b7b3c2603b902

--- pymatgen/core/composition.py
+++ pymatgen/core/composition.py
@@ -792,7 +792,7 @@ class ChemicalPotential(dict, PMGSONable):
 
     def __sub__(self, other):
         if isinstance(other, ChemicalPotential):
-            els = set(self.keys() + other.keys())
+            els = set(list(self.keys()) + list(other.keys()))
             return ChemicalPotential({e: self.get(e, 0) - other.get(e, 0)
                                       for e in els})
         else:
