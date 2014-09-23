02cfba9cca47e5fb48dd6423fc8d5dbda97a7eac
__sp__ <ongsp@eng.ucsd.edu>
2014-09-22 22:10:46 -0700

    Allow functionality to allow PymatgenTest to quickly load structures for unittesting.
    Former-commit-id: 954ede0e328691f2bcc4def909220758503db219 [formerly 70f3441fcf22c591033637c15e44a05ddf18d245]
    Former-commit-id: 6f38dc6215b3135dd537c52ce878d71945649103

--- pymatgen/io/tests/test_smartio.py
+++ pymatgen/io/tests/test_smartio.py
@@ -33,7 +33,7 @@ class MethodsTest(unittest.TestCase):
     def test_read_structure(self):
         test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..",
                                 'test_files')
-        for fname in ("Li2O.cif", "Li2O2.json", "vasprun.xml",
+        for fname in ("Li2O.cif", "vasprun.xml",
                       "vasprun_Si_bands.xml", "Si.cssr"):
             filename = os.path.join(test_dir, fname)
             struct = read_structure(filename)
