400ebe8f6597e4780fc889e12cf8a4c18b2a4bfe
__sp__ <ongsp@eng.ucsd.edu>
2014-09-25 10:21:54 -0700

    Some spelling and grammar.

--- pymatgen/dao.py
+++ pymatgen/dao.py
@@ -5,11 +5,11 @@ print("""
 The 道 of Pymatgen
 
 1. Great code enables great materials science.
-2. Comprehensive tests ensures robustness.
+2. Comprehensive tests ensure robustness.
 3. Clear documentation leads to more usage.
-4. More usage improves code quality (and citations)!
+4. More usage improves code quality (and increases citations).
 5. Even complex scientific ideas can be broken down into simple interfaces.
-6. Though deep understanding is often necessary to develop the right approach.
+6. Though deep (Hulk-level) understanding is necessary to develop the right interface design.
 7. Slow and accurate is better than fast and wrong.
 8. But efficiency matters for core classes.
 
