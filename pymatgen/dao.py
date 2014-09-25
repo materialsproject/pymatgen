608303059b7ec657d37f9eebc59bcea98ffaf69f
__sp__ <ongsp@eng.ucsd.edu>
2014-09-25 10:03:43 -0700

    The 道 of Pymatgen. WIP.
    Former-commit-id: d969ca613aa1870096257b1ade454e01378042da [formerly c3c6b5aa2be00753c1a8763cb37f918c96354edb]
    Former-commit-id: ac0fe0071a8d7b2fa60527d4acb3835aec3aecae

---
+++ pymatgen/dao.py
@@ -0,0 +1,18 @@
+#coding: utf-8
+
+
+print("""
+The 道 of Pymatgen
+
+1. Great code enables great materials science.
+2. Comprehensive tests ensures robustness.
+3. Clear documentation leads to more usage.
+4. More usage improves code quality (and citations)!
+5. Even complex scientific ideas can be broken down into simple interfaces.
+6. Though deep understanding is often necessary to develop the right approach.
+7. Slow and accurate is better than fast and wrong.
+8. But efficiency matters for core classes.
+
+---
+First Coder
+""")
