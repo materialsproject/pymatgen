dbb48c4cb908cf2ed20c4098bc8f7f87551d3951
__sp__ <ongsp@eng.ucsd.edu>
2014-09-28 10:37:30 -0700

    More assertEqual fixes.
    Former-commit-id: 3a0bb101aa25f3bf023fe70618fbe0742961eda3 [formerly a335f8f28b76c4794447c374e89fdc6782868ed1]
    Former-commit-id: a96381009d8e32e155b1f311fb53f979a301e7fd

--- pymatgen/electronic_structure/tests/test_core.py
+++ pymatgen/electronic_structure/tests/test_core.py
@@ -10,16 +10,16 @@ from pymatgen.electronic_structure.core import Orbital, Spin
 class SpinTest(unittest.TestCase):
 
     def test_init(self):
-        self.assertEquals(int(Spin.up), 1)
-        self.assertEquals(int(Spin.down), -1)
+        self.assertEqual(int(Spin.up), 1)
+        self.assertEqual(int(Spin.down), -1)
 
     def test_from_int(self):
-        self.assertEquals(Spin.from_int(1), Spin.up)
-        self.assertEquals(Spin.from_int(-1), Spin.down)
+        self.assertEqual(Spin.from_int(1), Spin.up)
+        self.assertEqual(Spin.from_int(-1), Spin.down)
         self.assertRaises(ValueError, Spin.from_int, 0)
 
     def test_cached(self):
-        self.assertEquals(id(Spin.from_int(1)), id(Spin.up))
+        self.assertEqual(id(Spin.from_int(1)), id(Spin.up))
 
 
 class OrbitalTest(unittest.TestCase):
@@ -30,7 +30,7 @@ class OrbitalTest(unittest.TestCase):
         self.assertRaises(IndexError, Orbital.from_vasp_index, 100)
 
     def test_cached(self):
-        self.assertEquals(id(Orbital.from_vasp_index(0)), id(Orbital.s))
+        self.assertEqual(id(Orbital.from_vasp_index(0)), id(Orbital.s))
 
 if __name__ == '__main__':
     unittest.main()
