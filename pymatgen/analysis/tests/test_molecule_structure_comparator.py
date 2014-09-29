76b3ce9721914b31ad9bad62f1f444f45acd3ba6
__sp__ <ongsp@eng.ucsd.edu>
2014-09-29 12:43:19 -0700

    Fix read_mol deprecation.

--- pymatgen/analysis/tests/test_molecule_structure_comparator.py
+++ pymatgen/analysis/tests/test_molecule_structure_comparator.py
@@ -6,7 +6,7 @@ import os
 from unittest import TestCase
 from pymatgen.analysis.molecule_structure_comparator import \
     MoleculeStructureComparator
-from pymatgen.io.smartio import read_mol
+from pymatgen.core.structure import Molecule
 
 __author__ = 'xiaohuiqu'
 
@@ -18,20 +18,20 @@ test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..",
 class TestMoleculeStructureComparator(TestCase):
     def test_are_equal(self):
         msc1 = MoleculeStructureComparator()
-        mol1 = read_mol(os.path.join(test_dir, "t1.xyz"))
-        mol2 = read_mol(os.path.join(test_dir, "t2.xyz"))
-        mol3 = read_mol(os.path.join(test_dir, "t3.xyz"))
+        mol1 = Molecule.from_file(os.path.join(test_dir, "t1.xyz"))
+        mol2 = Molecule.from_file(os.path.join(test_dir, "t2.xyz"))
+        mol3 = Molecule.from_file(os.path.join(test_dir, "t3.xyz"))
         self.assertFalse(msc1.are_equal(mol1, mol2))
         self.assertTrue(msc1.are_equal(mol2, mol3))
-        thio1 = read_mol(os.path.join(test_dir, "thiophene1.xyz"))
-        thio2 = read_mol(os.path.join(test_dir, "thiophene2.xyz"))
+        thio1 = Molecule.from_file(os.path.join(test_dir, "thiophene1.xyz"))
+        thio2 = Molecule.from_file(os.path.join(test_dir, "thiophene2.xyz"))
         # noinspection PyProtectedMember
         msc2 = MoleculeStructureComparator(
             priority_bonds=msc1._get_bonds(thio1))
         self.assertTrue(msc2.are_equal(thio1, thio2))
 
     def test_get_bonds(self):
-        mol1 = read_mol(os.path.join(test_dir, "t1.xyz"))
+        mol1 = Molecule.from_file(os.path.join(test_dir, "t1.xyz"))
         msc = MoleculeStructureComparator()
         # noinspection PyProtectedMember
         bonds = msc._get_bonds(mol1)
@@ -41,7 +41,7 @@ class TestMoleculeStructureComparator(TestCase):
                      (15, 18), (18, 19), (18, 20), (18, 21), (21, 22),
                      (21, 23), (23, 24), (23, 25)]
         self.assertEqual(bonds, bonds_ref)
-        mol2 = read_mol(os.path.join(test_dir, "MgBH42.xyz"))
+        mol2 = Molecule.from_file(os.path.join(test_dir, "MgBH42.xyz"))
         bonds = msc._get_bonds(mol2)
         self.assertEqual(bonds, [(1, 3), (2, 3), (3, 4), (3, 5), (6, 8), (7, 8),
                                  (8, 9), (8, 10)])
@@ -56,7 +56,7 @@ class TestMoleculeStructureComparator(TestCase):
         d1 = msc1.as_dict()
         d2 = MoleculeStructureComparator.from_dict(d1).as_dict()
         self.assertEqual(d1, d2)
-        thio1 = read_mol(os.path.join(test_dir, "thiophene1.xyz"))
+        thio1 = Molecule.from_file(os.path.join(test_dir, "thiophene1.xyz"))
         # noinspection PyProtectedMember
         msc2 = MoleculeStructureComparator(
             bond_length_cap=0.2,
