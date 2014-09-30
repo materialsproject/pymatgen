68e841fcdd339b1326f7988f5e5b8198127f77a4
__sp__ <ongsp@eng.ucsd.edu>
2014-09-30 10:43:33 -0700

    More refactoring.
    Former-commit-id: 658ce65e9c506be5db0d1930d4370447c6cfd7dc [formerly 76d1070567a6b263579e988e80d14780dec8cc6d]
    Former-commit-id: 8ce1464f0d674a197734fe3f979813d0b7fb6e0e

--- pymatgen/symmetry/spacegroup.py
+++ pymatgen/symmetry/spacegroup.py
@@ -14,62 +14,10 @@ __maintainer__ = "Shyue Ping Ong"
 __email__ = "shyuep@gmail.com"
 __date__ = "Mar 9, 2012"
 
-from pymatgen.core.sites import PeriodicSite
+from pymatgen.symmetry.analyzer import SpacegroupOperations
+from monty.dev import deprecated
 
 
-class Spacegroup(object):
-    """
-    Represents a space group, which is a collection of symmetry operations.
-
-    Args:
-        int_symbol (str): International symbol of the spacegroup.
-        int_number (int): International number of the spacegroup.
-        symmops ([SymmOp]): Symmetry operations associated with the
-            spacegroup.
-    """
-
-    def __init__(self, int_symbol, int_number, symmops):
-        self.int_symbol = int_symbol
-        self.int_number = int_number
-        self.symmops = symmops
-
-    def are_symmetrically_equivalent(self, sites1, sites2, symm_prec=1e-3):
-        """
-        Given two sets of PeriodicSites, test if they are actually
-        symmetrically equivalent under this space group.  Useful, for example,
-        if you want to test if selecting atoms 1 and 2 out of a set of 4 atoms
-        are symmetrically the same as selecting atoms 3 and 4, etc.
-
-        One use is in PartialRemoveSpecie transformation to return only
-        symmetrically distinct arrangements of atoms.
-
-        Args:
-            sites1 ([Site]): 1st set of sites
-            sites2 ([Site]): 2nd set of sites
-            symm_prec (float): Tolerance in atomic distance to test if atoms
-                are symmetrically similar.
-
-        Returns:
-            (bool): Whether the two sets of sites are symmetrically
-            equivalent.
-        """
-        def in_sites(site):
-            for test_site in sites1:
-                if test_site.is_periodic_image(site, symm_prec, False):
-                    return True
-            return False
-        for op in self.symmops:
-            newsites2 = [PeriodicSite(site.species_and_occu,
-                                      op.operate(site.frac_coords),
-                                      site.lattice) for site in sites2]
-            ismapping = True
-            for site in newsites2:
-                if not in_sites(site):
-                    ismapping = False
-                    break
-            if ismapping:
-                return True
-        return False
-
-    def __str__(self):
-        return "{} ({}) spacegroup".format(self.int_symbol, self.int_number)
+@deprecated(replacement=SpacegroupOperations)
+class Spacegroup(SpacegroupOperations):
+    pass
