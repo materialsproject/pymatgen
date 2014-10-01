c6cbd6e5ca53aabd8a0fdd4a619bbdd465fdecc1
__sp__ <ongsp@eng.ucsd.edu>
2014-09-30 21:23:21 -0700

    Implement lattice-spacegroup compatibility checking.

--- pymatgen/symmetry/tests/test_groups.py
+++ pymatgen/symmetry/tests/test_groups.py
@@ -16,6 +16,7 @@ __date__ = "4/10/14"
 import unittest
 import numpy as np
 
+from pymatgen.core.lattice import Lattice
 from pymatgen.symmetry.groups import PointGroup, SpaceGroup
 
 
@@ -26,6 +27,12 @@ class PointGroupTest(unittest.TestCase):
         for k, v in order.items():
             pg = PointGroup(k)
             self.assertEqual(order[k], len(pg.symmetry_ops))
+
+    def test_get_orbit(self):
+        pg = PointGroup("mmm")
+        self.assertEqual(len(pg.get_orbit([0.1, 0.1, 0.1])), 8)
+        self.assertEqual(len(pg.get_orbit([0, 0, 0.1])), 2)
+        self.assertEqual(len(pg.get_orbit([1.2, 1.2, 1])), 8)
 
 
 class SpaceGroupTest(unittest.TestCase):
@@ -46,6 +53,41 @@ class SpaceGroupTest(unittest.TestCase):
         p = np.random.random_integers(0, 100, size=(3,))
         p /= 100
         self.assertLessEqual(len(sg.get_orbit(p)), sg.order)
+
+    def test_is_compatible(self):
+        cubic = Lattice.cubic(1)
+        hexagonal = Lattice.hexagonal(1, 2)
+        rhom = Lattice.rhombohedral(3, 80)
+        tet = Lattice.tetragonal(1, 2)
+        ortho = Lattice.orthorhombic(1, 2, 3)
+        sg = SpaceGroup("Fm-3m")
+        self.assertTrue(sg.is_compatible(cubic))
+        self.assertFalse(sg.is_compatible(hexagonal))
+        sg = SpaceGroup("R-3mH")
+        self.assertFalse(sg.is_compatible(cubic))
+        self.assertTrue(sg.is_compatible(hexagonal))
+        sg = SpaceGroup("R-3m")
+        self.assertTrue(sg.is_compatible(cubic))
+        self.assertTrue(sg.is_compatible(rhom))
+        self.assertFalse(sg.is_compatible(hexagonal))
+        sg = SpaceGroup("Pnma")
+        self.assertTrue(sg.is_compatible(cubic))
+        self.assertTrue(sg.is_compatible(tet))
+        self.assertTrue(sg.is_compatible(ortho))
+        self.assertFalse(sg.is_compatible(rhom))
+        self.assertFalse(sg.is_compatible(hexagonal))
+        sg = SpaceGroup("P12/c1")
+        self.assertTrue(sg.is_compatible(cubic))
+        self.assertTrue(sg.is_compatible(tet))
+        self.assertTrue(sg.is_compatible(ortho))
+        self.assertFalse(sg.is_compatible(rhom))
+        self.assertFalse(sg.is_compatible(hexagonal))
+        sg = SpaceGroup("P-1")
+        self.assertTrue(sg.is_compatible(cubic))
+        self.assertTrue(sg.is_compatible(tet))
+        self.assertTrue(sg.is_compatible(ortho))
+        self.assertTrue(sg.is_compatible(rhom))
+        self.assertTrue(sg.is_compatible(hexagonal))
 
 if __name__ == '__main__':
     unittest.main()
