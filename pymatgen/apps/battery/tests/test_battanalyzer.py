8ec4c7f492ac8b07af4ffc9a2ced5893485c8750
Anubhav Jain <ajain@lbl.gov>
2014-09-30 17:41:21 -0700

    first commits of old unit tests, req structure files missing
    Former-commit-id: 194df2958f1448eec47b32033349c6ae810956d8 [formerly c43dfca45458d253e5d6840460e3117e745daabf]
    Former-commit-id: 5c67bf9c8b0d3592f097a64d46d6106ccd5b9e42

---
+++ pymatgen/apps/battery/tests/test_battanalyzer.py
@@ -0,0 +1,78 @@
+import unittest
+import os
+from pymatgen.apps.battery.battanalyzer import BatteryAnalyzer
+
+from pymatgen.io.cifio import CifParser
+import pymatgen
+
+module_dir = os.path.join(os.path.dirname(pymatgen.__file__), '..', 'test_files')
+
+"""
+class BatteryAnalyzerTest(unittest.TestCase):
+
+    def load_from_cif(self, filename, oxidations, cation="Li"):
+        s = CifParser(os.path.join(module_dir, filename)).get_structures()[0]
+        s.add_oxidation_state_by_element(oxidations)
+        return BatteryAnalyzer(s, cation)
+
+    def setUp(self):
+        self.lifepo4 = self.load_from_cif("LiFePO4.cif", {'Li': 1, 'Fe': 2, 'P': 5, 'O': -2})
+        self.nafepo4 = self.load_from_cif("NaFePO4.cif", {'Ba': 1, 'Fe': 2, 'P': 5, 'O': -2}, cation='Na')
+        self.fepo4 = self.load_from_cif("FePO4a.cif", {'Fe': 3, 'P': 5, 'O': -2})
+        self.lifemnpo4 = self.load_from_cif("Li4Fe3Mn1(PO4)4.cif", {'Li': 1, 'Fe': 2, 'Mn': 2, 'P': 5, 'O': -2})
+        self.li8nicofe208 = self.load_from_cif("Li8Fe2NiCoO8.cif", {'Li': 1, 'Fe': 2, 'Mn': 2, 'Co': 2, 'Ni': 2, 'O': -2})
+        self.li3v2p3o12 = self.load_from_cif("Li3V2(PO4)3.cif", {'Li': 1, 'V': 3, 'O': -2, 'P': 5})
+
+    def test_capacitygrav_calculations(self):
+
+        lifepo4_cap = 169.890533222 # same as fepo4 cap
+        nafepo4_cap = 154.203319253
+        li3v2p3o12_cap_remove = 197.253397983
+        li3v2p3o12_cap_insert = 127.17129162620655
+
+        self.assertAlmostEqual(self.lifepo4.get_max_capgrav(), lifepo4_cap, 3)
+        self.assertEqual(self.lifepo4.get_max_capgrav(remove = False), 0)
+        self.assertAlmostEqual(self.lifepo4.get_max_capgrav(insert = False), lifepo4_cap, 3)
+
+        self.assertAlmostEqual(self.nafepo4.get_max_capgrav(), nafepo4_cap, 3)
+        self.assertEqual(self.nafepo4.get_max_capgrav(remove = False), 0)
+
+        self.assertAlmostEqual(self.fepo4.get_max_capgrav(), lifepo4_cap, 3)
+        self.assertEqual(self.fepo4.get_max_capgrav(insert = False), 0)
+
+        self.assertAlmostEqual(self.li3v2p3o12.get_max_capgrav(insert=False), li3v2p3o12_cap_remove, 3)
+        self.assertAlmostEqual(self.li3v2p3o12.get_max_capgrav(remove=False), li3v2p3o12_cap_insert, 3)
+
+    def test_capacityvol_calculations(self):
+
+        lifepo4_cap = 594.175180631
+        nafepo4_cap = 542.861040835
+        fepo4_cap = 624.822898302 #this is different than lifepo4 cap if lifepo4 volume not known
+
+        self.assertAlmostEqual(self.lifepo4.get_max_capvol(), lifepo4_cap, 3)
+        self.assertEqual(self.lifepo4.get_max_capvol(remove = False), 0)
+        self.assertAlmostEqual(self.lifepo4.get_max_capvol(insert = False), lifepo4_cap, 3)
+
+        self.assertAlmostEqual(self.nafepo4.get_max_capvol(), nafepo4_cap, 3)
+        self.assertEqual(self.nafepo4.get_max_capvol(remove = False), 0)
+        self.assertAlmostEqual(self.nafepo4.get_max_capvol(insert = False), nafepo4_cap, 3)
+
+        self.assertAlmostEqual(self.fepo4.get_max_capvol(), fepo4_cap, 3)
+        self.assertAlmostEqual(self.fepo4.get_max_capvol(remove = False), fepo4_cap, 3)
+        self.assertEqual(self.fepo4.get_max_capvol(insert = False), 0)
+
+        #give the lifepo4 volume, should get lifepo4 capacity
+        self.assertAlmostEqual(self.fepo4.get_max_capvol(volume = self.lifepo4.struc_oxid.volume), lifepo4_cap, 3)
+
+    def test_delithiation(self):
+
+        self.assertEquals(self.lifemnpo4.get_delith_int_oxid(), set([1.0, 2.0, 3.0, 4.0]))
+
+        self.assertEquals(self.li8nicofe208.get_delith_int_oxid(), set([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]))
+
+        self.assertEqual(self.li3v2p3o12.get_delith_int_oxid(), set([4.0, 6.0]))
+
+
+if __name__ == '__main__':
+    unittest.main()
+"""
