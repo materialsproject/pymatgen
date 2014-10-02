8fe2c8521e34edbfcbf08e9c05222fb45a5d2655
gmatteo <gmatteo@gmail.com>
2014-10-02 15:16:38 +0200

    Skipif if os.environ['ABINIT_PS_EXT'] raises KeyError

--- pymatgen/io/gwwrapper/GWtasks.py
+++ pymatgen/io/gwwrapper/GWtasks.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from __future__ import division, unicode_literals
+from __future__ import division, unicode_literals, print_function
 
 """
 GW tasks for fireworks workflows, currently Vasp only
