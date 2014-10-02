fd950b35dee80ddcb9df79241ac2d73f112a6a33
setten <setten@gmail.com>
2014-10-02 17:55:47 +0200

    yet more unit tests ...
    Former-commit-id: a71e98620470daa52b172082b066da8f712fcd98 [formerly 0eaced837c79db7e7684eac0366c603ff6eb87b6]
    Former-commit-id: 86a51170b1141c9a91a803284874befd6134c080

--- pymatgen/io/gwwrapper/tests/test_classes.py
+++ pymatgen/io/gwwrapper/tests/test_classes.py
@@ -8,7 +8,8 @@ from pymatgen.core.structure import Structure
 from pymatgen.matproj.rest import MPRester, MPRestError
 from pymatgen.io.gwwrapper.datastructures import GWSpecs, GWConvergenceData, get_spec
 from pymatgen.io.gwwrapper.codeinterfaces import AbinitInterface, AbstractCodeInterface, VaspInterface, get_code_interface
-
+from pymatgen.io.gwwrapper.GWvaspinputsets import GWDFTDiagVaspInputSet, GWG0W0VaspInputSet, GWscDFTPrepVaspInputSet
+from pymatgen.io.gwwrapper.GWvaspinputsets import SingleVaspGWWork
 from pymatgen.io.gwwrapper.tests.test_helpers import structure
 
 #test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..", "..",'test_files')
@@ -61,3 +62,37 @@ class GWTestCodeInterfaces(PymatgenTest):
         self.assertEqual(len(interface.supported_methods), 2)
 
 
+class GWVaspInputSetTests(PymatgenTest):
+
+    def setUp(self):
+        self.structure = structure
+        self.spec = GWSpecs()
+        self.spec.data['code'] = 'VASP'
+        self.spec.update_code_interface()
+
+    def test_GWscDFTPrepVaspInputSet(self):
+        inpset = GWscDFTPrepVaspInputSet(structure=self.structure, spec=self.spec)
+        self.assertIsInstance(inpset, GWscDFTPrepVaspInputSet)
+        self.assertEqual(inpset.convs, {})
+
+    def test_GWDFTDiagVaspInputSet(self):
+        inpset = GWDFTDiagVaspInputSet(structure=self.structure, spec=self.spec)
+        self.assertIsInstance(inpset, GWDFTDiagVaspInputSet)
+        self.assertEqual(inpset.convs,
+                         {u'NBANDS': {u'test_range': (10, 20, 30, 40, 50, 60, 70), u'control': u'gap', u'method': u'set_nbands'}})
+        self.assertEqual(inpset.incar_settings, {u'ALGO': u'Exact', u'EDIFF': 1e-10, u'IBRION': -1, u'ICHARG': 1,
+                                                 u'ISMEAR': -5, u'ISPIN': 1, u'LOPTICS': u'TRUE', u'LORBIT': 11,
+                                                 u'LREAL': u'AUTO', u'LWAVE': True, u'MAGMOM': {u'Co': 5, u'Cr': 5,
+                                                 u'Fe': 5, u'Mn': 5, u'Mn3+': 4, u'Mn4+': 3, u'Mo': 5, u'Ni': 5,
+                                                 u'V': 5, u'W': 5}, u'NBANDS': 221, u'NELM': 1, u'NPAR': 13,
+                                                 u'PREC': u'Medium', u'SIGMA': 0.01})
+
+    def test_GWG0W0VaspInputSet(self):
+        inpset = GWG0W0VaspInputSet(structure=self.structure, spec=self.spec)
+        self.assertIsInstance(inpset, GWG0W0VaspInputSet)
+        self.assertEqual(inpset.convs,
+                         {u'ENCUTGW': {u'test_range': (200, 400, 600, 800), u'control': u'gap', u'method': u'incar_settings'}})
+
+    def test_SingleVaspGWWork(self):
+        work = SingleVaspGWWork(structure=self.structure, spec=self.spec, job='prep')
+        self.assertIsInstance(work, SingleVaspGWWork)
