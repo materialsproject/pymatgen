7b894be24cf5938fa315d0aeae9b5f98c7da3c1e
gmatteo <gmatteo@gmail.com>
2014-10-25 18:46:33 +0200

    Integrate new version for hybrid MPI-OMP runs.

--- pymatgen/io/abinitio/tests/test_qadapters.py
+++ pymatgen/io/abinitio/tests/test_qadapters.py
@@ -106,16 +106,16 @@ from pymatgen.io.abinitio.qadapters import Command
 class CommandTest(unittest.TestCase):
     def test_command(self):
         """Test Command class"""
-        command = Command("sleep 0.5")
+        sleep05 = Command("sleep 0.5")
 
-        command.run(timeout=1)
-        print(command)
-        self.assertEqual(command.status, 0)
-        self.assertFalse(command.killed)
+        sleep05.run(timeout=1)
+        print(sleep05)
+        self.assertEqual(sleep05.retcode, 0)
+        self.assertFalse(sleep05.killed)
 
-        command.run(timeout=0.2)
-        self.assertNotEqual(command.status, 0)
-        self.assertTrue(command.killed)
+        sleep05.run(timeout=0.1)
+        self.assertNotEqual(sleep05.retcode, 0)
+        self.assertTrue(sleep05.killed)
 
 
 if __name__ == '__main__':
