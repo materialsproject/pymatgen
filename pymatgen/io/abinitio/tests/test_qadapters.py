a61eb0e941345f5a183f3de49060645c76316dad
gmatteo <gmatteo@gmail.com>
2014-10-25 17:45:40 +0200

    Preliminary support for Hybrid MPI-OMP in PbsPro adapter

--- pymatgen/io/abinitio/tests/test_qadapters.py
+++ pymatgen/io/abinitio/tests/test_qadapters.py
@@ -1,11 +1,9 @@
 # coding: utf-8
-
 from __future__ import unicode_literals, division, print_function
 
 from collections import OrderedDict
 from pymatgen.util.testing import PymatgenTest
 from pymatgen.io.abinitio.qadapters import *
-from pymatgen.io.abinitio.qadapters import AbstractQueueAdapter
 
 class QadapterTest(PymatgenTest):
 
@@ -58,14 +56,49 @@ class QadapterTest(PymatgenTest):
 
     #def test_openmp(self)
     #    omp_env = {"OMP_NUM_THREADS": 2}
-
     #    for subc in AbstractQueueAdapter.__subclasses__()
     #        cls = qadapter_class(subc.QTYPE)
-
     #        # Create the adapter
     #        #qad = cls(qparams=None, setup=None, modules=modules, shell_env=shell_env, omp_env=omp_env, 
     #        #          pre_run=None, post_run=None, mpi_runner=mpi_runner)
     #        self.assertTrue(qad.has_omp)
+
+
+class PbsProadapterTest(PymatgenTest):
+    """Test suite for PbsPro adapter."""
+    def test_params_from_partition(self):
+        aequal = self.assertEqual
+        p = Partition("test_partition", num_nodes=100, sockets_per_node=2, cores_per_socket=4)
+        print("partition\n" + str(p))
+
+        qad = PbsProAdapter()
+        print(qad)
+
+        qad.set_mpi_ncpus(4)
+        params = qad.params_from_partition(p)
+        print(params)
+        # IN_CORE PURE MPI: MPI: 4, OMP: 1
+        aequal(params, {'ompthreads': 1, 'ncpus': 1, 'select': 4, 'mpiprocs': 1})
+
+        qad.set_omp_ncpus(2)
+        params = qad.params_from_partition(p)
+        print(params)
+        # HYBRID MPI-OPENMP run, perfectly divisible among nodes:  MPI: 4, OMP: 2
+        aequal(params, {'ompthreads': 2, 'ncpus': 8, 'select': 1, 'mpiprocs': 4})
+
+        qad.set_mpi_ncpus(12)
+        params = qad.params_from_partition(p)
+        print(params)
+        # HYBRID MPI-OPENMP run, perfectly divisible among nodes:  MPI: 12, OMP: 2
+        aequal(params, {'ompthreads': 2, 'ncpus': 8, 'select': 3, 'mpiprocs': 4})
+
+        qad.set_omp_ncpus(5)
+        qad.set_mpi_ncpus(3)
+        params = qad.params_from_partition(p)
+        print(params)
+        # HYBRID MPI-OPENMP, NOT commensurate with nodes:  MPI: 3, OMP: 5
+        aequal(params, {'ompthreads': 5, 'ncpus': 5, 'select': 3, 'mpiprocs': 1})
+
 
 
 import unittest
