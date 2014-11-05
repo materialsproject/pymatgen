4618267ae53d0e03a321927fc34e19eacc3f76f1
setten <setten@gmail.com>
2014-11-05 14:01:03 +0100

    phonon script
    Former-commit-id: 5f1f190801ea47dcf8c8416389a8df8c35abc953 [formerly de5b36ac6f129babd12cdab44f86afe6ba32c874]
    Former-commit-id: 742c492ac20bbe9fa614f5f410b0c780c2430cf3

--- pymatgen/io/abinitio/flows.py
+++ pymatgen/io/abinitio/flows.py
@@ -19,7 +19,7 @@ from prettytable import PrettyTable
 from monty.io import FileLock
 from monty.termcolor import cprint, colored, stream_has_colours
 from pymatgen.serializers.pickle_coders import pmg_pickle_load, pmg_pickle_dump 
-from .tasks import Dependency, Status, Node, NodeResults, Task, ScfTask, PhononTask, TaskManager
+from .tasks import Dependency, Status, Node, NodeResults, Task, ScfTask, PhononTask, TaskManager, AnaddbTask, QpMergeTask
 from .utils import Directory, Editor
 from .abiinspect import yaml_read_irred_perts
 from .workflows import Workflow, BandStructureWorkflow, PhononWorkflow, G0W0_Workflow, QptdmWorkflow
@@ -1294,7 +1294,7 @@ def g0w0_flow(workdir, manager, scf_input, nscf_input, scr_input, sigma_inputs):
     return flow.allocate()
 
 
-def phonon_flow(workdir, manager, scf_input, ph_inputs):
+def phonon_flow(workdir, manager, scf_input, ph_inputs, ana_input=None):
     """
     Build an `AbinitFlow` for phonon calculations.
 
@@ -1380,5 +1380,10 @@ def phonon_flow(workdir, manager, scf_input, ph_inputs):
             work_qpt.register(new_input, deps={scf_task: "WFK"}, task_class=PhononTask)
 
         flow.register_work(work_qpt)
+
+        if ana_input is not None:
+            merge_input = {}
+            qp_merge_task = flow.register_task(merge_input, deps={work_qpt: "DDB"}, task_class=QpMergeTask)
+            flow.register_task(ana_input, deps={qp_merge_task: "DDB"}, task_class=AnaddbTask)
                                             
     return flow.allocate()
