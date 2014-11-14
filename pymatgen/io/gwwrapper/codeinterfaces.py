bd5f9cff51e19bd52995993a43e6cce7b29d2d4b
setten <setten@gmail.com>
2014-11-14 14:12:26 +0100

    vasp parcing

--- pymatgen/io/gwwrapper/codeinterfaces.py
+++ pymatgen/io/gwwrapper/codeinterfaces.py
@@ -153,6 +153,7 @@ class VaspInterface(AbstractCodeInterface):
             if os.path.isfile(run):
                 try:
                     logger.debug(run)
+                    print(run)
                     data = Vasprun(run, ionic_step_skip=1)
                     parameters = data.__getattribute__('incar').as_dict
                     bandstructure = data.get_band_structure(kpoints)
