0666c074a3ecc092fba1945437dfdd17f9e3a106
gmatteo <gmatteo@gmail.com>
2014-11-14 14:15:20 +0100

    Add parser for GHT pseudos
    Former-commit-id: 695a8209ef21e7527a92b05740163d8247724b2b [formerly 6525960dc1e3b994ae9649d030cd94573ecf8765]
    Former-commit-id: a0cf8790e29883605fc74fefce683df2d153bc81

--- pymatgen/io/abinitio/pseudos.py
+++ pymatgen/io/abinitio/pseudos.py
@@ -675,6 +675,26 @@ class NcAbinitHeader(AbinitHeader):
         return NcAbinitHeader(summary, **header)
 
     @staticmethod
+    def gth_header(filename, ppdesc):
+        """Parse the GTH abinit header."""
+        # Example:
+        #Goedecker-Teter-Hutter  Wed May  8 14:27:44 EDT 1996
+        #1   1   960508                     zatom,zion,pspdat
+        #2   1   0    0    2001    0.       pspcod,pspxc,lmax,lloc,mmax,r2well
+        #0.2000000 -4.0663326  0.6778322 0 0     rloc, c1, c2, c3, c4
+        #0 0 0                              rs, h1s, h2s
+        #0 0                                rp, h1p
+        #  1.36 .2   0.6                    rcutoff, rloc
+        lines = _read_nlines(filename, -1)
+
+        header = _dict_from_lines(lines[:3], [0, 3, 6])
+        summary = lines[0]
+
+        header["dojo_report"] = read_dojo_report(filename)
+
+        return NcAbinitHeader(summary, **header)
+
+    @staticmethod
     def oncvpsp_header(filename, ppdesc):
         """Parse the ONCVPSP abinit header."""
         # Example
@@ -907,6 +927,7 @@ class PseudoParser(object):
     # TODO Recheck
     _PSPCODES = collections.OrderedDict( {
         1 : ppdesc(1, "TM",  "NC", None),
+        2 : ppdesc(2, "GTH",  "NC", None),
         3 : ppdesc(3, "HGH", "NC", None),
         #4 : ppdesc(4, "NC",     , None),
         #5 : ppdesc(5, "NC",     , None),
@@ -1047,6 +1068,7 @@ class PseudoParser(object):
 
         parsers = {
             "FHI"            : NcAbinitHeader.fhi_header,
+            "GTH"            : NcAbinitHeader.gth_header,
             "TM"             : NcAbinitHeader.tm_header,
             "HGH"            : NcAbinitHeader.hgh_header,
             "HGHK"           : NcAbinitHeader.hgh_header,
