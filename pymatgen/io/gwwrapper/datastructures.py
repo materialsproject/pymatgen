fa3f58eeee385087f0c108c9f55f8d67a15eeb66
setten <setten@gmail.com>
2014-11-20 15:41:43 +0100

    fix critical
    Former-commit-id: 566ef25032aa377a1a99283b08454a79c9f480f8 [formerly 3b122b92d67b352343a8841fa5423946dba754cd]
    Former-commit-id: a722260e745b56c5910036bbbdf0d294a6ce50a8

--- pymatgen/io/gwwrapper/datastructures.py
+++ pymatgen/io/gwwrapper/datastructures.py
@@ -152,7 +152,7 @@ class AbstractAbinitioSpec(MSONable):
 
         if self.data['source'] == 'mp-vasp':
             items_list = mp_list_vasp
-        elif self.data['source'] == 'poscar':
+        elif self.data['source'] in ['poscar', 'cif']:
             files = os.listdir('.')
             items_list = files
         elif self.data['source'] == 'mar_exp':
@@ -162,8 +162,10 @@ class AbstractAbinitioSpec(MSONable):
             pwd = os.environ['MAR_PAS']
             local_db_gaps.authenticate("setten", pwd)
             for c in local_db_gaps.exp.find():
-                print(Structure.from_dict(c['icsd_data']['structure']).composition.reduced_formula, c['icsd_id'],\
-                    c['MP_id'])
+                name = Structure.from_dict(c['icsd_data']['structure']).composition.reduced_formula, c['icsd_id'],\
+                    c['MP_id']
+                print(name)
+                Structure.from_dict(c['icsd_data']['structure']).to(fmt='cif',filename=name)
                 items_list.append({'name': 'mp-' + c['MP_id'], 'icsd': c['icsd_id'], 'mp': c['MP_id']})
         else:
             items_list = [line.strip() for line in open(self.data['source'])]
@@ -176,6 +178,7 @@ class AbstractAbinitioSpec(MSONable):
                 exp = local_db_gaps.exp.find({'MP_id': item['mp']})[0]
                 structure = Structure.from_dict(exp['icsd_data']['structure'])
                 structure = refine_structure(structure)
+                structure.to(fmt='cif', filename=item['name'])
                 try:
                     kpts = local_db_gaps.GGA_BS.find({'transformations.history.0.id': item['icsd']})[0]['calculations']\
                     [-1]['band_structure']['kpoints']
