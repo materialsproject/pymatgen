d94e65f229ff96d1a73ad97855cf1cb89d2cb352
setten <setten@gmail.com>
2014-11-21 12:47:58 +0100

    multy screening

--- pymatgen/io/abinitio/workflows.py
+++ pymatgen/io/abinitio/workflows.py
@@ -850,10 +850,12 @@ class G0W0_Workflow(Workflow):
         for sigma_input in sigma_inputs:
             if spread_scr:
                 scr_input.screening.ecuteps = sigma_input.sigma.ecuteps
+                print('\n  ---- \n')
                 print(scr_input.screening.ecuteps, sigma_input.sigma.ecuteps)
-                scr_input.electrons.nband = sigma_input.sigma.nband
+                scr_input.screening.nband = sigma_input.sigma.nband
+                scr_input.electrons.nband = = sigma_input.sigma.nband
                 print(scr_input.electrons.nband, sigma_input.sigma.nband)
-                #print(scr_input.make_input())
+                print(scr_input.make_input())
                 scr_task = self.register(scr_input, deps={nscf_task: "WFK"})
 
             task = self.register(sigma_input, deps={nscf_task: "WFK", scr_task: "SCR"})
