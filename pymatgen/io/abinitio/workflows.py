028d39e30d706932d447479804856fcfdff47d1f
setten <setten@gmail.com>
2014-11-21 14:37:11 +0100

    multy screening
    Former-commit-id: 243b60f0f32e196458ab7a26966edefb2517ec4e [formerly 83012d322b2eeef8aab0512fde9513153410e1bb]
    Former-commit-id: 960c0d3d0f3577b9f9bc74a26f8c829ec91eaf84

--- pymatgen/io/abinitio/workflows.py
+++ pymatgen/io/abinitio/workflows.py
@@ -853,15 +853,9 @@ class G0W0_Workflow(Workflow):
             if spread_scr:
                 new_scr_input = copy.deepcopy(scr_input)
                 new_scr_input.screening.ecuteps = sigma_input.sigma.ecuteps
-                print('\n  ---- \n')
-                print(new_scr_input.screening.ecuteps, sigma_input.sigma.ecuteps)
                 new_scr_input.screening.nband = sigma_input.sigma.nband
                 new_scr_input.electrons.nband = sigma_input.sigma.nband
-                print(new_scr_input.electrons.nband, sigma_input.sigma.nband)
-                print(new_scr_input.make_input())
                 scr_task = self.register(new_scr_input, deps={nscf_task: "WFK"})
-                print(' --- task ---')
-                print(scr_task.make_input())
 
             task = self.register(sigma_input, deps={nscf_task: "WFK", scr_task: "SCR"})
             self.sigma_tasks.append(task)
