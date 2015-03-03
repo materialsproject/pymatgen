22ec688c64c7f6e908568ebec850db78e43f2aff
gmatteo <gmatteo@gmail.com>
2015-03-03 22:30:54 +0100

    Output number of corrections in flow.show_status()
    Former-commit-id: 79b3f3ae83a3120988071b221ef23af9ceca5bcf [formerly 02b6744ae838c6ada4d874aa2a490427d73db940]
    Former-commit-id: 59b4b6c24e3e530085c592706de59539079ea51a

--- pymatgen/io/abinitio/mocks.py
+++ pymatgen/io/abinitio/mocks.py
@@ -2,18 +2,19 @@
 """Mock objects for unit tests."""
 from __future__ import division, print_function, unicode_literals
 
+from .nodes import Status
 from .tasks import AbinitTask
 
-def mock_task_start(task):
+
+def mock_task_start(task, mocked_status="Error"):
     """
     """
-    task.__class__ = ErroredAbinitTask
+    task.__class__ = AbinitTaskMockedStart
+    task.mocked_status = Status.as_status(mocked_status)
     return task
 
 
-class ErroredAbinitTask(AbinitTask): 
+class AbinitTaskMockedStart(AbinitTask): 
     def start(self, **kwargs):
-        self.set_status(self.S_ERROR, info_msg="Mocking Error")
+        self.set_status(self.mocked_status, msg="Mocking status with %s" % self.mocked_status)
         return 1
-
-
