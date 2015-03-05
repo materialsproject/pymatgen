69c1852e17e4f32712d12d9122b6e36e5570098a
gmatteo <gmatteo@gmail.com>
2015-03-05 12:55:14 +0100

    Node.event_handlers and API to set them if default behaviour is not wanted.

--- pymatgen/io/abinitio/tests/test_events.py
+++ pymatgen/io/abinitio/tests/test_events.py
@@ -1,29 +1,19 @@
 # coding: utf-8
 from __future__ import unicode_literals, division, print_function
 
-#import os
-#import tempfile
-#import shutil
-
 from pymatgen.util.testing import PymatgenTest
-#from monty.functools import lazy_property
-#from pymatgen.core.lattice import Lattice
-#from pymatgen.core.structure import Structure
 from pymatgen.io.abinitio import events
-#from pymatgen.io.abinitio.flows import *
-#from pymatgen.io.abinitio.tasks import *
-#from pymatgen.io.abinitio.pseudos import Pseudo
-
-#_test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..", "..", 'test_files')
-#
-#
-#def ref_file(filename):
-#    return os.path.join(_test_dir, filename)
 
 
 class EventHandlersTest(PymatgenTest):
-    def test_api(self):
+    def test_events(self):
+        # Autodoc
         events.autodoc_event_handlers()
+
+        for cls in events.get_event_handler_classes():
+            # Test pickle
+            handler = cls()
+            self.serialize_with_pickle(handler, test_eq=False)
 
 
 if __name__ == '__main__':
