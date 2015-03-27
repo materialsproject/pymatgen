7247ff6046c48b49e34d2457484b12563668d35d
__sp__ <ongsp@eng.ucsd.edu>
2015-03-27 13:29:19 -0700

    Basic PWSCF io.

---
+++ pymatgen/io/pwscfio.py
@@ -0,0 +1,99 @@
+# coding: utf-8
+
+from __future__ import division, unicode_literals
+
+"""
+This module implements input and output processing from PWSCF.
+"""
+
+from __future__ import division
+
+__author__ = "Shyue Ping Ong"
+__copyright__ = "Copyright 2012, The Materials Virtual Lab"
+__version__ = "0.1"
+__maintainer__ = "Shyue Ping Ong"
+__email__ = "ongsp@ucsd.edu"
+__date__ = "3/27/15"
+
+
+import six
+
+
+class PWSCFInput(object):
+    """
+    Base input file class. Right now, only supports no symmetry and is
+    very basic.
+    """
+
+    def __init__(self, structure, pseudo, control=None, system=None, electrons=None,
+                 ions=None, cell=None, kpoints_mode="automatic",
+                 kpoints_grid=(1, 1, 1),kpoints_shift=(0, 0, 0)):
+        self.structure = structure
+        sections = {}
+        sections["control"] = control or {"calculation": "scf"}
+        sections["system"] = system or {}
+        sections["electrons"] = electrons or {}
+        sections["ions"] = ions or {}
+        sections["cell"] = cell or {}
+        self.pseudo = pseudo
+        self.sections = sections
+        self.kpoints_mode = kpoints_mode
+        self.kpoints_grid = kpoints_grid
+        self.kpoints_shift = kpoints_shift
+
+    def __str__(self):
+        out = []
+        def to_str(v):
+            if isinstance(v, six.string_types):
+                return "'%s'" % v
+            return v
+        for k1 in ["control", "system", "electrons", "ions", "cell"]:
+            v1 = self.sections[k1]
+            out.append("&%s" % k1.upper())
+            sub = []
+            for k2, v2 in v1.items():
+                sub.append("  %s = %s" % (k2, to_str(v2)))
+            if k1 == "system":
+                sub.append("  ibrav = 0")
+                sub.append("  nat = %d" % len(self.structure))
+                sub.append("  ntyp = %d" % len(self.structure.composition))
+            sub.append("/")
+            out.append(",\n".join(sub))
+
+        out.append("ATOMIC_SPECIES")
+        for k, v in self.structure.composition.items():
+            out.append("  %s %.4f %s" % (k.symbol, k.atomic_mass,
+                                         self.pseudo[k.symbol]))
+        out.append("ATOMIC_POSITIONS crystal")
+        for site in self.structure:
+            out.append("  %s %.6f %.6f %.6f" % (site.specie.symbol, site.a,
+                                                site.b, site.c))
+        out.append("K_POINTS %s" % self.kpoints_mode)
+        kpt_str = ["%s" % i for i in self.kpoints_grid]
+        kpt_str.extend(["%s" % i for i in self.kpoints_shift])
+        out.append("  %s" % " ".join(kpt_str))
+        out.append("CELL_PARAMETERS angstrom")
+        for vec in self.structure.lattice.matrix:
+            out.append("  %f %f %f" % (vec[0], vec[1], vec[2]))
+        return "\n".join(out)
+
+    def write_file(self, filename):
+        with open(filename, "w") as f:
+            f.write(self.__str__())
+
+
+if __name__ == "__main__":
+    from pymatgen.core.structure import Structure
+    coords = []
+    coords.append([0, 0, 0])
+    coords.append([0.75, 0.5, 0.75])
+    lattice = [[3.8401979337, 0.00, 0.00],
+               [1.9200989668, 3.3257101909, 0.00],
+               [0.00, -2.2171384943, 3.1355090603]]
+    structure = Structure(lattice, ["Si", "Si"], coords)
+    pw = PWSCFInput(structure,
+                    control={"calculation": "scf", "pseudo_dir": './'},
+                    pseudo={"Si": "Si.pbe-n-kjpaw_psl.0.1.UPF"},
+                    system={"ecutwfc": 50})
+    print pw
+    pw.write_file("Si.pw.in")
