86d76fce4b8d09547b27d8c1a30712fdc36a9052
William Richards <wmdrichards@gmail.com>
2015-06-04 12:46:36 -0400

    Composition.get_integer_formula_and_factor nosetest fix for python3
    Former-commit-id: c3bfa531254916d17bee65d980c2cefb24c3b653 [formerly d481abd91af14fef572d1fafc71c288a4b70e13a]
    Former-commit-id: 2f90e7e75f1456ff9562a97c84e2bfcc68cde8b7

--- pymatgen/core/tests/test_composition.py
+++ pymatgen/core/tests/test_composition.py
@@ -148,8 +148,9 @@ class CompositionTest(unittest.TestCase):
                          ('Li2O', 0.25))
         self.assertEqual(Composition('O0.25').get_integer_formula_and_factor(),
                          ('O2', 0.125))
-        self.assertEqual(Composition("Li0.16666667B1.0H1.0").get_integer_formula_and_factor(),
-                         ('Li(BH)6', 1/6))
+        formula, factor = Composition("Li0.16666667B1.0H1.0").get_integer_formula_and_factor()
+        self.assertEqual(formula, 'Li(BH)6')
+        self.assertAlmostEqual(factor, 1 / 6)
 
     def test_num_atoms(self):
         correct_num_atoms = [20, 10, 7, 8, 20, 75, 2, 3]
