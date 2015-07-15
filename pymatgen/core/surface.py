2f45e19e368735c8662bfb78cbb154ff6f495556
__sp__ <ongsp@eng.ucsd.edu>
2015-07-15 05:09:27 -0700

    Add equivalent indices test for hexagonal system.
    Former-commit-id: c510398748d0957db5f65ad7814d742fd50d07f4 [formerly 26009467468f55428fe03e9aac151a1274240785]
    Former-commit-id: f02fc3b15699cae38157301017b70ed5e560d553

--- pymatgen/core/surface.py
+++ pymatgen/core/surface.py
@@ -317,7 +317,10 @@ class SlabGenerator(object):
         Also stores the initial information needed later on to generate a slab.
 
         Args:
-            initial_structure (Structure): Initial input structure.
+            initial_structure (Structure): Initial input structure. Note that to
+                ensure that the miller indices correspond to usual
+                crystallographic definitions, you should supply a conventional
+                unit cell structure.
             miller_index ([h, k, l]): Miller index of plane parallel to
                 surface. Note that this is referenced to the input structure. If
                 you need this to be based on the conventional cell,
@@ -685,7 +688,10 @@ def generate_all_slabs(structure, max_index, min_slab_size, min_vacuum_size,
     CsCl has equivalent slabs in the (0,0,1), (0,1,0), and (1,0,0) direction.
 
     Args:
-        structure (Structure): Initial input structure.
+        structure (Structure): Initial input structure. Note that to
+                ensure that the miller indices correspond to usual
+                crystallographic definitions, you should supply a conventional
+                unit cell structure.
         max_index (int): The maximum Miller index to go up to.
         symprec (float): Tolerance for symmetry finding. Defaults to 1e-3,
             which is fairly strict and works well for properly refined
