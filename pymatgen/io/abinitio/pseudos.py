4c43c3ccbef6287d96a31edc3c72800848fee370
gmatteo <gmatteo@gmail.com>
2015-07-25 21:49:14 +0200

    Add md5 to pseudopotential file
    Former-commit-id: d686b534a0861a1cc689886b550b70725ee751ba [formerly 44561d71fae6a285efc2779614093861561b4a2b]
    Former-commit-id: ad90450c5eedcf9591e82c800a6bafe28afd62ac

--- pymatgen/io/abinitio/pseudos.py
+++ pymatgen/io/abinitio/pseudos.py
@@ -259,7 +259,7 @@ class Pseudo(six.with_metaclass(abc.ABCMeta, PMGSONable, object)):
                 raise ValueError("md5 found in dojo_report does not agree\n"
                     "with the computed value\nFound %s\nComputed %s"  % (report["md5"], hash))
         else:
-            report["md5"] = self.md5
+            report["md5"] = self._compute_md5()
             self.write_dojo_report(report=report)
 
     #@abc.abstractproperty
@@ -471,10 +471,6 @@ class Pseudo(six.with_metaclass(abc.ABCMeta, PMGSONable, object)):
         except Exception as exc:
             logger.critical("Exception while reading PSPS file at %s:\n%s" % (filepath, str(exc)))
             return None
-
-    def write_notebook(self):
-        """Write an ipython noteboolk to `pseudo.path.ipynb."""
-        write_notebook(self.path)
 
 
 class NcPseudo(six.with_metaclass(abc.ABCMeta, object)):
@@ -2554,7 +2550,7 @@ class DojoReport(dict):
         xs = 1/ecuts if inv_ecut else ecuts
         ys = etotals_mev if inv_ecut else ediffs
 
-        line, = ax.plot(xs, ys, "-->", color="blue", linewidth=3.0, markersize=15)
+        line, = ax.plot(xs, ys, "-o", color="blue") #, linewidth=3.0, markersize=15)
         lines.append(line)
 
         label = kwargs.pop("label", None)
@@ -2709,7 +2705,7 @@ class DojoReport(dict):
             #    refval = 0.0
 
             # Plot difference pseudo - ref.
-            ax.plot(ecuts, values - refval, "bo-")
+            ax.plot(ecuts, values - refval, "o-")
 
             ax.grid(True)
             ax.set_ylabel("$\Delta$" + key)
@@ -2719,12 +2715,12 @@ class DojoReport(dict):
                 # Add horizontal lines (used to find hints for ecut).
                 last = values[-1]
                 xmin, xmax = min(ecuts), max(ecuts)
-                for pad, color in zip(self.ATOLS, ("green", "red", "violet")):
-                    ax.hlines(y=last + pad, xmin=xmin, xmax=xmax, colors=color, linewidth=3, linestyles='dotted')
-                    ax.hlines(y=last - pad, xmin=xmin, xmax=xmax, colors=color, linewidth=3, linestyles='dotted')
+                for pad, color in zip(self.ATOLS, ("blue", "red", "violet")):
+                    ax.hlines(y=last + pad, xmin=xmin, xmax=xmax, colors=color, linewidth=1, linestyles='dashed')
+                    ax.hlines(y=last - pad, xmin=xmin, xmax=xmax, colors=color, linewidth=1, linestyles='dashed')
               
                 # Set proper limits so that we focus on the relevant region.
-                ax.set_ylim(last - 2*self.ATOLS[0], last + 2*self.ATOLS[0])
+                ax.set_ylim(last - 1.1*self.ATOLS[0], last + 1.1*self.ATOLS[0])
 
         return fig
 
@@ -2774,11 +2770,6 @@ class DojoReport(dict):
 
         Args:
             ax_list: List of matplotlib Axes, if ax_list is None a new figure is created
-
-        ================  ==============================================================
-        kwargs            Meaning
-        ================  ==============================================================
-        ================  ==============================================================
 
         Returns:
             `matplotlib` figure.
@@ -2859,126 +2850,3 @@ class DojoReport(dict):
         ax[0].set_ylim(fmin, fmax)
 
         return fig
-
-
-def write_notebook(pseudopath):
-    """
-    See http://nbviewer.ipython.org/gist/fperez/9716279
-
-    """
-    from IPython.nbformat import current as nbf
-    #from IPython.nbformat import v3 as nbf
-    #import IPython.nbformat as nbf
-
-    nb = nbf.new_notebook()
-    
-    cells = [
-        nbf.new_heading_cell("This is an auto-generated notebook for %s" % os.path.basename(pseudopath)),
-        nbf.new_code_cell("""\
-from __future__ import print_function
-%matplotlib inline
-import mpld3
-from mpld3 import plugins as plugs
-plugs.DEFAULT_PLUGINS = [plugs.Reset(), plugs.Zoom(), plugs.BoxZoom(), plugs.MousePosition()]
-mpld3.enable_notebook()
-import seaborn as sns
-#sns.set(style="dark", palette="Set2")
-sns.set(style='ticks', palette='Set2')"""),
-
-        nbf.new_code_cell("""\
-# Construct the pseudo object and get the DojoReport
-from pymatgen.io.abinitio.pseudos import Pseudo
-pseudo = Pseudo.from_file('%s')
-report = pseudo.dojo_report""" % os.path.basename(pseudopath)),
-
-        nbf.new_heading_cell("ONCVPSP Input File:"),
-        nbf.new_code_cell("""\
-input_file = pseudo.filepath.replace(".psp8", ".in") 
-%cat $input_file"""),
-
-        nbf.new_code_cell("""\
-# Get data from the output file
-from pseudo_dojo.ppcodes.oncvpsp import OncvOutputParser, PseudoGenDataPlotter
-onc_parser = OncvOutputParser(pseudo.filepath.replace(".psp8", ".out"))
-# Parse the file and build the plotter
-onc_parser.scan()
-plotter = onc_parser.make_plotter()"""),
-
-        nbf.new_heading_cell("AE/PS radial wavefunctions $\phi(r)$:"),
-        nbf.new_code_cell("""fig = plotter.plot_radial_wfs(show=False)"""),
-
-        nbf.new_heading_cell("Arctan of the logarithmic derivatives:"),
-        nbf.new_code_cell("""fig = plotter.plot_atan_logders(show=False)"""),
-
-        nbf.new_heading_cell("Convergence in $G$-space estimated by ONCVPSP:"),
-        nbf.new_code_cell("""fig = plotter.plot_ene_vs_ecut(show=False)"""),
-
-        nbf.new_heading_cell("Projectors:"),
-        nbf.new_code_cell("""fig = plotter.plot_projectors(show=False)"""),
-
-        nbf.new_heading_cell("Core/Valence/Model charge densities:"),
-        nbf.new_code_cell("""fig = plotter.plot_densities(show=False)"""),
-
-        nbf.new_heading_cell("Local potential and $l$-dependent potentials:"),
-        nbf.new_code_cell("""fig = plotter.plot_potentials(show=False)"""),
-
-        #nbf.new_heading_cell("1-st order derivative of $v_l$ and $v_{loc}$ computed via finite differences:"),
-        #nbf.new_code_cell("""fig = plotter.plot_der_potentials(order=1, show=False)"""),
-
-        #nbf.new_heading_cell("2-nd order derivative of $v_l$ and $v_{loc}$ computed via finite differences:"),
-        #nbf.new_code_cell("""fig = plotter.plot_der_potentials(order=2, show=False)"""),
-
-        nbf.new_heading_cell("Model core charge and form factors computed by ABINIT"),
-        nbf.new_code_cell("""\
-with pseudo.open_pspsfile() as psps:
-    psps.plot_modelcore_rspace()
-    psps.plot_modelcore_qspace()
-    psps.plot_vlspl()
-    psps.plot_ffspl()"""),
-
-        nbf.new_heading_cell("Convergence of the total energy:"),
-        nbf.new_code_cell("""\
-# Convergence of the total energy (computed from the deltafactor runs with Wien2K equilibrium volume)
-fig = report.plot_etotal_vs_ecut(show=False)"""),
-
-        nbf.new_heading_cell("Convergence of the deltafactor results:"),
-        nbf.new_code_cell("""fig = report.plot_deltafactor_convergence(what=("dfact_meV", "dfactprime_meV"), show=False)"""),
-
-        nbf.new_heading_cell("Convergence of $\Delta v_0$, $\Delta b_0$, and $\Delta b_1$ (deltafactor tests)"),
-        nbf.new_code_cell("""\
-# Here we plot the difference wrt Wien2k results.
-fig = report.plot_deltafactor_convergence(what=("-dfact_meV", "-dfactprime_meV"), show=False)"""),
-
-        nbf.new_heading_cell("deltafactor EOS for the different cutoff energies:"),
-        nbf.new_code_cell("""fig = report.plot_deltafactor_eos(show=False)"""),
-
-        nbf.new_heading_cell("Convergence of the GBRV lattice parameters:"),
-        nbf.new_code_cell("""fig = report.plot_gbrv_convergence(show=False)"""),
-
-        nbf.new_heading_cell("GBRV EOS for the FCC structure:"),
-        nbf.new_code_cell("""fig = report.plot_gbrv_eos(struct_type="fcc", show=False)"""),
-
-        nbf.new_heading_cell("GBRV EOS for the BCC structure:"),
-        nbf.new_code_cell("""fig = report.plot_gbrv_eos(struct_type="bcc", show=False)"""),
-
-        nbf.new_heading_cell("Convergence of phonon frequencies at $\Gamma$:"),
-        nbf.new_code_cell("""fig = report.plot_phonon_convergence(show=False)"""),
-
-#        nbf.new_heading_cell("Comparison with the other pseudos in this table"),
-#        nbf.new_code_cell("""\
-#from pseudo_dojo import get_pseudos
-#pseudos = get_pseudos(".")
-#if len(pseudos) > 1:
-#    pseudos.dojo_compare()"""),
-
-    ]
-
-    # Now that we have the cells, we can make a worksheet with them and add it to the notebook:
-    nb['worksheets'].append(nbf.new_worksheet(cells=cells))
-
-    # Next, we write it to a file on disk that we can then open as a new notebook.
-    # Note: This should be as easy as: nbf.write(nb, fname), but the current api is 
-    # a little more verbose and needs a real file-like object.
-    root, ext = os.path.splitext(pseudopath)
-    with open(root + '.ipynb', 'wt') as f:
-        nbf.write(nb, f, 'ipynb')
