0c140b63ea562c9d74332c4db183697480cd7c0f
gmatteo <gmatteo@gmail.com>
2015-08-13 16:26:31 +0200

    Add support for XSF format
    Former-commit-id: 585e4e80be2f6995d309ffad9d709a963c3693f7 [formerly 397110c66860c82fdf8dbd6706964c68cc329fd4]
    Former-commit-id: ad1e10b5fc40766007ff567b3184bb807e5b9dc9

--- pymatgen/core/structure.py
+++ pymatgen/core/structure.py
@@ -517,6 +517,52 @@ class IStructure(SiteCollection, PMGSONable):
                    site_properties=all_site_properties)
 
     @classmethod
+    def from_xsf_string(cls, input_string):
+        """
+        Initialize a `Structure` object from a string with data in XSF format.
+        See http://www.xcrysden.org/doc/XSF.html
+        """
+        # CRYSTAL                                        see (1)
+        # these are primitive lattice vectors (in Angstroms) 
+        # PRIMVEC
+        #    0.0000000    2.7100000    2.7100000         see (2)
+        #    2.7100000    0.0000000    2.7100000
+        #    2.7100000    2.7100000    0.0000000
+
+        # these are conventional lattice vectors (in Angstroms)
+        # CONVVEC
+        #    5.4200000    0.0000000    0.0000000         see (3)
+        #    0.0000000    5.4200000    0.0000000
+        #    0.0000000    0.0000000    5.4200000
+
+        # these are atomic coordinates in a primitive unit cell  (in Angstroms)
+        # PRIMCOORD
+        # 2 1                                            see (4)
+        # 16      0.0000000     0.0000000     0.0000000  see (5)
+        # 30      1.3550000    -1.3550000    -1.3550000
+
+        lattice, coords, species = [], [], []
+        lines = input_string.splitlines()
+
+        for i in range(len(lines)):
+            if "PRIMVEC" in lines[i]:
+                for j in range(i+1, i+4):
+                    lattice.append([float(c) for c in lines[j].split()])
+
+            if "PRIMCOORD" in lines[i]:
+                num_sites = int(lines[i+1].split()[0])
+     
+                for j in range(i+2, i+2+num_sites):
+                    tokens = lines[j].split()
+                    species.append(int(tokens[0]))
+                    coords.append([float(j) for j in tokens[1:]])
+                break
+        else:
+            raise ValueError("Invalid XSF data")
+
+        return cls(lattice, species, coords, coords_are_cartesian=True)
+
+    @classmethod
     def from_abivars(cls, *args, **kwargs):
         """Build a :class:`Structure` object from a dictionary with ABINIT variables."""
         kwargs.update(dict(*args))
@@ -1251,6 +1297,32 @@ class IStructure(SiteCollection, PMGSONable):
 
         return d
 
+    def to_xsf_string(self):
+        """
+        Returns a string with the structure in XSF format
+        See http://www.xcrysden.org/doc/XSF.html
+        """
+        lines = []
+        app = lines.append
+
+        app("CRYSTAL")
+        app("# Primitive lattice vectors in Angstrom")
+        app("PRIMVEC")
+        cell = self.lattice_vectors(space="r") 
+        for i in range(3):
+            app(' %.14f %.14f %.14f' % tuple(cell[i]))
+
+        cart_coords = self.cart_coords  
+        app("# Cartesian coordinates in Angstrom.")
+        app("PRIMCOORD")
+        app(" %d 1" % len(cart_coords))
+
+        for a in range(len(cart_coords)):
+            sp = "%d" % self.atomic_numbers[a]
+            app(sp + ' %20.14f %20.14f %20.14f' % tuple(cart_coords[a]))
+
+        return "\n".join(lines)
+
     def dot(self, coords_a, coords_b, space="r", frac_coords=False):
         """
         Compute the scalar product of vector(s) either in real space or
@@ -1323,6 +1395,14 @@ class IStructure(SiteCollection, PMGSONable):
                 return
             else:
                 return s
+        elif fmt == "xsf" or fnmatch(fname.lower(), "*.xsf*"):
+            if filename:
+                with zopen(fname, "wt", encoding='utf8') as f:
+                    s = self.to_xsf_string()
+                    f.write(s)
+                    return s
+            else:
+                return self.to_xsf_string()
         else:
             if filename:
                 with open(filename, "w") as f:
@@ -1366,8 +1446,10 @@ class IStructure(SiteCollection, PMGSONable):
         elif fmt == "yaml":
             d = yaml.load(input_string)
             s = cls.from_dict(d)
+        elif fmt == "xsf":
+            s = cls.from_xsf_string(input_string)
         else:
-            raise ValueError("Unrecognized format!")
+            raise ValueError("Unrecognized format `%s`!" % fmt)
 
         if sort:
             s = s.get_sorted_structure()
@@ -1422,6 +1504,9 @@ class IStructure(SiteCollection, PMGSONable):
                                 primitive=primitive, sort=sort)
         elif fnmatch(fname, "*.yaml*"):
             return cls.from_str(contents, fmt="yaml",
+                                primitive=primitive, sort=sort)
+        elif fnmatch(fname, "*.xsf"):
+            return cls.from_str(contents, fmt="xsf",
                                 primitive=primitive, sort=sort)
         else:
             raise ValueError("Unrecognized file extension!")
@@ -1899,6 +1984,7 @@ class IMolecule(SiteCollection, PMGSONable):
                     return yaml.dump(self.as_dict(), f, Dumper=Dumper)
             else:
                 return yaml.dump(self.as_dict(), Dumper=Dumper)
+
         else:
             m = re.search("\.(pdb|mol|mdl|sdf|sd|ml2|sy2|mol2|cml|mrv)",
                           fname.lower())
