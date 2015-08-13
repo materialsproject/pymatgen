f73df72ec469991683b4bb0ddbcb1d402b1d2f96
__sp__ <ongsp@eng.ucsd.edu>
2015-08-13 12:29:18 -0700

    Fix graphite test.
    Former-commit-id: 58e842f5c6694053dd087f6a385ffa93c6bc6157 [formerly 6bbc15f1c3b50da4e04929c81265c93505858460]
    Former-commit-id: b7f86018a2438efd424b9496805237168e5ea059

--- pymatgen/symmetry/tests/test_analyzer.py
+++ pymatgen/symmetry/tests/test_analyzer.py
@@ -57,12 +57,12 @@ class SpacegroupAnalyzerTest(PymatgenTest):
         self.assertEqual(self.disordered_sg.get_spacegroup_symbol(),
                          "P4_2/nmc")
         self.assertEqual(self.sg3.get_spacegroup_symbol(), "Pnma")
-        self.assertEqual(self.sg4.get_spacegroup_symbol(), "R-3m")
+        self.assertEqual(self.sg4.get_spacegroup_symbol(), "P6_3/mmc")
 
     def test_get_space_number(self):
         self.assertEqual(self.sg.get_spacegroup_number(), 62)
         self.assertEqual(self.disordered_sg.get_spacegroup_number(), 137)
-        self.assertEqual(self.sg4.get_spacegroup_number(), 166)
+        self.assertEqual(self.sg4.get_spacegroup_number(), 194)
 
     def test_get_hall(self):
         self.assertEqual(self.sg.get_hall(), '-P 2ac 2n')
