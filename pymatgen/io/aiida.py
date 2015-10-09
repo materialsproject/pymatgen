72385a434b8a19cf80e1e71f730024806b4f7e2f
merkys <andrius.merkys@gmail.com>
2015-10-09 10:16:31 +0200

    Adding AiidaStructureAdaptor.

---
+++ pymatgen/io/aiida.py
@@ -0,0 +1,72 @@
+# coding: utf-8
+# Copyright (c) AiiDA Development Team.
+# Distributed under the terms of the MIT License.
+
+"""
+This module provides conversion between AiiDA StructureData object and
+pymatgen Molecule/Structure objects.
+"""
+
+
+__author__ = "Andrius Merkys"
+__copyright__ = "Copyright 2015, AiiDA Development Team"
+__version__ = "1.0"
+__maintainer__ = "Andrius Merkys"
+__email__ = "andrius.merkys@gmail.com"
+__date__ = "Oct 9, 2015"
+
+from pymatgen.core.structure import Molecule, Structure
+
+try:
+    from aiida.orm.data.structure import StructureData
+    aiida_loaded = True
+except ImportError:
+    aiida_loaded = False
+
+
+class AiidaStructureAdaptor(object):
+    """
+    Adaptor serves as a bridge between AiiDA StructureData and pymatgen
+    Molecule/Structure objects.
+    """
+
+    @staticmethod
+    def get_structuredata(structure):
+        """
+        Returns AiiDA StructureData object from pymatgen structure or
+        molecule.
+
+        Args:
+            structure: pymatgen.core.structure.Structure or
+                       pymatgen.core.structure.Molecule
+
+        Returns:
+            AiiDA StructureData object
+        """
+        return StructureData(pymatgen=structure);
+
+    @staticmethod
+    def get_molecule(structuredata):
+        """
+        Returns pymatgen molecule from AiiDA StructureData.
+
+        Args:
+            structuredata: AiiDA StructureData object
+
+        Returns:
+            pymatgen.core.structure.Molecule
+        """
+        return structuredata.get_pymatgen_molecule()
+
+    @staticmethod
+    def get_structure(structuredata):
+        """
+        Returns pymatgen structure from AiiDA StructureData.
+
+        Args:
+            structuredata: AiiDA StructureData object
+
+        Returns:
+            pymatgen.core.structure.Structure
+        """
+        return structuredata.get_pymatgen_structure()
