9a5e47d9ad101c9b360c8681cac0309c259a715b
Joseph Montoya <montoyjh@gmail.com>
2015-11-05 15:16:44 -0800

    changed name of phonons to elasticity

---
+++ pymatgen/elasticity/strain.py
@@ -0,0 +1,255 @@
+from pymatgen.core.lattice import Lattice
+from pymatgen.elasticity.tensors import SQTensor
+import warnings
+import numpy as np
+from six.moves import zip
+
+__author__ = "Maarten de Jong"
+__copyright__ = "Copyright 2012, The Materials Project"
+__credits__ = "Mark Asta, Anubhav Jain"
+__version__ = "1.0"
+__maintainer__ = "Maarten de Jong"
+__email__ = "maartendft@gmail.com"
+__status__ = "Development"
+__date__ = "March 13, 2012"
+
+
+class Deformation(SQTensor):
+    """
+    Subclass of SQTensor that describes the deformation gradient tensor
+    """
+
+    def __new__(cls, deformation_gradient):
+        obj = SQTensor(deformation_gradient).view(cls)
+        return obj
+
+    def __array_finalize__(self, obj):
+        if obj is None:
+            return
+
+    def __repr__(self):
+        return "Deformation({})".format(self.__str__())
+
+    def check_independent(self):
+        """
+        a check to determine whether the deformation matrix represents an
+        independent deformation, raises a ValueError if not.  If so, returns 
+        the indices of the deformation gradient entry representing the 
+        independent component
+
+        Args: tol
+        """
+        indices = list(zip(*np.asarray(self - np.eye(3)).nonzero()))
+        if len(indices) != 1:
+            raise ValueError("One and only one independent deformation"
+                             "must be applied.")
+        return indices[0]
+
+    @property
+    def green_lagrange_strain(self):
+        """
+        calculates the euler-lagrange strain from
+        the deformation gradient
+        """
+        return Strain.from_deformation(self)
+
+    def apply_to_structure(self, structure):
+        """
+        Apply the deformation gradient to a structure.
+        
+        Args:
+            structure (Structure object): the structure object to
+                be modified by the deformation
+        """
+        def_struct = structure.copy()
+        def_struct.modify_lattice(Lattice(np.dot(def_struct.lattice.matrix,
+                                                 self)))
+        return def_struct
+
+    @classmethod
+    def from_index_amount(cls, matrixpos, amt):
+        """
+        Factory method for constructing a Deformation object
+        from a matrix position and amount
+
+        Args:
+            matrixpos (tuple): tuple corresponding the matrix position to
+                have a perturbation added
+            amt (float): amount to add to the identity matrix at position 
+                matrixpos
+        """
+        f = np.identity(3)
+        f[matrixpos] += amt
+        return cls(f)
+
+
+class DeformedStructureSet(object):
+    """
+    class that generates a set of deformed structures that
+    can be used to fit the elastic tensor of a material
+    """
+
+    def __init__(self, rlxd_str, nd=0.01, ns=0.08,
+                 num_norm=4, num_shear=4, symmetry=False):
+        """
+        constructs the deformed geometries of a structure.  Generates
+        m + n deformed structures according to the supplied parameters.
+
+        Args:
+            rlxd_str (structure): structure to undergo deformation, if 
+                fitting elastic tensor is desired, should be a geometry 
+                optimized structure
+            nd (float): maximum perturbation applied to normal deformation
+            ns (float): maximum perturbation applied to shear deformation
+            m (int): number of deformation structures to generate for 
+                normal deformation
+            n (int): number of deformation structures to generate for 
+                shear deformation
+        """
+
+        if num_norm % 2 != 0:
+            raise ValueError("Number of normal deformations (num_norm)"
+                             " must be even.")
+        if num_shear % 2 != 0:
+            raise ValueError("Number of shear deformations (num_shear)"
+                             " must be even.")
+
+        norm_deformations = np.linspace(-nd, nd, num=num_norm + 1)
+        norm_deformations = norm_deformations[norm_deformations.nonzero()]
+        shear_deformations = np.linspace(-ns, ns, num=num_shear + 1)
+        shear_deformations = shear_deformations[shear_deformations.nonzero()]
+
+        self.undeformed_structure = rlxd_str
+        self.deformations = []
+        self.def_structs = []
+        if symmetry:
+            raise NotImplementedError("Symmetry reduction of deformed "
+                                      "structure set not yet implemented")
+        else:
+            self.symmetry = None
+            # Determine normal deformation gradients
+            # Apply normal deformations
+            for ind in [(0, 0), (1, 1), (2, 2)]:
+                for amount in norm_deformations:
+                    defo = Deformation.from_index_amount(ind, amount)
+                    self.deformations.append(defo)
+                    self.def_structs.append(defo.apply_to_structure(rlxd_str))
+
+            # Apply shear deformations 
+            for ind in [(0, 1), (0, 2), (1, 2)]:
+                for amount in shear_deformations:
+                    defo = Deformation.from_index_amount(ind, amount)
+                    self.deformations.append(defo)
+                    self.def_structs.append(defo.apply_to_structure(rlxd_str))
+
+    def __iter__(self):
+        return iter(self.def_structs)
+
+    def as_strain_dict(self):
+        """
+        Returns dictionary of deformed structures indexed by independent
+        strain objects in accordance with legacy behavior of elasticity
+        package
+        """
+        strains = [IndependentStrain(defo) for defo in self.deformations]
+        return dict(zip(strains, self.def_structs))
+
+
+class Strain(SQTensor):
+    """
+    Subclass of SQTensor that describes the Green-Lagrange strain tensor.
+    """
+
+    def __new__(cls, strain_matrix, dfm=None):
+        obj = SQTensor(strain_matrix).view(cls)
+        obj._dfm = dfm
+        if not obj.is_symmetric():
+            raise ValueError("Strain objects must be initialized "
+                             "with a symmetric array-like.")
+        if dfm is None:
+            warnings.warn("Constructing a strain object without a deformation "
+                          "matrix makes many methods unusable.  Use "
+                          "Strain.from_deformation to construct a Strain object"
+                          " from a deformation gradient.")
+        return obj
+
+    def __array_finalize__(self, obj):
+        if obj is None:
+            return
+        self._dfm = getattr(obj, "_dfm", None)
+
+    def __repr__(self):
+        return "Strain({})".format(self.__str__())
+
+    @classmethod
+    def from_deformation(cls, deformation):
+        """
+        constructor that returns a Strain object from a deformation
+        gradient
+
+        Args:
+            deformation (3x3 array-like):
+        """
+        dfm = Deformation(deformation)
+        return cls(0.5 * (np.dot(dfm.T,dfm) - np.eye(3)), dfm)
+
+    @property
+    def deformation_matrix(self):
+        """
+        returns the deformation matrix
+        """
+        return self._dfm
+
+    @property
+    def independent_deformation(self):
+        """
+        determines whether the deformation matrix represents an
+        independent deformation, raises a value error if not.
+        Returns the index of the deformation gradient corresponding
+        to the independent deformation
+        Args: tol
+        """
+        if self._dfm is None:
+            raise ValueError("No deformation matrix supplied "
+                             "for this strain tensor.")
+        return self._dfm.check_independent()
+
+    @property
+    def voigt(self):
+        """
+        translates a strain tensor into a voigt notation vector
+        """
+        return [self[0, 0], self[1, 1], self[2, 2],
+                2. * self[1, 2], 2. * self[0, 2], 2. * self[0, 1]]
+
+
+class IndependentStrain(Strain):
+    """
+    Class for independent strains intended for use with old Materials Project
+    elasticity workflow.  Note that the default constructor constructs from 
+    a deformation matrix, rather than an array representing the strain, to 
+    emulate the legacy behavior.
+    """
+
+    def __new__(cls, deformation_matrix):
+        obj = Strain.from_deformation(deformation_matrix).view(cls)
+        (obj._i, obj._j) = obj.independent_deformation
+        return obj
+
+    def __array_finalize__(self, obj):
+        if obj is None:
+            return
+        self._dfm = getattr(obj, "_dfm", None)
+        self._i = getattr(obj, "_i", None)
+        self._j = getattr(obj, "_j", None)
+
+    def __repr__(self):
+        return "IndependentStrain({})".format(self.__str__())
+ 
+    @property
+    def i(self):
+        return self._i
+
+    @property
+    def j(self):
+        return self._j
---
+++ pymatgen/elasticity/tests/test_strain.py
@@ -0,0 +1,172 @@
+#!/usr/bin/python
+
+import unittest
+import os
+import random
+
+import numpy as np
+from pymatgen.core.lattice import Lattice
+from pymatgen.core.structure import Structure
+from pymatgen.elasticity.strain import Strain, Deformation, DeformedStructureSet,\
+                                    IndependentStrain
+from pymatgen.util.testing import PymatgenTest
+from numpy.testing import *
+import warnings
+import math
+
+class DeformationTest(PymatgenTest):
+    def setUp(self):
+        self.norm_defo = Deformation.from_index_amount((0,0),0.02)
+        self.ind_defo = Deformation.from_index_amount((0,1),0.02)
+        self.non_ind_defo = Deformation([[1.0, 0.02, 0.02],
+                                         [0.0, 1.0, 0.0],
+                                         [0.0, 0.0, 1.0]])
+        lattice = Lattice([[3.8401979337, 0.00, 0.00],
+                           [1.9200989668, 3.3257101909, 0.00],
+                           [0.00, -2.2171384943, 3.1355090603]])
+        self.structure = Structure(lattice, ["Si", "Si"], [[0, 0, 0], 
+                                                           [0.75, 0.5, 0.75]])
+
+    def test_properties(self):
+        # green_lagrange_strain
+        self.assertArrayAlmostEqual(self.ind_defo.green_lagrange_strain,
+                                    [[0., 0.01, 0.],
+                                     [0.01, 0.0002, 0.],
+                                     [0., 0., 0.]])
+        self.assertArrayAlmostEqual(self.non_ind_defo.green_lagrange_strain,
+                                    [[0., 0.01, 0.01],
+                                     [0.01, 0.0002, 0.0002],
+                                     [0.01, 0.0002, 0.0002]])
+
+    def test_check_independent(self):
+        self.assertRaises(ValueError,self.non_ind_defo.check_independent)
+        self.assertEqual(self.ind_defo.check_independent(),(0,1))
+
+    def test_apply_to_structure(self):
+        strained_norm = self.norm_defo.apply_to_structure(self.structure)
+        strained_ind = self.ind_defo.apply_to_structure(self.structure)
+        strained_non = self.non_ind_defo.apply_to_structure(self.structure)
+        # Check lattices
+        self.assertArrayAlmostEqual(strained_norm.lattice.matrix,
+                                    [[3.9170018886, 0, 0],
+                                     [1.958500946136, 3.32571019, 0],
+                                     [0, -2.21713849,3.13550906]])
+        self.assertArrayAlmostEqual(strained_ind.lattice.matrix,
+                                    [[3.84019793, 0.07680396, 0],
+                                     [1.92009897, 3.36411217, 0],
+                                     [0, -2.21713849,3.13550906]])
+        self.assertArrayAlmostEqual(strained_non.lattice.matrix,
+                                    [[3.84019793, 0.07680396, 0.07680396],
+                                     [1.92009897, 3.36411217, 0.0384019794],
+                                     [0, -2.21713849,3.13550906]])
+        # Check coordinates
+        self.assertArrayAlmostEqual(strained_norm.sites[1].coords,
+                                    [3.91700189, 1.224e-06, 2.3516318])
+        self.assertArrayAlmostEqual(strained_ind.sites[1].coords,
+                                    [3.84019793, 0.07680518, 2.3516318])
+        self.assertArrayAlmostEqual(strained_non.sites[1].coords,
+                                    [3.84019793, 0.07680518, 2.42843575])
+
+class StrainTest(PymatgenTest):
+    def setUp(self):
+        self.norm_str = Strain.from_deformation([[1.02, 0, 0],
+                                                 [0, 1, 0],
+                                                 [0, 0, 1]])
+        self.ind_str = Strain.from_deformation([[1, 0.02, 0],
+                                                [0, 1, 0],
+                                                [0, 0, 1]])
+        
+        self.non_ind_str = Strain.from_deformation([[1, 0.02, 0.02],
+                                                    [0, 1, 0],
+                                                    [0, 0, 1]])
+        
+        with warnings.catch_warnings(record=True):
+            warnings.simplefilter("always")
+            self.no_dfm = Strain([[0., 0.01, 0.],
+                                  [0.01, 0.0002, 0.],
+                                  [0., 0., 0.]])
+
+    def test_new(self):
+        # test warning for constructing Strain without defo. matrix
+        with warnings.catch_warnings(record=True) as w:
+            Strain([[0., 0.01, 0.],
+                    [0.01, 0.0002, 0.],
+                    [0., 0., 0.]])
+            self.assertEqual(len(w),1)
+        self.assertRaises(ValueError,Strain,[[0.1, 0.1, 0],
+                                             [0, 0, 0],
+                                             [0, 0, 0]])
+    def test_from_deformation(self):
+        self.assertArrayAlmostEqual(self.norm_str,
+                                    [[0.0202, 0, 0],
+                                     [0, 0, 0],
+                                     [0, 0, 0]])
+        self.assertArrayAlmostEqual(self.ind_str,
+                                    [[0., 0.01, 0.],
+                                     [0.01, 0.0002, 0.],
+                                     [0., 0., 0.]])
+        self.assertArrayAlmostEqual(self.non_ind_str,
+                                    [[0., 0.01, 0.01],
+                                     [0.01, 0.0002, 0.0002],
+                                     [0.01, 0.0002, 0.0002]])
+
+    def test_properties(self):
+        # deformation matrix
+        self.assertArrayAlmostEqual(self.ind_str.deformation_matrix,
+                                    [[1, 0.02, 0],
+                                     [0, 1, 0],
+                                     [0, 0, 1]])
+        self.assertIsNone(self.no_dfm.deformation_matrix)
+        
+        # independent deformation
+        self.assertArrayEqual(self.ind_str.independent_deformation,(0,1))
+        with self.assertRaises(ValueError):
+            self.no_dfm.independent_deformation
+        
+        #voigt
+        self.assertArrayAlmostEqual(self.non_ind_str.voigt,
+                                    [0, 0.0002, 0.0002, 0.0004, 0.02, 0.02])
+
+class DeformedStructureSetTest(PymatgenTest):
+    def setUp(self):
+        lattice = Lattice([[3.8401979337, 0.00, 0.00],
+                           [1.9200989668, 3.3257101909, 0.00],
+                           [0.00, -2.2171384943, 3.1355090603]])
+        self.structure = Structure(lattice, ["Si", "Si"], [[0, 0, 0], 
+                                                           [0.75, 0.5, 0.75]])
+        self.default_dss = DeformedStructureSet(self.structure)
+    
+    def test_init(self):
+        with self.assertRaises(ValueError):
+            DeformedStructureSet(self.structure,num_norm=5)
+            DeformedStructureSet(self.structure,num_shear=5)
+        self.assertEqual(self.structure,self.default_dss.undeformed_structure)
+
+    def test_as_strain_dict(self):
+        strain_dict = self.default_dss.as_strain_dict()
+        for i, def_struct in enumerate(self.default_dss):
+            test_strain = IndependentStrain(self.default_dss.deformations[i])
+            strain_keys = [strain for strain in strain_dict.keys() 
+                           if (strain == test_strain).all()]
+            self.assertEqual(len(strain_keys),1)
+            self.assertEqual(self.default_dss.def_structs[i],
+                             strain_dict[strain_keys[0]])
+
+class IndependentStrainTest(PymatgenTest):
+    def setUp(self):
+        self.ind_strain = IndependentStrain([[1, 0.1, 0],
+                                             [0, 1, 0],
+                                             [0, 0, 1]])
+
+    def test_new(self):
+        with self.assertRaises(ValueError):
+            IndependentStrain([[0.1, 0.1, 0],
+                               [0, 0, 0],
+                               [0, 0, 0]])
+
+    def test_properties(self):
+        self.assertEqual(self.ind_strain.i,0)
+        self.assertEqual(self.ind_strain.j,1)
+
+if __name__ == '__main__':
+    unittest.main()
