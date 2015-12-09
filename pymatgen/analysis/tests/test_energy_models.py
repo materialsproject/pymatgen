21104edb67bc6762f273545434868706289e8d46
__sp__ <ongsp@eng.ucsd.edu>
2015-12-08 22:11:54 -0800

    Fix unittests.
    Former-commit-id: ee71073e39769cd0dfae90709330eb16ee255e13 [formerly d540f4d4db0a717377dcb05ad6f01775a4c29986]
    Former-commit-id: 431908cb33ff5c3fd120ed496e5be4bd53536dc2

--- pymatgen/analysis/tests/test_energy_models.py
+++ pymatgen/analysis/tests/test_energy_models.py
@@ -44,9 +44,9 @@ class EwaldElectrostaticModelTest(unittest.TestCase):
                        {"Si4+": 0.5, "O2-": 0.25, "P5+": 0.25}], coords)
 
         m = EwaldElectrostaticModel()
-        self.assertAlmostEqual(m.get_energy(s), 44.1070954178)
+        self.assertAlmostEqual(m.get_energy(s), -39.90013347055681)
         s2 = Structure.from_file(os.path.join(test_dir, "Li2O.cif"))
-        self.assertAlmostEqual(m.get_energy(s2), -145.39049924661001)
+        self.assertAlmostEqual(m.get_energy(s2), -145.39050015844839)
 
     def test_to_from_dict(self):
         m = EwaldElectrostaticModel()
