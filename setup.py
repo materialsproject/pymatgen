632d36c538ff92b46bc89e23609fd314c1fe37b2
__sp__ <ongsp@eng.ucsd.edu>
2016-01-15 12:43:21 -0800

    Minor fix to setup.py.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ def get_spglib_ext():
         extra_compile_args=c_opt)
 
 
-with open("README.rst") as f:
+with open(os.path.join(SETUP_PTH, "README.rst")) as f:
     long_desc = f.read()
     ind = long_desc.find("\n")
     long_desc = long_desc[ind + 1:]
