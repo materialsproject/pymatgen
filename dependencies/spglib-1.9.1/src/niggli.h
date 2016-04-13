62149e9e61249484ee54ceb1181f50ace2c847cb
__sp__ <ongsp@eng.ucsd.edu>
2016-04-12 20:23:43 -0700

    Move to spglib1.9.1.
    Former-commit-id: f47a10fd2a59876585e90417565cccdc09488ba6 [formerly c96c68337c3fdd30f043920c2316f83297337c92]
    Former-commit-id: f65035fb1cf3ac3de3b08f83e5654342ff0a979b

---
+++ dependencies/spglib-1.9.1/src/niggli.h
@@ -0,0 +1,47 @@
+/* Copyright (C) 2015 Atsushi Togo */
+/* All rights reserved. */
+
+/* This file is part of niggli. */
+
+/* Redistribution and use in source and binary forms, with or without */
+/* modification, are permitted provided that the following conditions */
+/* are met: */
+
+/* * Redistributions of source code must retain the above copyright */
+/*   notice, this list of conditions and the following disclaimer. */
+
+/* * Redistributions in binary form must reproduce the above copyright */
+/*   notice, this list of conditions and the following disclaimer in */
+/*   the documentation and/or other materials provided with the */
+/*   distribution. */
+
+/* * Neither the name of the phonopy project nor the names of its */
+/*   contributors may be used to endorse or promote products derived */
+/*   from this software without specific prior written permission. */
+
+/* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS */
+/* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT */
+/* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS */
+/* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE */
+/* COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, */
+/* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, */
+/* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; */
+/* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER */
+/* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT */
+/* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN */
+/* ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE */
+/* POSSIBILITY OF SUCH DAMAGE. */
+
+#ifndef __NIGGLI_H__
+#define __NIGGLI_H__
+
+#define NIGGLI_MAJOR_VERSION 0
+#define NIGGLI_MINOR_VERSION 1
+#define NIGGLI_MICRO_VERSION 2
+
+int niggli_get_major_version(void);
+int niggli_get_minor_version(void);
+int niggli_get_micro_version(void);
+int niggli_reduce(double *lattice_, const double eps_);
+
+#endif
