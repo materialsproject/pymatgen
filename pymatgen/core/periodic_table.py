1f5044c8fc960cffeb22d659c51baf7d87451a07
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-04-15 13:37:35 -0700

    remove paragmagnetic labels; take max element coordination from defaultdict()
    Former-commit-id: 44bc2cc4ea97b1956195ede148b8ff589fd03a42 [formerly 8540aad8b260c1f6310894c8bf8aea9819a01d6c]
    Former-commit-id: acf5f96283c0d78383018edae60ac65efb581c01

--- pymatgen/core/periodic_table.py
+++ pymatgen/core/periodic_table.py
@@ -399,7 +399,8 @@ class Element(Enum):
                 try:
                     val = float(val)
                 except ValueError:
-                    toks = val.replace("about", "").strip().split(" ", 1)
+                    toks_nobrackets = re.sub(r'\(.*\)', "", val)
+                    toks = toks_nobrackets.replace("about", "").strip().split(" ", 1)
                     if len(toks) == 2:
                         try:
                             unit = toks[1].replace("<sup>", "^").replace(
