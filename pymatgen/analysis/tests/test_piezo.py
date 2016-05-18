520ebbea003ce82ef07bfbba4363c01a81611f56
Joseph Montoya <montoyjh@gmail.com>
2016-05-18 13:22:00 -0700

    changed piezo code to subclass tensorbase, began rewriting unittests

--- pymatgen/analysis/tests/test_piezo.py
+++ pymatgen/analysis/tests/test_piezo.py
@@ -27,37 +27,42 @@ test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..",
 
 
 class PiezoTest(PymatgenTest):
+    def setUp(self):
+        self.piezo_struc = self.get_structure('BaNiO3')
+        self.voigt_matrix = np.array([[0., 0., 0., 0., 0.03839, 0.],
+                                      [0., 0., 0., 0.03839, 0., 0.],
+                                      [6.89822, 6.89822, 27.46280, 0., 0., 0.]])
+        self.full_tensor_array = [[[0., 0., 0.03839],
+                                   [0., 0., 0.],
+                                   [0.03839, 0., 0.]],
+                                  [[0., 0., 0.],
+                                   [0., 0., 0.03839],
+                                   [0.,  0.03839,  0.]],
+                                  [[6.89822, 0., 0.],
+                                   [0.,  6.89822, 0.],
+                                   [0.,  0.,  27.4628]]]
+    def test_new(self):
+        pt = PiezoTensor(self.full_tensor_array)
+        self.assertArrayAlmostEqual(pt, self.full_tensor_array)
+        bad_dim_array = np.zeros((3, 3))
+        self.assertRaises(ValueError, PiezoTensor, bad_dim_array)
 
-    def runTest(self):
+        bad_pt = PiezoTensor.from_voigt([[0., 0., 1., 0., 0.03839, 2.],
+                                         [0., 0., 0., 0.03839, 0., 0.],
+                                         [6.89822, 6.89822, 27.4628, 0., 0., 0.]])
 
-        piezo_struc = self.get_structure('BaNiO3')
-        tensor = np.asarray([[0., 0., 0., 0., 0.03839, 0.],
-                             [0., 0., 0., 0.03839, 0., 0.],
-                             [6.89822, 6.89822, 27.46280, 0., 0., 0.]]).reshape(3, 6)
-        pt = PiezoTensor(tensor)
-        full_tensor = [[[0., 0., 0.03839],
-                        [0., 0., 0.],
-                        [0.03839, 0., 0.]],
-                       [[0., 0., 0.],
-                        [0., 0., 0.03839],
-                        [0.,  0.03839,  0.]],
-                       [[6.89822, 0., 0.],
-                        [0.,  6.89822, 0.],
-                        [0.,  0.,  27.4628]]]
-        bad_pt = PiezoTensor([[0., 0., 1., 0., 0.03839, 2.],
-                              [0., 0., 0., 0.03839, 0., 0.],
-                              [6.89822, 6.89822, 27.4628, 0., 0., 0.]])
+        sym_pt = bad_pt.symmeterized(piezo_struc)
+        alt_tensor = pt(full_tensor)
 
-        sym_pt = bad_pt.symmeterize(piezo_struc)
-        alt_tensor = pt.from_full_tensor(full_tensor)
-
-        self.assertArrayAlmostEqual(pt.full_tensor, full_tensor)
+        self.assertArrayAlmostEqual(pt.voigt, full_tensor)
         self.assertArrayEqual(pt, alt_tensor)
         #TODO Recheck this test. Commented out for now to enable Py3k testing.
-        self.assertTrue(pt.is_valid(piezo_struc))
+        self.assertTrue(pt.is_fit_to_structure(piezo_struc))
 
-        self.assertTrue(sym_pt.is_valid(piezo_struc))
+        self.assertTrue(sym_pt.is_fit_to_structure(piezo_struc))
         self.assertArrayAlmostEqual(sym_pt, pt)
+
+    def test_from_voigt(self):
 
 if __name__ == '__main__':
     unittest.main()
