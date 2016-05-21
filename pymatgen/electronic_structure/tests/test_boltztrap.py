45d3427f97a28af4737ee0cf951fdb2b976c5b14
__sp__ <ongsp@eng.ucsd.edu>
2016-05-21 09:45:14 -0700

    Fix spin test.
    Former-commit-id: 8e46369a488467d0f4a32a85351a53b818441fe1 [formerly 063d9498925e22b7dbb10741db2ea077b545c9c1]
    Former-commit-id: 5716a4c44a92fec6480e9bd5e3bb367898e46906

--- pymatgen/electronic_structure/tests/test_boltztrap.py
+++ pymatgen/electronic_structure/tests/test_boltztrap.py
@@ -172,8 +172,8 @@ class BoltztrapAnalyzerTest(unittest.TestCase):
         structure = loadfn(os.path.join(test_dir,'boltztrap/structure_mp-12103.json'))
         cdos = self.bz_up.get_complete_dos(structure,self.bz_dw)
         spins = list(cdos.densities.keys())
-        self.assertIs(spins[0], Spin.down)
-        self.assertIs(spins[1], Spin.up)
+        self.assertIn(Spin.down, spins)
+        self.assertIn(Spin.up, spins)
         self.assertAlmostEqual(cdos.get_spd_dos()[OrbitalType.p].densities[Spin.up][3134],43.839230100999991)
         self.assertAlmostEqual(cdos.get_spd_dos()[OrbitalType.s].densities[Spin.down][716],6.5383268000000001)
         
