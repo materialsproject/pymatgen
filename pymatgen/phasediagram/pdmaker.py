9628b20b8bf00ea62afaa668525675671e847e08
__sp__ <ongsp@eng.ucsd.edu>
2016-06-13 08:26:00 -0700

    Remove deprecated PD.

--- pymatgen/phasediagram/pdmaker.py
+++
@@ -1,4 +0,0 @@
-import warnings
-warnings.warn("pymatgen.phasediagram.pdmaker has been moved pymatgen.phasediagram.maker. This stub "
-              "will be removed in pymatgen 4.0.", DeprecationWarning)
-from .maker import *
