016970657f7e9f6944d23ac4c6e0869c9dbdd448
__sp__ <shyuep@gmail.com>
2016-07-03 07:18:48 +0800

    Remove bad rotate_tensor.

--- pymatgen/analysis/elasticity/tensors.py
+++ pymatgen/analysis/elasticity/tensors.py
@@ -116,26 +116,6 @@ class TensorBase(np.ndarray):
                                                    [0., 0., 0.])
         return self.transform(sop)
 
-    def rotate_tensor(self, initial_struct, final_struct):
-        """ 
-        Applies a transformation on tensor and returns the tensor in the final
-        basis set given a initial and final structure files
-        """
-
-        initialsg = SpacegroupAnalyzer(initial_struct)
-        finalsg = SpacegroupAnalyzer(final_struct)
-
-        rotationinitial = initialsg.get_symmetry_dataset()['transformation_matrix']
-        rotationfinal = finalsg.get_symmetry_dataset()['transformation_matrix']
-
-        transmatrix = np.dot(rotationinitial, np.linalg.inv(rotationfinal)).T
-        import scipy
-        U = scipy.linalg.sqrtm(np.dot(transmatrix.T, transmatrix))
-        R = np.dot(transmatrix, np.linalg.inv(U))
-
-        return self.rotate(R)
-
-
     @property
     def symmetrized(self):
         """
