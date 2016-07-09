b38a20b0f6665525617871609e2db2c05367d91f
__sp__ <ongsp@eng.ucsd.edu>
2016-07-08 23:59:13 -0700

    v4.0.1 release

--- pymatgen/__init__.py
+++ pymatgen/__init__.py
@@ -4,8 +4,8 @@ __author__ = "Pymatgen Development Team"
 __email__ ="pymatgen@googlegroups.com"
 __maintainer__ = "Shyue Ping Ong"
 __maintainer_email__ ="shyuep@gmail.com"
-__date__ = "Jul 1 2016"
-__version__ = "4.0.0"
+__date__ = "Jul 8 2016"
+__version__ = "4.0.1"
 
 
 # Useful aliases for commonly used objects and modules.
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open(os.path.join(SETUP_PTH, "README.rst")) as f:
 setup(
     name="pymatgen",
     packages=find_packages(),
-    version="4.0.0",
+    version="4.0.1",
     install_requires=["numpy>=1.9", "six", "atomicfile", "requests",
                       "pybtex", "pyyaml", "monty>=0.7.0", "scipy>=0.14",
                       "tabulate", "enum34", "spglib"],
