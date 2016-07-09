3464ca03a060bee6e60288b9ab9e0ff7067c3542
Liam Deacon <liam.m.deacon@gmail.com>
2016-07-09 13:25:07 +0100

    Added fallback to pure-python coord_utils backend

--- pymatgen/util/__init__.py
+++ pymatgen/util/__init__.py
@@ -11,3 +11,8 @@ packages.
 
 __author__ = "Shyue"
 __date__ = "$Jun 6, 2011 7:30:05 AM$"
+
+try:
+  import coord_util_cython
+except ImportError:
+  import coord_util_python as coord_util_cython  # use pure-python fallback
