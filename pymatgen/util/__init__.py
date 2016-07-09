9260ae587c46f32047dbcfbe1610290282fcdf8c
__sp__ <ongsp@eng.ucsd.edu>
2016-07-09 06:12:09 -0700

    Fix spelling in coord_utils import.
    Former-commit-id: 03182f09c5e7cc2d7a5ca8b996baa82e4557e7fa [formerly 4011394beda266f4f43120e951b99bdc0470f93e]
    Former-commit-id: 78985fde51ef15145a5ff42d8bd9fe05a8890b22

--- pymatgen/util/__init__.py
+++ pymatgen/util/__init__.py
@@ -13,6 +13,6 @@ __author__ = "Shyue"
 __date__ = "$Jun 6, 2011 7:30:05 AM$"
 
 try:
-  import coord_util_cython
+  import coord_utils_cython
 except ImportError:
-  import coord_util_python as coord_util_cython  # use pure-python fallback
+  import coord_utils_python as coord_utils_cython  # use pure-python fallback
