ab8243be0f9b28e2efbfb2c39c754de56dc44aba
__sp__ <ongsp@eng.ucsd.edu>
2016-07-09 06:04:16 -0700

    Fix import
    Former-commit-id: 9e5937ee37540b7f465c7cfde497bd38818a0a55 [formerly 399bc4263974f44265d1d58f55375e7f81124191]
    Former-commit-id: 13185557c44a8c7fcfbc66ec02d53a88add5c44d

--- pymatgen/optimization/__init__.py
+++ pymatgen/optimization/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (c) Pymatgen Development Team.
 # Distributed under the terms of the MIT License.
 
-try
+try:
   import linear_assignment
 except ImportError:
   import linear_assignment_python as linear_assignment  # use pure-python fallback
