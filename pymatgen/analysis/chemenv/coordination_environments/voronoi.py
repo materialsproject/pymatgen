c536c87f0ad6dcfb5050671c8d5e6d045438ffbb
David Waroquiers <david.waroquiers@uclouvain.be>
2016-07-14 14:11:50 +0200

    Checking that the structures and voronois are the same when compute_structure_environments is performed in "recompute" mode of an existing StructureEnvironments object. "is_close_to" comparison of DetailedVoronoi (two voronoi obtained on two different machines can have numerical differences ...).

--- pymatgen/analysis/chemenv/coordination_environments/voronoi.py
+++ pymatgen/analysis/chemenv/coordination_environments/voronoi.py
@@ -951,6 +951,52 @@ class DetailedVoronoiContainer(MSONable):
     # def unique_coordinated_neighbors_parameters_indices(self, isite):
     #     return self._unique_coordinated_neighbors_parameters_indices[isite]
 
+    def is_close_to(self, other, rtol=0.0, atol=1e-8):
+        isclose = (np.isclose(self.normalized_angle_tolerance, other.normalized_angle_tolerance,
+                              rtol=rtol, atol=atol) and
+                   np.isclose(self.normalized_distance_tolerance, other.normalized_distance_tolerance,
+                              rtol=rtol, atol=atol) and
+                   self.additional_conditions == other.additional_conditions and
+                   self.valences == other.valences)
+        if not isclose:
+            return isclose
+        for isite, site_voronoi in enumerate(self.voronoi_list2):
+            self_to_other_nbs = {}
+            for inb, nb in enumerate(site_voronoi):
+                if nb is None:
+                    if other.voronoi_list2[isite] is None:
+                        continue
+                    else:
+                        return False
+                else:
+                    if other.voronoi_list2[isite] is None:
+                        return False
+                nb_other = None
+                for inb2, nb2 in enumerate(other.voronoi_list2[isite]):
+                    if nb['site'] == nb2['site']:
+                        self_to_other_nbs[inb] = inb2
+                        nb_other = nb2
+                        break
+                if nb_other is None:
+                    return False
+                if not np.isclose(nb['distance'], nb_other['distance'],
+                                  rtol=rtol, atol=atol):
+                    return False
+                if not np.isclose(nb['angle'], nb_other['angle'],
+                                  rtol=rtol, atol=atol):
+                    return False
+                if not np.isclose(nb['normalized_distance'], nb_other['normalized_distance'],
+                                  rtol=rtol, atol=atol):
+                    return False
+                if not np.isclose(nb['normalized_angle'], nb_other['normalized_angle'],
+                                  rtol=rtol, atol=atol):
+                    return False
+                if nb['index'] != nb_other['index']:
+                    return False
+                if nb['site'] != nb_other['site']:
+                    return False
+        return True
+
     def __eq__(self, other):
         return (self.normalized_angle_tolerance == other.normalized_angle_tolerance and
                 self.normalized_distance_tolerance == other.normalized_distance_tolerance and
