bc858f2d7dba4824ccd0a6cdb4975ad7d77920d3
__sp__ <ongsp@eng.ucsd.edu>
2016-07-26 06:50:55 -0700

    Improved MIC algo that supports more skewed cells by taking into account # of images.
    Former-commit-id: 576885eb6663a57c1193db734312262b0febaec1 [formerly a7d5d8a967fbdf1795524d5e515185f452eb83f4]
    Former-commit-id: 51ab992c29e80691f40ba8363b91c957feba47ec

--- pymatgen/core/lattice.py
+++ pymatgen/core/lattice.py
@@ -1098,12 +1098,13 @@ class Lattice(MSONable):
         cutoff = min(d, max(self._diags) / 2)
         n = np.array(np.ceil(cutoff * np.prod(self._lengths) /
                              (self.volume * self._lengths)), dtype=int)
-        if any(n > 50):
+        nimages = np.prod(n) # Total number of images / 8, approximately.
+        if nimages > 200000:
+            n_new = np.ceil(200000 / nimages * n)
             warnings.warn("Cell is highly skewed and requires a search "
                           "through image range of +- %s. For efficiency, "
-                          "we will limit the search to +-50 images in each "
-                          "direction. Recommend you do a niggli or LLL "
-                          "reduction of the cell before computing distances"
-                          % n)
-            n = np.minimum(n, 50)
+                          "we will limit the search to %s images. Recommend "
+                          "you do a niggli or LLL reduction of the cell "
+                          "before computing distances" % (n, n_new))
+            n = n_new.astype(dtype=np.int)
         return n
