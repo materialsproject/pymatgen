2b2c0f9dbf5b27227edd1f305ca16d047e875b15
__sp__ <ongsp@eng.ucsd.edu>
2016-09-19 05:51:03 -0700

    Minor refactor of pmg scripts.
    Former-commit-id: e2ed8d9d2b5069f058d44242cde5f5a75a8f6ece [formerly 2c2b9de566ded05e20e1b4272f1ec43339ed6da1]
    Former-commit-id: 474be704e501206fa59561f8c37c3c4f43644668

--- pymatgen/cli/pmg.py
+++ pymatgen/cli/pmg.py
@@ -21,7 +21,7 @@ from pymatgen.io.vasp import Incar
 
 from pymatgen.cli.pmg_analyze import parse_vasp
 from pymatgen.cli.pmg_config import configure_pmg
-from pymatgen.cli.pmg_generate_potcar import generate_potcar
+from pymatgen.cli.pmg_potcar import generate_potcar
 from pymatgen.cli.pmg_plot import plot
 from pymatgen.cli.pmg_structure import analyze_structures
 from pymatgen.cli.pmg_query import do_query
