ccc9cb178acb5a24a8d780ecad6915a9e348e6dd
__sp__ <ongsp@eng.ucsd.edu>
2016-10-19 14:55:33 -0700

    More improvements in batch script.

--- dev_scripts/build_conda.bat
+++ dev_scripts/build_conda.bat
@@ -1,3 +1,7 @@
+MKDIR tmpconda
+cd tmpconda
 FOR %%A IN (latexcodec tabulate monty pybtex palettable spglib pydispatcher pymatgen) DO conda skeleton pypi %%A
 FOR %%A IN (latexcodec tabulate monty pybtex palettable spglib pydispatcher pymatgen) DO conda build %%A
 FOR %%A IN (latexcodec tabulate monty pybtex palettable spglib pydispatcher pymatgen) DO anaconda upload %HOMEPATH%\Miniconda3\win-64\%AA-*py35*.tar.bz2
+cd ..
+RMDIR /s /q tmpconda
