6e126c91dce508ee5236a2c0243e09c371049af6
__sp__ <ongsp@eng.ucsd.edu>
2016-10-19 14:52:52 -0700

    Conda bat file for Windows.

--- dev_scripts/build_conda.bat
+++ dev_scripts/build_conda.bat
@@ -1,2 +1,3 @@
-FOR %A IN (latexcodec tabulate monty pybtex palettable spglib pydispatcher pymatgen) DO conda skeleton pypi %A
-FOR %A IN (latexcodec tabulate monty pybtex palettable spglib pydispatcher pymatgen) DO conda build %A
+FOR %%A IN (latexcodec tabulate monty pybtex palettable spglib pydispatcher pymatgen) DO conda skeleton pypi %%A
+FOR %%A IN (latexcodec tabulate monty pybtex palettable spglib pydispatcher pymatgen) DO conda build %%A
+FOR %%A IN (latexcodec tabulate monty pybtex palettable spglib pydispatcher pymatgen) DO anaconda upload %HOMEPATH%\Miniconda3\win-64\%AA-*py35*.tar.bz2
