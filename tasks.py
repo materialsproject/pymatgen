fef16c49cc264685fa7c0af8b713206953738932
__sp__ <ongsp@eng.ucsd.edu>
2016-10-19 15:27:10 -0700

    Add osx and linux tasks.

--- tasks.py
+++ tasks.py
@@ -164,13 +164,40 @@ def log_ver(ctx):
 
 
 @task
-def build_conda(ctx):
+def build_conda_osx(ctx):
     with ScratchDir(".") as d:
         for pkg in ["latexcodec", "tabulate", "monty", "pybtex", "palettable",
                     "spglib", "pydispatcher", "pymatgen"]:
             ctx.run("conda skeleton pypi %s" % pkg)
             ctx.run("conda build %s" % pkg)
-            ctx.run("anaconda upload --label mavrl --force $HOME/miniconda3/conda-bld/osx-64/%s-*py35*.tar.bz2" %
+            ctx.run("anaconda upload --force $HOME/miniconda3/conda-bld/osx-64/%s-*py35*.tar.bz2" %
+                    pkg)
+
+    with ScratchDir(".") as d:
+        for pkg in ["latexcodec", "tabulate", "monty", "pybtex", "palettable",
+                    "spglib", "pydispatcher", "pymatgen"]:
+            ctx.run("conda skeleton pypi --python-version 2.7 %s" % pkg)
+            ctx.run("conda build --python 2.7 %s" % pkg)
+            ctx.run("anaconda upload --force $HOME/miniconda3/conda-bld/osx-64/%s-*py27*.tar.bz2" %
+                    pkg)
+
+
+@task
+def build_conda_linux64(ctx):
+    with ScratchDir(".") as d:
+        for pkg in ["latexcodec", "tabulate", "monty", "pybtex", "palettable",
+                    "spglib", "pydispatcher", "pymatgen"]:
+            ctx.run("conda skeleton pypi %s" % pkg)
+            ctx.run("conda build %s" % pkg)
+            ctx.run("anaconda upload --force $HOME/miniconda3/conda-bld/osx-64/%s-*py35*.tar.bz2" %
+                    pkg)
+
+    with ScratchDir(".") as d:
+        for pkg in ["latexcodec", "tabulate", "monty", "pybtex", "palettable",
+                    "spglib", "pydispatcher", "pymatgen"]:
+            ctx.run("conda skeleton pypi --python-version 2.7 %s" % pkg)
+            ctx.run("conda build --python 2.7 %s" % pkg)
+            ctx.run("anaconda upload --force $HOME/miniconda3/conda-bld/osx-64/%s-*py27*.tar.bz2" %
                     pkg)
 
 
