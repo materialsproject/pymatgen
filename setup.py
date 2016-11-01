765b156d7fe02117626def7f8d7cadc8a195d023
__sp__ <ongsp@eng.ucsd.edu>
2016-11-01 10:01:57 -0700

    Fix build for numpy.

--- setup.py
+++ setup.py
@@ -16,8 +16,19 @@ class build_ext(_build_ext):
     def finalize_options(self):
         _build_ext.finalize_options(self)
         # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
+        if sys.version_info[0] >= 3:
+            import builtins
+            if hasattr(builtins, '__NUMPY_SETUP__'):
+                del builtins.__NUMPY_SETUP__
+
+        else:
+            import __builtin__
+            if hasattr(__builtin__, '__NUMPY_SETUP__'):
+                del __builtin__.__NUMPY_SETUP__
+        import importlib
         import numpy
+        importlib.reload(numpy)
+
         self.include_dirs.append(numpy.get_include())
 
 SETUP_PTH = os.path.dirname(__file__)
