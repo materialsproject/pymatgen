a9e224902805501534ef91733d244f41350343bc
__sp__ <ongsp@eng.ucsd.edu>
2016-11-24 04:57:16 +0800

    Remove build conda.

--- dev_scripts/build_conda.bat
+++
@@ -1,7 +0,0 @@
-MKDIR tmpconda
-cd tmpconda
-FOR %%A IN (latexcodec tabulate monty pybtex palettable spglib pydispatcher pymatgen) DO conda skeleton pypi %%A
-FOR %%A IN (latexcodec tabulate monty pybtex palettable spglib pydispatcher pymatgen) DO conda build %%A
-FOR %%A IN (latexcodec tabulate monty pybtex palettable spglib pydispatcher pymatgen) DO anaconda upload %HOMEPATH%\Miniconda3\win-64\%AA-*py35*.tar.bz2
-cd ..
-RMDIR /s /q tmpconda
