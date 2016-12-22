ba709d051eff44a28df378dfe2eee021762b9142
Anubhav Jain <ajain@lbl.gov>
2016-12-21 16:40:20 -0800

    test old format parsing

--- pymatgen/electronic_structure/tests/test_bandstructure.py
+++ pymatgen/electronic_structure/tests/test_bandstructure.py
@@ -168,5 +168,13 @@ class BandStructureSymmLine_test(PymatgenTest):
         s = json.dumps(self.bs_spin.as_dict())
         self.assertIsNotNone(s)
 
+    def test_old_format_load(self):
+        with open(os.path.join(test_dir, "bs_ZnS_old.json"),
+                  "r", encoding='utf-8') as f:
+            d = json.load(f)
+            bs_old = BandStructureSymmLine.from_dict(d)
+            self.assertEqual(bs_old.get_projection_on_elements()[
+                                 Spin.up][0][0]['Zn'], 0.0971)
+
 if __name__ == '__main__':
     unittest.main()
