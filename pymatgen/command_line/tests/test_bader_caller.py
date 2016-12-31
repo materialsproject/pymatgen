3acf55de66efcbe60cdf6c398f4711d3a79f211e
__sp__ <ongsp@eng.ucsd.edu>
2016-12-31 06:48:28 -0800

    Proper significant places for bader test.

--- pymatgen/command_line/tests/test_bader_caller.py
+++ pymatgen/command_line/tests/test_bader_caller.py
@@ -32,15 +32,15 @@ class BaderAnalysisTest(unittest.TestCase):
         analysis = BaderAnalysis(os.path.join(test_dir, "CHGCAR.Fe3O4"),
                                  os.path.join(test_dir, "POTCAR.Fe3O4"))
         self.assertEqual(len(analysis.data), 14)
-        self.assertAlmostEqual(analysis.data[0]["charge"], 6.1485)
+        self.assertAlmostEqual(analysis.data[0]["charge"], 6.1485297, 3)
         self.assertAlmostEqual(analysis.nelectrons, 96)
         self.assertAlmostEqual(analysis.vacuum_charge, 0)
         ans = [-1.8515, -1.8132, -1.8132, -1.5562, -1.8132, -1.8515, 1.3369,
                1.3378, 1.3373, 1.3374, 1.3374, 1.3369, 1.3373, 1.3378]
         for i in range(14):
-            self.assertAlmostEqual(ans[i], analysis.get_charge_transfer(i))
+            self.assertAlmostEqual(ans[i], analysis.get_charge_transfer(i), 3)
         s = analysis.get_oxidation_state_decorated_structure()
-        self.assertAlmostEqual(s[0].specie.oxi_state, -1.8515)
+        self.assertAlmostEqual(s[0].specie.oxi_state, -1.8515, 3)
 
 
 if __name__ == '__main__':
