234f1c32c7eadc28a1c9583d740467e3f9d51987
montoyjh <montoyjh@gmail.com>
2017-02-22 13:56:50 -0800

    cleanup tests

--- pymatgen/analysis/tests/test_adsorption.py
+++ pymatgen/analysis/tests/test_adsorption.py
@@ -79,24 +79,5 @@ class AdsorbateSiteFinderTest(PymatgenTest):
                                     frac_to_cart(slab.lattice,
                                                  slab.frac_coords[0]))
 
-if __name__ == '__main__':
-    """
-    from pymatgen import MPRester
-    from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
-    mpr = MPRester()
-    struct = mpr.get_structure_by_material_id("mp-18759")
-    conv = SpacegroupAnalyzer(struct).get_conventional_standard_structure()
-    slabs = generate_all_slabs(conv, min_slab_size=8.0, min_vacuum_size=10.0, max_index=1,
-            center_slab=True)
-    slabs = [slab for slab in slabs if slab.miller_index==(1, 0, 0)]
-    for n, slab in enumerate(slabs):
-        #import pdb; pdb.set_trace()
-        slab.to(filename="POSCAR_{}".format(n))
-    slab_dict = {slab.miller_index: slab for slab in slabs}
-    slab_dict[(1, 0, 0)].to(filename="POSCAR_bare_slab")
-    asf = AdsorbateSiteFinder(slab_dict[(1, 0, 0)], height=1.2)
-    asf.get_extended_surface_mesh().to(filename="POSCAR_surf_mesh")
-    Structure.from_sites(asf.surface_sites).to(filename="POSCAR_surf_sites")
-    sites = asf.find_adsorption_sites()
-    """
+if __name__ == '__main__': 
     unittest.main()
