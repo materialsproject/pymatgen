35a7a41142f0c8011a2edf3cff929b120cf7c385
montoyjh <montoyjh@gmail.com>
2017-02-27 12:10:26 -0800

    fixed reverted changes from TOEC

--- pymatgen/analysis/elasticity/tensors.py
+++ pymatgen/analysis/elasticity/tensors.py
@@ -33,7 +33,7 @@ from pymatgen.core.operations import SymmOp
 from pymatgen.core.lattice import Lattice
 from numpy.linalg import norm
 
-voigt_map = [[0, 0], [1, 1], [2, 2], [1, 2], [2, 0], [0, 1]]
+voigt_map = [(0, 0), (1, 1), (2, 2), (1, 2), (0, 2), (0, 1)]
 reverse_voigt_map = np.array([[0, 5, 4],
                               [5, 1, 3],
                               [4, 3, 2]])
