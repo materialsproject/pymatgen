7aee7b4c5d1b50591466a840a257c8b3cea3bad9
wmdrichards <wmdrichards@gmail.com>
2017-02-28 13:48:04 -0500

    refactor PDAnalyzer.get_element_profile

--- pymatgen/phasediagram/analyzer.py
+++ pymatgen/phasediagram/analyzer.py
@@ -299,45 +299,23 @@ class PDAnalyzer(object):
         if element not in self._pd.elements:
             raise ValueError("get_transition_chempots can only be called with"
                              " elements in the phase diagram.")
-        chempots = self.get_transition_chempots(element)
-        stable_entries = self._pd.stable_entries
         gccomp = Composition({el: amt for el, amt in comp.items()
                               if el != element})
         elref = self._pd.el_refs[element]
         elcomp = Composition(element.symbol)
-        prev_decomp = []
         evolution = []
 
-        def are_same_decomp(decomp1, decomp2):
-            for comp in decomp2:
-                if comp not in decomp1:
-                    return False
-            return True
-
-        for c in chempots:
-            gcpd = GrandPotentialPhaseDiagram(
-                stable_entries, {element: c - 1e-5}, self._pd.elements
-            )
-            analyzer = PDAnalyzer(gcpd)
-            gcdecomp = analyzer.get_decomposition(gccomp)
-            decomp = [gcentry.original_entry.composition
-                      for gcentry, amt in gcdecomp.items()
-                      if amt > comp_tol]
-            decomp_entries = [gcentry.original_entry
-                              for gcentry, amt in gcdecomp.items()
-                              if amt > comp_tol]
-
-            if not are_same_decomp(prev_decomp, decomp):
-                if elcomp not in decomp:
-                    decomp.append(elcomp)
-                rxn = Reaction([comp], decomp)
-                rxn.normalize_to(comp)
-                prev_decomp = decomp
-                amt = -rxn.coeffs[rxn.all_comp.index(elcomp)]
-                evolution.append({'chempot': c,
-                                  'evolution': amt,
-                                  'element_reference': elref,
-                                  'reaction': rxn, 'entries': decomp_entries})
+        for cc in self.get_critical_compositions(elcomp, gccomp)[1:]:
+            decomp_entries = self.get_decomposition(cc).keys()
+            decomp = [k.composition for k in decomp_entries]
+            rxn = Reaction([comp, elcomp], decomp)
+            rxn.normalize_to(comp)
+            c = self.get_composition_chempots(cc + elcomp * 1e-5)[element]
+            amt = -rxn.coeffs[rxn.all_comp.index(elcomp)]
+            evolution.append({'chempot': c,
+                              'evolution': amt,
+                              'element_reference': elref,
+                              'reaction': rxn, 'entries': decomp_entries})
         return evolution
 
     def get_chempot_range_map(self, elements, referenced=True, joggle=True):
