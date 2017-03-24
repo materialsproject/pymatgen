36d3b1fbc76f47ab4e37f3d3f8d0afcb303c655f
__sp__ <ongsp@eng.ucsd.edu>
2017-03-23 18:08:31 -0700

    Add numpy version to error message.

--- pymatgen/analysis/elasticity/tests/test_tensors.py
+++ pymatgen/analysis/elasticity/tests/test_tensors.py
@@ -229,8 +229,8 @@ class TensorBaseTest(PymatgenTest):
             ieee = TensorBase(entry['ieee_tensor'])
             struct = Structure.from_dict(entry['structure'])
             diff = np.max(abs(ieee - orig.convert_to_ieee(struct)))
-            err_msg = "{} IEEE conversion failed with max diff {}".format(
-                xtal, diff) 
+            err_msg = "{} IEEE conversion failed with max diff {}. Numpy version: {}".format(
+                xtal, diff, np.__version__)
             self.assertArrayAlmostEqual(ieee, orig.convert_to_ieee(struct),
                                         err_msg=err_msg, decimal=1)
 
