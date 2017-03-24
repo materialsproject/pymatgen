ce8cc36c68c457937b65c6bab6f4aea89ea7a663
__sp__ <ongsp@eng.ucsd.edu>
2017-03-24 14:57:59 -0700

    v4.7.2 release

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ with open(os.path.join(SETUP_PTH, "README.rst")) as f:
 setup(
     name="pymatgen",
     packages=find_packages(),
-    version="4.7.1",
+    version="4.7.2",
     cmdclass={'build_ext': build_ext},
     setup_requires=['numpy', 'setuptools>=18.0'],
     install_requires=["numpy>=1.9", "six", "requests", "pyyaml>=3.11",
