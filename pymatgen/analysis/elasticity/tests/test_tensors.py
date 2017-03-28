9b0f39214c72b17c7cff9cfc236af0295795053d
montoyjh <montoyjh@gmail.com>
2017-03-27 17:46:35 -0700

    updated tensors unit tests

--- pymatgen/analysis/elasticity/tests/test_tensors.py
+++ pymatgen/analysis/elasticity/tests/test_tensors.py
@@ -15,12 +15,12 @@ from pymatgen import Structure
 test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..", "..",
                         'test_files')
 
-class TensorBaseTest(PymatgenTest):
+class TensorTest(PymatgenTest):
     def setUp(self):
-        self.vec = TensorBase([1., 0., 0.])
-        self.rand_rank2 = TensorBase(np.random.randn(3,3))
-        self.rand_rank3 = TensorBase(np.random.randn(3,3,3))
-        self.rand_rank4 = TensorBase(np.random.randn(3,3,3,3))
+        self.vec = Tensor([1., 0., 0.])
+        self.rand_rank2 = Tensor(np.random.randn(3,3))
+        self.rand_rank3 = Tensor(np.random.randn(3,3,3))
+        self.rand_rank4 = Tensor(np.random.randn(3,3,3,3))
         a = 3.14 * 42.5 / 180
         self.non_symm = SquareTensor([[0.1, 0.2, 0.3],
                                       [0.4, 0.5, 0.6],
@@ -28,13 +28,13 @@ class TensorBaseTest(PymatgenTest):
         self.rotation = SquareTensor([[math.cos(a), 0, math.sin(a)],
                                       [0, 1, 0],
                                       [-math.sin(a), 0, math.cos(a)]])
-        self.low_val = TensorBase([[1e-6, 1 + 1e-5, 1e-6],
+        self.low_val = Tensor([[1e-6, 1 + 1e-5, 1e-6],
                                    [1 + 1e-6, 1e-6, 1e-6],
                                    [1e-7, 1e-7, 1 + 1e-5]])
-        self.symm_rank2 = TensorBase([[1, 2, 3],
+        self.symm_rank2 = Tensor([[1, 2, 3],
                                       [2, 4, 5],
                                       [3, 5, 6]])
-        self.symm_rank3 = TensorBase([[[1, 2, 3],
+        self.symm_rank3 = Tensor([[[1, 2, 3],
                                        [2, 4, 5],
                                        [3, 5, 6]],
                                       [[2, 4, 5],
@@ -43,7 +43,7 @@ class TensorBaseTest(PymatgenTest):
                                       [[3, 5, 6],
                                        [5, 8, 9],
                                        [6, 9, 10]]])
-        self.symm_rank4 = TensorBase([[[[1.2, 0.4, -0.92],
+        self.symm_rank4 = Tensor([[[[1.2, 0.4, -0.92],
                                         [0.4, 0.05, 0.11],
                                         [-0.92, 0.11, -0.02]],
                                        [[0.4, 0.05, 0.11],
@@ -72,7 +72,7 @@ class TensorBaseTest(PymatgenTest):
                                         [-0.3, -0.18, -0.51]]]])
 
         # Structural symmetries tested using BaNiO3 piezo/elastic tensors
-        self.fit_r3 = TensorBase([[[0., 0., 0.03839],
+        self.fit_r3 = Tensor([[[0., 0., 0.03839],
                                  [0., 0., 0.],
                                  [0.03839, 0., 0.]],
                                 [[0., 0., 0.],
@@ -81,7 +81,7 @@ class TensorBaseTest(PymatgenTest):
                                 [[6.89822, 0., 0.],
                                  [0., 6.89822, 0.],
                                  [0., 0., 27.4628]]])
-        self.fit_r4 = TensorBase([[[[157.9, 0., 0.],
+        self.fit_r4 = Tensor([[[[157.9, 0., 0.],
                                     [0., 63.1, 0.],
                                     [0., 0., 29.4]],
                                    [[0., 47.4, 0.],
@@ -109,7 +109,7 @@ class TensorBaseTest(PymatgenTest):
                                     [0., 29.4, 0.],
                                     [0., 0., 207.6]]]])
         
-        self.unfit4 = TensorBase([[[[161.26, 0., 0.],
+        self.unfit4 = Tensor([[[[161.26, 0., 0.],
                                     [0., 62.76, 0.],
                                     [0., 0., 30.18]],
                                    [[0., 47.08, 0.],
@@ -145,31 +145,31 @@ class TensorBaseTest(PymatgenTest):
     def test_new(self):
         bad_2 = np.zeros((4, 4))
         bad_3 = np.zeros((4, 4, 4))
-        self.assertRaises(ValueError, TensorBase, bad_2)
-        self.assertRaises(ValueError, TensorBase, bad_3)
+        self.assertRaises(ValueError, Tensor, bad_2)
+        self.assertRaises(ValueError, Tensor, bad_3)
         self.assertEqual(self.rand_rank2.rank, 2)
         self.assertEqual(self.rand_rank3.rank, 3)
         self.assertEqual(self.rand_rank4.rank, 4)
 
     def test_zeroed(self):
         self.assertArrayEqual(self.low_val.zeroed(),
-                              TensorBase([[0, 1 + 1e-5, 0],
+                              Tensor([[0, 1 + 1e-5, 0],
                                           [1 + 1e-6, 0, 0],
                                           [0, 0, 1 + 1e-5]]))
         self.assertArrayEqual(self.low_val.zeroed(tol=1e-6),
-                              TensorBase([[1e-6, 1 + 1e-5, 1e-6],
+                              Tensor([[1e-6, 1 + 1e-5, 1e-6],
                                           [1 + 1e-6, 1e-6, 1e-6],
                                           [0, 0, 1 + 1e-5]]))
-        self.assertArrayEqual(TensorBase([[1e-6, -30, 1],
+        self.assertArrayEqual(Tensor([[1e-6, -30, 1],
                                           [1e-7, 1, 0],
                                           [1e-8, 0, 1]]).zeroed(),
-                              TensorBase([[0, -30, 1],
+                              Tensor([[0, -30, 1],
                                           [0, 1, 0],
                                           [0, 0, 1]]))
 
     def test_transform(self):
         # Rank 3
-        tensor = TensorBase(np.arange(0, 27).reshape(3, 3, 3))
+        tensor = Tensor(np.arange(0, 27).reshape(3, 3, 3))
         symm_op = SymmOp.from_axis_angle_and_translation([0, 0, 1], 30,
                                                          False, [0, 0, 1])
         new_tensor = tensor.transform(symm_op)
@@ -204,7 +204,6 @@ class TensorBaseTest(PymatgenTest):
         self.assertTrue(self.rand_rank3.symmetrized.is_symmetric())
         self.assertTrue(self.rand_rank4.symmetrized.is_symmetric())
     
-    
     def test_is_symmetric(self):
         self.assertTrue(self.symm_rank2.is_symmetric())
         self.assertTrue(self.symm_rank3.is_symmetric())
@@ -225,8 +224,8 @@ class TensorBaseTest(PymatgenTest):
     def test_convert_to_ieee(self):
         for entry in self.ieee_data:
             xtal = entry['xtal']
-            orig = TensorBase(entry['original_tensor'])
-            ieee = TensorBase(entry['ieee_tensor'])
+            orig = Tensor(entry['original_tensor'])
+            ieee = Tensor(entry['ieee_tensor'])
             struct = Structure.from_dict(entry['structure'])
             diff = np.max(abs(ieee - orig.convert_to_ieee(struct)))
             err_msg = "{} IEEE conversion failed with max diff {}".format(
@@ -236,25 +235,25 @@ class TensorBaseTest(PymatgenTest):
 
     def test_from_voigt(self):
         with self.assertRaises(ValueError):
-            TensorBase.from_voigt([[59.33, 28.08, 28.08, 0],
+            Tensor.from_voigt([[59.33, 28.08, 28.08, 0],
                                    [28.08, 59.31, 28.07, 0],
                                    [28.08, 28.07, 59.32, 0, 0],
                                    [0, 0, 0, 26.35, 0],
                                    [0, 0, 0, 0, 26.35]])
         # Rank 4
-        TensorBase.from_voigt([[59.33, 28.08, 28.08, 0, 0, 0],
+        Tensor.from_voigt([[59.33, 28.08, 28.08, 0, 0, 0],
                                [28.08, 59.31, 28.07, 0, 0, 0],
                                [28.08, 28.07, 59.32, 0, 0, 0],
                                [0, 0, 0, 26.35, 0, 0],
                                [0, 0, 0, 0, 26.35, 0],
                                [0, 0, 0, 0, 0, 26.35]])
         # Rank 3
-        TensorBase.from_voigt(np.zeros((3, 6)))
+        Tensor.from_voigt(np.zeros((3, 6)))
         # Rank 2
-        TensorBase.from_voigt(np.zeros(6))
+        Tensor.from_voigt(np.zeros(6))
 
     def test_symmetry_reduce(self):
-        tbs = [TensorBase.from_voigt(row) for row in np.eye(6)*0.01]
+        tbs = [Tensor.from_voigt(row) for row in np.eye(6)*0.01]
         reduced = symmetry_reduce(tbs, self.get_structure("Sn"))
         self.assertEqual(len(reduced), 2)
         self.assertArrayEqual([len(i) for i in reduced.values()], [2, 2])
@@ -264,6 +263,91 @@ class TensorBaseTest(PymatgenTest):
         reconstructed = sorted(reconstructed, key = lambda x: np.argmax(x))
         self.assertArrayAlmostEqual([tb for tb in reconstructed], np.eye(6)*0.01)
 
+
+class TensorCollectionTest(PymatgenTest):
+    def setUp(self):
+        self.seq_tc = [t for t in np.arange(4*3**3).reshape((4, 3, 3, 3))]
+        self.seq_tc = TensorCollection(self.seq_tc)
+        self.rand_tc = TensorCollection([t for t in np.random.random((4, 3, 3))])
+        self.diff_rank = TensorCollection([np.ones([3]*i) for i in range(2, 5)])
+        self.struct = self.get_structure("Si")
+        ieee_file_path = os.path.join(test_dir, "ieee_conversion_data.json")
+        with open(ieee_file_path) as f:
+            self.ieee_data = json.load(f)
+
+    def list_based_function_check(self, attribute, coll, *args, **kwargs):
+        """
+        This function allows for more efficient testing of list-based
+        functions in a "collection"-style class like TensorCollection
+
+        It ensures that the test function
+        """
+        tc_orig = TensorCollection(coll)
+        tc_mod = getattr(tc_orig, attribute)
+        if callable(tc_mod):
+            tc_mod = tc_mod(*args, **kwargs)
+        for t_orig, t_mod in zip(tc_orig, tc_mod):
+            this_mod = getattr(t_orig, attribute)
+            if callable(this_mod):
+                this_mod = this_mod(*args, **kwargs)
+            if isinstance(this_mod, np.ndarray):
+                self.assertArrayAlmostEqual(this_mod, t_mod)
+
+    def test_list_based_functions(self):
+        # zeroed
+        tc = TensorCollection([1e-4*Tensor(np.eye(3))]*4)
+        for t in tc.zeroed():
+            self.assertArrayEqual(t, np.zeros((3, 3)))
+        for t in tc.zeroed(1e-5):
+            self.assertArrayEqual(t, 1e-4*np.eye(3))
+        self.list_based_function_check("zeroed", tc)
+        self.list_based_function_check("zeroed", tc, tol=1e-5)
+
+        # transform
+        symm_op = SymmOp.from_axis_angle_and_translation([0, 0, 1], 30,
+                                                         False, [0, 0, 1])
+        self.list_based_function_check("transform", self.seq_tc, symm_op=symm_op)
+        
+        # symmetrized
+        self.list_based_function_check("symmetrized", self.seq_tc)
+
+        # rotation
+        a = 3.14 * 42.5 / 180
+        rotation = SquareTensor([[math.cos(a), 0, math.sin(a)], [0, 1, 0],
+                                 [-math.sin(a), 0, math.cos(a)]])
+        self.list_based_function_check("rotate", self.diff_rank, matrix=rotation)
+
+        # is_symmetric
+        self.assertFalse(self.seq_tc.is_symmetric())
+        self.assertTrue(self.diff_rank.is_symmetric())
+
+        # fit_to_structure
+        self.list_based_function_check("fit_to_structure", self.diff_rank, self.struct)
+        self.list_based_function_check("fit_to_structure", self.seq_tc, self.struct)
+        
+        # fit_to_structure
+        self.list_based_function_check("fit_to_structure", self.diff_rank, self.struct)
+        self.list_based_function_check("fit_to_structure", self.seq_tc, self.struct)
+
+        # voigt
+        self.list_based_function_check("voigt", self.diff_rank)
+
+        # is_voigt_symmetric
+        self.assertTrue(self.diff_rank.is_voigt_symmetric)
+        self.assertFalse(self.seq_tc.is_voigt_symmetric)
+
+        # Convert to ieee
+        for entry in self.ieee_data[:2]:
+            xtal = entry['xtal']
+            tc = TensorCollection([entry['original_tensor']]*3)
+            struct = Structure.from_dict(entry['structure'])
+            self.list_based_function_check("convert_to_ieee", tc, struct)
+
+        # from_voigt
+        tc_input = [t for t in np.random.random((3, 6, 6))]
+        tc = TensorCollection.from_voigt(tc_input)
+        for t_input, t in zip(tc_input, tc):
+            self.assertArrayAlmostEqual(Tensor.from_voigt(t_input), t)
 
 class SquareTensorTest(PymatgenTest):
     def setUp(self):
