5de133a823688cffea8e31d0d850798f1388197b
__sp__ <ongsp@eng.ucsd.edu>
2017-05-08 06:27:01 -0700

    Restore raises regexp.

--- pymatgen/analysis/tests/test_reaction_calculator.py
+++ pymatgen/analysis/tests/test_reaction_calculator.py
@@ -259,14 +259,14 @@ class ReactionTest(unittest.TestCase):
                      Composition("Cl"),
                      Composition("Cl")]
         products = [Composition("LiCl")]
-        self.assertRaisesRegex(ReactionError, "underdetermined", Reaction,
+        self.assertRaisesRegexp(ReactionError, "underdetermined", Reaction,
                                reactants, products)
 
         reactants = [Composition("LiMnCl3"),
                      Composition("LiCl"),
                      Composition("MnCl2")]
         products = [Composition("Li2MnCl4")]
-        self.assertRaisesRegex(ReactionError, "underdetermined", Reaction,
+        self.assertRaisesRegexp(ReactionError, "underdetermined", Reaction,
                                reactants, products)
 
 
