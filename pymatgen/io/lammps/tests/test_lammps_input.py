d6d82a36717ec0d85c1cba3090d6c08892b846c8
Brandon Wood <bwood@Brandons-MBP.hsd1.ca.comcast.net>
2017-06-12 00:42:52 -0700

    unittest for read lammps input parser

--- pymatgen/io/lammps/tests/test_lammps_input.py
+++ pymatgen/io/lammps/tests/test_lammps_input.py
@@ -24,6 +24,10 @@ class TestLammpsInput(unittest.TestCase):
             "NVT", os.path.join(test_dir, "NVT.json"),
             data_filename=os.path.join(test_dir, "nvt.data"),
             is_forcefield=True)
+        cls.read_lammps = DictLammpsInput.read_lammps_input("peptide", "./test_files/in.peptide",
+                                               lammps_data="./test_files/data.peptide",
+                                               data_filename="data.peptide",
+                                               is_forcefield=True)
 
     def test_string_rep(self):
         self.lammps_input.config_dict["read_data"] = "nvt.data"
@@ -32,6 +36,12 @@ class TestLammpsInput(unittest.TestCase):
                               f.readlines()):
                 self.assertEqual(l1.strip(), l2.strip())
 
-
+    def test_read_lammps_input(self):
+        self.assertEqual(self.read_lammps.config_dict["pair_style"], "lj/charmm/coul/long 8.0 10.0 10.0")
+        self.assertEqual(self.read_lammps.config_dict["fix"], ["1 all nvt temp 275.0 275.0 100.0 tchain 1",
+                                                               "2 all shake 0.0001 10 100 b 4 6 8 10 12 14 18 a 31"])
+        test_box = self.read_lammps.as_dict()["lammps_data"]["box_size"][0]
+        box =  [36.840194, 64.211560]
+        self.assertEqual(box, test_box)
 if __name__ == "__main__":
     unittest.main()
