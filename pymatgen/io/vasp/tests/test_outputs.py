f3d1ce4e9002cf83774186796ab0759d4387b11f
montoyjh <montoyjh@gmail.com>
2017-06-20 12:52:10 -0700

    added unittest for selective dynamics

--- pymatgen/io/vasp/tests/test_outputs.py
+++ pymatgen/io/vasp/tests/test_outputs.py
@@ -269,6 +269,13 @@ class VasprunTest(unittest.TestCase):
         self.assertRaises(ValueError, Vasprun,
                           os.path.join(test_dir, 'vasprun.xml.wrong_sp'))
 
+    def test_selective_dynamics(self):
+        vsd = Vasprun(os.path.join(test_dir, 'vasprun.xml.sdynamics'))
+        self.assertIsNotNone(vsd.initial_structure.site_properties.get(
+            'selective_dynamics', None), "Selective dynamics parsing error")
+        self.assertIsNotNone(vsd.final_structure.site_properties.get(
+            'selective_dynamics', None), "Selective dynamics parsing error")
+
     def test_as_dict(self):
         filepath = os.path.join(test_dir, 'vasprun.xml')
         vasprun = Vasprun(filepath,
