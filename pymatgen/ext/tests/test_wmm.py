34d4186b2c94e9651ae57fd53a17305f1a17bf9f
montoyjh <montoyjh@gmail.com>
2017-06-22 09:57:45 -0700

    cleanup unittest

--- pymatgen/ext/tests/test_wmm.py
+++ pymatgen/ext/tests/test_wmm.py
@@ -1,12 +1,25 @@
+# coding: utf-8
+# Copyright (c) Pymatgen Development Team.
+# Distributed under the terms of the MIT License.
+
+from __future__ import division, unicode_literals
 import unittest
+import os
+
+from pymatgen.util.testing import PymatgenTest
+from pymatgen.ext.wmm import get_from_wmm
+from pymatgen.io.vasp.inputs import Incar
+
+test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..",
+                        'test_files')
+
 
 class WmmTest(PymatgenTest):
     def test_get_wmm_kpoints(self):
         si = PymatgenTest.get_structure("Si")
         file_name = os.path.join(test_dir, 'INCAR')
         incar = Incar.from_file(file_name)
-        kpoints = Kpoints.get_from_wmm(si, incar=incar)
-
+        kpoints = get_from_wmm(si, incar=incar)
 
 if __name__ == '__main__':
     unittest.main()
