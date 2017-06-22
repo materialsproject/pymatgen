2dc9c1433efc884d9f87ae0c87e77e46915241af
montoyjh <montoyjh@gmail.com>
2017-06-22 10:09:04 -0700

    minor cleanup

--- pymatgen/ext/jhu_kpoints.py
+++ pymatgen/ext/jhu_kpoints.py
@@ -1,5 +1,10 @@
+# coding: utf-8
+# Copyright (c) Pymatgen Development Team.
+# Distributed under the terms of the MIT License.
+
 """
-This module creates an interface to the JHU kpoints servlet
+This module creates an interface to the JHU kpoints servlet,
+see http://muellergroup.jhu.edu/K-Points.html.
 """
 
 import requests
