339a55a69262365dbbf276b444937c1a3acfbe35
Kiran Mathew <kmathew@lbl.gov>
2017-08-02 10:15:40 -0700

    add tests for forcefielddata

--- pymatgen/io/lammps/tests/test_lammps_forcefield_data.py
+++ pymatgen/io/lammps/tests/test_lammps_forcefield_data.py
@@ -24,11 +24,13 @@ test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..", "..",
 
 
 class TestLammpsForceFieldData(unittest.TestCase):
+
     @classmethod
     def setUpClass(cls):
-        polymer_chain = Molecule.from_file(os.path.join(test_dir,"polymer_chain.xyz"))
-        polymer_linear = Molecule.from_file(os.path.join(test_dir,"polymer_linear.xyz"))
-        cls.polymer_matrix = Molecule.from_file(os.path.join(test_dir,"polymer_matrix.xyz"))
+        polymer_chain = Molecule.from_file(os.path.join(test_dir, "polymer_chain.xyz"))
+        polymer_linear = Molecule.from_file(os.path.join(test_dir, "polymer_linear.xyz"))
+        cls.polymer_matrix = Molecule.from_file(os.path.join(test_dir, "polymer_matrix.xyz"))
+
         charges = [-0.1187, 0.0861, 0.0861, 0.0861, -0.2792, -0.0326, 0.0861,
                    0.0861, -0.0326, 0.0861, 0.0861, -0.2792, -0.0326, 0.0861,
                    0.0861, -0.0326, 0.0861, 0.0861, -0.2792, -0.0326, 0.0861,
@@ -39,6 +41,15 @@ class TestLammpsForceFieldData(unittest.TestCase):
                    0.0861, 0.0861]
         polymer_linear.add_site_property("charge", charges)
         topology = Topology.from_molecule(polymer_linear)
+
+        cls.polymer_linear_ff_decorated = Molecule.from_file(
+            os.path.join(test_dir,"polymer_linear.xyz"))
+        ff_map = ['C2', 'H3', 'H2', 'H3', 'O', 'C3', 'H2', 'H3', 'C2', 'H3',
+                  'H2', 'O', 'C2', 'H3', 'H2', 'C3', 'H2', 'H3', 'O', 'C3',
+                  'H2', 'H3', 'C2', 'H3', 'H2', 'O', 'C2', 'H3', 'H2', 'C3',
+                  'H2', 'H3', 'O', 'C3', 'H2', 'H3', 'C2', 'H3', 'H2', 'O',
+                  'C2', 'H3', 'H2', 'C3', 'H2', 'H3', 'O', 'C3', 'H2', 'H3', 'H2']
+        cls.polymer_linear_ff_decorated.add_site_property("ff_map", ff_map)
 
         atoms = OrderedDict([("C", "C"), ("H", "H"), ("O", "O")])
         bonds = OrderedDict([((u'C', u'O'), [1000, 1.4115]),
@@ -90,6 +101,36 @@ class TestLammpsForceFieldData(unittest.TestCase):
             self.assertEqual(len(self.lammps_ff_data_1.dihedrals_data),
                              sum([len(top.dihedrals) * mol_number
                                   for mol_number in self.mols_number]))
+
+    def test_system_info_with_ff_map(self):
+        natoms, natom_types, atomic_masses_dict = \
+            LammpsForceFieldData.get_basic_system_info(self.polymer_linear_ff_decorated)
+
+        ans_atom_masses_dict = OrderedDict([('H2', [1, 1.00794]),
+                                            ('H3', [2, 1.00794]),
+                                            ('C2', [3, 12.0107]),
+                                            ('C3', [4, 12.0107]),
+                                            ('O', [5, 15.9994])])
+
+        self.assertEqual(natoms, 51)
+        self.assertEqual(natom_types, 5)
+        self.assertEqual(atomic_masses_dict.keys(), ans_atom_masses_dict.keys())
+        for k, v in atomic_masses_dict.items():
+            self.assertEqual(ans_atom_masses_dict[k][1], v[1])
+
+    def test_atoms_data_with_ff_map(self):
+        natoms, natom_types, atomic_masses_dict = \
+            LammpsForceFieldData.get_basic_system_info(self.polymer_linear_ff_decorated)
+        topology = Topology.from_molecule(self.polymer_linear_ff_decorated,
+                                          ff_map="ff_map")
+        atoms_data, molid_to_atomid = LammpsForceFieldData.get_atoms_data(
+            [self.polymer_linear_ff_decorated], [1],
+            self.polymer_linear_ff_decorated, atomic_masses_dict,
+            [topology])
+
+        for i, atom in enumerate(atoms_data):
+            key = self.polymer_linear_ff_decorated[i].ff_map
+            self.assertEqual(atom[2], atomic_masses_dict[key][0])
 
     def test_to_and_from_file(self):
         self.lammps_ff_data_1.write_data_file(
