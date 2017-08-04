24d9cbc9b714b79fc30f5b9f263909c88b368f10
adengz <z4deng@eng.ucsd.edu>
2017-08-03 18:18:39 -0700

    add classmethod to init BaderAnalysis for given path

--- pymatgen/command_line/bader_caller.py
+++ pymatgen/command_line/bader_caller.py
@@ -40,10 +40,6 @@ __status__ = "Beta"
 __date__ = "4/5/13"
 
 
-@requires(which("bader") or which("bader.exe"),
-          "BaderAnalysis requires the executable bader to be in the path."
-          " Please download the library at http://theory.cm.utexas"
-          ".edu/vasp/bader/ and compile the executable.")
 class BaderAnalysis(object):
     """
     Bader analysis for a CHGCAR.
@@ -230,6 +226,49 @@ class BaderAnalysis(object):
             summary['charge_transfer'] = charge_transfer
 
         return summary
+
+    @classmethod
+    def from_path(cls, path, suffix=""):
+
+        def _get_filepath(filename):
+            name_pattern = filename + suffix + '*' if filename != 'POTCAR' \
+                else filename + '*'
+            paths = glob.glob(os.path.join(path, name_pattern))
+            if not paths:
+                if filename == "CHGCAR":
+                    raise IOError("Could not find CHGCAR!")
+                else:
+                    if filename == "POTCAR":
+                        warning_msg = "cannot calculate charge transfer."
+                    else:
+                        warning_msg = "interpret Bader results with caution."
+                    warnings.warn("Could not find %s, " % filename
+                                  + warning_msg)
+                    return None
+            if len(paths) > 1:
+                # using reverse=True because, if multiple files are present,
+                # they likely have suffixes 'static', 'relax', 'relax2', etc.
+                # and this would give 'static' over 'relax2' over 'relax'
+                # however, better to use 'suffix' kwarg to avoid this!
+                paths.sort(reverse=True)
+                warning_msg = "Multiple files detected, using %s" \
+                              % os.path.basename(paths[0])
+                warnings.warn(warning_msg)
+            fpath = paths[0]
+            return fpath
+
+        chgcar_filename = _get_filepath("CHGCAR")
+        potcar_filename = _get_filepath("POTCAR")
+        aeccar0 = _get_filepath("AECCAR0")
+        aeccar2 = _get_filepath("AECCAR2")
+        if (aeccar0 and aeccar2):
+            chgref = Chgcar.from_file(aeccar0) + Chgcar.from_file(aeccar2)
+            chgref_filename = "CHGCAR_ref"
+            chgref.write_file(chgref_filename)
+        else:
+            chgref_filename = None
+        return cls(chgcar_filename, potcar_filename=potcar_filename,
+                   chgref_filename=chgref_filename)
 
 
 def bader_analysis_from_path(path, suffix=''):
