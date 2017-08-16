63025bde958e890bbf656375f73835a6b24b3b4f
Anubhav Jain <ajain@lbl.gov>
2017-08-16 15:50:46 -0700

    oxi_state_guesses uses ICSD oxidation states by default

--- pymatgen/core/composition.py
+++ pymatgen/core/composition.py
@@ -619,7 +619,7 @@ class Composition(collections.Hashable, collections.Mapping, MSONable):
             elif all_oxi_states:
                 oxids = Element(el).oxidation_states
             else:
-                oxids = Element(el).common_oxidation_states
+                oxids = Element(el).icsd_oxidation_states
 
             # get all possible combinations of oxidation states
             # and sum each combination
