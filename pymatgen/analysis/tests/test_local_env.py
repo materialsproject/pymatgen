ac1b9261e8ea5b60d173bb6d4e2cf49f5c82bd6d
Nils Zimmermann <nerz@lbl.gov>
2017-08-18 11:42:28 -0700

    Cleaned-up point_defects.py and local_env.py (#790)
    * Added base class for coordination finders in structure_analyzer.py
    
    * Revised base class.
    
    * One unit test error away from a working new VoronoiCoordFinder and JMolCoordFinder.
    
    * Restructured coordination finding classes.
    
    * Reinserted previous VoronoiCoordFinder for backwards compatibility.
    
    * Reverting previous structure_analyzer.py
    
    * Moved near-neighbor finding classes into local_env.py
    
    * Added VIRE based neighbor finding class.
    
    * Added deprecation warnings to structure_analyzer.py
    
    * Resolved conflict.
    
    * Resolved conflict---second trial.
    
    * Fixed missing imports and unit tests and shuffled around functions.
    
    * Moved OKeeffe helper functions from point_defects.py to local_env.py

--- pymatgen/analysis/tests/test_local_env.py
+++ pymatgen/analysis/tests/test_local_env.py
@@ -9,7 +9,8 @@ import unittest
 import os
 
 from pymatgen.analysis.local_env import VoronoiNN, JMolNN, \
-        MinimumDistanceNN, MinimumOKeeffeNN, MinimumVIRENN
+        MinimumDistanceNN, MinimumOKeeffeNN, MinimumVIRENN, \
+        site_is_of_motif_type
 from pymatgen import Element, Structure, Lattice
 from pymatgen.util.testing import PymatgenTest
 
@@ -104,6 +105,85 @@ class MiniDistNNTest(PymatgenTest):
             nacl, 0), 6)
         self.assertAlmostEqual(MinimumVIRENN(tol=0.01).get_cn(
             cscl, 0), 8)
+
+
+class MotifIdentificationTest(PymatgenTest):
+
+    def setUp(self):
+        self.silicon = Structure(
+                Lattice.from_lengths_and_angles(
+                        [5.47, 5.47, 5.47],
+                        [90.0, 90.0, 90.0]),
+                ["Si", "Si", "Si", "Si", "Si", "Si", "Si", "Si"],
+                [[0.000000, 0.000000, 0.500000],
+                [0.750000, 0.750000, 0.750000],
+                [0.000000, 0.500000, 1.000000],
+                [0.750000, 0.250000, 0.250000],
+                [0.500000, 0.000000, 1.000000],
+                [0.250000, 0.750000, 0.250000],
+                [0.500000, 0.500000, 0.500000],
+                [0.250000, 0.250000, 0.750000]],
+                validate_proximity=False, to_unit_cell=False,
+                coords_are_cartesian=False, site_properties=None)
+        self.diamond = Structure(
+            Lattice([[2.189, 0, 1.264], [0.73, 2.064, 1.264],
+                     [0, 0, 2.528]]), ["C0+", "C0+"], [[2.554, 1.806, 4.423],
+                                                       [0.365, 0.258, 0.632]],
+            validate_proximity=False,
+            to_unit_cell=False, coords_are_cartesian=True,
+            site_properties=None)
+        self.nacl = Structure(
+            Lattice([[3.485, 0, 2.012], [1.162, 3.286, 2.012],
+                     [0, 0, 4.025]]), ["Na1+", "Cl1-"], [[0, 0, 0],
+                                                         [2.324, 1.643, 4.025]],
+            validate_proximity=False,
+            to_unit_cell=False, coords_are_cartesian=True,
+            site_properties=None)
+        self.cscl = Structure(
+            Lattice([[4.209, 0, 0], [0, 4.209, 0], [0, 0, 4.209]]),
+            ["Cl1-", "Cs1+"], [[2.105, 2.105, 2.105], [0, 0, 0]],
+            validate_proximity=False, to_unit_cell=False,
+            coords_are_cartesian=True, site_properties=None)
+        self.square_pyramid = Structure(
+            Lattice([[100, 0, 0], [0, 100, 0], [0, 0, 100]]),
+            ["C", "C", "C", "C", "C", "C"], [
+            [0, 0, 0], [1, 0, 0], [-1, 0, 0], [0, 1, 0], [0, -1, 0], \
+            [0, 0, 1]], validate_proximity=False, to_unit_cell=False,
+            coords_are_cartesian=True, site_properties=None)
+        self.trigonal_bipyramid = Structure(
+            Lattice([[100, 0, 0], [0, 100, 0], [0, 0, 100]]),
+            ["P", "Cl", "Cl", "Cl", "Cl", "Cl"], [
+            [0, 0, 0], [0, 0, 2.14], [0, 2.02, 0], [1.74937, -1.01, 0], \
+            [-1.74937, -1.01, 0], [0, 0, -2.14]], validate_proximity=False,
+            to_unit_cell=False, coords_are_cartesian=True,
+            site_properties=None)
+
+    def test_site_is_of_motif_type(self):
+        for i in range(self.diamond.num_sites):
+            self.assertEqual(site_is_of_motif_type(
+                    self.diamond, i), "tetrahedral")
+        for i in range(self.nacl.num_sites):
+            self.assertEqual(site_is_of_motif_type(
+                    self.nacl, i), "octahedral")
+        for i in range(self.cscl.num_sites):
+            self.assertEqual(site_is_of_motif_type(
+                    self.cscl, i), "bcc")
+        self.assertEqual(site_is_of_motif_type(
+                self.square_pyramid, 0), "square pyramidal")
+        for i in range(1, self.square_pyramid.num_sites):
+            self.assertEqual(site_is_of_motif_type(
+                    self.square_pyramid, i), "unrecognized")
+        self.assertEqual(site_is_of_motif_type(
+                self.trigonal_bipyramid, 0), "trigonal bipyramidal")
+        for i in range(1, self.trigonal_bipyramid.num_sites):
+            self.assertEqual(site_is_of_motif_type(
+                    self.trigonal_bipyramid, i), "unrecognized")
+
+    def tearDown(self):
+        del self.silicon
+        del self.diamond
+        del self.nacl
+        del self.cscl
 
 
 if __name__ == '__main__':
