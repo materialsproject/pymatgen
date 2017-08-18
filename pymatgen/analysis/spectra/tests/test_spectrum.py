01a5a738ca961c11c7adadfd52ddc59243fcbc29
czhengsci <czhengsci@gmail.com>
2017-08-17 18:27:39 -0700

    more generic class of XANES

--- pymatgen/analysis/spectra/tests/test_spectrum.py
+++ pymatgen/analysis/spectra/tests/test_spectrum.py
@@ -1,20 +1,23 @@
 import os
-from pymatgen.analysis.spectra.spectrum import XanesFEFF
+from pymatgen.analysis.spectra.spectrum import XANES
 import unittest
+import json
 
-test_dir = os.path.join(os.path.dirname(__file__),"..", "..", "..", "..",
+test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..", "..",
                         "test_files/spectrum_test")
 
 
-
-class XanesFEFFSetTest(unittest.TestCase):
+class XANESSetTest(unittest.TestCase):
     def setUp(self):
-        self.xaneobj = XanesFEFF.from_file(test_dir+'/Pd2O.json')
+        json_file = os.path.join(test_dir, 'Pd2O.json')
+        with open(json_file, 'r') as f:
+            data = json.load(f)
+        self.xaneobj = XANES.from_dict(data)
 
     def test_structure(self):
-        self.assertEqual(self.xaneobj.absorbing_atom_index, 2)
-        self.assertEqual(self.xaneobj.absorbing_specie, 'Pd')
+        self.assertEqual(self.xaneobj.structure.composition.reduced_formula, 'Pd2O')
+        self.assertEqual(self.xaneobj.absorption_specie, 'Pd')
 
-    def test_e0_interpolate(self):
-        self.xaneobj.e0_interpolate()
-        self.assertAlmostEqual(24377.535, self.xaneobj.e0)
+    def test_find_e0(self):
+        self.xaneobj.find_e0()
+        self.assertAlmostEqual(24374.508999999998, self.xaneobj.e0)
