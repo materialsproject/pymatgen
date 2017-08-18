46dbf732244e237e30f80fecc29c2a135a228933
czhengsci <czhengsci@gmail.com>
2017-08-18 11:25:18 -0700

    modify unitest

--- pymatgen/analysis/spectra/tests/test_spectrum.py
+++ pymatgen/analysis/spectra/tests/test_spectrum.py
@@ -1,18 +1,23 @@
+# coding: utf-8
+# Copyright (c) Pymatgen Development Team.
+# Distributed under the terms of the MIT License.
+
 import os
 from pymatgen.analysis.spectra.spectrum import XANES
 import unittest
 import json
+from monty.json import MontyDecoder
 
 test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..", "..",
                         "test_files/spectrum_test")
 
+with open(os.path.join(test_dir,'Pd2O.json')) as fp:
+    spect_data_dict = json.load(fp, cls=MontyDecoder)
+
 
 class XANESSetTest(unittest.TestCase):
     def setUp(self):
-        json_file = os.path.join(test_dir, 'Pd2O.json')
-        with open(json_file, 'r') as f:
-            data = json.load(f)
-        self.xaneobj = XANES.from_dict(data)
+        self.xaneobj = XANES.from_dict(spect_data_dict)
 
     def test_structure(self):
         self.assertEqual(self.xaneobj.structure.composition.reduced_formula, 'Pd2O')
