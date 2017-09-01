1fa52ff4b7c616002b0908f92e7fec4c5f9d6f51
Marco Esters <esters@uoregon.edu>
2017-09-01 14:10:58 -0700

    Update test_inputs.py

--- pymatgen/io/vasp/tests/test_inputs.py
+++ pymatgen/io/vasp/tests/test_inputs.py
@@ -531,7 +531,7 @@ SIGMA = 0.1"""
     def test_proc_types(self):
         self.assertEqual(Incar.proc_val("HELLO", "-0.85 0.85"), "-0.85 0.85")
 
-        
+
 class KpointsTest(PymatgenTest):
     def test_init(self):
         filepath = os.path.join(test_dir, 'KPOINTS.auto')
