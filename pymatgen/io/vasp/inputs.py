3375f496df68273b79454b5ea6598b206e1e07bb
Marco Esters <esters@uoregon.edu>
2017-09-01 16:40:49 -0700

    Bug fix for Hubbard-U in Vasprun, and added basic vdW support (#819)
    * Update outputs.py
    
    * Update outputs.py
    
    * Update outputs.py
    
    * Update inputs.py
    
    * Update inputs.py
    
    * Update outputs.py
    
    * Update inputs.py
    
    * Update inputs.py
    
    * Update inputs.py
    
    * Add basic vdW support to vasp INCAR and output, fix Hubbard bug in output, and add unit test for vdW INCAR tags.
    
    * Update inputs.py
    
    * Update test_inputs.py
    
    * Update inputs.py
    
    * Update test_inputs.py
    
    * Add files via upload
    
    * Add vdW parameter to DictSet
    
    * Update sets.py
    
    * Delete INCAR.vdW
    
    * Update sets.py
    
    * Update sets.py
    
    * Insert test for vdW parameters
    
    * Update outputs.py

--- pymatgen/io/vasp/inputs.py
+++ pymatgen/io/vasp/inputs.py
@@ -752,15 +752,16 @@ class Incar(dict, MSONable):
             key: INCAR parameter key
             val: Actual value of INCAR parameter.
         """
-        list_keys = ("LDAUU", "LDAUL", "LDAUJ", "MAGMOM", "DIPOL", "LANGEVIN_GAMMA",
-                     "QUAD_EFG", "EINT")
-        bool_keys = ("LDAU", "LWAVE", "LSCALU", "LCHARG", "LPLANE",
+        list_keys = ("LDAUU", "LDAUL", "LDAUJ", "MAGMOM", "DIPOL",
+                     "LANGEVIN_GAMMA", "QUAD_EFG", "EINT")
+        bool_keys = ("LDAU", "LWAVE", "LSCALU", "LCHARG", "LPLANE", "LUSE_VDW",
                      "LHFCALC", "ADDGRID", "LSORBIT", "LNONCOLLINEAR")
         float_keys = ("EDIFF", "SIGMA", "TIME", "ENCUTFOCK", "HFSCREEN",
-                      "POTIM", "EDIFFG")
+                      "POTIM", "EDIFFG", "AGGAC", "PARAM1", "PARAM2")
         int_keys = ("NSW", "NBANDS", "NELMIN", "ISIF", "IBRION", "ISPIN",
                     "ICHARG", "NELM", "ISMEAR", "NPAR", "LDAUPRINT", "LMAXMIX",
-                    "ENCUT", "NSIM", "NKRED", "NUPDOWN", "ISPIND", "LDAUTYPE")
+                    "ENCUT", "NSIM", "NKRED", "NUPDOWN", "ISPIND", "LDAUTYPE",
+                    "IVDW")
 
         def smart_int_or_float(numstr):
             if numstr.find(".") != -1 or numstr.lower().find("e") != -1:
