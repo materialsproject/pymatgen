750771b7587bf3cbb0abfd339d855d75d44e32fd
Iek-Heng Chu <ihchu@eng.ucsd.edu>
2017-09-22 06:06:54 -0700

    Adding phonopy-related functions (#849)
    * Add phonopy-related functions
    
    * Minor fix

--- pymatgen/io/tests/test_phonopy.py
+++ pymatgen/io/tests/test_phonopy.py
@@ -60,6 +60,50 @@ class PhonopyParserTest(PymatgenTest):
         self.assertIn(Element.Na, cdos.get_element_dos())
         self.assertIn(Element.Cl, cdos.get_element_dos())
 
+class StructureConversionTest(PymatgenTest):
+
+    def test_structure_conversion(self):
+        s_pmg = PymatgenTest.get_structure("LiFePO4")
+        s_ph = get_phonopy_structure(s_pmg)
+        s_pmg2 = get_pmg_structure(s_ph)
+
+        coords_ph = s_ph.get_scaled_positions()
+        symbols_pmg = set([e.symbol for e in s_pmg.composition.keys()])
+        symbols_pmg2 = set([e.symbol for e in s_pmg2.composition.keys()])
+
+        self.assertAlmostEqual(s_ph.get_cell()[1,1],
+                               s_pmg.lattice._matrix[1,1], 7)
+        self.assertAlmostEqual(s_pmg.lattice._matrix[1,1],
+                               s_pmg2.lattice._matrix[1,1], 7)
+        self.assertEqual(symbols_pmg, set(s_ph.symbols))
+        self.assertEqual(symbols_pmg, symbols_pmg2)
+        self.assertArrayAlmostEqual(coords_ph[3], s_pmg.frac_coords[3])
+        self.assertArrayAlmostEqual(s_pmg.frac_coords[3], s_pmg2.frac_coords[3])
+        self.assertEqual(s_ph.get_number_of_atoms(), s_pmg.num_sites)
+        self.assertEqual(s_pmg.num_sites, s_pmg2.num_sites)
+
+class GetDisplacedStructuresTest(PymatgenTest):
+
+    def test_get_displaced_structures(self):
+        pmg_s = Structure.from_file(os.path.join(test_dir, "POSCAR-unitcell"),
+                                    False)
+        supercell_matrix = [[2,0,0], [0,1,0], [0,0,2]]
+        structures = get_displaced_structures(pmg_structure=pmg_s,
+                                              atom_disp=0.01,
+                                              supercell_matrix=supercell_matrix)
+
+        self.assertEqual(len(structures), 49)
+        self.assertArrayAlmostEqual(structures[4].frac_coords[0],
+                                    np.array([0.10872682, 0.21783039, 0.12595286]),
+                                    7)
+        self.assertArrayAlmostEqual(structures[-1].frac_coords[9],
+                                    np.array([0.89127318, 0.78130015, 0.37404715]),
+                                    7)
+        self.assertEqual(structures[0].num_sites, 128)
+        self.assertEqual(structures[10].num_sites, 128)
+        self.assertArrayAlmostEqual(structures[0].lattice._matrix,
+                                    structures[8].lattice._matrix, 8)
+
 
 if __name__ == '__main__':
     unittest.main()
