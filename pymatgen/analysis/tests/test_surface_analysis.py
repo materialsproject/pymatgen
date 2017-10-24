dfdfd2165e27ad5e2fd51e3c35d84089cea4e60a
richardtran415 <rit634989@gmail.com>
2017-10-23 20:33:31 -0700

    added test files for adsorption, unit testing for adsorption (to calculation number of monolayers)

--- pymatgen/analysis/tests/test_surface_analysis.py
+++ pymatgen/analysis/tests/test_surface_analysis.py
@@ -31,10 +31,49 @@ class SurfaceEnergyCalculatorTest(PymatgenTest):
 
         self.entry_dict = get_entry_dict(os.path.join(get_path(""),
                                                       "Cu_entries.txt"))
-        with open(os.path.join(get_path(""), 'Cu_ucell_entry.txt')) as ucell_entry:
-            ucell_entry = json.loads(ucell_entry.read())
-        ucell_entry = ComputedStructureEntry.from_dict(ucell_entry)
-        self.Cu_analyzer = SurfaceEnergyCalculator(ucell_entry)
+        with open(os.path.join(get_path(""), 'ucell_entries.txt')) as ucell_entries:
+            ucell_entries = json.loads(ucell_entries.read())
+        self.ucell_entries = ucell_entries
+
+        Cu_ucell_entry = ComputedStructureEntry.from_dict(self.ucell_entries["Cu"])
+        self.Cu_analyzer = SurfaceEnergyCalculator(Cu_ucell_entry)
+
+        # entry_dict for the adsorption case, O adsorption on Ni, Rh and Pt
+        metals_O_entry_dict = {"Ni": {(1, 1, 1): {}, (1, 0, 0): {}},
+                               "Pt": {(1, 1, 1): {}},
+                               "Rh": {(1, 0, 0): {}}
+                               }
+
+        with open(os.path.join(get_path(""), "csentries_slabs.json")) as entries:
+            entries = json.loads(entries.read())
+        for k in entries.keys():
+            entry = ComputedStructureEntry.from_dict(entries[k])
+            for el in metals_O_entry_dict.keys():
+                if el in k:
+                    if "111" in k:
+                        metals_O_entry_dict[el][(1, 1, 1)][entry] = []
+                    if "110" in k:
+                        metals_O_entry_dict[el][(1, 1, 0)][entry] = []
+                    if "100" in k:
+                        metals_O_entry_dict[el][(1, 0, 0)][entry] = []
+
+        with open(os.path.join(get_path(""), "csentries_o_ads.json")) as entries:
+            entries = json.loads(entries.read())
+        for k in entries.keys():
+            entry = ComputedStructureEntry.from_dict(entries[k])
+            for el in metals_O_entry_dict.keys():
+                if el in k:
+                    if "111" in k:
+                        clean = list(metals_O_entry_dict[el][(1, 1, 1)].keys())[0]
+                        metals_O_entry_dict[el][(1, 1, 1)][clean] = [entry]
+                    if "110" in k:
+                        clean = list(metals_O_entry_dict[el][(1, 1, 0)].keys())[0]
+                        metals_O_entry_dict[el][(1, 1, 0)][clean] = [entry]
+                    if "100" in k:
+                        clean = list(metals_O_entry_dict[el][(1, 0, 0)].keys())[0]
+                        metals_O_entry_dict[el][(1, 0, 0)][clean] = [entry]
+
+        self.metals_O_entry_dict = metals_O_entry_dict
 
     def test_gamma_calculator(self):
 
@@ -68,7 +107,27 @@ class SurfaceEnergyCalculatorTest(PymatgenTest):
 
     def test_adsorption_quantities(self):
 
-        # monolayer
+        # Test cases for getting adsorption related quantities for a 1/4
+        # monolalyer adsorption of O on the low MMI surfaces of Pt, Ni and Rh
+
+        with open(os.path.join(get_path(""), 'isolated_O_entry.txt')) as isolated_O_entry:
+            isolated_O_entry = json.loads(isolated_O_entry.read())
+        O = ComputedStructureEntry.from_dict(isolated_O_entry)
+
+        for el in self.metals_O_entry_dict.keys():
+            el_ucell = ComputedStructureEntry.from_dict(self.ucell_entries[el])
+            se_calc = SurfaceEnergyCalculator(el_ucell, adsorbate_entry=O)
+            for hkl in self.metals_O_entry_dict[el].keys():
+                for clean in self.metals_O_entry_dict[el][hkl]:
+                    for ads in self.metals_O_entry_dict[el][hkl][clean]:
+
+                        # Determine the correct number of monolayers.
+                        ml = int(round(se_calc.get_monolayer(ads, clean)))
+                        self.assertEqual(ml, 4)
+
+
+
+
         # gibbs_binding_energy
         # Nads_in_slab
         # Nsurfs_ads_in_slab
@@ -93,10 +152,11 @@ class SurfaceEnergyPlotterTest(PymatgenTest):
 
         entry_dict = get_entry_dict(os.path.join(get_path(""),
                                                  "Cu_entries.txt"))
-        with open(os.path.join(get_path(""), 'Cu_ucell_entry.txt')) as ucell_entry:
-            ucell_entry = json.loads(ucell_entry.read())
-        ucell_entry = ComputedStructureEntry.from_dict(ucell_entry)
-        calculator = SurfaceEnergyCalculator(ucell_entry)
+        with open(os.path.join(get_path(""), 'ucell_entries.txt')) as ucell_entries:
+            ucell_entries = json.loads(ucell_entries.read())
+
+        Cu_ucell_entry = ComputedStructureEntry.from_dict(ucell_entries["Cu"])
+        calculator = SurfaceEnergyCalculator(Cu_ucell_entry)
         self.Cu_analyzer = SurfaceEnergyPlotter(entry_dict, calculator, [-1, 0])
 
     def test_max_adsorption_chempot_range(self):
