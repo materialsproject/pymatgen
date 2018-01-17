099101e233bd88f27296bd3be6da71d764eaccc9
Danny Broberg <dbroberg@berkeley.edu>
2018-01-17 14:03:58 -0800

    instantiate grand canonical phase diagram method

--- pymatgen/analysis/defects/thermodynamics.py
+++ pymatgen/analysis/defects/thermodynamics.py
@@ -11,6 +11,9 @@ __date__ = "January 11, 2018"
 from math import exp
 import numpy as np
 from monty.json import MSONable
+from scipy import integrate
+
+from utils import kb
 
 from pymatgen.analysis.defects.corrections import ChargeCorrection, OtherCorrection
 
@@ -174,6 +177,7 @@ class DefectEntry(object):
 
         return conc
 
+
 class DefectPhaseDiagram(MSONable):
     """
     This is similar to a PhaseDiagram object in pymatgen, but has ability to do quick analysis of defect formation energies
@@ -228,7 +232,7 @@ class DefectPhaseDiagram(MSONable):
                 prev_min_q = cur_min_q
 
             self.stable_charges[dfct.name] = chg_type[:]
-            self.finished_charges[dfct.name] = [e.charge for e in self.da._defects if e.name == t]
+            self.finished_charges[dfct.name] = [e.charge for e in self.dentries if e.name == t]
             self.transition_levels[dfct.name] = trans_level[:]
 
     def add_defect_entry(self, defect):
@@ -291,6 +295,25 @@ class DefectPhaseDiagram(MSONable):
 
         return formation_energies
 
+    @property
+    def list_defect_concentrations(self, mu_elts, temp=300, ef=0.):
+        """
+        Give list of all concentrations at specified efermi in the DefectPhaseDiagram
+        args:
+            mu_elts = {Element: number} is dictionary of chemical potentials to provide formation energies for
+            temp = temperature to produce concentrations from
+            ef: (float) is fermi level relative to valence band maximum
+                Default efermi = 0 = VBM energy
+        returns:
+            list of dictionaries of defect concentrations
+        """
+        concentrations = []
+        for dfct in self.dentries:
+            concentrations.append( {'conc':dfct.get_defect_concentration(mu_elts, temp=temp, ef=ef),
+                                        'name':dfct.name, 'charge': dfct.charge})
+
+        return concentrations
+
     def _get_all_defect_types(self):
         """
         return defect names from entry list. Note this works off of DefectEntry name field
@@ -337,3 +360,200 @@ class DefectPhaseDiagram(MSONable):
             fullrecommendset[t] = reccomendset[:]
 
         return fullrecommendset
+
+
+class IntrinsicCarrier(object):
+    """
+    A class for producing free carrier concentrations from a pymatgen DOS object
+    """
+    def __init__(self, dos, exp_gap=None):
+        self.dos = dos
+        self.exp_gap = exp_gap
+
+    def get_n_density(self, ef, T, ref='VBM', unitcell=True):
+        """
+        Obtain the free electron concentration as a function of Fermi level and
+        temperature using Fermi-Dirac statistics and conduction band DOS
+        Args:
+            ef: Fermi energy
+            dos: Dos object of pymatgen
+            ref: Options 'VBM' or 'CBM'
+                The second option is useful when ef > DFT gap but <
+                experimental gap. In that case, ef close to CBM can be
+                given as -ve value w.r.t. CBM.
+            unitcell: Output units in terms of unitcell or not,
+                    if True output is #/unitcell, if False output is #/cm^3
+        Returns:
+            Electron density
+        """
+        dos_gap = self.dos.get_gap()
+        cbm, vbm = self.dos.get_cbm_vbm()
+        gap = self.exp_gap if self.exp_gap else dos_gap
+
+        if ref == 'CBM':
+            ef += cbm
+        else:
+            ef += vbm + dos_gap - gap
+
+        energies = self.dos.energies
+        densities = self.dos.get_densities()
+        if energies[-1] - ef < 3.0:
+            print ("The upper limit of energy is within 3 eV of Fermi level. "
+                   "Check for the convergence of electron concentration")
+        i = np.searchsorted(energies, cbm)
+        fd_stat = 1./(1 + np.exp((energies[i:] - ef) / (kb*T)))
+        y = fd_stat * densities[i:]
+        den = integrate.trapz(y, energies[i:])
+        if not unitcell:
+            den *= (1e24 / self.dos.structure.volume)
+        return den
+
+    def get_p_density(self, ef, T, ref='VBM', unitcell=True):
+        """
+        Obtain the hole concentration as a function of Fermi level and
+        temperature using Fermi-Dirac statistics and conduction band DOS
+        Args:
+            ef: Fermi energy
+            dos: Dos object of pymatgen
+            ref: Options 'VBM' or 'CBM'
+                    The second option is useful when ef > DFT gap
+                    but < experimental gap
+            unitcell: Output units in terms of unitcell or not,
+                    if True output is #/unitcell, if False output is #/cm^3
+        Returns:
+            Hole density
+        """
+        dos_gap = self.dos.get_gap()
+        cbm, vbm = self.dos.get_cbm_vbm()
+        gap = self.exp_gap if self.exp_gap else dos_gap
+
+        if ref == 'CBM':
+            ef += cbm + gap - dos_gap
+        else:
+            ef += vbm
+
+        energies = self.dos.energies
+        densities = self.dos.get_densities()
+        if ef - energies[0] < 3.0:
+            print ("The lower limit of energy is within 3 eV of Fermi level. "
+                   "Check for the convergence of hole concentration")
+        i = np.searchsorted(energies, vbm) + 1
+        fd_stat = 1./(1 + np.exp((ef - energies[:i]) / (kb*T)))
+        y = fd_stat*densities[:i]
+        den = integrate.trapz(y, energies[:i])
+        if not unitcell:
+            den *= (1e24 / self.dos.structure.volume)
+        return  den
+
+
+class GrandCanonicalDefectPhaseDiagram(DefectPhaseDiagram):
+    """
+    Adapts the DefectPhaseDiagram class to a grand canonical approach
+    This allows for self consistently determining fermi levels,
+    defect concentrations, and free carrier concentrations as a function
+    of temperature.
+
+    Args:
+        mu_elts: {Element: float} is dictionary of chemical potentials, determined from a phase diagram
+        T: Temperature (Kelvin)
+        kwargs: Arguments to DefectsAnalyzer as keyword pair
+    """
+    def __init__(self, mu_elts, T=298.15,  **kwargs):
+        super(self.__class__, self).__init__(**kwargs)
+        self.mu_elts = mu_elts
+        self.T = T
+        self._ef = None
+
+    def generate_gcdpd_from_pda(self, dentries, pda, T=298.15):
+        pass
+
+    def set_T(self, T):
+        self.T = T
+
+    @property
+    def fermi_energy(self):
+        return self._ef
+
+    def _get_total_q(self, ef, gap, bulk_dos):
+        qd_tot = 0
+        for d in self.list_defect_concentrations(self.mu_elts, temp=self.T, ef=ef):
+            qd_tot += d['charge'] * d['conc']
+
+        q_h_cont = IntrinsicCarrier(bulk_dos, exp_gap=gap)
+        ef_ref_cbm = ef - gap
+        nden = q_h_cont.get_n_density(ef_ref_cbm, self.T, ref='CBM', unitcell=False)
+        pden = q_h_cont.get_p_density(ef, self.T, unitcell=False)
+        qd_tot += pden - nden
+        return qd_tot
+
+    def solve_for_fermi_energy(self, bulk_dos):
+        """
+        Solve for the Fermi energy self-consistently as a function of T
+        and p_O2
+        Observations are Defect concentrations, electron and hole conc
+        Args:
+            bulk_dos: bulk system dos (pymatgen Dos object)
+            gap: Can be used to specify experimental gap.
+                Will be useful if the self consistent Fermi level
+                is > DFT gap
+        Returns:
+            Fermi energy
+        """
+        from scipy.optimize import bisect
+        self._ef = bisect(lambda e: self._get_total_q(e, self.band_gap, bulk_dos), -1., self.band_gap+1.)
+
+    def solve_non_eq_fermilevel(self, lowT, highT, bulk_dos, show_carriers=True, hold_htemp_ef = True):
+        """
+        Solve for the Fermi energy at a low temperature value, but with defect concentrations frozen in
+            at some higher temperature value This means that only the fermi level and free charge carriers concentrations
+            will be solved self consistently (simulating a 'frozen-in' defect concentration approach)
+
+        Args:
+            lowT: the temperature at which you want to solve for fermi level and free carrier concentrations
+            highT: the fixed temperature that sets the defect concentrations at a higher temperature
+            bulk_dos: bulk system dos (pymatgen Dos object)
+            show_carriers: Dictates the outputs of this function
+                Set to True if you want the new carrier concentrations in addition to the fermi levels
+            hold_htemp_ef: Specifies whether the fermi level will be fixed for the defect concentrations at high T
+                Set to False if you want the fermi level of the high temperature defects to be the same as the fermi level
+                for the low temperature charge carriers (see below note on implementation for explanation of this flag)
+        Returns:
+            Fermi energy
+
+        A NOTE ON IMPLEMENTATION: while testing this function, it became apparent that negative defect formation energies
+            can sometimes cause stochastic results for the fermi level as a result of the defect concentrations not
+            being allowed to change within the self consistent result. To circumvent this, this function has a flag ('hold_htemp_ef')
+            to allow for the fermi level of high temperature defects to vary within the self consistent approach.
+            While this is no longer the fully physical 'frozen-in' approach, it helps remove the stocastic results
+            that result from numerical issues
+
+        """
+        from scipy.optimize import bisect
+        q_h_cont = IntrinsicCarrier(bulk_dos, exp_gap=self.band_gap)
+
+        if hold_htemp_ef:
+            self.solve_for_fermi_energy(bulk_dos, gap=self.band_gap)
+            defectlist = self.get_defects_concentration(highT, self._ef, unitcell=False)
+
+        def noneq_total_q(ef):
+            qd_tot = 0
+            if hold_htemp_ef:
+                for d in defectlist:
+                    qd_tot += d['charge'] * d['conc']
+            else:
+                for d in self.get_defects_concentration(highT, ef, unitcell=False):
+                    qd_tot += d['charge'] * d['conc']
+            ef_ref_cbm = ef - self.band_gap
+            nden = q_h_cont.get_n_density(ef_ref_cbm, lowT, ref='CBM', unitcell=False)
+            pden = q_h_cont.get_p_density(ef, lowT, unitcell=False)
+            qd_tot += pden - nden
+            return qd_tot
+
+        non_eq_ef = bisect(lambda e: noneq_total_q(e), -1., self.band_gap+1.)
+        if show_carriers:
+            ef_ref_cbm = non_eq_ef - self.band_gap
+            nden = q_h_cont.get_n_density(ef_ref_cbm, lowT, ref='CBM', unitcell=False)
+            pden = q_h_cont.get_p_density(non_eq_ef, lowT, unitcell=False)
+            return non_eq_ef, nden, pden
+        else:
+            return non_eq_ef
