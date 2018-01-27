265532c85edf4eb226721c2c6b4176afa5175699
__sp__ <shyuep@gmail.com>
2018-01-27 10:09:55 -0800

    Use sed for modifying version.

--- tasks.py
+++ tasks.py
@@ -98,23 +98,7 @@ def publish(ctx):
 
 @task
 def set_ver(ctx):
-    lines = []
-    with open("pymatgen/__init__.py", "rt") as f:
-        for l in f:
-            if "__version__" in l:
-                lines.append('__version__ = "%s"' % NEW_VER)
-            else:
-                lines.append(l.rstrip())
-    with open("pymatgen/__init__.py", "wt") as f:
-        f.write("\n".join(lines))
-
-    lines = []
-    with open("setup.py", "rt") as f:
-        for l in f:
-            lines.append(re.sub(r'version=([^,]+),', 'version="%s",' % NEW_VER,
-                                l.rstrip()))
-    with open("setup.py", "wt") as f:
-        f.write("\n".join(lines))
+    ctx.run("sed -i '' 's/__version__ = .*/__version__ = \"%s\"/' pymatgen/__init__.py" % NEW_VER)
 
 
 @task
