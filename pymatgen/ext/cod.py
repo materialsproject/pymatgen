240805253c5c0677a0494ab4f5baf88d654d9335
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2018-02-08 20:50:52 +0100

    Print cif in COD.get_structure_by_formula if exc and re-raise

--- pymatgen/ext/cod.py
+++ pymatgen/ext/cod.py
@@ -127,7 +127,12 @@ class COD(object):
                 cod_id, sg = l.split("\t")
                 r = requests.get("http://www.crystallography.net/cod/%s.cif"
                                  % cod_id.strip())
-                s = Structure.from_str(r.text, fmt="cif", **kwargs)
-                structures.append({"structure": s, "cod_id": int(cod_id),
-                                   "sg": sg})
+                try:
+                    s = Structure.from_str(r.text, fmt="cif", **kwargs)
+                    structures.append({"structure": s, "cod_id": int(cod_id),
+                                       "sg": sg})
+                except Exception:
+                    print("Structure.from_str failed while parsing CIF file:\n", r.text)
+                    raise
+
         return structures
