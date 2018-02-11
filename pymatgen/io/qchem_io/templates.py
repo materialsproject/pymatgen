99bd482dbb3e312312d4f0a14fed5d4a97a6c2cd
Brandon Wood <b.wood@berkeley.edu>
2018-02-10 16:21:04 -0800

    don't mind this going to remove templates.py

--- pymatgen/io/qchem_io/templates.py
+++ pymatgen/io/qchem_io/templates.py
@@ -3,7 +3,9 @@
 
 def molecule_template(molecule):
     mol_list = []
+    # need to add read exception, because you can read in molecule from a previous calc
     mol_list.append("$molecule")
+    # if molecule == "read"
     mol_list.append(" {charge} {spin_mult}".format(charge=molecule.charge, spin_mult=molecule.spin_multiplicity))
     for site in molecule.sites:
         mol_list.append(" {atom}     {x: .10f}     {y: .10f}     {z: .10f}".format(atom=site.species_string,
@@ -26,11 +28,11 @@ def opt_template(opt):
     opt_list.append("$opt")
     # loops over all opt sections
     for key, value in opt.items():
-        opt_list.append("   {section}".format(section=key))
+        opt_list.append("{section}".format(section=key))
         # loops over all values within the section
         for i in value:
             opt_list.append("   {val}".format(val=i))
-        opt_list.append("   END{section}".format(section=key))
+        opt_list.append("END{section}".format(section=key))
         opt_list.append("")
     del opt_list[-1]
     opt_list.append("$end")
