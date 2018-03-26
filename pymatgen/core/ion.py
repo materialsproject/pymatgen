d628c5019e338a31aa723c42bdfb7adf6bdcb55c
montoyjh <montoyjh@gmail.com>
2018-03-26 14:56:18 -0700

    refactor ion to subclass composition

--- pymatgen/core/ion.py
+++ pymatgen/core/ion.py
@@ -24,7 +24,7 @@ from monty.json import MSONable
 from pymatgen.util.string import formula_double_format
 
 
-class Ion(MSONable):
+class Ion(Composition, MSONable):
     """
     Basic ion object. It is just a Composition object with an additional
     variable to store charge.
@@ -36,20 +36,11 @@ class Ion(MSONable):
         Flexible Ion construction, similar to Composition.
         For more information, please see pymatgen.core.Composition
         """
-        self._composition = Composition(composition)
+        super(Ion, self).__init__(composition)
         self._charge = charge
-        self._properties = properties if properties else {}
 
-    def __getattr__(self, a):
-        if a in self._properties:
-            return self._properties[a]
-        try:
-            return getattr(self._composition, a)
-        except:
-            raise AttributeError(a)
-
-    @staticmethod
-    def from_formula(formula):
+    @classmethod
+    def from_formula(cls, formula):
         charge = 0.0
         f = formula
         m = re.search(r"\[([^\[\]]+)\]", f)
@@ -74,7 +65,7 @@ class Ion(MSONable):
                 charge += sgn
             f = f.replace(m_chg.group(), "", 1)
         composition = Composition(f)
-        return Ion(composition, charge)
+        return cls(composition, charge)
 
     @property
     def formula(self):
@@ -82,12 +73,12 @@ class Ion(MSONable):
         Returns a formula string, with elements sorted by electronegativity,
         e.g., Li4 Fe4 P4 O16.
         """
-        formula = self._composition.formula
+        formula = super(Ion, self).formula
         chg_str = ""
-        if self._charge > 0:
-            chg_str = " +" + formula_double_format(self._charge, False)
+        if self.charge > 0:
+            chg_str = " +" + formula_double_format(self.charge, False)
         elif self._charge < 0:
-            chg_str = " " + formula_double_format(self._charge, False)
+            chg_str = " " + formula_double_format(self.charge, False)
         return formula + chg_str
 
     @property
@@ -96,7 +87,7 @@ class Ion(MSONable):
         An anonymized formula. Appends charge to the end
         of anonymized composition
         """
-        anon_formula = self._composition.anonymized_formula
+        anon_formula = super(Ion, self).anonymized_formula
         chg = self._charge
         chg_str = ""
         if chg > 0:
@@ -110,9 +101,8 @@ class Ion(MSONable):
         """
         Returns a reduced formula string with appended charge.
         """
-        reduced_formula = self._composition.reduced_formula
-        charge = self._charge / float(self._composition.
-                                      get_reduced_composition_and_factor()[1])
+        reduced_formula = super(Ion, self).reduced_formula
+        charge = self._charge / self.get_reduced_composition_and_factor()[1]
         if charge > 0:
             if abs(charge) == 1:
                 chg_str = "[+]"
@@ -133,12 +123,12 @@ class Ion(MSONable):
         """
         Returns a reduced formula string with appended charge
         """
-        alph_formula = self._composition.alphabetical_formula
+        alph_formula = super(Ion, self).alphabetical_formula
         chg_str = ""
-        if self._charge > 0:
-            chg_str = " +" + formula_double_format(self._charge, False)
-        elif self._charge < 0:
-            chg_str = " " + formula_double_format(self._charge, False)
+        if self.charge > 0:
+            chg_str = " +" + formula_double_format(self.charge, False)
+        elif self.charge < 0:
+            chg_str = " " + formula_double_format(self.charge, False)
         return alph_formula + chg_str
 
     @property
@@ -148,20 +138,13 @@ class Ion(MSONable):
         """
         return self._charge
 
-    @property
-    def composition(self):
-        """
-        Return composition object
-        """
-        return self._composition
-
     def as_dict(self):
         """
         Returns:
             dict with composition, as well as charge
         """
-        d = self._composition.as_dict()
-        d['charge'] = self._charge
+        d = super(Ion, self).as_dict()
+        d['charge'] = self.charge
         return d
 
     @classmethod
@@ -173,9 +156,8 @@ class Ion(MSONable):
             d:
                 {symbol: amount} dict.
         """
-#        composition = Composition.from_dict(d['composition'])
-        charge = d['charge']
-        composition = Composition({i: d[i] for i in d if i != 'charge'})
+        charge = d.pop('charge')
+        composition = Composition(d)
         return Ion(composition, charge)
 
     @property
@@ -185,11 +167,13 @@ class Ion(MSONable):
             dict with element symbol and reduced amount e.g.,
             {"Fe": 2.0, "O":3.0}.
         """
-        reduced_formula = self._composition.reduced_formula
-        c = Composition(reduced_formula)
-        d = c.as_dict()
-        d['charge'] = self._charge
+        d = self.composition.to_reduced_dict
+        d['charge'] = self.charge
         return d
+
+    @property
+    def composition(self):
+        return Composition(self._data)
 
     def __eq__(self, other):
         if self.composition != other.composition:
@@ -197,9 +181,6 @@ class Ion(MSONable):
         if self.charge != other.charge:
             return False
         return True
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
 
     def __add__(self, other):
         """
@@ -226,17 +207,10 @@ class Ion(MSONable):
         return Ion(new_composition, new_charge)
 
     def __hash__(self):
-        #for now, just use the composition hash code.
-        return self._composition.__hash__()
-
-    def __len__(self):
-        return len(self._composition)
+        return hash((self.composition, self.charge))
 
     def __str__(self):
         return self.formula
 
     def __repr__(self):
         return "Ion: " + self.formula
-
-    def __getitem__(self, el):
-        return self._composition.get(el, 0)
