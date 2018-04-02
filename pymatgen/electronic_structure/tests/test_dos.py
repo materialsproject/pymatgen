79a0c0f0af3df51f521387016eab9d61a6ec83d8
Alireza <alireza@wustl.edu>
2018-04-02 13:30:41 -0700

    add FermiDosTest

--- pymatgen/electronic_structure/tests/test_dos.py
+++ pymatgen/electronic_structure/tests/test_dos.py
@@ -11,7 +11,7 @@ import json
 from monty.serialization import loadfn
 
 from pymatgen.electronic_structure.core import Spin, Orbital, OrbitalType
-from pymatgen.electronic_structure.dos import CompleteDos, DOS
+from pymatgen.electronic_structure.dos import CompleteDos, DOS, FermiDos
 from pymatgen.util.testing import PymatgenTest
 
 test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..",
@@ -46,6 +46,25 @@ class DosTest(unittest.TestCase):
         dens = dos.densities
         for spin in Spin:
             self.assertAlmostEqual(sum(dens[spin]), sum(smeared[spin]))
+
+
+class FermiDosTest(unittest.TestCase):
+    def setUp(self):
+        with open(os.path.join(test_dir, "complete_dos.json"), "r") as f:
+            self.dos = CompleteDos.from_dict(json.load(f))
+        self.dos = FermiDos(self.dos)
+
+    def test_doping_fermi(self):
+        fermi0 = self.dos.efermi
+        frange = [fermi0-0.5, fermi0, fermi0+2.0, fermi0+2.2]
+        dopings = [self.dos.get_doping(fermi=f, T=300) for f in frange]
+        ref_dopings = [3.48077e+21, 1.9235e+18, -2.6909e+16, -4.8723e+19]
+        for i, c_ref in enumerate(ref_dopings):
+            self.assertLessEqual(abs(dopings[i]/c_ref-1.0), 0.01)
+
+        calc_fermis = [self.dos.get_fermi(c=c, T=300) for c in ref_dopings]
+        for j, f_ref in enumerate(frange):
+            self.assertAlmostEqual(calc_fermis[j], f_ref, 4)
 
 
 class CompleteDosTest(unittest.TestCase):
