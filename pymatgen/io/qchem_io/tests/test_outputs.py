abb9e0557857094e96de5b3f20c930b26904daac
samblau <samblau1@gmail.com>
2018-05-01 17:11:34 -0700

    Major overhaul of outputs to improve types

--- pymatgen/io/qchem_io/tests/test_outputs.py
+++ pymatgen/io/qchem_io/tests/test_outputs.py
@@ -20,10 +20,31 @@ multi_job_dict = loadfn(os.path.join(os.path.dirname(__file__),"multi_job.json")
 test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..", "..",
                         'test_files', "molecules")
 
-property_list = {'GEN_SCFMAN', 'SCF', 'restricted_Mulliken', 'unrestricted_Mulliken', 'optimized_geometry', 'optimized_zmat', 'unrestricted', 'errors',
-                 'completion', 'using_GEN_SCFMAN', 'final_energy', 'optimization', 'energy_trajectory', 'frequency_job', 'frequencies', 'last_geom',
-                 'enthalpy', 'entropy', 'molecule_from_last_geometry', 'molecule_from_optimized_geometry', 'charge', 'multiplicity', 'freq_species',
-                 'frequency_mode_vectors', 'freq_geometry'}
+property_list = {"errors",
+                 "multiple_outputs",
+                 "completion",
+                 "unrestricted",
+                 "using_GEN_SCFMAN",
+                 "final_energy",
+                 "S2",
+                 "optimization",
+                 "energy_trajectory",
+                 "opt_constraint",
+                 "dihedral_constraint",
+                 "frequency_job",
+                 "charge",
+                 "multiplicity",
+                 "species",
+                 "initial_geometry",
+                 "initial_molecule",
+                 "SCF",
+                 "Mulliken",
+                 "optimized_geometry",
+                 "optimized_zmat",
+                 "molecule_from_optimized_geometry",
+                 "last_geometry",
+                 "molecule_from_last_geometry",
+                 "frequency_mode_vectors"}
 
 single_job_out_names = {"unable_to_determine_lambda_in_geom_opt.qcout",
                         "thiophene_wfs_5_carboxyl.qcout",
@@ -42,7 +63,6 @@ single_job_out_names = {"unable_to_determine_lambda_in_geom_opt.qcout",
                         "quinoxaline_anion.qcout",
                         "crowd_gradient_number.qcout",
                         "bsse.qcout",
-                        "ghost_atoms.qcout",
                         "thiophene_wfs_5_carboxyl.qcout",
                         "time_nan_values.qcout",
                         "pt_dft_180.0.qcout",
@@ -85,17 +105,18 @@ class TestQCOutput(PymatgenTest):
     @staticmethod
     def generate_single_job_dict():
         """
-        Used to generate test dictionary for single jobs
+        Used to generate test dictionary for single jobs.
         """
         single_job_dict = {}
         for file in single_job_out_names:
+            print(file)
             single_job_dict[file] = QCOutput(os.path.join(test_dir, file)).data
         dumpfn(single_job_dict, "single_job.json")
 
     @staticmethod
     def generate_multi_job_dict():
         """
-        Used to generate test dictionary for multiple jobs
+        Used to generate test dictionary for multiple jobs.
         """
         multi_job_dict = {}
         for file in multi_job_out_names:
@@ -114,11 +135,11 @@ class TestQCOutput(PymatgenTest):
                 self.assertArrayEqual(QCOutput(os.path.join(test_dir, file)).data.get(key), single_job_dict[file].get(key))
         for file in multi_job_out_names:
             outputs = QCOutput.multiple_outputs_from_file(QCOutput, os.path.join(test_dir, file), keep_sub_files=False)
-            for i, sub_output in enumerate(outputs):
+            for ii, sub_output in enumerate(outputs):
                 try:
-                    self.assertEqual(sub_output.data.get(key), multi_job_dict[file][i].get(key))
+                    self.assertEqual(sub_output.data.get(key), multi_job_dict[file][ii].get(key))
                 except ValueError:
-                    self.assertArrayEqual(sub_output.data.get(key), multi_job_dict[file][i].get(key))
+                    self.assertArrayEqual(sub_output.data.get(key), multi_job_dict[file][ii].get(key))
 
     def test_all(self):
         for key in property_list:
