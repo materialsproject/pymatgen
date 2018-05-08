967bd1770b3a4d34761d35aedfc218fc157569cb
montoyjh <montoyjh@gmail.com>
2018-05-08 16:55:56 -0700

    add structure transform method

--- pymatgen/analysis/elasticity/tensors.py
+++ pymatgen/analysis/elasticity/tensors.py
@@ -14,6 +14,7 @@ import string
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 from pymatgen.core.operations import SymmOp
 from pymatgen.core.lattice import Lattice
+from pymatgen.analysis.structure_matcher import StructureMatcher
 
 """
 This module provides a base class for tensor-like objects and methods for
@@ -387,6 +388,33 @@ class Tensor(np.ndarray):
         if initial_fit:
             result = result.fit_to_structure(structure)
         return result.rotate(rotation, tol=1e-2)
+
+    def structure_transform(self, original_structure, new_structure):
+        """
+        Transforms a tensor from one basis for an original structure
+        into a new basis defined by a new structure.
+
+        Args:
+            original_structure (Structure): structure corresponding
+                to the basis of the current tensor
+            new_structure (Structure): structure corresponding to the
+                desired basis
+
+        Returns:
+            Tensor that has been transformed such that its basis
+            corresponds to the new_structure's basis
+        """
+        sm = StructureMatcher()
+        if not sm.fit(original_structure, new_structure):
+            warnings.warn("original and new structures do not match!")
+        ieee_trans_1 = self.get_ieee_rotation(original_structure)
+        ieee_trans_2 = self.get_ieee_rotation(new_structure)
+        # Get the ieee format tensor
+        new = self.rotate(ieee_trans_1)
+        # Reverse the ieee format rotation for the second structure
+        new = new.rotate(np.transpose(ieee_trans_2))
+        import nose; nose.tools.set_trace()
+        return new
 
     @classmethod
     def from_values_indices(cls, values, indices, populate=False,
