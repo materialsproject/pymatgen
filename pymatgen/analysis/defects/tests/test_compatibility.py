747800528b3709759738081ee580e380bf164c02
Danny Broberg <dbroberg@berkeley.edu>
2018-06-07 17:37:23 -0700

    add skeletons of new unit tests to be added

---
+++ pymatgen/analysis/defects/tests/test_compatibility.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+# Copyright (c) Pymatgen Development Team.
+# Distributed under the terms of the MIT License.
+
+from __future__ import unicode_literals
+
+import unittest
+
+from pymatgen.util.testing import PymatgenTest
+
+
+class DefectCompatibilityTest(PymatgenTest):
+
+    def test_process_entry(self):
+        pass
+
+    def test_perform_all_corrections(self):
+        pass
+
+    def test_perform_freysoldt(self):
+        pass
+
+    def test_perform_kumagai(self):
+        pass
+
+    def test_run_bandfilling(self):
+        pass
+
+    def test_run_band_edge_shifting(self):
+        pass
+
+    def test_delocalization_analysis(self):
+        pass
+
+    def test_is_freysoldt_delocalized(self):
+        pass
+
+    def test_is_kumagai_delocalized(self):
+        pass
+
+    def test_is_final_relaxed_structure_delocalized(self):
+        pass
