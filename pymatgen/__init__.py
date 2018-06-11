90b1a3df84c6909cffc0e3c512735de6cfb4f5c7
__sp__ <ongsp@ucsd.edu>
2018-06-11 09:03:15 -0700

    Update docs

--- pymatgen/__init__.py
+++ pymatgen/__init__.py
@@ -9,7 +9,7 @@ __author__ = "Pymatgen Development Team"
 __email__ ="pymatgen@googlegroups.com"
 __maintainer__ = "Shyue Ping Ong"
 __maintainer_email__ ="shyuep@gmail.com"
-__version__ = "2018.5.22"
+__version__ = "2018.6.11"
 
 
 SETTINGS_FILE = os.path.join(os.path.expanduser("~"), ".pmgrc.yaml")
