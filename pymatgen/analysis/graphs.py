53325c23a48e5f608e4d189108372cdd78e9fe41
Logan Ward <ward.logan.t@gmail.com>
2018-06-22 09:05:27 -0500

    Adjusted StructureGraph to use 'get_all_nn_info'

--- pymatgen/analysis/graphs.py
+++ pymatgen/analysis/graphs.py
@@ -154,8 +154,7 @@ class StructureGraph(MSONable):
                                              edge_weight_name="weight",
                                              edge_weight_units="")
 
-        for n in range(len(structure)):
-            neighbors = strategy.get_nn_info(structure, n)
+        for neighbors in strategy.get_all_nn_info(structure):
             for neighbor in neighbors:
 
                 # local_env will always try to add two edges
