89e4b15de202fd63123a0760bd0a2d0a77dde897
samblau <samblau1@gmail.com>
2018-07-31 16:39:46 -0700

    debugging

--- pymatgen/analysis/bond_dissociation.py
+++ pymatgen/analysis/bond_dissociation.py
@@ -146,7 +146,7 @@ class BondDissociationEnergies(MSONable):
                 for frag2 in frag2_entries:
                     if frag1["calcs_reversed"][-1]["input"]["rem"]["method"] == frag2["calcs_reversed"][-1]["input"]["rem"]["method"] and frag1["calcs_reversed"][-1]["input"]["rem"]["basis"] == frag2["calcs_reversed"][-1]["input"]["rem"]["basis"]: # Add PCM check?
                         if frag1["output"]["initial_molecule"]["charge"] + frag2["output"]["initial_molecule"]["charge"] == self.molecule_entry["output"]["optimized_molecule"]["charge"] and self.molecule_entry["calcs_reversed"][-1]["input"]["rem"]["basis"] == frag1["calcs_reversed"][-1]["input"]["rem"]["basis"] and self.molecule_entry["calcs_reversed"][-1]["input"]["rem"]["method"] == frag1["calcs_reversed"][-1]["input"]["rem"]["method"]:
-                            new_entry = [bonds, self.mol_graph.node[bonds[0]], self.mol_graph.node[bonds[1]], self.molecule_entry["output"]["final_energy"] - (frag1["output"]["final_energy"] + frag2["output"]["final_energy"]), frag1["output"]["final_energy"], frag1["output"]["initial_molecule"]["charge"], frag2["output"]["final_energy"], frag2["output"]["initial_molecule"]["charge"]]
+                            new_entry = [bonds, self.mol_graph[bonds[0]], self.mol_graph[bonds[1]], self.molecule_entry["output"]["final_energy"] - (frag1["output"]["final_energy"] + frag2["output"]["final_energy"]), frag1["output"]["final_energy"], frag1["output"]["initial_molecule"]["charge"], frag2["output"]["final_energy"], frag2["output"]["initial_molecule"]["charge"]]
                             self.bond_dissociation_energies += [new_entry]
 
     def search_fragment_entries(self, frag):
