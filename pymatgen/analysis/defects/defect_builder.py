5ff6bfaa5cc39cc2222b0244ac31d948901146aa
Danny Broberg <dbroberg@berkeley.edu>
2018-08-13 10:20:41 -0700

    builder update

--- pymatgen/analysis/defects/defect_builder.py
+++ pymatgen/analysis/defects/defect_builder.py
@@ -27,7 +27,7 @@ def standardize_sc_matrix(sc_mat):
     elif (type( sc_mat[0]) == float) or (type( sc_mat[0]) == int):
         return [[float(sc_mat[0]),0., 0.], [0., float(sc_mat[1]), 0.], [0.,0., float(sc_mat[2])]]
     else:
-        return sc_mat
+        return list(sc_mat)
 
 
 def add_vr_eigenvalue_dict( task):
