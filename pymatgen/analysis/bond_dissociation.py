807277ac22712fd8e1ee05b2b4cfe5cbe6c9f618
samblau <samblau1@gmail.com>
2018-08-16 17:05:45 -0700

    bug fix

--- pymatgen/analysis/bond_dissociation.py
+++ pymatgen/analysis/bond_dissociation.py
@@ -182,16 +182,16 @@ class BondDissociationEnergies(MSONable):
                                         reorder=False,
                                         extend_structure=False)
             found_similar_entry = False
-            for filtered_entry,ii in enumerate(self.filtered_entries):
-                print(filtered_entry)
-                print(filtered_entry["initial"])
-                print(this_dict["initial"])
-                print(filtered_entry["initial"].isomorphic_to(this_dict["initial"]))
-                print(filtered_entry["final"])
-                print(this_dict["final"])
-                print(filtered_entry["final"].isomorphic_to(this_dict["final"]))
-                print(filtered_entry["doc"]["input"]["initial_molecule"]["charge"])
-                print(entry["input"]["initial_molecule"]["charge"])
+            for ii,filtered_entry in enumerate(self.filtered_entries):
+                # print(filtered_entry)
+                # print(filtered_entry["initial"])
+                # print(this_dict["initial"])
+                # print(filtered_entry["initial"].isomorphic_to(this_dict["initial"]))
+                # print(filtered_entry["final"])
+                # print(this_dict["final"])
+                # print(filtered_entry["final"].isomorphic_to(this_dict["final"]))
+                # print(filtered_entry["doc"]["input"]["initial_molecule"]["charge"])
+                # print(entry["input"]["initial_molecule"]["charge"])
                 if filtered_entry["initial"].isomorphic_to(this_dict["initial"]) and filtered_entry["final"].isomorphic_to(this_dict["final"]) and filtered_entry["doc"]["input"]["initial_molecule"]["charge"] == entry["input"]["initial_molecule"]["charge"]:
                     found_similar_entry = True
                     if entry["output"]["final_energy"] < filtered_entry["doc"]["output"]["final_energy"]:
