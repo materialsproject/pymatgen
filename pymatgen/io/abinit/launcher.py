5513946f20ed97cdc8a23bdf12a833f78dc68e4a
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2018-08-28 16:22:47 +0200

    Deactivate fix_qcritical

--- pymatgen/io/abinit/launcher.py
+++ pymatgen/io/abinit/launcher.py
@@ -308,7 +308,7 @@ class PyFlowScheduler(object):
             max_nlaunches: Maximum number of tasks launched in a single iteration of the scheduler.
                 (DEFAULT: -1 i.e. no limit)
             debug: Debug level. Use 0 for production (int, DEFAULT: 0)
-            fix_qcritical: "yes" if the launcher should try to fix QCritical Errors (DEFAULT: "yes")
+            fix_qcritical: "yes" if the launcher should try to fix QCritical Errors (DEFAULT: "no")
             rmflow: If "yes", the scheduler will remove the flow directory if the calculation
                 completed successfully. (DEFAULT: "no")
             killjobs_if_errors: "yes" if the scheduler should try to kill all the runnnig jobs
@@ -340,7 +340,7 @@ class PyFlowScheduler(object):
         #self.max_etime_s = kwargs.pop("max_etime_s", )
         self.max_nlaunches = kwargs.pop("max_nlaunches", -1)
         self.debug = kwargs.pop("debug", 0)
-        self.fix_qcritical = as_bool(kwargs.pop("fix_qcritical", True))
+        self.fix_qcritical = as_bool(kwargs.pop("fix_qcritical", False))
         self.rmflow = as_bool(kwargs.pop("rmflow", False))
         self.killjobs_if_errors = as_bool(kwargs.pop("killjobs_if_errors", True))
 
