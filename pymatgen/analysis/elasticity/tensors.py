1483034ca74376484fbe41efe1320b8e439e382d
montoyjh <montoyjh@gmail.com>
2018-08-31 15:32:44 -0700

    set up link with warning

---
+++ pymatgen/analysis/elasticity/tensors.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+# Copyright (c) Pymatgen Development Team.
+# Distributed under the terms of the MIT License.
+
+import warnings
+
+warnings.warn("pymatgen.analysis.elasticity.tensors has been moved to "
+              "pymatgen.core.tensors, please update dependencies accordingly. "
+              "Links will removed in v2019.1.1.")
+from pymatgen.core.tensors import *
+
