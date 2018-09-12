9b9ab46220633c641bc0640b102fa7ebecb31d65
Danny Broberg <dbroberg@berkeley.edu>
2018-09-12 13:54:30 -0700

    add kumagai correction and unit test

--- pymatgen/analysis/defects/tests/test_corrections.py
+++ pymatgen/analysis/defects/tests/test_corrections.py
@@ -8,11 +8,13 @@ import os
 import unittest
 import numpy as np
 
+from pymatgen.core import Lattice
 from pymatgen.util.testing import PymatgenTest
-from pymatgen.io.vasp import Vasprun
+from pymatgen.io.vasp import Vasprun, Poscar, Outcar
 from pymatgen.analysis.defects.core import DefectEntry, Vacancy
 from pymatgen.analysis.defects.corrections import FreysoldtCorrection,\
-            BandFillingCorrection, BandEdgeShiftingCorrection
+            BandFillingCorrection, BandEdgeShiftingCorrection, KumagaiCorrection
+from pymatgen.analysis.defects.utils import generate_R_and_G_vecs
 
 test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..", "..", 'test_files')
 
@@ -93,6 +95,82 @@ class DefectsCorrectionsTest(PymatgenTest):
         self.assertAlmostEqual(val['freysoldt_electrostatic'], 0.)
         self.assertAlmostEqual(val['freysoldt_potential_alignment'], 0.)
 
+    def test_kumagai(self):
+        gamma = 0.19357221
+        prec = 28
+        lattice = Lattice( [[ 4.692882, -8.12831 ,  0.],
+                            [ 4.692882,  8.12831 ,  0.],
+                            [ 0.,  0., 10.03391 ]])
+
+        #note that real/recip vector generation is not dependent on epsilon
+        g_vecs, _, r_vecs, _ = generate_R_and_G_vecs( gamma, prec, lattice, 80. * np.identity(3))
+
+        #test real space summation (bigger for large epsilon)
+        kc_high_diel = KumagaiCorrection( 80. * np.identity(3), gamma=gamma)
+        real_sum = kc_high_diel.get_real_summation( gamma, r_vecs[0])
+        self.assertAlmostEqual( real_sum, 0.00843104)
+
+        #test recip space summation (bigger for small epsilon)
+        kc_low_diel = KumagaiCorrection( 0.1 * np.identity(3), gamma=gamma)
+        recip_sum = kc_low_diel.get_recip_summation( gamma, g_vecs[0], lattice.volume)
+        self.assertAlmostEqual( recip_sum, 0.31117099)
+
+        #test self interaction
+        si_corr = kc_low_diel.get_self_interaction( gamma)
+        self.assertAlmostEqual( si_corr, -0.54965249)
+
+        #test potenital shift interaction correction
+        ps_corr = kc_low_diel.get_potential_shift( gamma, lattice.volume)
+        self.assertAlmostEqual( ps_corr, -0.00871593)
+
+        # """Test Defect Entry approach to correction """
+        bulk_struc = Poscar.from_file(os.path.join( test_dir, 'defect', 'CONTCAR_bulk')).structure
+        bulk_out = Outcar( os.path.join( test_dir, 'defect', 'OUTCAR_bulk.gz'))
+        defect_out = Outcar( os.path.join( test_dir, 'defect', 'OUTCAR_vac_Ga_-3.gz'))
+        epsilon = 18.118 * np.identity(3)
+        vac = Vacancy(bulk_struc, bulk_struc.sites[0], charge=-3)
+        defect_structure = vac.generate_defect_structure()
+        defect_frac_coords = [0.,0.,0.]
+
+        parameters = {'bulk_atomic_site_averages': bulk_out.electrostatic_potential,
+                      'defect_atomic_site_averages': defect_out.electrostatic_potential,
+                      'site_matching_indices': [[ind, ind-1] for ind in range(len(bulk_struc))],
+                      'defect_frac_sc_coords': defect_frac_coords,
+                      'initial_defect_structure': defect_structure}
+        dentry = DefectEntry( vac, 0., parameters=parameters)
+        kc = KumagaiCorrection( epsilon)
+        kcorr = kc.get_correction( dentry)
+        self.assertAlmostEqual( kcorr['kumagai_electrostatic'], 0.88236299)
+        self.assertAlmostEqual( kcorr['kumagai_potential_alignment'], 2.09704862)
+
+
+        # test ES correction
+        high_diel_es_corr = kc_high_diel.perform_es_corr( gamma, prec, lattice, -3.)
+        self.assertAlmostEqual( high_diel_es_corr, 0.25176240)
+
+        low_diel_es_corr = kc_low_diel.perform_es_corr( gamma, prec, lattice, -3.)
+        self.assertAlmostEqual( low_diel_es_corr, 201.28810966)
+
+        # test pot correction
+        site_list = []
+        for bs_ind, ds_ind in dentry.parameters['site_matching_indices']:
+            Vqb = -(defect_out.electrostatic_potential[ds_ind] - bulk_out.electrostatic_potential[bs_ind])
+            site_list.append([defect_structure[ds_ind], Vqb])
+
+        sampling_radius = dentry.parameters["kumagai_meta"]["sampling_radius"]
+        gamma = dentry.parameters["kumagai_meta"]["gamma"]
+        q = -3
+        g_vecs, _, r_vecs, _ = generate_R_and_G_vecs( gamma, 28, defect_structure.lattice, np.identity(3))
+        high_diel_pot_corr  = kc_high_diel.perform_pot_corr( defect_structure, defect_frac_coords,
+                                                             site_list, sampling_radius, q,
+                                                             r_vecs[0], g_vecs[0], gamma)
+        self.assertAlmostEqual( high_diel_pot_corr, 2.35840716)
+        low_diel_pot_corr  = kc_low_diel.perform_pot_corr( defect_structure, defect_frac_coords,
+                                                           site_list, sampling_radius, q,
+                                                           r_vecs[0], g_vecs[0], gamma)
+        self.assertAlmostEqual( low_diel_pot_corr, -58.83598095)
+
+
     def test_bandfilling(self):
         v = Vasprun(os.path.join(test_dir, 'vasprun.xml'))
         eigenvalues = v.eigenvalues.copy()
@@ -115,9 +193,6 @@ class DefectsCorrectionsTest(PymatgenTest):
         #test trivial performing bandfilling correction
         bf_corr = bfc.perform_bandfill_corr(eigenvalues, kptweights, potalign, vbm, cbm)
         self.assertAlmostEqual(bf_corr, 0.)
-        self.assertFalse(bfc.metadata['occupied_def_levels'])
-        self.assertFalse(bfc.metadata['unoccupied_def_levels'])
-        self.assertFalse(bfc.metadata['total_occupation_defect_levels'])
         self.assertFalse(bfc.metadata['num_elec_cbm'])
         self.assertFalse(bfc.metadata['num_hole_vbm'])
         self.assertFalse(bfc.metadata['potalign'])
@@ -162,18 +237,6 @@ class DefectsCorrectionsTest(PymatgenTest):
         self.assertAlmostEqual(bfc.metadata['num_elec_cbm'], 0.8541667349)
         self.assertFalse(bfc.metadata['num_hole_vbm'])
 
-        #modify the potalignment and introduce new occupied defect levels from vbm states
-        potalign = -0.1
-
-        bf_corr = bfc.perform_bandfill_corr(eigenvalues, kptweights, potalign, vbm, cbm)
-        self.assertAlmostEqual(bfc.metadata['num_hole_vbm'], 0.)
-        self.assertAlmostEqual(bf_corr, 0.)
-        occu = [[1.457,  0.0833333], [1.5204, 0.0833333], [1.53465, 0.0833333], [1.5498, 0.0416667]]
-        self.assertArrayAlmostEqual(
-            list(sorted(bfc.metadata['occupied_def_levels'], key=lambda x: x[0])), list(
-                sorted(occu, key=lambda x: x[0])))
-        self.assertAlmostEqual(bfc.metadata['total_occupation_defect_levels'], 0.29166669)
-        self.assertFalse(bfc.metadata['unoccupied_def_levels'])
 
     def test_bandedgeshifting(self):
         struc = PymatgenTest.get_structure("VO2")
