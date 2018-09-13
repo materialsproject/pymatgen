098c3250235de94e83a697abffb568cdfbd19f87
Danny Broberg <dbroberg@berkeley.edu>
2018-09-13 11:37:26 -0700

    update readme

--- pymatgen/analysis/defects/core.py
+++ pymatgen/analysis/defects/core.py
@@ -39,7 +39,7 @@ class Defect(six.with_metaclass(ABCMeta, MSONable)):
 
         Args:
             structure: Pymatgen Structure without any defects
-            defect_site (Site): the site for the interstial
+            defect_site (Site): site for defect within structure
                 must have same lattice as structure
             charge: (int or float) defect charge
                 default is zero, meaning no change to NELECT after defect is created in the structure
