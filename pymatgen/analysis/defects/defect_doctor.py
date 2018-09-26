fa8b2bc89c3afcd98cefd68f7d815b2c21aea989
Daniel P Broberg <dpbroberg@dpbroberg-m37.dyn.berkeley.edu>
2018-09-26 12:05:04 -0700

    update defect first aid

--- pymatgen/analysis/defects/defect_doctor.py
+++ pymatgen/analysis/defects/defect_doctor.py
@@ -2,6 +2,9 @@
 A set of classes and functions that are useful for defect workflow management
 """
 
+import os
+
+from atomate.vasp.drones import VaspDrone
 
 
 class DefectFirstAid(object):
@@ -18,7 +21,8 @@ class DefectFirstAid(object):
 
     If these tests fail, then attempts to return a set of files which can
     """
-    def __init__(self, file_path, check_for_procar = True, check_for_wavecar = True):
+    def __init__(self, file_path, check_for_procar = True,
+                 check_for_wavecar = True, runs=None):
         self.file_path = file_path
         self.check_for_procar = check_for_procar
         self.check_for_wavecar = check_for_wavecar
@@ -26,53 +30,38 @@ class DefectFirstAid(object):
         self.task_set = None
         self.db_ready = True
 
-        self.initial_atomate_check()
+        self.drone = VaspDrone(runs=runs, parse_dos=True, parse_locpot=True)
 
+        #run drone assimilation
+        try:
+            self.task_doc = self.drone.assimilate(self.file_path)
+        except:
+            print('assimilation not possible for ', self.file_path)
+            self.db_ready = False
+
+        #check if procar and wavecar exist
         if self.check_for_procar and self.db_ready:
-            # do procar checking routine
-
+            if 'procar' not in self.task_doc['calcs_reversed'][0]['output_file_paths'].keys():
+                print( 'assimilation worked, but no procar exists...')
+                self.db_ready = False
 
         if self.check_for_wavecar and self.db_ready:
-            # do wavecar checking routine
-
-    def initial_atomate_check(self):
-        """
-        run initial atomate drone to see if ready for database
-
-        If this fails, then can run a simple checkup - approach
-        which tries to get as much information as possible...
-        """
-
-
-        return
-
-    def get_rerun_info(self):
-        """
-        If calculation is not ready then this returns information
-        for setting up and rerunning atomate workflow...
-        """
-
-        if self.db_ready:
-            print("This calculation is finished! No rerun is neccessary")
-            return
-        else:
-            # TODO: create this functionality.
-            # Note that a similar funcitonality is neccessary for DefectResubber, but should not be identical
-            # Want to make this easy for doing a single workflow resubmission of data in a way that is easy to track
-
-            return
-
+            if 'wavecar' not in self.task_doc['calcs_reversed'][0]['output_file_paths'].keys():
+                print( 'assimilation worked, but no wavecar exists...')
+                self.db_ready = False
 
 
 class DefectResubber(object):
     """
     Takes a defect builder result and creates follow up
     workflow outputs as needed (from atomate)
-    for completing the defect thermodynamics desired
+    for completing the defect thermodynamics desired.
+
+    Does this based on supercell size scaling, delocalization metrics...
     """
 
-    def __init__(self, var):
-        self.var = var
+    def __init__(self, db):
+        self.db = db
 
     def test(self):
         return
