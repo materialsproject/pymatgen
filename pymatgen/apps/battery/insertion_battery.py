986475905974701e4a230680524669f521468685
JSX <jmmshn@gmail.com>
2018-10-08 13:57:41 -0700

    value change
    takes care of fringe cases if we look at materials that are away from the convex hull

--- pymatgen/apps/battery/insertion_battery.py
+++ pymatgen/apps/battery/insertion_battery.py
@@ -60,7 +60,7 @@ class InsertionElectrode(AbstractElectrode):
             elements.update(entry.composition.elements)
 
         # Set an artificial energy for each element for convex hull generation
-        element_energy = max([entry.energy_per_atom for entry in entries]) + 10
+        element_energy = max([entry.energy_per_atom for entry in entries]) + 100
 
         pdentries = []
         pdentries.extend(entries)
