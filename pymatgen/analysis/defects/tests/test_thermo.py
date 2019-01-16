f1d0aa2fb7d345643a0647493588edffda90e3b1
dbroberg <dbroberg@berkeley.edu>
2019-01-16 15:19:23 -0800

    fix thermo unit test

--- pymatgen/analysis/defects/tests/test_thermo.py
+++ pymatgen/analysis/defects/tests/test_thermo.py
@@ -26,8 +26,10 @@ class DefectsThermodynamicsTest(PymatgenTest):
         pd = DefectPhaseDiagram(self.entries, 2.6682, 2.0)
         suggested_charges = pd.suggest_charges()
         for k in [
-                "Int_As_mult1", "Int_Ga_mult1", "Sub_As_on_Ga_mult4", "Sub_Ga_on_As_mult4", "Vac_As_mult4",
-                "Vac_Ga_mult4"
+                    "Vac_As_mult4@0-1-2-3-4-5", "Sub_Ga_on_As_mult4@6-7-8-9-10-11", "Vac_Ga_mult4@12-13-14-15",
+                    "Sub_As_on_Ga_mult4@16-17-18-19-20-21", "Int_Ga_mult1@22-23-24-25",
+                    "Int_As_mult1@26-27-28-29-30-31-32-33-34", "Int_As_mult1@35-36-37-38-39-40-41-42-43",
+                    "Int_Ga_mult1@44-45-46-47"
         ]:
             self.assertTrue(
                 len(suggested_charges[k]) > 0, "Could not find any suggested charges for {} with band_gap of {}".format(
@@ -35,7 +37,7 @@ class DefectsThermodynamicsTest(PymatgenTest):
 
         pd = DefectPhaseDiagram(self.entries, 2.6682, 1.0)
         suggested_charges = pd.suggest_charges()
-        for k in ["Vac_As_mult4", "Vac_Ga_mult4"]:
+        for k in ["Vac_As_mult4@0-1-2-3-4-5", "Vac_Ga_mult4@12-13-14-15"]:
             self.assertTrue(
                 len(suggested_charges[k]) > 0, "Could not find any suggested charges for {} with band_gap of {}".format(
                     k, pd.band_gap))
