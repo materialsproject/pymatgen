9b70d63f5984cdf5ace61b8abc23b6825a8b6662
__sp__ <shyuep@gmail.com>
2019-02-04 13:08:36 -0800

    Update docs.

--- docs_rst/conf.py
+++ docs_rst/conf.py
@@ -101,14 +101,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'basic'
+html_theme = 'proBlue'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    "nosidebar": True
-}
+#html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ["."]
