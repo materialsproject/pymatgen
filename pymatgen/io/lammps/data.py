fix ignored rotation when orthogonal lattice not aligned with xyz

pymatgen/io/lammps/data.py
@@ -193,22 +193,16 @@ def lattice_2_lmpbox(lattice, origin=(0, 0, 0)):
     a, b, c = lattice.abc
     xlo, ylo, zlo = origin
     xhi = a + xlo
-    if lattice.is_orthogonal:
-        yhi = b + ylo
-        zhi = c + zlo
-        tilt = None
-        rot_matrix = np.eye(3)
-    else:
-        m = lattice.matrix
-        xy = np.dot(m[1], m[0] / a)
-        yhi = np.sqrt(b ** 2 - xy ** 2) + ylo
-        xz = np.dot(m[2], m[0] / a)
-        yz = (np.dot(m[1], m[2]) - xy * xz) / (yhi - ylo)
-        zhi = np.sqrt(c ** 2 - xz ** 2 - yz ** 2) + zlo
-        tilt = [xy, xz, yz]
-        rot_matrix = np.linalg.solve([[xhi - xlo, 0, 0],
-                                      [xy, yhi - ylo, 0],
-                                      [xz, yz, zhi - zlo]], m)
+    m = lattice.matrix
+    xy = np.dot(m[1], m[0] / a)
+    yhi = np.sqrt(b ** 2 - xy ** 2) + ylo
+    xz = np.dot(m[2], m[0] / a)
+    yz = (np.dot(m[1], m[2]) - xy * xz) / (yhi - ylo)
+    zhi = np.sqrt(c ** 2 - xz ** 2 - yz ** 2) + zlo
+    tilt = None if lattice.is_orthogonal else [xy, xz, yz]
+    rot_matrix = np.linalg.solve([[xhi - xlo, 0, 0],
+                                  [xy, yhi - ylo, 0],
+                                  [xz, yz, zhi - zlo]], m)
     bounds = [[xlo, xhi], [ylo, yhi], [zlo, zhi]]
     symmop = SymmOp.from_rotation_and_translation(rot_matrix, origin)
     return LammpsBox(bounds, tilt), symmop
@@ -949,7 +943,7 @@ class Topology(MSONable):
                 for i, j in hub_cons:
                     ks = [k for k in hub_spokes[i] if k != j]
                     ls = [l for l in hub_spokes[j] if l != i]
-                    dihedral_list.extend([[k, i, j, l] for k,l in
+                    dihedral_list.extend([[k, i, j, l] for k, l in
                                           itertools.product(ks, ls)
                                           if k != l])
 

pymatgen/io/lammps/tests/test_data.py
@@ -741,9 +741,16 @@ class FuncTest(unittest.TestCase):
         tetra_latt = Lattice.tetragonal(5, 5)
         tetra_box, _ = lattice_2_lmpbox(tetra_latt)
         self.assertIsNone(tetra_box.tilt)
-        orthorhombic_latt = Lattice.orthorhombic(5, 5, 5)
-        orthorhombic_box, _ = lattice_2_lmpbox(orthorhombic_latt)
-        self.assertIsNone(orthorhombic_box.tilt)
+        ortho_latt = Lattice.orthorhombic(5, 5, 5)
+        ortho_box, _ = lattice_2_lmpbox(ortho_latt)
+        self.assertIsNone(ortho_box.tilt)
+        rot_tetra_latt = Lattice([[5, 0, 0], [0, 2, 2], [0, -2, 2]])
+        _, rotop = lattice_2_lmpbox(rot_tetra_latt)
+        np.testing.\
+            assert_array_almost_equal(rotop.rotation_matrix,
+                                      [[1, 0, 0],
+                                       [0, 2 ** 0.5 / 2, 2 ** 0.5 / 2],
+                                       [0, -2 ** 0.5 / 2, 2 ** 0.5 / 2]])
 
     @unittest.skip("The function is deprecated")
     def test_structure_2_lmpdata(self):