3ab957ddd6c5a0c7c17b3d436c3017f402f54dd7
JSX <jmmshn@gmail.com>
2019-03-08 13:59:53 -0800

    moved the stability stuff to pymatgen

--- pymatgen/apps/battery/insertion_battery.py
+++ pymatgen/apps/battery/insertion_battery.py
@@ -330,7 +330,15 @@ class InsertionElectrode(AbstractElectrode):
              "fracA_charge": chg_comp.get_atomic_fraction(ion),
              "fracA_discharge": dischg_comp.get_atomic_fraction(ion),
              "max_instability": self.get_max_instability(),
-             "min_instability": self.get_min_instability()}
+             "min_instability": self.get_min_instability(),
+             "material_ids" : [itr_ent.entry_id for itr_ent in self._entries],
+             "stable_material_ids" : [itr_ent.entry_id for itr_ent in self.get_stable_entries()],
+             "unstable_material_ids": [itr_ent.entry_id for itr_ent in self.get_unstable_entries()],
+             "stability_charged": self.fully_charged_entry.data['decomposition_energy'],
+             "stability_discharged": self.fully_discharged_entry.data['decomposition_energy'],
+             "stability_data":{itr_ent.entry_id: itr_ent.data['decomposition_energy'] for itr_ent in self._entries},
+             "muO2_data" : {itr_ent.entry_id: itr_ent.data['muO2'] for itr_ent in self._entries}
+             }
         if print_subelectrodes:
             f_dict = lambda c: c.as_dict_summary(print_subelectrodes=False)
             d["adj_pairs"] = list(map(f_dict,
