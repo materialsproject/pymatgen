281a543705c12a5251d110d5293e51b8c28b0214
darnoceloc <conrad.r.cole@gmail.com>
2019-03-14 16:03:02 -0400

    Updated run_type method to account for a wider range of functionals.

--- pymatgen/io/vasp/outputs.py
+++ pymatgen/io/vasp/outputs.py
@@ -15,7 +15,6 @@ from pathlib import Path
 import xml.etree.cElementTree as ET
 from collections import defaultdict
 from io import StringIO
-import collections
 
 import numpy as np
 from monty.io import zopen, reverse_readfile
@@ -23,7 +22,8 @@ from monty.json import MSONable
 from monty.json import jsanitize
 from monty.re import regrep
 from monty.os.path import zpath
-from monty.dev import deprecated
+
+
 
 from pymatgen.core.composition import Composition
 from pymatgen.core.lattice import Lattice
@@ -39,7 +39,6 @@ from pymatgen.entries.computed_entries import \
 from pymatgen.io.vasp.inputs import Incar, Kpoints, Poscar, Potcar
 from pymatgen.util.io_utils import clean_lines, micro_pyawk
 from pymatgen.util.num import make_symmetric_matrix_from_upper_tri
-from scipy.io import FortranFile
 
 """
 Classes for reading/manipulating/writing VASP ouput files.
@@ -649,23 +648,34 @@ class Vasprun(MSONable):
         TODO: Fix for other functional types like PW91, other vdW types, etc.
         """
 
+        GGA_TYPES = {"PE": "PBE", "PS": "PBESol", "RP": "RevPBE"}
+
         METAGGA_TYPES = {"TPSS", "RTPSS", "M06L", "MBJL", "SCAN", "MS0", "MS1", "MS2"}
 
-        if self.parameters.get("LHFCALC", False):
+        if self.parameters.get("AEXX", 1.00) == 1.00:
             rt = "HF"
+        elif self.parameters.get("HFSCREEN", 0.30) == 0.30:
+            rt = "HSE03"
+        elif self.parameters.get("HFSCREEN", 0.20) == 0.20:
+            rt = "HSE06"
+        elif self.parameters.get("AEXX", 0.20) == 0.20:
+            rt = "B3LYP"
+        elif self.parameters.get("GGA", "").strip().upper() in GGA_TYPES:
+            gga = self.parameters.get("GGA", "").strip().upper()
+            rt = GGA_TYPES[gga]
         elif self.parameters.get("METAGGA", "").strip().upper() in METAGGA_TYPES:
-            rt = self.parameters["METAGGA"].strip().upper()
-        elif self.parameters.get("LUSE_VDW", False):
+            rt = incar["METAGGA"].strip().upper()
+        elif self.parameters.get("LUSE_VDW", True):
             vdw_gga = {"RE": "DF", "OR": "optPBE", "BO": "optB88",
-                       "MK": "optB86b", "ML": "DF2"}
-            gga = self.parameters.get("GGA").upper()
+                       "MK": "optB86b", "ML": "DF2", "SCAN": "rVV10"}
+            gga = self.parameters.get("GGA", "").strip().upper() in GGA_TYPES
             rt = "vdW-" + vdw_gga[gga]
         elif self.potcar_symbols[0].split()[0] == 'PAW':
             rt = "LDA"
         else:
             rt = "GGA"
-        if self.is_hubbard:
-            rt += "+U"
+            if self.is_hubbard:
+                rt += "+U"
         return rt
 
     @property
@@ -1626,8 +1636,7 @@ class Outcar:
 
         # Check if the calculation type is DFPT
         self.dfpt = False
-        self.read_pattern({'ibrion': r"IBRION =\s+([\-\d]+)"},
-                          terminate_on_match=True,
+        self.read_pattern({'ibrion': "IBRION =\s+([\-\d]+)"}, terminate_on_match=True,
                           postprocess=int)
         if self.data.get("ibrion", [[0]])[0][0] > 6:
             self.dfpt = True
@@ -1809,57 +1818,39 @@ class Outcar:
         LOPTICS). Frequencies (in eV) are in self.frequencies, and dielectric
         tensor function is given as self.dielectric_tensor_function.
         """
+        header_pattern = r"\s+frequency dependent\s+IMAGINARY " \
+                         r"DIELECTRIC FUNCTION \(independent particle, " \
+                         r"no local field effects\)(\sdensity-density)*$"
+        row_pattern = r"\s+".join([r"([\.\-\d]+)"] * 7)
 
-        plasma_pattern = r"plasma frequency squared.*"
-        dielectric_pattern = r"frequency dependent\s+IMAGINARY " \
-                             r"DIELECTRIC FUNCTION \(independent particle, " \
-                             r"no local field effects\)(\sdensity-density)*$"
-        row_pattern = r"\s+".join([r"([\.\-\d]+)"] * 3)
-        plasma_frequencies = collections.defaultdict(list)
-        read_plasma = False
-        read_dielectric = False
-        energies = []
+        lines = []
+        for l in reverse_readfile(self.filename):
+            lines.append(l)
+            if re.match(header_pattern, l):
+                break
+
+        freq = []
         data = {"REAL": [], "IMAGINARY": []}
+        lines.reverse()
         count = 0
         component = "IMAGINARY"
-        with zopen(self.filename, "rt") as f:
-            for l in f:
-                l = l.strip()
-                if re.match(plasma_pattern, l):
-                    read_plasma = "intraband" if "intraband" in l else "interband"
-                elif re.match(dielectric_pattern, l):
-                    read_plasma = False
-                    read_dielectric = True
-                    row_pattern = r"\s+".join([r"([\.\-\d]+)"] * 7)
-
-                if read_plasma and re.match(row_pattern, l):
-                    plasma_frequencies[read_plasma].append(
-                        [float(t) for t in l.strip().split()])
-                elif read_dielectric:
-                    if re.match(row_pattern, l.strip()):
-                        toks = l.strip().split()
-                        if component == "IMAGINARY":
-                            energies.append(float(toks[0]))
-                        xx, yy, zz, xy, yz, xz = [float(t) for t in toks[1:]]
-                        matrix = [[xx, xy, xz], [xy, yy, yz], [xz, yz, zz]]
-                        data[component].append(matrix)
-                    elif re.match(r"\s*-+\s*", l):
-                        count += 1
-                    if count == 2:
-                        component = "REAL"
-                    elif count == 3:
-                        break
-
-        self.plasma_frequencies = {k: np.array(v[:3])
-                                   for k, v in plasma_frequencies.items()}
-        self.dielectric_energies = np.array(energies)
+        for l in lines[3:]:  # Skip the preamble.
+            if re.match(row_pattern, l.strip()):
+                toks = l.strip().split()
+                if component == "IMAGINARY":
+                    freq.append(float(toks[0]))
+                xx, yy, zz, xy, yz, xz = [float(t) for t in toks[1:]]
+                matrix = [[xx, xy, xz], [xy, yy, yz], [xz, yz, zz]]
+                data[component].append(matrix)
+            elif re.match(r"\s*-+\s*", l):
+                count += 1
+            if count == 1:
+                component = "REAL"
+            elif count == 2:
+                break
+        self.frequencies = np.array(freq)
         self.dielectric_tensor_function = np.array(data["REAL"]) + \
             1j * np.array(data["IMAGINARY"])
-
-    @property
-    @deprecated(message="frequencies has been renamed to dielectric_energies.")
-    def frequencies(self):
-        return self.dielectric_energies
 
     def read_chemical_shielding(self):
         """
@@ -3137,7 +3128,7 @@ class Locpot(VolumetricData):
     """
 
     def __init__(self, poscar, data):
-        super().__init__(poscar.structure, data)
+        super(Locpot, self).__init__(poscar.structure, data)
         self.name = poscar.comment
 
     @staticmethod
@@ -3156,7 +3147,7 @@ class Chgcar(VolumetricData):
     """
 
     def __init__(self, poscar, data, data_aug=None):
-        super().__init__(poscar.structure, data, data_aug=data_aug)
+        super(Chgcar, self).__init__(poscar.structure, data, data_aug=data_aug)
         self.poscar = poscar
         self.name = poscar.comment
         self._distance_matrix = {}
@@ -4392,79 +4383,6 @@ class Wavederf:
             raise ValueError("Band index out of bounds")
 
         return self.data[:, band_i - 1, band_j - 1, :]
-
-
-class Waveder:
-    """
-    Class for reading a WAVEDER file.
-    The LOPTICS tag produces a WAVEDER file.
-    The WAVEDER contains the derivative of the orbitals with respect to k.
-    Author: Kamal Choudhary, NIST
-
-    Args:
-        filename: Name of file containing WAVEDER.
-    """
-
-    def __init__(self, filename):
-        with FortranFile(filename, "r") as f:
-            val = (f.read_reals(dtype=np.int32))
-            nbands = int(val[0])
-            nelect = int(val[1])
-            nk = int(val[2])
-            ispin = int(val[3])
-            nodes_in_dielectric_function = (f.read_reals(dtype=np.float))
-            wplasmon = (f.read_reals(dtype=np.float))
-            cder = np.array((f.read_reals(dtype=np.float)))
-            cder_data = cder.reshape((nbands, nelect, nk, ispin, 3))
-            self.cder_data = cder_data
-            self.nkpoints = nk
-            self.ispin = ispin
-            self.nelect = nelect
-            self.nbands = nbands
-
-    @property
-    def nbands(self):
-        """
-        Returns the number of bands in the calculation
-        """
-        return self.nbands
-
-    @property
-    def nkpoints(self):
-        """
-        Returns the number of k-points in the calculation
-        """
-        return self.nkpoints
-
-    @property
-    def nelect(self):
-        """
-        Returns the number of electrons in the calculation
-        """
-        return self.nelect
-
-    def get_orbital_derivative_between_states(self, band_i, band_j, kpoint, spin, cart_dir):
-        """
-        Method returning a value
-        between bands band_i and band_j for k-point index, spin-channel and cartesian direction.
-        Args:
-            band_i (Integer): Index of band i
-            band_j (Integer): Index of band j
-            kpoint (Integer): Index of k-point
-            spin   (Integer): Index of spin-channel (0 or 1)
-            cart_dir (Integer): Index of cartesian direction (0,1,2)
-
-        Returns:
-            a float value
-        """
-        if band_i < 0 or band_i > self.nbands - 1 or band_j < 0 or band_j > self.nelect - 1:
-            raise ValueError("Band index out of bounds")
-        if kpoint > self.nkpoints:
-            raise ValueError("K-point index out of bounds")
-        if cart_dir > 2 or cart_dir < 0:
-            raise ValueError("cart_dir index out of bounds")
-
-        return self.cder_data[band_i, band_j, kpoint, spin, cart_dir]
 
 
 class UnconvergedVASPWarning(Warning):
