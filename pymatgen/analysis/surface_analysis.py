2bc4c2e3727ba67a1ae24895539bdc033ada04ca
richardtran415 <rit634989@gmail.com>
2019-03-19 02:10:45 -0700

    improved labelling

--- pymatgen/analysis/surface_analysis.py
+++ pymatgen/analysis/surface_analysis.py
@@ -1416,8 +1416,7 @@ class WorkFunctionAnalyzer:
         bulk_p = []
         for r in self.slab_regions:
             bulk_p.extend([p for i, p in enumerate(self.locpot_along_c) if \
-                           r[1] >= self.along_c[i] \
-                           > r[0]])
+                           r[1] >= self.along_c[i] > r[0]])
         if len(self.slab_regions) > 1:
             bulk_p.extend([p for i, p in enumerate(self.locpot_along_c) if \
                            self.slab_regions[1][1] <= self.along_c[i]])
@@ -1502,46 +1501,43 @@ class WorkFunctionAnalyzer:
         Returns Labelled plt
         """
 
-        maxc = self.sorted_sites[-1].frac_coords[2]
-        minc = self.sorted_sites[0].frac_coords[2]
-        # determine whether most of the vacuum is to
-        # the left or right for labelling purposes
-        vleft = [i for i in self.along_c if i <= minc]
-        vright = [i for i in self.along_c if i >= maxc]
-        if max(vleft) - min(vleft) > max(vright) - min(vright):
-            label_in_vac = (max(vleft) - min(vleft))/2
+        # center of vacuum and bulk region
+        if len(self.slab_regions) > 1:
+            label_in_vac = (self.slab_regions[0][1] + self.slab_regions[1][0])/2
+            if abs(self.slab_regions[0][0]-self.slab_regions[0][1]) > \
+                    abs(self.slab_regions[1][0]-self.slab_regions[1][1]):
+                label_in_bulk = self.slab_regions[0][1]/2
+            else:
+                label_in_bulk = (self.slab_regions[1][1] + self.slab_regions[1][0]) / 2
         else:
-            label_in_vac = (max(vright) - min(vright))/2 + maxc
+            label_in_bulk = (self.slab_regions[0][0] + self.slab_regions[0][1])/2
+            if self.slab_regions[0][0] > 1-self.slab_regions[0][1]:
+                label_in_vac = self.slab_regions[0][0] / 2
+            else:
+                label_in_vac = (1 + self.slab_regions[0][1]) / 2
 
-        # label the vacuum locpot
-        label_in_bulk = maxc - (maxc - minc) / 2
         plt.plot([0, 1], [self.vacuum_locpot]*2, 'b--', zorder=-5, linewidth=1)
-        x = label_in_bulk + self.shift if label_in_bulk+self.shift < 1 else label_in_bulk+self.shift - 1
-        xy = [x, self.vacuum_locpot+self.ave_locpot*0.05]
+        xy = [label_in_bulk, self.vacuum_locpot+self.ave_locpot*0.05]
         plt.annotate(r"$V_{vac}=%.2f$" %(self.vacuum_locpot), xy=xy,
                      xytext=xy, color='b', fontsize=label_fontsize)
 
         # label the fermi energy
         plt.plot([0, 1], [self.efermi]*2, 'g--',
                  zorder=-5, linewidth=3)
-        x = label_in_bulk + self.shift if label_in_bulk + self.shift < 1 else label_in_bulk + self.shift - 1
-        xy = [x, self.efermi+self.ave_locpot*0.05]
+        xy = [label_in_bulk, self.efermi+self.ave_locpot*0.05]
         plt.annotate(r"$E_F=%.2f$" %(self.efermi), xytext=xy,
                      xy=xy, fontsize=label_fontsize, color='g')
 
         # label the bulk-like locpot
         plt.plot([0, 1], [self.ave_bulk_p]*2, 'r--', linewidth=1., zorder=-1)
-        x = label_in_vac + self.shift if label_in_vac + self.shift < 1 else label_in_vac + self.shift - 1
-        xy = [x, self.ave_bulk_p + self.ave_locpot * 0.05]
+        xy = [label_in_vac, self.ave_bulk_p + self.ave_locpot * 0.05]
         plt.annotate(r"$V^{interior}_{slab}=%.2f$" % (self.ave_bulk_p),
                      xy=xy, xytext=xy, color='r', fontsize=label_fontsize)
 
         # label the work function as a barrier
-        x = label_in_vac+self.shift if label_in_vac+self.shift < 1 else label_in_vac+self.shift - 1
-        plt.plot([x]*2, [self.efermi, self.vacuum_locpot],
+        plt.plot([label_in_vac]*2, [self.efermi, self.vacuum_locpot],
                  'k--', zorder=-5, linewidth=2)
-
-        xy = [x, self.efermi + self.ave_locpot * 0.05]
+        xy = [label_in_vac, self.efermi + self.ave_locpot * 0.05]
         plt.annotate(r"$\Phi=%.2f$" %(self.work_function),
                      xy=xy, xytext=xy, fontsize=label_fontsize)
 
