1032b1943504fb95035981b1793929748ad59569
JSX <jmmshn@gmail.com>
2019-04-02 17:39:09 -0700

    updated methods to look for unique edges

--- pymatgen/apps/battery/host_analyzer.py
+++ pymatgen/apps/battery/host_analyzer.py
@@ -13,7 +13,13 @@ from pymatgen.core.periodic_table import Element, Specie
 from pymatgen.core.structure import Composition, Structure
 from pymatgen.analysis.structure_matcher import StructureMatcher, ElementComparator
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
-
+from pymatgen.analysis.graphs import StructureGraph
+from pymatgen.analysis.local_env import NearNeighbors
+import operator
+import networkx as nx
+import numpy as np
+import pandas as pd
+from pandas.io.json import json_normalize
 __author__ = "Jimmy Shen"
 __copyright__ = "Copyright 2019, The Materials Project"
 __version__ = "0.1"
@@ -31,7 +37,74 @@ handler.setFormatter(formatter)
 root.addHandler(handler)
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
+
+grouper_sm = StructureMatcher()
+
+def generic_groupby(list_in, comp=operator.eq, lab_num=True):
+    '''
+    Group a list of unhasable objects
+    Return a list of labels that represent which group the entry is in
+    '''
+    list_out = ['TODO'] * len(list_in)
+    cnt = 0
+    for i1, ls1 in enumerate(list_out):
+        if ls1 != 'TODO':
+            continue
+
+        if not lab_num:
+            list_out[i1] = alpha[cnt]
+        else:
+            list_out[i1] = cnt
+        cnt += 1
+        for i2, ls2 in enumerate(list_out):
+            if comp(list_in[i1], list_in[i2]):
+                list_out[i2] = list_out[i1]
+    return list_out
+
+class ConnectSitesNN(NearNeighbors):
+    """
+    Local environment class to help look for migration paths through the lattice
+
+    Since we are interested in many migration pathways through the material, we need a more connections than other local_env classes offer
+
+    Args:
+        cutoff (float): cutoff radius in Angstrom to look for trial
+            near-neighbor sites (default: 10.0).
+    """
+
+    def __init__(self, cutoff=8.0):
+        self.cutoff = cutoff
+
+    def get_nn_info(self, structure, n):
+        """
+        Get all near-neighbor sites as well as the associated image locations
+        and weights of the site with index n using the closest neighbor
+        distance-based method.
+
+        Args:
+            structure (Structure): input structure.
+            n (integer): index of site for which to determine near
+                neighbors.
+
+        Returns:
+            siw (list of tuples (Site, array, float)): tuples, each one
+                of which represents a neighbor site, its image location,
+                and its weight.
+        """
+
+        site = structure[n]
+        neighs_dists = structure.get_neighbors(site, self.cutoff)
+        min_dist = min([dist for neigh, dist in neighs_dists])
+
+        siw = []
+        for s, dist in neighs_dists:
+            w = min_dist / dist
+            siw.append({'site': s,
+                        'image': self._get_image(structure, s),
+                        'weight': w,
+                        'site_index': self._get_original_site(structure, s)})
+        return siw
 
 
 class MigrationPathAnalyzer():
@@ -151,9 +224,92 @@ class MigrationPathAnalyzer():
         res.merge_sites(tol=1.0, mode='average')
         return res
 
-    def get_hops(self):
-        # enumerate all of the hops in a systems
-        pass
+    def get_graph(self):
+        # Generate the graph edges between these sites
+        self.gt = StructureGraph.with_local_env_strategy(self.full_sites, ConnectSitesNN())
+        self.gt.set_node_attributes()
+
+    def compare_edges(self, edge1, edge2):
+        # Test
+        # p0=nx.get_node_attributes(self.gt.graph, 'properties')[edge1[0]]['label']
+        # p1=nx.get_node_attributes(self.gt.graph, 'properties')[edge1[1]]['label']
+        # pp0=nx.get_node_attributes(self.gt.graph, 'properties')[edge2[0]]['label']
+        # pp1=nx.get_node_attributes(self.gt.graph, 'properties')[edge2[1]]['label']
+        # print(edge1, '{}->{}'.format(p0, p1), '{}->{}'.format(pp0, pp1), edge2)
+        temp_struct1 = self.base_entry.structure.copy()
+        temp_struct2 = self.base_entry.structure.copy()
+
+        temp_struct1.insert(0, self.cation, self._edgelist.iloc[edge1]['i_pos'])
+        temp_struct1.insert(0, self.cation, self._edgelist.iloc[edge1]['f_pos'])
+        temp_struct2.insert(0, self.cation, self._edgelist.iloc[edge2]['i_pos'])
+        temp_struct2.insert(0, self.cation, self._edgelist.iloc[edge2]['f_pos'])
+        return grouper_sm.fit(temp_struct1, temp_struct2)
+
+    def get_edges_labels(self, mask_file=None):
+        d = [{"isite" : u, "fsite" : v, "to_jimage" : d['to_jimage'], 'edge_tuple' : (u, v)} for u, v, d in self.gt.graph.edges(data=True)]
+        self._edgelist = pd.DataFrame(d)
+
+
+        self._edgelist['i_pos'] = self._edgelist.apply(lambda u : self.full_sites.sites[u.isite].frac_coords, axis=1)
+        self._edgelist['f_pos'] = self._edgelist.apply(lambda u : self.full_sites.sites[u.fsite].frac_coords + u.to_jimage, axis=1)
+
+        edge_lab = generic_groupby(self._edgelist.index.values, comp = self.compare_edges)
+        self._edgelist['edge_label'] = edge_lab
+
+        # write the image
+        self.unique_edges = self._edgelist.drop_duplicates('edge_label', keep='first')
+        print(self.unique_edges)
+
+        #
+        # # set up the grid
+        # aa = np.linspace(0, 1, len(self.chgcar.get_axis_grid(0)),
+        #                  endpoint=False)
+        # bb = np.linspace(0, 1, len(self.chgcar.get_axis_grid(1)),
+        #                  endpoint=False)
+        # cc = np.linspace(0, 1, len(self.chgcar.get_axis_grid(2)),
+        #                  endpoint=False)
+        # AA, BB, CC = np.meshgrid(aa, bb, cc, indexing='ij')
+        # fcoords = np.vstack([AA.flatten(), BB.flatten(), CC.flatten()]).T
+        #
+        # IMA, IMB, IMC = np.meshgrid([-1, 0, 1], [-1, 0, 1], [-1, 0, 1], indexing='ij')
+        # images = np.vstack([IMA.flatten(), IMB.flatten(), IMC.flatten()]).T
+        #
+        # # get the charge density masks for each hop (for plotting and sanity check purposes)
+        # idx_pbc_mask = np.zeros_like(AA)
+        # surf_idx=0
+        # total_chg=[]
+        # if mask_file:
+        #     mask_out = copy(self.chgcar)
+        #     mask_out.data['total'] = np.zeros_like(AA)
+        #
+        # for _, row in self.unique_edges.iterrows():
+        #     pbc_mask = np.zeros_like(AA).flatten()
+        #     e0 = row[['pos0x', 'pos0y', 'pos0z']].astype('float64').values
+        #     e1 = row[['pos1x', 'pos1y', 'pos1z']].astype('float64').values
+        #
+        #     cart_e0 = np.dot(e0, self.chgcar.structure.lattice.matrix)
+        #     cart_e1 = np.dot(e1, self.chgcar.structure.lattice.matrix)
+        #     pbc_mask = np.zeros_like(AA,dtype=bool).flatten()
+        #     for img in images:
+        #         grid_pos = np.dot(fcoords + img, self.chgcar.structure.lattice.matrix)
+        #         proj_on_line = np.dot(grid_pos - cart_e0, cart_e1 - cart_e0) / (np.linalg.norm(cart_e1 - cart_e0))
+        #         dist_to_line = np.linalg.norm(
+        #             np.cross(grid_pos - cart_e0, cart_e1 - cart_e0) / (np.linalg.norm(cart_e1 - cart_e0)), axis=-1)
+        #
+        #         mask = (proj_on_line >= 0) * (proj_on_line < np.linalg.norm(cart_e1 - cart_e0)) * (dist_to_line < 0.5)
+        #         pbc_mask = pbc_mask + mask
+        #     pbc_mask = pbc_mask.reshape(AA.shape)
+        #     if mask_file:
+        #         mask_out.data['total'] = pbc_mask
+        #         mask_out.write_file('{}_{}.vasp'.format(mask_file,row['edge_tuple']))
+        #
+        #
+        #     total_chg.append(self.chgcar.data['total'][pbc_mask].sum()/self.chgcar.ngridpts/self.chgcar.structure.volume)
+        #
+        # self.complete_mask=idx_pbc_mask
+        # self.unique_edges['chg_total']=total_chg
+
+
 
 def main():
     test_dir = os.path.join(
@@ -162,6 +318,9 @@ def main():
 
     mpa = MigrationPathAnalyzer(test_ents['ent_base'], test_ents['one_cation'])
     print(mpa.full_sites)
+    mpa.get_graph()
+    mpa.get_edges_labels()
+    print(mpa.unique_edges)
 
 
 if __name__ == "__main__":
