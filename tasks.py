073a1d698c6f43312987ce90eaf9ec60ecfebb34
Shyue Ping Ong <shyuep@gmail.com>
2019-05-01 15:37:12 -0700

    Update docs

--- tasks.py
+++ tasks.py
@@ -271,7 +271,11 @@ def release(ctx, notest=False, nodoc=False):
         ctx.run("nosetests")
     publish(ctx)
     if not nodoc:
-        update_doc(ctx)
+        # update_doc(ctx)
+        make_doc(ctx)
+        ctx.run("git add .")
+        ctx.run("git commit -a -m \"Update docs\"")
+        ctx.run("git push")
     merge_stable(ctx)
     release_github(ctx)
     post_discourse(ctx)
