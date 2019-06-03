a56e0301ed2c3f3692c6db30724f6c30cc40b58e
Handong Ling <handongling@berkeley.edu>
2019-06-03 11:40:25 -0700

    site symmetries and piezo sensitivity

--- pymatgen/symmetry/site_symmetries.py
+++ pymatgen/symmetry/site_symmetries.py
@@ -18,7 +18,7 @@ def get_site_symmetries(struc, precision = 0.1):
 
         # Place the origin of the cell at each atomic site
         pointops.append([])
-        for site2 in range(len(strucs.sites)):
+        for site2 in range(len(struc.sites)):
             tempstruc.replace(site2, tempstruc.sites[site2].specie, tempstruc.frac_coords[site2]-struc.frac_coords[site1])
 
 
@@ -31,15 +31,16 @@ def get_site_symmetries(struc, precision = 0.1):
 
 
 # Function that returns the symmetry operations shared by a pair of atoms 
-def get_shared_symmetry_operations(strucs, symops, tol = 0.1):
+def get_shared_symmetry_operations(struc, pointops, tol = 0.1):
+    numsites = len(struc)
     sharedops = [[0 for x in range(numsites)] for y in range(numsites)] 
     for site1 in range(numsites):
         for site2 in range(numsites):
             sharedops[site1][site2] = ([])
-            for op1 in range(len(symops[site1])):
-                for op2 in range(len(symops[site2])):
-                    if np.allclose(symops[site1][op1].rotation_matrix, symops[site2][op2].rotation_matrix):
-                        sharedops[site1][site2].append(symops[site1][op1])
+            for op1 in range(len(pointops[site1])):
+                for op2 in range(len(pointops[site2])):
+                    if np.allclose(pointops[site1][op1].rotation_matrix, pointops[site2][op2].rotation_matrix):
+                        sharedops[site1][site2].append(pointops[site1][op1])
 
     for site1 in range(len(sharedops)):
         for site2 in range(len(sharedops[site1])):
