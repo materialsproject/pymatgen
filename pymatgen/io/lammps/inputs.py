994224dbda061d77122aa83611abca0179ee789e
nwinner <nwinner@berkeley.edu>
2019-06-11 17:56:17 -0700

    Debug

--- pymatgen/io/lammps/inputs.py
+++ pymatgen/io/lammps/inputs.py
@@ -196,7 +196,7 @@ class LammpsInputSet(MSONable):
 
         input_script = script_template
 
-        read_data = re.search(r"read_data\s+(.*)\n", input_filename)
+        read_data = bool(re.search(r"read_data\s+(.*)\n", script_template))
         if read_data:
             if isinstance(self.lammps_data, LammpsData):
                 print("FOUND LAMMPS DATA OBJECT")
@@ -268,3 +268,4 @@ class LammpsInputSet(MSONable):
             string += '\n'
 
         return string
+set = LammpsInputSet.from_file('/Users/nwinner/calculations/sandbox/packingwf/in.lammps', config_dict={}, lammps_data=None)
