58843b257ccbbc19d0376c6a693aff0d7e5f2fee
Joseph Montoya <joseph.montoya@tri.global>
2019-07-23 11:47:21 -0700

    fix composition restriciton

--- pymatgen/analysis/pourbaix_diagram.py
+++ pymatgen/analysis/pourbaix_diagram.py
@@ -843,7 +843,10 @@ class PourbaixDiagram(MSONable):
 
         # Check composition consistency between forced entry and Pourbaix diagram:
         pbx_comp = Composition(self._elt_comp).reduced_composition
-        if entry.composition.reduced_composition != pbx_comp:
+        entry_pbx_comp = Composition(
+            {elt: coeff for elt, coeff in entry.composition.items()
+             if not elt in ELEMENTS_HO}).reduced_composition
+        if entry_pbx_comp != pbx_comp:
             raise ValueError("Composition of stability entry does not match "
                              "Pourbaix Diagram")
         entry_normalized_energy = entry.normalized_energy_at_conditions(pH, V)
