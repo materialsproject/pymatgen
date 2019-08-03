67177f4076990a400b030fbc6e5837b056b725be
Handong Ling <handongling@berkeley.edu>
2019-08-03 15:33:22 -0700

    mcsqs_caller and tests

--- pymatgen/command_line/mcsqs_caller.py
+++ pymatgen/command_line/mcsqs_caller.py
@@ -1,70 +1,119 @@
 import subprocess
 import os 
+import numpy as np
+import time
+from threading import Timer
 from pymatgen.io.vasp.inputs import Poscar
 from pymatgen.io import atat
 
 def run_mcsqs(structure, clusters, supercell = None, total_atoms = None, max_time = None):
     """
-    Helper function for calling aconvasp with different arguments
+    Helper function for calling mcsqs with different arguments
+    max_time (int): max time for running mcsqs in minutes
     """
     num_atoms = len(structure)
     if total_atoms == None:
         total_atoms = num_atoms
 
-    if supercell != None and total_atoms != num_atoms:
-        print("pick supercell or number of atoms")
+    if supercell is not None and total_atoms != num_atoms:
+        print("pick supercell OR number of atoms")
         return
 
     ## Set supercell
-    if supercell == None:
-        supercell = np.eye(3)
-    else:
-        supercell = np.array(supercell)
-        text_file = open("sqscell.out", "w")
-        text_file.write('1\n')
-        for i in range(len(supercell)):
-            text_file.write('\n')
-            for j in range(len(supercell[i])):
-                text_file.write(str(supercell[i][j]) +' ')
+
+    cell = np.eye(3)
+    text_file = open("sqscell.out", "w")
+    text_file.write('1\n')
+    for i in range(len(cell)):
+        text_file.write('\n')
+        for j in range(len(cell[i])):
+            text_file.write(str(cell[i][j]) +' ')
+    text_file.close()
+    struccopy = structure.copy()
+
+    if supercell is not None:
+        struccopy.make_supercell(supercell)
+        struc = atat.Mcsqs(struccopy)
+        text_file = open("rndstr.in", "w")
+        text_file.write(struc.to_string())
         text_file.close()
+
         
-    struc = atat.Mcsqs(structure)
-    text_file = open("rndstr.in", "w")
-    text_file.write(struc.to_string())
-    text_file.close()
-    
-    ## Generate Clusters
-    command = ['mcsqs']
-    for num in clusters:
-        command.append('-'+ str(num) + '=' + str(clusters[num]))
-    p = subprocess.Popen(command, stdout=subprocess.PIPE,
-                         stdin=subprocess.PIPE,
-                         stderr=subprocess.PIPE)
-    ## Create stop file
-    text_file = open("stopsqs", "w")
-    text_file.close()
-    ## Run mcsqs
-    command = ['mcsqs -n ' + str(np.prod(supercell)*total_atoms)]
-    p = subprocess.Popen(command, stdout=subprocess.PIPE,
-                         stdin=subprocess.PIPE,
-                         stderr=subprocess.PIPE)
-    os.remove('sqscell.out')
-    if max_time:
-        timed_out = False
-        timer = Timer(max_time*60, lambda p: p.kill(), [p])
+        ## Generate Clusters
+        command = ['mcsqs']
+        for num in clusters:
+            command.append('-'+ str(num) + '=' + str(clusters[num]))
 
-        try:
-            timer.start()
-            output = p.communicate()[0].decode("utf-8")
-        finally:
-            if not timer.is_alive():
-                timed_out = True
-            timer.cancel()
+        p = subprocess.Popen(command, stdout=subprocess.PIPE,
+                             stdin=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        ## Create stop file
+        text_file = open("stopsqs", "w")
+        text_file.close()
+        ## Run mcsqs
+        time.sleep(0.01)
+        command = ['mcsqs',  '-rc', "-n {}".format(len(structure))]
+        p = subprocess.Popen(command, stdout=subprocess.PIPE,
+                             stdin=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
 
-        if timed_out:
-            raise TimeoutError('Enumeration took too long.')
-            
+        if max_time:
+            timed_out = False
+            timer = Timer(max_time*60, lambda p: p.kill(), [p])
+
+            try:
+                timer.start()
+                output = p.communicate()[0].decode("utf-8")
+            finally:
+                if not timer.is_alive():
+                    timed_out = True
+                timer.cancel()
+
+            if timed_out:
+                raise TimeoutError('Cluster expansion took too long.')
+    else:
+        struc = atat.Mcsqs(struccopy)
+        text_file = open("rndstr.in", "w")
+        text_file.write(struc.to_string())
+        text_file.close()
+
+        
+        ## Generate Clusters
+        command = ['mcsqs']
+        for num in clusters:
+            command.append('-'+ str(num) + '=' + str(clusters[num]))
+
+        p = subprocess.Popen(command, stdout=subprocess.PIPE,
+                             stdin=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        ## Create stop file
+        text_file = open("stopsqs", "w")
+        text_file.close()
+        ## Run mcsqs
+
+        time.sleep(0.01)
+        command = ['mcsqs', "-n {}".format(total_atoms)]
+        p = subprocess.Popen(command, stdout=subprocess.PIPE,
+                             stdin=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        if max_time:
+            timed_out = False
+            timer = Timer(max_time*60, lambda p: p.kill(), [p])
+
+            try:
+                timer.start()
+                output = p.communicate()[0].decode("utf-8")
+            finally:
+                if not timer.is_alive():
+                    timed_out = True
+                timer.cancel()
+
+            if timed_out:
+                raise TimeoutError('Cluster expansion took too long.')
+        
+    time.sleep(0.01)
     text_file = open("bestsqs.out", "r")
     bestsqs = text_file.read()
+    text_file.close()
 
     return atat.Mcsqs.structure_from_string(bestsqs)
--- pymatgen/command_line/tests/test_mcsqs_caller.py
+++ pymatgen/command_line/tests/test_mcsqs_caller.py
@@ -1,20 +1,68 @@
 import unittest
 
 from pymatgen.core.structure import Structure
-from pymatgen.command_line.critic2_caller import *
+import subprocess
+import os 
+import numpy as np
+from pymatgen.io.vasp.inputs import Poscar
+from pymatgen.io import atat
+from pymatgen.util.testing import PymatgenTest
+from pymatgen.command_line.mcsqs_caller import run_mcsqs
 from monty.os.path import which
 
-__author__ = "Handong ling
+__author__ = "Handong ling"
 __version__ = "0.1"
 __maintainer__ = "Handong Ling"
 __email__ = "handongling@berkeley.edu"
 __date__ = "June 2019"
 
+test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..",
+                        'test_files', "mcsqs")
 
 @unittest.skipIf(not which('mcsqs'), "mcsqs executable not present")
-class Mcsqs_CallerTest(unittest.TestCase):
+class Mcsqs_CallerTest(PymatgenTest):
 	def setUp(self):
+		self.pztstrings = np.load(os.path.join(test_dir, "pztstrings.npy"), allow_pickle=True)
+		self.pztstrings2 = np.load(os.path.join(test_dir, "pztstrings2.npy"), allow_pickle=True)
+		self.struc = self.get_structure('Pb2TiZrO6')
+		self.struc.replace_species({'Ti': {'Ti':0.5, 'Zr':0.5 }, 'Zr': {'Ti':0.5, 'Zr':0.5 }})
 
-        self.pzt = Critic2Output(structure, reference_stdout)
-        self.pztstrings = np.load(os.path.join(test_dir, "pztist.npy"), allow_pickle=True)
-		self.pzt_struc = self.get_structure('Pb2TiZrO6')
+
+	def test_Mcsqs_Caller_supercell(self):
+		count = 0
+		strings = []
+		passed = [] 
+
+		sqs = run_mcsqs(self.struc, {2:6, 3:4}, supercell = [2,1,1], total_atoms = None, max_time = None)
+		self.assertEqual(atat.Mcsqs(sqs).to_string() in self.pztstrings, True)
+		os.remove('sqscell.out')
+		os.remove('rndstrgrp.out')
+		os.remove('bestcorr.out')
+		os.remove('rndstr.in')
+		os.remove('sym.out')
+		os.remove('mcsqs.log')
+		os.remove('bestsqs.out')
+		os.remove('clusters.out')
+
+	def test_Mcsqs_Caller_total_atoms(self):
+		sqs = run_mcsqs(self.struc, {2:6, 3:4}, total_atoms = 20, max_time = None)
+		self.assertEqual(atat.Mcsqs(sqs).to_string() in self.pztstrings2, True)
+		os.remove('sqscell.out')
+		os.remove('rndstrgrp.out')
+		os.remove('bestcorr.out')
+		os.remove('rndstr.in')
+		os.remove('sym.out')
+		os.remove('mcsqs.log')
+		os.remove('bestsqs.out')
+		os.remove('clusters.out')
+
+	def test_Mcsqs_Caller_supercell_timeout(self):
+		# sqs = run_mcsqs(self.struc, {2:6, 3:4}, supercell = 1000, max_time = 0.000001)
+		self.assertRaises(TimeoutError, run_mcsqs, self.struc, {2:6, 3:4}, supercell = [2,1,1], max_time = 0.000001)		
+		# os.remove('sqscell.out')
+		# os.remove('rndstrgrp.out')
+		# os.remove('bestcorr.out')
+		# os.remove('rndstr.in')
+		# os.remove('sym.out')
+		# os.remove('mcsqs.log')
+		# os.remove('clusters.out')
