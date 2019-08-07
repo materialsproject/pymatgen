c6c2b273eb8cbcfd5ae1b5e5805912a1f80a7e9e
ardunn <ardunn@lbl.gov>
2019-08-06 18:16:25 -0700

    add places arg for bv test

--- pymatgen/analysis/tests/test_bond_valence.py
+++ pymatgen/analysis/tests/test_bond_valence.py
@@ -72,14 +72,14 @@ class BondValenceSumTest(PymatgenTest):
         s = Structure.from_file(os.path.join(test_dir, "LiMn2O4.json"))
         neighbors = s.get_neighbors(s[0], 3.0)
         bv_sum = calculate_bv_sum(s[0], neighbors)
-        self.assertAlmostEqual(bv_sum, 0.7723402182087497)
+        self.assertAlmostEqual(bv_sum, 0.7723402182087497, places=5)
 
     def test_calculate_bv_sum_unordered(self):
         s = Structure.from_file(os.path.join(test_dir, "LiMn2O4.json"))
         s[0].species = Composition("Li0.5Na0.5")
         neighbors = s.get_neighbors(s[0], 3.0)
         bv_sum = calculate_bv_sum_unordered(s[0], neighbors)
-        self.assertAlmostEqual(bv_sum, 1.5494662306918852)
+        self.assertAlmostEqual(bv_sum, 1.5494662306918852, places=5)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
