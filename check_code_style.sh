cfc70f928b0808dab0b240f3d399ff3584197a67
zyrd <shyuep@gmail.com>
2019-08-23 06:35:25 -0700

    Fix minor bug.

--- check_code_style.sh
+++ check_code_style.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pycodestyle pymatgen/core pymatgen/ext pymatgen/entries pymatgen/util pymatgen/electronic_structure pymatgen/cli pymatgen/apps pymatgen/analysis pymatgen.alchemy pymatgen/vis pymatgen/symmetry pymatgen/io/vasp
+pycodestyle pymatgen/core pymatgen/ext pymatgen/entries pymatgen/util pymatgen/electronic_structure pymatgen/cli pymatgen/apps pymatgen/analysis pymatgen/alchemy pymatgen/vis pymatgen/symmetry pymatgen/io/vasp
