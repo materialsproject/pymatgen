55524306a133fb152ee5ade4538cdc46be190fe3
zyrd <shyuep@gmail.com>
2019-08-22 17:01:05 -0700

    add diffraction.

--- check_code_style.sh
+++ check_code_style.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pycodestyle pymatgen/core pymatgen/ext pymatgen/entries pymatgen/util pymatgen/electronic_structure pymatgen/cli pymatgen/apps pymatgen/analysis/hhi pymatgen/analysis/magnetism pymatgen/analysis/ferroelectricity pymatgen/analysis/structure_prediction
+pycodestyle pymatgen/core pymatgen/ext pymatgen/entries pymatgen/util pymatgen/electronic_structure pymatgen/cli pymatgen/apps pymatgen/analysis/hhi pymatgen/analysis/magnetism pymatgen/analysis/ferroelectricity pymatgen/analysis/structure_prediction pymatgen/analysis/diffraction
