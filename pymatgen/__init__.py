84c4cc3ac862967d9d5b53fdb72d93cf50f4ca3c
Matthew Horton <mkhorton@lbl.gov>
2019-09-08 01:32:12 -0400

    Update version to v2019.9.8

--- pymatgen/__init__.py
+++ pymatgen/__init__.py
@@ -11,7 +11,7 @@ __author__ = "Pymatgen Development Team"
 __email__ = "pymatgen@googlegroups.com"
 __maintainer__ = "Shyue Ping Ong"
 __maintainer_email__ = "shyuep@gmail.com"
-__version__ = "2019.9.7"
+__version__ = "2019.9.8"
 
 SETTINGS_FILE = os.path.join(os.path.expanduser("~"), ".pmgrc.yaml")
 
