21488988ef1ebc70d2ac9a2fbde91a7d13298708
Unknown <sivonxay@gmail.com>
2019-09-11 18:06:03 -0700

    Added frame properties and updated site properties

--- pymatgen/core/trajectory.py
+++ pymatgen/core/trajectory.py
@@ -2,13 +2,16 @@
 # Copyright (c) Pymatgen Development Team.
 # Distributed under the terms of the MIT License.
 
+import itertools
+import os
+import warnings
+from fnmatch import fnmatch
+
+import numpy as np
+from monty.io import zopen
+from monty.json import MSONable
 from pymatgen.core.structure import Structure, Lattice
 from pymatgen.io.vasp.outputs import Xdatcar, Vasprun
-from monty.json import MSONable
-from fnmatch import fnmatch
-import numpy as np
-import warnings
-import os
 
 """
 This module provides classes used to define a MD trajectory.
@@ -25,22 +28,20 @@ class Trajectory(MSONable):
     Provides basic functions such as slicing trajectory or obtaining displacements.
     """
 
-    def __init__(self, lattice, species, frac_coords, time_step=2, site_properties=None, constant_lattice=True,
+    def __init__(self, lattice, species, frac_coords, time_step=2, site_properties=None,
+                 frame_properties=None, constant_lattice=True,
                  coords_are_displacement=False, base_positions=None):
         """
         Create a trajectory object
-
         Args:
             lattice: The lattice as any 2D array. Each row should correspond to a lattice
                 vector. E.g., [[10,0,0], [20,10,0], [0,0,30]] specifies a
                 lattice with lattice vectors [10,0,0], [20,10,0] and [0,0,30].
             species: List of species on each site. Can take in flexible input,
                 including:
-
                 i.  A sequence of element / specie specified either as string
                     symbols, e.g. ["Li", "Fe2+", "P", ...] or atomic numbers,
                     e.g., (3, 56, ...) or actual Element or Specie objects.
-
                 ii. List of dict of elements/species and occupancies, e.g.,
                     [{"Fe" : 0.5, "Mn":0.5}, ...]. This allows the setup of
                     disordered structures.
@@ -52,6 +53,8 @@ class Trajectory(MSONable):
                 have to be the same length as the atomic species and fractional_coords. Number of supplied
                 dicts should match number of frames in trajectory
                 Defaults to None for no properties.
+            frame_properties (dict): Properties of the trajectory such as energy, pressure, etc. each property should
+                have a length equal to the trajectory length. eg: {'energy': [#, #, #, #], 'pressure': [0, 0.1, 0 0.02]}
             constant_lattice (bool): Whether the lattice changes during the simulation, such as in an NPT MD simulation.
             coords_are_displacement (bool): Whether supplied coordinates are given in displacements (True) or
                 positions (False)
@@ -69,20 +72,21 @@ class Trajectory(MSONable):
         self.frac_coords = frac_coords
         if coords_are_displacement:
             if base_positions is None:
-                warnings.warn(
-                    "Without providing starting positions, the positions for each time step will not be available")
+                warnings.warn("Without providing an array of starting positions, \
+                               the positions for each time step will not be available")
             self.base_positions = base_positions
         else:
             self.base_positions = frac_coords[0]
         self.coords_are_displacement = coords_are_displacement
 
         if not constant_lattice and np.shape(lattice) == (3, 3):
-            self.lattice = [lattice for i in range(self.frac_coords.shape[0])]
+            self.lattice = [lattice for i in range(np.shape(self.frac_coords)[0])]
         else:
             self.lattice = lattice
         self.constant_lattice = constant_lattice
         self.species = species
         self.site_properties = site_properties
+        self.frame_properties = frame_properties
         self.time_step = time_step
 
     def get_structure(self, i):
@@ -90,7 +94,6 @@ class Trajectory(MSONable):
         Returns structure at specified index
         Args:
             i (int): Index of structure
-
         Returns:
             (Structure) pymatgen structure object
         """
@@ -124,10 +127,8 @@ class Trajectory(MSONable):
     def extend(self, trajectory):
         """
         Concatenate another trajectory
-
         Args:
             trajectory (Trajectory): Trajectory to add
-
         """
         if self.time_step != trajectory.time_step:
             raise ValueError('Trajectory not extended: Time steps of trajectories is incompatible')
@@ -135,18 +136,21 @@ class Trajectory(MSONable):
         if len(self.species) != len(trajectory.species) and self.species != trajectory.species:
             raise ValueError('Trajectory not extended: species in trajectory do not match')
 
+        # Ensure both trajectories are in positions before combining
         self.to_positions()
         trajectory.to_positions()
 
         self.frac_coords = np.concatenate((self.frac_coords, trajectory.frac_coords), axis=0)
-        self.lattice, self.constant_lattice = self._combine_attribute(self.lattice, trajectory.lattice,
-                                                                      self.frac_coords.shape[0],
-                                                                      trajectory.frac_coords.shape[0])
-        self.site_properties = self._combine_attribute(self.site_properties, trajectory.site_properties,
-                                                       self.frac_coords.shape[0], trajectory.frac_coords.shape[0])
+        self.lattice, self.constant_lattice = self._combine_lattice(self.lattice, trajectory.lattice,
+                                                                    np.shape(self.frac_coords)[0],
+                                                                    np.shape(trajectory.frac_coords)[0])
+        if self.site_properties and trajectory.site_properties:
+            self.site_properties = self._combine_site_props(self.site_properties, trajectory.site_properties,
+                                                            np.shape(self.frac_coords)[0],
+                                                            np.shape(trajectory.frac_coords))
 
     def __iter__(self):
-        for i in range(self.frac_coords.shape[0]):
+        for i in range(np.shape(self.frac_coords)[0]):
             yield self[i]
 
     def __len__(self):
@@ -256,12 +260,10 @@ class Trajectory(MSONable):
     def from_file(cls, filename, constant_lattice=True, **kwargs):
         """
         Convenience constructor to obtain trajectory from XDATCAR or vasprun.xml file
-
         Args:
             filename (str): The filename to read from.
             constant_lattice (bool): Whether the lattice changes during the simulation, such as in an NPT MD
                 simulation. True results in
-
         Returns:
             (Trajectory)
         """
@@ -282,6 +284,7 @@ class Trajectory(MSONable):
              "@class": self.__class__.__name__,
              "species": self.species, "time_step": self.time_step,
              "site_properties": self.site_properties,
+             "frame_proprties": self.frame_properties,
              "constant_lattice": self.constant_lattice,
              "coords_are_displacement": self.coords_are_displacement,
              "base_positions": self.base_positions}
@@ -295,15 +298,129 @@ class Trajectory(MSONable):
         """
         Helper function to combine trajectory properties such as site_properties or lattice
         """
-        if isinstance(attr_1, list) or isinstance(attr_2, list):
+        if np.shape(attr_1) == (3, 3) and np.shape(attr_2) == (3, 3):
+            attribute = attr_1
+            attribute_constant = True
+        elif np.shape(attr_1) == 3 and np.shape(attr_2) == 3:
             attribute = np.concatenate((attr_1, attr_2), axis=0)
-            attribute_changes = True
+            attribute_constant = False
         else:
-            if isinstance(attr_1, list) and isinstance(attr_2, list) and np.allclose(attr_1, attr_2):
-                attribute = attr_1
-                attribute_changes = False
+            attribute = [attr_1.copy()] * len_1 if type(attr_1) != list else attr_1.copy()
+            attribute.extend([attr_2.copy()] * len_2 if type(attr_2 != list) else attr_2.copy())
+            attribute_constant = False
+        return attribute, attribute_constant
+
+    @staticmethod
+    def _combine_site_props(attr_1, attr_2, len_1, len_2):
+        if attr_1 is None and attr_2 is None:
+            return None
+        elif len(attr_1) == 1 and len(attr_2) == 1:
+            # If both properties lists are do not change within their respective trajectory
+            if attr_1 == attr_2:
+                # If both site_properties are the same, only store one
+                return attr_1
             else:
-                attribute = [attr_1.copy()] * len_1 if type(attr_1) != list else attr_1.copy()
-                attribute.extend([attr_2.copy()] * len_2 if type(attr_2 != list) else attr_2.copy())
-                attribute_changes = True
-        return attribute, attribute_changes
+                new_site_properties = [attr_1[0] for i in range(len_1)]
+                new_site_properties.extend([attr_2[0] for i in range(len_2)])
+                return new_site_properties
+        elif len(attr_1) > 1 and len(attr_2) > 1:
+            # Both properties have site properties that change within the trajectory, concat both together
+            return [*attr_1, *attr_2]
+        else:
+            new_site_properties = []
+            if attr_1 is None:
+                new_site_properties.extend([None for i in range(len_1)])
+            elif len(attr_1) == 1:
+                new_site_properties.extend([attr_1[0] for i in range(len_1)])
+            elif len(attr_1) > 1:
+                new_site_properties.extend(attr_1)
+
+            if attr_2 is None:
+                new_site_properties.extend([None for i in range(len_2)])
+            elif len(attr_2) == 1:
+                new_site_properties.extend([attr_2[0] for i in range(len_2)])
+            elif len(attr_2) > 1:
+                new_site_properties.extend(attr_2)
+
+            return new_site_properties
+
+    @staticmethod
+    def _combine_frame_props(attr_1, attr_2, len_1, len_2):
+        """
+        Helper function to combine frame properties such as energy or pressure
+        """
+        if attr_1 is None and attr_2 is None:
+            return None
+
+        # Find all common keys
+        all_keys = set(attr_1.keys()) + set(attr_2.keys())
+
+        # Initialize dict with the common keys
+        new_frame_props = dict(zip(all_keys, [[] for i in all_keys]))
+        for key in all_keys:
+            if key in attr_1.keys():
+                new_frame_props[key].extend(attr_1[key])
+            else:
+                # If key doesn't exist in the first trajectory, append None for each index
+                new_frame_props[key].extend([None for i in range(len_1)])
+
+            if key in attr_2.keys():
+                new_frame_props[key].extend(attr_2[key])
+            else:
+                # If key doesn't exist in the second trajectory, append None for each index
+                new_frame_props[key].extend([None for i in range(len_2)])
+
+        return new_frame_props
+
+    def write_Xdatcar(self, filename="XDATCAR", system=None, significant_figures=6):
+        """
+        Writes Xdatcar to a file. The supported kwargs are the same as those for
+        the Xdatcar_from_structs.get_string method and are passed through directly.
+        Args:
+            filename (str): name of file (It's prudent to end the filename with 'XDATCAR',
+                as most visualization and analysis software require this for autodetection)
+            system (str): Description of system
+            significant_figures (int): Significant figures in the output file
+        """
+
+        # Ensure trajectory is in position form
+        self.to_positions()
+
+        if system is None:
+            system = f'{self[0].composition.reduced_formula}'
+
+        lines = []
+        format_str = "{{:.{0}f}}".format(significant_figures)
+        syms = [site.specie.symbol for site in self[0]]
+        site_symbols = [a[0] for a in itertools.groupby(syms)]
+        syms = [site.specie.symbol for site in self[0]]
+        natoms = [len(tuple(a[1])) for a in itertools.groupby(syms)]
+
+        for si, frac_coords in enumerate(self.frac_coords):
+            # Only print out the info block if
+            if self.constant_lattice and si == 0:
+                lines.extend([system, "1.0"])
+
+                if self.constant_lattice:
+                    _lattice = self.lattice
+                else:
+                    _lattice = self.lattice[si]
+
+                for latt_vec in _lattice:
+                    lines.append(f'{" ".join([str(el) for el in latt_vec])}')
+
+                lines.append(" ".join(site_symbols))
+                lines.append(" ".join([str(x) for x in natoms]))
+
+            lines.append(f"Direct configuration=     {str(si + 1)}")
+
+            for (frac_coord, specie) in zip(frac_coords, self.species):
+                coords = frac_coord
+                line = f'{" ".join([format_str.format(c) for c in coords])} {specie}'
+                lines.append(line)
+
+        xdatcar_string = "\n".join(lines) + "\n"
+
+        with zopen(filename, "wt") as f:
+            f.write(xdatcar_string)
+
