2131e3e95477995947b43864ef9fda2294b2a6dc
zyrd <shyuep@gmail.com>
2019-10-07 16:48:48 -0700

    Fix copy for SymmetrizedStructure.

--- pymatgen/symmetry/structure.py
+++ pymatgen/symmetry/structure.py
@@ -2,19 +2,14 @@
 # Copyright (c) Pymatgen Development Team.
 # Distributed under the terms of the MIT License.
 
-import numpy as np
-from pymatgen.core.structure import Structure
-
 """
 This module implements symmetry-related structure forms.
 """
 
-__author__ = "Shyue Ping Ong"
-__copyright__ = "Copyright 2012, The Materials Project"
-__version__ = "0.1"
-__maintainer__ = "Shyue Ping Ong"
-__email__ = "shyuep@gmail.com"
-__date__ = "Mar 9, 2012"
+from typing import Sequence, List
+
+import numpy as np
+from pymatgen.core.structure import Structure, PeriodicSite
 
 
 class SymmetrizedStructure(Structure):
@@ -24,41 +19,51 @@ class SymmetrizedStructure(Structure):
     typically not called but instead is typically obtained by calling
     pymatgen.symmetry.analyzer.SpacegroupAnalyzer.get_symmetrized_structure.
 
-    Args:
-        structure (Structure): Original structure
-        spacegroup (SpacegroupOperations): An input SpacegroupOperations from
-            SpacegroupAnalyzer.
-        equivalent_positions: Equivalent positions from SpacegroupAnalyzer.
-
     .. attribute: equivalent_indices
 
         indices of structure grouped by equivalency
     """
 
-    def __init__(self, structure, spacegroup, equivalent_positions,
-                 wyckoff_letters):
+    def __init__(self, structure: Structure,
+                 spacegroup,
+                 equivalent_positions: Sequence[int],
+                 wyckoff_letters: Sequence[str]):
+        """
+        Args:
+            structure (Structure): Original structure
+            spacegroup (SpacegroupOperations): An input SpacegroupOperations from
+                SpacegroupAnalyzer.
+            equivalent_positions: Equivalent positions from SpacegroupAnalyzer.
+            wyckoff_letters: Wyckoff letters
+        """
         self.spacegroup = spacegroup
         u, inv = np.unique(equivalent_positions, return_inverse=True)
         self.site_labels = equivalent_positions
-
-        # site_properties = structure.site_properties
-        # site_properties["wyckoff"] = [
-        #     "%d%s" % (list(self.site_labels).count(self.site_labels[i]),
-        #               wyckoff_letters[i]) for i in range(len(structure))]
 
         super().__init__(
             structure.lattice, [site.species for site in structure],
             structure.frac_coords, site_properties=structure.site_properties)
 
-        self.equivalent_indices = [[] for i in range(len(u))]
-        self.equivalent_sites = [[] for i in range(len(u))]
-        wyckoff_symbols = [[] for i in range(len(u))]
+        equivalent_indices = [[] for _ in range(len(u))]  # type: ignore
+        equivalent_sites = [[] for _ in range(len(u))]  # type: ignore
+        wyckoff_symbols = [[] for _ in range(len(u))]  # type: ignore
         for i, inv in enumerate(inv):
-            self.equivalent_indices[inv].append(i)
-            self.equivalent_sites[inv].append(self.sites[i])
+            equivalent_indices[inv].append(i)
+            equivalent_sites[inv].append(self.sites[i])
             wyckoff_symbols[inv].append(wyckoff_letters[i])
+        self.equivalent_indices: List[int] = equivalent_indices  # type: ignore
+        self.equivalent_sites: List[PeriodicSite] = equivalent_sites  # type: ignore
+        self.wyckoff_letters = wyckoff_letters
         self.wyckoff_symbols = ["%d%s" % (len(w), w[0])
                                 for w in wyckoff_symbols]
+
+    def copy(self):
+        """
+        :return: Copy of structure.
+        """
+        return self.__class__(self, spacegroup=self.spacegroup,
+                              equivalent_positions=self.site_labels,
+                              wyckoff_letters=self.wyckoff_letters)
 
     def find_equivalent_sites(self, site):
         """
