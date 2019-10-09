7002de71878da2d294ed34d37dfb77077e487a43
JaGeo <janinegeorge.ulfen@gmail.com>
2019-10-09 14:39:51 +0200

    flake 8 error

--- pymatgen/io/lobster.py
+++ pymatgen/io/lobster.py
@@ -18,7 +18,7 @@ import fnmatch
 import itertools
 import warnings
 import spglib
-from typing import List, Dict, Any, Optional, Tuple
+from typing import List, Dict, Any, Optional
 from collections import defaultdict
 from pymatgen.electronic_structure.core import Spin, Orbital
 from pymatgen.io.vasp.outputs import Vasprun
