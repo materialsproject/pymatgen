1c5964fb7e7bf3966495f94401538096f67188b3
smheidrich <smheidrich@weltenfunktion.de>
2019-12-03 21:19:51 +0100

    Add pymatgen.Molecule support to AseAtomsAdaptor

--- pymatgen/io/tests/test_ase.py
+++ pymatgen/io/tests/test_ase.py
@@ -6,7 +6,7 @@
 import unittest
 import os
 
-from pymatgen import Composition
+from pymatgen import Composition, Molecule
 from pymatgen.io.vasp.inputs import Poscar
 import pymatgen.io.ase as aio
 
@@ -17,12 +17,23 @@ test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..",
 class AseAtomsAdaptorTest(unittest.TestCase):
 
     @unittest.skipIf(not aio.ase_loaded, "ASE not loaded.")
-    def test_get_atoms(self):
+    def test_get_atoms_from_structure(self):
         p = Poscar.from_file(os.path.join(test_dir, 'POSCAR'))
         structure = p.structure
         atoms = aio.AseAtomsAdaptor.get_atoms(structure)
         ase_composition = Composition(atoms.get_chemical_formula())
         self.assertEqual(ase_composition, structure.composition)
+        self.assertTrue(atoms.cell is not None and atoms.cell.any())
+        self.assertTrue(atoms.get_pbc() is not None and atoms.get_pbc().all())
+
+    @unittest.skipIf(not aio.ase_loaded, "ASE not loaded.")
+    def test_get_atoms_from_molecule(self):
+        m = Molecule.from_file(os.path.join(test_dir, 'acetylene.xyz'))
+        atoms = aio.AseAtomsAdaptor.get_atoms(m)
+        ase_composition = Composition(atoms.get_chemical_formula())
+        self.assertEqual(ase_composition, m.composition)
+        self.assertTrue(atoms.cell is None or not atoms.cell.any())
+        self.assertTrue(atoms.get_pbc() is None or not atoms.get_pbc().any())
 
     @unittest.skipIf(not aio.ase_loaded, "ASE not loaded.")
     def test_get_structure(self):
@@ -30,6 +41,13 @@ class AseAtomsAdaptorTest(unittest.TestCase):
         atoms = aio.AseAtomsAdaptor.get_atoms(p.structure)
         self.assertEqual(aio.AseAtomsAdaptor.get_structure(atoms).formula,
                          "Fe4 P4 O16")
+
+    @unittest.skipIf(not aio.ase_loaded, "ASE not loaded.")
+    def test_get_molecule(self):
+        m = Molecule.from_file(os.path.join(test_dir, 'acetylene.xyz'))
+        atoms = aio.AseAtomsAdaptor.get_atoms(m)
+        self.assertEqual(aio.AseAtomsAdaptor.get_molecule(atoms).formula,
+                         "H2 C2")
 
 
 if __name__ == "__main__":
