7babeb345de8f0c0b98c629abd3b99bb84546c46
Matthew Horton <mkhorton@lbl.gov>
2020-01-10 15:23:34 -0800

    v2020.1.10 release

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ who require Python 2.7 should install pymatgen v2018.x.
 setup(
     name="pymatgen",
     packages=find_packages(),
-    version="2019.12.22",
+    version="2020.1.10",
     cmdclass={'build_ext': build_ext},
     setup_requires=['numpy>=1.14.3', 'setuptools>=18.0'],
     python_requires='>=3.6',
