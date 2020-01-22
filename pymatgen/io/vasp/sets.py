0f6389699d66102dbde7ffa57eed48e4d6a8d129
Francesco Ricci <frankyricci@gmail.com>
2020-01-22 16:29:08 +0100

    isym=1 bug fix and automatic nedos

--- pymatgen/io/vasp/sets.py
+++ pymatgen/io/vasp/sets.py
@@ -1277,6 +1277,7 @@ class MPNonSCFSet(MPRelaxSet):
         prev_incar=None,
         mode="line",
         nedos=2001,
+        dedos=0.005,
         reciprocal_density=100,
         sym_prec=0.1,
         kpoints_line_density=20,
@@ -1313,6 +1314,7 @@ class MPNonSCFSet(MPRelaxSet):
         self.prev_incar = prev_incar
         self.kwargs = kwargs
         self.nedos = nedos
+        self.dedos = dedos
         self.reciprocal_density = reciprocal_density
         self.sym_prec = sym_prec
         self.kpoints_line_density = kpoints_line_density
@@ -1358,7 +1360,7 @@ class MPNonSCFSet(MPRelaxSet):
 
         if self.mode.lower() == "uniform":
             # use tetrahedron method for DOS and optics calculations
-            incar.update({"ISMEAR": -5})
+            incar.update({"ISMEAR": -5, "ISYM": 1})
         else:
             # if line mode, can't use ISMEAR=-5; also use small sigma to avoid
             # partial occupancies for small band gap materials.
@@ -1489,6 +1491,12 @@ class MPNonSCFSet(MPRelaxSet):
                     self.kpoints_line_density * self.small_gap_multiply[1]
                 )
 
+        # automatic setting of nedos using total energy range and energy step dedos
+        if self.nedos == 0:
+            emax = max([eigs.max() for eigs in vasprun.eigenvalues.values()])
+            emin = min([eigs.min() for eigs in vasprun.eigenvalues.values()])
+            self.nedos = int((emax - emin) / self.dedos) 
+            
         return self
 
     @classmethod
