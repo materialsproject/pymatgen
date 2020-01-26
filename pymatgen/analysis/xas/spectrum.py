296e70677407f650e490ae777fec660d383d0f49
Yiming Chen <yic111@eng.ucsd.edu>
2020-01-26 09:07:14 -0800

    add EXAFS and stitching

--- pymatgen/analysis/xas/spectrum.py
+++ pymatgen/analysis/xas/spectrum.py
@@ -2,19 +2,24 @@
 # Copyright (c) Pymatgen Development Team.
 # Distributed under the terms of the MIT License.
 
+"""
+This module defines classes to represent all xas and stitching methods
+"""
+
+from pymatgen.analysis.structure_matcher import StructureMatcher
+from pymatgen.core import Element
 from pymatgen.core.spectrum import Spectrum
+from scipy.interpolate import interp1d
 import numpy as np
+import math
 
-"""
-This module defines classes to represent all xas
-"""
 
-__author__ = "Chen Zheng"
+__author__ = "Chen Zheng, Yiming Chen"
 __copyright__ = "Copyright 2012, The Materials Project"
-__version__ = "2.0"
-__maintainer__ = "Chen Zheng"
-__email__ = "chz022@ucsd.edu"
-__date__ = "Aug 9, 2017"
+__version__ = "3.0"
+__maintainer__ = "Yiming Chen"
+__email__ = "chz022@ucsd.edu, yic111@ucsd.edu"
+__date__ = "Jan 26, 2020"
 
 
 class XANES(Spectrum):
@@ -24,9 +29,10 @@ class XANES(Spectrum):
     Args:
         x: A sequence of x-ray energies in eV
         y: A sequence of mu(E)
-        structure (Structure): Structure associated with the XANES.
-        absorption_specie (Specie): Specie associated with the XANES.
-        edge (str):
+        structure (Structure): Structure associated with the XANES
+        absorption_element (Element): Element associated with the XANES
+        edge (str): Absorption edge associated with the XANES
+
 
     .. attribute: x
         The sequence of energies
@@ -34,25 +40,176 @@ class XANES(Spectrum):
     .. attribute: y
         The sequence of mu(E)
 
-    .. attribute: absorption_specie
-        The absorption_species of the spectrum
+    .. attribute: absorption_element
+        The absorption_element of the spectrum
 
     .. attribute: edge
-        The edge of XANES spectrum
+        The edge of the spectrum
+
     """
     XLABEL = 'Energy'
     YLABEL = 'Intensity'
 
-    def __init__(self, x, y, structure, absorption_specie, edge):
-        super().__init__(x, y, structure, absorption_specie, edge)
+    def __init__(self, x, y, structure, absorption_element, edge):
+        super(XANES, self).__init__(x, y, structure, absorption_element, edge)
         self.structure = structure
-        self.absorption_specie = absorption_specie
+        self.absorption_element = absorption_element
         self.edge = edge
         self.e0 = self.x[np.argmax(np.gradient(self.y) / np.gradient(self.x))]
+        # Wavenumber, k is calculated based on equation
+        #             k^2=2*m/(hbar)^2*(E-E0)
+        self.k = [np.sqrt((i-self.e0)/3.854) if i > self.e0 else
+                           -np.sqrt((self.e0-i)/3.854) for i in self.x]
 
     def __str__(self):
         return "%s %s Edge for %s: %s" % (
-            self.absorption_specie, self.edge,
+            self.absorption_element, self.edge,
             self.structure.composition.reduced_formula,
-            super().__str__()
+            super(XANES, self).__str__()
         )
+
+
+class EXAFS(Spectrum):
+    """
+    Basic EXAFS object.
+
+    Args:
+        x: A sequence of x-ray energies in eV
+        y: A sequence of mu(E)
+        structure (Structure): Structure associated with the EXAFS
+        absorption_element (Element): Element associated with the EXAFS
+        edge (str): Absorption edge associated with the EXAFS
+
+    .. attribute: x
+        The sequence of energies
+
+    .. attribute: y
+        The sequence of mu(E)
+
+    .. attribute: absorption_element
+        The absorption_element of the spectrum
+
+    .. attribute: edge
+        The edge of XANES spectrum
+
+    """
+    XLABEL = 'Energy'
+    YLABEL = 'Intensity'
+
+    def __init__(self, x, y, structure, absorption_element, edge):
+        super(EXAFS, self).__init__(x, y, structure, absorption_element, edge)
+        self.structure = structure
+        self.absorption_element = absorption_element
+        self.edge = edge
+        self.e0 = self.x[np.argmax(np.gradient(self.y) / np.gradient(self.x))]
+        # Wavenumber, k is calculated based on equation
+        #             k^2=2*m/(hbar)^2*(E-E0)
+        self.k = [np.sqrt((i-self.e0)/3.854) if i > self.e0 else
+                  -np.sqrt((self.e0-i)/3.854) for i in self.x]
+
+    def __str__(self):
+        return "%s %s Edge for %s: %s" % (
+            self.absorption_element, self.edge,
+            self.structure.composition.reduced_formula,
+            super(EXAFS, self).__str__()
+        )
+
+
+def stitch_xanes_exafs(xanes, exafs, num_samples=500):
+    """
+    Stitch XANES and EXAFS object to get the full XAS spectrum.
+
+    The stitching will be based on wavenumber, k.
+        for k <= 3, XAS(k) = XAS[XANES(k)]
+        for 3 < k < max(xanes_k), will interpolate according to
+            XAS(k)=f(k)*mu[XANES(k)]+(1-f(k))*mu[EXAFS(k)]
+            where f(k)=cos^2((pi/2) (k-3)/(max(xanes_k)-3)
+        for k > max(xanes_k), XAS(k) = XAS[EXAFS(k)]
+
+    Args:
+        xanes(XANES): XANES object.
+        exafs(EXAFS): EXAFS object.
+        num_samples(int): Number of samples for interpolation.
+
+    Returns:
+        tuple: a plottable (x, y) pair for the full XAS spectrum
+    """
+    m = StructureMatcher()
+    if not m.fit(xanes.structure, exafs.structure):
+        raise ValueError("The input structures from XANES and EXAFS mismatch")
+    if max(xanes.x) < min(exafs.x):
+        raise ValueError(
+            "Energy overlap between XANES and EXAFS is needed for stitching")
+
+    # for k <= 3
+    wavenumber, mu = [], []
+    idx = xanes.k.index(min(xanes.k, key=lambda x: (abs(x - 3), x)))
+    mu.extend(xanes.y[:idx])
+    wavenumber.extend(xanes.k[:idx])
+
+    # for 3 < k < max(xanes_k)
+    ks, fs = [], []
+    for k in np.linspace(3, max(xanes.k), 50):
+        f = np.cos((math.pi / 2) * (k - 3)) ** 2
+        fs.append(f)
+        ks.append(k)
+    f_xanes = interp1d(
+        np.asarray(xanes.k), np.asarray(xanes.y),
+        bounds_error=False, fill_value=0)
+    f_exafs = interp1d(
+        np.asarray(exafs.k), np.asarray(exafs.y),
+        bounds_error=False, fill_value=0)
+    mu_xanes = f_xanes(ks)
+    mu_exafs = f_exafs(ks)
+    mus = [fs[i] * mu_xanes[i] + (1 - fs[i]) * mu_exafs[i] for i in
+           np.arange(len(ks))]
+    mu.extend(mus)
+    wavenumber.extend(ks)
+
+    # for k > max(xanes_k)
+    idx = exafs.k.index(min(exafs.k, key=lambda x: (abs(x - max(xanes.k)))))
+    mu.extend(exafs.y[idx:])
+    wavenumber.extend(exafs.k[idx:])
+
+    # interpolation
+    f_final = interp1d(
+        np.asarray(wavenumber), np.asarray(mu), bounds_error=False,
+        fill_value=0)
+    wavenumber = np.linspace(min(wavenumber), max(wavenumber),
+                             num=num_samples)
+    mu = f_final(wavenumber)
+    energy = [3.854 * i ** 2 + xanes.e0 if i > 0
+              else -3.854 * i ** 2 + xanes.e0 for i in wavenumber]
+    return (energy, mu)
+
+
+def stitch_l23(l2_xanes, l3_xanes, num_samples=200):
+    """
+    Stitch separate L2 and L3 XANES object to get the L2,3 XANES for elements with
+        atomic number <=30 so that the edge energies for L2 and L3 are close enough.
+
+
+    Args:
+        l2_xanes(XANES): XANES object for L2 edge.
+        l3_xanes(EXAFS): EXAFS object for L3 edge.
+        num_samples(int): Number of samples for interpolation.
+
+    Returns:
+        tuple: a plottable (x, y) pair for L2,3-edge spectrum
+    """
+    m = StructureMatcher()
+    if not m.fit(l2_xanes.structure, l3_xanes.structure):
+        raise ValueError(
+            "The structures for L2-edge XANES and L3-edge XANES mismatch")
+    if l2_xanes.absorption_element.number > 30:
+        raise ValueError("Does not support L2,3-edge XANES for {} element"
+                         .format(l2_xanes.absorption_element))
+
+    l2_f = interp1d(
+        l2_xanes.x, l2_xanes.y, bounds_error=False, fill_value=0)
+    # will add value of l3.y[-1] to avoid sudden change in absorption coeff.
+    l3_f = interp1d(
+        l2_xanes.x, l2_xanes.y, bounds_error=False, fill_value=l3_xanes.y[-1])
+    energy = np.linspace(min(l3_xanes.x), max(l2_xanes.x), num=num_samples)
+    mu = [i + j for i, j in zip(l2_f(energy), l3_f(energy))]
+    return (energy, mu)
