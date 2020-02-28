1a3fbf3ff5f214c219fc7ac13e9f27606794601f
Handong Ling <handongling@berkeley.edu>
2020-02-27 16:35:37 -0800

    mcsqs_caller scratch dir
    run mcsqs in scratch dir to deal with multiple files better

--- pymatgen/command_line/tests/test_mcsqs_caller.py
+++ pymatgen/command_line/tests/test_mcsqs_caller.py
@@ -2,7 +2,7 @@ import unittest
 
 from pymatgen.core.structure import Structure
 import subprocess
-import os 
+import os
 import numpy as np
 from pymatgen.io import atat
 from pymatgen.util.testing import PymatgenTest
@@ -30,28 +30,12 @@ class Mcsqs_CallerTest(PymatgenTest):
 		struc.replace_species({'Ti': {'Ti':0.5, 'Zr':0.5 }, 'Zr': {'Ti':0.5, 'Zr':0.5 }})
 		sqs = run_mcsqs(struc, {2:6, 3:4}, supercell = [2,1,1], total_atoms = None, search_time = 0.01)
 		self.assertEqual(atat.Mcsqs(sqs).to_string() in self.pztstrings, True)
-		os.remove('sqscell.out')
-		os.remove('rndstrgrp.out')
-		os.remove('bestcorr.out')
-		os.remove('rndstr.in')
-		os.remove('sym.out')
-		os.remove('mcsqs.log')
-		os.remove('bestsqs.out')
-		os.remove('clusters.out')
 
 	def test_Mcsqs_Caller_total_atoms(self):
 		struc = self.struc.copy()
 		struc.replace_species({'Ti': {'Ti':0.5, 'Zr':0.5 }, 'Zr': {'Ti':0.5, 'Zr':0.5 }})
 		sqs = run_mcsqs(struc, {2:6, 3:4}, total_atoms = 20, search_time = 0.01)
 		self.assertEqual(atat.Mcsqs(sqs).to_string() in self.pztstrings2, True)
-		os.remove('sqscell.out')
-		os.remove('rndstrgrp.out')
-		os.remove('bestcorr.out')
-		os.remove('rndstr.in')
-		os.remove('sym.out')
-		os.remove('mcsqs.log')
-		os.remove('bestsqs.out')
-		os.remove('clusters.out')
 
 	def test_Mcsqs_Caller_timeout_error(self):
 		struc = self.struc.copy()
@@ -59,7 +43,3 @@ class Mcsqs_CallerTest(PymatgenTest):
 		struc.replace_species({'Pb': {'Ti':0.2, 'Pb':0.8 }})
 		struc.replace_species({'O': {'F':0.8, 'O':0.2 }})
 		self.assertRaises(TimeoutError, run_mcsqs, struc, {2:6, 3:4},  None, 100, 0.000001)
-		os.remove('sqscell.out')
-		os.remove('rndstr.in')
-		os.remove('sym.out')
-		os.remove('clusters.out')
