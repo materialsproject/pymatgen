769926c0abb55b9c7583ebba8c97f14431bae1ee
zyrd <shyuep@gmail.com>
2020-03-07 08:07:45 -0800

    Changed enumlib link. Fixes #1788.

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -3,6 +3,7 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '2020.3.2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
