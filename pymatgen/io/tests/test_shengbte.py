d16b4280eb883334a60737ce7329befc08f3987d
zyrd <shyuep@gmail.com>
2020-03-11 09:00:03 -0700

    Add missing message.

--- pymatgen/io/tests/test_shengbte.py
+++ pymatgen/io/tests/test_shengbte.py
@@ -51,7 +51,7 @@ class TestShengBTE(PymatgenTest):
             "nanowires": False,
         }
 
-    @unittest.skipIf(f90nml is None)
+    @unittest.skipIf(f90nml is None, "No f90nml")
     def test_from_file(self):
         io = Control.from_file(self.filename)
         self.assertEqual(io["nelements"], 1)
@@ -94,7 +94,7 @@ class TestShengBTE(PymatgenTest):
         self.assertMultiLineEqual(test_string, reference_string)
         os.remove(os.path.join(test_dir, "test_control"))
 
-    @unittest.skipIf(f90nml is None)
+    @unittest.skipIf(f90nml is None, "No f90nml")
     def test_from_dict(self):
         io = Control.from_dict(self.test_dict)
         if os.path.exists(os.path.join(test_dir, "test_control")):
@@ -109,7 +109,7 @@ class TestShengBTE(PymatgenTest):
         self.assertMultiLineEqual(test_string, reference_string)
         os.remove(os.path.join(test_dir, "test_control"))
 
-    @unittest.skipIf(f90nml is None)
+    @unittest.skipIf(f90nml is None, "No f90nml")
     def test_MSONable_implementation(self):
         # tests as dict and from dict methods
         Controlinfromfile = Control.from_file(self.filename)
