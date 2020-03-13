3a3f21c19b8efc9a8a76eeea16cb5866fd267d44
nwinner <nwinner@berkeley.edu>
2020-03-13 16:12:28 -0700

    This shouldn't be here anymore. Moved to MPMorph.

--- pymatgen/analysis/dynamics/__init__.py
+++
@@ -1,5 +0,0 @@
-__author__ = "Nicholas Winner"
-__email__ = "nwinner@berkeley.edu"
-__maintainer_email__ = "nwinner@berkeley.edu"
-__version__ = "0.1"
-
--- pymatgen/analysis/dynamics/constants.py
+++
@@ -1,9 +0,0 @@
-"""
-Common physical constants provided in the units common to atomistic calculations (Å, ps, eV)
-
-"""
-
-kB   = 8.6173303e-5 # eV / K
-c    = 2997924.58   # Å / ps
-h    = 4.135667e-3  # eV ps
-hbar = 6.582120e-4  # eV ps
