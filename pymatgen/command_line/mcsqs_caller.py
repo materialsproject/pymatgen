65357f091dc04f23b60ddefc8b6f30697340097b
rwoodsrobinson <rwoodsrobinson@lbl.gov>
2020-03-31 17:06:23 -0700

    Added kwargs
    Added T, wr, wn, wd, and tol as arguments to mcsqs

--- pymatgen/command_line/mcsqs_caller.py
+++ pymatgen/command_line/mcsqs_caller.py
@@ -4,13 +4,14 @@ https://www.brown.edu/Departments/Engineering/Labs/avdw/atat/
 """
 
 import os
-import subprocess
+from subprocess import Popen, PIPE, TimeoutExpired
 from typing import Dict, Union, List, NamedTuple
 
 import numpy as np
 from monty.tempfile import ScratchDir
 from monty.dev import requires
 from monty.os.path import which
+import tempfile
 
 from pymatgen import Structure
 
@@ -28,19 +29,30 @@ def run_mcsqs(
     clusters: Dict[int, float],
     scaling: Union[int, List[int]],
     search_time: float = 0.01,
+    temperature: Union[int, float] = 1,
+    wr: float = 1,
+    wn: float = 1,
+    wd: float = 0,
+    tol: float = 1e-3
 ) -> Sqs:
     """
     Helper function for calling mcsqs with different arguments
     Args:
-        structure (Structure): disordered pymatgen Structure object
-        clusters (dict): dictionary of cluster interactions with entries in the form
+        structure (Structure): Disordered pymatgen Structure object
+        clusters (dict): Dictionary of cluster interactions with entries in the form
             number of atoms: cutoff in angstroms
-        scaling (int or list): scaling factor to determine supercell. Two options are possible:
+        scaling (int or list): Scaling factor to determine supercell. Two options are possible:
                 a. (preferred) Scales number of atoms, e.g., for a structure with 8 atoms,
                    scaling=4 would lead to a 32 atom supercell
-                b. An sequence of three scaling factors, e.g., [2, 1, 1], which
+                b. A sequence of three scaling factors, e.g., [2, 1, 1], which
                    specifies that the supercell should have dimensions 2a x b x c
-        search_time (int): The time spent looking for the ideal SQS in minutes
+        search_time (int): Time spent looking for the ideal SQS in minutes
+        temperature (int or float): Monte Carlo temperature (default: 1), "T" in atat code
+        wr (int or float): Weight assigned to range of perfect correlation match in objective
+            function (default = 1)
+        wn (int or float): Multiplicative decrease in weight per additional point in cluster (default: 1)
+        wd (int or float): Exponent of decay in weight as function of cluster diameter (default: 0)
+        tolerance (int or float): Tolerance for matching correlations (default: 1e-3)
 
     Returns:
         Tuple of Pymatgen structure, which is an SQS of the input structure, and the
@@ -52,69 +64,85 @@ def run_mcsqs(
     if structure.is_ordered:
         raise ValueError("Pick a disordered structure")
 
-    with ScratchDir("."):
+    tempdir = tempfile.mkdtemp()
+    os.chdir(tempdir)
 
-        if isinstance(scaling, (int, float)):
+    if isinstance(scaling, (int, float)):
 
-            if scaling % 1:
-                raise ValueError("Scaling should be an integer, not {}".format(scaling))
+        if scaling % 1:
+            raise ValueError("Scaling should be an integer, not {}".format(scaling))
 
-            mcsqs_find_sqs_cmd = ["mcsqs", "-n {}".format(scaling*num_atoms)]
+        mcsqs_find_sqs_cmd = ["mcsqs", "-n {}".format(scaling*num_atoms),
+                              "-T {}".format(temperature),
+                              "-wr {}".format(wr),
+                              "-wn {}".format(wn),
+                              "-wd {}".format(wd),
+                              "-tol {}".format(tol)]
 
-        else:
+    else:
 
-            # Set supercell to identity (will make supercell with pymatgen)
-            with open("sqscell.out", "w") as f:
-                f.write("1\n"
-                        "1 0 0\n"
-                        "0 1 0\n"
-                        "0 0 1\n")
+        # Set supercell to identity (will make supercell with pymatgen)
+        with open("sqscell.out", "w") as f:
+            f.write("1\n"
+                    "1 0 0\n"
+                    "0 1 0\n"
+                    "0 0 1\n")
 
-            structure = structure*scaling
-            mcsqs_find_sqs_cmd = ["mcsqs", "-rc", "-n {}".format(num_atoms)]
+        structure = structure*scaling
+        mcsqs_find_sqs_cmd = ["mcsqs", "-rc", "-n {}".format(num_atoms),
+                              "-T {}".format(temperature),
+                              "-wr {}".format(wr),
+                              "-wn {}".format(wn),
+                              "-wd {}".format(wd),
+                              "-tol {}".format(tol)]
 
-        structure.to(filename="rndstr.in")
+    structure.to(filename="rndstr.in")
 
-        # Generate clusters
-        mcsqs_generate_clusters_cmd = ["mcsqs"]
-        for num in clusters:
-            mcsqs_generate_clusters_cmd.append("-" + str(num) + "=" + str(clusters[num]))
+    # Generate clusters
+    mcsqs_generate_clusters_cmd = ["mcsqs"]
+    for num in clusters:
+        mcsqs_generate_clusters_cmd.append("-" + str(num) + "=" + str(clusters[num]))
 
-        # Run mcsqs to find clusters
-        p = subprocess.Popen(
-            mcsqs_generate_clusters_cmd
-        )
+    # Run mcsqs to find clusters
+    p = Popen(
+        mcsqs_generate_clusters_cmd,
+        # stdout=PIPE, stderr=PIPE,
+        # shell=True
+    )
+    p.communicate()
+
+    # Run mcsqs to find sqs structure
+    p = Popen(
+        mcsqs_find_sqs_cmd,
+        # stdout=PIPE, stderr=PIPE,
+        # shell=True,
+    )
+
+    try:
+        p.communicate(timeout=search_time * 60)
+        raise Exception("mcsqs exited before timeout reached")
+    except TimeoutExpired:
+        p.kill()
         p.communicate()
 
-        # Run mcsqs to find sqs structure
-        p = subprocess.Popen(
-            mcsqs_find_sqs_cmd
-        )
+        if os.path.exists("bestsqs.out") and os.path.exists("bestcorr.out"):
 
-        try:
-            p.communicate(timeout=search_time * 60)
-        except subprocess.TimeoutExpired:
-            p.kill()
+            # Convert output sqs structure to cif file
+            p = Popen(
+                "str2cif < bestsqs.out > bestsqs.cif",
+                shell=True
+            )
             p.communicate()
-            if os.path.exists("bestsqs.out") and os.path.exists("bestcorr.out"):
 
-                # Convert output sqs structure to cif file
-                p = subprocess.Popen(
-                    "str2cif < bestsqs.out > bestsqs.cif",
-                    shell=True
-                )
-                p.communicate()
+            # Get objective function
+            with open('bestcorr.out', 'r') as f:
+                lines = f.readlines()
+            objective_function = float(lines[-1].split('=')[-1].strip())
 
-                # Get objective function
-                with open('bestcorr.out', 'r') as f:
-                    lines = f.readlines()
-                objective_function = float(lines[-1].split('=')[-1].strip())
+            return Sqs(
+                bestsqs=Structure.from_file("bestsqs.cif"),
+                objective_function=objective_function
+            )
 
-                return Sqs(
-                    bestsqs=Structure.from_file("bestsqs.cif"),
-                    objective_function=objective_function
-                )
-
-            else:
-                raise TimeoutError("Cluster expansion took too long.")
-
+        else:
+            raise TimeoutError("Cluster expansion took too long.")
