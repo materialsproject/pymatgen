8215c0036c19d42bf59846f2a63d49960823710e
Rhys Goodall <reag2@cam.ac.uk>
2020-04-15 16:22:33 +0100

    try increasing cutoff

--- pymatgen/analysis/local_env.py
+++ pymatgen/analysis/local_env.py
@@ -767,7 +767,7 @@ class VoronoiNN(NearNeighbors):
             targets = self.targets
 
         # Initialize the list of sites with the atoms in the origin unit cell
-        #  The `get_all_neighbors` function returns neighbors for each site's image in the
+        #   The `get_all_neighbors` function returns neighbors for each site's image in the
         #   original unit cell. We start off with these central atoms to ensure they are
         #   included in the tessellation
 
@@ -789,8 +789,8 @@ class VoronoiNN(NearNeighbors):
         sites = np.array(sites)[uniq_inds]
 
         # Sort array such that atoms in the root image are first
-        #   Exploit the fact that the array is sorted by the unique operation such that
-        #   the images associated with atom 0 are first, followed by atom 1, etc.
+        # Exploit the fact that the array is sorted by the unique operation such that
+        # the images associated with atom 0 are first, followed by atom 1, etc.
         root_images, = np.nonzero(np.abs(indices[:, 1:]).max(axis=1) == 0)
 
         del indices  # Save memory (tessellations can be costly)
@@ -925,9 +925,8 @@ class VoronoiNN(NearNeighbors):
                     results[other_site]['verts'] = vind
 
         # all sites should have atleast two connected ridges in periodic system
-        if not bool(results): 
-            raise ValueError("results is empty dictionary - unphysical result,"
-                                "all sites should have connected ridges.") 
+        if not bool(results):
+            raise ValueError("No Voronoi neighbours found for site - try increasing cutoff")
 
         # Get only target elements
         resultweighted = {}
