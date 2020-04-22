f49c324c2f5a2095489c41b2484a7909ae0f3963
Ryan Kingsbury <RKingsbury@lbl.gov>
2020-04-22 14:42:11 -0700

    tests for energy_adjustment functionality

--- pymatgen/entries/tests/test_computed_entries.py
+++ pymatgen/entries/tests/test_computed_entries.py
@@ -4,11 +4,13 @@
 
 
 import unittest
+import pytest
 import os
 
+from collections import defaultdict
 from pymatgen.io.vasp.outputs import Vasprun
 from pymatgen.entries.computed_entries import ComputedEntry, \
-    ComputedStructureEntry, EnergyAdjustment
+    ComputedStructureEntry, EnergyAdjustment, ConstantEnergyAdjustment
 
 test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..",
                         'test_files')
@@ -38,6 +40,9 @@ class ComputedEntryTest(unittest.TestCase):
         self.entry3 = ComputedEntry("Fe2O3", 2.3)
         self.entry4 = ComputedEntry("Fe2O3", 2.3, entry_id=1)
         self.entry5 = ComputedEntry("Fe6O9", 6.9)
+        ea = ConstantEnergyAdjustment(-5, name="Dummy adjustment")
+        self.entry6 = ComputedEntry("Fe6O9", 6.9, correction=-10)
+        self.entry7 = ComputedEntry("Fe6O9", 6.9, energy_adjustments=[ea])
 
     def test_energy(self):
         self.assertAlmostEqual(self.entry.energy, -269.38319884)
@@ -58,16 +63,64 @@ class ComputedEntryTest(unittest.TestCase):
         self.assertAlmostEqual(entry.uncorrected_energy, 6.9 / 3)
         self.assertAlmostEqual(entry.correction, 1 / 3)
         self.assertAlmostEqual(entry.energy * 3, 6.9 + 1)
+        self.assertAlmostEqual(entry.energy_adjustments[0].value, 1/3)
         entry.normalize("atom")
         self.assertEqual(entry.composition.formula, "Fe0.4 O0.6")
         self.assertAlmostEqual(entry.uncorrected_energy, 6.9 / 15)
         self.assertAlmostEqual(entry.correction, 1 / 15)
         self.assertAlmostEqual(entry.energy * 15, 6.9 + 1)
+        self.assertAlmostEqual(entry.energy_adjustments[0].value, 1/15)
 
     def test_to_from_dict(self):
         d = self.entry.as_dict()
         e = ComputedEntry.from_dict(d)
         self.assertAlmostEqual(e.energy, -269.38319884)
+    
+    def test_to_from_dict_with_adjustment(self):
+        """
+        Legacy case where adjustment was provided manually
+        """
+        d = self.entry6.as_dict()
+        e = ComputedEntry.from_dict(d)
+        self.assertAlmostEqual(e.uncorrected_energy, 6.9)
+        self.assertEqual(e.energy_adjustments[0].value, self.entry6.energy_adjustments[0].value)
+    
+    def test_to_from_dict_with_adjustment_2(self):
+        """
+        Modern case where correction was provided manually
+        """
+        d = self.entry7.as_dict()
+        e = ComputedEntry.from_dict(d)
+        self.assertAlmostEqual(e.uncorrected_energy, 6.9)
+        self.assertEqual(e.energy_adjustments[0].value, self.entry7.energy_adjustments[0].value)
+    
+    def test_to_from_dict_with_adjustment_3(self):
+        """
+        Legacy case where the entry was serialized before the energy_adjustment
+        attribute was part of ComputedEntry
+        """
+        # same as entry6
+        d = {'@module': 'pymatgen.entries.computed_entries',
+             '@class': 'ComputedEntry',
+             'energy': 6.9,
+             'composition': defaultdict(float, {'Fe': 6.0, 'O': 9.0}),
+             'parameters': {},
+             'data': {},
+             'entry_id': None,
+             'correction': -10}
+        e = ComputedEntry.from_dict(d)
+        self.assertAlmostEqual(e.uncorrected_energy, 6.9)
+        self.assertAlmostEqual(e.correction, -10)
+        assert len(e.energy_adjustments) == 1
+    
+    def test_conflicting_correction_adjustment(self):
+        """
+        Should raise a ValueError if a user tries to manually set both the correction
+        and energy_adjustment, even if the values match.
+        """
+        ea = ConstantEnergyAdjustment(-10, name="Dummy adjustment")
+        with pytest.raises(ValueError, match="Argument conflict!"):
+            ComputedEntry("Fe6O9", 6.9, correction=-10, energy_adjustments=[ea])
 
     def test_entry_id(self):
         self.assertEqual(self.entry4.entry_id, 1)
