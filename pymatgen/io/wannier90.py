97690a2a954017ab6036eb6740c221c9247de851
Mark Turiansky <mark.turiansky@gmail.com>
2020-06-04 16:52:05 -0700

    type annotations and documentation for wannier90 io

--- pymatgen/io/wannier90.py
+++ pymatgen/io/wannier90.py
@@ -8,6 +8,7 @@ Modules for working with wannier90 input and output.
 
 import numpy as np
 from scipy.io import FortranFile, FortranEOFError
+from typing import Sequence
 
 __author__ = "Mark Turiansky"
 __copyright__ = "Copyright 2011, The Materials Project"
@@ -22,25 +23,65 @@ class Unk:
     """
     Object representing the data in a UNK file.
 
-    .. attribute:: 
+    .. attribute:: ik
 
-        blah
+        int index of kpoint for this file
+
+    .. attribute:: data
+
+        numpy.ndarray that contains the wavefunction data for in the UNK file.
+        The shape should be (nbnd, ngx, ngy, ngz) for regular calculations and
+        (nbnd, 2, ngx, ngy, ngz) for noncollinear calculations.
+
+    .. attribute:: is_noncollinear
+
+        bool that specifies if data is from a noncollinear calculation
+
+    .. attribute:: nbnd
+
+        int number of bands in data
+
+    .. attribute:: ng
+
+        sequence of three integers that correspond to the grid size of the
+        given data. The definition is ng = (ngx, ngy, ngz).
 
     """
+    ik: int
+    is_noncollinear: bool
+    nbnd: int
+    ng: Sequence[int]
 
-    def __init__(self, ik, data):
+    def __init__(self, ik: int, data: np.ndarray) -> None:
         """
+        Initialize Unk class.
+
+        Args:
+            ik (int): index of the kpoint UNK file is for
+            data (np.ndarray): data from the UNK file that has shape (nbnd,
+                ngx, ngy, ngz) or (nbnd, 2, ngx, ngy, ngz) if noncollinear
         """
         self.ik = ik
         self.data = data
 
     @property
-    def data(self):
+    def data(self) -> np.ndarray:
+        """
+        np.ndarray: contains the wavefunction data for in the UNK file.
+        The shape should be (nbnd, ngx, ngy, ngz) for regular calculations and
+        (nbnd, 2, ngx, ngy, ngz) for noncollinear calculations.
+        """
         return self._data
 
     @data.setter
-    def data(self, value):
+    def data(self, value: np.ndarray) -> None:
         """
+        Sets the value of data.
+
+        Args:
+            value (np.ndarray): data to replace stored data, must haveshape
+                (nbnd, ngx, ngy, ngz) or (nbnd, 2, ngx, ngy, ngz) if
+                noncollinear calculation
         """
         temp_val = np.array(value, dtype=np.complex128)
         if len(temp_val.shape) not in [4, 5]:
@@ -58,38 +99,52 @@ class Unk:
         self.ng = self.data.shape[-3:]
 
     @staticmethod
-    def from_file(filename):
+    def from_file(filename: str) -> object:
         """
+        Reads the UNK data from file.
+
+        Args:
+            filename (str): path to UNK file to read
+
+        Returns:
+            Unk object
         """
-        data = []
+        input_data = []
         with FortranFile(filename, 'r') as f:
             *ng, ik, nbnd = f.read_ints()
             for _ in range(nbnd):
-                data.append(
+                input_data.append(
                     # when reshaping need to specify ordering as fortran
                     f.read_record(np.complex128).reshape(ng, order='F')
                 )
             try:
                 for _ in range(nbnd):
-                    data.append(
+                    input_data.append(
                         f.read_record(np.complex128).reshape(ng, order='F')
                     )
                 is_noncollinear = True
             except FortranEOFError:
                 is_noncollinear = False
 
-        data = np.array(data, dtype=np.complex128)
+        # mypy made me create an extra variable here >:(
+        data = np.array(input_data, dtype=np.complex128)
 
         # spinors are interwoven, need to separate them
         if is_noncollinear:
-            tdata = np.empty((nbnd, 2, *ng))
-            tdata[:, 0, :, :, :] = data[::2, :, :, :]
-            tdata[:, 1, :, :, :] = data[1::2, :, :, :]
-            return Unk(ik, tdata)
+            temp_data = np.empty((nbnd, 2, *ng))
+            temp_data[:, 0, :, :, :] = data[::2, :, :, :]
+            temp_data[:, 1, :, :, :] = data[1::2, :, :, :]
+            return Unk(ik, temp_data)
         return Unk(ik, data)
 
-    def write_file(self, filename):
+    def write_file(self, filename: str) -> None:
         """
+        Write the UNK file.
+
+        Args:
+            filename (str): path to UNK file to write, the name should have the
+                form 'UNKXXXXX.YY' where XXXXX is the kpoint index (Unk.ik) and
+                YY is 1 or 2 for the spin index or NC if noncollinear
         """
         with FortranFile(filename, 'w') as f:
             f.write_record(
@@ -102,11 +157,14 @@ class Unk:
                 else:
                     f.write_record(self.data[ib].flatten('F'))
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'<UNK ik={self.ik} nbnd={self.nbnd} ncl={self.is_noncollinear}' \
             + f' ngx={self.ng[0]} ngy={self.ng[1]} ngz={self.ng[2]}>'
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Unk):
+            return NotImplemented
+
         if not np.allclose(self.ng, other.ng):
             return False
 
