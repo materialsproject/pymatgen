d4c62799bb7a31c9d6eab9100fb1a339691941e2
zyrd <shyuep@gmail.com>
2020-07-16 15:01:21 -0700

    Cleanup release task.

--- tasks.py
+++ tasks.py
@@ -178,23 +178,19 @@ def publish(ctx):
 
 @task
 def set_ver(ctx):
-    lines = []
     with open("pymatgen/__init__.py", "rt") as f:
-        for l in f:
-            if "__version__" in l:
-                lines.append('__version__ = "%s"' % NEW_VER)
-            else:
-                lines.append(l.rstrip())
-    with open("pymatgen/__init__.py", "wt") as f:
-        f.write("\n".join(lines))
+        contents = f.read()
+        contents = re.sub(r"__version__ = .*\n", '__version__ = "%s"\n' % NEW_VER, contents)
 
-    lines = []
+    with open("pymatgen/__init__.py", "wt") as f:
+        f.write(contents)
+
     with open("setup.py", "rt") as f:
-        for l in f:
-            lines.append(re.sub(r'version=([^,]+),', 'version="%s",' % NEW_VER,
-                                l.rstrip()))
+        contents = f.read()
+        contents = re.sub(r'version=([^,]+),', 'version="%s",' % NEW_VER, contents)
+
     with open("setup.py", "wt") as f:
-        f.write("\n".join(lines))
+        f.write(contents)
 
 
 @task
