30e247a98ea9efbd936901e8fcf6ba987060508e
zyrd <shyuep@gmail.com>
2020-07-15 18:49:38 -0700

    Cleanup ext.

--- pymatgen/ext/matproj.py
+++ pymatgen/ext/matproj.py
@@ -19,11 +19,11 @@ import platform
 import re
 import warnings
 from time import sleep
-import requests
-from monty.json import MontyDecoder, MontyEncoder
-
 from enum import Enum, unique
 from collections import defaultdict
+
+import requests
+from monty.json import MontyDecoder, MontyEncoder
 
 from pymatgen import SETTINGS, __version__ as pmg_version
 
@@ -31,23 +31,11 @@ from pymatgen.core.composition import Composition
 from pymatgen.core.periodic_table import Element
 from pymatgen.core.structure import Structure
 from pymatgen.core.surface import get_symmetrically_equivalent_miller_indices
-
-from pymatgen.entries.computed_entries import ComputedEntry, \
-    ComputedStructureEntry
+from pymatgen.entries.computed_entries import ComputedEntry, ComputedStructureEntry
 from pymatgen.entries.compatibility import MaterialsProjectCompatibility, MaterialsProjectAqueousCompatibility
 from pymatgen.entries.exp_entries import ExpEntry
-
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
-
 from pymatgen.util.sequence import get_chunks, PBar
-
-__author__ = "Shyue Ping Ong, Shreyas Cholia"
-__credits__ = "Anubhav Jain"
-__copyright__ = "Copyright 2012, The Materials Project"
-__version__ = "1.0"
-__maintainer__ = "Shyue Ping Ong"
-__email__ = "shyuep@gmail.com"
-__date__ = "Feb 22, 2013"
 
 
 @unique
@@ -176,8 +164,7 @@ class MPRester:
                     if data.get("warning"):
                         warnings.warn(data["warning"])
                     return data["response"]
-                else:
-                    raise MPRestError(data["error"])
+                raise MPRestError(data["error"])
 
             raise MPRestError("REST query returned with error status code {}"
                               .format(response.status_code))
@@ -365,8 +352,7 @@ class MPRester:
                 resp = json.loads(response.text, cls=MontyDecoder)
                 if resp['valid_response']:
                     return resp['response']
-                else:
-                    raise MPRestError(resp["error"])
+                raise MPRestError(resp["error"])
             raise MPRestError("REST error with status code {} and error {}"
                               .format(response.status_code, response.text))
         except Exception as ex:
@@ -840,17 +826,17 @@ class MPRester:
                                            chunk_size=0, mp_decode=mp_decode))
                     break
                 except MPRestError as e:
+                    # pylint: disable=E1101
                     match = re.search(r"error status code (\d+)", e.message)
                     if match:
                         if not match.group(1).startswith("5"):
                             raise e
-                        else:  # 5xx error. Try again
-                            num_tries += 1
-                            print(
-                                "Unknown server error. Trying again in five "
-                                "seconds (will try at most {} times)...".format(
-                                    max_tries_per_chunk))
-                            sleep(5)
+                        num_tries += 1
+                        print(
+                            "Unknown server error. Trying again in five "
+                            "seconds (will try at most {} times)...".format(
+                                max_tries_per_chunk))
+                        sleep(5)
             progress_bar.update(len(chunk))
         return data
 
@@ -929,8 +915,7 @@ class MPRester:
                     if resp.get("warning"):
                         warnings.warn(resp["warning"])
                     return resp['inserted_ids']
-                else:
-                    raise MPRestError(resp["error"])
+                raise MPRestError(resp["error"])
 
             raise MPRestError("REST error with status code {} and error {}"
                               .format(response.status_code, response.text))
@@ -965,8 +950,7 @@ class MPRester:
                     if resp.get("warning"):
                         warnings.warn(resp["warning"])
                     return resp
-                else:
-                    raise MPRestError(resp["error"])
+                raise MPRestError(resp["error"])
 
             raise MPRestError("REST error with status code {} and error {}"
                               .format(response.status_code, response.text))
@@ -1003,8 +987,7 @@ class MPRester:
                     if resp.get("warning"):
                         warnings.warn(resp["warning"])
                     return resp["response"]
-                else:
-                    raise MPRestError(resp["error"])
+                raise MPRestError(resp["error"])
 
             raise MPRestError("REST error with status code {} and error {}"
                               .format(response.status_code, response.text))
@@ -1097,8 +1080,7 @@ class MPRester:
                     if resp.get("warning"):
                         warnings.warn(resp["warning"])
                     return resp["response"]
-                else:
-                    raise MPRestError(resp["error"])
+                raise MPRestError(resp["error"])
             raise MPRestError("REST error with status code {} and error {}"
                               .format(response.status_code, response.text))
         except Exception as ex:
@@ -1208,8 +1190,8 @@ class MPRester:
             for one_surf in surf_list:
                 if tuple(one_surf['miller_index']) in eq_indices:
                     return one_surf
-        else:
-            return self._make_request(req)
+            raise ValueError("Bad miller index.")
+        return self._make_request(req)
 
     def get_wulff_shape(self, material_id):
         """
@@ -1287,9 +1269,7 @@ class MPRester:
                 gb_dict['work_of_separation'] = wsep
             return list_of_gbs
 
-        else:
-            return self._make_request("/grain_boundaries",
-                                      payload=payload)
+        return self._make_request("/grain_boundaries", payload=payload)
 
     def get_interface_reactions(self, reactant1, reactant2,
                                 open_el=None, relative_mu=None,
@@ -1403,17 +1383,15 @@ class MPRester:
         def parse_sym(sym):
             if sym == "*":
                 return [el.symbol for el in Element]
-            else:
-                m = re.match(r"\{(.*)\}", sym)
-                if m:
-                    return [s.strip() for s in m.group(1).split(",")]
-                else:
-                    return [sym]
+            m = re.match(r"\{(.*)\}", sym)
+            if m:
+                return [s.strip() for s in m.group(1).split(",")]
+            return [sym]
 
         def parse_tok(t):
             if re.match(r"\w+-\d+", t):
                 return {"task_id": t}
-            elif "-" in t:
+            if "-" in t:
                 elements = [parse_sym(sym) for sym in t.split("-")]
                 chemsyss = []
                 for cs in itertools.product(*elements):
@@ -1422,33 +1400,31 @@ class MPRester:
                         cs = [Element(s).symbol for s in cs]
                         chemsyss.append("-".join(sorted(cs)))
                 return {"chemsys": {"$in": chemsyss}}
-            else:
-                all_formulas = set()
-                explicit_els = []
-                wild_card_els = []
-                for sym in re.findall(
-                        r"(\*[\.\d]*|\{.*\}[\.\d]*|[A-Z][a-z]*)[\.\d]*", t):
-                    if ("*" in sym) or ("{" in sym):
-                        wild_card_els.append(sym)
-                    else:
-                        m = re.match(r"([A-Z][a-z]*)[\.\d]*", sym)
-                        explicit_els.append(m.group(1))
-                nelements = len(wild_card_els) + len(set(explicit_els))
-                parts = re.split(r"(\*|\{.*\})", t)
-                parts = [parse_sym(s) for s in parts if s != ""]
-                for f in itertools.product(*parts):
-                    c = Composition("".join(f))
-                    if len(c) == nelements:
-                        # Check for valid Elements in keys.
-                        for e in c.keys():
-                            Element(e.symbol)
-                        all_formulas.add(c.reduced_formula)
-                return {"pretty_formula": {"$in": list(all_formulas)}}
+            all_formulas = set()
+            explicit_els = []
+            wild_card_els = []
+            for sym in re.findall(
+                    r"(\*[\.\d]*|\{.*\}[\.\d]*|[A-Z][a-z]*)[\.\d]*", t):
+                if ("*" in sym) or ("{" in sym):
+                    wild_card_els.append(sym)
+                else:
+                    m = re.match(r"([A-Z][a-z]*)[\.\d]*", sym)
+                    explicit_els.append(m.group(1))
+            nelements = len(wild_card_els) + len(set(explicit_els))
+            parts = re.split(r"(\*|\{.*\})", t)
+            parts = [parse_sym(s) for s in parts if s != ""]
+            for f in itertools.product(*parts):
+                c = Composition("".join(f))
+                if len(c) == nelements:
+                    # Check for valid Elements in keys.
+                    for e in c.keys():
+                        Element(e.symbol)
+                    all_formulas.add(c.reduced_formula)
+            return {"pretty_formula": {"$in": list(all_formulas)}}
 
         if len(toks) == 1:
             return parse_tok(toks[0])
-        else:
-            return {"$or": list(map(parse_tok, toks))}
+        return {"$or": list(map(parse_tok, toks))}
 
 
 class MPRestError(Exception):
