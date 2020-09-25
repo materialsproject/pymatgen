93fe042024c7a826b3bbaf6186cd575080dfe604
Martin Siron <msiron@lbl.gov>
2020-09-25 13:11:58 -0700

    revert changes to vasp inputs

--- pymatgen/io/vasp/inputs.py
+++ pymatgen/io/vasp/inputs.py
@@ -186,11 +186,6 @@ class Poscar(MSONable):
         return [a[0] for a in itertools.groupby(syms)]
 
     @property
-    def sites(self):
-        sites_array = [site for site in self.structure]
-        return sites_array
-
-    @property
     def natoms(self):
         """
         Sequence of number of sites of each type associated with the Poscar.
