50f37ecdf8c1ee9ca10293a3181334baafd372f6
Matthew Horton <mkhorton@users.noreply.github.com>
2020-12-03 18:17:58 -0800

    Update version

--- pymatgen/__init__.py
+++ pymatgen/__init__.py
@@ -18,7 +18,7 @@ __author__ = "Pymatgen Development Team"
 __email__ = "pymatgen@googlegroups.com"
 __maintainer__ = "Shyue Ping Ong"
 __maintainer_email__ = "shyuep@gmail.com"
-__version__ = "2020.11.11"
+__version__ = "2020.12.3"
 
 SETTINGS_FILE = os.path.join(os.path.expanduser("~"), ".pmgrc.yaml")
 
