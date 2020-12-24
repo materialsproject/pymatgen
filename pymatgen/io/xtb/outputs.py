f33c115b8d1ad76faa0e61a3a2f39e7a6c727cbd
Alex Epstein <aepstein@lbl.gov>
2020-12-24 09:55:43 -0800

    Prep for PR
    Created test classes, deleted Evan's XTB contributions that aren't CREST

--- pymatgen/io/xtb/outputs.py
+++ pymatgen/io/xtb/outputs.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 # Copyright (c) Pymatgen Development Team.
 # Distributed under the terms of the MIT License.
-
+"""
+Parsers for XTB output files and directories
+"""
 import logging
 import argparse
 import os
@@ -15,170 +17,67 @@ from pymatgen.core import Molecule
 from pymatgen.io.xyz import XYZ
 from pymatgen.io.qchem.utils import (read_pattern)
 
-# Classes for reading/manipulating/writing QChem output files.
-
-__author__ = "Evan Spotte-Smith"
+__author__ = "Alex Epstein"
 __copyright__ = "Copyright 2020, The Materials Project"
 __version__ = "0.1"
-__maintainer__ = "Evan Spotte-Smith"
-__email__ = "espottesmith@gmail.com"
+__maintainer__ = "Alex Epstein"
+__email__ = "aepstein@lbl.gov"
+__credits__ = "Evan Spotte-Smith"
 
 logger = logging.getLogger(__name__)
 
 
-class XTBOutput(MSONable):
-    """
-    Class to parse XTB outputs
-    """
-
-    def __init__(self, path, output_filename, namespace=None):
-        """
-        Args:
-            path (str): Path to directory including output_filename and all
-                other xtb output files (xtbopt.log, vibspectrum, etc.)
-            output_filename (str): Filename to parse
-            namespace (str): If the namespace is not None (default), this will
-                be prepended to all filenames other than output_filename
-        """
-
-        self.path = path
-        self.filename = output_filename
-        self.namespace = namespace
-        self.data = dict()
-
-        self.data["errors"] = list()
-        self.data["warnings"] = dict()
-
-        self.text = ""
-        with zopen(output_filename, "rt") as f:
-            self.text = f.read()
-
-        self.data["input"] = dict()
-        self.data["output"] = dict()
-
-        self._read_setup()
-        # self._read_parameters()
-
-        # Need some condition to see if this is an opt job
-
-    def _read_setup(self):
-        """
-        Read calculation setup to extract, for instance, the command used
-        and the namespace
-
-        Note that this does not attempt to extract all information from the
-            setup.
-        """
-
-        parser = argparse.ArgumentParser()
-        parser.add_argument("geometry")
-        parser.add_argument("-c", "--chrg", type=int, nargs=1)
-        parser.add_argument("-u", "--uhf", type=int, nargs=1)
-        parser.add_argument("-a", "--acc", type=float, nargs=1)
-        parser.add_argument("--gfn", type=int, nargs=1)
-        parser.add_argument("-g", "--gbsa", nargs="+")
-        parser.add_argument("--scc", "--sp", action="store_true")
-        parser.add_argument("--grad", action="store_true")
-        parser.add_argument("-o", "--opt", nargs="?", default="normal")
-        parser.add_argument("--hess", action="store_true")
-        parser.add_argument("--pop", action="store_true")
-        parser.add_argument("--ohess", nargs="?", default="normal")
-        parser.add_argument("-I", "--input", nargs=1, type=str)
-        parser.add_argument("--namespace", nargs=1, type=str)
-        parser.add_argument("-P", "--parallel", nargs=1, type=int)
-
-        temp_dict = read_pattern(
-            self.text, {
-                "program_call": r"\s+program call\s+:\s+xtb (.+)\n",
-                "coordinate file": r"\s+coordinate file\s+:\s+([:\\\.\-/_0-9a-zA-Z]+)\n",
-                "charge": r"\s+charge\s+:\s+([\-0-9]+)\n",
-                "spin": r"\s+spin\s+:\s+([\.0-9]+)\n"
-            })
-
-        if temp_dict.get("program_call") is None:
-            self.data["setup"]["input"]["program_call"] = None
-        else:
-            namespace = parser.parse_args(temp_dict.get("program_call")[0][0].split())
-
-            self.data["input"]["accuracy"] = namespace.acc
-
-            if namespace.gbsa is None:
-                self.data["input"]["gbsa"] = None
-            else:
-                self.data["input"]["gbsa"] = dict()
-                if len(namespace.gbsa) == 1:
-                    self.data["input"]["gbsa"]["solvent"] = namespace.gbsa[0]
-                elif len(namespace.gbsa) == 2:
-                    self.data["input"]["gbsa"]["solvent"] = namespace.gbsa[0]
-                    self.data["input"]["gbsa"]["grid"] = namespace.gbsa[1]
-                else:
-                    self.data["input"]["gbsa"]["solvent"] = namespace.gbsa[0]
-                    self.data["input"]["gbsa"]["grid"] = namespace.gbsa[1]
-                    self.data["input"]["gbsa"]["other"] = namespace.gbsa[2:]
-
-
-class VibspectrumOutput(MSONable):
-    """
-    Class to parse vibspectrum files from xTB frequency output.
-    """
-    pass
-
-
-class OptLogOutput(MSONable):
-    """
-    Class to parse optimization log files from xTB optimization output.
-    """
-    pass
-
-
-class FakeGaussOutput(MSONable):
-    """
-    Class to parse "g98" files from xTB frequency output.
-    """
-    pass
-
-
 class CRESTOutput(MSONable):
+    """
+    Class to parse CREST output files
+    """
 
     def __init__(self, output_filename, path='.'):
         """
-        Currently assumes runtype is iMTD-GC [default]
+        Assumes runtype is iMTD-GC [default]
         Args:
-            path (str): Path to directory including output_filename and all
-                other xtb output files (xtbopt.log, vibspectrum, etc.)
             output_filename (str): Filename to parse
+            path (str): Path to directory including output_filename and all
+                other xtb output files (crest_best.xyz, etc.)
         """
 
         self.path = path
         self.filename = output_filename
+
         self.cmd_options = dict()
-        self.data = dict()
         self.sorted_structures_energies = []
         self.properly_terminated = False
         self._parse_crest_output()
 
     def _parse_crest_output(self):
         """
-        Parse output file to extract all command line inputs and output files
-        Needs functionality for incomplete jobs, error processing
+        Parse output file and directory to extract all command line inputs and output files.
+        Sets the attributes:
+            cmd_options: Dict of type {flag: value}
+            sorted_structrues_energies: n x m x 2 list, for n conformers, m rotamers per conformer, and tuple of
+                [Molecule, energy]
+            properly_terminated: True or False if run properly terminated
         """
         output_filepath = os.path.join(self.path, self.filename)
 
+        # Get CREST command
         crest_cmd = None
         with open(output_filepath, 'r') as xtbout_file:
-            # noinspection PyTypeChecker
             for line in xtbout_file:
                 if '> crest' in line:
                     crest_cmd = line.strip()[8:]
                     break
 
         split_cmd = crest_cmd.split(' ')
+
+        # Get input file if present
         try:
             self.coord_file = os.path.join(self.path, split_cmd[0])
             self.input_structure = Molecule.from_file(filename=self.coord_file)
         except FileNotFoundError:
             print("Input file {} not found".format(split_cmd[0]))
 
+        # Get CREST input flags
         for i, entry in enumerate(split_cmd):
             value = None
             if entry:
@@ -188,20 +87,22 @@ class CRESTOutput(MSONable):
                         if '-' not in split_cmd[i + 1]:
                             value = split_cmd[i + 1]
                     self.cmd_options[option] = value
+        # Get input charge for decorating parsed molecules
         chg = 0
-        if self.cmd_options["chrg"]:
+        if "chrg" in self.cmd_options.keys():
             str_chg = self.cmd_options["chrg"]
             if '-' in str_chg:
                 chg = int(str_chg)
             else:
                 chg = int(str_chg[-1])
-        elif self.cmd_options["c"]:
+        elif "chrg" in self.cmd_options.keys():
             str_chg = self.cmd_options["c"]
             if '-' in str_chg:
                 chg = int(str_chg)
             else:
                 chg = int(str_chg[-1])
 
+        # Check for proper termination
         with open(output_filepath, 'rb+') as xtbout_file:
             xtbout_file.seek(-2, 2)
             while xtbout_file.read(1) != b"\n":
@@ -211,7 +112,7 @@ class CRESTOutput(MSONable):
                 self.properly_terminated = True
 
         if self.properly_terminated:
-
+            # Parse for number of conformers and rotamers
             conformer_pattern = re.compile(
                 r"\s+\d+\s+(?P<Erel>\d*\.\d*)\s+(?P<Etot>-*\d+\.\d+)\s+(?P<weight>-*\d+\.\d+)\s+"
                 r"(?P<conformer>-*\d+\.\d+)\s+(?P<set>\d+)\s+(?P<degen>\d+)\s+(?P<origin>\w+)\n")
@@ -229,26 +130,19 @@ class CRESTOutput(MSONable):
                         energies.append(conformer_match['Etot'])
                     elif rotamer_match:
                         energies.append(rotamer_match['Etot'])
+            # Get final rotamers file and read in all molecules, sorted by conformer type and energy
             if 'crest_rotamers.xyz' in os.listdir(self.path):
-                final_rotamer_filename='crest_rotamers.xyz'
+                final_rotamer_filename = 'crest_rotamers.xyz'
             else:
                 n_rot_files = []
                 for f in os.listdir(self.path):
                     if 'crest_rotamers' in f:
                         n_rot_file = int(os.path.splitext(f)[0].split('_')[2])
                         n_rot_files.append(n_rot_file)
-                final_rotamer_filename = 'crest_rotamers_{}.xyz'.format(max(n_rot_files))
-
-            crestbest_path = os.path.join(self.path, 'crest_best.xyz')
-            rotamers_path = os.path.join(self.path, final_rotamer_filename)
-
+                if len(n_rot_files) > 0:
+                    final_rotamer_filename = 'crest_rotamers_{}.xyz'.format(max(n_rot_files))
             try:
-                lowest_e_struct = Molecule.from_file(crestbest_path)
-                lowest_e_struct.set_charge_and_spin(charge=chg)
-                self.lowest_energy_structure = lowest_e_struct
-            except FileNotFoundError:
-                print('{} not found'.format(crestbest_path))
-            try:
+                rotamers_path = os.path.join(self.path, final_rotamer_filename)
                 rotamer_structures = XYZ.from_file(rotamers_path).all_molecules
                 for r in rotamer_structures:
                     r.set_charge_and_spin(charge=chg)
@@ -258,10 +152,18 @@ class CRESTOutput(MSONable):
                     i = 0
                     for i in range(start, start + d):
                         self.sorted_structures_energies[n].append([rotamer_structures[i], energies[i]])
-                    start = i
-
+                    start = i + 1
             except FileNotFoundError:
-                print('{} not found'.format(rotamers_path))
+                print('{} not found, no rotamer list processed'.format(final_rotamer_filename))
+
+            # Get lowest energy conformer from 'crest_best.xyz'
+            crestbest_path = os.path.join(self.path, 'crest_best.xyz')
+            try:
+                lowest_e_struct = Molecule.from_file(crestbest_path)
+                lowest_e_struct.set_charge_and_spin(charge=chg)
+                self.lowest_energy_structure = lowest_e_struct
+            except FileNotFoundError:
+                print('{} not found'.format(crestbest_path))
 
         else:
             crestbest_path = os.path.join(self.path, 'crest_best.xyz')
@@ -271,48 +173,3 @@ class CRESTOutput(MSONable):
                 self.lowest_energy_structure = lowest_e_struct
             except FileNotFoundError:
                 print('{} not found'.format(crestbest_path))
-
-# def parse_xtb_output(file_path="xtb.out"):
-#     """
-#     Things we need to parse:
-#     - Final energy
-#     - Final enthalpy
-#     - Final entropy
-#     - Final free energy
-#     - Heat capacity
-#     - Charge?
-#
-#     :param file_path:
-#     :return:
-#     """
-#
-#     # In Hartree
-#     energy_pattern = re.compile(r"\s+\| TOTAL ENERGY\s+(?P<energy>[\-\.0-9]+) Eh")
-#
-#     # In hartree
-#     tot_enthalpy = re.compile(r"\s+\| TOTAL ENTHALPY\s+(?P<total_enthalpy>[\-\.0-9]+) Eh")
-#
-#     # In hartree
-#     tot_gibbs = re.compile(r"\s+\| TOTAL FREE ENERGY\s+(?P<total_free_energy>[\-\.0-9]+) Eh")
-#
-#     # In cal/mol, cal/mol-K, cal/mol-K, respectively
-#     tot_pattern = re.compile(r"TOT\s+(?P<enthalpy>[\-\.0-9]+)\s+(?P<heat_capacity>[\-\.0-9]+)\s+(?P<entropy>[\-\.0-9]+)")
-#
-#     with open(file_path, 'r') as xtbout_file:
-#         contents = xtbout_file.read()
-#
-#         total_energy = float(energy_pattern.search(contents).group("energy"))
-#         total_enthalpy = float(tot_enthalpy.search(contents).group("total_enthalpy"))
-#         total_gibbs = float(tot_gibbs.search(contents).group("total_free_energy"))
-#
-#         tot_match = tot_pattern.search(contents)
-#         enthalpy = float(tot_match.group("enthalpy")) / 1000
-#         heat_capacity = float(tot_match.group("heat_capacity"))
-#         entropy = float(tot_match.group("entropy"))
-#
-#         return {"energy": total_energy,
-#                 "total_h": total_enthalpy,
-#                 "total_g": total_gibbs,
-#                 "enthalpy": enthalpy,
-#                 "cp": heat_capacity,
-#                 "entropy": entropy}
