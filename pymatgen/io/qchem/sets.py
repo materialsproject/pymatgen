4c047c8503f28a274c74fbc069c03625a1937d15
Ryan Kingsbury <RKingsbury@lbl.gov>
2021-01-25 07:01:04 -0800

    documentation for QChemDictSet

--- pymatgen/io/qchem/sets.py
+++ pymatgen/io/qchem/sets.py
@@ -44,21 +44,47 @@ class QChemDictSet(QCInput):
         """
         Args:
             molecule (Pymatgen molecule object)
-            job_type (str)
-            basis_set (str)
-            scf_algorithm (str)
-            dft_rung (int)
-            pcm_dielectric (str)
-            max_scf_cycles (int)
-            geom_opt_max_cycles (int)
-            plot_cubes (bool)
-            overwrite_inputs (dict): This is dictionary of QChem input sections to add or overwrite variables,
-            the available sections are currently rem, pcm, and solvent. So the accepted keys are rem, pcm, or solvent
-            and the value is a dictionary of key value pairs relevant to the section. An example would be adding a
-            new variable to the rem section that sets symmetry to false.
-            ex. overwrite_inputs = {"rem": {"symmetry": "false"}}
-            ***It should be noted that if something like basis is added to the rem dict it will overwrite
-            the default basis.***
+            job_type (str): QChem job type to run. Valid options are "opt" for optimization,
+                "sp" for single point, "freq" for frequency calculation, or "force" for
+                force evaluation.
+            basis_set (str): Basis set to use. For example, "def2-tzvpd"
+            scf_algorithm (str): Algorithm to use for converging the SCF. Valid choices are
+                "DIIS", "DM", "DIIS_DM", "DIIS_GDM", "LS_DIIS", "GDM", "RCA", "RCA_DIIS", and
+                "ROOTHAN". In general, "DIIS" is recommended. Refer to the QChem manual for
+                further details.
+            dft_rung (int): Select the DFT functional among 5 recommended levels of theory,
+                in order of increasing accuracy/cost. 1 = B3LYP, 2=B3lYP+D3, 3=ωB97X-D,
+                4=ωB97X-V, 5=ωB97M-V. (Default: 4)
+
+                To set a functional not given by one of the above, set the overwrite_inputs
+                argument to {"method":"<NAME OF FUNCTIONAL>"}
+
+                **Note that the "rungs" in this argument do NOT correspond to rungs on "Jacob's
+                Ladder of Density Functional Approxmations"**
+            pcm_dielectric (int): Dielectric constant to use for PCM implicit solvation model. (Default: None)
+            smd_solvent (str): Solvent to use for SMD implicit solvation model. (Default: None)
+                Examples include "water", "tetrahydrofuran", "ethanol", "methanol", and "acetonitrile".
+                Refer to the QChem manual for a complete list of solvents available. To define a custom
+                solvent, set this argument to "custom" and populate custom_smd with the necessary parameters. 
+                
+                **Note that only one of smd_solvent and pcm_dielectric may be set.**
+            custom_smd (str): List of parameters to define a custom solvent in SMD. (Default: None)
+                Must be given as a string of seven comma separated values in the following order: 
+                "dielectric, refractive index, acidity, basicity, surface tension, aromaticity,
+                electronegative halogenicity"
+                Refer to the QChem manual for further details.
+            max_scf_cycles (int): Maximum number of SCF cycles. (Default: 200)
+            geom_opt_max_cycles (int): Maximum number of geometry optimization cycles. (Default: 200)
+            plot_cubes (bool): Whether to write CUBE files of the electron density. (Default: False)
+            overwrite_inputs (dict): Dictionary of QChem input sections to add or overwrite variables.
+                The currently available sections (keys) are currently rem, pcm, and solvent. The value of
+                each key is a dictionary of key value pairs relevant to that section. 
+                
+                For example, to add a new variable to the rem section that sets symmetry to false, use
+                overwrite_inputs = {"rem": {"symmetry": "false"}}
+
+                **Note that if something like basis is added to the rem dict it will overwrite
+                the default basis.**
         """
         self.molecule = molecule
         self.job_type = job_type
@@ -120,7 +146,7 @@ class QChemDictSet(QCInput):
 
         if self.pcm_dielectric is not None:
             mypcm = pcm_defaults
-            mysolvent["dielectric"] = self.pcm_dielectric
+            mysolvent["dielectric"] = str(self.pcm_dielectric)
             myrem["solvent_method"] = "pcm"
 
         if self.smd_solvent is not None:
