af9ec275dc2a74b2c960285133166a68de9ccab0
IamRoot! <shyuep@gmail.com>
2021-01-31 20:15:22 -0800

    Remove upgrade numpy.

--- setup.py
+++ setup.py
@@ -178,8 +178,10 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
