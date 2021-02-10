7e3a7df08e3d4e62af037f6f80cb012504a94fb7
Rhys Goodall <reag2@cam.ac.uk>
2021-02-10 16:23:46 +0000

    clean: remove robust dict checking from Entry as not used

--- pymatgen/entries/__init__.py
+++ pymatgen/entries/__init__.py
@@ -151,34 +151,7 @@ class Entry(MSONable, metaclass=ABCMeta):
 
         return self.composition == other.composition
 
-    def _is_dict_eq(self, other):
-        """
-        Check if entry dicts are equal using a robust check for
-        numerical values.
-        """
-        self_dict = self.as_dict()
-        other_dict = other.as_dict()
-
-        # NOTE use implicit generator to allow all() to short-circuit
-        return all(_is_robust_eq(other_dict[k], v) for k, v in self_dict.items())
-
     def __hash__(self):
         # NOTE truncate _energy to 8 dp to ensure same robustness
         # as np.allclose
-        return hash(f"{self.__class__.__name__}" f"{self._composition.formula}" f"{self._energy:.8f}")
-
-
-def _is_robust_eq(v_self, v_other):
-    """
-    Use np.allclose for numerical values for robustness
-    otherwise use default __eq__.
-
-    NOTE robustness doesn't reach to nested structures i.e. For a
-    ComputedStructureEntry where parameters stores the Incar this would
-    not be robust to fp changes in that Incar dictionary. For a
-    GrandPotPDEntry it will not be robust to fp changes in the chempots
-    """
-    if isinstance(v_self, Number) and isinstance(v_other, Number):
-        return np.allclose(v_self, v_other, atol=1e-8)
-
-    return v_self == v_other
+        return hash(f"{self.__class__.__name__}{self._composition.formula}{self._energy:.8f}")
