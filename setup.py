17f025260889702240d7981fe5c664eecb25eed5
IamRoot! <shyuep@gmail.com>
2021-02-12 14:52:45 -0800

    Remove py36 test and support. Add py39.

--- setup.py
+++ setup.py
@@ -181,7 +181,6 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
