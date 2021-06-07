144a51502f858dac6e6599f3386eaa8afd1879ad
Alex Epstein <aepstein@lbl.gov>
2021-06-07 14:30:17 -0700

    Working dir
    Added working_dir parameter to allow for writing files outside of current directory. Also improved docstrings

--- pymatgen/io/xtb/inputs.py
+++ pymatgen/io/xtb/inputs.py
@@ -5,6 +5,7 @@
 Classes for writing XTB input files
 """
 import logging
+import os
 from typing import Dict, Optional, Union, List
 
 from monty.json import MSONable
@@ -31,6 +32,7 @@ class CRESTInput(MSONable):
     def __init__(
         self,
         molecule: Molecule,
+        working_dir: str = ".",
         coords_filename: Optional[str] = "crest_in.xyz",
         constraints: Optional[Dict[str, Union[List[int], float]]] = None,
     ):
@@ -38,6 +40,8 @@ class CRESTInput(MSONable):
 
         :param molecule (pymatgen Molecule object):
             Input molecule, the only required CREST input.
+        :param working_dir (str):
+            Location to write input files, defaults to current directory
         :param coords_filename (str):
             Name of input coordinates file
         :param constraints (Dict):
@@ -48,18 +52,40 @@ class CRESTInput(MSONable):
         self.molecule = molecule
         self.coords_filename = coords_filename
         self.constraints = constraints
+        self.working_dir = working_dir
 
     def write_input_files(self):
-        self.molecule.to(filename=self.coords_filename)
+        """
+        Write input files to working directory
+        """
+
+        self.molecule.to(
+            filename=os.path.join(self.working_dir, self.coords_filename)
+        )
         if self.constraints:
             constrains_string = self.constrains_template(
-                molecule=self.molecule, reference_fnm=self.coords_filename,
-                constraints=self.constraints)
+                molecule=self.molecule,
+                reference_fnm=self.coords_filename,
+                constraints=self.constraints,
+            )
             with open(".constrains", "w") as f:
                 f.write(constrains_string)
 
     @staticmethod
     def constrains_template(molecule, reference_fnm, constraints) -> str:
+        """
+
+        :param molecule (pymatgen Molecule):
+            Molecule the constraints will be performed on
+        :param reference_fnm:
+            Name of file containing reference structure in same directory
+        :param constraints:
+           Dictionary of common editable parameters for .constrains file.
+            {"atoms": [List of 1-indexed atoms to fix], "force_constant":
+            float]
+        :return:
+            String for .constrains file
+        """
         atoms_to_constrain = constraints["atoms"]
         force_constant = constraints["force_constant"]
         reference_fnm = reference_fnm
