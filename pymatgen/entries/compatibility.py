materialsproject/pymatgen

commit e8028b0cfe783f8628c9090da07ff6e435244f6c

Linting

pymatgen/entries/compatibility.py
@@ -1218,11 +1218,14 @@ class MaterialsProjectAqueousCompatibility(Compatibility):
         """
         self.solid_compat = None
         # check whether solid_compat has been instantiated
-        if solid_compat:
-            if not isinstance(solid_compat, Compatibility):
-                self.solid_compat = solid_compat()
-            else:
-                self.solid_compat = solid_compat  # type: ignore
+        if solid_compat is None:
+            self.solid_compat = None
+        elif issubclass(solid_compat, Compatibility):
+            self.solid_compat = solid_compat()
+        elif issubclass(type(solid_compat),Compatibility):
+            self.solid_compat = solid_compat
+        else
+            raise ValueError("Expected a Compatability class, instance of a Compatability or None")
 
         self.o2_energy = o2_energy
         self.h2o_energy = h2o_energy

pymatgen/io/qchem/outputs.py
@@ -1449,7 +1449,7 @@ def jump_to_header(lines: List[str], header: str) -> List[str]:
             Truncated lines.
 
     Raises:
-            HeaderNotFoundError
+            RuntimeError
     """
 
     # Search for the header
@@ -1519,7 +1519,7 @@ def parse_natural_populations(lines: List[str]) -> List[pd.DataFrame]:
             Data frame of formatted output.
 
     Raises:
-            HeaderNotFoundError
+            RuntimeError
     """
 
     no_failures = True
@@ -1530,7 +1530,7 @@ def parse_natural_populations(lines: List[str]) -> List[pd.DataFrame]:
         # Natural populations
         try:
             lines = jump_to_header(lines, "Summary of Natural Population Analysis:")
-        except HeaderNotFoundError:
+        except RuntimeError:
             no_failures = False
 
         if no_failures:
@@ -1582,7 +1582,7 @@ def parse_hybridization_character(lines: List[str]) -> List[pd.DataFrame]:
             Data frames of formatted output.
 
     Raises:
-            HeaderNotFoundError
+            RuntimeError
     """
 
     # Orbitals
@@ -1596,7 +1596,7 @@ def parse_hybridization_character(lines: List[str]) -> List[pd.DataFrame]:
         # NBO Analysis
         try:
             lines = jump_to_header(lines, "(Occupancy)   Bond orbital/ Coefficients/ Hybrids")
-        except HeaderNotFoundError:
+        except RuntimeError:
             no_failures = False
 
         if no_failures:
@@ -1725,7 +1725,7 @@ def parse_perturbation_energy(lines: List[str]) -> List[pd.DataFrame]:
             Data frame of formatted output.
 
     Raises:
-            HeaderNotFoundError
+            RuntimeError
     """
 
     no_failures = True
@@ -1739,7 +1739,7 @@ def parse_perturbation_energy(lines: List[str]) -> List[pd.DataFrame]:
                 lines,
                 "SECOND ORDER PERTURBATION THEORY ANALYSIS OF FOCK MATRIX IN NBO BASIS",
             )
-        except HeaderNotFoundError:
+        except RuntimeError:
             no_failures = False
 
         if no_failures:
@@ -1809,7 +1809,7 @@ def nbo_parser(filename: str) -> Dict[str, List[pd.DataFrame]]:
             Data frames of formatted output.
 
     Raises:
-            HeaderNotFoundError
+            RuntimeError
     """
 
     # Open the lines

commit 9d1737d4fc4ae64a9851deb0f15b2d02b9b8caca

fix bug introduced by e8028b0cfe783f8628c9090da07ff6e435244f6c

pymatgen/entries/compatibility.py
@@ -1220,12 +1220,12 @@ class MaterialsProjectAqueousCompatibility(Compatibility):
         # check whether solid_compat has been instantiated
         if solid_compat is None:
             self.solid_compat = None
-        elif issubclass(solid_compat, Compatibility):
+        elif isinstance(solid_compat, str):
             self.solid_compat = solid_compat()
         elif issubclass(type(solid_compat), Compatibility):
             self.solid_compat = solid_compat
         else:
-            raise ValueError("Expected a Compatability class, instance of a Compatability or None")
+            raise ValueError("Expected a Compatability class as a string, instance of a Compatability or None")
 
         self.o2_energy = o2_energy
         self.h2o_energy = h2o_energy