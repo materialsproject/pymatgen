b3e9c3bf831da08bc63f120630ffa57d055fda00
Shyue Ping Ong <shyuep@gmail.com>
2021-06-24 06:56:52 -0700

    Remove offending to_csv line.

--- pymatgen/analysis/xps.py
+++ pymatgen/analysis/xps.py
@@ -31,8 +31,7 @@ from pymatgen.electronic_structure.dos import CompleteDos
 
 def _load_cross_sections(fname):
     data = pd.read_csv(fname)
-    data.to_csv(fname)
-
+    
     d = collections.defaultdict(dict)
     for index, row in data.iterrows():
         sym = row["element"]
