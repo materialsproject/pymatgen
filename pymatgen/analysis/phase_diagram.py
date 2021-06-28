e9a9aa5a9c86d612b20aeb5db68882c66fbee8f1
Rhys Goodall <reag2@cam.ac.uk>
2021-06-28 15:44:30 +0100

    Update phase_diagram.py

--- pymatgen/analysis/phase_diagram.py
+++ pymatgen/analysis/phase_diagram.py
@@ -632,7 +632,7 @@ class BasePhaseDiagram(MSONable):
         _, hull_energy = self.get_decomp_and_hull_energy_per_atom(comp)
         return comp.num_atoms * hull_energy
 
-    def get_decomp_and_e_above_hull(self, entry, allow_negative=False):
+    def get_decomp_and_e_above_hull(self, entry, allow_negative=False, check_stable=False):
         """
         Provides the decomposition and energy above convex hull for an entry.
         Due to caching, can be much faster if entries with the same composition
@@ -651,8 +651,9 @@ class BasePhaseDiagram(MSONable):
         """
         # Avoid computation for stable_entries.
         # NOTE scaled duplicates of stable_entries will not be caught.
-        if entry in list(self.stable_entries):
-            return {entry: 1}, 0
+        if check_stable:
+            if entry in list(self.stable_entries):
+                return {entry: 1}, 0
         
         decomp, hull_energy = self.get_decomp_and_hull_energy_per_atom(entry.composition)
         e_above_hull = entry.energy_per_atom - hull_energy
