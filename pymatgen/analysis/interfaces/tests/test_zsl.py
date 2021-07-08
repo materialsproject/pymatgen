3fbf8c2147d558828bc8ea3a702073a52edda5d6
Shyam D <shyamd@lbl.gov>
2021-07-08 13:10:17 -0700

    fix linting

--- pymatgen/analysis/interfaces/tests/test_zsl.py
+++ pymatgen/analysis/interfaces/tests/test_zsl.py
@@ -59,7 +59,7 @@ class ZSLGenTest(PymatgenTest):
         z.bidirectional = True
         matches = list(z(self.substrate.lattice.matrix[:2], self.film.lattice.matrix[:2]))
         self.assertEqual(len(matches), 48)
-        
+
         for match in matches:
             assert match is not None
 
