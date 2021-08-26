1f80afc0e273664b00c149ee85420975f8ebaa0e
Ryan Kingsbury <RKingsbury@lbl.gov>
2021-08-26 16:36:40 -0700

    rename args; split TemplateInputSet

--- pymatgen/io/input.py
+++ pymatgen/io/input.py
@@ -1,5 +1,5 @@
 """
-This module defines the abstract interface for pymatgen Input classes.
+This module defines the abstract interface for pymatgen InputSet and InputSetGenerator classes.
 """
 
 import abc
@@ -8,7 +8,6 @@ from string import Template
 from typing import Union, Optional, Dict
 from monty.json import MSONable
 from monty.io import zopen
-from pymatgen.core import Structure, Molecule
 
 
 __author__ = "Ryan Kingsbury"
@@ -17,80 +16,73 @@ __status__ = "Development"
 __date__ = "August 2021"
 
 
-class Input(MSONable):
+class InputSet(MSONable):
     """
-    Abstract base class for all Input classes. Input classes serve
+    Abstract base class for all InputSet classes. InputSet classes serve
     as containers for all calculation input data.
 
-    All Input must implement a write_inputs method. Implementing
-    the from_files method and is_valid property are optional.
+    All InputSet must implement a write_inputs method. Implementing
+    the from_files and validate methods is optional.
     """
 
     @abc.abstractmethod
     def write_inputs(
         self,
-        dir: Union[str, Path],
-        make_dir_if_not_present: bool = True,
-        overwrite_if_present: bool = True,
+        directory: Union[str, Path],
+        make_dir: bool = True,
+        overwrite: bool = True,
     ):
         """
         Write Inputs to one or more files
 
         Args:
-            dir: Directory to write input files to
-            make_dir_if_not_present: Whether to create the directory if it does not already exist.
-            overwrite_if_present: Whether to overwrite an input file if it already exists.
+            directory: Directory to write input files to
+            make_dir: Whether to create the directory if it does not already exist.
+            overwrite: Whether to overwrite an input file if it already exists.
         """
         pass
 
     @classmethod
     @abc.abstractmethod
-    def from_files(cls, dir: Union[str, Path]):
+    def from_directory(cls, directory: Union[str, Path]):
         """
-        Construct an Input from a directory of one or more files.
+        Construct an InputSet from a directory of one or more files.
 
         Args:
-            dir: Directory to read input files from
+            directory: Directory to read input files from
         """
-        raise NotImplementedError(f"from_files has not been implemented in {cls}")
+        raise NotImplementedError(f"from_directory has not been implemented in {cls}")
 
-    @property
-    def is_valid(self) -> bool:
+    def validate(self) -> bool:
         """
         A place to implement basic checks to verify the validity of an
         input set. Can be as simple or as complex as desired.
 
         Will raise a NotImplementedError unless overloaded by the inheriting class.
         """
-        raise NotImplementedError(f"is_valid has not been implemented in {self.__class__}")
+        raise NotImplementedError(f".validate() has not been implemented in {self.__class__}")
 
 
-class InputSettings(MSONable):
+class InputSetGenerator(MSONable):
     """
-    InputSettings classes serve as generators for Input objects. They contain
+    InputSetGenerator classes serve as generators for Input objects. They contain
     settings or sets of instructions for how to create Input from a set of
     coordinates or a previous calculation directory.
     """
 
-    @staticmethod
     @abc.abstractmethod
-    def get_input(coordinates: Union[Structure, Molecule], prev_dir: Union[str, Path] = None) -> Input:
+    def get_input_set(self) -> InputSet:
         """
-        Generate an Input object
-
-        Args:
-            coordinates: Atomic coordinates. May be passed as a Structure, Molecule, or path to
-                any file that can be converted to one via Structure.from_file() or Molecule.from_file().
-            prev_dir: Location of a previous calculation to inherit settings from.
+        Generate an InputSet object. Typically the first argument to this method
+        will be a Structure or other form of atomic coordinates.
         """
         pass
 
 
-class TemplateInput(Input):
+class TemplateInputSet(InputSet):
     """
-    Concrete implementation of Input that is based on a single template input
-    file with variables. The .write_inputs() method simply substitutes values into
-    the variables and writes out a new file.
+    Concrete implementation of InputSet that is based on a single template input
+    file with variables.
 
     This class is provided as a low-barrier way to support new codes and to provide
     an intuitive way for users to transition from manual scripts to pymatgen I/O
@@ -112,26 +104,26 @@ class TemplateInput(Input):
 
     def write_inputs(
         self,
-        dir: Union[str, Path],
-        make_dir_if_not_present: bool = True,
-        overwrite_if_present: bool = True,
+        directory: Union[str, Path],
+        make_dir: bool = True,
+        overwrite: bool = True,
         filename: str = "input.txt",
     ):
         """
         Args:
-            dir: Directory to write input files to
-            make_dir_if_not_present: Whether to create the directory if it does not already exist.
-            overwrite_if_present: Whether to overwrite an input file if it already exists.
+            directory: Directory to write input files to
+            make_dir: Whether to create the directory if it does not already exist.
+            overwrite: Whether to overwrite an input file if it already exists.
             filename: name of the file to be written
         """
-        path = dir if isinstance(dir, Path) else Path(dir)
+        path = directory if isinstance(directory, Path) else Path(directory)
         file = path / filename
 
         if not path.exists():
-            if make_dir_if_not_present:
+            if make_dir:
                 path.parent.mkdir(parents=True, exist_ok=True)
 
-        if file.exists() and not overwrite_if_present:
+        if file.exists() and not overwrite:
             raise FileExistsError(f"File {str(filename)} already exists!")
         file.touch()
 
@@ -145,3 +137,36 @@ class TemplateInput(Input):
         # write the file
         with zopen(file, "wt") as f:
             f.write(to_write)
+
+
+class TemplateInputSetGenerator(InputSetGenerator):
+    """
+    Concrete implementation of InputSetGenerator that is based on a single template
+    input file with variables. The .get_input_set() method simply substitutes values
+    into the variables.
+
+    This class is provided as a low-barrier way to support new codes and to provide
+    an intuitive way for users to transition from manual scripts to pymatgen I/O
+    classes.
+    """
+
+    def __init__(self, template: Union[str, Path]):
+        """
+        Args:
+            template: the input file template containing variable strings to be
+                replaced.
+        """
+        self.template = template
+
+    def get_input_set(self, variables: Optional[Dict] = None) -> TemplateInputSet:
+        """
+        Generate a TemplateInputSet object based on a dict of concrete variable
+        substitutions.
+
+        Args:
+            variables: dict of variables to replace in the template. Keys are the
+                text to replaced with the values, e.g. {"TEMPERATURE": 298} will
+                replace the text $TEMPERATURE in the template. See Python's
+                Template.safe_substitute() method documentation for more details.
+        """
+        return TemplateInputSet(self.template, variables)
