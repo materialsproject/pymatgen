e3c448bddace6febf9a9f33fc6c40998d9c18fdc
nwinner <nwinner@berkeley.edu>
2021-08-31 13:24:21 -0700

    Use the getattr operation to be case insensitive.

--- pymatgen/io/cp2k/inputs.py
+++ pymatgen/io/cp2k/inputs.py
@@ -505,11 +505,11 @@ class Section(MSONable):
 
         """
         _path = path.split("/")
-        if _path[0] == self.name:
+        if _path[0].upper() == self.name.upper():
             _path = _path[1:]
         s = self
         for p in _path:
-            s = s.subsections[p]  # only search subsections in case of repeat name
+            s = s[p]
         return s
 
     def get_string(self):
