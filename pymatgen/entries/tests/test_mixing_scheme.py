4c18c2242d766835fc32e66a3078cf63963bce31
Ryan Kingsbury <RKingsbury@lbl.gov>
2021-10-16 09:35:19 -0700

    WIP2 - 14/29

--- pymatgen/entries/tests/test_mixing_scheme.py
+++ pymatgen/entries/tests/test_mixing_scheme.py
@@ -519,7 +519,7 @@ def ms_gga_2_scan_diff_no_match(ms_complete):
     ground state and one unstable polymoprh of SnBr2 (r2scan-4 and r2scan-6)
     """
     gga_entries = ms_complete.gga_entries
-    scan_entries = [e for e in ms_complete.scan_entries if e.entry_id in ["r2scan-4", "r2scan-6"]]
+    scan_entries = [e for e in ms_complete.scan_entries if e.entry_id in ["r2scan-4"]]
     scan_entries.append(
         ComputedStructureEntry(
             Structure(
@@ -541,14 +541,14 @@ def ms_gga_2_scan_diff_no_match(ms_complete):
 
     # fmt: off
     row_list = [
-        ["Sn",   191,  1, "gga-1",       None, "GGA",     None,  0, np.nan, True,     0, np.nan],
-        ["Br",   191,  1, "gga-2",       None, "GGA",     None,  1, np.nan, False,    0, np.nan],
-        ["Br",    64,  4, "gga-3",       None, "GGA",     None,  0, np.nan, True,     0, np.nan],
-        ["SnBr2", 65,  3, "gga-4", "r2scan-4", "GGA", "R2SCAN", -6, np.nan, True,    -6, np.nan],
-        ["SnBr2",  2, 12, "gga-5",       None, "GGA",     None, -5, np.nan, False,   -6, np.nan],
-        ["SnBr2", 71,  3, "gga-6", "r2scan-4", "GGA", "R2SCAN", -4, np.nan, False,   -6, np.nan],
-        ["SnBr4",  8,  5, "gga-7",       None, "GGA",     None, -3, np.nan, False, -3.6, np.nan],
-        ["SnBr4",  8,  5,    None, "r2scan-8",  None, "R2SCAN", np.nan, -6, False, -3.6, -6],
+        ["Sn",   191,  1, "gga-1",       None, "GGA",     None,      0, np.nan, True,     0, np.nan],
+        ["Br",   191,  1, "gga-2",       None, "GGA",     None,      1, np.nan, False,    0, np.nan],
+        ["Br",    64,  4, "gga-3",       None, "GGA",     None,      0, np.nan, True,     0, np.nan],
+        ["SnBr2", 65,  3, "gga-4", "r2scan-4", "GGA", "R2SCAN",     -6,     -7, True,    -6, np.nan],
+        ["SnBr2",  2, 12, "gga-5",       None, "GGA",     None,     -5, np.nan, False,   -6, np.nan],
+        ["SnBr2", 71,  3, "gga-6",       None, "GGA",     None,     -4, np.nan, False,   -6, np.nan],
+        ["SnBr4",  8,  5, "gga-7",       None, "GGA",     None,     -3, np.nan, False, -3.6, np.nan],
+        ["SnBr4",  8,  5,    None, "r2scan-8",  None, "R2SCAN", np.nan,     -5, False, -3.6, np.nan],
     ]
     # fmt: on
     mixing_state = pd.DataFrame(row_list, columns=columns)
@@ -1160,9 +1160,8 @@ class TestMaterialsProjectDFTMixingSchemeStates:
 
         for e in ms_gga_2_scan_diff_match.scan_entries:
             if e.entry_id == "r2scan-7":
-                # gga-4 energy is -18 eV or -6 eV/atom. r2scan-6 is 1 eV/atom above r2scan-4 (ground state),
-                # so r2scan-6 should be adjusted to -5 eV/atom or -15 eV, which is 3 eV higher than its
-                # original energy of -18 eV.
+                # there is no matching GGA structure for r2scan-7, so there's no way
+                # to adjust its energy onto the GGA hull.
                 with pytest.raises(CompatibilityError, match="no R2SCAN reference energy"):
                     mixing_scheme_no_compat.get_adjustments(e, ms_gga_2_scan_diff_match.state_data)
             elif e.entry_id == "r2scan-4":
@@ -1190,10 +1189,7 @@ class TestMaterialsProjectDFTMixingSchemeStates:
                 assert e.correction == 0, f"{e.entry_id}"
                 assert e.parameters["run_type"] == "GGA"
 
-    # TODO - add an unstable polymorph of SnBr4 in SCAN only that doesn't match
-    # any GGA material.
-    @pytest.mark.skip(reason="Not implemented yet")
-    def test_state_gga_2_scan_diff_nomatch(self, mixing_scheme_no_compat):
+    def test_state_gga_2_scan_diff_nomatch(self, mixing_scheme_no_compat, ms_gga_2_scan_diff_no_match):
         """
         Mixing state in which we have a complete GGA PhaseDiagram and 2 SCAN entries
         at different compositions, where one of the SCAN materials does not match
@@ -1203,7 +1199,46 @@ class TestMaterialsProjectDFTMixingSchemeStates:
         to the GGA value, the corresponding GGA entry should be discarded, and the
         novel SCAN material that doesn't match anything should be discarded
         """
-        pass
+        state_data = mixing_scheme_no_compat.get_mixing_state_data(ms_gga_2_scan_diff_no_match.all_entries)
+        assert isinstance(state_data, pd.DataFrame), "get_mixing_state_data failed to generate a DataFrame."
+        assert sum(state_data["run_type_1"].isna()) == 1
+        assert sum(state_data["run_type_2"] == "R2SCAN") == 2
+        assert sum(state_data["is_stable_1"]) == 3
+        assert sum(state_data["ground_state_energy_1"].isna()) == 1
+        assert sum(np.isnan(state_data["ground_state_energy_2"])) == 6
+        assert all(state_data["hull_energy_1"].notna())
+        assert all(np.isnan(state_data["hull_energy_2"]))
+
+        for e in ms_gga_2_scan_diff_no_match.scan_entries:
+            if e.entry_id == "r2scan-8":
+                # there is no matching GGA structure for r2scan-8, so there's no way
+                # to adjust its energy onto the GGA hull.
+                with pytest.raises(CompatibilityError, match="no R2SCAN reference energy"):
+                    mixing_scheme_no_compat.get_adjustments(e, ms_gga_2_scan_diff_no_match.state_data)
+            elif e.entry_id == "r2scan-4":
+                # r2scan-4 energy is -7 eV/atom. Needs to be adjusted to -6 eV/atom (3 atoms)
+                assert mixing_scheme_no_compat.get_adjustments(e, ms_gga_2_scan_diff_no_match.state_data)[0].value == 3
+            else:
+                assert mixing_scheme_no_compat.get_adjustments(e, ms_gga_2_scan_diff_no_match.state_data) == []
+
+        for e in ms_gga_2_scan_diff_no_match.gga_entries:
+            if e.entry_id == "gga-4":
+                with pytest.raises(CompatibilityError, match="it is a GGA\\(\\+U\\) ground state"):
+                    mixing_scheme_no_compat.get_adjustments(e, ms_gga_2_scan_diff_no_match.state_data)
+            else:
+                assert mixing_scheme_no_compat.get_adjustments(e, ms_gga_2_scan_diff_no_match.state_data) == []
+
+        entries = mixing_scheme_no_compat.process_entries(ms_gga_2_scan_diff_no_match.all_entries)
+        assert len(entries) == 7
+        for e in entries:
+            if e.entry_id == "r2scan-4":
+                assert e.correction == 3
+                assert e.parameters["run_type"] == "R2SCAN"
+            elif e.entry_id in ["gga-4", "r2scan-8"]:
+                assert False, f"Entry {e.entry_id} should have been discarded"
+            else:
+                assert e.correction == 0, f"{e.entry_id}"
+                assert e.parameters["run_type"] == "GGA"
 
     @pytest.mark.skip(reason="Not implemented yet")
     def test_state_scan_1_gga(self, mixing_scheme_no_compat):
