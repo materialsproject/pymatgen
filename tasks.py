817ec7282e8f12b2a79343e9b2430200e81f521a
Shyue Ping Ong <shyuep@gmail.com>
2022-01-07 13:47:12 -0800

    Atuomated versioning again.

--- tasks.py
+++ tasks.py
@@ -3,7 +3,7 @@ Pyinvoke tasks.py file for automating releases and admin stuff.
 
 Author: Shyue Ping Ong
 """
-
+import datetime
 import glob
 import json
 import os
@@ -247,7 +247,7 @@ def post_discourse(ctx, version):
 
 
 @task
-def update_changelog(ctx, version, sim=False):
+def update_changelog(ctx, version=datetime.datetime.now().strftime("%Y.%m.%d"), sim=False):
     """
     Create a preliminary change log using the git logs.
 
@@ -289,7 +289,7 @@ def update_changelog(ctx, version, sim=False):
 
 
 @task
-def release(ctx, version, nodoc=False):
+def release(ctx, version=datetime.datetime.now().strftime("%Y.%m.%d"), nodoc=False):
     """
     Run full sequence for releasing pymatgen.
 
