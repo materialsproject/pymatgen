b65acbf60f15520f7781f25180873325ffd39a0d
Andrew <asrosen93@gmail.com>
2022-01-19 13:36:45 -0800

    Fix bader parsing

--- pymatgen/command_line/bader_caller.py
+++ pymatgen/command_line/bader_caller.py
@@ -196,26 +196,26 @@ class BaderAnalysis:
                         UserWarning,
                     )
 
-            data = []
-            with open(os.path.join(bader_output_path, "ACF.dat")) as f:
-                raw = f.readlines()
-                headers = ("x", "y", "z", "charge", "min_dist", "atomic_vol")
-                raw.pop(0)
-                raw.pop(0)
-                while True:
-                    l = raw.pop(0).strip()
-                    if l.startswith("-"):
-                        break
-                    vals = map(float, l.split()[1:])
-                    data.append(dict(zip(headers, vals)))
-                for l in raw:
-                    toks = l.strip().split(":")
-                    if toks[0] == "VACUUM CHARGE":
-                        self.vacuum_charge = float(toks[1])
-                    elif toks[0] == "VACUUM VOLUME":
-                        self.vacuum_volume = float(toks[1])
-                    elif toks[0] == "NUMBER OF ELECTRONS":
-                        self.nelectrons = float(toks[1])
+                data = []
+                with open(os.path.join(bader_output_path, "ACF.dat")) as f:
+                    raw = f.readlines()
+                    headers = ("x", "y", "z", "charge", "min_dist", "atomic_vol")
+                    raw.pop(0)
+                    raw.pop(0)
+                    while True:
+                        l = raw.pop(0).strip()
+                        if l.startswith("-"):
+                            break
+                        vals = map(float, l.split()[1:])
+                        data.append(dict(zip(headers, vals)))
+                    for l in raw:
+                        toks = l.strip().split(":")
+                        if toks[0] == "VACUUM CHARGE":
+                            self.vacuum_charge = float(toks[1])
+                        elif toks[0] == "VACUUM VOLUME":
+                            self.vacuum_volume = float(toks[1])
+                        elif toks[0] == "NUMBER OF ELECTRONS":
+                            self.nelectrons = float(toks[1])
             self.data = data
 
             if self.parse_atomic_densities:
