46415dab1cbf8c830434e21bf79e85da06a377ce
Andrew <asrosen93@gmail.com>
2022-01-20 16:47:35 -0800

    Add test for get_charge() in ChargemolAnalysis

--- pymatgen/command_line/chargemol_caller.py
+++ pymatgen/command_line/chargemol_caller.py
@@ -290,7 +290,7 @@ class ChargemolAnalysis:
             charge_transfer = -self.cm5_charges[atom_index]
         return charge_transfer
 
-    def get_charge(self, atom_index, charge_type="ddec"):
+    def get_charge(self, atom_index, nelect=None, charge_type="ddec"):
         """
         Convenience method to get the charge on a particular atom using the same
         sign convention as the BaderAnalysis. Note that this is *not* the partial
@@ -300,12 +300,19 @@ class ChargemolAnalysis:
 
         Args:
             atom_index (int): Index of atom to get charge for.
+            nelect (int): number of electrons associated with an isolated atom at this index.
+            For most DFT codes this corresponds to the number of valence electrons
+            associated with the pseudopotential. If None, this value will be automatically
+            obtained from the POTCAR (if present).
+                Default: None.
             charge_type (str): Type of charge to use ("ddec" or "cm5").
 
         Returns:
             float: charge on atom_index
         """
-        if self.potcar and self.natoms:
+        if nelect:
+            charge = nelect + self.get_charge_transfer(atom_index, charge_type=charge_type)
+        elif self.potcar and self.natoms:
             charge = None
             potcar_indices = []
             for i, v in enumerate(self.natoms):
