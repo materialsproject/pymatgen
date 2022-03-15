872da47137053e49d3467722982646184dfc4980
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-03-15 22:00:43 +0000

    fix typo

--- pymatgen/symmetry/groups.py
+++ pymatgen/symmetry/groups.py
@@ -216,10 +216,10 @@ class SpaceGroup(SymmetryGroup):
                 Hermann-Mauguin Symbol ("Pmmm") or abbreviated symbol. The
                 notation is a LaTeX-like string, with screw axes being
                 represented by an underscore. For example, "P6_3/mmc".
-                Alternative settings can be access by adding a ":identifier".
+                Alternative settings can be accessed by adding a ":identifier".
                 For example, the hexagonal setting  for rhombohedral cells can be
                 accessed by adding a ":H", e.g., "R-3m:H". To find out all
-                possible settings for a spacegroup, use the get_settings
+                possible settings for a spacegroup, use the get_settings()
                 classmethod. Alternative origin choices can be indicated by a
                 translation vector, e.g., 'Fm-3m(a-1/4,b-1/4,c-1/4)'.
         """
