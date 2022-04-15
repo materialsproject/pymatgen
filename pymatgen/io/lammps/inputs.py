354bee1b7b51d47e57fc28558b1a1dbf7cde5a2b
mholekev <mholekev@ucsd.edu>
2022-04-14 23:50:37 -0700

    method for str representation of LammpsInputFile

--- pymatgen/io/lammps/inputs.py
+++ pymatgen/io/lammps/inputs.py
@@ -38,6 +38,40 @@ class LammpsInputFile(InputFile):
         self.input_settings = input_settings
         self.constr = LammpsInputConstructor()
 
+    def get_string(self):
+        """
+        Generates and returns the string representation of LAMMPS input file.
+        Returns: String representation of LAMMPS input file
+        -------
+
+        """
+
+        # print(self.input_settings)
+        lammps_input = '## LAMMPS input generated from LammpsInputConstructor\n\n'
+        for stage, command_dict in self.input_settings.items():
+            # print(stage, command_dict)
+            for command, args in command_dict.items():
+                # args = self.input_settings[command]
+                if command.startswith('comment') or command.startswith('header'):
+                    if command.startswith('header'):
+                        lammps_input += '\n'
+                    lammps_input += self.add_string(args)
+                else:
+                    lammps_input += self.format_command(command, args)
+        return lammps_input
+
+    @staticmethod
+    def format_command(command, args):
+        """
+        Simple format helper for adding command with the arguments.
+        """
+        return "{0:20} {1}\n".format(command, args)
+
+    @staticmethod
+    def add_string(string):
+        # return '\n' + string + '\n'
+        return string + '\n'
+
 class CombinedData(InputFile):
     """
     Class representing a LAMMPS data file, e.g. system.data
