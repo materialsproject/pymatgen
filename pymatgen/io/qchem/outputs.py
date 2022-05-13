981642c42df1724482fdf51a787fb4c9f41bef0e
samblau <samblau1@gmail.com>
2022-05-13 13:28:58 -0700

    Fix NBO parsing bug

--- pymatgen/io/qchem/outputs.py
+++ pymatgen/io/qchem/outputs.py
@@ -1686,6 +1686,8 @@ def parse_hybridization_character(lines: list[str]) -> list[pd.DataFrame]:
                     break
                 if "Archival summary:" in line:
                     break
+                if "3-Center, 4-Electron A:-B-:C Hyperbonds (A-B :C <=> A: B-C)" in line:
+                    break
 
                 # Lone pair
                 if "LP" in line or "LV" in line:
