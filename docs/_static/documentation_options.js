1f1056cab8b9358f4d1ac2012f2a5732cd29bcf1
Shyue Ping Ong <shyuep@gmail.com>
2022-05-18 18:14:16 -0700

    Update docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2022.5.18.1',
+    VERSION: '2022.5.19',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
