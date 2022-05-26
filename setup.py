573bc8f8af7e8534607647c792e041ec6ad467f5
Matthew Horton <mkhorton@users.noreply.github.com>
2022-05-26 06:57:28 -0700

    Set version to v2022.5.26

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
         include=["pymatgen.*", "pymatgen.analysis.*", "pymatgen.io.*", "pymatgen.ext.*"],
         exclude=["pymatgen.*.tests", "pymatgen.*.*.tests", "pymatgen.*.*.*.tests"],
     ),
-    version="2022.5.19",
+    version="2022.5.26",
     python_requires=">=3.8",
     setup_requires=[
         "Cython>=0.29.23",
