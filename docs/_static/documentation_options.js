4f2424b814a90c2c0b18e63a440bbcc01932d0e3
Shyue Ping Ong <sp@ong.ai>
2022-07-24 14:51:13 -0700

    Update docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2022.7.24',
+    VERSION: '2022.7.24.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
