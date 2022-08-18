7f8a6db6600328a19542d4ebce36c9fb171ebc4b
Scott Thiel <sthiel@umass.edu>
2022-08-18 18:24:47 -0400

    add a test for res

---
+++ pymatgen/io/tests/test_res.py
@@ -0,0 +1,19 @@
+import os
+from pymatgen.io.res import ResParser
+from pymatgen.util.testing import PymatgenTest
+
+
+class TestResRead:
+    # results from AIRSS runs
+    res_coc = "coc-115925-9326-14.res"
+
+    def test_airss_result_as_structure(self):
+        file = os.path.join(PymatgenTest.TEST_FILES_DIR, "res", self.res_coc)
+        res = ResParser._parse_filename(file)
+        assert res.TITL is not None
+        assert res.TITL.seed == self.res_coc[:-4]
+        assert res.TITL.energy - -3.90427411e003 < 0.000001
+        assert res.TITL.spacegroup_label == "R3"
+        assert res.TITL.pressure - 15 < 1
+        assert res.CELL.alpha - 49.32125 < 0.000001
+        assert any(["AIRSS Version 0.9.1" in rem for rem in res.REMS])
