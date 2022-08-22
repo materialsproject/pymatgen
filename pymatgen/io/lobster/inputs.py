a5f9b897ab99c8cc89c10aa5ae10f0eec86a36e8
anaik <anaik@sv2218.zit.bam.de>
2022-08-22 09:13:37 +0200

    Fix liniting errors

--- pymatgen/io/lobster/inputs.py
+++ pymatgen/io/lobster/inputs.py
@@ -646,7 +646,7 @@ class Lobsterin(dict, MSONable):
         POSCAR_input: str = "POSCAR",
         INCAR_input: str = "INCAR",
         POTCAR_input: str | None = None,
-        Vasprun_output : str = "vasprun.xml",
+        Vasprun_output: str = "vasprun.xml",
         dict_for_basis: dict | None = None,
         option: str = "standard",
     ):
@@ -662,8 +662,8 @@ class Lobsterin(dict, MSONable):
 
             option (str): 'standard' will start a normal lobster run where COHPs, COOPs, DOS, CHARGE etc. will be
                 calculated
-                'standard_with_comp_range' will start a normal lobster run where COHPs, COOPs, DOS, CHARGE etc. will be
-                calculated for entire energy range of VASP static run. vasprun.xml file needs to be in current directory.
+                'standard_with_energy_range_from_vasprun' will start a normal lobster run for entire energy range 
+                of VASP static run. vasprun.xml file needs to be in current directory.
                 'standard_from_projection' will start a normal lobster run from a projection
                 'standard_with_fatband' will do a fatband calculation, run over all orbitals
                 'onlyprojection' will only do a projection
@@ -683,7 +683,7 @@ class Lobsterin(dict, MSONable):
             "standard",
             "standard_from_projection",
             "standard_with_fatband",
-            "standard_with_comp_range",
+            "standard_with_energy_range_from_vasprun",
             "onlyprojection",
             "onlydos",
             "onlycohp",
@@ -720,11 +720,11 @@ class Lobsterin(dict, MSONable):
         if option == "standard_from_projection":
             Lobsterindict["cohpGenerator"] = "from 0.1 to 6.0 orbitalwise"
             Lobsterindict["loadProjectionFromFile"] = True
-            
-        if option == "standard_with_comp_range":
-            Vr = Vasprun(Vasprun_output) 
-            Lobsterindict["COHPstartEnergy"] = round(min(Vr.complete_dos.energies-Vr.complete_dos.efermi),4)
-            Lobsterindict["COHPendEnergy"] = round(max(Vr.complete_dos.energies-Vr.complete_dos.efermi),4)
+
+        if option == "standard_with_energy_range_from_vasprun":
+            Vr = Vasprun(Vasprun_output)
+            Lobsterindict["COHPstartEnergy"] = round(min(Vr.complete_dos.energies - Vr.complete_dos.efermi), 4)
+            Lobsterindict["COHPendEnergy"] = round(max(Vr.complete_dos.energies - Vr.complete_dos.efermi), 4)
             Lobsterindict["COHPSteps"] = len(Vr.complete_dos.energies)
 
         # TODO: add cobi here! might be relevant lobster version
