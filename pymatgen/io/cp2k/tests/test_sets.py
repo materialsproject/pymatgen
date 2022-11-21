b3fb40bb48e3aee95981fee1709048be312ee181
nwinner <nwinner@berkeley.edu>
2022-11-21 08:45:59 -0800

    Test set

--- pymatgen/io/cp2k/tests/test_sets.py
+++ pymatgen/io/cp2k/tests/test_sets.py
@@ -4,17 +4,13 @@
 import unittest
 
 from pymatgen.core.structure import Molecule, Species, Structure
-from pymatgen.io.cp2k.sets import Cp2kInputSet, DftSet, HybridStaticSet, RelaxSet
+from pymatgen.io.cp2k.sets import DftSet
 from pymatgen.util.testing import PymatgenTest
 
 Si_structure = Structure(
     lattice=[[0, 2.734364, 2.734364], [2.734364, 0, 2.734364], [2.734364, 2.734364, 0]],
     species=["Si", "Si"],
     coords=[[0, 0, 0], [0.25, 0.25, 0.25]],
-)
-
-nonsense_Structure = Structure(
-    lattice=[[-1, -10, -100], [0.1, 0.01, 0.001], [7, 11, 21]], species=["X"], coords=[[-1, -1, -1]]
 )
 
 molecule = Molecule(species=["C", "H"], coords=[[0, 0, 0], [1, 1, 1]])
@@ -30,6 +26,7 @@ property_structure = Structure(
     ],
     coords=[[0, 0, 0], [0.25, 0.25, 0.25], [0.5, 0.5, 0.5], [1, 1, 1]],
 )
+basis_and_potential={"Si":{"basis": "TZVP", "aux_basis": "FIT3", 'potential': "GTH"}}
 
 
 # TODO More comprehensive testing
@@ -39,13 +36,11 @@ class SetTest(PymatgenTest):
 
     def test_all_sets(self):
         for s in [Si_structure, molecule]:
-            cis = Cp2kInputSet(s)
-            self.assertMSONable(cis)
-            cis = Cp2kInputSet.from_dict(cis.as_dict())
-            Cp2kInputSet.from_string(cis.get_string())
+            cis = DftSet(s)
+            cis = DftSet.from_dict(cis.as_dict())
+            DftSet.from_string(cis.get_string())
 
-            ss = DftSet(s)
-
+            ss = DftSet(s, basis_and_potential=basis_and_potential, cutoff=10)
             assert not ss.check("motion")
             ss.activate_motion()
             assert ss.check("motion")
@@ -54,14 +49,8 @@ class SetTest(PymatgenTest):
             assert ss.check("force_eval/dft/xc/hf")
             assert ss.check("force_eval/dft/auxiliary_density_matrix_method")
 
-    def test_aux_basis(self):
-        Si_aux_bases = ["FIT", "cFIT", "pFIT", "cpFIT"]
-        for s in Si_aux_bases:
-            ss = HybridStaticSet(Si_structure, aux_basis={"Si": s})
-            assert s in ss["force_eval"]["subsys"]["Si_1"]["BASIS_SET"][1].values[-1]
-
     def test_prints(self):
-        cis = RelaxSet(Si_structure, print_ldos=False, print_pdos=False, print_v_hartree=False, print_e_density=False)
+        cis = DftSet(Si_structure, print_ldos=False, print_pdos=False, print_v_hartree=False, print_e_density=False)
         self.assertFalse(cis.check("FORCE_EVAL/DFT/PRINT/PDOS"))
         cis.print_pdos()
         self.assertTrue(cis.check("FORCE_EVAL/DFT/PRINT/PDOS"))
