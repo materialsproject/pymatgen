3bec5c54b0cc59dbcfa0b3fc100dd02d5199d3e3
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-01-04 19:18:54 -0800

    fix SiteSymmetriesTest.test_get_site_symmetries()
      def test_get_site_symmetries(self):
            point_ops = ss.get_site_symmetries(self.piezo_struc)
    >       assert np.all(point_ops == self.point_ops)
    E       assert False
    E        +  where False = <function all at 0x7f64ed8c41f0>([[Rot:\n[[1. 0... 0. 0.]], ...] == array([list([... dtype=object)
    E        +    where <function all at 0x7f64ed8c41f0> = np.all
    E           Full diff:
    E           - array([list([Rot:

--- pymatgen/symmetry/tests/test_site_symmetries.py
+++ pymatgen/symmetry/tests/test_site_symmetries.py
@@ -10,6 +10,7 @@ __status__ = "Development"
 __date__ = "4/23/19"
 
 import os
+import pickle
 
 import numpy as np
 
@@ -21,14 +22,20 @@ test_dir = os.path.join(PymatgenTest.TEST_FILES_DIR, "site_symmetries")
 
 class SiteSymmetriesTest(PymatgenTest):
     def setUp(self):
-        self.point_ops = np.load(os.path.join(test_dir, "point_ops.npy"), allow_pickle=True)
-        self.shared_ops = np.load(os.path.join(test_dir, "shared_ops.npy"), allow_pickle=True)
+        with open(os.path.join(test_dir, "point_ops.pkl"), "rb") as f:
+            self.point_ops = pickle.load(f)
+        with open(os.path.join(test_dir, "shared_ops.pkl"), "rb") as f:
+            self.shared_ops = pickle.load(f)
         self.piezo_struc = self.get_structure("Pb2TiZrO6")
 
     def test_get_site_symmetries(self):
         point_ops = ss.get_site_symmetries(self.piezo_struc)
+        # with open(os.path.join(test_dir, "point_ops.pkl"), "wb") as f:
+        #     pickle.dump(point_ops, f)  # update test file
         assert np.all(point_ops == self.point_ops)
 
     def test_get_shared_symmetries_operations(self):
-        shared_ops = ss.get_shared_symmetry_operations(self.piezo_struc, self.point_ops)
+        shared_ops = ss.get_shared_symmetry_operations(self.piezo_struc, ss.get_site_symmetries(self.piezo_struc))
+        # with open(os.path.join(test_dir, "shared_ops.pkl"), "wb") as f:
+        #     pickle.dump(shared_ops, f)  # update test file
         assert np.all(shared_ops == self.shared_ops)
