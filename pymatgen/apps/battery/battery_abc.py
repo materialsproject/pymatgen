638a0ff9f04ca3ab0b1bd67fb641cbcd0a506ec5
@jmmshn <jmmshn@gmail.com>
2023-01-26 14:40:37 -0800

    docs

--- pymatgen/apps/battery/battery_abc.py
+++ pymatgen/apps/battery/battery_abc.py
@@ -56,16 +56,17 @@ class AbstractVoltagePair(MSONable):
     frac_charge: float
     frac_discharge: float
     working_ion_entry: ComputedEntry
-    framework_formula: str  # should be made into Composition whenever the as_dict and from dict are fixed
+    framework_formula: str  # TODO should be made into Composition whenever the as_dict and from_dict are fixed
 
     def __post_init__(self):
         # ensure the frame work is a reduced composition
-        self.framework_formula = self.framework.reduced_formula
+        fw = Composition(self.framework_formula)
+        self.framework_formula = fw.reduced_formula
 
     @property
     def working_ion(self) -> Element:
         """
-        working ion as pymatgen Element object
+        Working ion as pymatgen Element object
         """
         return self.working_ion_entry.composition.elements[0]
 
@@ -129,6 +130,7 @@ class AbstractElectrode(Sequence, MSONable):
 
     Developers implementing a new battery (other than the two general ones
     already implemented) need to implement a VoltagePair and an Electrode.
+
     Attributes:
         voltage_pairs: Objects that represent each voltage step
         working_ion: Representation of the working ion that only contains element type
@@ -159,7 +161,7 @@ class AbstractElectrode(Sequence, MSONable):
     @property
     def working_ion(self):
         """
-        working ion as pymatgen Element object
+        Working ion as pymatgen Element object
         """
         return self.working_ion_entry.composition.elements[0]
 
