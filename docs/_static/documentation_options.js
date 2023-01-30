0a6633446c7ab505e85acf4e0760eccd721bd94d
Shyue Ping Ong <sp@ong.ai>
2023-01-30 08:23:17 -0800

    Update docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2023.1.20',
+    VERSION: '2023.1.30',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         include=["pymatgen.*", "pymatgen.analysis.*", "pymatgen.io.*", "pymatgen.ext.*", "cmd_line"],
         exclude=["pymatgen.*.tests", "pymatgen.*.*.tests", "pymatgen.*.*.*.tests"],
     ),
-    version="2023.1.20",
+    version="2023.1.30",
     python_requires=">=3.8",
     install_requires=[
         "matplotlib>=1.5",
