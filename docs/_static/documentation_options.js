e0ee486040fb29b20042385f733643b986f05678
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-02-28 13:50:50 -0800

    Prepare release 2023.2.28 (#2867)
    * add PLW0603 to ruff ignore
    
    * invoke release
    
    * update CHANGES.rst
    
    * bump ruff pre-commit hook to v0.0.253

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2023.2.22',
+    VERSION: '2023.2.28',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
