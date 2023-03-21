d1852e9b3bacc4a7d1b32b9ab8faf98069ebe81d
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-03-21 12:02:16 -0700

    tweak oxygen_edge_extender() for readability

--- pymatgen/analysis/local_env.py
+++ pymatgen/analysis/local_env.py
@@ -4378,32 +4378,26 @@ class Critic2NN(NearNeighbors):
         ]
 
 
-def oxygen_edge_extender(mol_graph):
+def oxygen_edge_extender(mol_graph: MoleculeGraph) -> MoleculeGraph:
     """
-    Function to identify and add missed O-C or O-H bonds. This is particularly
-    important when oxygen is forming three bonds, e.g. in H3O+ or XOH2+
+    Identify and add missed O-C or O-H bonds. This is particularly
+    important when oxygen is forming three bonds, e.g. in H3O+ or XOH2+.
+    See https://github.com/materialsproject/pymatgen/pull/2903 for details.
 
     Args:
-        mol_graph: pymatgen.analysis.graphs.MoleculeGraph object
+        mol_graph (MoleculeGraph): molecule graph to extend
 
     Returns:
-        mol_graph: pymatgen.analysis.graphs.MoleculeGraph object with additional
-            O-C or O-H bonds (if any found) added
-
+        MoleculeGraph: object with additional O-C or O-H bonds added (if any found)
     """
     num_new_edges = 0
     for idx in mol_graph.graph.nodes():
         if mol_graph.graph.nodes()[idx]["specie"] == "O":
             neighbors = [site[2] for site in mol_graph.get_connected_sites(idx)]
             for ii, site in enumerate(mol_graph.molecule):
-                if (
-                    ii != idx
-                    and ii not in neighbors
-                    and (
-                        (str(site.specie) == "C" and site.distance(mol_graph.molecule[idx]) < 1.5)
-                        or (str(site.specie) == "H" and site.distance(mol_graph.molecule[idx]) < 1.0)
-                    )
-                ):
+                is_O_C_bond = str(site.specie) == "C" and site.distance(mol_graph.molecule[idx]) < 1.5
+                is_O_H_bond = str(site.specie) == "H" and site.distance(mol_graph.molecule[idx]) < 1
+                if ii != idx and ii not in neighbors and (is_O_C_bond or is_O_H_bond):
                     mol_graph.add_edge(idx, ii)
                     num_new_edges += 1
     return mol_graph
