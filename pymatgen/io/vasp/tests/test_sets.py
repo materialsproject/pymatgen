5992c6c269b745f83a1410406194948c79dcf580
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-05-03 18:02:21 -0700

    fix AttributeError no .exception in pytest.raises() exc_info (closes #2971)

--- pymatgen/io/vasp/tests/test_sets.py
+++ pymatgen/io/vasp/tests/test_sets.py
@@ -500,9 +500,9 @@ class MITMPRelaxSetTest(PymatgenTest):
         struct = self.structure.copy()
         get_valid_magmom_struct(structure=struct, inplace=True, spin_mode="v")
         struct.insert(0, "Li", [0, 0, 0], properties={"magmom": 10.0})
-        with pytest.raises(TypeError) as context:
+        with pytest.raises(TypeError) as exc_info:
             get_valid_magmom_struct(structure=struct, inplace=True, spin_mode="a")
-        assert "Magmom type conflict" in str(context.exception)
+        assert "Magmom type conflict" in str(exc_info.value)
 
         # Test the behavior of MPRelaxSet to atomacically fill in the missing magmom
         struct = self.structure.copy()
@@ -510,10 +510,10 @@ class MITMPRelaxSetTest(PymatgenTest):
         struct.insert(0, "Li", [0, 0, 0])
 
         vis = MPRelaxSet(struct, user_potcar_settings={"Fe": "Fe"}, validate_magmom=False)
-        with pytest.raises(TypeError) as context:
+        with pytest.raises(TypeError) as exc_info:
             print(vis.get_vasp_input())
 
-        assert "argument must be a string" in str(context.exception)
+        assert "argument must be a string" in str(exc_info.value)
         vis = MPRelaxSet(struct, user_potcar_settings={"Fe": "Fe"}, validate_magmom=True)
         assert vis.get_vasp_input()["INCAR"]["MAGMOM"] == [1.0] * len(struct)
 
