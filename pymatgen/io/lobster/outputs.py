bbfbea89f627110657b15522f7dcc1b9405c4509
JaGeo <janine.george@bam.de>
2023-05-16 23:51:57 +0200

    linting

--- pymatgen/io/lobster/outputs.py
+++ pymatgen/io/lobster/outputs.py
@@ -370,7 +370,7 @@ class Icohplist:
             list_icohp.append(icohp)
 
         if self.orbitalwise:
-            list_orb_icohp = [] # type: list[dict]
+            list_orb_icohp: list[dict] = []
             num_orbs = len(data_orbitals) // 2 if self.is_spin_polarized else len(data_orbitals)
 
             for i_data_orb in range(num_orbs):
