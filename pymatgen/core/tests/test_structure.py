8ce2bc867bb1e81eae946d70bc8a7b227e895130
Andrew S. Rosen <asrosen93@gmail.com>
2023-06-12 16:01:16 -0700

    Update test_structure.py

--- pymatgen/core/tests/test_structure.py
+++ pymatgen/core/tests/test_structure.py
@@ -1946,21 +1946,21 @@ class MoleculeTest(PymatgenTest):
         assert traj[0] != traj[-1]
         assert os.path.isfile("testing.traj")
 
-    @skipIf(TBLite is None, "Requires tblite.")
-    def test_calculate_gfnxtb(self):
-        mol = self.mol
-        new_mol = mol.calculate()
-        assert hasattr(new_mol, "calc")
-        assert not hasattr(new_mol, "dynamics")
-        assert new_mol.calc.results["energy"] == pytest.approx(-113.61022434200855)
-        assert isinstance(new_mol, Molecule)
+#     @skipIf(TBLite is None, "Requires tblite.")
+#     def test_calculate_gfnxtb(self):
+#         mol = self.mol
+#         new_mol = mol.calculate()
+#         assert hasattr(new_mol, "calc")
+#         assert not hasattr(new_mol, "dynamics")
+#         assert new_mol.calc.results["energy"] == pytest.approx(-113.61022434200855)
+#         assert isinstance(new_mol, Molecule)
 
-    @skipIf(TBLite is None, "Requires tblite.")
-    def test_relax_gfnxtb(self):
-        mol = self.mol
-        relaxed = mol.relax()
-        assert hasattr(relaxed, "calc")
-        assert hasattr(relaxed, "dynamics")
-        assert relaxed.calc.results.get("energy")
-        assert relaxed.dynamics == {"type": "optimization", "optimizer": "FIRE"}
-        assert relaxed.calc.results["energy"] == pytest.approx(-113.61346199239306)
+#     @skipIf(TBLite is None, "Requires tblite.")
+#     def test_relax_gfnxtb(self):
+#         mol = self.mol
+#         relaxed = mol.relax()
+#         assert hasattr(relaxed, "calc")
+#         assert hasattr(relaxed, "dynamics")
+#         assert relaxed.calc.results.get("energy")
+#         assert relaxed.dynamics == {"type": "optimization", "optimizer": "FIRE"}
+#         assert relaxed.calc.results["energy"] == pytest.approx(-113.61346199239306)
