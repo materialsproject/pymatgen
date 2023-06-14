162bbc2401a1ed05f36fedf4cfb05404f20dde9e
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-06-14 11:32:49 +0200

    refactor

--- pymatgen/io/xtb/tests/test_outputs.py
+++ pymatgen/io/xtb/tests/test_outputs.py
@@ -11,10 +11,8 @@ from pymatgen.util.testing import PymatgenTest
 
 try:
     from openbabel import openbabel as openbabel
-
-    have_babel = True
 except ImportError:
-    have_babel = False
+    openbabel = None
     print("OpenBabel not found, parsed molecules structures will not be  checked")
 
 __author__ = "Alex Epstein"
@@ -42,15 +40,15 @@ class TestCRESTOutput(PymatgenTest):
                 m = Molecule.from_file(os.path.join(expected_output_dir, filepath))
                 expected_sorted_structures[n_conf].insert(n_rot, m)
 
-        cout = CRESTOutput(output_filename="crest_out.out", path=test_dir)
+        crest_out = CRESTOutput(output_filename="crest_out.out", path=test_dir)
         exp_best = Molecule.from_file(os.path.join(expected_output_dir, "expected_crest_best.xyz"))
-        for i, c in enumerate(cout.sorted_structures_energies):
+        for i, c in enumerate(crest_out.sorted_structures_energies):
             for j, r in enumerate(c):
-                if have_babel:
+                if openbabel:
                     assert check_for_structure_changes(r[0], expected_sorted_structures[i][j]) == "no_change"
                 assert float(r[1]) == approx(float(expected_energies[i][j]), abs=1e-4)
 
-        assert cout.properly_terminated is True
-        if have_babel:
-            assert check_for_structure_changes(cout.lowest_energy_structure, exp_best) == "no_change"
-        assert cout.cmd_options == expected_cmd_options
+        assert crest_out.properly_terminated
+        if openbabel:
+            assert check_for_structure_changes(crest_out.lowest_energy_structure, exp_best) == "no_change"
+        assert crest_out.cmd_options == expected_cmd_options
