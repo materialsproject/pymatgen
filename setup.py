f4e605776b7afc6e74bd6e855f94280268aeb698
Shyue Ping Ong <sp@ong.ai>
2023-07-17 16:37:50 -0700

    Update docs

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         include=["pymatgen.*", "pymatgen.analysis.*", "pymatgen.io.*", "pymatgen.ext.*", "cmd_line"],
         exclude=["pymatgen.*.tests", "pymatgen.*.*.tests", "pymatgen.*.*.*.tests"],
     ),
-    version="2023.7.14",
+    version="2023.7.17",
     python_requires=">=3.8",
     install_requires=[
         "matplotlib>=1.5",
