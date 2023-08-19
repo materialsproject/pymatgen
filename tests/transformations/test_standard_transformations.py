e0f40f7ca2fdb2d453e9a91ea88bd81c0a11cdaa
Jingyang Wang <jywang72@stanford.edu>
2023-08-19 00:30:34 -0700

    Adding a test for class RandomStructureTransformation; change int to round in apply_transformation() of class RandomStructureTransformation.

--- tests/transformations/test_standard_transformations.py
+++ tests/transformations/test_standard_transformations.py
@@ -29,6 +29,7 @@ from pymatgen.transformations.standard_transformations import (
     OxidationStateDecorationTransformation,
     OxidationStateRemovalTransformation,
     PartialRemoveSpecieTransformation,
+    RandomStructureTransformation,
     PerturbStructureTransformation,
     PrimitiveCellTransformation,
     RemoveSpeciesTransformation,
@@ -318,6 +319,35 @@ class TestPartialRemoveSpecieTransformation(unittest.TestCase):
         struct = t1.apply_transformation(p.structure)
         trafo = PartialRemoveSpecieTransformation("Li+", 0.5, PartialRemoveSpecieTransformation.ALGO_BEST_FIRST)
         assert len(trafo.apply_transformation(struct)) == 26
+
+
+class TestRandomStructureTransformation(unittest.TestCase):
+    def test_apply_transformation(self):
+        trafo = RandomStructureTransformation()
+        coords = []
+        coords.append([0, 0, 0])
+        coords.append([0.25, 0.25, 0.25])
+        lattice = Lattice(
+            [
+                [3.521253, 0.000000, 2.032996],
+                [1.173751, 3.319869, 2.032996],
+                [0.000000, 0.000000, 4.065993]
+            ]
+        )
+        
+        struct = Structure(
+            lattice,
+            [
+                {"Ga3+": 0.5, "In3+": 0.5},
+                {"As3-": 0.5, "P3-": 0.5}
+            ],
+            coords)
+        
+        struct.make_supercell([3, 3, 3])
+        
+        output = trafo.apply_transformation(struct, n_copies=5)
+        assert len(output) == 5
+        assert isinstance(output[0], Structure)
 
 
 class TestOrderDisorderedStructureTransformation(unittest.TestCase):
