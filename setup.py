057d582953572f59df218a4c34d6970405aa610f
Shyue Ping Ong <sp@ong.ai>
2023-08-30 09:49:43 -0700

    Drop 3.8 support like numpy and pandas.

--- setup.py
+++ setup.py
@@ -161,7 +161,6 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
