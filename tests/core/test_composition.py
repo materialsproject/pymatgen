c87b2993f11547cfacde365d24fe9e6bc1d085dc
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-09-16 16:38:27 -0700

    refactor test_oxi_state_guesses

--- tests/core/test_composition.py
+++ tests/core/test_composition.py
@@ -557,14 +557,12 @@ class TestComposition(PymatgenTest):
         assert Composition("V2O4").oxi_state_guesses(oxi_states_override={"V": [2, 3, 5]}) == ({"V": 4, "O": -2},)
 
         # multiple solutions - Mn/Fe = 2+/4+ or 3+/3+ or 4+/2+
-        assert len(Composition("MnFeO3").oxi_state_guesses(oxi_states_override={"Mn": [2, 3, 4], "Fe": [2, 3, 4]})) == 3
+        MnFeO3 = Composition("MnFeO3")
+        MnFeO3_guesses = MnFeO3.oxi_state_guesses(oxi_states_override={"Mn": [2, 3, 4], "Fe": [2, 3, 4]})
+        assert len(MnFeO3_guesses) == 3
 
         # multiple solutions prefers 3/3 over 2/4 or 4/2
-        assert Composition("MnFeO3").oxi_state_guesses(oxi_states_override={"Mn": [2, 3, 4], "Fe": [2, 3, 4]})[0] == {
-            "Mn": 3,
-            "Fe": 3,
-            "O": -2,
-        }
+        assert MnFeO3_guesses[0] == {"Mn": 3, "Fe": 3, "O": -2}
 
         # https://github.com/materialsproject/pymatgen/issues/3324
         for anion in "O H N F Cl Br I Se Si Sb Te".split():
