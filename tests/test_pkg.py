aa9fbeba385081cc8151a546eabc4367d6f48d1a
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-10-04 19:04:23 -0700

    try fix test_egg_sources_txt_is_complete() on windows

--- tests/test_pkg.py
+++ tests/test_pkg.py
@@ -23,8 +23,8 @@ def test_egg_sources_txt_is_complete():
     # check that all files in pymatgen/ are listed in SOURCES.txt
     for ext in ("py", "json", "json.gz", "yaml", "csv"):
         for filepath in glob(f"pymatgen/**/*.{ext}", recursive=True):
-            # TODO (janosh): unclear why pymatgen/alchemy is missing from SOURCES.txt in CI (Windows only). investigate!
-            if "/tests/" in filepath or filepath.endswith("dao.py") or os.path.join("pymatgen", "alchemy") in filepath:
+            filepath = filepath.replace("\\", "/")
+            if filepath.endswith("dao.py"):
                 continue
             if filepath not in sources:
                 raise ValueError(
