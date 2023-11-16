1e7f93afc987c2798c1241f3fd1a0ac098037a11
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-11-16 12:12:18 -0800

    add units to ElasticTensor methods
    fix typos like lower case voigt

--- pymatgen/core/tensors.py
+++ pymatgen/core/tensors.py
@@ -53,7 +53,7 @@ class Tensor(np.ndarray, MSONable):
             input_array: (array-like with shape 3^N): array-like representing
                 a tensor quantity in standard (i. e. non-voigt) notation
             vscale: (N x M array-like): a matrix corresponding
-                to the coefficients of the voigt-notation tensor
+                to the coefficients of the Voigt-notation tensor
             check_rank: (int): If not None, checks that input_array's rank == check_rank.
                 Defaults to None.
         """
@@ -270,7 +270,7 @@ class Tensor(np.ndarray, MSONable):
 
     @property
     def voigt_symmetrized(self):
-        """Returns a "voigt"-symmetrized tensor, i. e. a voigt-notation
+        """Returns a "voigt"-symmetrized tensor, i. e. a Voigt-notation
         tensor such that it is invariant w.r.t. permutation of indices.
         """
         if not (self.rank % 2 == 0 and self.rank >= 2):
@@ -635,7 +635,7 @@ class Tensor(np.ndarray, MSONable):
 
         Args:
             voigt (bool): flag for whether to store entries in
-                voigt-notation. Defaults to false, as information
+                Voigt notation. Defaults to false, as information
                 may be lost in conversion.
 
         Returns (dict):
@@ -668,11 +668,11 @@ class TensorCollection(collections.abc.Sequence, MSONable):
     or for having a tensor expansion.
     """
 
-    def __init__(self, tensor_list, base_class=Tensor):
+    def __init__(self, tensor_list: Sequence, base_class=Tensor) -> None:
         """:param tensor_list: List of tensors.
         :param base_class: Class to be used.
         """
-        self.tensors = [base_class(t) if not isinstance(t, base_class) else t for t in tensor_list]
+        self.tensors = [tensor if isinstance(tensor, base_class) else base_class(tensor) for tensor in tensor_list]
 
     def __len__(self):
         return len(self.tensors)
@@ -848,7 +848,7 @@ class SquareTensor(Tensor):
             input_array (3x3 array-like): the 3x3 array-like
                 representing the content of the tensor
             vscale (6x1 array-like): 6x1 array-like scaling the
-                voigt-notation vector with the tensor entries
+                Voigt-notation vector with the tensor entries
         """
         obj = super().__new__(cls, input_array, vscale, check_rank=2)
         return obj.view(cls)
