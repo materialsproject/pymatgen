566d8b7271be26660e409c479b6e8a1fe735788c
Andrew S. Rosen <asrosen93@gmail.com>
2023-11-26 01:52:19 -0500

    Update outputs.py

--- pymatgen/io/qchem/outputs.py
+++ pymatgen/io/qchem/outputs.py
@@ -2812,13 +2812,13 @@ def gradient_parser(filename: str = "131.0") -> NDArray:
     ]
     return np.array(grad)
 
-def hessian_parser(filename: str = "132.0", n_atoms: int) -> NDArray:
+def hessian_parser(filename: str = "132.0", n_atoms: int | None = None) -> NDArray:
     """
     Parse the Hessian data from a Hessian scratch file.
 
     Args:
         filename: Path to the Hessian scratch file (typically "132.0")
-        n_atoms: Number of atoms in the molecule
+        n_atoms: Number of atoms in the molecule. If None, no reshaping will be done.
 
     Returns:
         NDArray: Hessian, formatted as 3n_atoms x 3n_atoms. Units are Hartree/Bohr^2/amu.
@@ -2826,11 +2826,14 @@ def hessian_parser(filename: str = "132.0", n_atoms: int) -> NDArray:
     tmp_hess_data = []
     with zopen(filename, mode="rb") as file:
         binary = file.read()
-    tmp_hess_data.extend(struct.unpack("d", binary[ii * 8 : (ii + 1) * 8])[0] for ii in range(len(binary) // 8))
-    return np.reshape(
-        np.array(tmp_hess_data),
-        (n_atoms * 3, n_atoms * 3),
-    )
+    hessian = np.array(tmp_hess_data.extend(struct.unpack("d", binary[ii * 8 : (ii + 1) * 8])[0] for ii in range(len(binary) // 8)))
+    if n_atoms:
+        return np.reshape(
+            hessian,
+            (n_atoms * 3, n_atoms * 3),
+        )
+    else:
+        return hessian
 
 def orbital_coeffs_parser(filename: str = "53.0") -> NDArray:
     """
