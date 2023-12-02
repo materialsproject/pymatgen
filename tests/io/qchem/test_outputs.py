cd39425157570b03b27b84b38278f84dbec50655
Andrew S. Rosen <asrosen93@gmail.com>
2023-12-02 12:38:34 -0800

    Don't chdir

--- tests/io/qchem/test_outputs.py
+++ tests/io/qchem/test_outputs.py
@@ -502,19 +502,18 @@ class TestQCOutput(PymatgenTest):
 
 
 def test_gradient(tmp_path):
-    os.chdir(tmp_path)
+    tmp_path
     with gzip.open(f"{TEST_FILES_DIR}/qchem/131.0.gz", "rb") as f_in, open(tmp_path / "131.0", "wb") as f_out:
         shutil.copyfileobj(f_in, f_out)
-    gradient = gradient_parser("131.0")
+    gradient = gradient_parser(tmp_path / "131.0")
     assert np.shape(gradient) == (14, 3)
     assert gradient.all()
 
 
 def test_hessian(tmp_path):
-    os.chdir(tmp_path)
     with gzip.open(f"{TEST_FILES_DIR}/qchem/132.0.gz", "rb") as f_in, open(tmp_path / "132.0", "wb") as f_out:
         shutil.copyfileobj(f_in, f_out)
-    hessian = hessian_parser("132.0", natoms=14)
+    hessian = hessian_parser(tmp_path / "132.0", natoms=14)
     assert np.shape(hessian) == (42, 42)
     assert hessian.all()
 
@@ -524,10 +523,9 @@ def test_hessian(tmp_path):
 
 
 def test_prev_orbital_coeffs(tmp_path):
-    os.chdir(tmp_path)
     with gzip.open(f"{TEST_FILES_DIR}/qchem/53.0.gz", "rb") as f_in, open(tmp_path / "53.0", "wb") as f_out:
         shutil.copyfileobj(f_in, f_out)
-    orbital_coeffs = orbital_coeffs_parser("53.0")
+    orbital_coeffs = orbital_coeffs_parser(tmp_path / "53.0")
     assert len(orbital_coeffs) == 360400
     assert orbital_coeffs.all()
 
