5dfb8a915b12ca7e8c460192ed19f61be88215bb
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-12-22 17:04:27 -0800

    make all_structures not class attr

--- pymatgen/core/structure.py
+++ pymatgen/core/structure.py
@@ -396,19 +396,19 @@ class SiteCollection(collections.abc.Sequence, metaclass=ABCMeta):
         return all(site.is_ordered for site in self)
 
     @property
-    def sublattices(self) -> dict[Composition, list]:
+    def sub_lattices(self) -> dict[Composition, list]:
         """
         Returns dict of lists of atom indices belonging to every unique
         sublattice.
         """
         unique_species_and_occu = set(self.species_and_occu)
 
-        sublattices_dict = dict()
+        sub_lattices = dict()
 
-        for usp in unique_species_and_occu:
-            sublattices_dict[usp] = [i for i, sp in enumerate(self.species_and_occu) if sp == usp]
+        for uniq_sp in unique_species_and_occu:
+            sub_lattices[uniq_sp] = [idx for idx, sp in enumerate(self.species_and_occu) if sp == uniq_sp]
 
-        return sublattices_dict
+        return sub_lattices
 
     def get_angle(self, i: int, j: int, k: int) -> float:
         """Returns angle specified by three sites.
