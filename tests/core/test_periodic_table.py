c6167cc03cf43435bb50e1cb5cf1b15dbd582c23
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-01-13 17:15:58 +0100

    tighten atomic mass test on H isotopes

--- tests/core/test_periodic_table.py
+++ tests/core/test_periodic_table.py
@@ -18,7 +18,7 @@ class ElementTestCase(PymatgenTest):
     def test_init(self):
         assert Element("Fe").symbol == "Fe"
 
-        fictional_symbols = ["Dolphin", "Tyrannosaurus", "Zebra"]
+        fictional_symbols = ("Dolphin", "Tyrannosaurus", "Zebra")
 
         for sym in fictional_symbols:
             with pytest.raises(ValueError, match=f"{sym!r} is not a valid Element"):
@@ -359,11 +359,12 @@ class ElementTestCase(PymatgenTest):
         assert Element("Bi").is_post_transition_metal, True
 
     def test_isotope(self):
-        elements = [Element(x) for x in ("H", "D", "T")]
-        assert all(x.symbol == "H" for x in elements)
-        assert all(str(x) == "H" for x in elements)
-        assert [x.A for x in elements] == [1, 2, 3]
-        assert all(abs(x.atomic_mass - i - 1) < 0.1 for i, x in enumerate(elements))
+        elems = [Element(el) for el in ("H", "D", "T")]
+        assert [x.symbol for x in elems] == ["H", "H", "H"]
+        assert list(map(str, elems)) == ["H", "H", "H"]
+        assert [el.A for el in elems] == [1, 2, 3]
+        assert all(abs(el.atomic_mass - idx - 1) < 0.1 for idx, el in enumerate(elems))
+        assert [el.atomic_mass for el in elems] == [1.00794, 2.013553212712, 3.0155007134]
 
 
 class SpeciesTestCase(PymatgenTest):
