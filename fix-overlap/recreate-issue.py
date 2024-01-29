7b0941b3cca85266096df48dbcb4bc31222a121f
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-01-29 11:53:45 +0800

    clean up test code

--- fix-overlap/recreate-issue.py
+++ fix-overlap/recreate-issue.py
@@ -1,11 +1,12 @@
 # https://github.com/materialsproject/pymatgen/issues/2591
 
+from pymatgen.core.structure import Structure
 from pymatgen.core.surface import SlabGenerator
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
-from pymatgen.core.structure import Structure
 
 struct = Structure.from_file('CONTCAR')
 struct = SpacegroupAnalyzer(struct).get_conventional_standard_structure()
+
 slab = SlabGenerator(
     struct,
     miller_index=[0, 1, 0],
@@ -15,4 +16,4 @@ slab = SlabGenerator(
 
 for n, s in enumerate(slab.get_slabs(bonds={('Al', 'O'): 2.1}, repair=True)):
     s = s.get_sorted_structure()
-    s.to(filename='POSCAR-'+str(n), fmt='poscar')
+    s.to(filename=f'POSCAR-{str(n)}', fmt='poscar')
