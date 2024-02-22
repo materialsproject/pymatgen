a276c09e1c717c61a50b25cf010a5ef44c55319e
Matteo Giantomassi <gmatteo@gmail.com>
2024-02-22 10:02:10 +0100

    Fix test_to_from_abivars

--- tests/core/test_structure.py
+++ tests/core/test_structure.py
@@ -1307,9 +1307,15 @@ class TestStructure(PymatgenTest):
 
     def test_to_from_abivars(self):
         """Test as_dict, from_dict with fmt == abivars."""
-        dct = self.struct.as_dict(fmt="abivars")
+        # Properties are not supported if fmt="abivars"
+        # as abivars is not a serialization protocol.
+        # but a format that allows one to get a dict with the abinit variables defining the structure.
+        my_struct = self.struct.copy()
+        my_struct.properties = {}
+        dct = my_struct.as_dict(fmt="abivars")
+        assert "properties" not in dct
         s2 = Structure.from_dict(dct, fmt="abivars")
-        assert s2 == self.struct
+        assert s2 == my_struct
         assert isinstance(s2, Structure)
 
     def test_to_from_file_str(self):
