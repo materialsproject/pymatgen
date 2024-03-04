b96d9e147e21489b377166f8839de8fab47e3e69
Andrew S. Rosen <asrosen93@gmail.com>
2024-03-04 13:31:42 -0800

    Ensure `MSONAtoms` is indeed `MSONable` when `Atoms.info` is loaded with goodies (#3670)
    * Ensure `MSONAtoms` is indeed `MSONable`
    
    * pre-commit auto-fixes
    
    * Update ase.py
    
    Signed-off-by: Andrew S. Rosen <asrosen93@gmail.com>
    
    * Update test_ase.py
    
    Signed-off-by: Andrew S. Rosen <asrosen93@gmail.com>
    
    * pre-commit auto-fixes
    
    * Update ase.py
    
    Signed-off-by: Andrew S. Rosen <asrosen93@gmail.com>
    
    * Update test_ase.py
    
    Signed-off-by: Andrew S. Rosen <asrosen93@gmail.com>
    
    * fix
    
    * pre-commit auto-fixes
    
    * Update ase.py
    
    Signed-off-by: Andrew S. Rosen <asrosen93@gmail.com>
    
    ---------
    
    Signed-off-by: Andrew S. Rosen <asrosen93@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/io/test_ase.py
+++ tests/io/test_ase.py
@@ -316,15 +316,30 @@ def test_back_forth_v4():
 
 @skip_if_no_ase
 def test_msonable_atoms():
+    structure = Structure.from_file(f"{TEST_FILES_DIR}/POSCAR")
+
     atoms = ase.io.read(f"{TEST_FILES_DIR}/OUTCAR")
+    atoms_info = {"test": "hi", "structure": structure}
+    atoms.info = atoms_info
     assert not isinstance(atoms, MSONAtoms)
-    ref = {"@module": "pymatgen.io.ase", "@class": "MSONAtoms", "atoms_json": ase.io.jsonio.encode(atoms)}
+
+    ref_atoms = atoms.copy()
+    ref_atoms.info = {}
+
     msonable_atoms = MSONAtoms(atoms)
     assert atoms == msonable_atoms
-    assert msonable_atoms.as_dict() == ref
-    assert MSONAtoms.from_dict(ref) == atoms
 
-    structure = Structure.from_file(f"{TEST_FILES_DIR}/POSCAR")
+    msonable_atoms_dict = msonable_atoms.as_dict()
+    assert msonable_atoms_dict == {
+        "@module": "pymatgen.io.ase",
+        "@class": "MSONAtoms",
+        "atoms_json": ase.io.jsonio.encode(ref_atoms),
+        "atoms_info": jsanitize(atoms_info, strict=True),
+    }
+
+    atoms_back = MSONAtoms.from_dict(msonable_atoms_dict)
+    assert atoms_back == atoms
+    assert atoms_back.info == atoms.info
 
     atoms = AseAtomsAdaptor.get_atoms(structure, msonable=True)
     assert callable(atoms.as_dict)
