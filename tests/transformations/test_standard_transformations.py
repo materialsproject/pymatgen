ad354fca11ef699cab2af2ca13a6f23849067d50
Haoyu Yang <yanghaoyu97@outlook.com>
2024-03-08 12:18:47 +0800

    reverse POSCAR_LiO2 to POSCAR.LiO2

--- tests/transformations/test_standard_transformations.py
+++ tests/transformations/test_standard_transformations.py
@@ -189,7 +189,7 @@ class TestOxidationStateDecorationTransformation(unittest.TestCase):
 class TestAutoOxiStateDecorationTransformation(unittest.TestCase):
     def test_apply_transformation(self):
         trafo = AutoOxiStateDecorationTransformation()
-        struct = trafo.apply_transformation(Structure.from_file(f"{VASP_IN_DIR}/POSCAR_LiFePO4"))
+        struct = trafo.apply_transformation(Structure.from_file(f"{VASP_IN_DIR}/POSCAR.LiFePO4"))
         expected_oxi = {"Li": 1, "P": 5, "O": -2, "Fe": 2}
         for site in struct:
             assert site.specie.oxi_state == expected_oxi[site.specie.symbol]
@@ -248,13 +248,13 @@ class TestPartialRemoveSpecieTransformation(unittest.TestCase):
 
     def test_apply_transformations_complete_ranking(self):
         t1 = OxidationStateDecorationTransformation({"Li": 1, "Fe": 2, "P": 5, "O": -2})
-        struct = t1.apply_transformation(Structure.from_file(f"{VASP_IN_DIR}/POSCAR_LiFePO4"))
+        struct = t1.apply_transformation(Structure.from_file(f"{VASP_IN_DIR}/POSCAR.LiFePO4"))
         trafo = PartialRemoveSpecieTransformation("Li+", 0.5, PartialRemoveSpecieTransformation.ALGO_COMPLETE)
         assert len(trafo.apply_transformation(struct, 10)) == 6
 
     def test_apply_transformations_best_first(self):
         t1 = OxidationStateDecorationTransformation({"Li": 1, "Fe": 2, "P": 5, "O": -2})
-        struct = t1.apply_transformation(Structure.from_file(f"{VASP_IN_DIR}/POSCAR_LiFePO4"))
+        struct = t1.apply_transformation(Structure.from_file(f"{VASP_IN_DIR}/POSCAR.LiFePO4"))
         trafo = PartialRemoveSpecieTransformation("Li+", 0.5, PartialRemoveSpecieTransformation.ALGO_BEST_FIRST)
         assert len(trafo.apply_transformation(struct)) == 26
 
