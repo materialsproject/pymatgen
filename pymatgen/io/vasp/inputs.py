1bf43c5e4f09ce9fa9f2d262352d13f93f641a40
Haoyu Yang <yanghaoyu97@outlook.com>
2024-03-08 12:34:11 +0800

    remove accidental DEBUG info

--- pymatgen/io/vasp/inputs.py
+++ pymatgen/io/vasp/inputs.py
@@ -2610,11 +2610,6 @@ class VaspInput(dict, MSONable):
 
     def as_dict(self):
         """MSONable dict."""
-        for key, val in self.items():
-            if val is None:
-                raise ValueError(key)
-            val.as_dict()
-
         dct = {key: val.as_dict() for key, val in self.items()}
 
         dct["@module"] = type(self).__module__
