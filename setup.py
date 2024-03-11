998bda17bb614e02b431b037abb8c58564e988f2
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-03-11 16:41:34 +0100

    down pin chgnet>=0.3.0

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         "tblite": ["tblite[ase]>=0.3.0"],
         "vis": ["vtk>=6.0.0"],
         "abinit": ["netcdf4"],
-        "relaxation": ["matgl", "chgnet"],
+        "relaxation": ["matgl", "chgnet>=0.3.0"],
         "electronic_structure": ["fdint>=2.0.2"],
         "dev": [
             "mypy",
@@ -73,7 +73,7 @@ setup(
             # caused CI failure due to ModuleNotFoundError: No module named 'packaging'
             # "BoltzTraP2>=22.3.2; platform_system!='Windows'",
             "chemview>=0.6",
-            "chgnet",
+            "chgnet>=0.3.0",
             "f90nml>=1.1.2",
             "galore>=0.6.1",
             "h5py>=3.8.0",
