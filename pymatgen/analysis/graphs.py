f5d7be771f873c282c739bacfd838bac9523c71a
Haoyu Yang <yanghaoyu97@outlook.com>
2024-03-19 10:12:59 +0800

    fix typo

--- pymatgen/analysis/graphs.py
+++ pymatgen/analysis/graphs.py
@@ -383,7 +383,7 @@ class StructureGraph(MSONable):
 
         # sanitize types
         # TODO(@DanielYang59) fix mypy error
-        from_jimage,  = tuple(map(int, from_jimage)), tuple(map(int, to_jimage))
+        from_jimage, to_jimage = tuple(map(int, from_jimage)), tuple(map(int, to_jimage))
         from_index, to_index = int(from_index), int(to_index)
 
         # if edge is from site i to site i, constrain direction of edge
