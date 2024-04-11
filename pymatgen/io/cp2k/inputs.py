cb28af61e75102cd14670b5f2d58237bc03400d8
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-04-11 05:53:54 +0000

    fix cp2k input

--- pymatgen/io/cp2k/inputs.py
+++ pymatgen/io/cp2k/inputs.py
@@ -2749,14 +2749,13 @@ class DataFile(MSONable):
     objects: Sequence | None = None
 
     @classmethod
-    def from_file(cls, filename) -> None:
-        """Load from a file."""
-        raise NotImplementedError
-        # with open(filename, encoding="utf-8") as file:
-        #     data = cls.from_str(file.read())
-        #     for obj in data.objects:
-        #         obj.filename = filename
-        #     return data
+    def from_file(cls, filename) -> Self:
+        """Load from a file, reserved for child classes."""
+        with open(filename, encoding="utf-8") as file:
+            data = cls.from_str(file.read())  # type: ignore[call-arg]
+            for obj in data.objects:  # type: ignore[attr-defined]
+                obj.filename = filename
+            return data  # type: ignore[return-value]
 
     @classmethod
     def from_str(cls) -> None:
