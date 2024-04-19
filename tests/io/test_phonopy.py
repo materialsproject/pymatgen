c381d962d7f85b760f1d1058745264c3478000db
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-04-19 10:58:50 +0800

    relocate `gruneisen`

--- tests/io/test_phonopy.py
+++ tests/io/test_phonopy.py
@@ -209,13 +209,13 @@ class TestPhonopyFromForceConstants(TestCase):
 class TestGruneisen:
     def test_ph_bs_symm_line(self):
         self.bs_symm_line_1 = get_gruneisen_ph_bs_symm_line(
-            gruneisen_path=f"{TEST_FILES_DIR}/gruneisen/gruneisen_band_Si.yaml",
-            structure_path=f"{TEST_FILES_DIR}/gruneisen/eq/POSCAR_Si",
+            gruneisen_path=f"{TEST_DIR}/gruneisen/gruneisen_band_Si.yaml",
+            structure_path=f"{TEST_DIR}/gruneisen/eq/POSCAR_Si",
             fit=True,
         )
         self.bs_symm_line_2 = get_gruneisen_ph_bs_symm_line(
-            gruneisen_path=f"{TEST_FILES_DIR}/gruneisen/gruneisen_band_Si.yaml",
-            structure_path=f"{TEST_FILES_DIR}/gruneisen/eq/POSCAR_Si",
+            gruneisen_path=f"{TEST_DIR}/gruneisen/gruneisen_band_Si.yaml",
+            structure_path=f"{TEST_DIR}/gruneisen/eq/POSCAR_Si",
             fit=False,
         )
 
@@ -223,12 +223,12 @@ class TestGruneisen:
 
         assert self.bs_symm_line_1.gruneisen[0][0] != self.bs_symm_line_2.gruneisen[0][0]
         with pytest.raises(ValueError, match="Please provide a structure or structure path"):
-            get_gruneisen_ph_bs_symm_line(gruneisen_path=f"{TEST_FILES_DIR}/gruneisen/gruneisen_eq_plus_minus_InP.yaml")
+            get_gruneisen_ph_bs_symm_line(gruneisen_path=f"{TEST_DIR}/gruneisen/gruneisen_eq_plus_minus_InP.yaml")
 
     def test_gruneisen_parameter(self):
         self.gruneisenobject_Si = get_gruneisenparameter(
-            f"{TEST_FILES_DIR}/gruneisen/gruneisen_mesh_Si.yaml",
-            structure_path=f"{TEST_FILES_DIR}/gruneisen/eq/POSCAR_Si",
+            f"{TEST_DIR}/gruneisen/gruneisen_mesh_Si.yaml",
+            structure_path=f"{TEST_DIR}/gruneisen/eq/POSCAR_Si",
         )
 
         assert self.gruneisenobject_Si.frequencies[0][0] == approx(0.2523831291)
@@ -236,7 +236,7 @@ class TestGruneisen:
 
         # catch the exception when no structure is present
         with pytest.raises(ValueError, match="Please provide a structure or structure path"):
-            get_gruneisenparameter(f"{TEST_FILES_DIR}/gruneisen/gruneisen_mesh_InP_without_struct.yaml")
+            get_gruneisenparameter(f"{TEST_DIR}/gruneisen/gruneisen_mesh_InP_without_struct.yaml")
 
 
 @pytest.mark.skipif(Phonopy is None, reason="Phonopy not present")
