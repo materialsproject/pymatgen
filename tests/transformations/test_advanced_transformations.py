5b1d7df09093721e83b7606a1ef991aff651aac2
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-04-19 10:50:18 +0800

    relocate `struct_predictor`

--- tests/transformations/test_advanced_transformations.py
+++ tests/transformations/test_advanced_transformations.py
@@ -59,7 +59,7 @@ def get_table():
     initialization time, and make unit tests insensitive to changes in the
     default lambda table.
     """
-    json_path = f"{TEST_FILES_DIR}/struct_predictor/test_lambda.json"
+    json_path = f"{TEST_FILES_DIR}/analysis/struct_predictor/test_lambda.json"
     with open(json_path) as file:
         return json.load(file)
 
