0f05ed2a4b42e6a9ae669f7634fe73187ff3f22f
matthewkuner <matthewkuner@gmail.com>
2024-04-23 10:59:43 -0700

    try removing test file

--- tests/io/vasp/test_outputs.py
+++ tests/io/vasp/test_outputs.py
@@ -78,7 +78,7 @@ class TestVasprun(PymatgenTest):
         # Test for case where EDIFFG is set to 0.
         # This should pass if all ionic steps complete and
         # are electronically converged.
-        print([cur_path for cur_path in os.walk(VASP_OUT_DIR)])
+        print(list(os.walk(VASP_OUT_DIR)))
         vasp_run = Vasprun(f"{VASP_OUT_DIR}/vasprun.ediffg_set_to_0.xml.gz")
         assert len(vasp_run.ionic_steps) == 3
         assert vasp_run.final_energy == approx(-34.60164204)
