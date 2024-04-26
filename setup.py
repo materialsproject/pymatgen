6568e05e139f2f5493df7cbc2ab1afd34b9adb1e
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-04-26 10:52:12 +0800

    Revert "enable `boltztrap2`"
    This reverts commit 3b1b0771cae6dd0bdb0d497d86660e8fa7a336d9.

--- setup.py
+++ setup.py
@@ -69,7 +69,10 @@ setup(
         ],
         "optional": [
             "ase>=3.22.1",
-            "BoltzTraP2>=24.1.1; platform_system!='Windows'",
+            # TODO restore BoltzTraP2 when install fixed, hopefully following merge of
+            # https://gitlab.com/sousaw/BoltzTraP2/-/merge_requests/18
+            # caused CI failure due to ModuleNotFoundError: No module named 'packaging'
+            # "BoltzTraP2>=22.3.2; platform_system!='Windows'",
             "chemview>=0.6",
             "chgnet",
             "f90nml>=1.1.2",
