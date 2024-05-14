005f722c248931711515ed20ae285253d6f7b69f
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-05-14 11:05:40 +0800

    format more docs to Google format

--- pymatgen/io/aims/sets/core.py
+++ pymatgen/io/aims/sets/core.py
@@ -18,10 +18,8 @@ if TYPE_CHECKING:
 class StaticSetGenerator(AimsInputGenerator):
     """Common class for ground-state generators.
 
-    Parameters
-    ----------
-    calc_type: str
-        The type of calculation
+    Attributes:
+        calc_type (str): The type of calculation
     """
 
     calc_type: str = "static"
@@ -29,12 +27,9 @@ class StaticSetGenerator(AimsInputGenerator):
     def get_parameter_updates(self, structure: Structure | Molecule, prev_parameters: dict[str, Any]) -> dict[str, Any]:
         """Get the parameter updates for the calculation.
 
-        Parameters
-        ----------
-        structure: Structure or Molecule
-            The structure to calculate the bands for
-        prev_parameters: Dict[str, Any]
-            The previous parameters
+        Args:
+            structure (Structure or Molecule): The structure to calculate the bands for
+            prev_parameters (Dict[str, Any]): The previous parameters
 
         Returns:
             dict: The updated for the parameters for the output section of FHI-aims
@@ -46,16 +41,11 @@ class StaticSetGenerator(AimsInputGenerator):
 class RelaxSetGenerator(AimsInputGenerator):
     """Generate FHI-aims relax sets for optimizing internal coordinates and lattice params.
 
-    Parameters
-    ----------
-    calc_type: str
-        The type of calculation
-    relax_cell: bool
-        If True then relax the unit cell from the structure
-    max_force: float
-        Maximum allowed force in the calculation
-    method: str
-        Method used for the geometry optimization
+    Attributes:
+        calc_type (str): The type of calculation
+        relax_cell (bool): If True then relax the unit cell from the structure
+        max_force (float): Maximum allowed force in the calculation
+        method (str): Method used for the geometry optimization
     """
 
     calc_type: str = "relaxation"
@@ -66,12 +56,9 @@ class RelaxSetGenerator(AimsInputGenerator):
     def get_parameter_updates(self, structure: Structure | Molecule, prev_parameters: dict[str, Any]) -> dict:
         """Get the parameter updates for the calculation.
 
-        Parameters
-        ----------
-        structure: Structure or Molecule
-            The structure to calculate the bands for
-        prev_parameters: Dict[str, Any]
-            The previous parameters
+        Args:
+            structure (Structure or Molecule): The structure to calculate the bands for
+        prev_parameters (Dict[str, Any]): The previous parameters
 
         Returns:
             dict: The updated for the parameters for the output section of FHI-aims
@@ -89,14 +76,10 @@ class RelaxSetGenerator(AimsInputGenerator):
 class SocketIOSetGenerator(AimsInputGenerator):
     """Generate FHI-aims input sets for running with the socket.
 
-    Parameters
-    ----------
-    calc_type: str
-        The type of calculation
-    host: str
-        The hostname for the server the socket is on
-    port: int
-        The port the socket server is listening on
+    Attributes:
+        calc_type (str): The type of calculation
+        host (str): The hostname for the server the socket is on
+        port (int): The port the socket server is listening on
     """
 
     calc_type: str = "multi_scf"
@@ -106,12 +89,9 @@ class SocketIOSetGenerator(AimsInputGenerator):
     def get_parameter_updates(self, structure: Structure | Molecule, prev_parameters: dict[str, Any]) -> dict:
         """Get the parameter updates for the calculation.
 
-        Parameters
-        ----------
-        structure: Structure or Molecule
-            The structure to calculate the bands for
-        prev_parameters: Dict[str, Any]
-            The previous parameters
+        Args:
+            structure (Structure or Molecule): The structure to calculate the bands for
+            prev_parameters (Dict[str, Any]): The previous parameters
 
         Returns:
             dict: The updated for the parameters for the output section of FHI-aims
