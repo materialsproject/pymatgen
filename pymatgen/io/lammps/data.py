e03e1f8a30df027e65bea0e528f744899558507a
Alexander Bonkowski <alexander.bonkowski@rwth-aachen.de>
2024-05-28 14:05:38 +0200

    Replace deprecated 'delim_whitespace=True` with `sep="\s+"`

--- pymatgen/io/lammps/data.py
+++ pymatgen/io/lammps/data.py
@@ -667,14 +667,14 @@ class LammpsData(MSONable):
             sio = StringIO("".join(sec_lines[2:]))  # skip the 2nd line
             if kw.endswith("Coeffs") and not kw.startswith("PairIJ"):
                 df_list = [
-                    pd.read_csv(StringIO(line), header=None, comment="#", delim_whitespace=True)
+                    pd.read_csv(StringIO(line), header=None, comment="#", sep="\s+")
                     for line in sec_lines[2:]
                     if line.strip()
                 ]
                 df = pd.concat(df_list, ignore_index=True)
                 names = ["id"] + [f"coeff{i}" for i in range(1, df.shape[1])]
             else:
-                df = pd.read_csv(sio, header=None, comment="#", delim_whitespace=True)
+                df = pd.read_csv(sio, header=None, comment="#", sep="\s+")
                 if kw == "PairIJ Coeffs":
                     names = ["id1", "id2"] + [f"coeff{i}" for i in range(1, df.shape[1] - 1)]
                     df.index.name = None
@@ -1386,7 +1386,7 @@ class CombinedData(LammpsData):
             sio,
             header=None,
             comment="#",
-            delim_whitespace=True,
+            sep="\s+",
             names=["atom", "x", "y", "z"],
         )
         df.index += 1
