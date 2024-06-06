6b9589ad681c1e53ab62cf5763cb090f94133541
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-06-06 18:32:32 +0800

    fix replacement

--- pymatgen/core/structure.py
+++ pymatgen/core/structure.py
@@ -284,10 +284,10 @@ class SiteCollection(collections.abc.Sequence, ABC):
         return [site.species for site in self]
 
     @property
-    @deprecated(message="Use n_type_sp instead")
+    @deprecated(message="Use n_elems instead")
     def ntypesp(self) -> int:
         """Number of types of atoms."""
-        return len(self.types_of_species)
+        return self.n_elems
 
     @property
     def n_elems(self) -> int:
