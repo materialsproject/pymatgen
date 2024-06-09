0631f254b8c1139e2c75662b33b200aae0f3fd6e
Matthew Horton <matthew.horton@microsoft.com>
2024-06-08 23:57:24 -0700

    Update to subclass `InputFile`, has type errors

--- pymatgen/io/castep/inputs.py
+++ pymatgen/io/castep/inputs.py
@@ -1,23 +1,28 @@
-import collections
+from __future__ import annotations
+
 import re
 from dataclasses import dataclass, field
 from difflib import get_close_matches
-from enum import Enum
-from typing import Dict, Any, Union, List, Literal
+from enum import EnumMeta
+from typing import TYPE_CHECKING, Any, NamedTuple
 from warnings import warn
 
 import numpy as np
 from monty.io import zopen
-from monty.json import MSONable
 
 from pymatgen.core.lattice import Lattice
 from pymatgen.core.structure import Structure
-from pymatgen.io.castep.constants import CellKeyword, ParamKeyword, OTFPseudopotentialLibrary
+from pymatgen.io.castep.constants import CellKeyword, OTFPseudopotentialLibrary, ParamKeyword
+from pymatgen.io.core import InputFile
+
+if TYPE_CHECKING:
+    from enum import Enum
+    from os import PathLike
 
 """
 This module provides an interface between pymatgen and the CASTEP (http://www.castep.org)
-electronic structure code for the purposes of generating input files or parsing output files, pymatgen 
-does not offer any capability to run CASTEP directly. To run or use CASTEP, an appropriate license has 
+electronic structure code for the purposes of generating input files or parsing output files, pymatgen
+does not offer any capability to run CASTEP directly. To run or use CASTEP, an appropriate license has
 to be obtained from the CASTEP developers.
 
 "First principles methods using CASTEP", Zeitschrift fuer Kristallographie 220(5-6) pp. 567-570 (2005)
@@ -33,31 +38,37 @@ _ry_to_ev = 13.605693009
 # From 2002 CODATA values
 to_angstrom = {"ang": 1, "bohr": 0.5291772108, "m": 1e10, "cm": 1e8, "nm": 10}
 
-Tag = collections.namedtuple("Tag", "value comment")
-Block = collections.namedtuple("Block", "values comments")
+
+class Tag(NamedTuple):
+    value: Any
+    comment: str | None
 
 
-def _get_enum(key: str, enum: Enum):
+class Block(NamedTuple):
+    values: list[Any]
+    comments: list[str] | None
+
+
+def _get_enum(key: str | Enum, enum: EnumMeta) -> EnumMeta:
     """
     Utility function to create a specified enum from a string.
     """
+    if isinstance(key, EnumMeta):
+        return key
     if isinstance(key, str):
         try:
-            key = enum[key.upper()]
-            return key
+            return enum[key.upper()]
         except KeyError:
             error_message = f"{key} is not a valid keyword."
             possible_matches = get_close_matches(key.upper(), enum.__members__.keys(), n=3)
             if possible_matches:
                 error_message += f" Perhaps you mean: {', '.join(possible_matches)}?"
             raise KeyError(error_message)
-    elif not isinstance(key, enum):
-        raise ValueError(f"{key} is provided incorrectly, it is not an {enum} or a string and cannot be parsed.")
-    return key
+    raise ValueError(f"{key} is provided incorrectly, it is not an {enum} or a string and cannot be parsed.")
 
 
 @dataclass
-class Cell(MSONable):
+class Cell(InputFile):
     """
     An interface for CASTEP's .cell file which defines atomic
     positions, k-point grids etc. The CASTEP documentation is
@@ -75,10 +86,10 @@ class Cell(MSONable):
     'values' (a list of lists) and 'comments' (a commensurate list of str).
     """
 
-    blocks: Dict[CellKeyword, Block] = field(default_factory=dict)
-    tags: Dict[CellKeyword, Tag] = field(default_factory=dict)
+    blocks: dict[CellKeyword, Block] = field(default_factory=dict)
+    tags: dict[CellKeyword, Tag] = field(default_factory=dict)
 
-    def set_block(self, name: Union[str, CellKeyword], block: Union[List[List[str]], Block]):
+    def set_block(self, name: str | CellKeyword, block: list[list[str]] | Block):
         """
         Set a block in the Cell.
 
@@ -95,7 +106,7 @@ class Cell(MSONable):
             block = Block(values=block, comments=None)
         self.blocks[name] = block
 
-    def set_tag(self, name: Union[str, CellKeyword], tag: Union[str, Tag]):
+    def set_tag(self, name: str | CellKeyword, tag: str | Tag):
         """
         Set a tag in the Cell.
 
@@ -112,7 +123,7 @@ class Cell(MSONable):
             tag = Tag(value=tag, comment=None)
         self.tags[name] = tag
 
-    def set_pseudopotential(self, library: Union[str, OTFPseudopotentialLibrary]):
+    def set_pseudopotential(self, library: str | OTFPseudopotentialLibrary):
         """
         Convenience method to add one of the default pseudopotential libraries for all elements.
 
@@ -124,7 +135,7 @@ class Cell(MSONable):
             library = OTFPseudopotentialLibrary[library]
         self.set_block(CellKeyword.SPECIES_POT, [[library.name]])
 
-    def __str__(self):
+    def get_str(self):
         """
         Returns: The string representation Cell, i.e. the contents of the .cell file
         """
@@ -133,29 +144,24 @@ class Cell(MSONable):
 
         for tag, content in self.tags.items():
             if content.comment in (None, ""):
-                lines.append("{0: <24}: {1}".format(tag.name.lower(), " ".join(content.value)))
+                lines.append("{: <24}: {}".format(tag.name.lower(), " ".join(content.value)))
             else:
-                lines.append(
-                    "{0: <24}: {1: <16} ! {2}".format(tag.name.lower(), " ".join(content.value), content.comment)
-                )
+                lines.append("{: <24}: {: <16} ! {}".format(tag.name.lower(), " ".join(content.value), content.comment))
         for block, content in self.blocks.items():
-            lines.append("\n%block {}".format(block.name.lower()))
-            if content.comments is None:
-                comments = [""] * len(content.values)
-            else:
-                comments = content.comments
+            lines.append(f"\n%block {block.name.lower()}")
+            comments = [""] * len(content.values) if content.comments is None else content.comments
 
             for row, comment in zip(content.values, comments):
                 line = " ".join(map(str, row))
                 if comment != "":
-                    line = "{0: <30} ! {1}".format(line, comment)
+                    line = f"{line: <30} ! {comment}"
                 lines.append(line)
 
-            lines.append("%endblock {}".format(block.name.lower()))
+            lines.append(f"%endblock {block.name.lower()}")
 
         return "\n".join(lines)
 
-    def write_file(self, filename):
+    def write_file(self, filename: str | PathLike) -> None:
         """
         Write the .cell file
 
@@ -179,10 +185,7 @@ class Cell(MSONable):
         comment_split_line = re.split("[#;!]", line)
 
         line_content = comment_split_line[0]
-        if len(comment_split_line) == 1:
-            comment = ""
-        else:
-            comment = line[len(line_content) :]
+        comment = "" if len(comment_split_line) == 1 else line[len(line_content) :]
 
         if in_block:
             tag, data = None, line.split()
@@ -200,29 +203,23 @@ class Cell(MSONable):
         return tag, data, comment
 
     @classmethod
-    def from_file(cls, filename):
+    def from_str(cls, contents: str) -> Cell:
         """
-        Load a .cell file.
+        Load from the string representation of a .cell file.
 
         Args:
-            filename: filename of .cell file
+            contents: .cell file string
 
         Returns: a Cell
         """
 
-        with zopen(filename, "rt") as f:
-            lines = [line.strip() for line in f]
+        lines = [line.strip() for line in contents.splitlines()]
 
         # Remove lines which are entirely commented or empty
         def _is_not_empty_line(line):
-            if len(line) == 0:
+            if len(line) == 0 or line[0] in "#;!":
                 return False
-            elif line[0] in "#;!":
-                return False
-            elif len(line) > 6 and line[:7] == "COMMENT":
-                return False
-            else:
-                return True
+            return not (len(line) > 6 and line[:7] == "COMMENT")
 
         lines = list(filter(_is_not_empty_line, lines))
 
@@ -234,20 +231,19 @@ class Cell(MSONable):
         for line in lines:
             if len(line.split()) == 0:
                 continue
-            elif line[:6].lower() == "%block":
+            if line[:6].lower() == "%block":
                 if in_block:
-                    raise IOError("Cell file contains nested blocks. This possibility was not anticipated.")
-                else:
-                    current_block_label = CellKeyword[line.split()[1].upper()]
-                    in_block = True
-                    continue
-            elif line[:9].lower() == "%endblock":
+                    raise OSError("Cell file contains nested blocks. This possibility was not anticipated.")
+                current_block_label = CellKeyword[line.split()[1].upper()]
+                in_block = True
+                continue
+            if line[:9].lower() == "%endblock":
                 if not in_block:
-                    raise IOError("Cannot cope with line {}: not currently in a block.".format(line))
+                    raise OSError(f"Cannot cope with line {line}: not currently in a block.")
                 if CellKeyword[line.split()[1].upper()] != current_block_label:
-                    raise IOError(
-                        "Endblock {} does not match current block "
-                        "{}: cannot interpret cell file.".format(line.split()[1], current_block_label)
+                    raise OSError(
+                        f"Endblock {line.split()[1]} does not match current block "
+                        f"{current_block_label}: cannot interpret cell file."
                     )
                 blocks[current_block_label] = Block(current_block_values, current_block_comments)
                 current_block_values, current_block_comments = [], []
@@ -300,7 +296,9 @@ class Cell(MSONable):
         self._structure = self.structure
 
     @classmethod
-    def from_structure(cls, structure, blocks: Dict[CellKeyword, Block] = None, tags: Dict[CellKeyword, Tag] = None):
+    def from_structure(
+        cls, structure, blocks: dict[CellKeyword, Block] | None = None, tags: dict[CellKeyword, Tag] | None = None
+    ):
         """
         Initialize a .cell file with a provided input structure.
 
@@ -326,12 +324,12 @@ class Cell(MSONable):
         Returns: the Structure object defined by this .cell file
         """
 
-        if hasattr(self, "_structure") and getattr(self, "_structure"):
+        if hasattr(self, "_structure") and self._structure:
             return self._structure
 
         # Get lattice vectors
         if CellKeyword.LATTICE_CART in self.blocks:
-            lattice_cart = self.blocks[CellKeyword.LATTICE_CART].values
+            lattice_cart = self.blocks[CellKeyword.LATTICE_CART].values  # noqa: PD011
             if lattice_cart[0][0].lower() in ("ang", "nm", "cm", "m", "bohr", "a0"):
                 unit = lattice_cart[0][0].lower()
                 vectors = lattice_cart[1:]
@@ -345,7 +343,7 @@ class Cell(MSONable):
             vectors *= to_angstrom[unit]
             lattice = Lattice(vectors)
         elif CellKeyword.LATTICE_ABC in self.blocks:
-            lattice_abc = self.blocks[CellKeyword.LATTICE_ABC].values
+            lattice_abc = self.blocks[CellKeyword.LATTICE_ABC].values  # noqa: PD011
             if lattice_abc[0][0].lower() in ("ang", "nm", "cm", "m", "bohr", "a0"):
                 unit = lattice_abc[0][0].lower()
                 lengths_and_angles = lattice_abc[1:]
@@ -372,7 +370,7 @@ class Cell(MSONable):
             elements, coords = zip(*elements_coords)
             structure = Structure(lattice, elements, coords, coords_are_cartesian=False)
         elif CellKeyword.POSITIONS_ABS in self.blocks:
-            positions_abs = self.blocks[CellKeyword.POSITIONS_ABS].values
+            positions_abs = self.blocks[CellKeyword.POSITIONS_ABS].values  # noqa: PD011
             if positions_abs[0][0].lower() in ("ang", "nm", "cm", "m", "bohr", "a0"):
                 unit = positions_abs[0][0].lower()
                 positions_abs = positions_abs[1:]
@@ -392,9 +390,7 @@ class Cell(MSONable):
             raise ValueError("Couldn't find any atomic positions in cell file!")
 
         if magmoms:
-            structure.add_site_property(
-                "magmom", [magmoms[idx] if idx in magmoms else None for idx in range(len(structure))]
-            )
+            structure.add_site_property("magmom", [magmoms.get(idx, None) for idx in range(len(structure))])
 
         # store to avoid re-build if this property is called again
         self._structure = structure
@@ -403,7 +399,7 @@ class Cell(MSONable):
 
 
 @dataclass
-class Param(MSONable):
+class Param(InputFile):
     """
     An interface for CASTEP's .param file which defines
     calculation parameters. The CASTEP documentation is
@@ -417,9 +413,9 @@ class Param(MSONable):
     Tag NamedTuple with the attributes 'value' and 'comment'.
     """
 
-    tags: Dict[ParamKeyword, Tag] = field(default_factory=dict)
+    tags: dict[ParamKeyword, Tag] = field(default_factory=dict)
 
-    def set_tag(self, name: Union[str, ParamKeyword], tag: Union[str, Tag]):
+    def set_tag(self, name: str | ParamKeyword, tag: str | Tag):
         """
         Set a tag in the Param.
 
@@ -437,7 +433,7 @@ class Param(MSONable):
         self.tags[name] = tag
 
     @classmethod
-    def from_tags(cls, tags: Dict[Union[str, ParamKeyword], Any]):
+    def from_tags(cls, tags: dict[str | ParamKeyword, Any]):
         """
         Convenience method to create a Param file using a dictionary
         of tags and their associated values.
@@ -453,3 +449,38 @@ class Param(MSONable):
         for name, value in tags.items():
             param.set_tag(name, value)
         return param
+
+    def get_str(self) -> str:
+        """
+        Returns: The string representation Param, i.e. the contents of the .param file
+        """
+
+        lines = []
+        for keyword, tag in self.tags.items():
+            if tag.comment:
+                lines.append(f"{keyword:20} = {tag.value} ! {tag.comment}")
+            else:
+                lines.append(f"{keyword:20} = {tag.value}")
+
+        return "\n".join(lines)
+
+    @classmethod
+    def from_str(cls, contents: str) -> Param:
+        tags = {}
+        lines = contents.splitlines()
+
+        for line in lines:
+            line = line.strip()
+            if not line.startswith("!"):  # comment line
+                keyword, line = line.split("=")
+                value = line.split("!")
+                if len(value) == 2:
+                    value, comment = value
+                    comment = comment.strip()
+                else:
+                    value = value[0]
+                    comment = None
+                keyword = ParamKeyword(keyword.strip().upper())
+                tags[keyword] = Tag(value=value.strip(), comment=comment)
+
+        return cls(tags)
