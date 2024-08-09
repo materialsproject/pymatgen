c8230184f659738d9f1f5d842f17b2cef6a30341
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-08-09 14:23:31 +0800

    make installation script modular

--- .github/workflows/install-opt-dependency.sh
+++ .github/workflows/install-opt-dependency.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-# Install optional Ubuntu dependencies for test workflow
+# Install optional Ubuntu dependencies for pymatgen test workflow
 
 set -e
 
-# Install BoltzTraP
-wget -O BoltzTraP.tar.bz2 https://owncloud.tuwien.ac.at/index.php/s/s2d55LYlZnioa3s/download
-tar -jxvf BoltzTraP.tar.bz2
-ln -s boltztrap-1.2.5/src/x_trans /usr/local/bin/
-ln -s boltztrap-1.2.5/src/BoltzTraP /usr/local/bin/
+install_boltztrap() {
+    # Install BoltzTraP
+    wget -O BoltzTraP.tar.bz2 https://owncloud.tuwien.ac.at/index.php/s/s2d55LYlZnioa3s/download
+    tar -jxvf BoltzTraP.tar.bz2
+
+    ln -s boltztrap-1.2.5/src/x_trans /usr/local/bin/
+    ln -s boltztrap-1.2.5/src/BoltzTraP /usr/local/bin/
+
+    echo "BoltzTraP installation completed."
+}
+
+# Main script
+install_boltztrap
