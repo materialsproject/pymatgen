eb57d138b7ed5c327cc86be2357977a96d60ad00
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-08-09 23:11:13 +0800

    try to install Vampire

--- .github/workflows/install-opt-dependency.sh
+++ .github/workflows/install-opt-dependency.sh
@@ -13,12 +13,25 @@ install_boltztrap() {
     echo "BoltzTraP installation completed."
 }
 
+install_vampire() {
+    # Install Vampire 5.0
+    wget https://vampire.york.ac.uk/resources/release-5/vampire-5.0-linux.tar.gz
+    tar -zxvf vampire-5.0-linux.tar.gz
+    mv linux vampire-5.0
+
+    echo "vampire-5.0" >> $GITHUB_PATH
+
+    echo "Vampire installation completed."
+}
+
 install_babel() {
     # Install openbabel
+    # TODO: not working
     sudo apt install openbabel
 }
 
 
 # Main script
 install_boltztrap
+install_vampire
 install_babel
