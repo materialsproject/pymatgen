e94df2f9196c8796085e04b8335eb52961498002
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-08-09 14:15:58 +0800

    fix decompress command

--- .github/workflows/install-opt-dependency.sh
+++ .github/workflows/install-opt-dependency.sh
@@ -5,6 +5,6 @@ set -e
 
 # Install BoltzTraP
 wget -O BoltzTraP.tar.bz2 https://owncloud.tuwien.ac.at/index.php/s/s2d55LYlZnioa3s/download
-tar -zxvf BoltzTraP.tar.bz2
+tar -jxvf BoltzTraP.tar.bz2
 ln -s boltztrap-1.2.5/src/x_trans /usr/local/bin/
 ln -s boltztrap-1.2.5/src/BoltzTraP /usr/local/bin/
