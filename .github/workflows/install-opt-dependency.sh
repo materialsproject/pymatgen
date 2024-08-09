9e9bf63c5cb91bb665eb1b3adbd59070c761c018
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-08-09 14:11:13 +0800

    try to install BoltzTraP

---
+++ .github/workflows/install-opt-dependency.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Install optional Ubuntu dependencies for test workflow
+
+set -e
+
+# Install BoltzTraP
+wget -O BoltzTraP.tar.bz2 https://owncloud.tuwien.ac.at/index.php/s/s2d55LYlZnioa3s/download
+tar -zxvf BoltzTraP.tar.bz2
+ln -s boltztrap-1.2.5/src/x_trans /usr/local/bin/
+ln -s boltztrap-1.2.5/src/BoltzTraP /usr/local/bin/
