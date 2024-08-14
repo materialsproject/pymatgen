a788b1b249b1af08d080f90a9a46f29b6f5d36ef
Haoyu Yang <yanghaoyu97@outlook.com>
2024-08-14 16:43:02 +0800

    try to use absolute path in PATH

--- .github/workflows/install-opt-dependency.sh
+++ .github/workflows/install-opt-dependency.sh
@@ -8,19 +8,19 @@ install_boltztrap() {
     wget -O BoltzTraP.tar.bz2 https://owncloud.tuwien.ac.at/index.php/s/s2d55LYlZnioa3s/download
     tar -jxvf BoltzTraP.tar.bz2
 
-    echo "boltztrap-1.2.5/src" >> $GITHUB_PATH
+    echo "$(realpath boltztrap-1.2.5/src/)" >> $GITHUB_PATH
 
     echo "BoltzTraP installation completed."
 }
 
 install_vampire() {
     # Install Vampire 5.0
+    # TODO: not working, got `FileNotFoundError: [Errno 2] No such file or directory: 'vampire-serial`
     wget https://vampire.york.ac.uk/resources/release-5/vampire-5.0-linux.tar.gz
     tar -zxvf vampire-5.0-linux.tar.gz
     mv linux vampire-5.0
-    chmod +x vampire-5.0/vampire-serial
 
-    echo "vampire-5.0" >> $GITHUB_PATH
+    echo "$(realpath vampire-5.0/)" >> $GITHUB_PATH
 
     echo "Vampire installation completed."
 }
