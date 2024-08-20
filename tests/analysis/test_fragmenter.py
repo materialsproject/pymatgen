f3dc772c25cc06b26b0702af37f1d278da8b6169
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-08-20 13:18:56 +0800

    skip failing openbabel tests for Windows

--- tests/analysis/test_fragmenter.py
+++ tests/analysis/test_fragmenter.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import os
+
 import pytest
 
 from pymatgen.analysis.fragmenter import Fragmenter
@@ -67,6 +69,7 @@ class TestFragmentMolecule(PymatgenTest):
         fragmenter = Fragmenter(molecule=self.pc_frag1, depth=0)
         assert fragmenter.total_unique_fragments == 12
 
+    @pytest.mark.skipif(os.name == "nt", reason="Tests for openbabel failing on Win")
     def test_babel_pc_old_defaults(self):
         pytest.importorskip("openbabel")
         fragmenter = Fragmenter(molecule=self.pc, open_rings=True)
@@ -109,6 +112,7 @@ class TestFragmentMolecule(PymatgenTest):
         fragmenter = Fragmenter(molecule=self.tfsi, depth=0)
         assert fragmenter.total_unique_fragments == 156
 
+    @pytest.mark.skipif(os.name == "nt", reason="Tests for openbabel failing on Win")
     def test_babel_pc_with_ro_depth_0_vs_depth_10(self):
         pytest.importorskip("openbabel")
         fragmenter0 = Fragmenter(molecule=self.pc, depth=0, open_rings=True, opt_steps=1000)
