fddb185b0d7f3e1e4d951494ee0d017dce2287f7
Jason Xie <jasonxie@sz.tsinghua.edu.cn>
2024-09-05 09:25:28 +0800

    Update src/pymatgen/core/surface.py
    Co-authored-by: Janosh Riebesell <janosh.riebesell@gmail.com>
    Signed-off-by: Jason Xie <jasonxie@sz.tsinghua.edu.cn>

--- src/pymatgen/core/surface.py
+++ src/pymatgen/core/surface.py
@@ -1194,7 +1194,7 @@ class SlabGenerator:
         symmetrize: bool = False,
         repair: bool = False,
         ztol: float = 0,
-        filting_out_sym_slabs=True,
+        filter_out_sym_slabs: bool = True,
     ) -> list[Slab]:
         """Generate slabs with shift values calculated from the internal
         gen_possible_terminations func. If the user decide to avoid breaking
