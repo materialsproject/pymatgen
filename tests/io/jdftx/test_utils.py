e6a201e0bdc8829c75f4877c35deb8698d965714
Ben Rich <tiggzzss@gmail.com>
2024-09-26 16:37:11 -0600

    get_Start_lines error handling test

--- tests/io/jdftx/test_utils.py
+++ tests/io/jdftx/test_utils.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import pytest
 
 from pymatgen.io.jdftx.generic_tags import flatten_list
+from pymatgen.io.jdftx.jdftxoutfileslice_helpers import get_start_lines
 
 
 def test_flatten_list():
@@ -12,3 +13,10 @@ def test_flatten_list():
     assert flatten_list("", [1, [2, [3, [4]]]]) == [1, 2, 3, 4]
     with pytest.raises(TypeError):
         flatten_list("", 1)
+
+
+def test_get_start_lines():
+    with pytest.raises(ValueError, match="Outfile parser fed an empty file."):
+        get_start_lines([])
+    with pytest.raises(ValueError, match="No JDFTx calculations found in file."):
+        get_start_lines(["\n", "\n"])
