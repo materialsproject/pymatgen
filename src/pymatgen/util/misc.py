843bb2d18da4876def9b48193f30fdb5617f7ff1
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-10-13 13:35:01 +0800

    remove experimental implementation for now

--- src/pymatgen/util/misc.py
+++ src/pymatgen/util/misc.py
@@ -19,25 +19,3 @@ def is_np_dict_equal(dict1, dict2, /) -> bool:
         return False
 
     return all(np.array_equal(dict1[key], dict2[key]) for key in dict1)
-
-
-def is_np_dict_equal_try_except(dict1, dict2, /) -> bool:
-    """Another implementation with try-except.
-
-    TODO: need speed test.
-    """
-    if dict1.keys() != dict2.keys():
-        return False
-
-    for key in dict1:
-        value1 = dict1[key]
-        value2 = dict2[key]
-
-        try:
-            if value1 != value2:
-                return False
-        except ValueError:
-            if not np.array_equal(value1, value2):
-                return False
-
-    return True
