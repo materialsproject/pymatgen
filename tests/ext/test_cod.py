6d8499a20099509034ec29acc7a65d06d2083d74
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-10-13 17:47:14 +0800

    use module level skip mark

--- tests/ext/test_cod.py
+++ tests/ext/test_cod.py
@@ -17,10 +17,9 @@ if "CI" in os.environ:  # test is slow and flaky, skip in CI. see
 try:
     WEBSITE_DOWN = requests.get("https://www.crystallography.net", timeout=60).status_code != 200
 except (requests.exceptions.ConnectionError, urllib3.exceptions.ConnectTimeoutError):
-    WEBSITE_DOWN = True
+    pytest.skip(reason="www.crystallography.net is down", allow_module_level=True)
 
 
-@pytest.mark.skipif(WEBSITE_DOWN, reason="www.crystallography.net is down")
 class TestCOD(TestCase):
     @pytest.mark.skipif(not which("mysql"), reason="No mysql")
     def test_get_cod_ids(self):
