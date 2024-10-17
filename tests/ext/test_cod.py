d32f06c325b2f441731e94be4ac20977b358a8f1
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-10-17 19:20:46 +0800

    capture timeout errors in ci

--- tests/ext/test_cod.py
+++ tests/ext/test_cod.py
@@ -1,36 +1,55 @@
 from __future__ import annotations
 
-from unittest import TestCase
+import os
 
 import pytest
 import requests
-import urllib3
 
 from pymatgen.ext.cod import COD
 
 TIMEOUT = 10  # use a tighter timeout in CI
-# TODO: suppress timeout error in CI
 
 try:
     WEBSITE_DOWN = requests.get("https://www.crystallography.net", timeout=TIMEOUT).status_code != 200
-except (requests.exceptions.ConnectionError, urllib3.exceptions.ConnectTimeoutError):
+except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, requests.exceptions.ReadTimeout):
     WEBSITE_DOWN = True
 
 if WEBSITE_DOWN:
     pytest.skip(reason="www.crystallography.net is down", allow_module_level=True)
 
 
-class TestCOD(TestCase):
+class TestCOD:
     def test_get_cod_ids(self):
-        ids = COD(timeout=TIMEOUT).get_cod_ids("Li2O")
+        try:
+            ids = COD(timeout=TIMEOUT).get_cod_ids("Li2O")
+        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, requests.exceptions.ReadTimeout):
+            if "CI" in os.environ:
+                pytest.skip("request timeout")
+            else:
+                raise
+
         assert len(ids) > 15
         assert set(ids).issuperset({1010064, 1011372})
 
     def test_get_structure_by_formula(self):
-        data = COD(timeout=TIMEOUT).get_structure_by_formula("Li2O")
+        try:
+            data = COD(timeout=TIMEOUT).get_structure_by_formula("Li2O")
+        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, requests.exceptions.ReadTimeout):
+            if "CI" in os.environ:
+                pytest.skip("request timeout")
+            else:
+                raise
+
         assert len(data) > 15
         assert data[0]["structure"].reduced_formula == "Li2O"
 
     def test_get_structure_by_id(self):
-        struct = COD(timeout=TIMEOUT).get_structure_by_id(2_002_926)
+        try:
+            struct = COD(timeout=TIMEOUT).get_structure_by_id(2_002_926)
+        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, requests.exceptions.ReadTimeout):
+            if "CI" in os.environ:
+                pytest.skip("request timeout")
+            else:
+                raise
+
         assert struct.formula == "Be8 H64 N16 F32"
