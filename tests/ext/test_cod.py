0a8699de6e7a7951f64d4760dd73156ede03f782
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-10-17 19:01:17 +0800

    use tighter timeout in test

--- tests/ext/test_cod.py
+++ tests/ext/test_cod.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import os
 from unittest import TestCase
 
 import pytest
@@ -9,12 +8,11 @@ import urllib3
 
 from pymatgen.ext.cod import COD
 
-if "CI" in os.environ:  # test is slow and flaky, skip in CI. see
-    # https://github.com/materialsproject/pymatgen/pull/3777#issuecomment-2071217785
-    pytest.skip(allow_module_level=True, reason="Skip COD test in CI")
+TIMEOUT = 10  # use a tighter timeout in CI
+# TODO: suppress timeout error in CI
 
 try:
-    WEBSITE_DOWN = requests.get("https://www.crystallography.net", timeout=10).status_code != 200
+    WEBSITE_DOWN = requests.get("https://www.crystallography.net", timeout=TIMEOUT).status_code != 200
 except (requests.exceptions.ConnectionError, urllib3.exceptions.ConnectTimeoutError):
     WEBSITE_DOWN = True
 
@@ -24,14 +22,15 @@ if WEBSITE_DOWN:
 
 class TestCOD(TestCase):
     def test_get_cod_ids(self):
-        ids = COD().get_cod_ids("Li2O")
+        ids = COD(timeout=TIMEOUT).get_cod_ids("Li2O")
         assert len(ids) > 15
+        assert set(ids).issuperset({1010064, 1011372})
 
     def test_get_structure_by_formula(self):
-        data = COD().get_structure_by_formula("Li2O")
+        data = COD(timeout=TIMEOUT).get_structure_by_formula("Li2O")
         assert len(data) > 15
         assert data[0]["structure"].reduced_formula == "Li2O"
 
     def test_get_structure_by_id(self):
-        struct = COD().get_structure_by_id(2_002_926)
+        struct = COD(timeout=TIMEOUT).get_structure_by_id(2_002_926)
         assert struct.formula == "Be8 H64 N16 F32"
