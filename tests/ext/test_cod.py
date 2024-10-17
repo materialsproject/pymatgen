4177e1cfe6c21223be5c2744e1c0653fc69021a5
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-10-17 19:28:52 +0800

    make the timeout skip a wrapper

--- tests/ext/test_cod.py
+++ tests/ext/test_cod.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import os
+from functools import wraps
 
 import pytest
 import requests
@@ -8,6 +9,7 @@ import requests
 from pymatgen.ext.cod import COD
 
 TIMEOUT = 10  # use a tighter timeout in CI
+
 
 try:
     WEBSITE_DOWN = requests.get("https://www.crystallography.net", timeout=TIMEOUT).status_code != 200
@@ -18,38 +20,34 @@ if WEBSITE_DOWN:
     pytest.skip(reason="www.crystallography.net is down", allow_module_level=True)
 
 
-class TestCOD:
-    def test_get_cod_ids(self):
+def skip_on_timeout(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
         try:
-            ids = COD(timeout=TIMEOUT).get_cod_ids("Li2O")
+            return func(*args, **kwargs)
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, requests.exceptions.ReadTimeout):
-            if "CI" in os.environ:
-                pytest.skip("request timeout")
+            if os.getenv("CI"):
+                pytest.skip("Request timeout in CI environment")
             else:
                 raise
 
+    return wrapper
+
+
+class TestCOD:
+    @skip_on_timeout
+    def test_get_cod_ids(self):
+        ids = COD(timeout=TIMEOUT).get_cod_ids("Li2O")
         assert len(ids) > 15
         assert set(ids).issuperset({1010064, 1011372})
 
+    @skip_on_timeout
     def test_get_structure_by_formula(self):
-        try:
-            data = COD(timeout=TIMEOUT).get_structure_by_formula("Li2O")
-        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, requests.exceptions.ReadTimeout):
-            if "CI" in os.environ:
-                pytest.skip("request timeout")
-            else:
-                raise
-
+        data = COD(timeout=TIMEOUT).get_structure_by_formula("Li2O")
         assert len(data) > 15
         assert data[0]["structure"].reduced_formula == "Li2O"
 
+    @skip_on_timeout
     def test_get_structure_by_id(self):
-        try:
-            struct = COD(timeout=TIMEOUT).get_structure_by_id(2_002_926)
-        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, requests.exceptions.ReadTimeout):
-            if "CI" in os.environ:
-                pytest.skip("request timeout")
-            else:
-                raise
-
+        struct = COD(timeout=TIMEOUT).get_structure_by_id(2_002_926)
         assert struct.formula == "Be8 H64 N16 F32"
