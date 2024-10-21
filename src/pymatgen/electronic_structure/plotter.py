a28e1dae228d0b8926918a691520c52894684ed2
Haoyu (Daniel) YANG <yanghaoyu97@outlook.com>
2024-10-22 04:52:55 +0800

    Fix `savefig` in `pmg.cli.plot` (#4109)
    * fix plot save fig and enhance type
    
    * tweak decorator usage
    
    * plt -> fig to avoid shadowing matplotlib.pyplot as plt
    
    * fix type error for electronic_structure plotter
    
    * pin 3.12 in ci
    
    * fix sources path for src layout
    
    * split tests for cli
    
    * Revert "split tests for cli"
    
    This reverts commit f159d5ee9cf7521b63311356a5661b9b95d62479.
    
    * Revert "fix sources path for src layout"
    
    This reverts commit 3dfe21c020465468bd89b2f43698d8681c8fda81.
    
    * recover no type check decorator, intend for another PR
    
    * Revert "recover no type check decorator, intend for another PR"
    
    This reverts commit f4dabf93acd9efee1a1162896a52a3d7ad23fae6.
    
    * Reapply "split tests for cli"
    
    This reverts commit b01deabd1c839f8060aef58bc19a3abc130afd76.
    
    * Reapply "fix sources path for src layout"
    
    This reverts commit a7b7e259ce10bf0c181ed4109d3bcec92420910b.
    
    * pin mypy for now
    
    * improve comment
    
    * add unit test for plot
    
    * also check incorrect usage

--- src/pymatgen/electronic_structure/plotter.py
+++ src/pymatgen/electronic_structure/plotter.py
@@ -6,7 +6,6 @@ import copy
 import itertools
 import logging
 import math
-import typing
 import warnings
 from collections import Counter
 from typing import TYPE_CHECKING, cast
@@ -20,6 +19,7 @@ from matplotlib.collections import LineCollection
 from matplotlib.gridspec import GridSpec
 from monty.dev import requires
 from monty.json import jsanitize
+from numpy.typing import ArrayLike
 
 from pymatgen.core import Element
 from pymatgen.electronic_structure.bandstructure import BandStructureSymmLine
@@ -35,8 +35,6 @@ except ImportError:
 if TYPE_CHECKING:
     from collections.abc import Sequence
     from typing import Literal
-
-    from numpy.typing import ArrayLike
 
     from pymatgen.electronic_structure.dos import CompleteDos, Dos
 
@@ -130,7 +128,6 @@ class DosPlotter:
         """
         return jsanitize(self._doses)
 
-    @typing.no_type_check
     def get_plot(
         self,
         xlim: tuple[float, float] | None = None,
@@ -163,8 +160,8 @@ class DosPlotter:
         # Note that this complicated processing of energies is to allow for
         # stacked plots in matplotlib.
         for dos in self._doses.values():
-            energies = dos["energies"]
-            densities = dos["densities"]
+            energies = cast(ArrayLike, dos["energies"])
+            densities = cast(ArrayLike, dos["densities"])
             if not ys:
                 ys = {
                     Spin.up: np.zeros(energies.shape),
@@ -211,10 +208,14 @@ class DosPlotter:
             ax.set_ylim(ylim)
         elif not invert_axes:
             xlim = ax.get_xlim()
+            if xlim is None:
+                raise RuntimeError("xlim cannot be None.")
             relevant_y = [p[1] for p in all_pts if xlim[0] < p[0] < xlim[1]]
             ax.set_ylim((min(relevant_y), max(relevant_y)))
         if not xlim and invert_axes:
             ylim = ax.get_ylim()
+            if ylim is None:
+                raise RuntimeError("ylim cannot be None.")
             relevant_y = [p[0] for p in all_pts if ylim[0] < p[1] < ylim[1]]
             ax.set_xlim((min(relevant_y), max(relevant_y)))
 
@@ -3900,7 +3901,7 @@ class CohpPlotter:
         plt.show()
 
 
-@requires(mlab is not None, "MayAvi mlab not imported! Please install mayavi.")
+@requires(mlab is not None, "MayAvi mlab not installed! Please install mayavi.")
 def plot_fermi_surface(
     data,
     structure,
