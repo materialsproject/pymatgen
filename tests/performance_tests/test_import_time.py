3dd2cdfd74cd0a9a0444ce6dd8d7f990dbf147a6
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-10-23 17:45:35 +0800

    add place holder

---
+++ tests/performance_tests/test_import_time.py
@@ -0,0 +1,69 @@
+"""
+Test the import time of several important modules.
+"""
+
+from __future__ import annotations
+
+
+class TestCore:
+    def test_bonds(self):
+        pass
+
+    def test_composition(self):
+        pass
+
+    def test_interface(self):
+        pass
+
+    def test_ion(self):
+        pass
+
+    def test_lattice(self):
+        pass
+
+    def test_libxcfunc(self):
+        pass
+
+    def test_molecular_orbitals(self):
+        pass
+
+    def test_operations(self):
+        pass
+
+    def test_peridict_table(self):
+        pass
+
+    def test_settings(self):
+        pass
+
+    def test_sites(self):
+        pass
+
+    def test_spectrum(self):
+        pass
+
+    def test_structure(self):
+        pass
+
+    def test_surface(self):
+        pass
+
+    def test_tensors(self):
+        pass
+
+    def test_trajectory(self):
+        pass
+
+    def test_units(self):
+        pass
+
+    def test_xcfunc(self):
+        pass
+
+
+class TestIoVasp:
+    def test_inputs(self):
+        pass
+
+    def test_outputs(self):
+        pass
