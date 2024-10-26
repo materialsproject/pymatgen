313d5530578f2ca2c8bd051d35f6deeaa5fc78c3
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-10-26 12:59:36 +0800

    tweak placeholder

---
+++ tests/performance/test_performance.py
@@ -0,0 +1,14 @@
+"""
+Test the import time of several important modules.
+
+TODO:
+    Test the runtime of several important modules.
+"""
+
+from __future__ import annotations
+
+
+class TestImportTime:
+    """
+    Test import time of important modules.
+    """
