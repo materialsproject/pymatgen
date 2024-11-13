materialsproject/pymatgen

commit c2c5ea6282110130bf9f497e43dad9ea55d387a8

SpaceGroup changes (#3859)
* Replaced SpaceGroup symbol attribute with its Hermann-Mauguin symbol, corrected Schoenflies point group attribute, changed handling of rhombohedral space group type settings by adding a SpaceGroup hexagonal bool attribute, modified and added tests.

* Removed crystal class key from symm_ops.json.

* Test for correct setting of hexagonal attribute when instantiating from int number.

* Noted change and replacement option for SpaceGroup symbol in compatibility.md

* Added from_space_group class method to PointGroup, added tests

* Added mapping to standard setting in PointGroup.from_space_group(), modified symm_ops.json and symm_data.json (documented in dev_scripts/update_space_group_data.py) to have same notation, added point group and short Hermann Mauguin symbol to symm_ops, fixed some typos, fixed rhombohedral space group type orbit issue.

* Updated core/test_surface.py to assign lattice as in SpaceGroup is_compatible().

* Modified databases and SpaceGroup init to ensure compatibility with non-underscore space group notations.

* Added tests for issue #3862, modified full_symbol and point_group attribute setting.

* Modified PointGroup.from_space_group() to also handle symbols with identity blickrichtungen and missed underscores, added warning to SpaceGroup init if full symbol is not available (for non-standard settings), added tests.

* Added test for warning if SpaceGroup.full_symbol is not available.

* Removed warning test.

* tweak incompat warning

* add test_full_symbol_warning

* add author + date to dev_scripts/update_spacegroup_data.py

* typos

* warning occurs only once, move test_full_symbol_warning up as workaround to annoying test pollution from side effects to std lib warnings registry

* Updated compatibility.md to also handle old symbol replacement of P2_12_12_1 and I2_12_12_1.

* pre-commit auto-fixes

* Updated dev script path to new src layout.

---------

dev_scripts/update_spacegroup_data.py
@@ -1,0 +1,95 @@
+"""Script to update symm_ops.json and symm_data.yaml in symmetry module due to issues #3845 and #3862.
+symm_ops.json:
+- adds Hermann_mauguin point group key and short Hermann Mauguin space group symbol
+- converts screw axis notation to symm_data standard
+symm_data.json
+- removes mapping of rhombohedral space group types onto symbol + appended H
+- replaces I/P2_12_121 key with I/P2_12_12_1
+"""
+
+from __future__ import annotations
+
+import sys
+
+from monty.serialization import dumpfn, loadfn
+from pymatgen.symmetry.groups import PointGroup
+
+__author__ = "Katharina Ueltzen @kaueltzen"
+__date__ = "2024-06-06"
+
+SYMM_OPS = loadfn("../src/pymatgen/symmetry/symm_ops.json")
+SYMM_DATA = loadfn("../src/pymatgen/symmetry/symm_data.json")
+
+
+def convert_symmops_to_sg_encoding(symbol: str) -> str:
+    """
+    Utility function to convert SYMMOPS space group type symbol notation
+    into SYMM_DATA["space_group_encoding"] key notation with underscores before
+    translational part of screw axes.
+    Args:
+        symbol (str): "hermann_mauguin" or "universal_h_m" key of symmops.json
+    Returns:
+        symbol in the format of SYMM_DATA["space_group_encoding"] keys
+    """
+    symbol_representation = symbol.split(":")
+    representation = ":" + "".join(symbol_representation[1].split(" ")) if len(symbol_representation) > 1 else ""
+
+    blickrichtungen = symbol_representation[0].split(" ")
+    blickrichtungen_new = []
+    for br in blickrichtungen:
+        if len(br) > 1 and br[0].isdigit() and br[1].isdigit():
+            blickrichtungen_new.append(br[0] + "_" + br[1:])
+        else:
+            blickrichtungen_new.append(br)
+    return "".join(blickrichtungen_new) + representation
+
+
+def remove_identity_from_full_hermann_mauguin(symbol: str) -> str:
+    """
+    Utility function to remove identity along blickrichtung (except in P1).
+    Args:
+        symbol (str): "hermann_mauguin" key of symmops.json
+    Returns:
+        short "hermann_mauguin" key
+    """
+    if symbol in ("P 1", "C 1", "P 1 "):
+        return symbol
+    blickrichtungen = symbol.split(" ")
+    blickrichtungen_new = []
+    for br in blickrichtungen:
+        if br != "1":
+            blickrichtungen_new.append(br + " ")
+    return "".join(blickrichtungen_new)
+
+
+new_symm_data = {}
+for k, v in SYMM_DATA["space_group_encoding"].items():
+    if k.endswith("H"):
+        new_symm_data[k.removesuffix("H")] = v
+    elif k == "I2_12_121":
+        new_symm_data["I2_12_12_1"] = v
+    elif k == "P2_12_121":
+        new_symm_data["P2_12_12_1"] = v
+    else:
+        new_symm_data[k] = v
+
+SYMM_DATA["space_group_encoding"] = new_symm_data
+
+for spg_idx, spg in enumerate(SYMM_OPS):
+    if "(" in spg["hermann_mauguin"]:
+        SYMM_OPS[spg_idx]["hermann_mauguin"] = spg["hermann_mauguin"].split("(")[0]
+
+    short_h_m = remove_identity_from_full_hermann_mauguin(SYMM_OPS[spg_idx]["hermann_mauguin"])
+    SYMM_OPS[spg_idx]["short_h_m"] = convert_symmops_to_sg_encoding(short_h_m)
+    SYMM_OPS[spg_idx]["hermann_mauguin_u"] = convert_symmops_to_sg_encoding(spg["hermann_mauguin"])
+
+for spg_idx, spg in enumerate(SYMM_OPS):
+    try:
+        pg = PointGroup.from_space_group(spg["short_h_m"])
+    except AssertionError as e:
+        print(spg, str(e))
+        sys.exit(1)
+    SYMM_OPS[spg_idx]["point_group"] = pg.symbol
+
+dumpfn(SYMM_DATA, "../src/pymatgen/symmetry/symm_data.json")
+dumpfn(SYMM_OPS, "../src/pymatgen/symmetry/symm_ops.json")

src/pymatgen/symmetry/groups.py
@@ -112,6 +112,7 @@ class PointGroup(SymmetryGroup):
 
     def __init__(self, int_symbol: str) -> None:
         """Initialize a Point Group from its international symbol.
+        Please note that only the 32 crystal classes are supported right now.
 
         Args:
             int_symbol (str): International or Hermann-Mauguin Symbol.
@@ -165,6 +166,54 @@ class PointGroup(SymmetryGroup):
                 orbit.append(pp)
         return orbit
 
+    @classmethod
+    def from_space_group(cls, sg_symbol: str) -> PointGroup:
+        """Instantiate one of the 32 crystal classes from a space group symbol in
+        Hermann Mauguin notation (int symbol or full symbol).
+
+        Args:
+            sg_symbol: space group symbol in Hermann Mauguin notation.
+
+        Raises:
+            AssertionError if a valid crystal class cannot be created
+        Returns:
+            crystal class in Hermann-Mauguin notation.
+        """
+        abbrev_map = {
+            "2/m2/m2/m": "mmm",
+            "4/m2/m2/m": "4/mmm",
+            "-32/m": "-3m",
+            "6/m2/m2/m": "6/mmm",
+            "2/m-3": "m-3",
+            "4/m-32/m": "m-3m",
+        }
+        non_standard_map = {
+            "m2m": "mm2",
+            "2mm": "mm2",
+            "-4m2": "-42m",  # technically not non-standard
+            "-62m": "-6m2",  # technically not non-standard
+        }
+        symbol = re.sub(r" ", "", sg_symbol)
+
+        symm_ops = loadfn(os.path.join(os.path.dirname(__file__), "symm_ops.json"))  # get short symbol if possible
+        for spg in symm_ops:
+            if symbol in [spg["hermann_mauguin"], spg["universal_h_m"], spg["hermann_mauguin_u"]]:
+                symbol = spg["short_h_m"]
+
+        assert symbol[0].isupper(), f"Invalid sg_symbol {sg_symbol}"
+        assert not symbol[1:].isupper(), f"Invalid sg_symbol {sg_symbol}"
+
+        symbol = symbol[1:]  # Remove centering
+        symbol = symbol.translate(str.maketrans("abcden", "mmmmmm"))  # Remove translation from glide planes
+        symbol = re.sub(r"_.", "", symbol)  # Remove translation from screw axes
+        symbol = abbrev_map.get(symbol, symbol)
+        symbol = non_standard_map.get(symbol, symbol)
+
+        assert (
+            symbol in SYMM_DATA["point_group_encoding"]
+        ), f"Could not create a valid crystal class ({symbol}) from sg_symbol {sg_symbol}"
+        return cls(symbol)
+
 
 @cached_class
 class SpaceGroup(SymmetryGroup):
@@ -194,7 +243,7 @@ class SpaceGroup(SymmetryGroup):
         v["full_symbol"]: k for k, v in SYMM_DATA["space_group_encoding"].items()
     }
 
-    def __init__(self, int_symbol: str) -> None:
+    def __init__(self, int_symbol: str, hexagonal: bool = True) -> None:
         """Initialize a Space Group from its full or abbreviated international
         symbol. Only standard settings are supported.
 
@@ -209,8 +258,25 @@ class SpaceGroup(SymmetryGroup):
                 possible settings for a spacegroup, use the get_settings()
                 classmethod. Alternative origin choices can be indicated by a
                 translation vector, e.g. 'Fm-3m(a-1/4,b-1/4,c-1/4)'.
+            hexagonal (bool): For rhombohedral groups, whether to handle as in
+                hexagonal setting (default) or rhombohedral setting.
+                If the int_symbol of a rhombohedral spacegroup is given with the
+                setting ("(:)H"/"(:)R"), this parameter is overwritten accordingly
+                (please note that the setting is not contained in the symbol
+                attribute anymore).
         """
         from pymatgen.core.operations import SymmOp
+
+        if int_symbol.endswith("H"):
+            self.hexagonal = True
+            if not int_symbol.endswith(":H"):
+                int_symbol = int_symbol[:-1] + ":H"
+        elif int_symbol.endswith("R"):
+            self.hexagonal = False
+            if not int_symbol.endswith(":R"):
+                int_symbol = int_symbol[:-1] + ":R"
+        else:
+            self.hexagonal = hexagonal
 
         int_symbol = re.sub(r" ", "", int_symbol)
         if int_symbol in SpaceGroup.abbrev_sg_mapping:
@@ -221,15 +287,22 @@ class SpaceGroup(SymmetryGroup):
         self._symmetry_ops: set[SymmOp] | None
 
         for spg in SpaceGroup.SYMM_OPS:
-            if int_symbol in [spg["hermann_mauguin"], spg["universal_h_m"]]:
+            if int_symbol in [spg["hermann_mauguin"], spg["universal_h_m"], spg["hermann_mauguin_u"]]:
                 ops = [SymmOp.from_xyz_str(s) for s in spg["symops"]]
-                self.symbol = re.sub(r":", "", re.sub(r" ", "", spg["universal_h_m"]))
+                self.symbol = spg["hermann_mauguin_u"]
                 if int_symbol in SpaceGroup.sg_encoding:
                     self.full_symbol = SpaceGroup.sg_encoding[int_symbol]["full_symbol"]
                     self.point_group = SpaceGroup.sg_encoding[int_symbol]["point_group"]
+                elif self.symbol in SpaceGroup.sg_encoding:
+                    self.full_symbol = SpaceGroup.sg_encoding[self.symbol]["full_symbol"]
+                    self.point_group = SpaceGroup.sg_encoding[self.symbol]["point_group"]
                 else:
-                    self.full_symbol = re.sub(r" ", "", spg["universal_h_m"])
-                    self.point_group = spg["schoenflies"]
+                    self.full_symbol = spg["hermann_mauguin_u"]
+                    warnings.warn(
+                        f"Full symbol not available, falling back to short Hermann Mauguin symbol "
+                        f"{self.symbol} instead"
+                    )
+                    self.point_group = spg["point_group"]
                 self.int_number = spg["number"]
                 self.order = len(ops)
                 self._symmetry_ops = {*ops}
@@ -399,7 +472,7 @@ class SpaceGroup(SymmetryGroup):
         if crys_system == "hexagonal" or (
             crys_system == "trigonal"
             and (
-                self.symbol.endswith("H")
+                self.hexagonal
                 or self.int_number
                 in [143, 144, 145, 147, 149, 150, 151, 152, 153, 154, 156, 157, 158, 159, 162, 163, 164, 165]
             )
@@ -531,7 +604,8 @@ def sg_symbol_from_int_number(int_number: int, hexagonal: bool = True) -> str:
             hexagonal setting (default) or rhombohedral setting.
 
     Returns:
-        str: Spacegroup symbol
+        str: Spacegroup symbol / Space group symbol + "H" if group is
+            rhombohedral and hexagonal=True
     """
     syms = []
     for n, v in SYMM_DATA["space_group_encoding"].items():

tests/symmetry/test_groups.py
@@ -16,18 +16,18 @@ __date__ = "4/10/14"
 
 ORDERED_SYMBOLS = (
     "P1 P-1 P121 P12_11 C121 P1m1 P1c1 C1m1 C1c1 P12/m1 P12_1/m1 C12/m1 P12/c1 P12_1/c1 C12/c1 P222 P222_1"
-    " P2_12_12 P2_12_121 C222_1 C222 F222 I222 I2_12_121 Pmm2 Pmc2_1 Pcc2 Pma2 Pca2_1 Pnc2 Pmn2_1 Pba2 Pna2_1 Pnn2 "
-    "Cmm2 Cmc2_1 Ccc2 Amm2 Aem2 Ama2 Aea2 Fmm2 Fdd2 Imm2 Iba2 Ima2 Pmmm Pnnn1 Pccm Pban1 Pmma Pnna Pmna Pcca Pbam "
-    "Pccn Pbcm Pnnm Pmmn1 Pbcn Pbca Pnma Cmcm Cmce Cmmm Cccm Cmme Ccce1 Fmmm Fddd1 Immm Ibam Ibca Imma P4 P4_1 P4_2 "
-    "P4_3 I4 I4_1 P-4 I-4 P4/m P4_2/m P4/n1 P4_2/n I4/m I4_1/a P422 P42_12 P4_122 P4_12_12 P4_222 P4_22_12 P4_322 "
+    " P2_12_12 P2_12_12_1 C222_1 C222 F222 I222 I2_12_12_1 Pmm2 Pmc2_1 Pcc2 Pma2 Pca2_1 Pnc2 Pmn2_1 Pba2 Pna2_1 Pnn2 "
+    "Cmm2 Cmc2_1 Ccc2 Amm2 Aem2 Ama2 Aea2 Fmm2 Fdd2 Imm2 Iba2 Ima2 Pmmm Pnnn Pccm Pban Pmma Pnna Pmna Pcca Pbam "
+    "Pccn Pbcm Pnnm Pmmn Pbcn Pbca Pnma Cmcm Cmce Cmmm Cccm Cmme Ccce Fmmm Fddd Immm Ibam Ibca Imma P4 P4_1 P4_2 "
+    "P4_3 I4 I4_1 P-4 I-4 P4/m P4_2/m P4/n P4_2/n I4/m I4_1/a P422 P42_12 P4_122 P4_12_12 P4_222 P4_22_12 P4_322 "
     "P4_32_12 I422 I4_122 P4mm P4bm P4_2cm P4_2nm P4cc P4nc P4_2mc P4_2bc I4mm I4cm I4_1md I4_1cd P-42m P-42c P-42_1m "
-    "P-42_1c P-4m2 P-4c2 P-4b2 P-4n2 I-4m2 I-4c2 I-42m I-42d P4/mmm P4/mcc P4/nbm1 P4/nnc1 P4/mbm P4/mnc P4/nmm1 "
-    "P4/ncc1 P4_2/mmc P4_2/mcm P4_2/nbc P4_2/nnm P4_2/mbc P4_2/mnm P4_2/nmc P4_2/ncm I4/mmm I4/mcm I4_1/amd I4_1/acd "
-    "P3 P3_1 P3_2 R3H P-3 R-3H P312 P321 P3_112 P3_121 P3_212 P3_221 R32H P3m1 P31m P3c1 P31c R3mH R3cH P-31m P-31c "
-    "P-3m1 P-3c1 R-3mH R-3cH P6 P6_1 P6_5 P6_2 P6_4 P6_3 P-6 P6/m P6_3/m P622 P6_122 P6_522 P6_222 P6_422 P6_322 "
+    "P-42_1c P-4m2 P-4c2 P-4b2 P-4n2 I-4m2 I-4c2 I-42m I-42d P4/mmm P4/mcc P4/nbm P4/nnc P4/mbm P4/mnc P4/nmm "
+    "P4/ncc P4_2/mmc P4_2/mcm P4_2/nbc P4_2/nnm P4_2/mbc P4_2/mnm P4_2/nmc P4_2/ncm I4/mmm I4/mcm I4_1/amd I4_1/acd "
+    "P3 P3_1 P3_2 R3 P-3 R-3 P312 P321 P3_112 P3_121 P3_212 P3_221 R32 P3m1 P31m P3c1 P31c R3m R3c P-31m P-31c "
+    "P-3m1 P-3c1 R-3m R-3c P6 P6_1 P6_5 P6_2 P6_4 P6_3 P-6 P6/m P6_3/m P622 P6_122 P6_522 P6_222 P6_422 P6_322 "
     "P6mm P6cc P6_3cm P6_3mc P-6m2 P-6c2 P-62m P-62c P6/mmm P6/mcc P6_3/mcm P6_3/mmc P23 F23 I23 P2_13 I2_13 Pm-3 "
-    "Pn-31 Fm-3 Fd-31 Im-3 Pa-3 Ia-3 P432 P4_232 F432 F4_132 I432 P4_332 P4_132 I4_132 P-43m F-43m I-43m P-43n F-43c "
-    "I-43d Pm-3m Pn-3n1 Pm-3n Pn-3m1 Fm-3m Fm-3c Fd-3m1 Fd-3c1 Im-3m Ia-3d"
+    "Pn-3 Fm-3 Fd-3 Im-3 Pa-3 Ia-3 P432 P4_232 F432 F4_132 I432 P4_332 P4_132 I4_132 P-43m F-43m I-43m P-43n F-43c "
+    "I-43d Pm-3m Pn-3n Pm-3n Pn-3m Fm-3m Fm-3c Fd-3m Fd-3c Im-3m Ia-3d"
 ).split()
 
 
@@ -59,6 +59,14 @@ class TestPointGroup:
         # assert pg3m.is_subgroup(pgm3m)
         assert pg_3m.is_subgroup(pg_6mmm)
         assert not pg_m3m.is_supergroup(pg_6mmm)
+
+    def test_from_space_group(self):
+        assert PointGroup.from_space_group("P 2_1/n2_1/m2_1/a").symbol == "mmm"
+        assert PointGroup.from_space_group("F d d d").symbol == "mmm"
+        assert PointGroup.from_space_group("I -4").symbol == "-4"
+        assert PointGroup.from_space_group("P4_32_12_1").symbol == "422"
+        assert PointGroup.from_space_group("F4_1/d-32/m").symbol == "m-3m"
+        assert PointGroup.from_space_group("P121").symbol == "2"
 
 
 class TestSpaceGroup:
@@ -99,17 +107,9 @@ class TestSpaceGroup:
 
     def test_get_settings(self):
         assert SpaceGroup.get_settings("Fm-3m") == {"Fm-3m(a-1/4,b-1/4,c-1/4)", "Fm-3m"}
-        assert SpaceGroup.get_settings("Pmmn") == {
-            "Pmmn",
-            "Pmnm:1",
-            "Pnmm:2",
-            "Pmnm:2",
-            "Pnmm",
-            "Pnmm:1",
-            "Pmmn:1",
-            "Pmnm",
-            "Pmmn:2",
-        }
+
+        pmmn_settings = {"Pmmn", "Pmnm:1", "Pnmm:2", "Pmnm:2", "Pnmm", "Pnmm:1", "Pmmn:1", "Pmnm", "Pmmn:2"}
+        assert SpaceGroup.get_settings("Pmmn") == pmmn_settings
         assert SpaceGroup.get_settings("Pmna") == {"Pnmb", "Pman", "Pncm", "Pmna", "Pcnm", "Pbmn"}
 
     def test_crystal_system(self):
@@ -146,6 +146,13 @@ class TestSpaceGroup:
         assert not sg.is_compatible(cubic)
         assert sg.is_compatible(hexagonal)
         sg = SpaceGroup("R-3m:R")
+        assert sg.is_compatible(cubic)
+        assert sg.is_compatible(rhom)
+        assert not sg.is_compatible(hexagonal)
+        sg = SpaceGroup("R-3m", hexagonal=True)
+        assert not sg.is_compatible(cubic)
+        assert sg.is_compatible(hexagonal)
+        sg = SpaceGroup("R-3m", hexagonal=False)
         assert sg.is_compatible(cubic)
         assert sg.is_compatible(rhom)
         assert not sg.is_compatible(hexagonal)
@@ -200,21 +207,45 @@ class TestSpaceGroup:
     def test_hexagonal(self):
         for num in (146, 148, 155, 160, 161, 166, 167):
             sg = SpaceGroup.from_int_number(num, hexagonal=False)
-            assert not sg.symbol.endswith("H")
+            assert sg.hexagonal is False
+            sg = SpaceGroup.from_int_number(num, hexagonal=True)
+            assert sg.hexagonal is True
+
+    def test_full_symbol_warning(self):
+        warning_msg = "Full symbol not available, falling back to short Hermann Mauguin symbol P2_1ma instead"
+        # TODO need to find a way to reset the warnings registry.
+        # by default, warnings are raised only once and the same warning being tested here is raised below by
+        # assert sg.to_unicode_string() == "P2₁ma"
+        # so don't move this test below test_string
+
+        with pytest.warns(UserWarning, match=warning_msg) as warns:
+            _ = SpaceGroup("P2_1ma")
+        assert len(warns) == 1
 
     def test_string(self):
         sg = SpaceGroup("R-3c")
-        assert sg.to_latex_string() == r"R$\overline{3}$cH"
+        assert sg.to_latex_string() == r"R$\overline{3}$c"
         sg = SpaceGroup("P6/mmm")
         assert sg.to_latex_string() == "P6/mmm"
         sg = SpaceGroup("P4_1")
         assert sg.to_unicode_string() == "P4₁"
+        sg = SpaceGroup("P41")  # Added after issue #3862
+        assert sg.to_unicode_string() == "P4₁"
+        sg = SpaceGroup("P21ma")  # Added after issue #3862
+        assert sg.to_unicode_string() == "P2₁ma"
+        sg = SpaceGroup("P2_1ma")  # Added after issue #3862
+        assert sg.to_unicode_string() == "P2₁ma"
 
     def test_repr(self):
         for num in range(1, 231):
             sg = SpaceGroup.from_int_number(num)
             symbol = ORDERED_SYMBOLS[num - 1]
             assert repr(sg) in f"SpaceGroup({symbol=})"
+
+    def test_valid_symbol(self):  # Added after issue #3845
+        for num in range(1, 231):
+            symbol = SpaceGroup.from_int_number(num).symbol
+            assert SpaceGroup(symbol).int_number == num
 
     def test_raises_on_bad_int_number(self):
         for num in (-5, 0, 231, 1000):

commit 08dc6eb991b19065dda867ee19dad29ab1c13588

`is_subgroup()` modifications in `SpaceGroup` and `PointGroup` (#3941)
* Modified SpaceGroup.is_subgroup() following issue #3937, added tests.

* Added crystal_system attribute to PointGroup, modified is_subgroup warning of SymmetryGroup.

* Modified is_subgroup of PointGroup to raise NotImplementedError if group subgroup relationship would be possible (fr. crystal system), but cannot be checked currently due to crystallographic direction issue.

* Enabled PointGroup init from different settings, added test for that, added missing setting differences (trigonal groups), re-enabled SpaceGroup.is_subgroup shortcut with constraint of non-klassengleiche group subgroup relationship

* Removed bug in short Hermann Mauguin symbol of some trigonal groups introduced by myself in #3859

---------

Signed-off-by: Katharina Ueltzen <94910364+kaueltzen@users.noreply.github.com>

dev_scripts/update_spacegroup_data.py
@@ -56,6 +56,8 @@ def remove_identity_from_full_hermann_mauguin(symbol: str) -> str:
     if symbol in ("P 1", "C 1", "P 1 "):
         return symbol
     blickrichtungen = symbol.split(" ")
+    if blickrichtungen[1].startswith("3"):
+        return symbol
     blickrichtungen_new = []
     for br in blickrichtungen:
         if br != "1":
@@ -75,6 +77,11 @@ for k, v in SYMM_DATA["space_group_encoding"].items():
         new_symm_data[k] = v
 
 SYMM_DATA["space_group_encoding"] = new_symm_data
+
+for sg_symbol in SYMM_DATA["space_group_encoding"]:
+    SYMM_DATA["space_group_encoding"][sg_symbol]["point_group"] = PointGroup.from_space_group(
+        sg_symbol=sg_symbol
+    ).symbol
 
 for spg in SYMM_OPS:
     if "(" in spg["hermann_mauguin"]:

src/pymatgen/symmetry/groups.py
@@ -44,6 +44,24 @@ if TYPE_CHECKING:
 
 SYMM_DATA = loadfn(os.path.join(os.path.dirname(__file__), "symm_data.json"))
 
+PG_ABBREV_MAP = {
+    "2/m2/m2/m": "mmm",
+    "4/m2/m2/m": "4/mmm",
+    "-32/m": "-3m",
+    "6/m2/m2/m": "6/mmm",
+    "2/m-3": "m-3",
+    "4/m-32/m": "m-3m",
+}
+PG_SETTINGS_MAP = {  # only one setting per crystal class in SYMM_DATA database available right now
+    "m2m": "mm2",
+    "2mm": "mm2",
+    "-4m2": "-42m",
+    "-62m": "-6m2",
+    "312": "32",
+    "31m": "3m",
+    "-31m": "-3m",
+}
+
 
 class SymmetryGroup(Sequence, Stringify, ABC):
     """Abstract class representing a symmetry group."""
@@ -86,7 +104,10 @@ class SymmetryGroup(Sequence, Stringify, ABC):
         Returns:
             bool: True if this group is a subgroup of the supplied group.
         """
-        warnings.warn("This is not fully functional. Only trivial subsets are tested right now. ")
+        warnings.warn(
+            "This is not fully functional. Only trivial subsets are tested right now. "
+            "This will not work if the crystallographic directions of the two groups are different."
+        )
         return set(self.symmetry_ops).issubset(supergroup.symmetry_ops)
 
     def is_supergroup(self, subgroup: SymmetryGroup) -> bool:
@@ -98,7 +119,10 @@ class SymmetryGroup(Sequence, Stringify, ABC):
         Returns:
             bool: True if this group is a supergroup of the supplied group.
         """
-        warnings.warn("This is not fully functional. Only trivial subsets are tested right now. ")
+        warnings.warn(
+            "This is not fully functional. Only trivial subsets are tested right now. "
+            "This will not work if the crystallographic directions of the two groups are different."
+        )
         return set(subgroup.symmetry_ops).issubset(self.symmetry_ops)
 
     def to_latex_string(self) -> str:
@@ -125,9 +149,15 @@ class PointGroup(SymmetryGroup):
         Please note that only the 32 crystal classes are supported right now.
 
         Args:
-            int_symbol (str): International or Hermann-Mauguin Symbol.
+            int_symbol (str): International or Hermann-Mauguin Symbol. Please note that the PointGroup object
+            may have a different setting than specified in the int_symbol as only one setting per point group
+            is available right now.
         """
         from pymatgen.core.operations import SymmOp
+
+        int_symbol = int_symbol.replace(" ", "")
+        int_symbol = PG_ABBREV_MAP.get(int_symbol, int_symbol)
+        int_symbol = PG_SETTINGS_MAP.get(int_symbol, int_symbol)
 
         self.symbol = int_symbol
         self.generators = [
@@ -135,6 +165,7 @@ class PointGroup(SymmetryGroup):
         ]
         self._symmetry_ops = {SymmOp.from_rotation_and_translation(m) for m in self._generate_full_symmetry_ops()}
         self.order = len(self._symmetry_ops)
+        self.crystal_system = SYMM_DATA["point_group_crystal_system_map"][int_symbol]
 
     @property
     def symmetry_ops(self) -> set[SymmOp]:
@@ -176,10 +207,49 @@ class PointGroup(SymmetryGroup):
                 orbit.append(pp)
         return orbit
 
+    def is_subgroup(self, supergroup: PointGroup) -> bool:
+        """True if this group is a subgroup of the supplied group.
+            Modification of SymmetryGroup method with a few more constraints.
+
+        Args:
+            supergroup (pointGroup): Supergroup to test.
+
+        Returns:
+            bool: True if this group is a subgroup of the supplied group.
+        """
+        possible_but_direction_differences = (
+            ["trigonal", "cubic"],
+            ["monoclinic", "tetragonal"],
+            ["monoclinic", "hexagonal"],
+            ["monoclinic", "trigonal"],
+            ["orthorhombic", "hexagonal"],
+            ["orthorhombic", "tetragonal"],
+        )
+        if [self.crystal_system, supergroup.crystal_system] in possible_but_direction_differences:
+            raise NotImplementedError
+        warnings.warn(
+            "This is not fully functional. Only trivial subsets are tested right now. "
+            "This will not work if the crystallographic directions of the two groups are different."
+        )
+        return set(self.symmetry_ops).issubset(supergroup.symmetry_ops)
+
+    def is_supergroup(self, subgroup: PointGroup) -> bool:
+        """True if this group is a subgroup of the supplied group.
+            Modification of SymmetryGroup method with a few more constraints.
+
+        Args:
+            subgroup (PointGroup): Subgroup to test.
+
+        Returns:
+            bool: True if this group is a supergroup of the supplied group.
+        """
+        return subgroup.is_subgroup(self)
+
     @classmethod
     def from_space_group(cls, sg_symbol: str) -> PointGroup:
         """Instantiate one of the 32 crystal classes from a space group symbol in
         Hermann Mauguin notation (int symbol or full symbol).
+        Please note that the axes of space group and crystal class may be different.
 
         Args:
             sg_symbol: space group symbol in Hermann Mauguin notation.
@@ -190,20 +260,6 @@ class PointGroup(SymmetryGroup):
         Returns:
             crystal class in Hermann-Mauguin notation.
         """
-        abbrev_map = {
-            "2/m2/m2/m": "mmm",
-            "4/m2/m2/m": "4/mmm",
-            "-32/m": "-3m",
-            "6/m2/m2/m": "6/mmm",
-            "2/m-3": "m-3",
-            "4/m-32/m": "m-3m",
-        }
-        non_standard_map = {
-            "m2m": "mm2",
-            "2mm": "mm2",
-            "-4m2": "-42m",  # technically not non-standard
-            "-62m": "-6m2",  # technically not non-standard
-        }
         symbol = re.sub(r" ", "", sg_symbol)
 
         symm_ops = loadfn(os.path.join(os.path.dirname(__file__), "symm_ops.json"))  # get short symbol if possible
@@ -223,8 +279,8 @@ class PointGroup(SymmetryGroup):
         symbol = symbol[1:]  # Remove centering
         symbol = symbol.translate(str.maketrans("abcden", "mmmmmm"))  # Remove translation from glide planes
         symbol = re.sub(r"_.", "", symbol)  # Remove translation from screw axes
-        symbol = abbrev_map.get(symbol, symbol)
-        symbol = non_standard_map.get(symbol, symbol)
+        symbol = PG_ABBREV_MAP.get(symbol, symbol)
+        symbol = PG_SETTINGS_MAP.get(symbol, symbol)
 
         if symbol not in SYMM_DATA["point_group_encoding"]:
             raise ValueError(f"Could not create a valid crystal class ({symbol}) from {sg_symbol=}")
@@ -565,7 +621,7 @@ class SpaceGroup(SymmetryGroup):
         if not isinstance(supergroup, SpaceGroup):
             return NotImplemented
 
-        if len(supergroup.symmetry_ops) < len(self.symmetry_ops):
+        if len(supergroup.symmetry_ops) < len(self.symmetry_ops) and supergroup.point_group != self.point_group:
             return False
 
         groups = [{supergroup.int_number}]
@@ -574,7 +630,10 @@ class SpaceGroup(SymmetryGroup):
         while True:
             new_sub_groups = set()
             for i in groups[-1]:
-                new_sub_groups.update([j for j in max_subgroups[i] if j not in all_groups])
+                if len(groups) == 1:
+                    new_sub_groups.update(list(max_subgroups[i]))
+                else:
+                    new_sub_groups.update([j for j in max_subgroups[i] if j not in all_groups])
             if self.int_number in new_sub_groups:
                 return True
 

tests/symmetry/test_groups.py
@@ -56,10 +56,15 @@ class TestPointGroup:
         pg_m3m = PointGroup("m-3m")
         pg_6mmm = PointGroup("6/mmm")
         pg_3m = PointGroup("-3m")
-        # TODO: Fix the test below.
+        # TODO: Fix the tests below.
         # assert pg3m.is_subgroup(pgm3m)
+        # assert PointGroup("32").is_subgroup(PointGroup("-6m2"))
         assert pg_3m.is_subgroup(pg_6mmm)
         assert not pg_m3m.is_supergroup(pg_6mmm)
+
+    def test_init_different_settings_and_full_notation(self):
+        PointGroup("2/m 2/m 2/m")
+        PointGroup("31m")
 
     def test_from_space_group(self):
         assert PointGroup.from_space_group("P 2_1/n2_1/m2_1/a").symbol == "mmm"
@@ -227,6 +232,8 @@ class TestSpaceGroup:
     def test_subgroup_supergroup(self):
         assert SpaceGroup("Pma2").is_subgroup(SpaceGroup("Pccm"))
         assert not SpaceGroup.from_int_number(229).is_subgroup(SpaceGroup.from_int_number(230))
+        assert SpaceGroup("P3").is_subgroup(SpaceGroup("P3"))  # Added after #3937
+        assert SpaceGroup("Fm-3m").is_subgroup(SpaceGroup("Pm-3m"))  # Added after #3937
 
     def test_hexagonal(self):
         for num in (146, 148, 155, 160, 161, 166, 167):