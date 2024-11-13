327606d9179a60937f9e4ed2b6b297b6486a1b7d
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-11-13 17:00:36 -0500

    add TestDftSet.test_kind_magnetization for checking different ways of setting element-specific initial magnetization

--- tests/io/cp2k/test_sets.py
+++ tests/io/cp2k/test_sets.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import numpy as np
 import pytest
 from pytest import approx
 
@@ -7,7 +8,7 @@ from pymatgen.core.structure import Molecule, Structure
 from pymatgen.io.cp2k.sets import SETTINGS, Cp2kValidationError, DftSet, GaussianTypeOrbitalBasisSet, GthPotential
 from pymatgen.util.testing import TEST_FILES_DIR, PymatgenTest
 
-TEST_DIR = f"{TEST_FILES_DIR}/io/cp2k"
+CP2K_TEST_DIR = f"{TEST_FILES_DIR}/io/cp2k"
 
 Si_structure = Structure(
     lattice=[[0, 2.734364, 2.734364], [2.734364, 0, 2.734364], [2.734364, 2.734364, 0]],
@@ -16,13 +17,42 @@ Si_structure = Structure(
 )
 molecule = Molecule(species=["Si"], coords=[[0, 0, 0]])
 
+# Basis set / potential with objects
+Si_gto_basis = """
+Si SZV-MOLOPT-GTH SZV-MOLOPT-GTH-q4
+1
+2 0 1 6 1 1
+    2.693604434572  0.015333179500 -0.005800105400
+    1.359613855428 -0.283798205000 -0.059172026000
+    0.513245176029 -0.228939692700  0.121487149900
+    0.326563011394  0.728834000900  0.423382421100
+    0.139986977410  0.446205299300  0.474592116300
+    0.068212286977  0.122025292800  0.250129397700
+"""
+Si_gth_pot = """Si GTH-BLYP-q4 GTH-BLYP
+2    2
+0.44000000    1    -6.25958674
+2
+0.44465247    2     8.31460936    -2.33277947
+                                    3.01160535
+0.50279207    1     2.33241791"""
+
+BASIS_AND_POTENTIAL: dict[str, dict[str, GaussianTypeOrbitalBasisSet | GthPotential]] = {
+    "Si": {"basis": GaussianTypeOrbitalBasisSet.from_str(Si_gto_basis), "potential": GthPotential.from_str(Si_gth_pot)},
+    "Fe": {
+        "basis": GaussianTypeOrbitalBasisSet.from_str(Si_gto_basis.replace("Si", "Fe")),
+        "potential": GthPotential.from_str(Si_gth_pot.replace("Si", "Fe")),
+    },
+}
+
 
 class TestDftSet(PymatgenTest):
-    def test_dft_set(self):
-        SETTINGS["PMG_CP2K_DATA_DIR"] = TEST_DIR
+    def test_dft_set(self) -> None:
+        """Test various DFT set configurations."""
+        SETTINGS["PMG_CP2K_DATA_DIR"] = CP2K_TEST_DIR
 
         # Basis sets / potentials searching
-        basis_and_potential = {
+        basis_and_potential: dict[str, str | None | dict[str, str | None]] = {
             "basis_type": "SZV",
             "potential_type": "Pseudopotential",
             "functional": None,
@@ -55,35 +85,10 @@ class TestDftSet(PymatgenTest):
         }
         dft_set = DftSet(Si_structure, basis_and_potential=basis_and_potential, xc_functionals="PBE")
 
-        # Basis set / potential with objects
-        gto = """
-         Si SZV-MOLOPT-GTH SZV-MOLOPT-GTH-q4
-            1
-            2 0 1 6 1 1
-                2.693604434572  0.015333179500 -0.005800105400
-                1.359613855428 -0.283798205000 -0.059172026000
-                0.513245176029 -0.228939692700  0.121487149900
-                0.326563011394  0.728834000900  0.423382421100
-                0.139986977410  0.446205299300  0.474592116300
-                0.068212286977  0.122025292800  0.250129397700
-        """
-        pot = """Si GTH-BLYP-q4 GTH-BLYP
-                2    2
-                0.44000000    1    -6.25958674
-                2
-                0.44465247    2     8.31460936    -2.33277947
-                                                    3.01160535
-                0.50279207    1     2.33241791"""
-        basis_and_potential = {
-            "Si": {
-                "basis": GaussianTypeOrbitalBasisSet.from_str(gto),
-                "potential": GthPotential.from_str(pot),
-            }
-        }
         set_kwargs = dict.fromkeys(("print_pdos", "print_dos", "print_v_hartree", "print_e_density"), False)
         dft_set = DftSet(
             Si_structure,
-            basis_and_potential=basis_and_potential,
+            basis_and_potential=BASIS_AND_POTENTIAL,
             xc_functionals="PBE",
             **set_kwargs,
         )
@@ -131,6 +136,48 @@ class TestDftSet(PymatgenTest):
         ):
             dft_set.validate()
 
+        # Test non-periodic settings
         dft_set = DftSet(molecule, basis_and_potential=basis_and_potential, xc_functionals="PBE")
         assert dft_set.check("force_eval/dft/poisson")
         assert dft_set["force_eval"]["dft"]["poisson"].get("periodic").values[0].upper() == "NONE"
+
+    def test_kind_magnetization(self) -> None:
+        """Test different ways of setting kind (i.e. element-specific) initial magnetization."""
+        fe_structure = Structure(
+            lattice=np.eye(3) * 3,
+            species=["Fe", "Fe"],
+            coords=[[0, 0, 0], [0.5, 0.5, 0.5]],
+        )
+
+        # Test 1: Setting via site properties
+        dft_set = DftSet(
+            fe_structure.copy().add_site_property("magmom", [2.0, -2.0]),
+            basis_and_potential=BASIS_AND_POTENTIAL,
+            xc_functionals="PBE",
+        )
+        fe1_kind = dft_set["force_eval"]["subsys"]["Fe_1"]
+        fe2_kind = dft_set["force_eval"]["subsys"]["Fe_2"]
+        assert {fe1_kind["magnetization"].values[0], fe2_kind["magnetization"].values[0]} == {2.0, -2.0}
+
+        # Test 2: Setting via element defaults
+        element_defaults = {"Fe": {"magnetization": 4.0}}
+        dft_set = DftSet(
+            fe_structure,
+            basis_and_potential=BASIS_AND_POTENTIAL,
+            xc_functionals="PBE",
+            element_defaults=element_defaults,
+        )
+        fe_kind = dft_set["force_eval"]["subsys"]["Fe_1"]
+        assert fe_kind["magnetization"].values[0] == 4.0
+
+        # Test 3: Site properties take precedence over element defaults
+        fe_structure.add_site_property("magmom", [2.0, -2.0])
+        dft_set = DftSet(
+            fe_structure,
+            basis_and_potential=BASIS_AND_POTENTIAL,
+            xc_functionals="PBE",
+            element_defaults=element_defaults,
+        )
+        fe1_kind = dft_set["force_eval"]["subsys"]["Fe_1"]
+        fe2_kind = dft_set["force_eval"]["subsys"]["Fe_2"]
+        assert {fe1_kind["magnetization"].values[0], fe2_kind["magnetization"].values[0]} == {2.0, -2.0}
