cfd9041458a505bc4950b58ab42b24f4d5dfa954
Ben Rich <tiggzzss@gmail.com>
2024-11-21 16:24:38 -0700

    sourcing changes made in feature-jdftx-inputs

--- tests/io/jdftx/test_input_utils.py
+++ tests/io/jdftx/test_input_utils.py
@@ -2,13 +2,13 @@ from __future__ import annotations
 
 import pytest
 
-from pymatgen.io.jdftx._input_utils import flatten_list
+from pymatgen.io.jdftx.generic_tags import _flatten_list
 
 
 def test_flatten_list():
-    assert flatten_list("", [1, 2, 3]) == [1, 2, 3]
-    assert flatten_list("", [1, [2, 3]]) == [1, 2, 3]
-    assert flatten_list("", [1, [2, [3]]]) == [1, 2, 3]
-    assert flatten_list("", [1, [2, [3, [4]]]]) == [1, 2, 3, 4]
+    assert _flatten_list("", [1, 2, 3]) == [1, 2, 3]
+    assert _flatten_list("", [1, [2, 3]]) == [1, 2, 3]
+    assert _flatten_list("", [1, [2, [3]]]) == [1, 2, 3]
+    assert _flatten_list("", [1, [2, [3, [4]]]]) == [1, 2, 3, 4]
     with pytest.raises(TypeError):
-        flatten_list("", 1)
+        _flatten_list("", 1)
