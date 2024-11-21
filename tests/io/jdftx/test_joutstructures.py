8d3cc80d8c7f90452d757c623af18ec4dca5ab83
Ben Rich <tiggzzss@gmail.com>
2024-11-21 16:34:39 -0700

    sourcing output code from master to this branch

---
+++ tests/io/jdftx/test_joutstructures.py
@@ -0,0 +1,93 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import pytest
+from pytest import approx
+
+from pymatgen.core.units import Ha_to_eV
+from pymatgen.io.jdftx.joutstructures import JOutStructure, JOutStructures
+from pymatgen.util.testing import TEST_FILES_DIR
+
+ex_files_dir = Path(TEST_FILES_DIR) / "io" / "jdftx" / "example_files"
+ex_outslice_fname1 = ex_files_dir / "ex_out_slice_latmin"
+ex_outslice1 = []
+with open(ex_outslice_fname1) as f:
+    ex_outslice1 = list.copy(list(f))
+ex_outslice1_known = {
+    "mu0_0": 0.713855355 * Ha_to_eV,
+    "mu0_-1": 0.703866408 * Ha_to_eV,
+    "nEminSteps0": 18,
+    "etype0": "F",
+    "E0": -246.531007900240667 * Ha_to_eV,
+    "conv0": True,
+    "mu-1_0": 0.704400512 * Ha_to_eV,
+    "mu-1_-1": 0.704399109 * Ha_to_eV,
+    "nEminSteps-1": 4,
+    "etype-1": "F",
+    "E-1": -246.531042396724303 * Ha_to_eV,
+    "nGeomSteps": 7,
+    "conv-1": True,
+    "nelec0_0": 64.0,
+    "nelec0_-1": 64.0,
+    "nelec-1_0": 64.0,
+    "nelec-1_-1": 64.0,
+}
+ex_outslice_fname2 = ex_files_dir / "ex_out_slice_ionmin"
+with open(ex_outslice_fname2) as f:
+    ex_outslice2 = list.copy(list(f))
+ex_outslice2_known = {
+    "mu0_0": -0.190000000 * Ha_to_eV,
+    "mu0_-1": -0.190000000 * Ha_to_eV,
+    "nEminSteps0": 101,
+    "etype0": "G",
+    "E0": -1058.990493255521415 * Ha_to_eV,
+    "conv0": False,
+    "mu-1_0": -0.190000000 * Ha_to_eV,
+    "mu-1_-1": -0.190000000 * Ha_to_eV,
+    "nEminSteps-1": 13,
+    "etype-1": "G",
+    "E-1": -1059.062593502930213 * Ha_to_eV,
+    "nGeomSteps": 7,
+    "conv-1": True,
+    "nelec0_0": 325.000000,
+    "nelec0_-1": 325.610434,
+    "nelec-1_0": 325.541001,
+    "nelec-1_-1": 325.541406,
+}
+
+
+@pytest.mark.parametrize(
+    ("ex_slice", "ex_slice_known", "opt_type"),
+    [(ex_outslice1, ex_outslice1_known, "lattice")],
+)
+def test_jstructures(ex_slice: list[str], ex_slice_known: dict[str, float], opt_type: str):
+    jstruct = JOutStructures._from_out_slice(ex_slice, opt_type=opt_type)
+    assert isinstance(jstruct, JOutStructures)
+    assert isinstance(jstruct[0], JOutStructure)
+    assert jstruct[0].elecmindata[0].mu == approx(ex_slice_known["mu0_0"])
+    assert jstruct[0].elecmindata[-1].mu == approx(ex_slice_known["mu0_-1"])
+    assert jstruct[-1].elecmindata[0].mu == approx(ex_slice_known["mu-1_0"])
+    assert jstruct[-1].elecmindata[-1].mu == approx(ex_slice_known["mu-1_-1"])
+    assert jstruct.elecmindata[-1].mu == approx(ex_slice_known["mu-1_-1"])
+    assert jstruct.elecmindata.mu == approx(ex_slice_known["mu-1_-1"])
+    assert jstruct.mu == approx(ex_slice_known["mu-1_-1"])
+    assert jstruct["mu"] == approx(ex_slice_known["mu-1_-1"])
+    assert jstruct[0].elecmindata[0].nelectrons == approx(ex_slice_known["nelec0_0"])
+    assert jstruct[0].elecmindata[-1].nelectrons == approx(ex_slice_known["nelec0_-1"])
+    assert jstruct[-1].elecmindata[0].nelectrons == approx(ex_slice_known["nelec-1_0"])
+    assert jstruct[-1].elecmindata[-1].nelectrons == approx(ex_slice_known["nelec-1_-1"])
+    assert jstruct.elecmindata[-1].nelectrons == approx(ex_slice_known["nelec-1_-1"])
+    assert len(jstruct[0].elecmindata) == ex_slice_known["nEminSteps0"]
+    assert len(jstruct[-1].elecmindata) == ex_slice_known["nEminSteps-1"]
+    assert len(jstruct.elecmindata) == ex_slice_known["nEminSteps-1"]
+    assert jstruct[0].etype == ex_slice_known["etype0"]
+    assert approx(ex_slice_known["E0"]) == jstruct[0].e
+    assert jstruct[-1].etype == ex_slice_known["etype-1"]
+    assert jstruct.etype == ex_slice_known["etype-1"]
+    assert approx(ex_slice_known["E-1"]) == jstruct[-1].e
+    assert jstruct[0].elecmindata.converged == ex_slice_known["conv0"]
+    assert jstruct[-1].elecmindata.converged == ex_slice_known["conv-1"]
+    assert jstruct.elecmindata.converged == ex_slice_known["conv-1"]
+    assert len(jstruct) == ex_slice_known["nGeomSteps"]
+    assert jstruct.selective_dynamics is not None
---
+++ tests/io/jdftx/test_output_utils.py
@@ -0,0 +1,75 @@
+from __future__ import annotations
+
+import pytest
+
+from pymatgen.io.jdftx._output_utils import find_first_range_key, get_start_lines, multi_getattr, multi_hasattr
+from pymatgen.io.jdftx.joutstructures import _get_joutstructures_start_idx
+
+
+def test_get_start_lines():
+    with pytest.raises(ValueError, match="Outfile parser fed an empty file."):
+        get_start_lines([])
+    with pytest.raises(ValueError, match="No JDFTx calculations found in file."):
+        get_start_lines(["\n", "\n"])
+
+
+def test_find_first_range_key():
+    out1 = find_first_range_key("barbie", ["barbie"])
+    assert len(out1) == 1
+    assert out1[0] == 0
+    out1 = find_first_range_key("barbie", ["# barbie"])
+    assert len(out1) == 0
+    out1 = find_first_range_key("barbie", ["# barbie"], skip_pound=True)
+    assert len(out1) == 1
+    assert out1[0] == 0
+    out1 = find_first_range_key("barbie", ["barbie", "barbie", "barbie"])
+    assert len(out1) == 3
+    out1 = find_first_range_key("barbie", ["barbie", "ken", "barbie"])
+    assert len(out1) == 2
+    out1 = find_first_range_key("barbie", ["barbie", "ken", "barbie"], startline=1)
+    assert len(out1) == 1
+
+
+def test_get_joutstructures_start_idx():
+    start_flag = "barbie"
+    assert _get_joutstructures_start_idx(["ken", "barbie"], out_slice_start_flag=start_flag) == 1
+    assert _get_joutstructures_start_idx(["barbie", "ken"], out_slice_start_flag=start_flag) == 0
+    assert _get_joutstructures_start_idx(["ken", "ken"], out_slice_start_flag=start_flag) is None
+
+
+def test_multihasattr():
+    class A:
+        def __init__(self):
+            self.v1: int = 1
+
+    class B:
+        def __init__(self):
+            self.a = A()
+            self.v2: int = 2
+
+    a = A()
+    b = B()
+    assert multi_hasattr(a, "v1")
+    assert multi_hasattr(b, "a")
+    assert multi_hasattr(b, "a.v1")
+    assert not multi_hasattr(b, "a.v2")
+    assert not multi_hasattr(b, "v1")
+
+
+def test_multigetattr():
+    class A:
+        def __init__(self):
+            self.v1: int = 1
+
+    class B:
+        def __init__(self):
+            self.a = A()
+            self.v2: int = 2
+
+    a = A()
+    b = B()
+    assert multi_getattr(a, "v1") == 1
+    assert multi_getattr(b, "v2") == 2
+    assert multi_getattr(b, "a.v1") == 1
+    with pytest.raises(AttributeError):
+        multi_getattr(b, "v1")
