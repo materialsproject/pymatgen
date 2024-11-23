86620d5cb2f350b5077569277b4369f512288506
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-11-23 12:01:06 +0800

    revert unrelated changes

--- src/pymatgen/util/io_utils.py
+++ src/pymatgen/util/io_utils.py
@@ -2,17 +2,14 @@
 
 from __future__ import annotations
 
+import os
 import re
-import warnings
 from typing import TYPE_CHECKING
 
 from monty.io import zopen
 
 if TYPE_CHECKING:
-    from collections.abc import Callable, Iterator
-    from typing import Any
-
-    from pymatgen.util.typing import PathLike
+    from collections.abc import Iterator
 
 __author__ = "Shyue Ping Ong, Rickard Armiento, Anubhav Jain, G Matteo, Ioannis Petousis"
 __copyright__ = "Copyright 2011, The Materials Project"
@@ -28,7 +25,7 @@ def clean_lines(
     remove_empty_lines: bool = True,
     rstrip_only: bool = False,
 ) -> Iterator[str]:
-    """Remove leading and trailing whitespaces from a list of strings.
+    """Strips whitespace, carriage returns and empty lines from a list of strings.
 
     Args:
         string_list (list[str]): List of strings.
@@ -38,10 +35,10 @@ def clean_lines(
             to retain leading whitespaces). Defaults to False.
 
     Yields:
-        str: clean string with no leading and trailing whitespaces.
+        str: clean strings with no whitespaces.
     """
     for string in string_list:
-        clean_string: str = string
+        clean_string = string
         if "#" in string:
             clean_string = string[: string.index("#")]
 
@@ -51,62 +48,52 @@ def clean_lines(
             yield clean_string
 
 
-def micro_pyawk(
-    filename: PathLike,
-    search: list[tuple[re.Pattern | str, Callable, Callable]],
-    results: Any | None = None,
-    debug: Callable | None = None,
-    postdebug: Callable | None = None,
-) -> Any:
+def micro_pyawk(filename, search, results=None, debug=None, postdebug=None):
     """Small awk-mimicking search routine.
 
-    This function goes through each line in the file, and if `regex` matches that
-    line AND test(results, line) is True (OR test is None) we execute
-    run(results, match), where match is the Match object from running
-    Pattern.match.
+    'file' is file to search through.
+    'search' is the "search program", a list of lists/tuples with 3 elements;
+    i.e. [[regex, test, run], [regex, test, run], ...]
+    'results' is a an object that your search program will have access to for
+    storing results.
 
-    Args:
-        filename (PathLike): The file to search through.
-        search (list[tuple[Pattern | str, Callable, Callable]]): The "search program" of
-            3 elements, i.e. [(regex, test, run), ...].
-            Here `regex` is either a Pattern object, or a string that we compile
-            into a Pattern.
-        results: An object to store results. Default as an empty dictionary.
-            Passing a results object let you interact with it via `run` and `test`.
-            Hence, in many occasions it is clever to use the instance itself as results.
-        debug (Callable): Debug `run`.
-        postdebug (Callable): Post debug `run` after debug `run`.
+    Here regex is either as a Regex object, or a string that we compile into a
+    Regex. test and run are callable objects.
 
-    Returns:
-        Any: The updated `results` object.
+    This function goes through each line in filename, and if regex matches that
+    line *and* test(results,line)==True (or test is None) we execute
+    run(results,match), where match is the match object from running
+    Regex.match.
+
+    The default results is an empty dictionary. Passing a results object let
+    you interact with it in run() and test(). Hence, in many occasions it is
+    thus clever to use results=self.
 
     Author: Rickard Armiento, Ioannis Petousis
-    """
-    # TODO: remove `debug` and `postdebug` after 2025-11-09 if no one is opposing
-    if debug is not None:
-        warnings.warn("arg debug is scheduled for removal, see PR4160", DeprecationWarning, stacklevel=2)
-    if postdebug is not None:
-        warnings.warn("arg postdebug is scheduled for removal, see PR4160", DeprecationWarning, stacklevel=2)
 
+    Returns:
+        dict[str, Any]: The results dictionary.
+    """
     if results is None:
         results = {}
 
-    # Compile regex strings to Patterns
-    searches: list[tuple[re.Pattern, Callable, Callable]] = [
-        (re.compile(regex), test, run) for regex, test, run in search
-    ]
+    # Compile regex strings
+    for entry in search:
+        entry[0] = re.compile(entry[0])
 
     with zopen(filename, mode="rt") as file:
         for line in file:
-            for regex, test, run in searches:
-                match = re.search(regex, line)
-
-                if match is not None and (test is None or test(results, line)):
+            for entry in search:
+                match = re.search(entry[0], line)
+                if match and (entry[1] is None or entry[1](results, line)):
                     if debug is not None:
                         debug(results, match)
-
-                    run(results, match)
+                    entry[2](results, match)
                     if postdebug is not None:
                         postdebug(results, match)
 
     return results
+
+
+umask = os.umask(0)
+os.umask(umask)
