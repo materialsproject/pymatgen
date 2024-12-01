c94f2f535abc4a1cdd97775ac6822ef4b6a03b34
Haoyu Yang <yanghaoyu97@outlook.com>
2024-12-01 20:19:37 +0800

    check future warnings

--- tests/util/test_testing_migrate.py
+++ tests/util/test_testing_migrate.py
@@ -25,26 +25,49 @@ import pytest
 from pymatgen.util.testing import PymatgenTest
 
 
+@pytest.mark.filterwarnings("ignore", message="will not be supported by pytest after migration", category=FutureWarning)
 class TestPymatgenTestTestCase(PymatgenTest):
     """Baseline inspector for migration side effects."""
 
     def test_unittest_testcase_specific_funcs(self):
-        """Make sure TestCase-specific methods are available until migration.
-        TODO: check warnings
+        """Make sure TestCase-specific methods are available until migration,
+        and FutureWarning is emitted.
         """
+        msg = "will not be supported by pytest after migration"
 
         # Testing setUp and tearDown methods
-        self.setUp()
-        self.tearDown()
+        with pytest.warns(FutureWarning, match=msg):
+            self.setUp()
+        with pytest.warns(FutureWarning, match=msg):
+            self.tearDown()
 
         # Testing class-level setUp and tearDown methods
-        self.setUpClass()
-        self.tearDownClass()
+        with pytest.warns(FutureWarning, match=msg):
+            self.setUpClass()
+        with pytest.warns(FutureWarning, match=msg):
+            self.tearDownClass()
 
         # Test the assertion methods
-        self.assertTrue(True)
-        self.assertFalse(False)
-        self.assertEqual(1, 1)
+        with pytest.warns(FutureWarning, match=msg):
+            self.assertEqual(1, 1)
+
+        with pytest.warns(FutureWarning, match=msg):
+            self.assertNotEqual(1, 2)
+
+        with pytest.warns(FutureWarning, match=msg):
+            self.assertTrue(True)
+
+        with pytest.warns(FutureWarning, match=msg):
+            self.assertFalse(False)
+
+        with pytest.warns(FutureWarning, match=msg):
+            self.assertIsNone(None)
+
+        with pytest.warns(FutureWarning, match=msg):
+            self.assertIsNotNone("hello")
+
+        with pytest.warns(FutureWarning, match=msg), self.assertRaises(ValueError):
+            raise ValueError("hi")
 
 
 class TestPymatgenTestPytest:
