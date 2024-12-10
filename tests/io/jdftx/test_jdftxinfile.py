d391836edbaa7243a247847c9b88879d95af7ad2
Ben Rich <tiggzzss@gmail.com>
2024-12-10 15:53:18 -0700

    Cleanup of inputs - homogenized wording in error messages, added value validation methods to AbstractTag for validations that are called multiple times, cleaned up cluttered tests and added comments to make it more clear what is being tested

--- tests/io/jdftx/test_jdftxinfile.py
+++ tests/io/jdftx/test_jdftxinfile.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import os
 import re
+from copy import deepcopy
 from typing import TYPE_CHECKING, Any
 
 import numpy as np
@@ -63,14 +64,18 @@ def test_JDFTXInfile_known_lambda(infile_fname: str, bool_func: Callable[[JDFTXI
     ],
 )
 def test_JDFTXInfile_set_values(val_key: str, val: Any):
+    """Test value setting for various tags"""
     jif = JDFTXInfile.from_file(ex_infile1_fname)
     jif[val_key] = val
+    # Test that the JDFTXInfile object is still consistent
     JDFTXInfile_self_consistency_tester(jif)
 
 
 def test_JDFTXInfile_from_dict():
+    """Test the from_dict method"""
     jif = JDFTXInfile.from_file(ex_infile1_fname)
     jif_dict = jif.as_dict()
+    # Test that dictionary can be modified and that _from_dict will fix set values
     jif_dict["elec-cutoff"] = 20
     jif2 = JDFTXInfile.from_dict(jif_dict)
     JDFTXInfile_self_consistency_tester(jif2)
@@ -87,8 +92,13 @@ def test_JDFTXInfile_from_dict():
     ],
 )
 def test_JDFTXInfile_append_values(val_key: str, val: Any):
+    """Test the append_tag method"""
     jif = JDFTXInfile.from_file(ex_infile1_fname)
+    val_old = None if val_key not in jif else deepcopy(jif[val_key])
     jif.append_tag(val_key, val)
+    val_new = jif[val_key]
+    assert val_old != val_new
+    # Test that the append_tag does not break the JDFTXInfile object
     JDFTXInfile_self_consistency_tester(jif)
 
 
@@ -96,42 +106,55 @@ def test_JDFTXInfile_expected_exceptions():
     jif = JDFTXInfile.from_file(ex_infile1_fname)
     with pytest.raises(KeyError):
         jif["barbie"] = "ken"
-    with pytest.raises(ValueError, match="The initial-state tag cannot be repeated and thus cannot be appended"):
-        jif.append_tag("initial-state", "$VAR")
+    # non-repeating tags raise value-errors when appended
+    tag = "initial-state"
+    with pytest.raises(ValueError, match=re.escape(f"The tag '{tag}' cannot be repeated and thus cannot be appended")):
+        jif.append_tag(tag, "$VAR")
+    # Phonon and Wannier tags raise value-errors at _preprocess_line
     with pytest.raises(ValueError, match="Phonon functionality has not been added!"):
         jif._preprocess_line("phonon idk")
     with pytest.raises(ValueError, match="Wannier functionality has not been added!"):
         jif._preprocess_line("wannier idk")
+    # Tags not in MASTER_TAG_LIST raise value-errors at _preprocess_line
     err_str = f"The barbie tag in {['barbie', 'ken allan']} is not in MASTER_TAG_LIST and is not a comment, "
-    err_str += "something is wrong with this input data!"
+    "something is wrong with this input data!"
     with pytest.raises(ValueError, match=re.escape(err_str)):
         jif._preprocess_line("barbie ken allan")
-
+    # include tags raise value-errors if the file cannot be found
     _filename = "barbie"
     err_str = f"The include file {_filename} ({_filename}) does not exist!"
     with pytest.raises(ValueError, match=re.escape(err_str)):
         JDFTXInfile.from_str(f"include {_filename}\n")
+    # If it does exist, no error should be raised
     filename = ex_in_files_dir / "barbie"
     err_str = f"The include file {_filename} ({filename}) does not exist!"
     str(err_str)
+    # If the wrong parent_path is given for a file that does exist, error
     with pytest.raises(ValueError, match=re.escape(err_str)):
         JDFTXInfile.from_str(f"include {_filename}\n", path_parent=ex_in_files_dir)
+    # JDFTXInfile cannot be constructed without lattice and ion tags
     with pytest.raises(ValueError, match="This input file is missing required structure tags"):
         JDFTXInfile.from_str("dump End DOS\n")
+    # "barbie" here is supposed to be "list-to-dict" or "dict-to-list"
     with pytest.raises(ValueError, match="Conversion type barbie is not 'list-to-dict' or 'dict-to-list'"):
         jif._needs_conversion("barbie", ["ken"])
+    # Setting tags with unfixable values immediately raises an error
     tag = "exchange-params"
     value = {"blockSize": 1, "nOuterVxx": "barbie"}
     err_str = str(f"The {tag} tag with value:\n{value}\ncould not be fixed!")
     with pytest.raises(ValueError, match=re.escape(err_str)):
         # Implicitly tests validate_tags
         jif[tag] = value
+    # Setting tags with unfixable values through "update" side-steps the error, but will raise it once
+    # "validate_tags" is inevitably called
     jif2 = jif.copy()
     jif2.update({tag: value})
     with pytest.raises(ValueError, match=re.escape(err_str)):
         jif2.validate_tags(try_auto_type_fix=True)
+    # The inevitable error can be reduced to a warning if you tell it not to try to fix the values
     with pytest.warns(UserWarning):
         jif2.validate_tags(try_auto_type_fix=False)
+    # Setting a non-string tag raises an error within the JDFTXInfile object
     err_str = str(f"{1.2} is not a string!")
     with pytest.raises(TypeError, match=err_str):
         jif[1.2] = 3.4
@@ -159,18 +182,24 @@ def test_JDFTXInfile_niche_cases():
 
 
 def test_JDFTXInfile_add_method():
+    """Test the __add__ method"""
+    # No new values are being assigned in jif2, so jif + jif2 should be the same as jif
+    # Since the convenience of this method would be lost if the user has to pay special attention to duplicating
+    # repeatable values, repeatable tags are not append to each other
     jif = JDFTXInfile.from_file(ex_infile1_fname)
     jif2 = jif.copy()
     jif3 = jif + jif2
     assert_idential_jif(jif, jif3)
+    # If a tag is repeated, the values must be the same since choice of value is ambiguous
     key = "elec-ex-corr"
-    val_old = jif[key]
+    val_old = deepcopy(jif[key])
     val_new = "lda"
     assert val_old != val_new
     jif2[key] = val_new
     err_str = f"JDFTXInfiles have conflicting values for {key}: {val_old} != {val_new}"
     with pytest.raises(ValueError, match=re.escape(err_str)):
         jif3 = jif + jif2
+    # Normal expected behavior
     key_add = "target-mu"
     val_add = 0.5
     assert key_add not in jif
@@ -182,6 +211,7 @@ def test_JDFTXInfile_add_method():
 
 @pytest.mark.parametrize(("infile_fname", "knowns"), [(ex_infile1_fname, ex_infile1_knowns)])
 def test_JDFTXInfile_knowns_simple(infile_fname: PathLike, knowns: dict):
+    """Test that known values that can be tested with assert_same_value are correct"""
     jif = JDFTXInfile.from_file(infile_fname)
     for key, val in knowns.items():
         assert_same_value(jif[key], val)
@@ -189,11 +219,15 @@ def test_JDFTXInfile_knowns_simple(infile_fname: PathLike, knowns: dict):
 
 @pytest.mark.parametrize("infile_fname", [ex_infile3_fname, ex_infile1_fname, ex_infile2_fname])
 def test_JDFTXInfile_self_consistency(infile_fname: PathLike):
+    """Test that JDFTXInfile objects with different assortments of tags survive inter-conversion done within
+    "JDFTXInfile_self_consistency_tester"""
     jif = JDFTXInfile.from_file(infile_fname)
     JDFTXInfile_self_consistency_tester(jif)
 
 
 def JDFTXInfile_self_consistency_tester(jif: JDFTXInfile):
+    """Create an assortment of JDFTXinfile created from the same data but through different methods, and test that
+    they are all equivalent through "assert_idential_jif" """
     dict_jif = jif.as_dict()
     # # Commenting out tests with jif2 due to the list representation asserted
     jif2 = JDFTXInfile.get_dict_representation(JDFTXInfile._from_dict(dict_jif))
@@ -212,6 +246,7 @@ def JDFTXInfile_self_consistency_tester(jif: JDFTXInfile):
 
 
 def test_jdftxstructure():
+    """Test the JDFTXStructure object associated with the JDFTXInfile object"""
     jif = JDFTXInfile.from_file(ex_infile2_fname)
     struct = jif.to_jdftxstructure(jif)
     assert isinstance(struct, JDFTXStructure)
@@ -220,6 +255,7 @@ def test_jdftxstructure():
     assert struct.natoms == 16
     with open(ex_infile2_fname) as f:
         lines = list.copy(list(f))
+    # Test different ways of creating a JDFTXStructure object create the same object if data is the same
     data = "\n".join(lines)
     struct2 = JDFTXStructure.from_str(data)
     assert_equiv_jdftxstructure(struct, struct2)
