9ee5c465c875a56ae2bcdea35cb055e791def724
benrich37 <76569522+benrich37@users.noreply.github.com>
2025-01-24 12:18:05 -0700

    The value for a JOutStructures should be the most recent non-None value (some values will not be logged again for parsing if they are not updated at an optimization step)

--- src/pymatgen/io/jdftx/joutstructures.py
+++ src/pymatgen/io/jdftx/joutstructures.py
@@ -164,7 +164,12 @@ class JOutStructures:
             raise Warning("iter type interpreted as single-point calculation, but multiple structures found")
         self.t_s = self._get_t_s()
         for var in _joss_atrs_from_last_slice:
-            setattr(self, var, getattr(self.slices[-1], var))
+            for i in range(len(self.slices)):
+                val = getattr(self.slices[-i], var)
+                if val is not None:
+                    setattr(self, var, getattr(self.slices[-1], var))
+                    break
+                setattr(self, var, None)
         self._check_convergence()
 
     def _get_t_s(self) -> float | None:
