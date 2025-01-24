49705124f373d8fdc4ca49afe3ca85d7e86e1350
benrich37 <76569522+benrich37@users.noreply.github.com>
2025-01-24 12:46:24 -0700

    The value for a JDFTx output object containing slices should be the most recent non-None value (some values will not be logged again for parsing if they are not updated at an optimization step)

--- src/pymatgen/io/jdftx/outputs.py
+++ src/pymatgen/io/jdftx/outputs.py
@@ -564,7 +564,12 @@ class JDFTXOutfile:
     def __post_init__(self):
         if len(self.slices):
             for var in _jof_atr_from_last_slice:
-                setattr(self, var, getattr(self.slices[-1], var))
+                val = None
+                for i in range(len(self.slices)):
+                    val = getattr(self.slices[-i], var)
+                    if val is not None:
+                        break
+                setattr(self, var, val)
             self.trajectory = self._get_trajectory()
 
     def _get_trajectory(self) -> Trajectory | None:
