e3b7ab8557e67d4538161f54ac0a6899838c1b49
Shyue Ping Ong <shyuep@gmail.com>
2025-04-22 18:43:49 -0700

    Yield the tmpdir in case we need it.

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,5 +9,5 @@ from monty.tempfile import ScratchDir
 @pytest.fixture(autouse=True)
 def setup_teardown() -> typing.Generator:
     """Use ScratchDir for all tests in this session."""
-    with ScratchDir(".", copy_from_current_on_enter=False, copy_to_current_on_exit=False):
-        yield
+    with ScratchDir(".", copy_from_current_on_enter=False, copy_to_current_on_exit=False) as tmpdir:
+        yield tmpdir
