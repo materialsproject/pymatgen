1a00793758aa0d414ebf62095a0f24e9799db18f
Koki Muraoka <muraok_k@chemsys.t.u-tokyo.ac.jp>
2025-04-23 18:34:15 +0900

    Testing merge_sites with a structure with one site

--- tests/core/test_structure.py
+++ tests/core/test_structure.py
@@ -1557,6 +1557,10 @@ class TestStructure(PymatgenTest):
         assert len(navs2) == 12
         assert 51.5 in [itr.properties["prop1"] for itr in navs2]
 
+        cu = Structure(Lattice.from_parameters(2.545584, 2.545584, 2.545584, 60, 60, 60), species=["Cu"], coords=[[0, 0, 0]])
+        cu.merge_sites(mode="delete")
+        assert len(cu) == 1
+
     def test_properties(self):
         assert self.struct.num_sites == len(self.struct)
         self.struct.make_supercell(2)
