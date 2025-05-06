4fd2be8b8f7475945929f99d3f4df966ac85e1c0
Ben Rich <tiggzzss@gmail.com>
2025-05-05 18:30:39 -0600

    Giving consistent number of decimals for "d0"-"d2"

--- tests/io/jdftx/test_ion_tag.py
+++ tests/io/jdftx/test_ion_tag.py
@@ -90,30 +90,78 @@ def test_ion_reading(value_str: str, expected_dict: dict):
     assert_same_value(parsed_tag, expected_dict)
 
 
-# @pytest.mark.parametrize(
-#     ("value_str"),
-#     [
-#         ("Rhombohedral 1.0 1.0"),
-#         ("Triclinic 1.0 1.0 1.0 1.0 1.0 1.0"),
-#         ("Hexagonal 1.0 1.0"),
-#         ("Body-Centered Cubic 1.0"),
-#         ("Cubic 1.0"),
-#         ("Orthorhombic 1.0 1.0 1.0"),
-#         ("Base-Centered Orthorhombic 1.0 1.0 1.0"),
-#         ("Monoclinic 1.0 1.0 1.0 1.0"),
-#         ("Base-Centered Monoclinic 1.0 1.0 1.0 1.0"),
-#         ("Tetragonal 1.0 1.0"),
-#         ("Body-Centered Tetragonal 1.0 1.0"),
-#     ],
-# )
-# def test_lattice_writing(value_str: str):
-#     mft_lattice_tag = get_tag_object("lattice")
-#     assert mft_lattice_tag is not None
-#     i = mft_lattice_tag.get_format_index_for_str_value("lattice", value_str)
-#     tag_object = mft_lattice_tag.format_options[i]
-#     parsed_tag = tag_object.read("lattice", value_str)
-#     output = tag_object.write("lattice", parsed_tag)
-#     assert_same_value(
-#         ("lattice " + value_str).strip().split(),
-#         output.strip().split(),
-#     )
+@pytest.mark.parametrize(
+    ("expected_out", "stored_dict"),
+    [
+        (
+            "ion H 1.000000000000 1.000000000000 1.000000000000 0",
+            {
+                "species-id": "H",
+                "x0": 1.0,
+                "x1": 1.0,
+                "x2": 1.0,
+                "moveScale": 0,
+            },
+        ),
+        (
+            "ion H 1.000000000000 1.000000000000 1.000000000000 v 1.000000000000 1.000000000000 1.000000000000 0",
+            {
+                "species-id": "H",
+                "x0": 1.0,
+                "x1": 1.0,
+                "x2": 1.0,
+                "v": {
+                    "vx0": 1.0,
+                    "vx1": 1.0,
+                    "vx2": 1.0,
+                },
+                "moveScale": 0,
+            },
+        ),
+        (
+            "ion H 1.000000000000 1.000000000000 1.000000000000 1 Linear 1.000000000000 1.000000000000 1.000000000000",
+            {
+                "species-id": "H",
+                "x0": 1.0,
+                "x1": 1.0,
+                "x2": 1.0,
+                "moveScale": 1,
+                "constraint type": "Linear",
+                "d0": 1.0,
+                "d1": 1.0,
+                "d2": 1.0,
+            },
+        ),
+        (
+            "ion H 1.000000000000 1.000000000000 1.000000000000 1 HyperPlane 1.000000000000 1.000000000000 "
+            "1.000000000000 g1 HyperPlane 1.000000000000 1.000000000000 1.000000000000 g1",
+            {
+                "species-id": "H",
+                "x0": 1.0,
+                "x1": 1.0,
+                "x2": 1.0,
+                "moveScale": 1,
+                "HyperPlane": [
+                    {
+                        "d0": 1.0,
+                        "d1": 1.0,
+                        "d2": 1.0,
+                        "group": "g1",
+                    },
+                    {
+                        "d0": 1.0,
+                        "d1": 1.0,
+                        "d2": 1.0,
+                        "group": "g1",
+                    },
+                ],
+            },
+        ),
+    ],
+)
+def test_ion_writing(expected_out: str, stored_dict: dict):
+    ion_tag: MultiformatTag = get_tag_object("ion")
+    i, _ = ion_tag._determine_format_option("ion", stored_dict)
+    tag_object: TagContainer = ion_tag.format_options[i]
+    output = tag_object.write("ion", stored_dict)
+    assert_same_value(output.strip().split(), expected_out.strip().split())
