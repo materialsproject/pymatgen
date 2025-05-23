6b6737d2f984af0c89e7ab198d50214410063fa2
William Laderer <wtladerer@gmail.com>
2025-05-23 16:13:38 -0700

    more robust with warnings

--- src/pymatgen/io/common.py
+++ src/pymatgen/io/common.py
@@ -130,9 +130,12 @@ class VolumetricData(MSONable):
         return self.linear_add(other, 1.0)
 
     def __radd__(self, other):
-        if other == 0:
+        """Right-side addition for sum() and similar use cases."""
+        if other == 0 or other is None:
             return self
-        return self.__add__(other)
+        if isinstance(other, Dos):
+            return self.__add__(other)
+        raise TypeError(f"Unsupported operand type(s) for +: '{type(other).__name__}' and '{type(self).__name__}'")
 
     def __sub__(self, other):
         return self.linear_add(other, -1.0)
