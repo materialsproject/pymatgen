178fa74cec42bd0be3b81005cc4211dba0f53ad2
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2025-07-13 12:47:20 +0200

    try dependency cache

--- .github/workflows/_install_opt_unit_deps.sh
+++ .github/workflows/_install_opt_unit_deps.sh
@@ -2,28 +2,29 @@
 set -euo pipefail
 set -x
 
+# Use a custom bin directory for easier GitHub Actions caching
+BIN_DIR="$PWD/opt/bin"  && mkdir -p "$BIN_DIR"
+
 # Install BoltzTraP
-wget -O BoltzTraP.tar.bz2 https://owncloud.tuwien.ac.at/index.php/s/s2d55LYlZnioa3s/download
+wget --no-verbose -O BoltzTraP.tar.bz2 https://owncloud.tuwien.ac.at/index.php/s/s2d55LYlZnioa3s/download
 tar -jxf BoltzTraP.tar.bz2
-echo "$(realpath boltztrap-1.2.5/src)" >> "$GITHUB_PATH"
+cp boltztrap-1.2.5/src/{BoltzTraP,x_trans} "$BIN_DIR"
 
 # Install Vampire 5.0
-# TODO: https://github.com/richard-evans/vampire/issues/122
-wget --no-check-certificate https://vampire.york.ac.uk/resources/release-5/vampire-5.0-linux.tar.gz
-tar -zxf vampire-5.0-linux.tar.gz
-mv linux vampire-5.0
-echo "$(realpath vampire-5.0)" >> "$GITHUB_PATH"
+# TODO: Accepts self-signed cert (https://github.com/richard-evans/vampire/issues/122)
+wget --no-verbose --no-check-certificate https://vampire.york.ac.uk/resources/release-5/vampire-5.0-linux.tar.gz
+tar -zxf vampire-5.0-linux.tar.gz && mv linux vampire-5.0
+cp vampire-5.0/vampire-serial "$BIN_DIR"
 
 # Install Voro++ and ZEO++
-wget http://www.zeoplusplus.org/zeo++-0.3.tar.gz
+wget --no-verbose http://www.zeoplusplus.org/zeo++-0.3.tar.gz
 tar -xzf zeo++-0.3.tar.gz
-
 make -C zeo++-0.3/voro++/src -s CFLAGS="-w"
-echo "$(realpath zeo++-0.3/voro++/src)" >> "$GITHUB_PATH"
-
 make -C zeo++-0.3 -s CFLAGS="-w"
-echo "$(realpath zeo++-0.3)" >> "$GITHUB_PATH"
+cp zeo++-0.3/voro++/src/voro++ "$BIN_DIR"
+cp zeo++-0.3/network "$BIN_DIR"
 
-# TODO: Install mcsqs (from ATAT)
-# wget https://axelvandewalle.github.io/www-avdw/atat/atat3_50.tar.gz
-# tar -zxf atat3_50.tar.gz && cd atat
+# TODO: Install mcsqs
+
+# Add to path
+echo "$BIN_DIR" >> "$GITHUB_PATH"
