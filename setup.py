b04aacaa43954489508e0973723ecaf4b5fee77f
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2025-09-01 23:58:41 +0200

    use scipy implementation

--- setup.py
+++ setup.py
@@ -14,11 +14,6 @@ extra_link_args = ["-Wl,--allow-multiple-definition"] if is_win_64 else []
 setup(
     ext_modules=[
         Extension(
-            "pymatgen.optimization.linear_assignment",
-            ["src/pymatgen/optimization/linear_assignment.pyx"],
-            extra_link_args=extra_link_args,
-        ),
-        Extension(
             "pymatgen.util.coord_cython",
             ["src/pymatgen/util/coord_cython.pyx"],
             extra_link_args=extra_link_args,
