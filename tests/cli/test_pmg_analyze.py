b9eec5f4cd42febab2181765ad5e114ead18e325
Haoyu Yang <yanghaoyu97@outlook.com>
2026-08-01 12:46:31 +0200

    fix pmg analyze --mag All

--- tests/cli/test_pmg_analyze.py
+++ tests/cli/test_pmg_analyze.py
@@ -11,3 +11,13 @@ def test_pmg_analyze():
         ["analyze", f"{TEST_FILES_DIR}/io/vasp/fixtures/scan_relaxation"],
     )
     assert os.path.isfile("vasp_data.gz")
+
+
+def test_pmg_analyze_all_magnetizations(capsys):
+    assert (
+        pmg.main(
+            ["analyze", "--mag", "All", f"{TEST_FILES_DIR}/io/vasp/fixtures/scan_relaxation"],
+        )
+        == 0
+    )
+    assert "OUTCAR" in capsys.readouterr().out
